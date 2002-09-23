64c2f6caf5be3509fc83507d0b0036a51fdd80d8
Patrick Julien <freak@ideasandassociates.com>
2002-09-23 19:50:56 +0000

    New widget that will replace all the different choosers in krita/ui
    svn path=/trunk/koffice/; revision=179951

---
+++ krita/ui/kis_itemchooser.cc
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (c) 2002 Patrick Julein <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include <qhbox.h>
+#include <qlabel.h>
+#include <qlayout.h>
+#include <kinstance.h>
+#include <klocale.h>
+#include <kstandarddirs.h>
+#include <koIconChooser.h>
+#include "integerwidget.h"
+#include "kis_itemchooser.h"
+
+KisItemChooser::KisItemChooser(const vKoIconItem& items, QWidget *parent, const char *name) : super(parent, name)
+{
+	m_lbSpacing = new QLabel(i18n("Spacing:"), this);
+	m_slSpacing = new IntegerWidget( 1, 100, this, "int widget" );
+	m_slSpacing -> setTickmarks(QSlider::Below);
+	m_slSpacing -> setTickInterval(10);
+	QObject::connect(m_slSpacing, SIGNAL(valueChanged(int)), this, SLOT(slotSetBrushSpacing(int)));
+    	m_frame = new QHBox(this);
+	m_frame -> setFrameStyle(QFrame::Panel | QFrame::Sunken);
+	m_chooser = new KoIconChooser(QSize(30,30), m_frame, "icon_chooser");
+	QObject::connect(m_chooser, SIGNAL(selected(KoIconItem*)), this, SLOT(slotItemSelected(KoIconItem*)));
+	initGUI();
+
+	QPtrListIterator<KoIconItem> itr(items);
+
+	for (itr.toFirst(); itr.current(); ++itr)
+		m_chooser -> addItem(itr.current());
+}
+
+KisItemChooser::~KisItemChooser()
+{
+}
+
+void KisItemChooser::initGUI()
+{
+	QVBoxLayout *mainLayout = new QVBoxLayout(this, 2, -1, "main layout");
+	QHBoxLayout *spacingLayout = new QHBoxLayout( -1, "spacing layout" );
+
+	mainLayout -> addWidget(m_frame, 10);
+	mainLayout -> addLayout(spacingLayout, 1);
+	spacingLayout -> addWidget(m_lbSpacing, 0);
+	spacingLayout -> addStretch();
+	spacingLayout -> addWidget(m_slSpacing, 1);
+}
+
+void KisItemChooser::setCurrent(KoIconItem *item)
+{
+	m_chooser -> setCurrentItem(item);
+//	m_slSpacing -> setValue( brush->spacing() );
+}
+
+KoIconItem* KisItemChooser::currentItem()
+{
+	return m_chooser -> currentItem();
+}
+
+void KisItemChooser::slotItemSelected(KoIconItem *item)
+{
+//	m_slSpacing->setValue( brush->spacing() );
+	emit selected(item);
+}
+
+void KisItemChooser::slotSetItemSpacing(int )
+{
+#if 0
+	KisBrush *brush = (KisBrush *) currentBrush();
+
+	if ( brush )
+		brush->setSpacing(spacing);
+#endif
+}
+
+void KisItemChooser::addItem(KoIconItem *item)
+{
+	m_chooser -> addItem(item);
+}
+
+void KisItemChooser::addItem(const vKoIconItem& items)
+{
+	QPtrListIterator<KoIconItem> itr(items);
+
+	for (itr.toFirst(); itr.current(); ++itr)
+		m_chooser -> addItem(itr.current());
+}
+
---
+++ krita/ui/kis_itemchooser.h
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2002 Patrick Julein <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#if !defined KIS_BRUSH_CHOOSER_H_
+#define KIS_BRUSH_CHOOSER_H_
+
+#include <qptrlist.h>
+#include <qwidget.h>
+#include <qframe.h>
+#include <koIconChooser.h>
+#include "kfloatingdialog.h"
+
+class QHBox;
+class QLabel;
+class KoIconChooser;
+class KoIconItem;
+class IntegerWidget;
+
+typedef QPtrList<KoIconItem> vKoIconItem;
+
+class KisItemChooser : public KFloatingDialog {
+	typedef KFloatingDialog super;
+	Q_OBJECT
+
+public:
+	KisItemChooser(const vKoIconItem& items, QWidget *parent, const char *name = 0);
+	virtual ~KisItemChooser();
+
+	KoIconItem *currentItem();
+	void setCurrent(KoIconItem *item);
+	void addItem(KoIconItem *item);
+	void addItem(const vKoIconItem& items);
+
+signals:
+	void selected(KoIconItem *item);
+
+private:
+	void initGUI();
+
+private slots:
+	void slotItemSelected(KoIconItem *item);
+	void slotSetItemSpacing(int spacing);
+
+private:
+	QHBox *m_frame;
+	QWidget *m_container;
+	QLabel *m_lbSpacing;
+	IntegerWidget *m_slSpacing;
+	KoIconChooser *m_chooser;
+};
+
+#endif // KIS_BRUSH_CHOOSER_H_
+

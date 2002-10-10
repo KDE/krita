57f6718de4a0eeddfc576073011a2aa85fd1094f
Andy Fawcett <andy@athame.co.uk>
2002-10-10 15:28:37 +0000

    include "kis_tool_memento.h" to prevent forward declaration.
    svn path=/trunk/koffice/; revision=183121

--- krita/tools/kis_tool_non_paint.cc
+++ krita/tools/kis_tool_non_paint.cc
@@ -15,6 +15,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+#include "kis_tool_memento.h"
 #include "kis_tool_non_paint.h"
 #include "kis_view.h"
 

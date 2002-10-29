ea67b5b4098e9c6369c3c398b3cc91d9a524ff3d
David Faure <faure@kde.org>
2002-10-29 16:47:54 +0000

    KoApplication::addCmdLineOptions();
    svn path=/trunk/koffice/; revision=186403

--- krita/main.cc
+++ krita/main.cc
@@ -32,6 +32,7 @@ int main(int argc, char **argv)
 {
 	KCmdLineArgs::init(argc, argv, newKrayonAboutData());
 	KCmdLineArgs::addCmdLineOptions(options);
+	KoApplication::addCmdLineOptions();
 
 	KoApplication app;
 

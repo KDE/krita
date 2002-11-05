d1de18c325d88b7a772da15caae1ce737aa3491a
Patrick Julien <freak@ideasandassociates.com>
2002-11-05 16:36:35 +0000

    added missing files
    svn path=/trunk/koffice/; revision=187529

---
+++ krita/core/kis_command.cc
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 1999 Michael Koch    <koch@kde.org>
+ *  Copyright (c) 2002 Patrick Julien <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <kcommand.h>
+#include <klocale.h>
+#include "kis_command.h"
+
+KisCommand::KisCommand(KisDoc *doc)
+{
+	m_name = "";
+	m_doc = doc;
+}
+
+KisCommand::KisCommand(const QString& name, KisDoc *doc)
+{
+	m_name = name;
+	m_doc = doc;
+}
+
+KisCommand::~KisCommand()
+{
+}
+
+QString KisCommand::name() const
+{
+	return m_name;
+}
+
---
+++ krita/core/kis_command.h
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 1999 Michael Koch    <koch@kde.org>
+ *  Copyright (c) 2002 Patrick Julien <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#if !defined KIS_COMMAND_H_
+#define KIS_COMMAND_H_
+
+#include <qstring.h>
+#include <kcommand.h>
+
+class KisDoc;
+
+class KisCommand : public KCommand {
+public:
+	KisCommand(KisDoc *doc);
+	KisCommand(const QString& name, KisDoc *doc);
+	virtual ~KisCommand();
+
+	virtual void execute() = 0;
+	virtual void unexecute() = 0;
+	virtual QString name() const;
+
+protected:
+	KisDoc *m_doc;
+	QString m_name;
+};
+
+#endif // KIS_COMMAND_H_
+
+

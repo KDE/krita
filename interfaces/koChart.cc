46beddc2015ecca8e348b1fbc4244ee4332a828a
David Faure <faure@kde.org>
2002-12-16 09:02:12 +0000

    Made LGPL after asking Kalle, so that kspread is indeed LGPL.
    svn path=/trunk/koffice/; revision=194211

--- interfaces/koChart.cc
+++ interfaces/koChart.cc
@@ -1,3 +1,20 @@
+/* This file is part of the KDE project
+   Copyright (C) 2000-2002 Kalle Dalheimer <kalle@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #include "koChart.h"
 

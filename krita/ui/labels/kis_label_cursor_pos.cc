0cc7dc396b08c1b1dbb19b5e22ddfa66f607cd45
Patrick Julien <freak@ideasandassociates.com>
2003-01-10 12:47:12 +0000

    added missing files
    svn path=/trunk/koffice/; revision=198741

---
+++ krita/ui/labels/kis_label_cursor_pos.cc
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2003 Patrick Julien  <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.g
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include "kis_label_cursor_pos.h"
+
+KisLabelCursorPos::KisLabelCursorPos(QWidget *parent, const char *name, WFlags f) : super(parent, name, f)
+{
+	setText("0:0");
+	m_doUpdates = true;
+}
+
+KisLabelCursorPos::~KisLabelCursorPos()
+{
+}
+
+void KisLabelCursorPos::updatePos(Q_INT32 xpos, Q_INT32 ypos)
+{
+	if (m_doUpdates) {
+		QString s;
+
+		s.sprintf("%d:%d", xpos, ypos);
+		setText(s);
+	}
+}
+
+void KisLabelCursorPos::enter()
+{
+	m_doUpdates = true;
+}
+
+void KisLabelCursorPos::leave()
+{
+	m_doUpdates = false;
+	setText(QString::null);
+}
+
---
+++ krita/ui/labels/kis_label_cursor_pos.h
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2003 Patrick Julien  <freak@codepimps.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.g
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#if !defined KIS_LABEL_CURSOR_POS_H_
+#define KIS_LABEL_CURSOR_POS_H_
+
+#include <qlabel.h>
+
+class KisLabelCursorPos : public QLabel {
+	Q_OBJECT
+	typedef QLabel super;
+
+public:
+	KisLabelCursorPos(QWidget *parent, const char *name = 0, WFlags f = 0);
+	virtual ~KisLabelCursorPos();
+
+public slots:
+	void updatePos(Q_INT32 xpos, Q_INT32 ypos);
+	void enter();
+	void leave();
+
+private:
+	bool m_doUpdates;
+	Q_INT32 m_ypos;
+};
+
+#endif // KIS_LABEL_CURSOR_POS_H_
+

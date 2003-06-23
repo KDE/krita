5a02b21e6d237fa84666b60265c6a40d10068ead
Laurent Montel <montel@kde.org>
2003-06-23 11:16:54 +0000

    includemoc
    svn path=/trunk/koffice/; revision=233207

--- krita/ui/labels/kis_label_cursor_pos.cc
+++ krita/ui/labels/kis_label_cursor_pos.cc
@@ -16,6 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #include "kis_label_cursor_pos.h"
+#include "kis_label_cursor_pos.moc"
 
 KisLabelCursorPos::KisLabelCursorPos(QWidget *parent, const char *name, WFlags f) : super(parent, name, f)
 {

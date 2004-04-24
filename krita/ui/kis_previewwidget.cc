8444d921446aabf837aedb95d9a0073665803372
Bart Coppens <kde@bartcoppens.be>
2004-04-24 14:34:30 +0000

    Implementation of a basic preview widget. Supports zooming (buggy), moving of the preview and applying effects to the preview.
    svn path=/trunk/koffice/; revision=306066

--- krita/ui/kis_previewwidget.cc
+++ krita/ui/kis_previewwidget.cc
@@ -1,7 +1,7 @@
 /*
- *  kis_previewwidget.cc - part of Krayon
+ *  kis_previewwidget.cc - part of Krita
  *
- *  Copyright (c) 2001 John Califf  <jwcaliff@compuzone.net>
+ *  Copyright (c) 2004 Bart Coppens <kde@bartcoppens.be>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -11,7 +11,7 @@
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.g
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
@@ -19,38 +19,196 @@
  */
 
 #include <qpainter.h>
+#include <qpoint.h>
+#include <qpushbutton.h>
+#include <qlayout.h>
+#include <qlabel.h>
 
+#include <koColor.h>
+
+#include "kis_undo_adapter.h"
+#include "kis_global.h"
 #include "kis_layer.h"
-#include "kis_previewwidget.h"
+#include "kis_image.h"
+#include "kis_painter.h"
+#include "kis_types.h"
+#include "color_strategy/kis_colorspace_factory.h"
+#include "color_strategy/kis_strategy_colorspace.h"
 
-#define ITEMSIZE 34
+#include "kis_previewwidget.h"
+#include "kis_previewwidgetbase.h"
+
+/*FIXME make this unnecessary */
+#define SIZE 150
 
 KisPreviewWidget::KisPreviewWidget( QWidget* parent, const char* name )
-    : QFrame( parent, name )
+    : PreviewWidgetBase( parent, name ), m_undo(0) /* seems to work nicely */
 {
-    setBackgroundColor( white );
-    setFrameStyle( Panel | Sunken );
-    // pix.resize(ITEMSIZE, ITEMSIZE);
+	connect(pushButton1/*plus*/, SIGNAL(clicked()), this, SLOT(zoomIn()));
+	connect(pushButton2/*minus*/, SIGNAL(clicked()), this, SLOT(zoomOut()));
+	m_pos = QPoint(0,0);
+	m_moved = false;
+	m_zoom = 1;
 }
 
-void KisPreviewWidget::slotSetPreview(KisLayer * /*lay*/)
+void KisPreviewWidget::slotSetPreview(KisLayerSP lay)
 {
-    // scale passed in layer here
-    repaint();
+	m_layer = lay;
+	KisPainter gc;
+	KisPaintDeviceSP pd(m_layer.data());
+
+	/* left original image */
+	KisImageSP img1;
+	img1 = new KisImage(m_undo, SIZE, SIZE, lay->type(), "previewOriginal");
+	layerNew1 = new KisLayer(img1, SIZE, SIZE, img1 -> nextLayerName(), OPACITY_OPAQUE);
+	gc.begin(layerNew1.data());
+	
+	gc.bitBlt(0, 0, COMPOSITE_OVER, pd);
+	gc.end();
+	img1 -> add(layerNew1, -1);
+	m_original = img1;
+
+	/* right preview image */
+	KisImageSP img2;
+	img2 = new KisImage(m_undo, SIZE, SIZE, lay->type(), "previewPreview");
+	layerNew2 = new KisLayer(img2, SIZE, SIZE, img2 -> nextLayerName(), OPACITY_OPAQUE);
+	gc.begin(layerNew2.data());
+	gc.bitBlt(0, 0, COMPOSITE_OVER, pd);
+	gc.end();
+	img2 -> add(layerNew2, -1);
+	m_preview = img2;
+
+	repaint();
+	emit updated();
 }
 
-void KisPreviewWidget::drawContents ( QPainter */*p*/ )
-{
-    //int x = 0;
-    //int y = 0;
-
-    // p->drawPixmap(x, y, pix);
+void KisPreviewWidget::zoomIn() {
+	if (m_zoom * 1.5 < 8) {
+		m_zoom *= 1.5;
+		m_startDrag *= 1.5;
+		updateWidgets(m_startDrag);
+		emit updated();
+	}
 }
 
-void KisPreviewWidget::mousePressEvent ( QMouseEvent * )
+void KisPreviewWidget::zoomOut() {
+	if (m_zoom / 1.5 > 1/8) {
+		m_zoom /= 1.5;
+		m_startDrag /= 1.5;
+		updateWidgets(m_startDrag);
+		emit updated();
+	}
+}
+
+void KisPreviewWidget::slotRenewLayer() {
+	updateWidgets(m_startDrag);
+}
+
+KisLayerSP KisPreviewWidget::getLayer()
 {
-    emit clicked();
-    // take snapshot of current layer
+	return layerNew2;
+}
+
+void KisPreviewWidget::slotUpdate()
+{
+	m_preview->invalidate();
+	repaint();
+}
+
+void KisPreviewWidget::paintEvent(QPaintEvent*)
+{
+	QPainter paintOriginal(m_originalWidget);
+	render(paintOriginal, m_original, m_zoom, m_zoom);
+	QPainter paintPreview(m_previewWidget);
+	render(paintPreview, m_preview, m_zoom, m_zoom);
+}
+
+void KisPreviewWidget::mousePressEvent(QMouseEvent * e)
+{
+	if(!m_moved) {
+		m_startDrag = e->pos();
+		m_moved = true;
+	} else {
+		m_startDrag = e->pos() - m_startDrag;
+	}
+}
+
+void KisPreviewWidget::mouseMoveEvent(QMouseEvent * e)
+{
+	if (! m_original || ! m_preview)
+		return;
+	QPoint delta = (e->pos() - m_startDrag); // m_zoom;
+	if (delta.x() > 0)
+		delta.rx() = 0;
+	if (delta.y() > 0)
+		delta.ry() = 0;
+	updateWidgets(delta);
+	repaint();
+}
+
+void KisPreviewWidget::mouseReleaseEvent(QMouseEvent * e)
+{
+	mouseMoveEvent(e);
+	emit updated();
+	m_startDrag = e->pos() - m_startDrag;
+	if (m_startDrag.x() > 0)
+		m_startDrag.rx() = 0;
+	if (m_startDrag.y() > 0)
+		m_startDrag.ry() = 0;
+}
+
+void KisPreviewWidget::updateWidgets(QPoint delta)
+{
+	KisPainter gc;
+	KisPaintDeviceSP pd(m_layer.data());
+
+	//delta *= m_zoom;
+	/* left */
+	gc.begin(layerNew1.data());
+	gc.bitBlt(0, 0, COMPOSITE_OVER, pd, -delta.x(), -delta.y(), -1, -1);
+	gc.end();
+	m_original->invalidate();
+
+	/* right */
+	gc.begin(layerNew2.data());
+	gc.bitBlt(0, 0, COMPOSITE_OVER, pd, -delta.x(), -delta.y(), -1, -1);
+	gc.end();
+	m_preview->invalidate();
+}
+
+void KisPreviewWidget::render(QPainter &painter, KisImageSP image, double zoomX, double zoomY)
+{
+	Q_INT32 x1 = 0;
+	Q_INT32 y1 = 0;
+	Q_INT32 x2 = image -> width();
+	Q_INT32 y2 = image -> height();
+	Q_INT32 tileno;
+	KisColorSpaceFactoryInterface *factory = KisColorSpaceFactoryInterface::singleton();
+
+	if (!image)
+		return;
+
+	KisStrategyColorSpaceSP colorstate = factory->create(image->nativeImgType());
+		
+	if (zoomX != 1.0 || zoomY != 1.0)
+		painter.scale(zoomX, zoomY);
+
+	for (Q_INT32 y = y1; y <= y2; y += TILE_HEIGHT - (y % TILE_HEIGHT)) {
+		for (Q_INT32 x = x1; x <= x2; x += TILE_WIDTH - (x % TILE_WIDTH)) {
+			if ((tileno = image -> tileNum(x, y)) < 0)
+				continue;
+
+			image -> validate(tileno);
+		}
+	}
+
+	for (Q_INT32 y = y1; y < y2; y += RENDER_HEIGHT)
+		for (Q_INT32 x = x1; x < x2; x += RENDER_WIDTH)
+			colorstate -> render(	image,
+						painter,
+						x, y,
+						QMIN(x2 - x, RENDER_WIDTH),
+						QMIN(y2 - y, RENDER_HEIGHT));
 }
 
 #include "kis_previewwidget.moc"
--- krita/ui/kis_previewwidget.h
+++ krita/ui/kis_previewwidget.h
@@ -1,7 +1,8 @@
 /*
- *  kis_previewwidget.h - part of Krayon
+ *  kis_previewwidget.h - part of Krita
  *
  *  Copyright (c) 2001 John Califf  <jcaliff@compuzone.net>
+ *  Copyright (c) 2004 Bart Coppens <kde@bartcoppens.be>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -21,29 +22,73 @@
 #ifndef __kis_previewwidget_h__
 #define __kis_previewwidget_h__
 
-#include <qframe.h>
+#include <qwidget.h>
 #include <qpixmap.h>
-#include "kis_layer.h"
 
-class KisPreviewWidget : public QFrame
+#include "kis_types.h"
+//class PreviewWidgetBase;
+#include "kis_previewwidgetbase.h"
+class KisUndoAdapter;
+
+/**
+ * A widget that can be used by plugins to show a preview of the effect of the
+ * plugin to the user. It would be nice if every plugin that needs to show a preview
+ * (maybe not those that create a new image) would use this. This can prevent the distracting
+ * effect the GIMP has with a different preview for almost every filter.
+ */
+class KisPreviewWidget : public PreviewWidgetBase
 {
   Q_OBJECT
 
  public:
+    /** Constructs the widget */
     KisPreviewWidget( QWidget* parent = 0, const char* name = 0 );
+    /** @return the layer, so the dialog can apply it's effect on it */
+    KisLayerSP getLayer();
+    /**
+     * returns the zoom factor. This could be useful if the filter has to rely on
+     * the whole layer. With this and getPos(), there is enough information to
+     * paint the preview from a source different from the layer in @see getLayer */
+    double getZoom() { return m_zoom; }
+    /** returns the 'vector' the image in the preview has been moved by. @see getZoom */
+    QPoint getPos() { return m_startDrag; }
+    
 
  public slots:
-    void slotSetPreview(KisLayer * lay);
+    /** Sets the preview to use the layer specified as argument */
+    void slotSetPreview(KisLayerSP lay);
+    /** 
+     * This should be called at the beginning of the effect. This ensures that 
+     * the layer in the preview widget is in the right state. */
+    void slotRenewLayer();
+    /**
+     * Call this when the effect has finished updating the layer. Makes the preview
+     * repaint itself. */
+    void slotUpdate();
 
  signals:
-    void clicked();
+    /** This is emitted when the position or zoom factor of the widget has changed */
+    void updated();
   
  protected:
-    virtual void drawContents ( QPainter * );
-    virtual void mousePressEvent ( QMouseEvent * );
+    virtual void paintEvent(QPaintEvent*);
+    virtual void mousePressEvent(QMouseEvent * e);
+    virtual void mouseMoveEvent(QMouseEvent * e);
+    virtual void mouseReleaseEvent(QMouseEvent * e);
 
  private:
-   QPixmap pix; 
+   void render(QPainter &painter, KisImageSP image, double zoomX, double zoomY);
+   void updateWidgets(QPoint delta);
+   KisLayerSP m_layer;
+   KisImageSP m_original, m_preview;
+   QPoint m_startDrag, m_pos;
+   KisUndoAdapter* m_undo;
+   KisLayerSP layerNew1 /*original image*/, layerNew2/*preview image*/;
+   bool m_moved;
+   double m_zoom;
+ private slots:
+   void zoomIn();
+   void zoomOut();
 };
 
 #endif

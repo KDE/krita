6e97d688b776e5f5293077ad38e524377ece073b
Boudewijn Rempt <boud@valdyas.org>
2004-05-29 21:54:30 +0000

    Actually added the screenmode plugin...
    svn path=/trunk/koffice/; revision=315957

---
+++ krita/plugins/screenmode/screenmode.h
@@ -0,0 +1,48 @@
+/* 
+ * screenmode.h -- Part of Krita
+ *
+ * Copyright (c) 2004 Boudewijn Rempt (boud@valdyas.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef SCREENMODE_H
+#define SCREENMODE_H
+
+#include <kparts/plugin.h>
+
+class KisView;
+
+class ScreenMode : public KParts::Plugin
+{
+	Q_OBJECT
+public:
+	ScreenMode(QObject *parent, const char *name, const QStringList &);
+	virtual ~ScreenMode();
+	
+private slots:
+
+	void slotFullScreen();
+	void slotFullScreenMenu();
+	void slotNormal();
+
+private:
+	KisView * m_view;
+	KisPainter * m_painter;
+	
+};
+
+#endif // SCREENMODE_H

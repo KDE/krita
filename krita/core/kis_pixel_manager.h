8b3ceca06567ade75fd41d47916115186cc8a2a8
Boudewijn Rempt <boud@valdyas.org>
2004-08-31 17:01:48 +0000

    Oops.
    svn path=/trunk/koffice/; revision=343001

---
+++ krita/core/kis_pixel_manager.h
@@ -0,0 +1,25 @@
+/*
+ *  copyright (c) 2004 Boudewijn Rempt <boud@valdyas.org
+ *
+ *  this program is free software; you can redistribute it and/or modify
+ *  it under the terms of the gnu general public license as published by
+ *  the free software foundation; either version 2 of the license, or
+ *  (at your option) any later version.
+ *
+ *  this program is distributed in the hope that it will be useful,
+ *  but without any warranty; without even the implied warranty of
+ *  merchantability or fitness for a particular purpose.  see the
+ *  gnu general public license for more details.
+ *
+ *  you should have received a copy of the gnu general public license
+ *  along with this program; if not, write to the free software
+ *  foundation, inc., 675 mass ave, cambridge, ma 02139, usa.
+ */
+class KisPixelManagerInterface {
+
+public:
+
+	KisPixelManagerInterface(){;}
+	virtual ~KisPixelManagerInterface(){;}
+
+};

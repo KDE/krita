0e4903c68ebcd8e4b0bd9e89f2c929527161d08d
Andrew Coles <andrew.i.coles@googlemail.com>
2004-09-03 14:12:29 +0000

    CVS_SILENT
    Corrected typo.
    
    svn path=/trunk/koffice/; revision=343729

--- krita/tools/kis_tool_gradient.cc
+++ krita/tools/kis_tool_gradient.cc
@@ -268,7 +268,7 @@ QWidget* KisToolGradient::createOptionWidget(QWidget* parent)
 	m_cmbShape -> insertItem(i18n("Radial"));
 	m_cmbShape -> insertItem(i18n("Square"));
 	m_cmbShape -> insertItem(i18n("Conical"));
-	m_cmbShape -> insertItem(i18n("Conical Symetric"));
+	m_cmbShape -> insertItem(i18n("Conical Symmetric"));
 
 	m_cmbRepeat = new QComboBox(false, m_optWidget, "repeat_combo");
 	connect(m_cmbRepeat, SIGNAL(activated(int)), this, SLOT(slotSetRepeat(int)));

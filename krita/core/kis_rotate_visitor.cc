8d699f089b196750504aa7e5610509634405b195
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2004-09-24 18:52:00 +0000

    Added kis_rotate_visitor.h and kis_rotate_visitor.cc
    svn path=/trunk/koffice/; revision=349120

---
+++ krita/core/kis_rotate_visitor.cc
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2004 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include <qwmatrix.h>
+
+#include <kdebug.h>
+#include <klocale.h>
+
+#include "kis_paint_device.h"
+#include "kis_rotate_visitor.h"
+#include "kis_progress_display_interface.h"
+
+void KisRotateVisitor::rotate(double angle, KisProgressDisplayInterface *m_progress) 
+{
+        kdDebug() << "Rotating Code called! Going to rotate image by (angle): " << angle << "\n";
+}
---
+++ krita/core/kis_rotate_visitor.h
@@ -0,0 +1,58 @@
+/*
+ *  copyright (c) 2004 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  this program is free software; you can redistribute it and/or modify
+ *  it under the terms of the gnu general public license as published by
+ *  the free software foundation; either version 2 of the license, or
+ *  (at your option) any later version.
+ *
+ *  this program is distributed in the hope that it will be useful,
+ *  but without any warranty; without even the implied warranty of
+ *  merchantability or fitness for a particular purpose.  see the
+ *  gnu general public license for more details.
+ *
+ *  you should have received a copy of the gnu general public license
+ *  along with this program; if not, write to the free software
+ *  foundation, inc., 675 mass ave, cambridge, ma 02139, usa.
+ */
+#ifndef KIS_ROTATE_VISITOR_H_
+#define KIS_ROTATE_VISITOR_H_
+
+#include "kis_types.h"
+#include "kis_progress_subject.h"
+
+class KisPaintDevice;
+class KisProgressDisplayInterface;
+
+class KisRotateVisitor : public KisProgressSubject {
+        typedef KisProgressSubject super;  
+        
+        /* Structs for the image rescaling routine */
+	
+public:
+        KisRotateVisitor();
+        ~KisRotateVisitor();
+        void visitKisPaintDevice(KisPaintDevice* dev);
+        void rotate(double angle, KisProgressDisplayInterface *m_progress);
+private:
+        KisPaintDevice* m_dev;
+        
+	// Implement KisProgressSubject
+	bool m_cancelRequested;
+        virtual void cancel() { m_cancelRequested = true; }
+
+};
+
+inline KisRotateVisitor::KisRotateVisitor()
+{
+}
+
+inline KisRotateVisitor::~KisRotateVisitor()
+{
+}
+
+inline void KisRotateVisitor::visitKisPaintDevice(KisPaintDevice* dev)
+{
+        m_dev=dev;
+}
+#endif // KIS_ROTATE_VISITOR_H_

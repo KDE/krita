e2efa7b7c5477cf5975e8664c87151be27a9870e
Casper Boemann <cbr@boemann.dk>
2005-02-23 22:09:03 +0000

    fix invert filter
    svn path=/trunk/koffice/; revision=392692

--- krita/plugins/example/example.cc
+++ krita/plugins/example/example.cc
@@ -84,7 +84,7 @@ void KisFilterInvert::process(KisPaintDeviceSP src, KisPaintDeviceSP dst, KisFil
 {
 	KisRectIteratorPixel dstIt = dst->createRectIterator(rect.x(), rect.y(), rect.width(), rect.height(), true );
 	KisRectIteratorPixel srcIt = src->createRectIterator(rect.x(), rect.y(), rect.width(), rect.height(), false);
-	Q_INT32 depth = src -> colorStrategy() -> nColorChannels() - 1;
+	Q_INT32 depth = src -> colorStrategy() -> nColorChannels();
 	
 	while( ! srcIt.isDone() )
 	{

7b551f0575ca9bced9f25f781e4a124592fe24de
Casper Boemann <cbr@boemann.dk>
2005-03-28 21:38:48 +0000

    copyright changes
    svn path=/trunk/koffice/; revision=401418

--- krita/plugins/tool_transform/kis_tool_transform.cc
+++ krita/plugins/tool_transform/kis_tool_transform.cc
@@ -2,6 +2,7 @@
  *  kis_tool_transform.cc -- part of Krita
  *
  *  Copyright (c) 2004 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2005 Boudewijn Rempt <boud@valdyas.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

d8a4cb2ad85461f8bcef4306b30022707b7b83d0
Adrian Page <adrian@pagenet.plus.com>
2005-06-08 22:36:16 +0000

    Make the pen tool paint hard strokes only with no soft edges.
    BUG: 107066
    
    svn path=/branches/koffice/1.4/koffice/; revision=423582

--- krita/paintops/kis_penop.cc
+++ krita/paintops/kis_penop.cc
@@ -31,6 +31,8 @@
 #include "kis_painter.h"
 #include "kis_types.h"
 #include "kis_paintop.h"
+#include "kis_iterator.h"
+#include "kis_iterators_pixel.h"
 
 #include "kis_penop.h"
 
@@ -88,7 +90,7 @@ void KisPenOp::paintAt(const KisPoint &pos,
 		KisAlphaMaskSP mask = brush -> mask(pressure);
 		dab = computeDab(mask);
 	}
-	
+
 	m_painter -> setPressure(pressure);
 	QRect dabRect = QRect(0, 0, brush -> maskWidth(pressure), brush -> maskHeight(pressure));
 	QRect dstRect = QRect(x, y, dabRect.width(), dabRect.height());
@@ -101,13 +103,31 @@ void KisPenOp::paintAt(const KisPoint &pos,
 
 	if (dstRect.isNull() || dstRect.isEmpty() || !dstRect.isValid()) return;
 
+	if (dab -> alpha()) {
+		// Set all alpha > opaque/2 to opaque, the rest to transparent.
+		// XXX: Using 4/10 as the 1x1 circle brush paints nothing with 0.5.
+
+		KisRectIterator pixelIt = dab -> createRectIterator(dabRect.x(), dabRect.y(), dabRect.width(), dabRect.height(), true);
+
+		while (!pixelIt.isDone()) {
+
+			KisPixel pixel = dab -> toPixel(pixelIt.rawData());
+
+			if (pixel.alpha() < (4 * OPACITY_OPAQUE) / 10) {
+				pixel.alpha() = OPACITY_TRANSPARENT;
+			} else {
+				pixel.alpha() = OPACITY_OPAQUE;
+			}
+
+			++pixelIt;
+		}
+	}
+
 	Q_INT32 sx = dstRect.x() - x;
 	Q_INT32 sy = dstRect.y() - y;
 	Q_INT32 sw = dstRect.width();
 	Q_INT32 sh = dstRect.height();
 
-	// XXX: Set all alpha > opaque/2 to opaque, the rest to transparent
-	
 	m_painter -> bltSelection(dstRect.x(), dstRect.y(), m_painter -> compositeOp(), dab.data(), m_painter -> opacity(), sx, sy, sw, sh);
 	m_painter -> addDirtyRect(dstRect);
 }

48c53126ca0f293037695e94c4f3e88b50a81f10
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2005-07-27 20:59:17 +0000

    Initial checkin of a new filter that creates an image by compositing tiles with small versions of the original image. It just hangs for now, so don't use it.
    svn path=/trunk/koffice/; revision=439345

---
+++ krita/plugins/smalltilesfilter/kis_small_tiles_filter.cc
@@ -0,0 +1,112 @@
+/*
+ * This file is part of Krita
+ *
+ * Copyright (c) 2005 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ * ported from Gimp, Copyright (C) 1997 Eiichi Takamori <taka@ma1.seikyou.ne.jp>
+ * original pixelize.c for GIMP 0.54 by Tracy Scott
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <stdlib.h>
+#include <vector>
+
+#include <qpoint.h>
+#include <qspinbox.h>
+
+#include <klocale.h>
+#include <kiconloader.h>
+#include <kinstance.h>
+#include <kmessagebox.h>
+#include <kstandarddirs.h>
+#include <ktempfile.h>
+#include <kdebug.h>
+#include <kgenericfactory.h>
+#include <knuminput.h>
+
+#include <kis_doc.h>
+#include <kis_image.h>
+#include <kis_iterators_pixel.h>
+#include <kis_layer.h>
+#include <kis_filter_registry.h>
+#include <kis_global.h>
+#include <kis_types.h>
+#include <kis_progress_display_interface.h>
+#include <kis_paint_device.h>
+#include <kis_filter_strategy.h>
+
+#include "kis_multi_integer_filter_widget.h"
+#include "kis_small_tiles_filter.h"
+
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+KisSmallTilesFilter::KisSmallTilesFilter() : KisFilter(id(), "artistic", "&Small tiles...")
+{
+}
+
+void KisSmallTilesFilter::process(KisPaintDeviceSP src, KisPaintDeviceSP dst, KisFilterConfiguration* configuration, const QRect& rect)
+{
+        Q_INT32 x = rect.x(), y = rect.y();
+        Q_INT32 width = rect.width();
+        Q_INT32 height = rect.height();
+        
+        //read the filter configuration values from the KisFilterConfiguration object
+        Q_UINT32 numberOfTiles = ((KisSmallTilesFilterConfiguration*)configuration)->numberOfTiles();
+        
+        createSmallTiles(src, dst, rect, numberOfTiles);
+}
+
+void KisSmallTilesFilter::createSmallTiles(KisPaintDeviceSP src, KisPaintDeviceSP dst, const QRect& rect, Q_UINT32 numberOfTiles)
+{
+	KisPaintDeviceSP tmp = new KisPaintDevice( *(src.data()) );
+	tmp -> scale( rect.width() / numberOfTiles, rect.height() / numberOfTiles, 0, new KisMitchellFilterStrategy() );
+	
+	KisRectIteratorPixel dstIt = dst->createRectIterator(rect.x(), rect.y(), rect.width(), rect.height(), true );
+	KisRectIteratorPixel tmpIt = tmp->createRectIterator(rect.x(), rect.y(), rect.width(), rect.height(), false);
+	Q_INT32 depth = tmp -> colorStrategy() -> nColorChannels();
+
+	while( ! tmpIt.isDone() )
+	{
+		if(tmpIt.isSelected())
+		{
+			for( int i = 0; i < depth; i++)
+			{
+				dstIt.rawData()[i] = tmpIt.oldRawData()[i];
+			}
+		}
+		++tmpIt;
+		++dstIt;
+	}
+}
+
+KisFilterConfigWidget * KisSmallTilesFilter::createConfigurationWidget(QWidget* parent, KisPaintDeviceSP dev)
+{
+	vKisIntegerWidgetParam param;
+	param.push_back( KisIntegerWidgetParam( 2, 5, 1, i18n("Number of Tiles") ) );
+	return new KisMultiIntegerFilterWidget(parent, id().id().ascii(), id().id().ascii(), param );
+}
+
+KisFilterConfiguration* KisSmallTilesFilter::configuration(QWidget* nwidget, KisPaintDeviceSP dev)
+{
+	KisMultiIntegerFilterWidget* widget = (KisMultiIntegerFilterWidget*) nwidget;
+	if( widget == 0 )
+	{
+		return new KisSmallTilesFilterConfiguration( 2 );
+	} else {
+		return new KisSmallTilesFilterConfiguration( widget->valueAt( 0 ) );
+	}
+}
---
+++ krita/plugins/smalltilesfilter/kis_small_tiles_filter.h
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the KDE project
+ *
+ * Copyright (c) Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_SMALL_TILES_FILTER_H_
+#define _KIS_SMALL_TILES_FILTER_H_
+
+#include "kis_filter.h"
+#include "kis_view.h"
+#include <kdebug.h>
+
+class KisSmallTilesFilterConfiguration : public KisFilterConfiguration
+{
+	public:
+                KisSmallTilesFilterConfiguration(Q_UINT32 numberOfTiles) : m_numberOfTiles(numberOfTiles) {};
+	public:
+                inline Q_UINT32 numberOfTiles() { return m_numberOfTiles; };
+        private:
+                Q_UINT32 m_numberOfTiles;
+};
+
+class KisSmallTilesFilter : public KisFilter
+{
+public:
+	KisSmallTilesFilter();
+public:
+	virtual void process(KisPaintDeviceSP,KisPaintDeviceSP, KisFilterConfiguration* , const QRect&);
+	static inline KisID id() { return KisID("smalltiles", i18n("Small tiles")); };
+	virtual bool supportsPainting() { return true; }
+	virtual bool supportsPreview() { return true; }
+	virtual std::list<KisFilterConfiguration*> listOfExamplesConfiguration(KisPaintDeviceSP )
+	{ std::list<KisFilterConfiguration*> list; list.insert(list.begin(), new KisSmallTilesFilterConfiguration(2)); return list; }
+public:
+	virtual KisFilterConfigWidget * createConfigurationWidget(QWidget* parent, KisPaintDeviceSP dev);
+	virtual KisFilterConfiguration* configuration(QWidget*, KisPaintDeviceSP dev);
+private:
+	void createSmallTiles(KisPaintDeviceSP src, KisPaintDeviceSP dst, const QRect& rect, Q_UINT32 numberOfTiles);
+};
+
+#endif

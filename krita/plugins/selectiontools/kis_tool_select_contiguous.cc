ff9dd93feba30e0267602b47c56ecf4ce4042fbd
Boudewijn Rempt <boud@valdyas.org>
2005-07-30 10:38:04 +0000

    This header file no longer exists.
    svn path=/trunk/koffice/; revision=440392

--- krita/plugins/selectiontools/kis_tool_select_contiguous.cc
+++ krita/plugins/selectiontools/kis_tool_select_contiguous.cc
@@ -42,7 +42,6 @@
 #include <kis_selection.h>
 #include <kis_paint_device.h>
 #include <kis_iterators_pixel.h>
-#include <kis_color_utilities.h>
 #include <kis_selection_options.h>
 #include <kis_canvas_observer.h>
 #include <kis_fill_painter.h>

02a0676060fff83e9e68533d46df80eaf0e911ea
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2005-07-30 09:02:54 +0000

    A multi bool widget that can be used for filters that have only bool options. Currently it is only used by the sobel filter.
    svn path=/trunk/koffice/; revision=440361

---
+++ krita/ui/widgets/kis_multi_bool_filter_widget.cc
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2005 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_multi_bool_filter_widget.h"
+
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qcheckbox.h>
+
+#include <klocale.h>
+
+#include "kis_filter.h"
+
+KisBoolWidgetParam::KisBoolWidgetParam(  bool ninitvalue, QString nname) :
+	initvalue(ninitvalue),
+	name(nname)
+{
+
+}
+
+KisMultiBoolFilterWidget::KisMultiBoolFilterWidget(QWidget * parent, const char * name, const char * caption, vKisBoolWidgetParam iwparam) : 
+	KisFilterConfigWidget( parent, name )
+{
+	Q_INT32 m_nbboolWidgets = iwparam.size();
+
+	this->setCaption(caption);
+
+	QGridLayout *widgetLayout = new QGridLayout(this, m_nbboolWidgets + 1, 3);
+	widgetLayout -> setColStretch ( 1, 1 );
+
+	m_boolWidgets = new QCheckBox*[ m_nbboolWidgets ];
+
+	for( Q_INT32 i = 0; i < m_nbboolWidgets; ++i)
+	{
+		m_boolWidgets[i] = new QCheckBox( this, iwparam[i].name.ascii());
+		m_boolWidgets[i] -> setChecked( iwparam[i].initvalue );
+
+		connect(m_boolWidgets[i], SIGNAL(toggled( bool ) ), SIGNAL(sigPleaseUpdatePreview()));
+
+		QLabel* lbl = new QLabel(iwparam[i].name+":", this);
+		widgetLayout -> addWidget( lbl, i , 0);
+
+		widgetLayout -> addWidget( m_boolWidgets[i], i , 1);
+	}
+	QSpacerItem * sp = new QSpacerItem(1, 1);
+	widgetLayout -> addItem(sp, m_nbboolWidgets, 0);
+
+}
+
+#include "kis_multi_bool_filter_widget.moc"
---
+++ krita/ui/widgets/kis_multi_bool_filter_widget.h
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2005 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_MULTI_BOOL_FILTER_WIDGET_H_
+#define _KIS_MULTI_BOOL_FILTER_WIDGET_H_
+
+#include <vector> 
+
+#include <qcheckbox.h>
+
+#include "koffice_export.h"
+
+#include "kis_filter.h"
+
+class KIntNumInput;
+
+struct KisBoolWidgetParam {
+	KRITA_EXPORT KisBoolWidgetParam(  bool ninitvalue, QString nname  );
+	bool initvalue;
+	QString name;
+};
+
+typedef std::vector<KisBoolWidgetParam> vKisBoolWidgetParam;
+
+class KRITA_EXPORT KisMultiBoolFilterWidget : public KisFilterConfigWidget
+{
+	Q_OBJECT
+public:
+	KisMultiBoolFilterWidget(QWidget * parent,  const char * name, const char *caption, vKisBoolWidgetParam iwparam);
+public:
+	inline Q_INT32 nbValues() { return m_nbboolWidgets; };
+	inline Q_INT32 valueAt( Q_INT32 i ) { return m_boolWidgets[i]->isChecked(); };
+private:
+	QCheckBox** m_boolWidgets;
+	Q_INT32 m_nbboolWidgets;
+};
+
+#endif

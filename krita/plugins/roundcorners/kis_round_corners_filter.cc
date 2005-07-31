25c7901aea81efe382d6b95a44545f017f6e2c7d
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2005-07-31 11:40:02 +0000

    A new filter for Krita. This filter "rounds" ther corner of an image. It does not create a background yet, so for now if you want to have a background, just create a new layer and fill it with some color.
    svn path=/trunk/koffice/; revision=441600

---
+++ krita/plugins/roundcorners/kis_round_corners_filter.cc
@@ -0,0 +1,158 @@
+/*
+ * This file is part of Krita
+ *
+ * Copyright (c) 2005 Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ * ported from Gimp, Copyright (C) 1997 Eiichi Takamori <taka@ma1.seikyou.ne.jp>
+ * original pixelize.c for GIMP 0.54 by Tracy Scott
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+
+#include <qpoint.h>
+#include <qspinbox.h>
+
+#include <klocale.h>
+#include <kiconloader.h>
+#include <kinstance.h>
+#include <kmessagebox.h>
+#include <kstandarddirs.h>
+#include <ktempfile.h>
+#include <kdebug.h>
+#include <kgenericfactory.h>
+#include <knuminput.h>
+
+#include <kis_doc.h>
+#include <kis_image.h>
+#include <kis_iterators_pixel.h>
+#include <kis_layer.h>
+#include <kis_filter_registry.h>
+#include <kis_global.h>
+#include <kis_types.h>
+#include <kis_progress_display_interface.h>
+
+#include "kis_multi_integer_filter_widget.h"
+#include "kis_round_corners_filter.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+KisRoundCornersFilter::KisRoundCornersFilter() : KisFilter(id(), "decor", "&Round corners...")
+{
+}
+
+void KisRoundCornersFilter::process(KisPaintDeviceSP src, KisPaintDeviceSP dst, KisFilterConfiguration* configuration, const QRect& rect)
+{
+        //read the filter configuration values from the KisFilterConfiguration object
+        Q_UINT32 radius = ((KisRoundCornersFilterConfiguration*)configuration)->radius();
+	Q_UINT32 pixelSize = src -> pixelSize();
+	
+	setProgressTotalSteps( rect.height() );
+        setProgressStage(i18n("Applying pixelize filter..."),0);
+
+	for (Q_INT32 y = rect.y(); y < rect.height(); y++)
+	{
+		Q_UINT32 x = rect.x();
+		Q_UINT32 x0 = rect.x();
+		Q_UINT32 y0 = rect.x();
+		Q_UINT32 width = rect.width();
+		Q_UINT32 height = rect.height();
+		KisHLineIteratorPixel dstIt = dst->createHLineIterator(x, y, width, true );
+		KisHLineIteratorPixel srcIt = src->createHLineIterator(x, y, width, false);
+		while( ! srcIt.isDone() )
+		{
+			if(srcIt.isSelected())
+			{
+				for( int i = 0; i < pixelSize; i++)
+				{
+					if ( i < pixelSize - 1 )
+					{
+						dstIt.rawData()[i] = srcIt.oldRawData()[i];
+					}
+					else
+					{
+						if( x <= radius && y <= radius)
+						{	
+							double dx = radius - x;
+							double dy = radius - y;
+							double dradius = static_cast<double>(radius);
+							if ( dx >= sqrt( dradius*dradius - dy*dy ) )
+							{
+								dstIt.rawData()[i] = 0;
+							}
+						}
+						else if( x >= x0 + width - radius && y <= radius)
+						{
+							double dx = x + radius - x0 - width;
+							double dy = radius - y;
+							double dradius = static_cast<double>(radius);
+							if ( dx >= sqrt( dradius*dradius - dy*dy ) )
+							{
+								dstIt.rawData()[i] = 0;
+							}
+						}
+						else if( x <= radius && y >= y0 + height - radius)
+						{
+							double dx = radius - x;
+							double dy = y + radius - y0 - height;
+							double dradius = static_cast<double>(radius);
+							if ( dx >= sqrt( dradius*dradius - dy*dy ) )
+							{
+								dstIt.rawData()[i] = 0;
+							}
+						}
+						else if( x >= x0 + width - radius && y >= y0 + height - radius)
+						{
+							
+							double dx = x + radius - x0 - width;
+							double dy = y + radius - y0 - height;
+							double dradius = static_cast<double>(radius);
+							if ( dx >= sqrt( dradius*dradius - dy*dy ) )
+							{
+								dstIt.rawData()[i] = 0;
+							}
+						}
+					}
+				}
+			}
+			++srcIt;
+			++dstIt;
+			++x;
+		}
+		setProgress(y);
+	}
+	setProgressDone();
+}
+
+KisFilterConfigWidget * KisRoundCornersFilter::createConfigurationWidget(QWidget* parent, KisPaintDeviceSP dev)
+{
+	vKisIntegerWidgetParam param;
+	param.push_back( KisIntegerWidgetParam( 2, 100, 30, i18n("Radius") ) );
+	return new KisMultiIntegerFilterWidget(parent, id().id().ascii(), id().id().ascii(), param );
+}
+
+KisFilterConfiguration* KisRoundCornersFilter::configuration(QWidget* nwidget, KisPaintDeviceSP dev)
+{
+	KisMultiIntegerFilterWidget* widget = (KisMultiIntegerFilterWidget*) nwidget;
+	if( widget == 0 )
+	{
+		return new KisRoundCornersFilterConfiguration( 30 );
+	} else {
+		return new KisRoundCornersFilterConfiguration( widget->valueAt( 0 ) );
+	}
+}
---
+++ krita/plugins/roundcorners/kis_round_corners_filter.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the KDE project
+ *
+ * Copyright (c) Michael Thaler <michael.thaler@physik.tu-muenchen.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_ROUND_CORNERS_FILTER_H_
+#define _KIS_ROUND_CORNERS_FILTER_H_
+
+#include "kis_filter.h"
+#include "kis_view.h"
+#include <kdebug.h>
+
+class KisRoundCornersFilterConfiguration : public KisFilterConfiguration
+{
+	public:
+                KisRoundCornersFilterConfiguration(Q_INT32 radius) : m_radius(radius){};
+	public:
+                inline Q_INT32 radius() { return m_radius; };
+	private:
+                Q_INT32 m_radius;
+};
+
+class KisRoundCornersFilter : public KisFilter
+{
+public:
+	KisRoundCornersFilter();
+public:
+	virtual void process(KisPaintDeviceSP,KisPaintDeviceSP, KisFilterConfiguration* , const QRect&);
+	static inline KisID id() { return KisID("roundcorners", i18n("Round corners")); };
+	virtual bool supportsPainting() { return true; }
+	virtual bool supportsPreview() { return true; }
+	virtual std::list<KisFilterConfiguration*> listOfExamplesConfiguration(KisPaintDeviceSP )
+	{ std::list<KisFilterConfiguration*> list; list.insert(list.begin(), new KisRoundCornersFilterConfiguration(30)); return list; }
+public:
+	virtual KisFilterConfigWidget * createConfigurationWidget(QWidget* parent, KisPaintDeviceSP dev);
+	virtual KisFilterConfiguration* configuration(QWidget*, KisPaintDeviceSP dev);
+private:
+};
+
+#endif

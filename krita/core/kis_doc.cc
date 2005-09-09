c8a0a11da0d6a55ac534974dd050b27ed347ceb0
Sven Langkamp <sven.langkamp@gmail.com>
2005-09-09 19:36:07 +0000

    reopen template dialog when pressing cancel in new image dialog BUG: 110187
    svn path=/trunk/koffice/; revision=459078

--- krita/core/kis_doc.cc
+++ krita/core/kis_doc.cc
@@ -839,7 +839,7 @@ bool KisDoc::slotNewImage()
 
         return true;
     }
-    return false;
+    return initDoc( KoDocument::InitDocAppStarting);
 }
 
 KoView* KisDoc::createViewInstance(QWidget* parent, const char *name)

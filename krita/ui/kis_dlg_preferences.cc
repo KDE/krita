0747190f5a050369bfed5e84094eaa25ab265018
Jakub Stachowski <qbast@go2.pl>
2005-10-30 13:55:21 +0000

    Actually include qgl.h if HAVE_GL is defined
    svn path=/trunk/koffice/; revision=475751

--- krita/ui/kis_dlg_preferences.cc
+++ krita/ui/kis_dlg_preferences.cc
@@ -18,6 +18,10 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <qvbox.h>
 #include <qlabel.h>
 #include <qlayout.h>

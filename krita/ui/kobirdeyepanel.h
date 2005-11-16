737b50e0bfe4676c2faceef4a8768f7f058065e0
Boudewijn Rempt <boud@valdyas.org>
2005-11-16 21:15:54 +0000

    SVN_SILENT: vi'ism
    svn path=/trunk/koffice/; revision=480861

--- krita/ui/kobirdeyepanel.h
+++ krita/ui/kobirdeyepanel.h
@@ -140,8 +140,7 @@ public:
      * @param zoomListener the object that listens to the zoom instructions we give
      * @param thumbnailProvider the class that creates the small image at the right
      *        zoomlevel
-     * @param canvas the place the document is painted.:q
-
+     * @param canvas the place the document is painted.
      * @param parent the parent widget
      * @param name the QObject name of this bird eye widget
      * @param f the widget flags (@see QWidget)

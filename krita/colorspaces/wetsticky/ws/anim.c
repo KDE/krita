de6663a8b11cf16d762663e5075bd4ff4caf3c4c
Script Kiddy <scripty@kde.org>
2005-12-22 12:32:17 +0000

    Fix FSF address (except in kexi/3rdparty) (Mainly old FSF address) (goutte)
    svn path=/trunk/koffice/; revision=490579

--- krita/colorspaces/wetsticky/ws/anim.c
+++ krita/colorspaces/wetsticky/ws/anim.c
@@ -14,7 +14,7 @@ Liverpool L3 3AF
 United Kingdom
 Phone +44 151 231 2271
 
-Wet and Sticky is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. Wet and Sticky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Wet and Sticky; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+Wet and Sticky is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. Wet and Sticky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Wet and Sticky; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA 
 
 */
 

48d8b874df0ffaf97896573d327f06ee57db6a20
Thomas Zander <zander@kde.org>
2006-02-26 04:42:58 +0000

    Fix typos SVNSILENT
    svn path=/trunk/koffice/; revision=513688

--- krita/core/kis_thread_pool.cc
+++ krita/core/kis_thread_pool.cc
@@ -1,7 +1,7 @@
 /*
  *  copyright (c) 2006 Boudewijn Rempt
  *
- *  This program is free software; you can CYANistribute it and/or modify
+ *  This program is free software; you can distribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.

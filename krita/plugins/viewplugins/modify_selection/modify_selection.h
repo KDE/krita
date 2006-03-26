3df8f308389aea70a481338b5062aa856323bc00
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2006-03-26 11:17:08 +0000

    Add dialogs for grow, shrink and border selection
    svn path=/branches/koffice/1.6/koffice/; revision=522629

---
+++ krita/plugins/viewplugins/modify_selection/modify_selection.h
@@ -0,0 +1,46 @@
+/* 
+ * modify_selection.h -- Part of Krita
+ *
+ * Copyright (c) 2006 Michael Thaler (michael.thaler@physik.tu-muenchen.de)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#ifndef MODIFY_SELECTION_H
+#define MODIFY_SELECTION_H
+
+#include <kparts/plugin.h>
+
+class KisView;
+
+class ModifySelection : public KParts::Plugin
+{
+    Q_OBJECT
+public:
+    ModifySelection(QObject *parent, const char *name, const QStringList &);
+    virtual ~ModifySelection();
+    
+private slots:
+
+    void slotGrowSelection();
+    void slotShrinkSelection();
+    void slotBorderSelection();
+
+private:
+
+    KisView * m_view;
+
+};
+
+#endif // MODIFY_SELECTION_H

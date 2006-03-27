e10f14822089c4314f010b275a832cc0fda59df3
David Faure <faure@kde.org>
2006-03-27 09:13:36 +0000

    Improve error message. But this file is duplicated in every module... :(
    svn path=/trunk/koffice/; revision=523048

--- cmake/modules/FindKDE4.cmake
+++ cmake/modules/FindKDE4.cmake
@@ -25,7 +25,7 @@ IF (KDE4_KDECONFIG_EXECUTABLE)
    # replace the ":" with ";" so that it becomes a valid cmake list
    STRING(REGEX REPLACE ":" ";" _data_DIR "${_data_DIR}")
 
-   MESSAGE(STATUS "datadir: ${_data_DIR}")
+   #MESSAGE(STATUS "datadir: ${_data_DIR}")
 
    FIND_PATH(KDE4_DATA_DIR cmake/modules/FindKDE4Internal.cmake 
       ${_data_DIR}
@@ -45,6 +45,8 @@ IF (KDE4_KDECONFIG_EXECUTABLE)
 
       FIND_PACKAGE(KDE4Internal ${_req} ${_quiet})
 
+   ELSE (KDE4_DATA_DIR)
+      MESSAGE(FATAL_ERROR "ERROR: cmake/modules/FindKDE4Internal.cmake not found in ${_data_DIR}")
    ENDIF (KDE4_DATA_DIR)
 
 
@@ -52,7 +54,7 @@ ENDIF (KDE4_KDECONFIG_EXECUTABLE)
 
 
 IF (KDE4_FIND_REQUIRED AND NOT KDE4_FOUND)
-   MESSAGE(FATAL_ERROR "Could not find KDE4")
+   MESSAGE(FATAL_ERROR "ERROR: Could not find KDE4")
 ENDIF (KDE4_FIND_REQUIRED AND NOT KDE4_FOUND)
 
 

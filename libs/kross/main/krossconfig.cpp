3d23333dd9454a7e1700b93f868a7ca48313a131
Sebastian Sauer <mail@dipe.org>
2006-04-07 18:54:01 +0000

    Same missing "svn add". SVN_SILENT
    svn path=/trunk/koffice/; revision=527332

---
+++ libs/kross/main/krossconfig.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+ * krossconfig.h
+ * This file is part of the KDE project
+ * copyright (C)2004-2005 by Sebastian Sauer (mail@dipe.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ ***************************************************************************/
+
+#include "krossconfig.h"
+
+#ifdef KROSS_DEBUG_ENABLED
+
+#include <kdebug.h>
+
+void Kross::krossdebug(const QString &s)
+{
+    kdDebug() << "Kross: " << s << endl;
+}
+
+void Kross::krosswarning(const QString &s)
+{
+    kdWarning() << "Kross: " << s << endl;
+}
+
+#endif

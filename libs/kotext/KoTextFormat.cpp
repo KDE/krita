3e1d520829c42e0651df3c05a811c3320aa43052
Laurent Montel <montel@kde.org>
2006-04-11 06:57:31 +0000

    Adapt to new kdelibs api
    svn path=/trunk/koffice/; revision=528468

--- libs/kotext/KoTextFormat.cpp
+++ libs/kotext/KoTextFormat.cpp
@@ -1609,7 +1609,7 @@ void KoTextFormat::setLanguage( const QString & _lang)
 QStringList KoTextFormat::underlineTypeList()
 {
     QStringList lst;
-    lst <<i18n("Underline Style", "None");
+    lst <<i18nc("Underline Style", "None");
     lst <<i18n("Single");
     lst <<i18n("Double");
     lst <<i18n("Simple Bold");
@@ -1620,7 +1620,7 @@ QStringList KoTextFormat::underlineTypeList()
 QStringList KoTextFormat::strikeOutTypeList()
 {
     QStringList lst;
-    lst <<i18n("Strikeout Style", "None");
+    lst <<i18nc("Strikeout Style", "None");
     lst <<i18n("Single");
     lst <<i18n("Double");
     lst <<i18n("Simple Bold");

707ea4b3793b36c7e6c5fe79af5477995a4c919d
Adrian Page <adrian@pagenet.plus.com>
2006-04-14 20:46:27 +0000

    Fix an assert in KUrl about a url not starting with '/'.
    Now we can open files in krita.
    
    svn path=/trunk/koffice/; revision=529939

--- libs/kofficecore/KoDocument.cpp
+++ libs/kofficecore/KoDocument.cpp
@@ -1264,7 +1264,7 @@ QString KoDocument::autoSaveFile( const QString & path ) const
     }
     else
     {
-        KUrl url( path );
+        KUrl url = KUrl::fromPathOrURL( path );
         Q_ASSERT( url.isLocalFile() );
         QString dir = url.directory(false);
         QString filename = url.fileName();

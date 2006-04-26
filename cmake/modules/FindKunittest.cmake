7d7bd07156c1d690a9053225fe7b162908a6726c
Laurent Montel <montel@kde.org>
2006-04-26 09:40:03 +0000

    Add detect kunittest Now necessary to readd kunittest krita program
    svn path=/trunk/koffice/; revision=534045

---
+++ cmake/modules/FindKunittest.cmake
@@ -0,0 +1,35 @@
+# - Try to find the kunittest directory library
+# Once done this will define
+#
+#  KUNITTEST_FOUND - system has kunittest
+#  KUNITTEST_INCLUDE_DIR - the kunittest include directory
+#  KUNITTEST_LIBRARIES - The libraries needed to use kunittest
+
+FIND_PATH(KUNITTEST_INCLUDE_DIR kunittest/tester.h
+   /usr/include
+   /usr/local/include
+	${KDE4_INCLUDE_DIR}
+)
+
+FIND_LIBRARY(KUNITTEST_LIBRARIES NAMES kunittest
+   PATHS
+   /usr/lib
+   /usr/local/lib
+   ${KDE4_LIB_DIR}
+)
+
+MESSAGE(STATUS "KUNITTEST_LIBRARIES :<${KUNITTEST_LIBRARIES}>, KUNITTEST_INCLUDE_DIR :<${KUNITTEST_INCLUDE_DIR}>")
+
+if (KUNITTEST_INCLUDE_DIR AND KUNITTEST_LIBRARIES)
+   set(KUNITTEST_FOUND TRUE)
+endif (KUNITTEST_INCLUDE_DIR AND KUNITTEST_LIBRARIES)
+
+
+if (KUNITTEST_FOUND)
+   if (NOT KUNITTEST_FIND_QUIETLY)
+      message(STATUS "Found kunittest: ${KUNITTEST_LIBRARIES}")
+   endif (NOT KUNITTEST_FIND_QUIETLY)
+endif (KUNITTEST_FOUND)
+
+MARK_AS_ADVANCED(KUNITTEST_INCLUDE_DIR KUNITTEST_LIBRARIES)
+

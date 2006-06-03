11bb5054926b4ed1706af1693a6e09487d97f64a
Casper Boemann <cbr@boemann.dk>
2006-06-03 15:21:34 +0000

    Commit the beginnings of KOffice wide colormangement
    codename "pigment"
    
    Not build yet
    
    
    svn path=/trunk/koffice/; revision=547850

---
+++ libs/pigment/KoColor.cpp
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2005 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include <QColor>
+ 
+#include "KoColor.h"
+
+KoColor::KoColor()
+{
+    m_data = new quint8[4];
+}
+
+KoColor::~KoColor()
+{
+    delete [] m_data;
+}
+
+KoColor::KoColor(const KoColor & rhs)
+{
+    if (this == &rhs) return;
+
+    memcpy(m_data, rhs.data(), 4);
+}
+
+KoColor & KoColor::operator=(const KoColor & rhs)
+{
+    memcpy(m_data, rhs.data(), 4);
+    return * this;
+}
+
+// To save the user the trouble of doing color->colorSpace()->toQColor(color->data(), &c, &a, profile
+void KoColor::toQColor(QColor *c) const
+{
+    c.setRgb(m_data[0],m_data[1],m_data[2]);
+}
+
+void KoColor::toQColor(QColor *c, quint8 *opacity) const
+{
+    c.setRgb(m_data[0],m_data[1],m_data[2]);
+    *opacity = m_data[3];
+}
+
+QColor KoColor::toQColor() const
+{
+    QColor c;
+    toQColor(&c);
+    return c;
+}
---
+++ libs/pigment/KoColor.h
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2005 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#ifndef KOCOLOR_H_
+#define KOCOLOR_H_
+
+#include <QColor>
+#include <koffice_export.h>
+
+/**
+ * A KoColor describes a color in a certain colorspace.
+ *
+ */
+class PIGMENT_EXPORT KoColor {
+
+public:
+    /// Create an empty KoColor. It will be valid, but also black and transparent
+    KoColor();
+
+    virtual ~KoColor();
+
+    /// Copy constructor -- deep copies the colors.
+    KoColor(const KoColor & rhs);
+
+    /// Effective C++, item 11
+    KoColor &operator=(const KoColor &);
+
+    /// For easy memcpy'ing etc.
+    quint8 * data() const { return m_data; }
+
+    /// To save the user the trouble of doing color->colorSpace()->toQColor(color->data(), &c, &a
+    void toQColor(QColor *c) const;
+    void toQColor(QColor *c, quint8 *opacity) const;
+
+    QColor toQColor() const;
+
+private:
+
+    quint8 * m_data;
+};
+
+#endif

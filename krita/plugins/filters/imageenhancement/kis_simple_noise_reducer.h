2ab282e5d6b65246ec23a65444ae4de5dd9d0ae9
Cyrille Berger <cyb@lepi.org>
2006-06-18 16:10:04 +0000

    compilation fix
    svn path=/trunk/koffice/; revision=552635

--- krita/plugins/filters/imageenhancement/kis_simple_noise_reducer.h
+++ krita/plugins/filters/imageenhancement/kis_simple_noise_reducer.h
@@ -49,7 +49,7 @@ class KisSimpleNoiseReducer : public KisFilter
         virtual KisFilterConfiguration * configuration() { return new KisSimpleNoiseReducerConfiguration( 50, 1); };
         virtual KisFilterConfigWidget * createConfigurationWidget(QWidget* parent, KisPaintDeviceSP dev);
 
-        static inline KisID id() { return KisID("gaussiannoisereducer", i18n("Gaussian Noise Reducer")); };
+        static inline KoID id() { return KoID("gaussiannoisereducer", i18n("Gaussian Noise Reducer")); };
         virtual bool supportsPainting() { return false; }
         virtual bool supportsPreview() { return true; }
         virtual bool supportsIncrementalPainting() { return false; }

4705a9e5490b3405551b4f4d7098ca292dfdf050
Stefan Nikolaus <stefan.nikolaus@kdemail.net>
2006-06-29 15:30:30 +0000

    - skip the searching, if cached values are present
    svn path=/trunk/koffice/; revision=556204

--- cmake/modules/FindGSL.cmake
+++ cmake/modules/FindGSL.cmake
@@ -4,49 +4,70 @@
 # This module defines
 #  GSL_FOUND
 #  GSL_LIBRARIES
+#  GSL_CBLAS_LIBRARIES
 #  GSL_INCLUDE_DIR
 #  GSL_VERSION
 #
 
-FIND_LIBRARY (GSL_LIBRARIES gsl
-	PATHS
-	/usr/lib
-	/usr/local/lib
-)
+IF (GSL_INCLUDE_DIR AND GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_VERSION)
 
-FIND_LIBRARY (GSL_CBLAS_LIBRARIES gslcblas
-	PATHS
-	/usr/lib
-	/usr/local/lib
-)
+	# Already in cache
+	SET(GSL_FOUND TRUE)
 
-FIND_PATH (GSL_INCLUDE_DIR gsl_multimin.h
-	/usr/include/gsl
-	/usr/local/include/gsl
-)
+ELSE (GSL_INCLUDE_DIR AND GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_VERSION)
 
-FIND_PROGRAM (GSL_CONFIG gsl-config
-	/usr/bin
-	/usr/local/bin
-)
+	FIND_LIBRARY (GSL_LIBRARIES gsl
+		PATHS
+		/usr/lib
+		/usr/local/lib
+	)
 
-IF (GSL_CONFIG)
-	EXEC_PROGRAM (${GSL_CONFIG} ARGS "--version" OUTPUT_VARIABLE GSL_VERSION)
-#	EXEC_PROGRAM (${GSL_CONFIG} ARGS "--cflags" OUTPUT_VARIABLE gsl_include_dir)
-#	EXEC_PROGRAM (${GSL_CONFIG} ARGS "--libs" OUTPUT_VARIABLE gsl_libraries)
+	FIND_LIBRARY (GSL_CBLAS_LIBRARIES gslcblas
+		PATHS
+		/usr/lib
+		/usr/local/lib
+	)
 
-#	STRING (REGEX REPLACE "-I([^ ]*)" "\\1" GSL_INCLUDE_DIR "${gsl_include_dir}")
-#	STRING (REGEX REPLACE "-L([^ ]*)" "\\1" GSL_LIBRARIES "${gsl_libraries}")
-ENDIF (GSL_CONFIG)
+	FIND_PATH (GSL_INCLUDE_DIR gsl_multimin.h
+		/usr/include/gsl
+		/usr/local/include/gsl
+	)
+
+	FIND_PROGRAM (GSL_CONFIG gsl-config
+		/usr/bin
+		/usr/local/bin
+	)
+
+	IF (GSL_CONFIG)
+		EXEC_PROGRAM (${GSL_CONFIG} ARGS "--version" OUTPUT_VARIABLE gsl_version)
+	#	EXEC_PROGRAM (${GSL_CONFIG} ARGS "--cflags" OUTPUT_VARIABLE gsl_include_dir)
+	#	EXEC_PROGRAM (${GSL_CONFIG} ARGS "--libs" OUTPUT_VARIABLE gsl_libraries)
+	
+	#	STRING (REGEX REPLACE "-I([^ ]*)" "\\1" GSL_INCLUDE_DIR "${gsl_include_dir}")
+	#	STRING (REGEX REPLACE "-L([^ ]*)" "\\1" GSL_LIBRARIES "${gsl_libraries}")
+		SET (GSL_VERSION ${gsl_version} CACHE STRING "GNU Scientific Library Version")
+		# TODO check version! 1.6 suffices?
+	ENDIF (GSL_CONFIG)
+
+
+	#
+	# everything necessary found?
+	#
+	IF (GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_INCLUDE_DIR)
+		SET (GSL_FOUND TRUE)
+	ELSE (GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_INCLUDE_DIR)
+		SET (GSL_FOUND FALSE)
+	ENDIF (GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_INCLUDE_DIR)
+
+ENDIF (GSL_INCLUDE_DIR AND GSL_LIBRARIES AND GSL_CBLAS_LIBRARIES AND GSL_VERSION)
 
 
 #
-# everything necessary found?
+# output status
 #
-IF (GSL_LIBRARIES AND GSL_INCLUDE_DIR)
-	MESSAGE (STATUS "Found GNU Scientific Library ${GSL_VERSION}: ${GSL_INCLUDE_DIR}, ${GSL_LIBRARIES}")
-	SET (GSL_FOUND TRUE)
-ELSE (GSL_LIBRARIES AND GSL_INCLUDE_DIR)
-	MESSAGE (STATUS "GNU Scientific Library not found.")
-	SET (GSL_FOUND FALSE)
-ENDIF (GSL_LIBRARIES AND GSL_INCLUDE_DIR)
+IF (GSL_FOUND)
+	MESSAGE (STATUS "Found GNU Scientific Library ${GSL_VERSION}: ${GSL_INCLUDE_DIR} ${GSL_LIBRARIES};${GSL_CBLAS_LIBRARIES}")
+ELSE (GSL_FOUND)
+	MESSAGE (STATUS "GNU Scientific Library not found. "
+			"KSpread's solver plugin won't be compiled.")
+ENDIF (GSL_FOUND)

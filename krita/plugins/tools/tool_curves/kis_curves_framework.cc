77b630eaf7b9ef18cf7bf7989bc1faaa04222e5d
Emanuele Tamponi <emanuele@valinor.it>
2006-07-05 21:20:50 +0000

    SoC project: Code for a really Basic Curve Framework infrastructure... more to come :)
    svn path=/branches/koffice/1.6/koffice/; revision=558703

--- krita/plugins/tools/tool_curves/kis_curves_framework.cc
+++ krita/plugins/tools/tool_curves/kis_curves_framework.cc
@@ -18,4 +18,126 @@
  
 /* Initial Commit for the Curves Framework. E.T. */
 
+#include <qvaluevector.h>
+#include "kis_paint_device.h"
+#include "kis_painter.h"
+#include "kis_point.h"
+
 #include "kis_curves_framework.h"
+
+/* ****************************** *
+ * CurvePoint methods definitions *
+ * ****************************** */
+
+inline CurvePoint::CurvePoint ()
+    : m_point(0), m_pivot(0), m_selected(0)
+{
+
+}
+    
+inline CurvePoint::CurvePoint (KisPoint &pt, bool p = false, bool s = false)
+    : m_pivot(p), m_selected((p) ? s : false)
+{
+    m_point = new KisPoint;
+    *m_point = pt;
+}
+
+inline CurvePoint::CurvePoint (double x, double y, bool p = false, bool s = false)
+    : m_pivot(p), m_selected((p) ? s : false)
+{
+    m_point = new KisPoint(x,y);
+}
+
+inline CurvePoint::CurvePoint (QPoint& pt, bool p = false, bool s = false)
+    : m_pivot(p), m_selected((p) ? s : false)
+{
+    m_point = new KisPoint(pt);
+}
+    
+inline CurvePoint::CurvePoint (KoPoint& pt, bool p = false, bool s = false)
+    : m_pivot(p), m_selected((p) ? s : false)
+{
+    m_point = new KisPoint(pt);
+}
+
+
+inline bool CurvePoint::setPoint(KisPoint p)
+{
+    if (!m_point)
+        m_point = new KisPoint();
+    *m_point = p;
+    
+    return ((m_point) ? true : false);
+}
+
+inline bool CurvePoint::setPoint(double x, double y)
+{
+    if (m_point)
+        delete m_point;
+    m_point = new KisPoint(x,y);
+    
+    return ((m_point) ? true : false);
+}
+
+inline bool CurvePoint::setPoint(QPoint &pt)
+{
+    if (m_point)
+        delete m_point;
+    m_point = new KisPoint(pt);
+    
+    return ((m_point) ? true : false);
+}
+
+inline bool CurvePoint::setPoint(KoPoint &pt)
+{
+    if (m_point)
+        delete m_point;
+    m_point = new KisPoint(pt);
+    
+    return ((m_point) ? true : false);
+}
+
+
+/* **************************** *
+ * KisCurve methods definitions *
+ * **************************** */
+
+
+int KisCurve::add (CurvePoint point, int index = -1)
+{
+    if (index < 0) {
+        m_curve.append (point);
+        return m_curve.count()-1;
+    } else {
+        PointList::iterator it = &m_curve.at(index);
+        m_curve.insert (it, point);
+        return index;
+    } 
+}
+
+int KisCurve::add (KisPoint point, bool pivot, bool selection, int index = -1) {
+    CurvePoint temp (point, pivot, selection);
+
+    return add (temp, index);
+}
+
+bool KisCurve::setPivot (CurvePoint pivot) {
+    PointList::iterator it = qFind(m_curve.begin(),m_curve.end(),pivot);
+    if (it != m_curve.end())
+        it->setPivot(true);
+    else
+        return false;
+
+    return true;
+}
+
+bool KisCurve::setPivot (int index) {
+    PointList::iterator it = &m_curve.at(index);
+    if (it)
+        it->setPivot(true);
+    else
+        return false;
+
+    return true;
+}
+
--- krita/plugins/tools/tool_curves/kis_curves_framework.h
+++ krita/plugins/tools/tool_curves/kis_curves_framework.h
@@ -18,15 +18,104 @@
 
 /* Initial Commit for the Curves Framework. E.T. */
 
-#include <qpointarray.h>
+class CurvePoint {
+
+    KisPoint* m_point;
+    bool m_pivot;
+    bool m_selected; // Only pivots can be selected
+    
+public:
+
+    /* Constructors and Destructor */
+    
+    CurvePoint ();
+    CurvePoint (KisPoint&, bool, bool);
+    CurvePoint (double, double, bool, bool);
+    CurvePoint (QPoint&, bool, bool);
+    CurvePoint (KoPoint&, bool, bool);
+    
+    virtual ~CurvePoint () {delete m_point;}
+    
+public:
+
+    /* Generic Functions */
+
+    bool operator!= (CurvePoint p2) const { if (p2.getPoint() != *m_point) return true; else return false;}
+    
+    KisPoint getPoint() {return *m_point;}
+    
+    bool setPoint(KisPoint);
+    bool setPoint(double, double);
+    bool setPoint(QPoint&);
+    bool setPoint(KoPoint&);
+    
+    bool isPivot() {return m_pivot;}
+    bool isSelected() {return m_selected;}
+    
+    void setPivot(bool p) {m_pivot = p;}
+    void setSelected(bool s) {m_selected = ((m_pivot) ? s : false);}  /* Only pivots can be selected */
+};
+
+
+typedef QValueVector<CurvePoint> PointList;
 
 class KisCurve {
+
+    enum Status {DRAWING, EDITING, ENDING};
+
+    PointList m_curve;
+    PointList m_pivots;
+    Status m_current;
+
+    KisPaintDeviceSP m_dev;
     
-    QPointArray* tmp;
+public:
     
-    public:
+    KisCurve () {m_dev = 0; m_current=DRAWING;}
+    KisCurve (KisPaintDevice dev) {m_dev = new KisPaintDevice(dev); m_current=DRAWING;}
     
-    KisCurve () {tmp = new QPointArray;}
-    virtual ~KisCurve () {delete tmp;}
+    virtual ~KisCurve ();
     
+public:
+
+    CurvePoint& operator[](int i) {return m_curve[i];}
+    
+/*
+    void operator+(CurvePoint pt);
+    void operator+(KisCurve cv);
+    void operator+(PointList cv);
+*/
+
+    void setPaintDevice(KisPaintDevice dev) {if (m_dev) delete m_dev; m_dev = new KisPaintDevice (dev);}
+    void setStatus(Status st) {m_current = st;}
+
+    int add(CurvePoint, int);
+    int add(KisPoint, bool, bool, int);
+
+    PointList getCurve() {return m_curve;}
+    PointList getCurve(CurvePoint, CurvePoint);
+    PointList getCurve(int, int);
+
+    bool setPivot (CurvePoint);
+    bool setPivot (int);
+    
+    PointList getPivots() {return m_pivots;}
+    
+    CurvePoint getLastPivot() {return m_pivots.last();}
+    int getLastPivotIndex() {return m_pivots.count()-1;}
+
+    bool deleteLastPivot();
+
+    virtual bool selectPivot(CurvePoint);
+    virtual bool selectPivot(int);
+
+    virtual bool calculateCurve(KisPoint, KisPoint) = 0;
+    virtual bool calculateCurve(CurvePoint, CurvePoint) = 0;
+
+    virtual bool movePivot(CurvePoint, KisPoint);
+    virtual bool movePivot(CurvePoint, CurvePoint);
+    virtual bool movePivot(int, CurvePoint);
+
+    virtual bool deletePivot(CurvePoint);
+    virtual bool deletePivot(int);
 };

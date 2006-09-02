1534ec1731e9f36e788546c8247c98b83a5bc980
Bart Coppens <kde@bartcoppens.be>
2006-09-02 16:51:57 +0000

    Honor the selection when painting indirectly
    svn path=/branches/koffice/1.6/koffice/; revision=580109

--- krita/ui/kis_tool_freehand.cc
+++ krita/ui/kis_tool_freehand.cc
@@ -175,6 +175,9 @@ void KisToolFreehand::initPaint(KisEvent *)
                 layer->setTemporaryCompositeOp(m_compositeOp);
                 layer->setTemporaryOpacity(m_opacity);
 
+                if (device->hasSelection())
+                    m_target->setSelection(device->selection());
+
                 // Hack for the painting of single-layered layers using indirect painting,
                 // because the group layer would not have a correctly synched cache (
                 // because of an optimization that would happen, having this layer as

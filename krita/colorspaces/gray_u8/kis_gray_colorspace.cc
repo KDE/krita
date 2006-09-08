5d1e8b0ab33036dc2ae250062a44006eabe4c4eb
Bart Coppens <kde@bartcoppens.be>
2006-09-08 13:05:07 +0000

    Long overdue: now the grey8 colorspace also works antialiased when opacity<100% on adjustment layers
    svn path=/branches/koffice/1.6/koffice/; revision=582170

--- krita/colorspaces/gray_u8/kis_gray_colorspace.cc
+++ krita/colorspaces/gray_u8/kis_gray_colorspace.cc
@@ -302,7 +302,7 @@ void KisGrayColorSpace::compositeOver(Q_UINT8 *dstRowStart, Q_INT32 dstRowStride
             if (srcAlpha != OPACITY_TRANSPARENT) {
 
                 if (opacity != OPACITY_OPAQUE) {
-                    srcAlpha = UINT8_MULT(src[PIXEL_GRAY_ALPHA], opacity);
+                    srcAlpha = UINT8_MULT(srcAlpha, opacity);
                 }
 
                 if (srcAlpha == OPACITY_OPAQUE) {

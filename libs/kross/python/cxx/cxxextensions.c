27e447cf4459b885ba3f5b470847893f8d6188da
Sebastian Sauer <mail@dipe.org>
2006-10-05 20:36:21 +0000

    warning--
    svn path=/trunk/koffice/; revision=592803

--- libs/kross/python/cxx/cxxextensions.c
+++ libs/kross/python/cxx/cxxextensions.c
@@ -3,6 +3,14 @@
  All rights reserved. See Legal.htm for full text and disclaimer.
 */
 
+/* Prevent warnings */
+#if defined(_XOPEN_SOURCE)
+  #undef _XOPEN_SOURCE
+#endif
+#if defined(_POSIX_C_SOURCE)
+  #undef _POSIX_C_SOURCE
+#endif
+
 #include "Python.h"
 #ifdef __cplusplus
 extern "C" {

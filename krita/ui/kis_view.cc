70974da48152c2ae36dbec90c28f7c55aa5d23ed
Bart Coppens <kde@bartcoppens.be>
2006-10-15 14:40:23 +0000

    Forward port r581467 (Actually execute the command at insertion in the undostack)
    svn path=/trunk/koffice/; revision=595731

--- krita/ui/kis_view.cc
+++ krita/ui/kis_view.cc
@@ -2626,7 +2626,7 @@ namespace {
             {
                 QApplication::setOverrideCursor(KisCursor::waitCursor());
                 m_config->fromXML(m_after);
-                Q_ASSERT(m_after == m_config->toString());
+                //Q_ASSERT(m_after == m_config->toString());
                 m_layer->setFilter(m_config);
                 m_layer->setDirty();
                 QApplication::restoreOverrideCursor();
@@ -2636,6 +2636,7 @@ namespace {
             {
                 QApplication::setOverrideCursor(KisCursor::waitCursor());
                 m_config->fromXML(m_before);
+                //Q_ASSERT(m_before == m_config->toString());
                 m_layer->setFilter(m_config);
                 m_layer->setDirty();
                 QApplication::restoreOverrideCursor();
@@ -2671,11 +2672,13 @@ void KisView::showLayerProperties(KisLayerSP layer)
         QString before = dlg.filterConfiguration()->toString();
         if (dlg.exec() == QDialog::Accepted)
         {
-            m_adapter->addCommand(new KisChangeFilterCmd(alayer,
-                                  dlg.filterConfiguration(),
-                                  before,
-                                  dlg.filterConfiguration()->toString()));
-            //m_doc->setModified( true );
+            KisChangeFilterCmd * cmd = new KisChangeFilterCmd(alayer,
+                    dlg.filterConfiguration(),
+                    before,
+                    dlg.filterConfiguration()->toString());
+            cmd->execute();
+            m_adapter->addCommand(cmd);
+            m_doc->setModified( true );
         }
     }
     else

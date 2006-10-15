e188c478f0fef992d73d21cf9a8b4ccd3cd6af2f
Bart Coppens <kde@bartcoppens.be>
2006-10-15 13:11:06 +0000

    Forward port r579705 (Loading and saving of masks in the native format)
    svn path=/trunk/koffice/; revision=595703

--- krita/ui/kis_savexml_visitor.h
+++ krita/ui/kis_savexml_visitor.h
@@ -56,6 +56,7 @@ public:
         layerElement.setAttribute("layertype", "paintlayer");
         layerElement.setAttribute("filename", QString("layer%1").arg(m_count));
         layerElement.setAttribute("colorspacename", layer->paintDevice()->colorSpace()->id());
+        layerElement.setAttribute("hasmask", layer->hasMask());
 
         m_elem.appendChild(layerElement);
 

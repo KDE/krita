d113dc3bc4b637de2509dd427bbd7d3a65c95fff
Cyrille Berger <cyb@lepi.org>
2006-11-25 10:14:24 +0000

    fix licence, this one is LGPL and not GPL
    svn path=/trunk/koffice/; revision=607590

--- libs/pigment/colorspaces/KoAlphaColorSpace.cpp
+++ libs/pigment/colorspaces/KoAlphaColorSpace.cpp
@@ -1,8 +1,9 @@
 /*
  *  Copyright (c) 2004 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2006 Cyrille Berger <cberger@cberger.net>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
@@ -11,7 +12,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the Lesser GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */

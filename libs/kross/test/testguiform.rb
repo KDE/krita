af5a909e273f5221cebaa2ea006dda09ebc73998
Sebastian Sauer <mail@dipe.org>
2006-11-27 18:36:13 +0000

    move unittests
    svn path=/trunk/koffice/; revision=608507

--- libs/kross/test/testguiform.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env kross
-
-require 'Kross'
-
-dialog = Kross.createDialog("TestGuiFormDialog")
-form = dialog
-print "form %s %s" % (form,dir(form))
-form.loadUiFile("/home/kde4/koffice/libs/kross/test/testguiform.ui")
-form.exec_loop()

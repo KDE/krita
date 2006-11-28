ac3f1786bf1858948e430160cccd01440f756d8c
Ariya Hidayat <ariya@kde.org>
2006-11-28 21:56:44 +0000

    SVN_SILENT e-mail in copyright
    svn path=/trunk/koffice/; revision=608914

--- plugins/colorspaces/cmyk_u8/composite.h
+++ plugins/colorspaces/cmyk_u8/composite.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2006 Boudewijn Rempt
+ *  Copyright (c) 2006 Boudewijn Rempt (boud@valdyas.org)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

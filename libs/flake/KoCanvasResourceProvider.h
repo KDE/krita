aa0b5895a99b9ce12baccc2ca633e756dc18b5ba
Ariya Hidayat <ariya@kde.org>
2006-11-30 22:16:03 +0000

    SVN_SILENT silence krazy
    svn path=/trunk/koffice/; revision=609472

--- libs/flake/KoCanvasResourceProvider.h
+++ libs/flake/KoCanvasResourceProvider.h
@@ -1,6 +1,6 @@
 /*
- *  Copyright (c) 2006 Boudewijn Rempt
- *
+   Copyright (c) 2006 Boudewijn Rempt (boud@valdyas.org)
+ 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either

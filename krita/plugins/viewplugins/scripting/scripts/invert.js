f8ac3748ed627975d97eabc476806912a2e89eeb
Sebastian Sauer <mail@dipe.org>
2006-12-14 09:38:26 +0000

    Add invert javascript. Does not work yet.
    svn path=/trunk/koffice/; revision=613616

---
+++ krita/plugins/viewplugins/scripting/scripts/invert.js
@@ -0,0 +1,35 @@
+// fetch the image.
+var image = Krita.image();
+
+// we like to manipulate the active painting layer.
+var layer = image.activePaintLayer();
+
+// get the height and the width the layer has.
+var width = layer.width();
+var height = layer.height();
+
+// we like to use the progressbar
+var progress = Krita.progress();
+progress.setProgressTotalSteps(width * height);
+
+// tell Krita that painting starts. the whole painting session will be
+// counted till layer.endPainting() was called as one undo/redo-step.
+layer.beginPainting("invert");
+
+// create an iterator to walk over all pixels the layer has.
+it = layer.createRectIterator(0, 0, width, height);
+
+// iterate over all pixels and invert each pixel.
+while( ! it.isDone() ) {
+    // invert the color of the pixel.
+    it.invertColor();
+
+    // increment the progress to show, that work on this pixel is done.
+    progress.incProgress();
+
+    // go to the next pixel.
+    it.next();
+}
+
+// painting is done now.
+layer.endPainting();

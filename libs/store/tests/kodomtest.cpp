0ef75a8951d88a67e4290747150e11a57e4c0d53
Brad Hards <bradh@frogmouth.net>
2006-12-23 00:58:01 +0000

    Convert the KoXml (DOM) reader test to QTestLib.
    I've also changed the test names for a couple of
    other tests, so you can easily find out where the
    tests are coming from when you run "make test" in
    the top of the koffice/ directory.
    
    svn path=/trunk/koffice/; revision=615875

--- libs/store/tests/kodomtest.cpp
+++ libs/store/tests/kodomtest.cpp
@@ -19,36 +19,49 @@
 
 #include "KoXmlReader.h"
 
-static const char* const KoXmlNS_office = "urn:oasis:names:tc:opendocument:xmlns:office:1.0";
-static const char* const KoXmlNS_text = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
+static QString const KoXmlNS_office( "urn:oasis:names:tc:opendocument:xmlns:office:1.0" );
+static QString const KoXmlNS_text( "urn:oasis:names:tc:opendocument:xmlns:text:1.0" );
 
-#include <QApplication>
-//Added by qt3to4:
-#include <QByteArray>
-#include <assert.h>
+#include "kodomtest.h"
 
 //static void debugElemNS( const QDomElement& elem )
 //{
 //    qDebug( "nodeName=%s tagName=%s localName=%s prefix=%s namespaceURI=%s", elem.nodeName().latin1(), elem.tagName().latin1(), elem.localName().latin1(), elem.prefix().latin1(), elem.namespaceURI().latin1() );
 //}
 
-void testQDom( const KoXmlDocument& doc )
-{
-    KoXmlElement docElem = doc.documentElement();
-    //debugElemNS( docElem );
-    assert( docElem.nodeName() == "o:document-content" );
-    assert( docElem.tagName() == "document-content" );
-    assert( docElem.localName() == "document-content" );
-    assert( docElem.prefix() == "o" );
-    assert( docElem.namespaceURI() == KoXmlNS_office );
 
-    KoXmlElement elem = KoXml::namedItemNS( docElem, KoXmlNS_office, "body" );
+void KoDomTest::initTestCase ( )
+{
+    const QByteArray xml = QByteArray( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                                   "<o:document-content xmlns:o=\"" )
+                         + KoXmlNS_office.toUtf8()
+                         + "\" xmlns=\"" + KoXmlNS_text.toUtf8()
+                         + "\" xmlns:text=\"" + KoXmlNS_text.toUtf8()
+                         + "\">\n"
+		"<o:body><p text:style-name=\"L1\">foobar</p><p>2nd</p></o:body><o:styles></o:styles>\n"
+		"</o:document-content>\n";
+    QVERIFY( m_doc.setContent( xml, true /* namespace processing */ ) );
+
+}
+
+
+void KoDomTest::testQDom()
+{
+    KoXmlElement docElem = m_doc.documentElement();
+    //debugElemNS( docElem );
+    QCOMPARE( docElem.nodeName(), QString( "o:document-content" ) );
+    QCOMPARE( docElem.tagName(), QString( "document-content" ) );
+    QCOMPARE( docElem.localName(), QString( "document-content" ) );
+    QCOMPARE( docElem.prefix(), QString( "o" ) );
+    QCOMPARE(  docElem.namespaceURI(), KoXmlNS_office );
+
+    KoXmlElement elem = KoXml::namedItemNS( docElem, KoXmlNS_office.toUtf8(), "body" );
 
     //debugElemNS( elem );
-    assert( elem.tagName() == "body" );
-    assert( elem.localName() == "body" );
-    assert( elem.prefix() == "o" );
-    assert( elem.namespaceURI() == KoXmlNS_office );
+    QCOMPARE( elem.tagName(), QString( "body" ) );
+    QCOMPARE( elem.localName(), QString( "body" ) );
+    QCOMPARE( elem.prefix(), QString( "o" ) );
+    QCOMPARE( elem.namespaceURI(), KoXmlNS_office );
 
     KoXmlNode n = elem.firstChild();
     for ( ; !n.isNull() ; n = n.nextSibling() ) {
@@ -56,75 +69,55 @@ void testQDom( const KoXmlDocument& doc )
             continue;
         KoXmlElement e = n.toElement();
         //debugElemNS( e );
-        assert( e.tagName() == "p" );
-        assert( e.localName() == "p" );
-        assert( e.prefix().isEmpty() );
-        assert( e.namespaceURI() == KoXmlNS_text );
+        QCOMPARE( e.tagName(), QString( "p" ) );
+        QCOMPARE( e.localName(), QString( "p" ) );
+        QVERIFY( e.prefix().isEmpty() );
+        QCOMPARE( e.namespaceURI(), KoXmlNS_text );
     }
-
-    qDebug("testQDom... ok");
 }
 
-void testKoDom( const KoXmlDocument& doc )
+
+void KoDomTest::testKoDom()
 {
-    KoXmlElement docElem = KoXml::namedItemNS( doc, KoXmlNS_office, "document-content" );
-    assert( !docElem.isNull() );
-    assert( docElem.localName() == "document-content" );
-    assert( docElem.namespaceURI() == KoXmlNS_office );
+    KoXmlElement docElem = KoXml::namedItemNS( m_doc, KoXmlNS_office.toUtf8(), "document-content" );
+    QCOMPARE( docElem.isNull(), false );
+    QCOMPARE( docElem.localName(), QString( "document-content" ) );
+    QCOMPARE( docElem.namespaceURI(), KoXmlNS_office );
 
-    KoXmlElement body = KoXml::namedItemNS( docElem, KoXmlNS_office, "body" );
-    assert( !body.isNull() );
-    assert( body.localName() == "body" );
-    assert( body.namespaceURI() == KoXmlNS_office );
+    KoXmlElement body = KoXml::namedItemNS( docElem, KoXmlNS_office.toUtf8(), "body" );
+    QCOMPARE( body.isNull(), false );
+    QCOMPARE( body.localName(), QString( "body" ) );
+    QCOMPARE( body.namespaceURI(),KoXmlNS_office );
 
-    KoXmlElement p = KoXml::namedItemNS( body, KoXmlNS_text, "p" );
-    assert( !p.isNull() );
-    assert( p.localName() == "p" );
-    assert( p.namespaceURI() == KoXmlNS_text );
+    KoXmlElement p = KoXml::namedItemNS( body, KoXmlNS_text.toUtf8(), "p" );
+    QCOMPARE( p.isNull(), false );
+    QCOMPARE( p.localName(), QString( "p" ) );
+    QCOMPARE( p.namespaceURI(), KoXmlNS_text );
 
-    const KoXmlElement officeStyle = KoXml::namedItemNS( docElem, KoXmlNS_office, "styles" );
-    assert( !officeStyle.isNull() );
+    const KoXmlElement officeStyle = KoXml::namedItemNS( docElem, KoXmlNS_office.toUtf8(), "styles" );
+    QCOMPARE( officeStyle.isNull(), false );
 
     // Look for a non-existing element
-    KoXmlElement notexist = KoXml::namedItemNS( body, KoXmlNS_text, "notexist" );
-    assert( notexist.isNull() );
+    KoXmlElement notexist = KoXml::namedItemNS( body, KoXmlNS_text.toUtf8(), "notexist" );
+    QVERIFY( notexist.isNull() );
 
     int count = 0;
     KoXmlElement elem;
     forEachElement( elem, body ) {
-        assert( elem.localName() == "p" );
-        assert( elem.namespaceURI() == KoXmlNS_text );
+        QCOMPARE( elem.localName(), QString( "p" ) );
+        QCOMPARE( elem.namespaceURI(), KoXmlNS_text );
         ++count;
     }
-    assert( count == 2 );
+    QCOMPARE( count, 2 );
 
     // Attributes
     // ### Qt bug: it doesn't work if using style-name instead of text:style-name in the XML
     const QString styleName = p.attributeNS( KoXmlNS_text, "style-name", QString::null );
-    qDebug( "%s", qPrintable( styleName ) );
-    assert( styleName == "L1" );
-
-    qDebug("testKoDom... ok");
+    // qDebug( "%s", qPrintable( styleName ) );
+    QCOMPARE( styleName, QString( "L1" ) );
 }
 
-int main( int argc, char** argv ) {
-    QApplication app( argc, argv, QApplication::Tty );
 
-    const QByteArray xml = QByteArray( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                                   "<o:document-content xmlns:o=\"" )
-                         + KoXmlNS_office
-                         + "\" xmlns=\"" + KoXmlNS_text
-                         + "\" xmlns:text=\"" + KoXmlNS_text
-                         + "\">\n"
-		"<o:body><p text:style-name=\"L1\">foobar</p><p>2nd</p></o:body><o:styles></o:styles>\n"
-		"</o:document-content>\n";
-    KoXmlDocument doc;
-    //QXmlSimpleReader reader;
-    //reader.setFeature( "http://xml.org/sax/features/namespaces", TRUE );
-    //reader.setFeature( "http://xml.org/sax/features/namespace-prefixes", FALSE );
-    bool ok = doc.setContent( xml, true /* namespace processing */ );
-    assert( ok );
+QTEST_MAIN(KoDomTest)
 
-    testQDom(doc);
-    testKoDom(doc);
-}
+#include "kodomtest.moc"

3602f02723c730e0570a76546d4308293facd7a9
Cyrille Berger <cyb@lepi.org>
2006-12-27 20:30:15 +0000

    implement convertToQImage, at least now, krita can start with an hdr colorspace and not crash shortly after
    svn path=/trunk/koffice/; revision=617038

--- krita/colorspaces/rgb_float_hdr/kis_rgb_float_hdr_colorspace.h
+++ krita/colorspaces/rgb_float_hdr/kis_rgb_float_hdr_colorspace.h
@@ -32,6 +32,57 @@ class KisRgbFloatHDRColorSpace : public KoIncompleteColorSpace<_CSTraits, KoRGB1
         {
         
         }
+
+        virtual QImage convertToQImage(const quint8 *dataU8, qint32 width, qint32 height,
+                                                    KoColorProfile *  /*dstProfile*/,
+                                                    qint32 /*renderingIntent*/, float exposure) const
+        {
+            const typename _CSTraits::channels_type *data = reinterpret_cast<const typename _CSTraits::channels_type *>(dataU8);
+        
+            QImage img = QImage(width, height, QImage::Format_ARGB32);
+        
+            qint32 i = 0;
+            uchar *j = img.bits();
+        
+            // XXX: For now assume gamma 2.2.
+            double gamma = 1 / 2.2;
+            double exposureFactor = pow(2, exposure + 2.47393);
+        
+            while ( i < width * height * 4) {
+                *( j + 3)  = KoColorSpaceMaths<float, quint8>::scaleToA(*( data + i + 3 ));
+                *( j + 2 ) = convertToDisplay(*( data + i + 2 ), exposureFactor, gamma); //red
+                *( j + 1 ) = convertToDisplay(*( data + i + 1 ), exposureFactor, gamma); //green
+                *( j + 0 ) = convertToDisplay(*( data + i + 0 ), exposureFactor, gamma); //blue
+                i += 4;
+                j += 4;
+            }
+        
+            /*
+            if (srcProfile != 0 && dstProfile != 0) {
+                convertPixelsTo(img.bits(), srcProfile,
+                        img.bits(), this, dstProfile,
+                        width * height, renderingIntent);
+            }
+            */
+            return img;
+        }
+    private:
+        quint8 convertToDisplay(double value, double exposureFactor, double gamma) const
+        {
+            //value *= pow(2, exposure + 2.47393);
+            value *= exposureFactor;
+        
+            value = pow(value, gamma);
+        
+            // scale middle gray to the target framebuffer value
+        
+            value *= 84.66f;
+        
+            int valueInt = (int)(value + 0.5);
+        
+            return CLAMP(valueInt, 0, 255);
+        }
+
 };
 
 #endif // KIS_STRATEGY_COLORSPACE_RGB_H_

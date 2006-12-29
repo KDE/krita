a4fe7ea3ccdf0b134296cef84462af7cb8646732
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2006-12-29 17:02:15 +0000

    Make GlyphElement compile
    svn path=/trunk/koffice/; revision=617599

--- libs/kformula/GlyphElement.h
+++ libs/kformula/GlyphElement.h
@@ -33,7 +33,7 @@ public:
      * Calculates our width and height and
      * our children's parentPosition.
      */
-    virtual void calcSizes( const ContextStyle& style,
+    virtual void calcSizes( const ContextStyle& context,
 						    ContextStyle::TextStyle tstyle,
 						    ContextStyle::IndexStyle istyle,
 							StyleAttributes& style );

4bd615e32f5f4ba0823a2238ec61a0f9a9531cfe
Cyrille Berger <cyb@lepi.org>
2007-01-08 00:11:35 +0000

    new feature : bracketing2hdr, convert a set of LDR images taken in bracketing to an HDR images. For now only the linear response is implemented.
    svn path=/trunk/koffice/; revision=621002

---
+++ krita/plugins/viewplugins/bracketing2hdr/bracketing2hdr.cc
@@ -0,0 +1,327 @@
+/*
+ *
+ *  Copyright (c) 2007 Cyrille Berger (cberger@cberger.net)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "bracketing2hdr.h"
+
+#include <stdlib.h>
+
+#include <kdebug.h>
+#include <kfiledialog.h>
+#include <kgenericfactory.h>
+#include <kinstance.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <kstandarddirs.h>
+
+#include <KoColorSpaceRegistry.h>
+
+#include "kis_config.h"
+#include "kis_cursor.h"
+#include "kis_doc2.h"
+#include "kis_exif_info.h"
+#include "kis_global.h"
+#include "kis_group_layer.h"
+#include "kis_image.h"
+#include "kis_meta_registry.h"
+#include "kis_paint_device.h"
+#include "kis_paint_layer.h"
+#include "kis_types.h"
+#include "kis_view2.h"
+
+#include "ui_wdgbracketing2hdr.h"
+
+typedef KGenericFactory<Bracketing2HDRPlugin> Bracketing2HDRPluginFactory;
+K_EXPORT_COMPONENT_FACTORY( kritabracketing2hdr, Bracketing2HDRPluginFactory( "krita" ) )
+
+
+Bracketing2HDRPlugin::Bracketing2HDRPlugin(QObject *parent, const QStringList &)
+    : KParts::Plugin(parent), m_wdgBracketing2HDR(0),
+    m_responseType(RESPONSE_LINEAR), m_bitDepth(8), m_numberOfInputLevels(2 << (m_bitDepth-1))
+{
+    if ( parent->inherits("KisView2") )
+    {
+        m_view = (KisView2*) parent;
+
+        setInstance(Bracketing2HDRPluginFactory::instance());
+
+        setXMLFile(KStandardDirs::locate("data","kritaplugins/bracketing2hdr.rc"), true);
+
+        KAction *action = new KAction(i18n("HDR Layer from bracketing"), actionCollection(), "Bracketing2HDR");
+        connect(action, SIGNAL(triggered()), this, SLOT(slotNewHDRLayerFromBracketing()));
+    }
+}
+
+Bracketing2HDRPlugin::~Bracketing2HDRPlugin()
+{
+    m_view = 0;
+}
+
+void Bracketing2HDRPlugin::addImage(const QString& filename)
+{
+    KisDoc2 d;
+    d.import(filename);
+    KisImageSP importedImage = d.currentImage();
+    KisPaintDeviceSP projection = 0;
+    if(importedImage)
+    {
+        projection = importedImage->projection();
+    }
+    if(!projection)
+    {
+        kDebug() << "Image " << filename << " has fail to load." << endl;
+        return;
+    }
+    KisExifInfo* exifInfo = projection->exifInfo();
+    ExifValue v;
+    bool found = exifInfo->getValue("ExposureTime", v); // type = 5
+    kDebug() << " Exposure Time : " << found << " " << v.toString() << " " << v.type() << endl;
+    double exposure = 0.;
+    if(found)
+    {
+        KisExifRational r = v.asRational(0);
+        exposure = r.numerator / (double) r.denominator;
+        kDebug() << exposure << " = " <<  r.numerator << " / " << r.denominator << endl;
+    }
+    found = exifInfo->getValue("ApertureValue", v); // type = 5
+    kDebug() << " Aperture : " << found << " " << v.toString() << " " << v.type() << endl;
+    double aperture = 0.;
+    if(found)
+    {
+        KisExifRational r = v.asRational(0);
+        aperture = r.numerator / (double) r.denominator;
+        aperture = pow(2.0, aperture * 0.5 );
+        kDebug() << aperture << " = " << r.numerator << " / " << r.denominator << endl;
+    }
+    found = exifInfo->getValue("ISOSpeedRatings", v); // type = 3
+    kDebug() << " ISO : " << found << " " << v.toString() << " " << v.type() << endl;
+    qint32 iso = 100;
+    if(found)
+    {
+        iso = v.asShort(0);
+        kDebug() << iso << endl;
+    }
+    int index = m_wdgBracketing2HDR->tableWidgetImages->rowCount();
+    m_wdgBracketing2HDR->tableWidgetImages->insertRow( index );
+    m_wdgBracketing2HDR->tableWidgetImages->setItem(index, 0, new QTableWidgetItem(filename)); // TODO: generate a preview
+    m_wdgBracketing2HDR->tableWidgetImages->setItem(index, 1, new QTableWidgetItem(QString::number(exposure)));
+    m_wdgBracketing2HDR->tableWidgetImages->setItem(index, 2, new QTableWidgetItem(QString::number(aperture)));
+    m_wdgBracketing2HDR->tableWidgetImages->setItem(index, 3, new QTableWidgetItem(QString::number(iso)));
+}
+
+void Bracketing2HDRPlugin::slotAddImages()
+{
+    kDebug() << "Add image" << endl;
+    QStringList openfiles = KFileDialog::getOpenFileNames(KUrl(),"*", m_view);
+    kDebug() << openfiles.size() << " files selected for inclusion" << endl;
+    QString filename;
+    foreach(filename, openfiles)
+    {
+        addImage( filename );
+    }
+}
+
+void Bracketing2HDRPlugin::slotNewHDRLayerFromBracketing()
+{
+    if(m_wdgBracketing2HDR) delete m_wdgBracketing2HDR;
+    m_wdgBracketing2HDR = new Ui_WdgBracketing2HDR();
+    QDialog* dialog = new QDialog(m_view);
+    dialog->setModal(true);
+    m_wdgBracketing2HDR->setupUi(dialog);
+    connect(m_wdgBracketing2HDR->pushButtonCancel, SIGNAL(released()), dialog, SLOT(reject ()));
+    connect(m_wdgBracketing2HDR->pushButtonCreateHDRLayer, SIGNAL(released()), dialog, SLOT(accept ()));
+    connect(m_wdgBracketing2HDR->pushButtonAddImages, SIGNAL(released()), this, SLOT(slotAddImages()));
+    connect(m_wdgBracketing2HDR->pushButtonCalculateCameraResponse, SIGNAL(released()), this, SLOT(computeCameraResponse()));
+    if(dialog->exec()==QDialog::Accepted)
+    {
+        kDebug() << "Start creating the HDR layer" << endl;
+        computeCameraResponse();
+        KisImageSP img = m_view->image();
+        KoColorSpace* cs = KisMetaRegistry::instance()->csRegistry()->colorSpace("RGBAF32", 0);
+        if(!cs)
+        {
+            KMessageBox::error(m_view, i18n("No HDR colorspace found, please check your installation."), i18n("Layer Error"));
+            return;
+        }
+        KisPaintLayerSP layer = new KisPaintLayer(img, i18n("HDR Layer"), 255, cs);
+        Q_ASSERT(layer);
+        KisGroupLayerSP parent;
+        KisLayerSP above;
+        if (KisGroupLayer* pactive = qobject_cast<KisGroupLayer*>(m_view->image()->activeLayer().data()))
+        {
+            parent = pactive;
+        }
+        if(!parent)
+            parent = m_view->image()->rootLayer();
+        above = parent->firstChild();
+        img->addLayer(layer, parent, above);
+        
+        KisPaintDeviceSP dstdev = layer->paintDevice();
+        int width = m_images[0].image->width();
+        int height = m_images[0].image->height();
+        for(int k = 0; k < m_images.size(); k++)
+        {
+            m_images[k].it = new KisHLineConstIteratorPixel( m_images[k].device->createHLineConstIterator(0, 0, width) );
+        }
+        KisHLineIteratorPixel dstIt = dstdev->createHLineIterator(0, 0, width);
+        QColor c;
+        struct { double rsum, rdiv, gsum, gdiv, bsum, bdiv;  } v;
+        for(int j = 0; j < height; j++)
+        {
+            while(!dstIt.isDone())
+            {
+                v.rsum = v.rdiv = v.gsum = v.gdiv = v.bsum = v.bdiv = 0.0;
+                for(int k = 0; k < m_images.size(); k++)
+                {
+                    double coef = m_images[k].apexBrightness;
+                    m_images[k].device->colorSpace()->toQColor(m_images[k].it->rawData(), &c);
+                    v.rsum += m_intensityR[ c.red() ] * m_weights[ c.red() ] * coef ;
+                    v.rdiv += m_weights[ c.red() ] * coef * coef ;
+                    v.gsum += m_intensityG[ c.green() ] * m_weights[ c.green() ] * coef ;
+                    v.gdiv += m_weights[ c.green() ] * coef * coef ;
+                    v.bsum += m_intensityB[ c.blue() ] * m_weights[ c.blue() ] * coef ;
+                    v.bdiv += m_weights[ c.blue() ] * coef * coef ;
+                    ++(*m_images[k].it);
+                }
+                float* pixelData = reinterpret_cast<float*>(dstIt.rawData());
+                if( v.rdiv != 0.0)
+                {
+                    pixelData[2] = v.rsum / v.rdiv;
+                } else {
+                    pixelData[2] = 0.0;
+                }
+                if( v.gdiv != 0.0)
+                {
+                    pixelData[1] = v.gsum / v.gdiv;
+                } else {
+                    pixelData[1] = 0.0;
+                }
+                if( v.bdiv != 0.0)
+                {
+                    pixelData[0] = v.bsum / v.bdiv;
+                } else {
+                    pixelData[0] = 0.0;
+                }
+                dstdev->colorSpace()->setAlpha(dstIt.rawData(), 255, 1);
+                ++dstIt;
+            }
+            dstIt.nextRow();
+            for(int k = 0; k < m_images.size(); k++)
+            {
+                m_images[k].it->nextRow();
+            }
+        }
+        
+        for(int k = 0; k < m_images.size(); k++)
+        {
+            delete m_images[k].it;
+        }
+        m_view->canvas()->update();
+    }
+    delete dialog;
+}
+
+void Bracketing2HDRPlugin::computeCameraResponse()
+{
+    kDebug() << "computeCameraResponse()" << endl;
+    loadImagesInMemory();
+    switch(responseType())
+    {
+        case RESPONSE_LINEAR:
+            computeLinearResponse(m_intensityR);
+            computeLinearResponse(m_intensityG);
+            computeLinearResponse(m_intensityB);
+            computePseudoGaussianWeights();
+            break;
+        default:
+            kError() << "NOT IMPLEMENTED YET !" << endl;
+            Q_ASSERT(false);
+    }
+}
+
+bool Bracketing2HDRPlugin::loadImagesInMemory()
+{
+    m_images.clear();
+    for(int i = 0; i < m_wdgBracketing2HDR->tableWidgetImages->rowCount(); i++)
+    {
+        // read the info about the frame in the table
+        QString fileName = m_wdgBracketing2HDR->tableWidgetImages->item(i,0)->text();
+    
+//     $Av = 2 * log($aperture) / log(2);
+//     $Tv = log($inv_exposure_time) / log(2);
+//     $Sv = log( $iso_speed / 3.125 ) / log(2);
+// 
+//     ## Brightness value
+//     $Bv = $Av+$Tv-$Sv;
+
+        BracketingFrame f;
+        f.exposure = m_wdgBracketing2HDR->tableWidgetImages->item(i,1)->text().toDouble();
+        f.aperture = m_wdgBracketing2HDR->tableWidgetImages->item(i,2)->text().toDouble();
+        f.sensitivity = m_wdgBracketing2HDR->tableWidgetImages->item(i,3)->text().toInt();
+        
+        f.apexBrightness = 2.0 * log(f.aperture ) + log( 1.0 / f.exposure) - log( f.sensitivity / 3.125 );
+        f.apexBrightness /= log(2.0);
+        kDebug() << f.apexBrightness << endl;
+        f.apexBrightness = 1.0 / ( powf(2.0, f.apexBrightness) * ( 1.0592f * 11.4f / 3.125f ) ); // TODO: the magic number is apparrently dependent of the camera, this value is taken from pfscalibrate, this need to be configurable
+        
+        kDebug() << "Loading fileName " << fileName << " Exposure = " << f.exposure << " Adjusted exposure = " << f.apexBrightness << " Aperture " << f.aperture << " Sensitivity = " << f.sensitivity << endl;
+        // import the image
+        KisDoc2 d;
+        d.import(fileName);
+        f.image = d.currentImage();
+        f.device = 0;
+        if(f.image)
+        {
+            f.device = f.image->projection();
+        }
+        if(!f.device)
+        {
+            kDebug() << "Image " << fileName << " has fail to load." << endl;
+            return false;
+        }
+        // compute the maximum and minimu response
+        // add the frame to the list of images
+        m_images.push_back(f);
+    }
+    return true;
+}
+
+void Bracketing2HDRPlugin::computeLinearResponse(QVector<double>& intensity)
+{
+    intensity.resize(numberOfInputLevels());
+    for(int i = 0; i < numberOfInputLevels(); i++)
+    {
+        intensity[i] = i * (1. / numberOfInputLevels());
+    }
+}
+
+
+void Bracketing2HDRPlugin::computePseudoGaussianWeights()
+{
+    m_weights.resize(numberOfInputLevels());
+    for(int i = 0; i < numberOfInputLevels(); i++)
+    {
+        double v = (i - 127.5) * ( 1. / 127.5);
+        m_weights[i] = exp( -8.0 * v * v );
+        if( m_weights[i] < 1e-3)
+            m_weights[i] = 0.;
+    }
+}
+
+
+#include "bracketing2hdr.moc"
---
+++ krita/plugins/viewplugins/bracketing2hdr/bracketing2hdr.h
@@ -0,0 +1,100 @@
+/*
+ *
+ *  Copyright (c) 2007 Cyrille Berger (cberger@cberger.net)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _BRACKETING2HDR_H_
+#define _BRACKETING2HDR_H_
+
+#include <kparts/plugin.h>
+
+#include <kis_types.h>
+#include <kis_iterators_pixel.h>
+
+class KisView2;
+
+class Ui_WdgBracketing2HDR;
+
+/**
+ * Template of view plugin
+ */
+class Bracketing2HDRPlugin : public KParts::Plugin
+{
+    Q_OBJECT
+    public:
+        enum ResponseType {
+            RESPONSE_AUTO,
+            RESPONSE_LINEAR,
+            RESPONSE_GAMMA,
+            RESPONSE_LOG10
+        };
+        struct BracketingFrame {
+            double exposure; // in ms
+            double apexBrightness;
+            double aperture;
+            int sensitivity; // in iso
+            KisImageSP image;
+            KisPaintDeviceSP device;
+            KisHLineConstIteratorPixel* it;
+        };
+    public:
+        Bracketing2HDRPlugin(QObject *parent, const QStringList &);
+        virtual ~Bracketing2HDRPlugin();
+    
+        /**
+         * This function gives access to the response type. This is is an important parameter,
+         * as it controls how the light is perceive by the camera.
+         * @return the response type
+         */
+        inline ResponseType responseType() { return m_responseType; }
+        
+        /**
+         * @return the bit depth use for the computation, only 8bits is supported for now
+         */
+        inline int bitDepth() { return m_bitDepth; }
+        /**
+         * @return the number of input level, it should be equal to 2 ^ bitDepth()
+         */
+        inline int numberOfInputLevels() {
+            Q_ASSERT( m_numberOfInputLevels == (2 << (bitDepth()-1)));
+            return m_numberOfInputLevels;
+        }
+    private slots:
+        void slotNewHDRLayerFromBracketing();
+        void slotAddImages();
+        void addImage(const QString& filename);
+        
+        void computeCameraResponse();
+    private:
+        /// Initialize a linear response table
+        void computeLinearResponse(QVector<double>& intensity);
+        void computePseudoGaussianWeights();
+        /// load the images in memory
+        bool loadImagesInMemory();
+    private:
+        KisView2 * m_view;
+        Ui_WdgBracketing2HDR* m_wdgBracketing2HDR;
+        QList<BracketingFrame> m_images;
+        // Camera response
+        QVector<double> m_intensityR, m_intensityG, m_intensityB, m_weights;
+        // Options for the calibration
+        ResponseType m_responseType;
+        int m_bitDepth;
+        int m_numberOfInputLevels;
+};
+
+#endif // Bracketing2HDRPlugin_H

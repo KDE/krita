2db240b24f38f3865ffe21fdf6cf57347a6f2101
Thomas Zander <zander@kde.org>
2007-01-15 11:37:12 +0000

    Add ui file, in hopes that someone can find what I'm doing wrong in the CMakeLists file since it refuses to uic among other problems.
    svn path=/trunk/koffice/; revision=623727

---
+++ plugins/variables/FixedDateFormat.h
@@ -0,0 +1,37 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2006 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef FIXEDDATEFORMAT_H
+#define FIXEDDATEFORMAT_H
+
+#include <QWidget>
+
+#include "ui_FixedDateFormat.h"
+
+class KoDateVariable;
+
+class FixedDateFormat : public QWidget {
+public:
+    FixedDateFormat(KoDateVariable *variable);
+
+private:
+    Ui::FixedDateFormat widget;
+    KoDateVariable *m_variable;
+};
+
+#endif

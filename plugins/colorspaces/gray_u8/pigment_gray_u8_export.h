1bce6c2b7e2a2e191cb887ce7c2c08177a8c41a2
David Faure <faure@kde.org>
2007-01-18 15:38:26 +0000

    LGPLv2 -> LGPLv2+ in the files I just added.
    svn path=/trunk/koffice/; revision=624970

--- plugins/colorspaces/gray_u8/pigment_gray_u8_export.h
+++ plugins/colorspaces/gray_u8/pigment_gray_u8_export.h
@@ -3,7 +3,8 @@
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
-    License version 2 as published by the Free Software Foundation.
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of

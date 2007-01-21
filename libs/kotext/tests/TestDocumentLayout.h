f8cf7156e90f7e50059c69abde3cb642963f5c5c
Thomas Zander <zander@kde.org>
2007-01-21 16:18:35 +0000

    Remove tests for code that no longer lives here
    svn path=/trunk/koffice/; revision=625922

--- libs/kotext/tests/TestDocumentLayout.h
+++ libs/kotext/tests/TestDocumentLayout.h
@@ -15,8 +15,6 @@ class MockTextShape;
 class QTextDocument;
 class QTextLayout;
 
-#define ROUNDING 0.126
-
 class TestDocumentLayout : public QObject {
     Q_OBJECT
 public:
@@ -28,50 +26,6 @@ private slots:
     /// Test the hittest of KoTextDocumentLayout
     void testHitTest();
 
-    /// make sure empty paragraphs are initialized properly
-    void testEmptyParag();
-
-    /// Test breaking lines based on the width of the shape.
-    void testLineBreaking();
-    /// Test breaking lines for frames with different widths.
-    void testMultiFrameLineBreaking();
-    /// Tests incrementing Y pos based on the font size
-    void testBasicLineSpacing();
-    /// Tests incrementing Y pos based on the font size
-    void testBasicLineSpacing2();
-    /// Tests advanced linespacing options provided in our style.
-    void testAdvancedLineSpacing();
-
-// Block styles
-    /// Test top, left, right and bottom margins of paragraphs.
-    void testMargins();
-    void testMultipageMargins();
-    void testTextIndent();
-    void testBasicTextAlignments();
-    void testTextAlignments();
-    void testPageBreak();
-    void testPageBreak2();
-
-// Lists
-    void testBasicList();
-    void testNumberedList();
-    void testInterruptedLists(); // consecutiveNumbering
-    void testNestedLists();
-    void testAutoRestartList();
-    void testListParagraphIndent();
-    void testRomanNumbering();
-    void testUpperAlphaNumbering();
-    void testRestartNumbering();
-
-
-
-// relativeBulletSize
-
-    //etc
-    void testParagOffset();
-    void testParagraphBorders();
-    void testBorderData();
-
 private:
     void initForNewTest(const QString &initText = QString());
 
@@ -81,9 +35,7 @@ private:
     MockTextShape *shape1;
     QTextDocument *doc;
     KoTextDocumentLayout *layout;
-    QTextLayout *blockLayout;
     QString loremIpsum;
-    KoStyleManager *styleManager;
 };
 
 class MockTextShape : public KoShape {

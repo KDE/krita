e98354f433dd1a534d22f9eae75dd83f87ec5570
Thomas Zander <zander@kde.org>
2007-01-27 11:02:33 +0000

    Fix Copyright headers
    svn path=/trunk/koffice/; revision=627599

--- libs/kotext/DateFormatWidget.cpp
+++ libs/kotext/DateFormatWidget.cpp
@@ -1,3 +1,23 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2002 Montel Laurent <lmontel@mandrakesoft.com>
+ * Copyright (C) 2002 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include "ui_timedateformatwidget.h"
 #include "DateFormatWidget.h"
 #include "DateFormatWidget.moc"

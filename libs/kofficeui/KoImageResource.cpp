5ac64a52c8a8ed385c6f9a9b779e868c981bec0a
Thomas Zander <zander@kde.org>
2007-01-27 11:19:48 +0000

    I'm wondering if this will fix the EBN warning
    svn path=/trunk/koffice/; revision=627603

--- libs/kofficeui/KoImageResource.cpp
+++ libs/kofficeui/KoImageResource.cpp
@@ -1,7 +1,7 @@
 /*
  *  koImageResource.cc - part of KOffice
  *
- *  Copyright (c) 2005 Thomas Zander <zander@kde.org>
+ *  Copyright (C) 2005 Thomas Zander <zander@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public

4f86bec32fa065ebfdb8ea054a782063fb9dcfba
Laurent Montel <montel@kde.org>
2007-03-30 15:11:08 +0000

    Start to fix enable-final
    svn path=/trunk/koffice/; revision=648120

--- libs/kotext/KoVariable.h
+++ libs/kotext/KoVariable.h
@@ -16,6 +16,8 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
+#ifndef __KOVARIABLE_H__
+#define __KOVARIABLE_H__
 
 // KOffice libs
 #include "KoInlineObject.h"
@@ -99,3 +101,5 @@ private:
 private:
     VariablePrivate * const d;
 };
+#endif
+

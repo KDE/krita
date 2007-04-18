a87f54ae6f13a689a3ce4aa9d9b2f69e52e9323a
Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
2007-04-18 15:33:03 +0000

    Remove attribute handling code handled by AttributeManager now
    svn path=/trunk/koffice/; revision=655494

--- libs/kformula/TokenStyleElement.h
+++ libs/kformula/TokenStyleElement.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2006 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
+   Copyright (C) 2006-2007 Alfredo Beaumont Sainz <alfredo.beaumont@gmail.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -21,7 +21,7 @@
 #define TOKENSTYLEELEMENT_H
 
 #include "kformuladefs.h"
-#include "SequenceElement.h"
+#include "RowElement.h"
 
 KFORMULA_NAMESPACE_BEGIN
 
@@ -32,135 +32,18 @@ KFORMULA_NAMESPACE_BEGIN
  * It is in charge of reading and saving elements' attributes and setting
  * rendering information according to these attributes.
  */
-class TokenStyleElement : public SequenceElement {
-    typedef SequenceElement inherited;
+class TokenStyleElement : public RowElement {
+    typedef RowElement inherited;
 public:
 
     TokenStyleElement( BasicElement* parent = 0 );
 
-    virtual void calcSizes( const ContextStyle& context,
-                            ContextStyle::TextStyle tstyle,
-                            ContextStyle::IndexStyle istyle,
-                            StyleAttributes& style );
-
-    virtual void draw( QPainter& painter, const LuPixelRect& r,
-                       const ContextStyle& context,
-                       ContextStyle::TextStyle tstyle,
-                       ContextStyle::IndexStyle istyle,
-                       StyleAttributes& style,
-                       const LuPixelPoint& parentOrigin );
-
 protected:
-//    virtual bool readAttributesFromMathMLDom( const QDomElement &element );
-    virtual void writeMathMLAttributes( QDomElement& element ) const ;
-
-    void setAbsoluteSize( double s, bool fontsize = false );
-    void setRelativeSize( double s, bool fontsize = false );
-    void setPixelSize( double s, bool fontsize = false );
-
-    void setCharStyle( CharStyle cs ) { 
-        m_charStyle = cs;
-        m_customMathVariant = true; 
-    }
-    CharStyle charStyle() const { return m_charStyle; }
-
-    void setCharFamily( CharFamily cf ) { 
-        m_charFamily = cf; 
-        m_customMathVariant = true;
-    }
-    CharFamily charFamily() const { return m_charFamily; }
-
-    void setMathColor( const QColor& c ) {
-        m_mathColor = c; 
-        m_customMathColor = true;
-    }
-    QColor mathColor() const { return m_mathColor; }
-
-    void setMathBackground( const QColor& bg ) { 
-        m_mathBackground = bg; 
-        m_customMathBackground = true;
-    }
-    QColor mathBackground() const { return m_mathBackground; }
-
-    void setFontWeight( bool w ) { 
-        m_fontWeight = w;
-        m_customFontWeight = true; 
-    }
-    bool fontWeight() const { return m_fontWeight; }
-
-    void setFontStyle( bool s ) { 
-        m_fontStyle = s;
-        m_customFontStyle = true;
-    }
-    bool fontStyle() const { return m_fontStyle; }
-
-    void setFontFamily( const QString& s ) { 
-        m_fontFamily = s;
-        m_customFontFamily = true;
-    }
-    QString fontFamily() const { return m_fontFamily; }
-
-    void setColor( const QColor& c ) { 
-        m_color = c; 
-        m_customColor = true;
-    }
-    QColor color() const { return m_color; }
-
-    bool customMathVariant() const { return m_customMathVariant; }
-    bool customMathColor() const { return m_customMathColor; }
-    bool customMathBackground() const { return m_customMathBackground; }
-    bool customFontWeight() const { return m_customFontWeight; }
-    bool customFontStyle() const { return m_customFontStyle; }
-    bool customFontFamily() const { return m_customFontFamily; }
-    bool customColor() const { return m_customColor; }
-
-    virtual void setStyleSize( const ContextStyle& context, StyleAttributes& style );
-    /**
-     * Set the mathvariant related info in style stacks, including info for
-     * deprecated attributes. It may be redefined by token elements whose
-     * behaviour differs from default one (e.g. identifiers)
-     */
-    virtual void setStyleVariant( StyleAttributes& style );
-
-    void setStyleColor( StyleAttributes& style );
-    virtual void setStyleBackground( StyleAttributes& style );
-
-    virtual void resetStyle( StyleAttributes& style );
     /**
      * Return RGB string from HTML Colors. See HTML Spec, section 6.5
      */
     QString getHtmlColor( const QString& colorStr );
 
-private:
-
-    double sizeFactor( const ContextStyle& context, double factor );
-
-    // MathML 2.0 attributes
-    SizeType m_mathSizeType;
-    double m_mathSize;
-    CharStyle m_charStyle;
-    CharFamily m_charFamily;
-    QColor m_mathColor;
-    QColor m_mathBackground;
-
-    // Deprecated MathML 1.01 attributes
-    SizeType m_fontSizeType;
-    double m_fontSize;
-    QString m_fontFamily;
-    QColor m_color;
-    bool m_fontWeight;
-    bool m_fontStyle;
-
-    // MathML 2.0 attributes set ?
-    bool m_customMathVariant;
-    bool m_customMathColor;
-    bool m_customMathBackground;
-
-    // Deprecated MathML 1.01 attributes set ?
-    bool m_customFontWeight;
-    bool m_customFontStyle;
-    bool m_customFontFamily;
-    bool m_customColor;
 };
 
 KFORMULA_NAMESPACE_END

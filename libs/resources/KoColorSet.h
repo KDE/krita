902591d1df29ea0ccb882eb35b462a76a4ebcb36
Thomas Zander <zander@kde.org>
2007-04-29 17:51:30 +0000

    Fix license header
    svn path=/trunk/koffice/; revision=659207

--- libs/resources/KoColorSet.h
+++ libs/resources/KoColorSet.h
@@ -1,10 +1,10 @@
-/*
+/*  This file is part of the KDE project
     Copyright (c) 2005 Boudewijn Rempt <boud@valdyas.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+    version 2 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of

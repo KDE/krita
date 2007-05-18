2dc63f2fa85bd47f9e6289a3c1d8a26828326e9e
Boudewijn Rempt <boud@valdyas.org>
2007-05-18 08:36:57 +0000

    In the example filter, don't use the rect iterators anymore and some stuff to show off different ways of accessing pixels. Surprisingly, per-pixel isSelected isn't even that slow.
    svn path=/trunk/koffice/; revision=665853

--- krita/plugins/filters/example/example.cc
+++ krita/plugins/filters/example/example.cc
@@ -20,6 +20,7 @@
 #include <vector>
 
 #include <QPoint>
+#include <QTime>
 
 #include <klocale.h>
 #include <kiconloader.h>
@@ -31,12 +32,13 @@
 
 #include <KoColorTransformation.h>
 
+#include <kis_types.h>
+#include <kis_selection.h>
 #include <kis_image.h>
 #include <kis_iterators_pixel.h>
 #include <kis_layer.h>
 #include <kis_filter_registry.h>
 #include <kis_global.h>
-#include <kis_types.h>
 
 // #include <kmessagebox.h>
 
@@ -70,8 +72,8 @@ void KisFilterInvert::process(const KisPaintDeviceSP src, const QPoint& srcTopLe
     Q_ASSERT(!src.isNull());
     Q_ASSERT(!dst.isNull());
 
-    KisRectIteratorPixel dstIt = dst->createRectIterator(dstTopLeft.x(), dstTopLeft.y(), size.width(), size.height());
-    KisRectConstIteratorPixel srcIt = src->createRectConstIterator(srcTopLeft.x(), srcTopLeft.y(), size.width(), size.height());
+    KisHLineIteratorPixel dstIt = dst->createHLineIterator(dstTopLeft.x(), dstTopLeft.y(), size.width());
+    KisHLineConstIteratorPixel srcIt = src->createHLineConstIterator(srcTopLeft.x(), srcTopLeft.y(), size.width());
 
     int pixelsProcessed = 0;
     setProgressTotalSteps(size.width() * size.height());
@@ -79,16 +81,106 @@ void KisFilterInvert::process(const KisPaintDeviceSP src, const QPoint& srcTopLe
     KoColorSpace * cs = src->colorSpace();
 
     KoColorTransformation* inverter = cs->createInvertTransformation();
-    while( ! srcIt.isDone() )
-    {
-        if(srcIt.isSelected())
-        {
-            inverter->transform( srcIt.oldRawData(), dstIt.rawData(), 1);
+
+// Method one: iterate and check every pixel for selectedness. It is
+// only slightly slower than the next method and the code is very
+// clear. Note that using nextRow() instead of recreating the iterators
+// for every row makes a huge difference.
+#if 0
+    for ( int row = 0; row < size.height(); ++row ) {
+        while ( !srcIt.isDone() ) {
+            if ( srcIt.isSelected() ) {
+                inverter->transform( srcIt.oldRawData(), dstIt.rawData(), 1);
+            }
+            ++srcIt;
+            ++dstIt;
+
         }
-        setProgress(++pixelsProcessed);
-        ++srcIt;
-        ++dstIt;
+        srcIt.nextRow();
+        dstIt.nextRow();
     }
+
+
+#endif
+
+    bool hasSelection = src->hasSelection();
+
+// Method two: check the number of consecutive pixels the iterators
+// points to. Take as large stretches of unselected pixels as possible
+// and pass those to the color space transform object in one go. The
+// speed advantage in this case is slight: littlecms takes most of the
+// time, not the iteration.
+    for (int row = 0; row < size.height(); ++row) {
+        while( ! srcIt.isDone() )
+        {
+            int srcItConseq = srcIt.nConseqHPixels();
+            int dstItConseq = srcIt.nConseqHPixels();
+            int conseqPixels = qMin( srcItConseq, dstItConseq );
+
+            int pixels = 0;
+
+            if ( hasSelection ) {
+                // Get largest horizontal row of selected pixels
+
+                while ( srcIt.isSelected() && pixels < conseqPixels ) {
+                    ++pixels;
+                }
+                inverter->transform( srcIt.oldRawData(), dstIt.rawData(), pixels);
+
+                // We apparently found a non-selected pixels, or the row
+                // was done; get the stretch of non-selected pixels
+                while ( !srcIt.isSelected() && pixels < conseqPixels ) {
+                    ++ pixels;
+                }
+            }
+            else {
+                pixels = conseqPixels;
+                inverter->transform( srcIt.oldRawData(), dstIt.rawData(), pixels );
+            }
+
+            // Update progress
+            srcIt += pixels;
+            dstIt += pixels;
+        }
+        srcIt.nextRow();
+        dstIt.nextRow();
+    }
+
+// Method three (incomplete, the case where there's a selection still
+// needs to be handled). It should be the fastest one because we cache
+// whole scanlines. In fact, it isn't faster than method 2 at all.
+#if 0
+    quint32 w = size.width();
+    quint32 h = size.height();
+    quint32 length = w * src->pixelSize();
+
+    // Allocate the buffers for the scanlines
+    quint8 * srcPixels = new quint8[length];
+    quint8 * selPixels = 0;
+    if ( hasSelection )
+         selPixels = new quint8[w * src->selection()->pixelSize()];
+    quint8 * dstPixels = new quint8[length];
+
+    for ( int row = 0; row < h; ++row ) {
+        // Prepare the scanlines
+        const_cast<KisPaintDevice*>( src.data() )->readBytes( srcPixels, dstTopLeft.x(), dstTopLeft.y() + row, w, 1 );
+        memset( dstPixels, 0, length );
+
+        // If there's a selection, iterate over the selection to find
+        // the stretches of non selected pixels and selected pixels.
+        if ( hasSelection ) {
+            const_cast<KisSelection*>( src->selection().data() )->readBytes( selPixels, dstTopLeft.x(), dstTopLeft.y() + row, size.width(), 1 );
+            // XXX: iterate over the selection bytes to determine
+            // the selected stretches
+        }
+        else {
+            inverter->transform( srcPixels, dstPixels, w );
+        }
+        dst->writeBytes( dstPixels, dstTopLeft.x(), dstTopLeft.y() + row, w, 1 );
+
+    }
+#endif
+
     delete inverter;
     setProgressDone(); // Must be called even if you don't really support progression
 }

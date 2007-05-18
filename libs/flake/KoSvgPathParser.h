de0be9a50358e17f8438f72107ad212e21ec652e
Jan Hambrecht <jaham@gmx.net>
2007-05-18 16:06:25 +0000

    move svgpathparser into flake so i can use it there to load pathes from odf
    svn path=/trunk/koffice/; revision=666042

--- libs/flake/KoSvgPathParser.h
+++ libs/flake/KoSvgPathParser.h
@@ -17,12 +17,12 @@
  * Boston, MA 02110-1301, USA.
 */
 
-#ifndef __SVGPATHPARSER_H__
-#define __SVGPATHPARSER_H__
+#ifndef __KOSVGPATHPARSER_H__
+#define __KOSVGPATHPARSER_H__
 
 class QString;
 
-#include <kopainter_export.h>
+#include <flake_export.h>
 /**
  * Parser for svg path data, contained in the d attribute.
  *
@@ -34,27 +34,27 @@ class QString;
  * svg path data commands and parameters. In the second mode, it will convert all
  * relative coordinates to absolute ones, and convert all curves to cubic beziers.
  */
-class KOPAINTER_EXPORT SVGPathParser
+class FLAKE_EXPORT KoSvgPathParser
 {
 public:
-	virtual ~SVGPathParser(){}
-	void parseSVG( const QString &d, bool process = false );
+    virtual ~KoSvgPathParser(){}
+    void parseSVG( const QString &d, bool process = false );
 
 protected:
-	virtual void svgMoveTo( double x1, double y1, bool abs = true ) = 0;
-	virtual void svgLineTo( double x1, double y1, bool abs = true ) = 0;
-	virtual void svgLineToHorizontal( double x, bool abs = true );
-	virtual void svgLineToVertical( double y, bool abs = true );
-	virtual void svgCurveToCubic( double x1, double y1, double x2, double y2, double x, double y, bool abs = true ) = 0;
-	virtual void svgCurveToCubicSmooth( double x, double y, double x2, double y2, bool abs = true );
-	virtual void svgCurveToQuadratic( double x, double y, double x1, double y1, bool abs = true );
-	virtual void svgCurveToQuadraticSmooth( double x, double y, bool abs = true );
-	virtual void svgArcTo( double x, double y, double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag, bool abs = true );
-	virtual void svgClosePath() = 0;
+    virtual void svgMoveTo( double x1, double y1, bool abs = true ) = 0;
+    virtual void svgLineTo( double x1, double y1, bool abs = true ) = 0;
+    virtual void svgLineToHorizontal( double x, bool abs = true );
+    virtual void svgLineToVertical( double y, bool abs = true );
+    virtual void svgCurveToCubic( double x1, double y1, double x2, double y2, double x, double y, bool abs = true ) = 0;
+    virtual void svgCurveToCubicSmooth( double x, double y, double x2, double y2, bool abs = true );
+    virtual void svgCurveToQuadratic( double x, double y, double x1, double y1, bool abs = true );
+    virtual void svgCurveToQuadraticSmooth( double x, double y, bool abs = true );
+    virtual void svgArcTo( double x, double y, double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag, bool abs = true );
+    virtual void svgClosePath() = 0;
 
 private:
-	const char *getCoord( const char *, double & );
-	void calculateArc( bool relative, double &curx, double &cury, double angle, double x, double y, double r1, double r2, bool largeArcFlag, bool sweepFlag );
+    const char *getCoord( const char *, double & );
+    void calculateArc( bool relative, double &curx, double &cury, double angle, double x, double y, double r1, double r2, bool largeArcFlag, bool sweepFlag );
 };
 
 #endif

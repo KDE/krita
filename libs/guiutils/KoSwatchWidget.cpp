fc7102c4697f13563b13d7f0cc77ce36c4d150c4
Casper Boemann <cbr@boemann.dk>
2007-05-26 15:33:41 +0000

    First try on a new swatch widget - basically only an empty shell still
    Please create and add an action to KSPread so we can test this during 
    developement!
    
    
    svn path=/trunk/koffice/; revision=668481

---
+++ libs/guiutils/KoSwatchWidget.cpp
@@ -0,0 +1,131 @@
+/* This file is part of the KDE project
+   Copyright (c) 2007 Casper Boemann <cbr@boemann.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+#include "KoSwatchWidget.h"
+
+#include <QTimer>
+#include <QApplication>
+#include <QSize>
+#include <QSlider>
+#include <QHBoxLayout>
+#include <QFrame>
+#include <QMenu>
+#include <QMouseEvent>
+#include <QDoubleSpinBox>
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kdebug.h>
+
+class KoSwatchContainer : public QFrame
+{
+public:
+    KoSwatchContainer(KoSwatchWidget *parent) : QFrame(parent, Qt::Popup ), m_parent(parent) {}
+
+protected:
+private:
+    KoSwatchWidget *m_parent;
+};
+
+class KoSwatchWidget::KoSwatchWidgetPrivate {
+public:
+    KoSwatchWidget *thePublic;
+    QTimer m_timer;
+    KoSwatchContainer *container;
+    bool firstShowOfContainer;
+
+    void showPopup();
+    void hidePopup();
+};
+
+KoSwatchWidget::KoSwatchWidget(QWidget *parent)
+   : QPushButton(parent)
+    ,d(new KoSwatchWidgetPrivate())
+{
+    d->thePublic = this;
+    d->container = new KoSwatchContainer(this);
+    d->container->setAttribute(Qt::WA_WindowPropagation);
+
+    d->firstShowOfContainer = true;
+
+/*
+    QHBoxLayout * l = new QHBoxLayout();
+    l->setMargin(2);
+    l->setSpacing(2);
+    l->addWidget(d->slider);
+    d->container->setLayout(l);
+*/    d->container->resize(200, 20);
+
+/*
+    connect(d->slider, SIGNAL(valueChanged(int)), SLOT(sliderValueChanged(int)));
+    connect(d->slider, SIGNAL(sliderReleased()), SLOT(sliderReleased()));
+    connect(lineEdit(), SIGNAL(editingFinished()), SLOT(lineEditFinished()));
+*/
+}
+
+KoSwatchWidget::~KoSwatchWidget()
+{
+    delete d;
+}
+
+void KoSwatchWidget::KoSwatchWidgetPrivate::showPopup()
+{
+    if(firstShowOfContainer) {
+        container->show(); //show container a bit early so the slider can be layout'ed
+        firstShowOfContainer = false;
+    }
+
+    container->move(thePublic->pos());
+
+    container->raise();
+    container->show();
+}
+
+void KoSwatchWidget::KoSwatchWidgetPrivate::hidePopup()
+{
+    container->hide();
+}
+
+void KoSwatchWidget::hideEvent(QHideEvent *)
+{
+    d->hidePopup();
+}
+
+void KoSwatchWidget::enterEvent(QEvent *)
+{
+    d->showPopup();
+}
+
+void KoSwatchWidget::changeEvent(QEvent *e)
+{
+    switch (e->type())
+    {
+        case QEvent::EnabledChange:
+            if (!isEnabled())
+                d->hidePopup();
+            break;
+        case QEvent::PaletteChange:
+            d->container->setPalette(palette());
+            break;
+        default:
+            break;
+    }
+    QPushButton::changeEvent(e);
+}
+
+#include "KoSwatchWidget.moc"
---
+++ libs/guiutils/KoSwatchWidget.h
@@ -0,0 +1,80 @@
+/* This file is part of the KDE project
+   Copyright (c) 2007 Casper Boemann <cbr@boemann.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KOSWATCHWIDGET_H_
+#define KOSWATCHWIDGET_H_
+
+#include <QPushButton>
+
+#include <koguiutils_export.h>
+
+class KoColor;
+
+/**
+ * @short A colormanaged widget for chooosing a color from a swatch
+ *
+ * KoSwatchWidget is an icon for choosing colormanaged (via pigment) color. Normally it show an icon
+ * and above a small rect of the current color. When the user moves the pointer on top then a larger
+ * widget is popped up. This larger widget provides lots of functionallity to choose the color from a
+ * swatch.
+ */
+class KOGUIUTILS_EXPORT KoSwatchWidget : public QPushButton
+{
+
+    Q_OBJECT
+
+public:
+
+    /**
+     * Constructor for the widget, where color is initially blackpoint of sRGB
+     *
+     * @param parent parent QWidget
+     */
+    KoSwatchWidget(QWidget *parent=0);
+
+    /**
+     * Destructor
+     */
+    virtual ~KoSwatchWidget();
+
+public slots:
+
+signals:
+
+    /**
+     * Emitted every time the color changes (by calling setColor() or
+     * by user interaction.
+     * @param color the new color
+     * @param final if the value is final (ie not produced by the pointer moving over around)
+     */
+    void colorChanged(KoColor &color, bool final);
+
+protected:
+    virtual void enterEvent(QEvent *); ///< reimplemented from QComboBox
+    virtual void hideEvent(QHideEvent *); ///< reimplemented from QComboBox
+    virtual void changeEvent(QEvent *e); ///< reimplemented from QComboBox
+
+private:
+//    Q_PRIVATE_SLOT(d, void sliderValueChanged(int value))
+
+    class KoSwatchWidgetPrivate;
+    KoSwatchWidgetPrivate * const d;
+};
+
+#endif

d4806f0641f8efe72238b281c5eb7084b690ab6c
Cyrille Berger <cyb@lepi.org>
2007-06-04 19:46:59 +0000

    oups, clean
    svn path=/trunk/koffice/; revision=671441

--- krita/plugins/viewplugins/scripting/scripts/filters/invert.rb
+++ krita/plugins/viewplugins/scripting/scripts/filters/invert.rb
@@ -2,13 +2,6 @@ require 'Krita'
 require 'KritaFilter'
 
 def scriptProcess( src, srcTopLeft, dst, dstTopLeft, size, config )
-    puts "LOOOOOOOOOOOOOOOOOOOOOOOL"
-    puts KritaFilter.category()
-    puts src
-    puts srcTopLeft
-    puts dst
-    puts dstTopLeft
-    puts size
   for i in 0...size[1]
     itSrc = src.createHLineConstIterator( srcTopLeft[0], srcTopLeft[1] + i, size[0] )
     itDst = dst.createHLineIterator( dstTopLeft[0], dstTopLeft[1] + i, size[0] )

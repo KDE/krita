15246efe1c9d31652e6e5cbf8a3cf735b2bc4244
Thomas Zander <zander@kde.org>
2007-06-22 10:34:01 +0000

    Forgot to commit these (thanks for notifying me thosten!)
    svn path=/trunk/koffice/; revision=678819

---
+++ libs/guiutils/KoPasteController.cpp
@@ -0,0 +1,64 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2006-2007 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "KoPasteController.h"
+
+//   #include <KoTool.h>
+#include <KoCanvasBase.h>
+#include <KoToolProxy.h>
+//   #include <KoToolSelection.h>
+//
+#include <KDebug>
+#include <QAction>
+
+class KoPasteController::Private {
+public:
+    Private(KoPasteController *p, KoCanvasBase *c, QAction *a) : parent(p), canvas(c), action(a) {
+    }
+
+    void paste() {
+kDebug() << "Paste!\n";
+        if(! canvas->toolProxy()->paste()) {
+            // means paste failed
+            // TODO find a shape that can be created to hold the relevant content and load it.
+        }
+    }
+
+    void selectionChanged() {
+        // TODO connect here and enable the clipboard if we can handle the paste.
+    }
+
+    KoPasteController *parent;
+    KoCanvasBase *canvas;
+    QAction *action;
+};
+
+KoPasteController::KoPasteController(KoCanvasBase *canvas, QAction *pasteAction)
+    : QObject(pasteAction),
+    d(new Private(this, canvas, pasteAction))
+{
+    //connect(canvas->toolProxy(), SIGNAL(selectionChanged(bool)), this, SLOT(selectionChanged(bool)));
+    connect(pasteAction, SIGNAL(triggered()), this, SLOT(paste()));
+}
+
+KoPasteController::~KoPasteController() {
+    delete d;
+}
+
+#include "KoPasteController.moc"
---
+++ libs/guiutils/KoPasteController.h
@@ -0,0 +1,60 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2006-2007 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef KOPASTECONTROLLER_H
+#define KOPASTECONTROLLER_H
+
+#include "koguiutils_export.h"
+
+#include <QObject>
+
+class QAction;
+class KoCanvasBase;
+
+/**
+ * This class takes care of the paste actions integration into flake.
+ * Whenever the paste (KStandardAction::Paste) action is triggered the controller
+ * will use the currently selected tool and try to paste using that tool.
+ * Additionally; when the tool does not allow pasting (KoTool::hasSelection() returns false)
+ * the signal pasteRequested will be emitted for applications to connect to.
+ */
+class KOGUIUTILS_EXPORT KoPasteController : public QObject {
+    Q_OBJECT
+public:
+    /**
+     * Constructor
+     * @param canvas the canvas this controller will work on.
+     * @param pasteAction the action that we will listen to and respond to when it is activated.  Additionally, the
+     *     action is used as a parent for the QObject for memory management purposes.
+     */
+    KoPasteController(KoCanvasBase *canvas, QAction *pasteAction);
+    ~KoPasteController();
+
+signals:
+    /// emitted when the user pressed paste and the current tool had no selection.
+    void pasteRequested();
+
+private:
+    Q_PRIVATE_SLOT(d, void paste())
+    Q_PRIVATE_SLOT(d, void selectionChanged())
+
+    class Private;
+    Private * const d;
+};
+
+#endif

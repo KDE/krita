b0d6f85909903c0df38a8f44e576f0811b2a9162
Jan Hambrecht <jaham@gmx.net>
2007-07-06 22:23:05 +0000

    api docs fix
    svn path=/trunk/koffice/; revision=684559

--- libs/flake/commands/KoShapeTransformCommand.h
+++ libs/flake/commands/KoShapeTransformCommand.h
@@ -36,9 +36,9 @@ public:
     /**
      * A command to transform a selection of shapes with the same transformation.
      * @param shapes all the shapes that should be transformed
-     * @param transformations the transformation to apply to all shapes
+     * @param transformation the transformation to apply to all shapes
      */
-    KoShapeTransformCommand( const QList<KoShape*> &shapes, const QMatrix &transformations, QUndoCommand *parent = 0 );
+    KoShapeTransformCommand( const QList<KoShape*> &shapes, const QMatrix &transformation, QUndoCommand *parent = 0 );
 
     /**
      * A command to transform a selection of shapes with the same transformation.

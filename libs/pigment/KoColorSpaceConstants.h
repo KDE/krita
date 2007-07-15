b14a212653546e64f84eb939129c1f6b8f7a706e
Adrian Page <adrian@pagenet.plus.com>
2007-07-15 20:41:34 +0000

    Fix includes.
    svn path=/trunk/koffice/; revision=688364

--- libs/pigment/KoColorSpaceConstants.h
+++ libs/pigment/KoColorSpaceConstants.h
@@ -20,6 +20,9 @@
 #ifndef _KO_COLOR_SPACE_CONSTANTS_H_
 #define _KO_COLOR_SPACE_CONSTANTS_H_
 
+#include <climits>
+#include <qglobal.h>
+
 // TODO: find a better place or way to define those stuff
 const quint8 OPACITY_TRANSPARENT = 0;
 const quint8 OPACITY_OPAQUE = UCHAR_MAX;

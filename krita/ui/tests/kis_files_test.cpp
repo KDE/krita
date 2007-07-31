2d9856437f56e25e6aa3121cc263dc3a5e2c9125
Cyrille Berger <cyb@lepi.org>
2007-07-31 23:31:58 +0000

    add code for testing krita file loading add the "official" png test suit (that krita miserably fails, not surprisingly as it was allready known that png files needed to be at least of 64x64 to be correctly opened ;) ) and some other files that krita can open but that were causing problem in the past (and for which I know that I can make them public)
    svn path=/trunk/koffice/; revision=694881

---
+++ krita/ui/tests/kis_files_test.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2007 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_files_test.h"
+
+#include <QTest>
+#include <QCoreApplication>
+
+#include <qtest_kde.h>
+#include <kis_doc2.h>
+#include <kis_image.h>
+
+#include <ktemporaryfile.h>
+
+#ifndef FILES_DATA_DIR
+    #error "FILES_DATA_DIR not set. A directory with the data used for testing the importing of files in krita"
+#endif
+
+
+void KisFilesTest::testFiles()
+{
+    QDir dirSources ( QString(FILES_DATA_DIR) + "/sources" );
+    QDir dirResults ( QString(FILES_DATA_DIR) + "/results" );
+    foreach(QFileInfo sourceFileInfo, dirSources.entryInfoList())
+    {
+        if( not sourceFileInfo.isHidden())
+        {
+            kDebug() << "Testing : " << sourceFileInfo.absoluteFilePath() << endl;
+            QFileInfo resultFileInfo(  QString(FILES_DATA_DIR) + "/results/" + sourceFileInfo.fileName() + ".png" );
+            QVERIFY(resultFileInfo.exists());
+            KisDoc2 doc;
+            doc.import( sourceFileInfo.absoluteFilePath() );
+            QVERIFY(doc.image());
+            KTemporaryFile tmpFile;
+            tmpFile.setSuffix(".png");
+            tmpFile.open();
+            doc.setOutputMimeType("image/png");
+            doc.saveAs( "file://" + tmpFile.fileName());
+            QImage resultImg(resultFileInfo.absoluteFilePath());
+            QImage sourceImg(tmpFile.fileName());
+            kDebug() << resultImg.width() << " " << sourceImg.width() << endl;
+            QVERIFY(resultImg.width() == sourceImg.width());
+            QVERIFY(resultImg.height() == sourceImg.height());
+            QVERIFY(resultImg.numBytes() == sourceImg.numBytes());
+            QVERIFY(memcmp( resultImg.bits(), sourceImg.bits(), resultImg.numBytes() )==0);
+#if 0
+            for(int i = 0; i < sourceImg.numBytes(); i++)
+            {
+//                 kDebug() <<(int)resultImg.bits()[i] << " " << (int)sourceImg.bits()[i] << endl;
+//                 QVERIFY2(resultImg.bits()[i] == sourceImg.bits()[i], QString("pixel %1 is different").arg(i).toAscii().data());
+            }
+#endif
+        }
+    }
+}
+QTEST_KDEMAIN(KisFilesTest, GUI)
+
+#include "kis_files_test.moc"

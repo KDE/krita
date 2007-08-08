611871661630fe5b4afbb30888adbd0278de3343
Boudewijn Rempt <boud@valdyas.org>
2007-08-08 07:45:29 +0000

    Move to gplv2+ (checked with Cyrille)
    svn path=/trunk/koffice/; revision=697620

--- krita/image/kis_random_accessor.cpp
+++ krita/image/kis_random_accessor.cpp
@@ -5,7 +5,8 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License.
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -37,7 +38,7 @@ KisRandomConstAccessor::KisRandomConstAccessor(const KisRandomConstAccessor& rhs
 
 KisRandomConstAccessor::~KisRandomConstAccessor()
 {
-    
+
 }
 
 void KisRandomConstAccessor::moveTo(qint32 x, qint32 y)
--- krita/image/kis_random_sub_accessor.h
+++ krita/image/kis_random_sub_accessor.h
@@ -5,7 +5,8 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License.
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+ */
 
 #ifndef KIS_CURVE_ITERATOR_H
 #define KIS_CURVE_ITERATOR_H

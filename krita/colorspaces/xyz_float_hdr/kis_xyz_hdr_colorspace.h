9e86981be8109d6c5a28ee0fa67356f26e09d66d
Cyrille Berger <cyb@lepi.org>
2007-09-18 07:40:33 +0000

    nearly working color transformation, except that it doesn't seem to map the normal range to [0,1]
    svn path=/trunk/koffice/; revision=713905

--- krita/colorspaces/xyz_float_hdr/kis_xyz_hdr_colorspace.h
+++ krita/colorspaces/xyz_float_hdr/kis_xyz_hdr_colorspace.h
@@ -20,10 +20,23 @@
 #ifndef _KIS_XYZ_HDR_COLORSPACE_H_
 #define _KIS_XYZ_HDR_COLORSPACE_H_
 
+#include <math.h>
+
 #include "KoIncompleteColorSpace.h"
 
 #include "compositeops/KoCompositeOpOver.h"
 #include "compositeops/KoCompositeOpErase.h"
+
+// Observer= 2°, Illuminant= D65
+#define X_r 95.047
+#define Y_r 100.000
+#define Z_r 108.883
+
+#define k 0.008856
+#define delta (6.0 / 29.0)
+
+#define UINT16_TO_FLOAT(v) (KoColorSpaceMaths<quint16, typename _CSTraits::channels_type >::scaleToA(v))
+#define FLOAT_TO_UINT16(v) (KoColorSpaceMaths<typename _CSTraits::channels_type, quint16>::scaleToA(v))
 
 template <class _CSTraits>
 class KisXyzFloatHDRColorSpace : public KoIncompleteColorSpace<_CSTraits, KoLAB16Fallback>
@@ -64,15 +77,68 @@ class KisXyzFloatHDRColorSpace : public KoIncompleteColorSpace<_CSTraits, KoLAB1
             addCompositeOp( new KoCompositeOpErase<_CSTraits>( this ) );
 
         }
-    public:
-        virtual void toLabA16(const quint8 * src, quint8 * dst, quint32 nPixels) const
+    private:
+        inline double f_xyz_to_lab(double v) const
         {
-            // TODO !
+            if(v > k)
+            {
+                return powf(v, 1.0/3.0);
+            } else {
+                return 7.787 * v + 16.0 / 116.0;
+            }
         }
-
-        virtual void fromLabA16(const quint8 * src, quint8 * dst, quint32 nPixels) const
+    public:
+        virtual void toLabA16(const quint8 * src8, quint8 * dst8, quint32 nPixels) const
         {
-            // TODO !
+            KoLabU16Traits::Pixel* dst = reinterpret_cast<KoLabU16Traits::Pixel*>(dst8);
+            const typename _CSTraits::Pixel* src = reinterpret_cast<const typename _CSTraits::Pixel*>(src8);
+            while(nPixels > 0)
+            {
+                double fx = f_xyz_to_lab( src->X / X_r );
+                double fy = f_xyz_to_lab( src->Y / Y_r );
+                double fz = f_xyz_to_lab( src->Z / Z_r );
+                
+                dst->L = FLOAT_TO_UINT16((116 * fy - 16) );
+                dst->a = FLOAT_TO_UINT16((500 * ( fx - fy )) );
+                dst->b = FLOAT_TO_UINT16((200 * ( fy - fz )) );
+                dst->alpha = FLOAT_TO_UINT16(src->alpha);
+                
+                nPixels--;
+                dst++;
+                src++;
+            }
+        }
+    private:
+        inline double f_lab_to_xyz(double v, double r) const
+        {
+            if( v > delta)
+            {
+                return r * pow(v, 3.0);
+            } else {
+                return r * (v - 16.0 / 116.0) * 3.0 * delta *delta;
+            }
+        }
+    public:
+        virtual void fromLabA16(const quint8 * src8, quint8 * dst8, quint32 nPixels) const
+        {
+            const KoLabU16Traits::Pixel* src = reinterpret_cast<const KoLabU16Traits::Pixel*>(src8);
+            typename _CSTraits::Pixel* dst = reinterpret_cast<typename _CSTraits::Pixel*>(dst8);
+            while(nPixels > 0)
+            {
+                double fy = (UINT16_TO_FLOAT(src->L ) + 16.0) / 116.0;
+                double fx = fy + UINT16_TO_FLOAT(src->a ) / 500.0;
+                double fz = fy - UINT16_TO_FLOAT(src->b ) / 200.0;
+                
+                dst->X = f_lab_to_xyz(fx, X_r);
+                dst->Y = f_lab_to_xyz(fy, Y_r);
+                dst->Z = f_lab_to_xyz(fz, Z_r);
+                dst->alpha = UINT16_TO_FLOAT(src->alpha);
+                kDebug() << src->L << " " << src->a << " " << src->b << " " << dst->X << " " << dst->Y << " " << dst->Z;
+                nPixels--;
+                dst++;
+                src++;
+            }
+            
         }
         virtual bool profileIsCompatible(KoColorProfile* profile) const
         {

cffccc1715c7290f8a2c91e40e989defcdf6c0a3
Cyrille Berger <cyb@lepi.org>
2007-09-30 08:09:47 +0000

    add command line arguments direct output to ps
    svn path=/trunk/koffice/; revision=719056

--- libs/pigment/tests/CSSGraph.cpp
+++ libs/pigment/tests/CSSGraph.cpp
@@ -17,6 +17,10 @@
  * Boston, MA 02110-1301, USA.
 */
 
+#include <QFile>
+#include <QProcess>
+#include <QTemporaryFile>
+
 #include <KAboutData>
 #include <KCmdLineArgs>
 #include <KApplication>
@@ -38,9 +42,63 @@ int main(char argc, char** argv)
                          ki18n(""),
                          "www.koffice.org",
                          "submit@bugs.kde.org");
- 
     KCmdLineArgs::init( argc, argv, &aboutData );
+    // Initialize the list of options
+    KCmdLineOptions options;
+    options.add("graphs", ki18n("return the list of available graphs"));
+    options.add("graph <type>", ki18n("specify the type of graph (see --graphs to get the full list, the default is full)"), "full");
+    options.add("output <type>", ki18n("specify the output (can be ps or dot, the default is ps)"), "ps");
+    options.add("+outputfile", ki18n("name of the output file"));
+    KCmdLineArgs::addCmdLineOptions( options );
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    if( args->isSet("graphs"))
+    {
+        std::cout << "full : show all the connection on the graph" << std::endl;
+        exit(0);
+    }
+    QString graphType = args->getOption("graph");
+    QString outputType = args->getOption("output");
+    if( args->count() != 1 )
+    {
+        kError() << "No output file name specified";
+        args->usage();
+        exit(-1);
+    }
+    QString outputFileName = args->arg(0);
+    args->clear();
+    // Generate the graph
     KApplication app;
-    QString dot = KoColorSpaceRegistry::instance()->colorConversionSystem()->toDot();
-    std::cout << dot.toLatin1().data();
+    QString dot;
+    if(graphType == "full")
+    {
+        dot = KoColorSpaceRegistry::instance()->colorConversionSystem()->toDot();
+    } else {
+        std::cout << "Unknow graph type : " << graphType.latin1() << std::endl;
+        exit(-1);
+    }
+    if(outputType == "dot")
+    {
+        QFile file(outputFileName);
+        if (not file.open(QIODevice::WriteOnly | QIODevice::Text))
+            exit(-1);
+        QTextStream out(&file);
+        out << dot;
+    } else if(outputType == "ps")
+    {
+        QTemporaryFile file;
+        if (not file.open())
+            exit(-1);
+        QTextStream out(&file);
+        out << dot;
+        QString cmd = QString("dot -Tps %1 -o %2").arg(file.fileName()).arg(outputFileName);
+        file.close();
+        kDebug() << cmd;
+        if(QProcess::execute(cmd) != 0)
+        {
+            kError() << "An error has occured when executing : '" << cmd << "' it's most likely that the command 'dot' is missing, and that you should install graphviz (from http://www.graphiz.org)";
+        }
+    } else {
+        std::cout << "Unknow output type : " << outputType.latin1() << std::endl;
+        exit(-1);
+    }
 }

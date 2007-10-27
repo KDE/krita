a28c89bbd7836dd11fe4372e8ce3e9f66b632f6e
Inge Wallin <inge@lysator.liu.se>
2007-10-27 20:02:05 +0000

    forgot to commit this
    svn path=/trunk/koffice/; revision=730055

--- interfaces/koChart.h
+++ interfaces/koChart.h
@@ -74,7 +74,6 @@ namespace KoChart
 	// The new interface.
 	virtual void resizeData( int rows, int columns ) = 0 ;
 	virtual void setCellData( int row, int column, const QVariant &) = 0;
-	virtual void analyzeHeaders( ) = 0;
 #endif
         virtual void setCanChangeValue(bool b )=0;
 

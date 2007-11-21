52469f497213b0e6112b939e3b8e062d336e2612
Sebastian Sauer <mail@dipe.org>
2007-11-21 04:11:38 +0000

    now the sample even draws something for me. Seems to create the Qt::Painter in the paintEvent is not a good idea for QtRuby since it does not work for me that way. Probably cause begin+end are not called correct or things are done somehow outside the paintEvent or...
    svn path=/trunk/koffice/; revision=739430

--- krita/plugins/viewplugins/scripting/scripts/dockers/palettedocker.rb
+++ krita/plugins/viewplugins/scripting/scripts/dockers/palettedocker.rb
@@ -2,7 +2,7 @@ GC.disable
 # GC.disable
 # require 'Krita'
 require 'KritaDockFactory'
-if(require 'korundum4')
+if(require 'Qt') #'korundum4'
     class Kikoo < Qt::Object
         slots 'timeout()'
         def initialize()
@@ -12,33 +12,35 @@ if(require 'korundum4')
             @timer.start(100)
         end
         def timeout
-            while(true)
-                puts "kikoo #{Qt::EventLoop::AllEvents | Qt::EventLoop::WaitForMoreEvents | Qt::EventLoop::DeferredDeletion}"
-                Qt::Application.instance().processEvents(Qt::EventLoop::AllEvents | Qt::EventLoop::WaitForMoreEvents | Qt::EventLoop::DeferredDeletion)
-            end
+#             while(true)
+#                 puts "kikoo #{Qt::EventLoop::AllEvents | Qt::EventLoop::WaitForMoreEvents | Qt::EventLoop::DeferredDeletion}"
+#                 Qt::Application.instance().processEvents(Qt::EventLoop::AllEvents | Qt::EventLoop::WaitForMoreEvents | Qt::EventLoop::DeferredDeletion)
+#             end
         end
     end 
     class PaletteWidget < Qt::Widget
         def initialize(parent)
             super(parent)
             @count = 5
+            @painter = Qt::Painter.new()
         end
-#         def paintEvent( event )
-#             painter = Qt::Painter.new( self )
-#             width = self.width
-#             height = self.height
-#             s = width / @count
-#             s = height if s > height
-#             for i in 0...@count
-#                 painter.drawEllipse( i * s, 0, s, s )
-#             end
-#         end
-#         def heightForWidth ( w )
-#             return w / @count + 1
-#         end
+        def paintEvent( event )
+            @painter.begin(self)
+            width = self.width
+            height = self.height
+            s = width / @count
+            s = height if s > height
+            for i in 0...@count
+                @painter.drawEllipse( i * s, 0, s, s )
+            end
+            @painter.end()
+        end
+        def heightForWidth ( w )
+            return w / @count + 1
+        end
     end
     def createDockWidget
-        $wdg = Qt::DockWidget.new(Qt::Object::tr("Palette"))
+        $wdg = Qt::DockWidget.new(Qt::Object::tr("Ruby Palette"))
         $label = PaletteWidget.new($wdg)
         $wdg.setWidget($label)
         voidptr = Qt::Internal.smoke2kross($wdg)

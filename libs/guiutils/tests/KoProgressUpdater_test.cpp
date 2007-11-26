af7fd0e1e10b00e4bb23ee06c7a6c7ce4634d678
Boudewijn Rempt <boud@valdyas.org>
2007-11-26 20:04:34 +0000

    lgpl not gpl
    svn path=/trunk/koffice/; revision=741948

--- libs/guiutils/tests/KoProgressUpdater_test.cpp
+++ libs/guiutils/tests/KoProgressUpdater_test.cpp
@@ -1,19 +1,20 @@
 /*
  *  Copyright (c) 2007 Boudewijn Rempt boud@valdyas.org
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 #include <qtest_kde.h>
 
@@ -126,8 +127,8 @@ void KoProgressUpdaterTest::testSimpleThreadedProgress()
     pu.start();
     KoUpdater u = pu.startSubtask();
     TestJob t(&u);
-    t.start();
-    QTest::qSleep(2500); // allow the action to do its job.
+    t.wait();
+    QTest::qSleep(250); // allow the action to do its job.
     QCoreApplication::processEvents(); // allow the actions 'gui' stuff to run.
     QCOMPARE(bar.value, 100);
 }

e6375d396df0bb35fd51592ebec36e15bcd49d26
Cyrille Berger <cyb@lepi.org>
2007-11-30 13:08:44 +0000

    fix licence (thanks for noticing) CCMAIL: zander@kde.org
    svn path=/trunk/koffice/; revision=743271

--- libs/pigment/KoScaleColorConversionTransformation.h
+++ libs/pigment/KoScaleColorConversionTransformation.h
@@ -2,7 +2,7 @@
  *  Copyright (c) 2007 Cyrille Berger <cberger@cberger.net>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
@@ -11,7 +11,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */

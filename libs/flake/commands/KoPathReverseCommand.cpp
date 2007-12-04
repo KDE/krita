5ca4d74caea2e761cda19257e0f01f35a0c3f7c0
Jan Hambrecht <jaham@gmx.net>
2007-12-04 23:41:07 +0000

    implement command for reversing pathes and use than in karbon
    svn path=/trunk/koffice/; revision=744961

---
+++ libs/flake/commands/KoPathReverseCommand.cpp
@@ -0,0 +1,74 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2007 Jan Hambrecht <jaham@gmx.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "KoPathReverseCommand.h"
+#include "KoPathShape.h"
+#include <klocale.h>
+
+class KoPathReverseCommand::Private
+{
+public:
+    Private(const QList<KoPathShape*> &p)
+        : paths(p)
+    {
+    }
+    ~Private() {
+    }
+
+    void reverse()
+    {
+        if( ! paths.size() )
+            return;
+
+        foreach( KoPathShape* shape, paths )
+        {
+            int subpathCount = shape->subpathCount();
+            for( int i = 0; i < subpathCount; ++i )
+                shape->reverseSubpath( i );
+        }
+    }
+
+    QList<KoPathShape*> paths;
+};
+
+KoPathReverseCommand::KoPathReverseCommand( const QList<KoPathShape*> &paths, QUndoCommand *parent )
+    : QUndoCommand( parent ),
+    d(new Private(paths))
+{
+    setText( i18n( "Reverse paths" ) );
+}
+
+KoPathReverseCommand::~KoPathReverseCommand()
+{
+    delete d;
+}
+
+void KoPathReverseCommand::redo()
+{
+    QUndoCommand::redo();
+
+    d->reverse();
+}
+
+void KoPathReverseCommand::undo()
+{
+    QUndoCommand::undo();
+
+    d->reverse();
+}

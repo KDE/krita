e6e773db51e3ae538f434812dcde31f94ff89562
Emanuele Tamponi <emanuele@valinor.it>
2007-12-22 22:52:46 +0000

    Color space traits for KS Color Space.
    svn path=/trunk/koffice/; revision=751836

---
+++ krita/plugins/painterlyframework/kis_ks_colorspace_traits.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2007 Emanuele Tamponi <emanuele@valinor.it>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KIS_KS_COLORSPACE_TRAITS_H_
+#define KIS_KS_COLORSPACE_TRAITS_H_
+
+template<int _wavelen_number_>
+struct KisKSColorSpaceTrait : public KoColorSpaceTrait<float, 2*(_wavelen_number_)+1, 6> {
+
+    float K[_wavelen_number_];
+    float S[_wavelen_number_];
+    float opacity;
+
+    inline static float &K(quint8* data, int wavelen)
+    {
+        if (wavelen < -1 || wavelen >= _wavelen_number_)
+            return -1.0;
+        float *d = nativeArray(data);
+        // User asked for K that's in the first [0 ... _wavelen_number_-1] positions
+        return d[wavelen];
+    }
+
+    inline static float &S(quint8* data, int wavelen)
+    {
+        if (wavelen < -1 || wavelen >= _wavelen_number_)
+            return -1.0;
+        float *d = nativeArray(data);
+        // User asked for S that's in the [ _wavelen_number_ ... 2*_wavelen_number_ - 1] positions
+        return d[_wavelen_number_ + wavelen];
+    }
+};
+
+typedef KisKS3ColorSpaceTrait KisKSColorSpaceTrait<3>;
+typedef KisKS9ColorSpaceTrait KisKSColorSpaceTrait<9>;
+
+#endif // KIS_KS_COLORSPACE_TRAITS_H_

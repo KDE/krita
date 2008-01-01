a479ed9e7496bc0de616b141f25b66ce035e9f13
Emanuele Tamponi <emanuele@valinor.it>
2008-01-01 15:51:36 +0000

    Use N
    svn path=/trunk/koffice/; revision=755439

--- krita/plugins/painterlyframework/tests/kis_ks_colorspace_test.cpp
+++ krita/plugins/painterlyframework/tests/kis_ks_colorspace_test.cpp
@@ -46,7 +46,7 @@ void print_vector(quint8 *v, const QString &text)
     QString vstr;
     qDebug() << text;
     for (int i = 0; i < n; i++)
-        vstr += QString::number(reinterpret_cast<type*>(v)[i]) + " ";
+        vstr += QString::number(reinterpret_cast<type*>(v)[i]/256) + " ";
     qDebug() << vstr;
 }
 
@@ -56,7 +56,7 @@ void KisKSColorSpaceTest::testToFromRgbA16()
 {
     QString d659 = KGlobal::mainComponent().dirs()->findAllResources("illuminant_profiles", "D65_9_high.ill",  KStandardDirs::Recursive)[0];
     const KisIlluminantProfile *profile = new KisIlluminantProfile(d659);
-    const KoColorSpace *cs = KoColorSpaceRegistry::instance()->colorSpace(KisKSQPColorSpace<float,9>::ColorSpaceId().id(), profile);
+    const KoColorSpace *cs = KoColorSpaceRegistry::instance()->colorSpace(KisKSQPColorSpace<float,N>::ColorSpaceId().id(), profile);
     QVERIFY2(cs != 0, "Created colorspace");
     QVERIFY(cs->profileIsCompatible(profile));
 
@@ -68,7 +68,7 @@ void KisKSColorSpaceTest::testToFromRgbA16()
     quint16 blue[4]   = { val,   0, 0,   val };
     quint16 green[4]  = { 0,   val, 0,   val };
     quint16 red[4]    = { 0,     0, val, val };
-    quint16 yellow[4] = { val, val, val, val };
+    quint16 yellow[4] = { 0,   val, val, val };
 
     quint16 bluegreen[8] = { val, 0, 0, val, 0, val, 0, val };
 
@@ -78,11 +78,10 @@ void KisKSColorSpaceTest::testToFromRgbA16()
     print_vector<float, 2*N+1>(kas1, "BLUE IN KS:");
     cs->toRgbA16(kas1, rgb2, 1);
     print_vector<quint16, 4>(rgb2, "BLUE AGAIN:");
-
-//     QVERIFY(rgb1[0] == rgb2[0]);
-//     QVERIFY(rgb1[1] == rgb2[1]);
-//     QVERIFY(rgb1[2] == rgb2[2]);
-//     QVERIFY(rgb1[3] == rgb2[3]);
+    QVERIFY(rgb1[0] == rgb2[0]);
+    QVERIFY(rgb1[1] == rgb2[1]);
+    QVERIFY(rgb1[2] == rgb2[2]);
+    QVERIFY(rgb1[3] == rgb2[3]);
 
     rgb1 = reinterpret_cast<quint8*>(green);
     cs->fromRgbA16(rgb1, kas1, 1);
@@ -101,10 +100,10 @@ void KisKSColorSpaceTest::testToFromRgbA16()
     print_vector<quint16, 4>(rgb1, "RED:");
     print_vector<float, 2*N+1>(kas1, "RED IN KS:");
     print_vector<quint16, 4>(rgb2, "RED AGAIN:");
-//     QVERIFY(rgb1[0] == rgb2[0]);
-//     QVERIFY(rgb1[1] == rgb2[1]);
-//     QVERIFY(rgb1[2] == rgb2[2]);
-//     QVERIFY(rgb1[3] == rgb2[3]);
+    QVERIFY(rgb1[0] == rgb2[0]);
+    QVERIFY(rgb1[1] == rgb2[1]);
+    QVERIFY(rgb1[2] == rgb2[2]);
+    QVERIFY(rgb1[3] == rgb2[3]);
 
     rgb1 = reinterpret_cast<quint8*>(yellow);
     cs->fromRgbA16(rgb1, kas1, 1);
@@ -123,14 +122,14 @@ void KisKSColorSpaceTest::testToFromRgbA16()
     print_vector<quint16, 8>(rgb1, "BLUE AND GREEN:");
     print_vector<float, 2*2*N+1>(kas1, "BLUE AND GREEN IN KS:");
     print_vector<quint16, 8>(rgb2, "BLUE AND GREEN AGAIN:");
-//     QVERIFY(rgb1[0] == rgb2[0]);
-//     QVERIFY(rgb1[1] == rgb2[1]);
-//     QVERIFY(rgb1[2] == rgb2[2]);
-//     QVERIFY(rgb1[3] == rgb2[3]);
-//     QVERIFY(rgb1[4] == rgb2[4]);
-//     QVERIFY(rgb1[5] == rgb2[5]);
-//     QVERIFY(rgb1[6] == rgb2[6]);
-//     QVERIFY(rgb1[7] == rgb2[7]);
+    QVERIFY(rgb1[0] == rgb2[0]);
+    QVERIFY(rgb1[1] == rgb2[1]);
+    QVERIFY(rgb1[2] == rgb2[2]);
+    QVERIFY(rgb1[3] == rgb2[3]);
+    QVERIFY(rgb1[4] == rgb2[4]);
+    QVERIFY(rgb1[5] == rgb2[5]);
+    QVERIFY(rgb1[6] == rgb2[6]);
+    QVERIFY(rgb1[7] == rgb2[7]);
 
     delete [] kas1;
     delete [] rgb2;
@@ -141,7 +140,7 @@ void KisKSColorSpaceTest::testMixing()
 {
     QString d659 = KGlobal::mainComponent().dirs()->findAllResources("illuminant_profiles", "D65_9_high.ill",  KStandardDirs::Recursive)[0];
     const KisIlluminantProfile *profile = new KisIlluminantProfile(d659);
-    const KoColorSpace *cs = KoColorSpaceRegistry::instance()->colorSpace(KisKSQPColorSpace<float,9>::ColorSpaceId().id(), profile);
+    const KoColorSpace *cs = KoColorSpaceRegistry::instance()->colorSpace(KisKSQPColorSpace<float,N>::ColorSpaceId().id(), profile);
     QVERIFY2(cs != 0, "Created colorspace");
     QVERIFY(cs->profileIsCompatible(profile));
 
@@ -151,24 +150,15 @@ void KisKSColorSpaceTest::testMixing()
     quint8 *kasm = new quint8[cs->pixelSize()];
     quint8 *rgbm = new quint8[2*4];
 
-    quint32 val = 65535;
-    quint16 blue[4]   = { val,   0, 0,   val };
-    quint16 green[4]  = { 0,   val, 0,   val };
-    quint16 red[4]    = { 0,     0, val, val };
-    quint16 yellow[4] = { 0,   val, val, val };
+    quint32 val1 = 65535, val2 = 10000;
+    quint16 red[4]    = { 0,    val2, val1, val1 };
+    quint16 green[4]  = { val2, val1, val2, val1 };
+    quint16 blue[4]   = { val1, val2, 0,    val1 };
+    quint16 yellow[4] = { 0,    val1, val1, val1 };
+    quint16 violet[4] = { val1, 0,    val1, val1 };
+    quint16 cyan[4]   = { val1, val1, 0,    val1 };
 
-    rgb1 = reinterpret_cast<quint8*>(blue);
-    rgb2 = reinterpret_cast<quint8*>(yellow);
-    cs->fromRgbA16(rgb1, kas1, 1);
-    cs->fromRgbA16(rgb2, kas2, 1);
-    for (int i = 0; i < 2*N+1; i++)
-        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
-                                             reinterpret_cast<float*>(kas2)[i]) / 2.0;
-    cs->toRgbA16(kasm, rgbm, 1);
-    print_vector<float,2*N+1>(kasm, "BLUE + YELLOW IN KS:");
-    print_vector<quint16,4>(rgbm, "BLUE + YELLOW BACK IN RBG:");
-
-    rgb1 = reinterpret_cast<quint8*>(blue);
+    rgb1 = reinterpret_cast<quint8*>(red);
     rgb2 = reinterpret_cast<quint8*>(green);
     cs->fromRgbA16(rgb1, kas1, 1);
     cs->fromRgbA16(rgb2, kas2, 1);
@@ -176,8 +166,8 @@ void KisKSColorSpaceTest::testMixing()
         reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
                                              reinterpret_cast<float*>(kas2)[i]) / 2.0;
     cs->toRgbA16(kasm, rgbm, 1);
-    print_vector<float,2*N+1>(kasm, "BLUE + GREEN IN KS:");
-    print_vector<quint16,4>(rgbm, "BLUE + GREEN BACK IN RBG:");
+//     print_vector<float,2*N+1>(kasm, "RED + GREEN IN KS:");
+    print_vector<quint16,4>(rgbm, "RED + GREEN IN RBG:");
 
     rgb1 = reinterpret_cast<quint8*>(red);
     rgb2 = reinterpret_cast<quint8*>(blue);
@@ -187,8 +177,151 @@ void KisKSColorSpaceTest::testMixing()
         reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
                                              reinterpret_cast<float*>(kas2)[i]) / 2.0;
     cs->toRgbA16(kasm, rgbm, 1);
-    print_vector<float,2*N+1>(kasm, "RED + BLUE IN KS:");
-    print_vector<quint16,4>(rgbm, "RED + BLUE BACK IN RBG:");
+//     print_vector<float,2*N+1>(kasm, "RED + BLUE IN KS:");
+    print_vector<quint16,4>(rgbm, "RED + BLUE IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(red);
+    rgb2 = reinterpret_cast<quint8*>(yellow);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "RED + YELLOW IN KS:");
+    print_vector<quint16,4>(rgbm, "RED + YELLOW IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(red);
+    rgb2 = reinterpret_cast<quint8*>(violet);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "RED + VIOLET IN KS:");
+    print_vector<quint16,4>(rgbm, "RED + VIOLET IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(red);
+    rgb2 = reinterpret_cast<quint8*>(cyan);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "RED + CYAN IN KS:");
+    print_vector<quint16,4>(rgbm, "RED + CYAN IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(green);
+    rgb2 = reinterpret_cast<quint8*>(blue);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+                                             reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "GREEN + BLUE IN KS:");
+    print_vector<quint16,4>(rgbm, "GREEN + BLUE IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(green);
+    rgb2 = reinterpret_cast<quint8*>(yellow);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "GREEN + YELLOW IN KS:");
+    print_vector<quint16,4>(rgbm, "GREEN + YELLOW IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(green);
+    rgb2 = reinterpret_cast<quint8*>(violet);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "GREEN + VIOLET IN KS:");
+    print_vector<quint16,4>(rgbm, "GREEN + VIOLET IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(green);
+    rgb2 = reinterpret_cast<quint8*>(cyan);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "GREEN + CYAN IN KS:");
+    print_vector<quint16,4>(rgbm, "GREEN + CYAN IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(blue);
+    rgb2 = reinterpret_cast<quint8*>(yellow);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "BLUE + YELLOW IN KS:");
+    print_vector<quint16,4>(rgbm, "BLUE + YELLOW IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(blue);
+    rgb2 = reinterpret_cast<quint8*>(violet);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "BLUE + VIOLET IN KS:");
+    print_vector<quint16,4>(rgbm, "BLUE + VIOLET IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(blue);
+    rgb2 = reinterpret_cast<quint8*>(cyan);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "BLUE + CYAN IN KS:");
+    print_vector<quint16,4>(rgbm, "BLUE + CYAN IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(yellow);
+    rgb2 = reinterpret_cast<quint8*>(violet);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "YELLOW + VIOLET IN KS:");
+    print_vector<quint16,4>(rgbm, "YELLOW + VIOLET IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(yellow);
+    rgb2 = reinterpret_cast<quint8*>(cyan);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "YELLOW + CYAN IN KS:");
+    print_vector<quint16,4>(rgbm, "YELLOW + CYAN IN RBG:");
+
+    rgb1 = reinterpret_cast<quint8*>(violet);
+    rgb2 = reinterpret_cast<quint8*>(cyan);
+    cs->fromRgbA16(rgb1, kas1, 1);
+    cs->fromRgbA16(rgb2, kas2, 1);
+    for (int i = 0; i < 2*N+1; i++)
+        reinterpret_cast<float*>(kasm)[i] = (reinterpret_cast<float*>(kas1)[i] +
+        reinterpret_cast<float*>(kas2)[i]) / 2.0;
+    cs->toRgbA16(kasm, rgbm, 1);
+//     print_vector<float,2*N+1>(kasm, "VIOLET + CYAN IN KS:");
+    print_vector<quint16,4>(rgbm, "VIOLET + CYAN IN RBG:");
 
     delete [] rgbm;
     delete [] kasm;

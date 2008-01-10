df1bf2e852c8ad7456d5525e46e14c2f39743803
Boudewijn Rempt <boud@valdyas.org>
2008-01-10 15:09:00 +0000

    Add framework for unittests -- maybe I'll be home early enough tonight to write a few tests.
    svn path=/trunk/koffice/; revision=759439

---
+++ plugins/tableshape/tests/TestTableCell.cpp
@@ -0,0 +1,33 @@
+/* This file is part of the KDE project
+ * Copyright (C) Boudewijn Rempt <boud@valdyas.org>, (C) 2008
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "TestTableCell.h"
+
+#include <QtGui>
+
+#include <kdebug.h>
+#include <kcomponentdata.h>
+
+
+void TestTableCell::testCreate() {
+}
+
+QTEST_KDEMAIN(TestTableCell, GUI)
+
+#include "TestTableCell.moc"

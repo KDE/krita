4ba8fc60c4a0e950fabd1395defc46f52fef40f8
Sharan Rao <sharanrao@gmail.com>
2008-01-27 19:54:30 +0000

    Better searching, setting of variables
    svn path=/trunk/koffice/; revision=767325

--- cmake/modules/FindFreeTDS.cmake
+++ cmake/modules/FindFreeTDS.cmake
@@ -6,20 +6,37 @@
 #  FreeTDS_LIBRARIES - Link these to use FreeTDS
 
 
-FIND_PATH(FreeTDS_INCLUDE_DIR NAMES sqldb.h sqlfront.h
-    /usr/include/
-    /usr/local/include
-)
+IF (FreeTDS_INCLUDE_DIR AND FreeTDS_LIBRARIES )
 
-FIND_LIBRARY(FreeTDS_TDS_LIBRARIES NAMES tds )
+	# Already in cache
+	SET(FreeTDS_FOUND TRUE)
 
-FIND_LIBRARY(FreeTDS_SYBDB_LIBRARIES NAMES sybdb )
+ELSE (FreeTDS_INCLUDE_DIR AND FreeTDS_LIBRARIES)
+
+       FIND_PATH(FreeTDS_INCLUDE_DIR NAMES sqldb.h sqlfront.h
+        /usr/include/
+        /usr/local/include
+       )
+
+       FIND_LIBRARY(FreeTDS_TDS_LIBRARIES NAMES tds )
+
+       FIND_LIBRARY(FreeTDS_SYBDB_LIBRARIES NAMES sybdb )
   
-set(FreeTDS_LIBRARIES ${FreeTDS_SYBDB_LIBRARIES} ${FreeTDS_TDS_LIBRARIES} CACHE STRING "Libraries needed for sybase/mssql driver")
+       set(FreeTDS_LIBRARIES ${FreeTDS_SYBDB_LIBRARIES} ${FreeTDS_TDS_LIBRARIES} CACHE STRING "Libraries needed for sybase/mssql driver")
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(FreeTDS DEFAULT_MSG FreeTDS_INCLUDE_DIR FreeTDS_LIBRARIES )
+	#
+	# everything necessary found?
+	#
+       IF (FreeTDS_LIBRARIES AND FreeTDS_INCLUDE_DIR)
+		SET (FreeTDS_FOUND TRUE)
+       ELSE (FreeTDS_LIBRARIES AND FreeTDS_INCLUDE_DIR)
+		SET (FreeTDS_FOUND FALSE)
+       ENDIF (FreeTDS_LIBRARIES AND FreeTDS_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(FreeTDS_INCLUDE_DIR FreeTDS_LIBRARIES)
+       include(FindPackageHandleStandardArgs)
+       FIND_PACKAGE_HANDLE_STANDARD_ARGS(FreeTDS DEFAULT_MSG FreeTDS_INCLUDE_DIR FreeTDS_LIBRARIES )
 
+       MARK_AS_ADVANCED(FreeTDS_INCLUDE_DIR FreeTDS_LIBRARIES)
+
+ENDIF (FreeTDS_INCLUDE_DIR AND FreeTDS_LIBRARIES )
 

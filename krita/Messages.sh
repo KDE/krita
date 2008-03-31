3e45c31d328a39a47d648ad7eb7833a45518dce3
Cyrille Berger <cyb@lepi.org>
2008-03-31 21:56:26 +0000

    ignore sdk/templates since it's not seen by user (and use a grep since I didn't manage to make find's operand works)
    svn path=/trunk/koffice/; revision=792365

--- krita/Messages.sh
+++ krita/Messages.sh
@@ -2,6 +2,7 @@
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp 
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
 perl extracti18n.pl > i18ndata
-$XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h` ui/kis_aboutdata.h -o $podir/krita.pot
+# ignore sdk/templates since it contains templates for use a future plugins, none of the strings there will ever be seen by any user
+$XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h | grep -v './sdk/templates'` ui/kis_aboutdata.h -o $podir/krita.pot
 rm -f i18ndata
 

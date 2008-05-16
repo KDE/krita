d8ed050822e0db4b333615c10560c481d60da6f6
Girish Ramakrishnan <girish@forwardbias.in>
2008-05-16 12:36:16 +0000

    Add DocumentStructureTest
    svn path=/trunk/koffice/; revision=808358

---
+++ libs/main/tests/documentstructuretest.cpp
@@ -0,0 +1,105 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2008 Girish Ramakrishnan <girish@forwardbias.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "documentstructuretest.h"
+
+#include <QDebug>
+#include <QtTest>
+#include <QtGui>
+
+#include <kdeversion.h>
+
+#include <KoDocument.h>
+#include <KoDocumentInfo.h>
+#include <KoStore.h>
+#include <KoXmlReader.h>
+#include <KoOdfReadStore.h>
+#include <KoXmlNS.h>
+#include <kcomponentdata.h>
+
+// May the testing begin
+DocumentStructureTest::DocumentStructureTest() 
+{
+    componentData = new KComponentData("DocumentStructureTest");
+}
+
+DocumentStructureTest::~DocumentStructureTest()
+{
+    delete componentData;
+}
+
+// initTestCase/cleanupTestCase are called beginning and end of test suite
+void DocumentStructureTest::initTestCase()
+{
+}
+
+void DocumentStructureTest::cleanupTestCase()
+{
+}
+
+// init/cleanup are called beginning and end of every test case
+void DocumentStructureTest::init()
+{
+}
+
+void DocumentStructureTest::cleanup()
+{
+}
+
+void DocumentStructureTest::predefinedMetaData_test() 
+{
+    QString odt = QString(FILES_DATA_DIR) + "/DocumentStructure/predefined.odt";
+    QVERIFY(QFile(odt).exists());
+
+    store = KoStore::createStore(odt, KoStore::Read, "", KoStore::Zip);
+    KoOdfReadStore odfReadStore(store);
+    KoXmlDocument metaDoc;
+    KoDocumentInfo *documentInfo = new KoDocumentInfo;
+    QString error;
+    QVERIFY(odfReadStore.loadAndParse("meta.xml", metaDoc, error));
+    QVERIFY(documentInfo->loadOasis(metaDoc));
+
+    QCOMPARE(documentInfo->aboutInfo("title"), QString("Meta Test Document"));
+    QCOMPARE(documentInfo->aboutInfo("description"), QString("A test document to test meta elements"));
+    QCOMPARE(documentInfo->aboutInfo("subject"), QString("Subject of the document"));
+    QCOMPARE(documentInfo->aboutInfo("creation-date"), QString("2005-12-19T12:50:56"));
+    QCOMPARE(documentInfo->aboutInfo("initial-creator"), QString("Majid Khan"));
+    QCOMPARE(documentInfo->aboutInfo("date"), QString("2005-12-19T12:50:56"));
+    QString keyword = documentInfo->aboutInfo("keyword");
+    QStringList keywords = keyword.split(",");
+    QVERIFY(keywords.count() == 3);
+    QCOMPARE(keywords[0].trimmed(), QString("First keyword"));
+    QCOMPARE(keywords[1].trimmed(), QString("Second keyword"));
+    QCOMPARE(keywords[2].trimmed(), QString("Third keyword"));
+
+    QCOMPARE(documentInfo->authorInfo("creator"), QString("Majid Khan"));
+
+    delete store;
+    store = 0;
+    delete documentInfo;
+    documentInfo = 0;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    return QTest::qExec(new DocumentStructureTest, argc, argv);
+}
+
+#include "documentstructuretest.moc"
---
+++ libs/main/tests/documentstructuretest.h
@@ -0,0 +1,50 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2008 Girish Ramakrishnan <girish@forwardbias.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DOCUMENTINFOTEST_H 
+#define DOCUMENTINFOTEST_H
+
+#include <QObject>
+
+class KoStore;
+class KComponentData;
+
+class DocumentStructureTest : public QObject 
+{
+    Q_OBJECT
+public:
+    DocumentStructureTest();
+    ~DocumentStructureTest();
+
+private slots:
+    void init();
+    void cleanup();
+    void initTestCase();
+    void cleanupTestCase();
+
+
+    void predefinedMetaData_test();
+
+private:
+    KComponentData *componentData;
+    KoStore *store;
+};
+
+#endif // DOCUMENTSTRUCTURE_H
+

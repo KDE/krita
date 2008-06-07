4771948533833364a56ecc8802ffdda506d46bcc
Thorsten Zachmann <t.zachmann@zagge.de>
2008-06-07 18:57:13 +0000

    o added command for adding a event action
    svn path=/trunk/koffice/; revision=818112

---
+++ libs/flake/commands/KoEventActionAddCommand.h
@@ -0,0 +1,46 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2008 Thorsten Zachmann <zachmann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KOEVENTACTIONADDCOMMAND_H
+#define KOEVENTACTIONADDCOMMAND_H
+
+#include <QUndoCommand>
+#include "flake_export.h"
+
+class KoShape;
+class KoEventAction;
+
+class FLAKE_EXPORT KoEventActionAddCommand : public QUndoCommand
+{
+public:
+    KoEventActionAddCommand( KoShape * shape, KoEventAction * eventAction, QUndoCommand *parent = 0 );
+    virtual ~KoEventActionAddCommand();
+
+    /// redo the command
+    void redo();
+    /// revert the actions done in redo
+    void undo();
+
+private:
+    KoShape * m_shape;
+    KoEventAction * m_eventAction;
+    bool m_deleteEventAction;
+};
+
+#endif /* KOEVENTACTIONADDCOMMAND_H */

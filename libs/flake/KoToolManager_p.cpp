c46900a3b783427b3c7872537aa4413e5ced19ca
Thomas Zander <zander@kde.org>
2008-06-22 09:23:46 +0000

    Don't force this, let the style do the correct drawing of the button
    svn path=/trunk/koffice/; revision=822968

--- libs/flake/KoToolManager_p.cpp
+++ libs/flake/KoToolManager_p.cpp
@@ -36,7 +36,7 @@ ToolHelper::ToolHelper(KoToolFactory *tool) {
 QToolButton* ToolHelper::createButton() {
     QToolButton *but = new QToolButton();
     but->setIcon(KIcon( m_toolFactory->icon() ).pixmap(22));
-    but->setAutoRaise(true);
+    //but->setAutoRaise(true);
     but->setToolTip(m_toolFactory->toolTip());
     connect(but, SIGNAL(clicked()), this, SLOT(buttonPressed()));
     return but;

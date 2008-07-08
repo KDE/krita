b84b82390e76c53da1cfd94af78cea433ab728bf
Sven Langkamp <sven.langkamp@gmail.com>
2008-07-08 18:11:36 +0000

    fixed crash, patch from boud
    svn path=/trunk/koffice/; revision=829609

--- krita/plugins/tools/defaulttools/kis_tool_brush.cc
+++ krita/plugins/tools/defaulttools/kis_tool_brush.cc
@@ -124,7 +124,7 @@ void KisToolBrush::mouseMoveEvent(KoPointerEvent *e) {
     KisConfig cfg;
     if (m_mode != PAINT && cfg.cursorStyle() == CURSOR_STYLE_OUTLINE)
         paintOutline(e->pos());
-    if ( m_painter->paintOp()->incremental()) {
+    if ( m_painter && m_painter->paintOp() && m_painter->paintOp()->incremental()) {
         m_timer->start( m_rate );
     }
 }

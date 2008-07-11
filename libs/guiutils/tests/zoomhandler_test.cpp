e6617d8c31fc433bf87e9dfd70e9e87d118fbf26
Thomas Zander <zander@kde.org>
2008-07-11 06:39:58 +0000

    Make the test use QCOMPARE instead of QVERIFY and stop using the now no longer existing setZoom(int)
    svn path=/trunk/koffice/; revision=830746

--- libs/guiutils/tests/zoomhandler_test.cpp
+++ libs/guiutils/tests/zoomhandler_test.cpp
@@ -40,14 +40,14 @@ void zoomhandler_test::testConstruction()
 
     KoZoomHandler * zoomHandler = new KoZoomHandler();
 
-    QVERIFY( zoomHandler->zoomFactorX() == 1 );
-    QVERIFY( zoomHandler->zoomFactorY() == 1 );
-    QVERIFY( ( int )INCH_TO_POINT( zoomHandler->resolutionX() ) == ( int )KoGlobal::dpiX() );
-    QVERIFY( ( int )INCH_TO_POINT( zoomHandler->resolutionY() ) == ( int )KoGlobal::dpiY() );
-    QVERIFY( ( int )INCH_TO_POINT( zoomHandler->zoomedResolutionX() ) == ( int )KoGlobal::dpiX() );
-    QVERIFY( ( int )INCH_TO_POINT( zoomHandler->zoomedResolutionY() ) == ( int )KoGlobal::dpiY() );
-    QVERIFY( zoomHandler->zoomMode() == KoZoomMode::ZOOM_CONSTANT );
-    QVERIFY( zoomHandler->zoomInPercent() == 100 );
+    QCOMPARE( zoomHandler->zoomFactorX(), 1. );
+    QCOMPARE( zoomHandler->zoomFactorY(), 1. );
+    QCOMPARE( ( int )INCH_TO_POINT( zoomHandler->resolutionX() ), ( int )KoGlobal::dpiX() );
+    QCOMPARE( ( int )INCH_TO_POINT( zoomHandler->resolutionY() ), ( int )KoGlobal::dpiY() );
+    QCOMPARE( ( int )INCH_TO_POINT( zoomHandler->zoomedResolutionX() ), ( int )KoGlobal::dpiX() );
+    QCOMPARE( ( int )INCH_TO_POINT( zoomHandler->zoomedResolutionY() ), ( int )KoGlobal::dpiY() );
+    QCOMPARE( zoomHandler->zoomMode(), KoZoomMode::ZOOM_CONSTANT );
+    QCOMPARE( zoomHandler->zoomInPercent(), 100 );
     delete zoomHandler;
 }
 
@@ -57,38 +57,38 @@ void zoomhandler_test::testApi()
     double x, y;
 
     zoomHandler.setResolution( 128, 129 );
-    QVERIFY( zoomHandler.resolutionX() == 128 );
-    QVERIFY( zoomHandler.resolutionY() == 129 );
+    QCOMPARE( zoomHandler.resolutionX(), 128. );
+    QCOMPARE( zoomHandler.resolutionY(), 129. );
 
     zoomHandler.setZoomedResolution( 50, 60 );
-    QVERIFY( zoomHandler.zoomedResolutionX() == 50);
-    QVERIFY( zoomHandler.zoomedResolutionY() == 60);
+    QCOMPARE( zoomHandler.zoomedResolutionX(), 50.);
+    QCOMPARE( zoomHandler.zoomedResolutionY(), 60.);
 
-    zoomHandler.setZoom( 10 );
-    QVERIFY( zoomHandler.zoomInPercent() == 10 );
-    QVERIFY( zoomHandler.resolutionX() == 128 );
-    QVERIFY( zoomHandler.resolutionY() == 129 );
-    QVERIFY( zoomHandler.zoomedResolutionX() == 12.8 );
-    QVERIFY( zoomHandler.zoomedResolutionY() == 12.9 );
+    zoomHandler.setZoom( 0.1 ); // is 10%
+    QCOMPARE( zoomHandler.zoomInPercent(), 10);
+    QCOMPARE( zoomHandler.resolutionX(), 128. );
+    QCOMPARE( zoomHandler.resolutionY(), 129. );
+    QCOMPARE( zoomHandler.zoomedResolutionX(), 12.8 );
+    QCOMPARE( zoomHandler.zoomedResolutionY(), 12.9 );
     zoomHandler.zoom( &x, &y );
     QVERIFY( x == 12.8 && y == 12.9 );
 
-    zoomHandler.setZoom( 100 );
+    zoomHandler.setZoom( 1. );
     zoomHandler.setZoom( 0.1 );
-    QVERIFY( zoomHandler.zoomInPercent() == 10 );
-    QVERIFY( zoomHandler.resolutionX() == 128 );
-    QVERIFY( zoomHandler.resolutionY() == 129 );
-    QVERIFY( zoomHandler.zoomedResolutionX() == 12.8 );
-    QVERIFY( zoomHandler.zoomedResolutionY() == 12.9 );
+    QCOMPARE( zoomHandler.zoomInPercent(), 10 );
+    QCOMPARE( zoomHandler.resolutionX(), 128. );
+    QCOMPARE( zoomHandler.resolutionY(), 129. );
+    QCOMPARE( zoomHandler.zoomedResolutionX(), 12.8 );
+    QCOMPARE( zoomHandler.zoomedResolutionY(), 12.9 );
     zoomHandler.zoom( &x, &y );
     QVERIFY( x == 12.8 && y == 12.9 );
 
     zoomHandler.setZoomMode( KoZoomMode::ZOOM_CONSTANT );
-    QVERIFY( zoomHandler.zoomMode() == KoZoomMode::ZOOM_CONSTANT );
+    QCOMPARE( zoomHandler.zoomMode(), KoZoomMode::ZOOM_CONSTANT );
     zoomHandler.setZoomMode( KoZoomMode::ZOOM_WIDTH );
-    QVERIFY( zoomHandler.zoomMode() == KoZoomMode::ZOOM_WIDTH );
+    QCOMPARE( zoomHandler.zoomMode(), KoZoomMode::ZOOM_WIDTH );
     zoomHandler.setZoomMode( KoZoomMode::ZOOM_PAGE );
-    QVERIFY( zoomHandler.zoomMode() == KoZoomMode::ZOOM_PAGE );
+    QCOMPARE( zoomHandler.zoomMode(), KoZoomMode::ZOOM_PAGE );
 
 }
 
@@ -97,11 +97,11 @@ void zoomhandler_test::testViewToDocument()
     KoZoomHandler zoomHandler;
     zoomHandler.setZoomAndResolution(100, 100, 100 );
 
-    QVERIFY( zoomHandler.viewToDocument( QPointF( 0, 0 ) ) == QPointF( 0, 0 ) );
+    QCOMPARE( zoomHandler.viewToDocument( QPointF( 0, 0 ) ), QPointF( 0, 0 ) );
     // 100 view pixels are 72 postscript points at 100% zoom, 100ppi.
-    QVERIFY( zoomHandler.viewToDocument( QRectF( 0, 0, 100, 100 ) ) == QRectF( 0, 0, 72, 72 ) );
-    QVERIFY( zoomHandler.viewToDocumentX( 0 ) == 0 );
-    QVERIFY( zoomHandler.viewToDocumentY( 0 ) == 0 );
+    QCOMPARE( zoomHandler.viewToDocument( QRectF( 0, 0, 100, 100 ) ), QRectF( 0, 0, 72, 72 ) );
+    QCOMPARE( zoomHandler.viewToDocumentX( 0 ), 0. );
+    QCOMPARE( zoomHandler.viewToDocumentY( 0 ), 0. );
 
 }
 
@@ -110,10 +110,10 @@ void zoomhandler_test::testDocumentToView()
     KoZoomHandler zoomHandler;
     zoomHandler.setZoomAndResolution(100, 100, 100 );
 
-    QVERIFY( zoomHandler.documentToView(  QPointF( 0,0 ) ) == QPointF( 0, 0 ) );
-    QVERIFY( zoomHandler.documentToView(  QRectF( 0, 0, 72, 72 ) ) == QRectF( 0, 0, 100, 100) );
-    QVERIFY( qFuzzyCompare( zoomHandler.documentToViewX( 72 ), 100 ) );
-    QVERIFY( qFuzzyCompare( zoomHandler.documentToViewY( 72 ), 100 ) );
+    QCOMPARE( zoomHandler.documentToView(  QPointF( 0,0 ) ), QPointF( 0, 0 ) );
+    QCOMPARE( zoomHandler.documentToView(  QRectF( 0, 0, 72, 72 ) ), QRectF( 0, 0, 100, 100) );
+    QCOMPARE( zoomHandler.documentToViewX( 72 ), 100. );
+    QCOMPARE( zoomHandler.documentToViewY( 72 ), 100. );
 
 }
 

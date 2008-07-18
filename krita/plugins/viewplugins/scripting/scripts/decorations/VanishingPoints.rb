4b129633c92bd6006e731888ccc082c4b750a888
Cyrille Berger <cyb@lepi.org>
2008-07-18 18:01:19 +0000

    much more VanishingPoints decoration
    svn path=/trunk/koffice/; revision=834362

--- krita/plugins/viewplugins/scripting/scripts/decorations/VanishingPoints.rb
+++ krita/plugins/viewplugins/scripting/scripts/decorations/VanishingPoints.rb
@@ -15,7 +15,13 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 def drawDecoration( gc, documentOffset, area, converter)
-  painter = Qt::Internal.kross2smoke( gc, Qt::Painter )
-  painter.setPen( Qt::Pen.new( Qt::Brush.new(Qt::black), 1, Qt::SolidLine ) )
-  painter.drawLine(0,0, 200, 200)
+    x_c = 800
+    y_c = 600
+    painter = Qt::Internal.kross2smoke( gc, Qt::Painter )
+    painter.setPen( Qt::Pen.new( Qt::Brush.new(Qt::black), 1, Qt::SolidLine ) )
+    painter.drawLine(0, y_c, 1000, y_c)
+    painter.drawLine(0, 0, x_c, y_c)
+    painter.drawLine(0, 1000, x_c, y_c)
+    painter.drawLine(1000, 0, x_c, y_c)
+    painter.drawLine(1000, 1000, x_c, y_c)
 end

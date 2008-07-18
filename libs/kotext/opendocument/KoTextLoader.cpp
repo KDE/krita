df0dfc889bb8ee037593bb30a31b31e01b32dff6
Pierre Ducroquet <pinaraf@gmail.com>
2008-07-18 22:09:30 +0000

    Adding the assert wasn't the logical answer to this problem...
    svn path=/trunk/koffice/; revision=834440

--- libs/kotext/opendocument/KoTextLoader.cpp
+++ libs/kotext/opendocument/KoTextLoader.cpp
@@ -151,8 +151,7 @@ void KoTextLoader::loadBody( const KoXmlElement& bodyElem, QTextCursor& cursor )
         d->styleManager = layout->styleManager();
     }
 
-    if (document->isEmpty()) {
-        Q_ASSERT(d->styleManager);
+    if ((document->isEmpty()) && (d->styleManager)) {
         QTextBlock block = cursor.block();
         d->styleManager->defaultParagraphStyle()->applyStyle(block);
     }

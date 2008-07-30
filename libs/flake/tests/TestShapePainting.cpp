67a32388135622dab6ceb24b0889b4f15793698e
Johannes Simon <johannes.simon@gmail.com>
2008-07-30 15:35:56 +0000

    Extend the shape painting test to check different clipping properties with two shapes (i.e. one clipped, the other one not). The first should be painted by the container, the second by the shape manager.
    svn path=/trunk/koffice/; revision=839694

--- libs/flake/tests/TestShapePainting.cpp
+++ libs/flake/tests/TestShapePainting.cpp
@@ -8,20 +8,25 @@
 #include <kcomponentdata.h>
 
 void TestShapePainting::testPaintShape() {
-    MockShape shape;
+    MockShape shape1;
+    MockShape shape2;
     MockContainer container;
 
     KComponentData componentData( "TestShapePainting" );  // we need an instance for that canvas
 
-    container.addChild(&shape);
-    QCOMPARE(shape.parent(), &container);
-    container.setClipping(&shape, false);
-    QCOMPARE(container.childClipped(&shape), false);
+    container.addChild(&shape1);
+    container.addChild(&shape2);
+    QCOMPARE(shape1.parent(), &container);
+    QCOMPARE(shape2.parent(), &container);
+    container.setClipping(&shape1, false);
+    container.setClipping(&shape2, false);
+    QCOMPARE(container.childClipped(&shape1), false);
+    QCOMPARE(container.childClipped(&shape2), false);
 
     MockCanvas canvas;
     KoShapeManager manager(&canvas);
     manager.add(&container);
-    QCOMPARE(manager.shapes().count(), 2);
+    QCOMPARE(manager.shapes().count(), 3);
 
     QImage image(100, 100,  QImage::Format_Mono);
     QPainter painter(&image);
@@ -29,27 +34,35 @@ void TestShapePainting::testPaintShape() {
     manager.paint(painter, vc, false);
 
     // with the shape not being clipped, the shapeManager will paint it for us.
-    QCOMPARE(shape.paintedCount, 1);
+    QCOMPARE(shape1.paintedCount, 1);
+    QCOMPARE(shape2.paintedCount, 1);
     QCOMPARE(container.paintedCount, 1);
 
     // the container should thus not paint the shape
-    shape.paintedCount = 0;
+    shape1.paintedCount = 0;
+    shape2.paintedCount = 0;
     container.paintedCount = 0;
     container.paint(painter, vc);
-    QCOMPARE(shape.paintedCount, 0);
+    QCOMPARE(shape1.paintedCount, 0);
+    QCOMPARE(shape2.paintedCount, 0);
     QCOMPARE(container.paintedCount, 1);
 
 
-    container.setClipping(&shape, true);
-    QCOMPARE(container.childClipped(&shape), true);
+    container.setClipping(&shape1, false);
+    container.setClipping(&shape2, true);
+    QCOMPARE(container.childClipped(&shape1), false);
+    QCOMPARE(container.childClipped(&shape2), true);
 
-    shape.paintedCount = 0;
+    shape1.paintedCount = 0;
+    shape2.paintedCount = 0;
     container.paintedCount = 0;
     manager.paint(painter, vc, false);
 
-    // with the shape being clipped, the shapeManager will paint just the container, but the container will
-    // paint the child shape for us.
-    QCOMPARE(shape.paintedCount, 1);
+
+    // with this shape not being clipped, the shapeManager will paint the container and this shape
+    QCOMPARE(shape1.paintedCount, 1);
+    // with this shape being clipped, the container will paint it for us.
+    QCOMPARE(shape2.paintedCount, 1);
     QCOMPARE(container.paintedCount, 1);
 }
 

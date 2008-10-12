eee97910daca1459f3cd54fe7dbea5fc901b2e50
Jan Hambrecht <jaham@gmx.net>
2008-10-12 17:37:32 +0000

    move the line style dropdown up to the top of the docker as it is probably the most important options together with the width
    svn path=/trunk/koffice/; revision=870534

--- plugins/dockers/strokedocker/StrokeDocker.cpp
+++ plugins/dockers/strokedocker/StrokeDocker.cpp
@@ -5,7 +5,7 @@
    Copyright (C) 2005-2006 Tim Beaulen <tbscope@gmail.com>
    Copyright (C) 2005-2007 Thomas Zander <zander@kde.org>
    Copyright (C) 2005-2006 Inge Wallin <inge@lysator.liu.se>
-   Copyright (C) 2005-2007 Jan Hambrecht <jaham@gmx.net>
+   Copyright (C) 2005-2008 Jan Hambrecht <jaham@gmx.net>
    Copyright (C) 2006 Casper Boemann <cbr@boemann.dk>
    Copyright (C) 2006 Peter Simonsson <psn@linux.se>
    Copyright (C) 2006 Laurent Montel <montel@kde.org>
@@ -70,19 +70,26 @@ StrokeDocker::StrokeDocker()
     QWidget *mainWidget = new QWidget( this );
     QGridLayout *mainLayout = new QGridLayout( mainWidget );
 
+    QLabel * styleLabel = new QLabel( i18n( "Style:" ), mainWidget );
+    mainLayout->addWidget( styleLabel, 0, 0 );
+    d->lineStyle = new KoLineStyleSelector( mainWidget );
+    mainLayout->addWidget( d->lineStyle, 0, 1, 1, 3 );
+
+    connect( d->lineStyle, SIGNAL(currentIndexChanged( int ) ), this, SLOT( styleChanged() ) );
+
     QLabel* widthLabel = new QLabel( i18n ( "Width:" ), mainWidget );
-    mainLayout->addWidget( widthLabel, 0, 0 );
+    mainLayout->addWidget( widthLabel, 1, 0 );
     // set min/max/step and value in points, then set actual unit
     d->setLineWidth = new KoUnitDoubleSpinBox( mainWidget );
     d->setLineWidth->setMinMaxStep( 0.0, 1000.0, 0.5 );
     d->setLineWidth->setDecimals( 1 );
     d->setLineWidth->setUnit( KoUnit(KoUnit::Point) );
     d->setLineWidth->setToolTip( i18n( "Set line width of actual selection" ) );
-    mainLayout->addWidget( d->setLineWidth, 0, 1, 1, 3 );
+    mainLayout->addWidget( d->setLineWidth, 1, 1, 1, 3 );
     connect( d->setLineWidth, SIGNAL( valueChangedPt( qreal ) ), this, SLOT( widthChanged() ) );
 
     QLabel* capLabel = new QLabel( i18n ( "Cap:" ), mainWidget );
-    mainLayout->addWidget( capLabel, 1, 0 );
+    mainLayout->addWidget( capLabel, 2, 0 );
     d->capGroup = new QButtonGroup( mainWidget );
     d->capGroup->setExclusive( true );
     d->capGroup->setExclusive( true );
@@ -94,26 +101,26 @@ StrokeDocker::StrokeDocker()
     button->setCheckable( true );
     button->setToolTip( i18n( "Butt cap" ) );
     d->capGroup->addButton( button, Qt::FlatCap );
-    mainLayout->addWidget( button, 1, 1 );
+    mainLayout->addWidget( button, 2, 1 );
 
     button = new QRadioButton( mainWidget );
     button->setIcon( SmallIcon( "cap_round" ) );
     button->setCheckable( true );
     button->setToolTip( i18n( "Round cap" ) );
     d->capGroup->addButton( button, Qt::RoundCap );
-    mainLayout->addWidget( button, 1, 2 );
+    mainLayout->addWidget( button, 2, 2 );
 
     button = new QRadioButton( mainWidget );
     button->setIcon( SmallIcon( "cap_square" ) );
     button->setCheckable( true );
     button->setToolTip( i18n( "Square cap" ) );
     d->capGroup->addButton( button, Qt::SquareCap );
-    mainLayout->addWidget( button, 1, 3 );
+    mainLayout->addWidget( button, 2, 3 );
 
     connect( d->capGroup, SIGNAL( buttonClicked( int ) ), this, SLOT( slotCapChanged( int ) ) );
 
     QLabel* joinLabel = new QLabel( i18n ( "Join:" ), mainWidget );
-    mainLayout->addWidget( joinLabel, 2, 0 );
+    mainLayout->addWidget( joinLabel, 3, 0 );
 
     d->joinGroup = new QButtonGroup( mainWidget );
     d->joinGroup->setExclusive( true );
@@ -123,41 +130,34 @@ StrokeDocker::StrokeDocker()
     button->setCheckable( true );
     button->setToolTip( i18n( "Miter join" ) );
     d->joinGroup->addButton( button, Qt::MiterJoin );
-    mainLayout->addWidget( button, 2, 1 );
+    mainLayout->addWidget( button, 3, 1 );
 
     button = new QRadioButton( mainWidget );
     button->setIcon( SmallIcon( "join_round" ) );
     button->setCheckable( true );
     button->setToolTip( i18n( "Round join" ) );
     d->joinGroup->addButton( button, Qt::RoundJoin );
-    mainLayout->addWidget( button, 2, 2 );
+    mainLayout->addWidget( button, 3, 2 );
 
     button = new QRadioButton( mainWidget );
     button->setIcon( SmallIcon( "join_bevel" ) );
     button->setCheckable( true );
     button->setToolTip( i18n( "Bevel join" ) );
     d->joinGroup->addButton( button, Qt::BevelJoin );
-    mainLayout->addWidget( button, 2, 3 );
+    mainLayout->addWidget( button, 3, 3 );
 
     connect( d->joinGroup, SIGNAL( buttonClicked( int ) ), this, SLOT( slotJoinChanged( int ) ) );
 
     QLabel* miterLabel = new QLabel( i18n ( "Miter limit:" ), mainWidget );
-    mainLayout->addWidget( miterLabel, 3, 0 );
+    mainLayout->addWidget( miterLabel, 4, 0 );
     // set min/max/step and value in points, then set actual unit
     d->miterLimit = new KoUnitDoubleSpinBox( mainWidget );
     d->miterLimit->setMinMaxStep( 0.0, 1000.0, 0.5 );
     d->miterLimit->setDecimals( 1 );
     d->miterLimit->setUnit( KoUnit(KoUnit::Point) );
     d->miterLimit->setToolTip( i18n( "Set miter limit" ) );
-    mainLayout->addWidget( d->miterLimit, 3, 1, 1, 3 );
+    mainLayout->addWidget( d->miterLimit, 4, 1, 1, 3 );
     connect( d->miterLimit, SIGNAL( valueChangedPt( qreal ) ), this, SLOT( miterLimitChanged() ) );
-
-    QLabel * styleLabel = new QLabel( i18n( "Style:" ), mainWidget );
-    mainLayout->addWidget( styleLabel, 4, 0 );
-    d->lineStyle = new KoLineStyleSelector( mainWidget );
-    mainLayout->addWidget( d->lineStyle, 4, 1, 1, 3 );
-
-    connect( d->lineStyle, SIGNAL(currentIndexChanged( int ) ), this, SLOT( styleChanged() ) );
 
     mainLayout->setRowStretch( 5, 1 );
     mainLayout->setColumnStretch( 1, 1 );

fe12b1c609ce8ec48ab311e900276cf4d13315cd
Boudewijn Rempt <boud@valdyas.org>
2008-10-17 12:15:57 +0000

    OS X is a weird platform
    svn path=/trunk/koffice/; revision=872532

--- krita/image/kis_paintop.cc
+++ krita/image/kis_paintop.cc
@@ -108,7 +108,11 @@ static double paintBezierCurve(KisPaintOp *paintOp,
     qreal d1 = line.absDistance(control1);
     qreal d2 = line.absDistance(control2);
 
-    if ((d1 < BEZIER_FLATNESS_THRESHOLD && d2 < BEZIER_FLATNESS_THRESHOLD)  || isnan(d1) || isnan(d2)) {
+#ifdef Q_WS_MAC
+#define isnan(x)  std::isnan(x)
+#endif
+    if ((d1 < BEZIER_FLATNESS_THRESHOLD && d2 < BEZIER_FLATNESS_THRESHOLD)  
+            || isnan(d1) || isnan(d2)) {
         newDistance = paintOp->paintLine(pi1, pi2, savedDist);
     } else {
         // Midpoint subdivision. See Foley & Van Dam Computer Graphics P.508

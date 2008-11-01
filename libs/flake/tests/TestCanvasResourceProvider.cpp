7877a635455cf14a46b7150c5ec729ef0c38cee9
Thorsten Zachmann <t.zachmann@zagge.de>
2008-11-01 12:49:29 +0000

    o added test for KoCanvasResourceProvider o test setResource for KoShape*
    svn path=/trunk/koffice/; revision=878499

---
+++ libs/flake/tests/TestCanvasResourceProvider.cpp
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+   Copyright (C) 2008 Thorsten Zachmann <zachmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#include "TestCanvasResourceProvider.h"
+
+#include "KoCanvasResourceProvider.h"
+#include "KoPathShape.h"
+
+void TestCanvasResourceProvider::koShapeResource()
+{
+    KoPathShape * shape = new KoPathShape();
+    int key = 9001;
+
+    KoCanvasResourceProvider rp( 0 );
+    rp.setResource( key, shape );
+    QVERIFY( shape == rp.koShapeResource( key ) );
+}
+
+QTEST_MAIN(TestCanvasResourceProvider)
+#include "TestCanvasResourceProvider.moc"

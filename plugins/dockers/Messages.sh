1d78ce1d7506ad257f9702199cce3f3f8adf10ef
Burkhard Lück <lueck@hube-lueck.de>
2008-11-11 02:58:46 +0000

    fix wrong subdie name
    svn path=/trunk/koffice/; revision=882633

--- plugins/dockers/Messages.sh
+++ plugins/dockers/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$XGETTEXT *.cpp shapeselector/*.cpp strokedocker/*.cpp shapeproperties/*.cpp shapecollection/*.cpp -o $podir/koffice-dockers.pot
+$XGETTEXT *.cpp shapeselector/*.cpp shadowdocker/*.cpp shapeproperties/*.cpp shapecollection/*.cpp -o $podir/koffice-dockers.pot
 

53c22994a2e8fa8ce860825e18ce2ff9fdc47d8b
Alexia Allanic <alexia_allanic@yahoo.fr>
2008-11-16 19:32:57 +0000

    add copyright and licence headers
    svn path=/branches/work/koffice-isi/; revision=885217

--- plugins/dockers/shapeendlines/KoShapeEndLines.cpp
+++ plugins/dockers/shapeendlines/KoShapeEndLines.cpp
@@ -1,9 +1,22 @@
-/*
- * KoShapeEndLines.cpp
+/* This file is part of the KDE project
+ * Copyright (C) 2008 Jan Hambrecht <jaham@gmx.net>
  *
- *  Created on: 10 nov. 2008
- *      Author: alexia
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
+
 
 #include "KoShapeEndLines.h"
 #include <QGridLayout>
@@ -11,7 +24,6 @@
 #include <klocale.h>
 
 KoShapeEndLines::KoShapeEndLines(QWidget* parent): QDockWidget(parent){
-	// TODO Auto-generated constructor stub
 	setWindowTitle(i18n("End lines"));
 
 	    QWidget* mainWidget = new QWidget(this);
@@ -35,5 +47,5 @@ KoShapeEndLines::KoShapeEndLines(QWidget* parent): QDockWidget(parent){
 }
 
 KoShapeEndLines::~KoShapeEndLines() {
-	// TODO Auto-generated destructor stub
+
 }

74a820922eecb7f0d1baf0aa5ffeab17d9d04c87
Cyrille Berger <cyb@lepi.org>
2008-12-07 12:34:34 +0000

    add a class that inherits KoProgressProxy and will allow nodes to report processing update
    svn path=/trunk/koffice/; revision=893750

---
+++ krita/image/kis_node_progress_proxy.h
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2008 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_NODE_PROGRESS_PROXY_H_
+#define _KIS_NODE_PROGRESS_PROXY_H_
+
+#include <KoProgressUpdater.h>
+#include <QObject>
+
+#include <kis_types.h>
+
+/**
+ * This class implements \ref KoProgressProxy and allows node to report progress.
+ */
+class KisNodeProgressProxy : public QObject, public KoProgressProxy {
+    Q_OBJECT
+    public:
+        KisNodeProgressProxy( KisNode* );
+        ~KisNodeProgressProxy();
+        
+    public:
+        const KisNodeSP node() const;
+        virtual int maximum() const ;
+        virtual void setValue( int value );
+        virtual void setRange( int minimum, int maximum );
+        virtual void setFormat( const QString & format );
+    signals:
+        /**
+         * Emited when the percentage of the proxy is changed.
+         * @param _percentage is the progress value in percent
+         * @param _node is the node that own this \ref KisNodeProgressProxy
+         */
+        void percentageChanged( int _percentage, const KisNodeSP _node);
+    private:
+        struct Private;
+        Private* const d;
+};
+
+#endif

b9a30ca3c289c11c72b4b3a2f7e1d2b8caa6eadc
Thorsten Zachmann <t.zachmann@zagge.de>
2008-12-10 06:40:34 +0000

    o Fix save/load and copy/paste to keep the type of the points. This uses   the attribute koffice:nodeTypes. It has the same syntax for the field   as inkscape uses for saving that information. Inkscape however uses   the ns sodipodi.
    svn path=/trunk/koffice/; revision=895165

--- libs/flake/KoPathShape.cpp
+++ libs/flake/KoPathShape.cpp
@@ -68,6 +68,7 @@ void KoPathShape::saveOdf(KoShapeSavingContext & context) const
     saveOdfAttributes(context, OdfAllAttributes | OdfViewbox);
 
     context.xmlWriter().addAttribute("svg:d", toString(QMatrix()));
+    context.xmlWriter().addAttribute("koffice:nodeTypes", nodeTypes());
 
     saveOdfCommonChildElements(context);
     context.xmlWriter().endElement();
@@ -112,6 +113,7 @@ bool KoPathShape::loadOdf(const KoXmlElement & element, KoShapeLoadingContext &c
     } else { // path loading
         KoPathShapeLoader loader(this);
         loader.parseSvg(element.attributeNS(KoXmlNS::svg, "d"), true);
+        loadNodeTypes(element);
     }
 
     applyViewboxTransformation(element);
@@ -1103,6 +1105,82 @@ QString KoPathShape::toString(const QMatrix &matrix) const
     }
 
     return d;
+}
+
+char nodeType( const KoPathPoint * point )
+{
+    if (point->properties() & KoPathPoint::IsSmooth) {
+        return 's';
+    }
+    else if (point->properties() & KoPathPoint::IsSymmetric) {
+        return 'z';
+    }
+    else {
+        return 'c';
+    }
+}
+
+QString KoPathShape::nodeTypes() const
+{
+    QString types;
+    KoSubpathList::const_iterator pathIt(m_subpaths.begin());
+    for (; pathIt != m_subpaths.end(); ++pathIt) {
+        KoSubpath::const_iterator it((*pathIt)->begin());
+        for (; it != (*pathIt)->end(); ++it) {
+            if (it == (*pathIt)->begin()) {
+                types.append( 'c' );
+            }
+            else {
+                types.append( nodeType( *it ) );
+            }
+
+            if ((*it)->properties() & KoPathPoint::StopSubpath
+                && (*it)->properties() & KoPathPoint::CloseSubpath) {
+                KoPathPoint * firstPoint = (*pathIt)->first();
+                types.append( nodeType( firstPoint ) );
+            }
+        }
+    }
+    return types;
+}
+
+void updateNodeType( KoPathPoint * point, const QChar & nodeType )
+{
+    if (nodeType == 's') {
+        point->setProperty(KoPathPoint::IsSmooth);
+    }
+    else if (nodeType == 'z') {
+        point->setProperty(KoPathPoint::IsSymmetric);
+    }
+}
+
+void KoPathShape::loadNodeTypes(const KoXmlElement & element)
+{
+    if (element.hasAttributeNS(KoXmlNS::koffice, "nodeTypes")) {
+        QString nodeTypes = element.attributeNS(KoXmlNS::koffice, "nodeTypes");
+        QString::const_iterator nIt(nodeTypes.begin());
+        KoSubpathList::const_iterator pathIt(m_subpaths.begin());
+        for (; pathIt != m_subpaths.end(); ++pathIt) {
+            KoSubpath::iterator it((*pathIt)->begin());
+            for (; it != (*pathIt)->end(); ++it, nIt++) {
+                // be sure not to crash if there are not enough nodes in nodeTypes
+                if (nIt == nodeTypes.end()) {
+                    kWarning(30006) << "not enough nodes in koffice:nodeTypes";
+                    return;
+                }
+                // the first node is always of type 'c'
+                if (it != (*pathIt)->begin()) {
+                    updateNodeType(*it, *nIt);
+                }
+
+                if ((*it)->properties() & KoPathPoint::StopSubpath
+                    && (*it)->properties() & KoPathPoint::CloseSubpath) {
+                    ++nIt;
+                    updateNodeType((*pathIt)->first(), *nIt);
+                }
+            }
+        }
+    }
 }
 
 Qt::FillRule KoPathShape::fillRule() const
--- libs/flake/KoPathShape.h
+++ libs/flake/KoPathShape.h
@@ -475,6 +475,36 @@ protected:
     /// Applies the viewbox transformation defined in the given element
     void applyViewboxTransformation(const KoXmlElement & element);
 
+    /**
+     * @brief Save the node types
+     *
+     * This is inspired by inkscape and uses the same mechanism as they do.
+     * The only difference is that they use sodipodi:nodeTypes as element and 
+     * we use koffice:nodeTyes as attribute.
+     * This attribute contains of a string which has the node type of each point
+     * in it. The following node types exist:
+     *
+     * c corner
+     * s smooth
+     * z symetric
+     *
+     * The first point of a path is always of the type c.
+     * If the path is closed the type of the first point is saved in the last elemeent
+     * E.g. you have a closed path with 2 points in it. The first one (start/end of path) 
+     * is symetric and the second one is smooth that will result in the nodeType="czs"
+     * So if there is a closed sub path the nodeTypes contain one more entry then there 
+     * are points. That is due to the first and the last pojnt of a closed sub path get 
+     * merged into one when they are on the same position.
+     *
+     * @return The node types as string
+     */
+    QString nodeTypes() const;
+
+    /**
+     * @brief Load node types
+     */
+    void loadNodeTypes(const KoXmlElement & element);
+
     KoSubpathList m_subpaths;
 
 private:

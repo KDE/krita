7d8af67e86b8dc827b351ae457ef28214a5b83b2
Cyrille Berger <cyb@lepi.org>
2008-12-22 20:17:08 +0000

    use the same colorspace as end point color, this fix segment spliting
    svn path=/trunk/koffice/; revision=900412

--- krita/image/kis_autogradient_resource.cc
+++ krita/image/kis_autogradient_resource.cc
@@ -112,7 +112,7 @@ void KisAutogradientResource::splitSegment(KoGradientSegment* segment)
     Q_ASSERT(segment != 0);
     QList<KoGradientSegment*>::iterator it = qFind(m_segments.begin(), m_segments.end(), segment);
     if (it != m_segments.end()) {
-        KoColor midleoffsetColor;
+        KoColor midleoffsetColor(  segment->endColor().colorSpace() );
         segment->colorAt(midleoffsetColor, segment->middleOffset());
         KoGradientSegment* newSegment = new KoGradientSegment(
             segment->interpolation(), segment->colorInterpolation(),

0992c5b83d8fbb3bf6d42112496288103ac0d87c
Cyrille Berger <cyb@lepi.org>
2008-12-23 11:43:54 +0000

    also don't extract translation for chalk
    svn path=/trunk/koffice/; revision=900598

--- krita/Messages.sh
+++ krita/Messages.sh
@@ -3,6 +3,6 @@ $EXTRACTRC `find . -name \*.ui` >> rc.cpp
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
 perl extracti18n.pl > i18ndata
 # ignore sdk/templates since it contains templates for use a future plugins, none of the strings there will ever be seen by any user
-$XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h  -o -name \*.cpp | grep -v './sdk/templates' | grep -v './plugins/paintops/deform'` ui/kis_aboutdata.h -o $podir/krita.pot
+$XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h  -o -name \*.cpp | grep -v './sdk/templates' | grep -v './plugins/paintops/deform' | grep -v './plugins/paintops/chalk'` ui/kis_aboutdata.h -o $podir/krita.pot
 rm -f i18ndata
 

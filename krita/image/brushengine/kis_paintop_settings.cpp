f7ea622c39ad117ae8518351f8b0ec9f12a4f15f
Boudewijn Rempt <boud@valdyas.org>
2008-12-30 11:31:47 +0000

    create a temporary layer so paintops that need a node work correctly
    svn path=/trunk/koffice/; revision=903281

--- krita/image/brushengine/kis_paintop_settings.cpp
+++ krita/image/brushengine/kis_paintop_settings.cpp
@@ -28,6 +28,7 @@
 #include <KoColorSpaceRegistry.h>
 
 #include "kis_node.h"
+#include "kis_paint_layer.h"
 #include "kis_image.h"
 #include "kis_painter.h"
 #include "kis_paint_device.h"
@@ -78,19 +79,21 @@ KisNodeSP KisPaintOpSettings::node() const
 QImage KisPaintOpSettings::sampleStroke(const QSize& size )
 {
     const KoColorSpace * cs = KoColorSpaceRegistry::instance()->rgb8();
-    KisPaintDeviceSP mydevice = new KisPaintDevice(cs, "preview device");
-
-    KisPainter painter(mydevice);
-    painter.setPaintColor(KoColor() );
-
-    KisPaintOpPresetSP preset = new KisPaintOpPreset();
-    preset->setSettings( this );
-
     int width = size.width();
     int height = size.height();
 
     KisImageSP img = new KisImage(0, width, height, cs, "temporary for stroke sample");
-    painter.setPaintOpPreset( preset, img);
+    KisLayerSP layer = new KisPaintLayer(img, "temporary for stroke sample", OPACITY_OPAQUE, cs);
+    
+    
+    KisPainter painter(layer->paintDevice());
+    painter.setPaintColor(KoColor() );
+
+    KisPaintOpPresetSP preset = new KisPaintOpPreset();
+    preset->setSettings( this ); // This clones
+    preset->settings()->setNode( layer );
+    
+painter.setPaintOpPreset( preset, img);
 
     QPointF p1(1.0 / 6.0*width, 2.0 / 3.0*height);
     QPointF p2(2.0 / 6.0*width, 1.0 / 3.0*height);
@@ -133,5 +136,5 @@ QImage KisPaintOpSettings::sampleStroke(const QSize& size )
     c2.setY(p3.y() + 5);
     painter.paintBezierCurve(pi3, c1, c2, pi4, 0);
 
-    return mydevice->convertToQImage(0);
+    return layer->paintDevice()->convertToQImage(0);
 }

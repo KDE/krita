3247a5e69e9a0f7e080bb093ab05a10ef879243d
Thomas Zander <zander@kde.org>
2009-01-01 16:03:30 +0000

    Make this a QTest based unit test.
    svn path=/trunk/koffice/; revision=904196

--- libs/store/tests/TestXmlWriter.cpp
+++ libs/store/tests/TestXmlWriter.cpp
@@ -17,7 +17,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
-#include "KoXmlWriter.h"
+#include <KoXmlWriter.h>
 
 #include <Q3CString>
 #include <qtest_kde.h>

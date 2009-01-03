1d1f8c1ee2a25c7ac17ae149d519f823c1ac4982
Thomas Zander <zander@kde.org>
2009-01-03 19:41:23 +0000

    Loads of little updates
    svn path=/trunk/koffice/; revision=905143

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-EXCLUDE="-path ./koproperty"
+EXCLUDE=""
 LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.ui -o -name \*.rc \) -type f | grep -v -e '/\.'`
 $EXTRACTRC $LIST >> rc.cpp
 LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.cpp -o -name \*.cc \) -type f | grep -v -e '/\.'`

e54b1b95a4f0da3badd3561322dd96bbf36deb1b
Cyrille Berger <cyb@lepi.org>
2009-02-21 17:13:15 +0000

    Fix: crash when pasting again in an image created from "paste into a new image" (and probably countless of other crashes that would happen when using the color space of the image)
    Paint device own their own copy of the color space, when creating an image, we should fetch the one from the color space registry.
    
    BUG:182970
    
    svn path=/trunk/koffice/; revision=929594

--- krita/ui/kis_selection_manager.cc
+++ krita/ui/kis_selection_manager.cc
@@ -515,7 +515,8 @@ void KisSelectionManager::pasteNew()
     Q_ASSERT(doc->undoAdapter() != 0);
     doc->undoAdapter()->setUndo(false);
 
-    KisImageSP img = new KisImage(doc->undoAdapter(), r.width(), r.height(), clip->colorSpace(), "Pasted");
+    KisImageSP img = new KisImage(doc->undoAdapter(), r.width(), r.height(),
+                                  KoColorSpaceRegistry::instance()->colorSpace( clip->colorSpace()->id(), clip->colorSpace()->profile() ), "Pasted" ); // TODO should be translated ?
     KisPaintLayerSP layer = new KisPaintLayer(img.data(), clip->objectName(), OPACITY_OPAQUE, clip->colorSpace());
 
     KisPainter p(layer->paintDevice());

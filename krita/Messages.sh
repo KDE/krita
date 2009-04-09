18c14104ead4add2c14267ee4928bac4a8658794
Albert Astals Cid <tsdgeos@terra.es>
2009-04-09 17:40:31 +0000

    seems this moved to a different dir
    svn path=/trunk/koffice/; revision=951583

--- krita/Messages.sh
+++ krita/Messages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp 
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
-$EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment plugins/viewplugins/scripting/scripts/*.rc >> rc.cpp || exit 12
+$EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment plugins/extensions/scripting/scripts/*.rc >> rc.cpp || exit 12
 perl extracti18n.pl > i18ndata
 # ignore sdk/templates since it contains templates for use a future plugins, none of the strings there will ever be seen by any user
 $XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h  -o -name \*.cpp | grep -v './sdk/templates'` ui/kis_aboutdata.h -o $podir/krita.pot

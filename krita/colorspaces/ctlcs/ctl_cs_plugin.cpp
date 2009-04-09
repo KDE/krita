44406aaefb7018157445ac6486d3e06f0773d6c8
Cyrille Berger <cyb@lepi.org>
2009-04-09 20:19:33 +0000

    add missing files...
    svn path=/trunk/koffice/; revision=951635

---
+++ krita/colorspaces/ctlcs/ctl_cs_plugin.cpp
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#include "ctl_cs_plugin.h"
+
+#include <kcomponentdata.h>
+#include <kgenericfactory.h>
+#include <KoColorSpaceRegistry.h>
+#include "KoBasicHistogramProducers.h"
+#include <KoCtlColorProfile.h>
+#include <kstandarddirs.h>
+
+#include <OpenCTL/ModulesManager.h>
+#include "kis_debug.h"
+
+// #include "kis_lms_f32_colorspace.h"
+
+typedef KGenericFactory<CTLCSPlugin> CTLCSPluginPluginFactory;
+K_EXPORT_COMPONENT_FACTORY(krita_ctlcs_plugin, CTLCSPluginPluginFactory("krita"))
+
+
+CTLCSPlugin::CTLCSPlugin(QObject *parent, const QStringList &)
+        : KParts::Plugin(parent)
+{
+
+    KoColorSpaceRegistry * f = KoColorSpaceRegistry::instance();
+    {
+        // Set PigmentCMS's ctl module directory
+        QStringList ctlModulesDirs = KGlobal::mainComponent().dirs()->findDirs( "data", "pigmentcms/ctlmodules/");
+        dbgPlugins << ctlModulesDirs;
+        foreach(const QString & dir, ctlModulesDirs)
+        {
+            dbgPlugins << "Append : " << dir << " to the list of CTL modules";
+            OpenCTL::ModulesManager::instance()->addDirectory( dir.toAscii().data());
+        }
+        
+        // Load CTL Profiles
+        KGlobal::mainComponent().dirs()->addResourceType("ctl_profiles", "data", "pigmentcms/ctlprofiles/");
+        QStringList ctlprofileFilenames;
+        ctlprofileFilenames += KGlobal::mainComponent().dirs()->findAllResources("ctl_profiles", "*.ctlp",  KStandardDirs::Recursive);
+        dbgPlugins << "There are " << ctlprofileFilenames.size() << " CTL profiles";
+        if (!ctlprofileFilenames.empty()) {
+            KoColorProfile* profile = 0;
+            for( QStringList::Iterator it = ctlprofileFilenames.begin(); it != ctlprofileFilenames.end(); ++it ) {
+                dbgPlugins << "Load profile : " << *it;
+                profile = new KoCtlColorProfile(*it);
+                profile->load();
+                if (profile->valid()) {
+                    dbgPlugins << "Valid profile : " << profile->name();
+                    f->addProfile( profile );
+                } else {
+                    dbgPlugins << "Invalid profile : " << profile->name();
+                    delete profile;
+                }
+            }
+        }
+
+#if 0
+        KisLmsAF32ColorSpaceFactory * csf  = new KisLmsAF32ColorSpaceFactory();
+        f->add(csf);
+        const KoCtlColorProfile* profile = static_cast<const KoCtlColorProfile*>(f->profileByName(csf->defaultProfile()));
+        Q_ASSERT(profile);
+        KoColorSpace * colorSpaceLMSF32  = new KisLmsAF32ColorSpace(profile);
+
+        KoHistogramProducerFactoryRegistry::instance()->add(
+            new KoBasicHistogramProducerFactory<KoBasicF32HistogramProducer>
+            (KoID("LMSF32HISTO", i18n("Float32 Histogram")), colorSpaceLMSF32));
+#endif
+    }
+
+}
+
+CTLCSPlugin::~CTLCSPlugin()
+{
+}
+
+#include "ctl_cs_plugin.moc"

f60d36cf5160ca3a055d54ef037f3676c0f5c06c
Albert Astals Cid <tsdgeos@terra.es>
2009-04-12 18:58:02 +0000

    I18N_NOOP2 is evil and should be used with extreme care, otherwise you end up with untranslatable messages CCMAIL: js@iidea.pl
    svn path=/trunk/koffice/; revision=952881

--- libs/main/KoPageFormat.cpp
+++ libs/main/KoPageFormat.cpp
@@ -56,35 +56,35 @@ struct PageFormatInfo {
 // - the comments "should be..." indicates the exact values if the inch sizes would be multiplied by 25.4 mm/inch
 
 const PageFormatInfo pageFormatInfo[] = {
-    { KoPageFormat::IsoA3Size,       QPrinter::A3,        "A3",        I18N_NOOP("ISO A3"),       297.0,  420.0 },
-    { KoPageFormat::IsoA4Size,       QPrinter::A4,        "A4",        I18N_NOOP("ISO A4"),       210.0,  297.0 },
-    { KoPageFormat::IsoA5Size,       QPrinter::A5,        "A5",        I18N_NOOP("ISO A5"),       148.0,  210.0 },
-    { KoPageFormat::UsLetterSize,    QPrinter::Letter,    "Letter",    I18N_NOOP("US Letter"),    215.9,  279.4 },
-    { KoPageFormat::UsLegalSize,     QPrinter::Legal,     "Legal",     I18N_NOOP("US Legal"),     215.9,  355.6 },
-    { KoPageFormat::ScreenSize,      QPrinter::A4,        "Screen",    I18N_NOOP("Screen"), PG_A4_HEIGHT, PG_A4_WIDTH }, // Custom, so fall back to A4
-    { KoPageFormat::CustomSize,      QPrinter::A4,        "Custom",    I18N_NOOP2("Custom size", "Custom"), PG_A4_WIDTH, PG_A4_HEIGHT }, // Custom, so fall back to A4
-    { KoPageFormat::IsoB5Size,       QPrinter::B5,        "B5",        I18N_NOOP("ISO B5"),       182.0,  257.0 },
-    { KoPageFormat::UsExecutiveSize, QPrinter::Executive, "Executive", I18N_NOOP("US Executive"), 191.0,  254.0 }, // should be 190.5 mm x 254.0 mm
-    { KoPageFormat::IsoA0Size,       QPrinter::A0,        "A0",        I18N_NOOP("ISO A0"),       841.0, 1189.0 },
-    { KoPageFormat::IsoA1Size,       QPrinter::A1,        "A1",        I18N_NOOP("ISO A1"),       594.0,  841.0 },
-    { KoPageFormat::IsoA2Size,       QPrinter::A2,        "A2",        I18N_NOOP("ISO A2"),       420.0,  594.0 },
-    { KoPageFormat::IsoA6Size,       QPrinter::A6,        "A6",        I18N_NOOP("ISO A6"),       105.0,  148.0 },
-    { KoPageFormat::IsoA7Size,       QPrinter::A7,        "A7",        I18N_NOOP("ISO A7"),        74.0,  105.0 },
-    { KoPageFormat::IsoA8Size,       QPrinter::A8,        "A8",        I18N_NOOP("ISO A8"),        52.0,   74.0 },
-    { KoPageFormat::IsoA9Size,       QPrinter::A9,        "A9",        I18N_NOOP("ISO A9"),        37.0,   52.0 },
-    { KoPageFormat::IsoB0Size,       QPrinter::B0,        "B0",        I18N_NOOP("ISO B0"),      1030.0, 1456.0 },
-    { KoPageFormat::IsoB1Size,       QPrinter::B1,        "B1",        I18N_NOOP("ISO B1"),       728.0, 1030.0 },
-    { KoPageFormat::IsoB10Size,      QPrinter::B10,       "B10",       I18N_NOOP("ISO B10"),       32.0,   45.0 },
-    { KoPageFormat::IsoB2Size,       QPrinter::B2,        "B2",        I18N_NOOP("ISO B2"),       515.0,  728.0 },
-    { KoPageFormat::IsoB3Size,       QPrinter::B3,        "B3",        I18N_NOOP("ISO B3"),       364.0,  515.0 },
-    { KoPageFormat::IsoB4Size,       QPrinter::B4,        "B4",        I18N_NOOP("ISO B4"),       257.0,  364.0 },
-    { KoPageFormat::IsoB6Size,       QPrinter::B6,        "B6",        I18N_NOOP("ISO B6"),       128.0,  182.0 },
-    { KoPageFormat::IsoC5Size,       QPrinter::C5E,       "C5",        I18N_NOOP("ISO C5"),       163.0,  229.0 }, // Some sources tells: 162 mm x 228 mm
-    { KoPageFormat::UsComm10Size,    QPrinter::Comm10E,   "Comm10",    I18N_NOOP("US Common 10"), 105.0,  241.0 }, // should be 104.775 mm x 241.3 mm
-    { KoPageFormat::IsoDLSize,       QPrinter::DLE,       "DL",        I18N_NOOP("ISO DL"),       110.0,  220.0 },
-    { KoPageFormat::UsFolioSize,     QPrinter::Folio,     "Folio",     I18N_NOOP("US Folio"),     210.0,  330.0 }, // should be 209.54 mm x 330.2 mm
-    { KoPageFormat::UsLedgerSize,    QPrinter::Ledger,    "Ledger",    I18N_NOOP("US Ledger"),    432.0,  279.0 }, // should be 431.8 mm x 297.4 mm
-    { KoPageFormat::UsTabloidSize,   QPrinter::Tabloid,   "Tabloid",   I18N_NOOP("US Tabloid"),   279.0,  432.0 },  // should be 297.4 mm x 431.8 mm
+    { KoPageFormat::IsoA3Size,       QPrinter::A3,        "A3",        I18N_NOOP2("Page size", "ISO A3"),       297.0,  420.0 },
+    { KoPageFormat::IsoA4Size,       QPrinter::A4,        "A4",        I18N_NOOP2("Page size", "ISO A4"),       210.0,  297.0 },
+    { KoPageFormat::IsoA5Size,       QPrinter::A5,        "A5",        I18N_NOOP2("Page size", "ISO A5"),       148.0,  210.0 },
+    { KoPageFormat::UsLetterSize,    QPrinter::Letter,    "Letter",    I18N_NOOP2("Page size", "US Letter"),    215.9,  279.4 },
+    { KoPageFormat::UsLegalSize,     QPrinter::Legal,     "Legal",     I18N_NOOP2("Page size", "US Legal"),     215.9,  355.6 },
+    { KoPageFormat::ScreenSize,      QPrinter::A4,        "Screen",    I18N_NOOP2("Page size", "Screen"), PG_A4_HEIGHT, PG_A4_WIDTH }, // Custom, so fall back to A4
+    { KoPageFormat::CustomSize,      QPrinter::A4,        "Custom",    I18N_NOOP2("Page size", "Custom"), PG_A4_WIDTH, PG_A4_HEIGHT }, // Custom, so fall back to A4
+    { KoPageFormat::IsoB5Size,       QPrinter::B5,        "B5",        I18N_NOOP2("Page size", "ISO B5"),       182.0,  257.0 },
+    { KoPageFormat::UsExecutiveSize, QPrinter::Executive, "Executive", I18N_NOOP2("Page size", "US Executive"), 191.0,  254.0 }, // should be 190.5 mm x 254.0 mm
+    { KoPageFormat::IsoA0Size,       QPrinter::A0,        "A0",        I18N_NOOP2("Page size", "ISO A0"),       841.0, 1189.0 },
+    { KoPageFormat::IsoA1Size,       QPrinter::A1,        "A1",        I18N_NOOP2("Page size", "ISO A1"),       594.0,  841.0 },
+    { KoPageFormat::IsoA2Size,       QPrinter::A2,        "A2",        I18N_NOOP2("Page size", "ISO A2"),       420.0,  594.0 },
+    { KoPageFormat::IsoA6Size,       QPrinter::A6,        "A6",        I18N_NOOP2("Page size", "ISO A6"),       105.0,  148.0 },
+    { KoPageFormat::IsoA7Size,       QPrinter::A7,        "A7",        I18N_NOOP2("Page size", "ISO A7"),        74.0,  105.0 },
+    { KoPageFormat::IsoA8Size,       QPrinter::A8,        "A8",        I18N_NOOP2("Page size", "ISO A8"),        52.0,   74.0 },
+    { KoPageFormat::IsoA9Size,       QPrinter::A9,        "A9",        I18N_NOOP2("Page size", "ISO A9"),        37.0,   52.0 },
+    { KoPageFormat::IsoB0Size,       QPrinter::B0,        "B0",        I18N_NOOP2("Page size", "ISO B0"),      1030.0, 1456.0 },
+    { KoPageFormat::IsoB1Size,       QPrinter::B1,        "B1",        I18N_NOOP2("Page size", "ISO B1"),       728.0, 1030.0 },
+    { KoPageFormat::IsoB10Size,      QPrinter::B10,       "B10",       I18N_NOOP2("Page size", "ISO B10"),       32.0,   45.0 },
+    { KoPageFormat::IsoB2Size,       QPrinter::B2,        "B2",        I18N_NOOP2("Page size", "ISO B2"),       515.0,  728.0 },
+    { KoPageFormat::IsoB3Size,       QPrinter::B3,        "B3",        I18N_NOOP2("Page size", "ISO B3"),       364.0,  515.0 },
+    { KoPageFormat::IsoB4Size,       QPrinter::B4,        "B4",        I18N_NOOP2("Page size", "ISO B4"),       257.0,  364.0 },
+    { KoPageFormat::IsoB6Size,       QPrinter::B6,        "B6",        I18N_NOOP2("Page size", "ISO B6"),       128.0,  182.0 },
+    { KoPageFormat::IsoC5Size,       QPrinter::C5E,       "C5",        I18N_NOOP2("Page size", "ISO C5"),       163.0,  229.0 }, // Some sources tells: 162 mm x 228 mm
+    { KoPageFormat::UsComm10Size,    QPrinter::Comm10E,   "Comm10",    I18N_NOOP2("Page size", "US Common 10"), 105.0,  241.0 }, // should be 104.775 mm x 241.3 mm
+    { KoPageFormat::IsoDLSize,       QPrinter::DLE,       "DL",        I18N_NOOP2("Page size", "ISO DL"),       110.0,  220.0 },
+    { KoPageFormat::UsFolioSize,     QPrinter::Folio,     "Folio",     I18N_NOOP2("Page size", "US Folio"),     210.0,  330.0 }, // should be 209.54 mm x 330.2 mm
+    { KoPageFormat::UsLedgerSize,    QPrinter::Ledger,    "Ledger",    I18N_NOOP2("Page size", "US Ledger"),    432.0,  279.0 }, // should be 431.8 mm x 297.4 mm
+    { KoPageFormat::UsTabloidSize,   QPrinter::Tabloid,   "Tabloid",   I18N_NOOP2("Page size", "US Tabloid"),   279.0,  432.0 },  // should be 297.4 mm x 431.8 mm
     {(KoPageFormat::Format) - 1, (QPrinter::PageSize) - 1,   "",   "",   -1,  -1 }
 };
 
@@ -153,14 +153,14 @@ KoPageFormat::Format KoPageFormat::defaultFormat()
 
 QString KoPageFormat::name(Format format)
 {
-    return i18n(pageFormatInfo[ format ].descriptiveName);
+    return i18n("Page size", pageFormatInfo[ format ].descriptiveName);
 }
 
 QStringList KoPageFormat::allFormats()
 {
     QStringList lst;
     for (int i = 0; pageFormatInfo[i].format != -1 ;i++) {
-        lst << i18n(pageFormatInfo[ i ].descriptiveName);
+        lst << i18n("Page size", pageFormatInfo[ i ].descriptiveName);
     }
     return lst;
 }

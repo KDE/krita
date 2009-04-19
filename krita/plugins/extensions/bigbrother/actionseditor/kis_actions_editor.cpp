59b12744cfc2f2357bcc779abb423571e12ce977
Cyrille Berger <cyb@lepi.org>
2009-04-19 14:29:20 +0000

    start working on an action editor
    svn path=/trunk/koffice/; revision=956212

---
+++ krita/plugins/extensions/bigbrother/actionseditor/kis_actions_editor.cpp
@@ -0,0 +1,32 @@
+/* This file is part of the KDE project
+ * Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_actions_editor.h"
+
+#include "ui_wdgactionseditor.h"
+
+KisActionsEditor::KisActionsEditor(QWidget* parent) : QWidget(parent), m_form(new Ui::ActionsEditor)
+{
+    m_form->setupUi(this);
+}
+
+KisActionsEditor::~KisActionsEditor()
+{
+    delete m_form;
+}

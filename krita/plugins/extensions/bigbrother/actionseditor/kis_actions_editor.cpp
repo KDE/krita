ee0914d381d8e4ca18e49e9fd8436adfb845f8f3
Cyrille Berger <cyb@lepi.org>
2009-04-19 19:39:07 +0000

    use the action editor in the actions editor
    svn path=/trunk/koffice/; revision=956297

--- krita/plugins/extensions/bigbrother/actionseditor/kis_actions_editor.cpp
+++ krita/plugins/extensions/bigbrother/actionseditor/kis_actions_editor.cpp
@@ -22,10 +22,15 @@
 #include "ui_wdgactionseditor.h"
 
 #include "kis_macro_model.h"
+#include <recorder/kis_recorded_action.h>
+#include <recorder/kis_macro.h>
 
-KisActionsEditor::KisActionsEditor(QWidget* parent) : QWidget(parent), m_form(new Ui::ActionsEditor), m_macro(0), m_model(0)
+KisActionsEditor::KisActionsEditor(QWidget* parent) : QWidget(parent), m_currentEditor(0), m_form(new Ui::ActionsEditor), m_macro(0), m_model(0), m_widgetLayout(0)
+
 {
     m_form->setupUi(this);
+    
+    // Setup buttons
     m_form->bnAdd->setIcon(SmallIcon("list-add"));
 
     m_form->bnDelete->setIcon(SmallIcon("list-remove"));
@@ -37,6 +42,12 @@ KisActionsEditor::KisActionsEditor(QWidget* parent) : QWidget(parent), m_form(ne
     m_form->bnLower->setIcon(SmallIcon("go-down"));
 
     m_form->bnDuplicate->setIcon(SmallIcon("edit-copy"));
+
+    // Setup actions list
+    connect(m_form->actionsList, SIGNAL(clicked(const QModelIndex&)), SLOT(slotActionActivated(const QModelIndex&)));
+
+    // Editor
+    m_widgetLayout = new QGridLayout(m_form->editorWidget);
 }
 
 KisActionsEditor::~KisActionsEditor()
@@ -52,3 +63,28 @@ void KisActionsEditor::setMacro(KisMacro* _macro)
     m_form->actionsList->setModel(m_model);
     delete oldModel;
 }
+
+void KisActionsEditor::slotActionActivated(const QModelIndex& item)
+{
+    if( item.isValid() && m_macro )
+    {
+        setCurrentAction( m_macro->actions()[item.row()] );
+        
+    }
+}
+
+void KisActionsEditor::setCurrentAction(KisRecordedAction* _action)
+{
+    delete m_currentEditor;
+    m_currentEditor = 0;
+    if(_action) {
+        m_currentEditor = _action->createEditor(this);
+    }
+    if(!m_currentEditor)
+    {
+        m_currentEditor = new QLabel(i18n("No editor"), this);
+    }
+    m_widgetLayout->addWidget(m_currentEditor, 0 , 0);
+}
+
+#include "kis_actions_editor.moc"

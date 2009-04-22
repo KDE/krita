30fb19899131c6a79f319d7945df8fb9b6840817
Cyrille Berger <cyb@lepi.org>
2009-04-22 21:02:47 +0000

    add KisNodeQueryPath, some sort of XQuery-like stuff to represent a generic index to access a node in the tree
    svn path=/trunk/koffice/; revision=957780

---
+++ krita/image/recorder/kis_node_query_path.cc
@@ -0,0 +1,179 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_node_query_path.h"
+
+#include <QStringList>
+#include <kis_node.h>
+
+struct PathElement {
+    enum Type {
+        Wildcard,
+        Parent,
+        Index
+    };
+    PathElement(Type _type) : type(_type) { Q_ASSERT(type == Wildcard || type == Parent); }
+    PathElement(int _i) : type(Index), index(_i) {}
+    Type type;
+    unsigned int index;
+};
+
+struct KisNodeQueryPath::Private {
+    QList<PathElement> elements;
+    /// This function will remove uneeded call to parent, for instance, "1/../3/../5" => "5"
+    void simplifyPath()
+    {
+        // No elements then return
+        if( elements.isEmpty() ) return;
+        QList<PathElement> newelements;
+        int i = 0;
+        for( ; i < elements.count() && elements[i].type == PathElement::Parent; ++i)
+        {
+            newelements.push_back(PathElement::Parent);
+        }
+        // Loop ofver the element of the list
+        for( ; i < elements.count(); ++i)
+        {
+            PathElement pe = elements[i];
+            // If it's the last element, or the next element isn't a parent
+            if( pe.type != PathElement::Parent )
+            {
+                newelements.push_back(pe);
+            } else {
+                if( newelements.isEmpty() || newelements.last().type == PathElement::Parent )
+                {
+                    newelements.push_back(PathElement::Parent);
+                } else {
+                    newelements.removeLast();
+                }
+            }
+        }
+        // Set the new list
+        elements = newelements;
+    }
+    void queryLevel(int _level, KisNodeSP _node, QList<KisNodeSP>& _result)
+    {
+        if( _level < elements.size() )
+        {
+            _result.push_back(_node);
+        } else {
+            PathElement pe = elements[_level];
+            
+            switch(pe.type)
+            {
+                case PathElement::Wildcard:
+                {
+                    for( KisNodeSP child = _node->firstChild();
+                         child != 0; child = child->nextSibling() )
+                    {
+                        queryLevel(_level + 1, child, _result);
+                    }
+                }
+                    break;
+                case PathElement::Parent:
+                {
+                    if( _node->parent() )
+                    {
+                        queryLevel(_level + 1, _node->parent(), _result);
+                    } else {
+                        kWarning() << "No parent";
+                    }
+                    break;
+                }
+                case PathElement::Index:
+                {
+                    if( pe.index < _node->childCount() )
+                    {
+                        queryLevel( _level + 1, _node->at(pe.index), _result);
+                    } else {
+                        kWarning() << "No parent";
+                    }
+                    break;
+                }
+            }
+        }
+    }
+};
+
+KisNodeQueryPath::KisNodeQueryPath() : d(new Private)
+{
+}
+
+KisNodeQueryPath::~KisNodeQueryPath()
+{
+}
+
+KisNodeQueryPath::KisNodeQueryPath(const KisNodeQueryPath& nqp) : d(new Private(*nqp.d))
+{
+}
+
+KisNodeQueryPath& KisNodeQueryPath::operator=(const KisNodeQueryPath& nqp)
+{
+    *d = *nqp.d;
+}
+
+QList<KisNodeSP> KisNodeQueryPath::queryNodes(KisNodeSP _node)
+{
+    QList<KisNodeSP> result;
+    
+    d->queryLevel( 0, _node, result);
+    
+    return result;
+}
+
+QString KisNodeQueryPath::toString() const
+{
+    QString str = "";
+    for(int i = 0; i < d->elements.count(); ++i)
+    {
+        PathElement pe = d->elements[i];
+        switch(pe.type)
+        {
+            case PathElement::Wildcard:
+                str += "*";
+                break;
+            case PathElement::Parent:
+                str += "..";
+                break;
+            case PathElement::Index:
+                str += QString::number(pe.index);
+                break;
+        }
+    }
+    return str;
+}
+
+KisNodeQueryPath KisNodeQueryPath::fromString(const QString& _path)
+{
+    KisNodeQueryPath path;
+    QStringList indexes = _path.split("\\");
+    foreach(const QString& index, indexes)
+    {
+        if(index == "*")
+        {
+            path.d->elements.push_back(PathElement::Wildcard);
+        } else if( index == ".." )
+        {
+            path.d->elements.push_back(PathElement::Parent);
+        } else {
+            path.d->elements.push_back(index.toInt());
+        }
+    }
+    path.d->simplifyPath();
+    return path;
+}
---
+++ krita/image/recorder/kis_node_query_path.h
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_NODE_QUERY_PATH_H_
+#define _KIS_NODE_QUERY_PATH_H_
+
+#include <kis_types.h>
+
+/**
+ * This class represent a path to access a node starting from an other node.
+ */
+class KisNodeQueryPath {
+    KisNodeQueryPath();
+public:
+    ~KisNodeQueryPath();
+    KisNodeQueryPath(const KisNodeQueryPath& );
+    KisNodeQueryPath& operator=(const KisNodeQueryPath& );
+    QList<KisNodeSP> queryNodes(KisNodeSP);
+    /**
+     * This function return a string representing this path. Which is a list seperated by '\' of:
+     * - '*': represents all layers
+     * - '..': represents the parent layer
+     * - number: index of the layer
+     *
+     * For instance: "1/*" return all children of the first layer, "../3" return the third layer of the parent
+     * of the current layer
+     */
+    QString toString() const;
+    /**
+     * @param path
+     * @param err if non null, it will be filled with an error message
+     * @see toString for an explanation of the string format
+     */
+    static KisNodeQueryPath fromString(const QString& path);
+private:
+    struct Private;
+    Private* const d;
+};
+
+#endif

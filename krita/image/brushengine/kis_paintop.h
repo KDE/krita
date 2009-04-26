bf7a5b93c019b46ac18bada36201c7d776c17f03
Cyrille Berger <cyb@lepi.org>
2009-04-26 16:41:27 +0000

    add interface to allow paintop to paint outlines
    svn path=/trunk/koffice/; revision=959599

--- krita/image/brushengine/kis_paintop.h
+++ krita/image/brushengine/kis_paintop.h
@@ -35,6 +35,7 @@ class QPointF;
 class KoColorSpace;
 class KoInputDevice;
 class KoPointerEvent;
+class KoViewConverter;
 
 class KisPainter;
 class KisPaintInformation;
@@ -102,6 +103,18 @@ public:
     virtual bool incremental() const {
         return false;
     }
+    /**
+     * This function allow the paintop to draw an outline at a given position.
+     */
+    virtual void paintOutline(const QPointF& pos, QPainter &painter, const KoViewConverter &converter);
+    /**
+     * This function allow the paintop to draw an outline a long a line.
+     */
+    virtual void paintOutline(const QLineF& pos, QPainter &painter, const KoViewConverter &converter);
+    /**
+     * This function allow the paintop to draw an outline a long a curve.
+     */
+    virtual void paintOutline(const QPolygon& pos, QPainter &painter, const KoViewConverter &converter);
 
 protected:
 

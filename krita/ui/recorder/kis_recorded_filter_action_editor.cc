ccb38e448c01e96c65eebca2724eeb1b8674257a
Cyrille Berger <cyb@lepi.org>
2009-05-03 10:17:12 +0000

    add a pluggable editor factory for actions
    svn path=/trunk/koffice/; revision=962765

---
+++ krita/ui/recorder/kis_recorded_filter_action_editor.cc
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
+#include "kis_recorded_filter_action_editor.h"
+#include <recorder/kis_recorded_filter_action.h>
+#include <filter/kis_filter.h>
+#include <qgridlayout.h>
+#include <kis_config_widget.h>
+#include <filter/kis_filter_configuration.h>
+#include <QLabel>
+#include <kis_paint_device.h>
+
+KisRecordedFilterActionEditor::KisRecordedFilterActionEditor( QWidget* parent, KisRecordedAction* action) :
+        m_action(dynamic_cast<KisRecordedFilterAction*>(action)),
+        m_gridLayout(new QGridLayout(this))
+{
+    Q_ASSERT(m_action);
+    m_configWidget = m_action->filter()->createConfigurationWidget(this, 0 ,0);
+    if(m_configWidget)
+    {
+        m_gridLayout->addWidget(m_configWidget);
+        connect(m_configWidget, SIGNAL(sigConfigChanged()), SLOT(configurationUpdated()));
+        m_configWidget->setConfiguration(m_action->filterConfiguration());
+    } else {
+        m_gridLayout->addWidget(new QLabel("No configuration option.", this ));
+    }
+}
+
+KisRecordedFilterActionEditor::~KisRecordedFilterActionEditor()
+{
+}
+
+void KisRecordedFilterActionEditor::configurationUpdated()
+{
+    KisFilterConfiguration* config = dynamic_cast<KisFilterConfiguration*>( m_configWidget->configuration() );
+    if(config)
+    {
+        m_action->setFilterConfiguration(config);
+        emit(actionEdited());
+    }
+}
+
+KisRecordedFilterActionEditorFactory::KisRecordedFilterActionEditorFactory()
+{
+}
+
+KisRecordedFilterActionEditorFactory::~KisRecordedFilterActionEditorFactory()
+{
+}
+
+QWidget* KisRecordedFilterActionEditorFactory::createEditor(QWidget* parent, KisRecordedAction* action) const
+{
+    return new KisRecordedFilterActionEditor(parent, action);
+}
+
+bool KisRecordedFilterActionEditorFactory::canEdit( const KisRecordedAction* action) const
+{
+    return action->id() == "FilterAction";
+}
+
---
+++ krita/ui/recorder/kis_recorded_filter_action_editor.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_RECORDED_FILTER_ACTION_EDITOR_H_
+#define _KIS_RECORDED_FILTER_ACTION_EDITOR_H_
+
+#include <QWidget>
+#include "kis_recorded_action_editor_factory.h"
+
+class KisRecordedFilterAction;
+class QGridLayout;
+class KisConfigWidget;
+
+class KisRecordedFilterActionEditor : public QWidget {
+    Q_OBJECT
+    public:
+        KisRecordedFilterActionEditor( QWidget* parent, KisRecordedAction* action);
+        ~KisRecordedFilterActionEditor();
+    private slots:
+        void configurationUpdated();
+    signals:
+        void actionEdited();
+    private:
+        KisRecordedFilterAction* m_action;
+        QGridLayout* m_gridLayout;
+        KisConfigWidget* m_configWidget;
+};
+
+class KisRecordedFilterActionEditorFactory : public KisRecordedActionEditorFactory {
+    public:
+        KisRecordedFilterActionEditorFactory();
+        virtual ~KisRecordedFilterActionEditorFactory();
+        virtual QWidget* createEditor(QWidget* parent, KisRecordedAction* action) const;
+        virtual bool canEdit( const KisRecordedAction* action) const;
+};
+
+#endif

3b5d76b4a428ba27fec6be821c08290a980ec361
Thomas Zander <zander@kde.org>
2009-05-08 08:00:22 +0000

    fix initializing and undo of segment change type command added new convenience ctor for only changing one segment added some simple tests for the fixed cases
    Written by; jaham
    Reviewed by; Thorsten
    Committed by; Thomas
    
    svn path=/branches/koffice/2.0/koffice/; revision=965137

--- libs/flake/commands/KoPathSegmentTypeCommand.cpp
+++ libs/flake/commands/KoPathSegmentTypeCommand.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
- * Copyright (C) 2006 Jan Hambrecht <jaham@gmx.net>
+ * Copyright (C) 2006,2009 Jan Hambrecht <jaham@gmx.net>
  * Copyright (C) 2006,2007 Thorsten Zachmann <zachmann@kde.org>
  *
  * This library is free software; you can redistribute it and/or
@@ -21,43 +21,21 @@
 #include "KoPathSegmentTypeCommand.h"
 #include <klocale.h>
 
+KoPathSegmentTypeCommand::KoPathSegmentTypeCommand(const KoPathPointData & pointData, SegmentType segmentType, QUndoCommand *parent)
+: QUndoCommand(parent)
+, m_segmentType(segmentType)
+{
+    QList<KoPathPointData> pointDataList;
+    pointDataList.append(pointData);
+    initialize(pointDataList);
+}
+
 KoPathSegmentTypeCommand::KoPathSegmentTypeCommand(const QList<KoPathPointData> & pointDataList, SegmentType segmentType,
         QUndoCommand *parent)
         : QUndoCommand(parent)
         , m_segmentType(segmentType)
 {
-    QList<KoPathPointData>::const_iterator it(pointDataList.begin());
-    for (; it != pointDataList.end(); ++it) {
-        KoPathSegment segment = it->pathShape->segmentByIndex(it->pointIndex);
-        if (segment.isValid()) {
-            if (m_segmentType == Curve) {
-                if (segment.first()->activeControlPoint2() || segment.second()->activeControlPoint1())
-                    continue;
-            } else {
-                if (! segment.first()->activeControlPoint2() && ! segment.second()->activeControlPoint1())
-                    continue;
-            }
-
-            m_pointDataList.append(*it);
-            SegmentTypeData segmentData;
-
-            KoPathShape * pathShape = segment.first()->parent();
-
-            if (m_segmentType == Curve) {
-                segmentData.m_controlPoint2 = pathShape->shapeToDocument(segment.first()->controlPoint2());
-                segmentData.m_controlPoint1 = pathShape->shapeToDocument(segment.second()->controlPoint1());
-            }
-            segmentData.m_properties2 = segment.first()->properties();
-            segmentData.m_properties1 = segment.second()->properties();
-            m_segmentData.append(segmentData);
-        }
-    }
-
-    if (m_segmentType == Curve) {
-        setText(i18n("Change segments to curves"));
-    } else {
-        setText(i18n("Change segments to lines"));
-    }
+    initialize(pointDataList);
 }
 
 KoPathSegmentTypeCommand::~KoPathSegmentTypeCommand()
@@ -75,10 +53,12 @@ void KoPathSegmentTypeCommand::redo()
         KoPathSegment segment = pathShape->segmentByIndex(it->pointIndex);
 
         if (m_segmentType == Curve) {
+            // we change type to curve -> set control point positions
             QPointF pointDiff = segment.second()->point() - segment.first()->point();
             segment.first()->setControlPoint2(segment.first()->point() + pointDiff / 3.0);
             segment.second()->setControlPoint1(segment.first()->point() + pointDiff * 2.0 / 3.0);
         } else {
+            // we are changing type to line -> remove control points
             segment.first()->removeControlPoint2();
             segment.second()->removeControlPoint1();
         }
@@ -96,10 +76,17 @@ void KoPathSegmentTypeCommand::undo()
         pd.pathShape->update();
         KoPathSegment segment = pd.pathShape->segmentByIndex(pd.pointIndex);
         const SegmentTypeData segmentData(m_segmentData.at(i));
-        if (m_segmentType == Curve) {
+
+        if (m_segmentType == Line) {
+            // change type back to curve -> reactivate control points and their positions
             segment.first()->setControlPoint2(pd.pathShape->documentToShape(segmentData.m_controlPoint2));
             segment.second()->setControlPoint1(pd.pathShape->documentToShape(segmentData.m_controlPoint1));
+        } else {
+            // change back to line -> remove control points
+            segment.first()->removeControlPoint2();
+            segment.second()->removeControlPoint1();
         }
+        
         segment.first()->setProperties(segmentData.m_properties2);
         segment.second()->setProperties(segmentData.m_properties1);
 
@@ -108,3 +95,42 @@ void KoPathSegmentTypeCommand::undo()
     }
 }
 
+void KoPathSegmentTypeCommand::initialize(const QList<KoPathPointData> & pointDataList)
+{
+    QList<KoPathPointData>::const_iterator it(pointDataList.begin());
+    for (; it != pointDataList.end(); ++it) {
+        KoPathSegment segment = it->pathShape->segmentByIndex(it->pointIndex);
+        if (segment.isValid()) {
+            if (m_segmentType == Curve) {
+                // don not change segment if already a curve
+                if (segment.first()->activeControlPoint2() || segment.second()->activeControlPoint1())
+                    continue;
+            } else {
+                // do not change segment if already a line
+                if (! segment.first()->activeControlPoint2() && ! segment.second()->activeControlPoint1())
+                    continue;
+            }
+            
+            m_pointDataList.append(*it);
+            SegmentTypeData segmentData;
+            
+            KoPathShape * pathShape = segment.first()->parent();
+            
+            // we are changing a curve to a line -> save control point positions
+            if (m_segmentType == Line) {
+                segmentData.m_controlPoint2 = pathShape->shapeToDocument(segment.first()->controlPoint2());
+                segmentData.m_controlPoint1 = pathShape->shapeToDocument(segment.second()->controlPoint1());
+            }
+            // save point properties
+            segmentData.m_properties2 = segment.first()->properties();
+            segmentData.m_properties1 = segment.second()->properties();
+            m_segmentData.append(segmentData);
+        }
+    }
+    
+    if (m_segmentType == Curve) {
+        setText(i18n("Change segments to curves"));
+    } else {
+        setText(i18n("Change segments to lines"));
+    }
+}

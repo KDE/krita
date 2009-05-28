20498fc521a7e1cb922cfb77de2954224fb4852d
Boudewijn Rempt <boud@valdyas.org>
2009-05-28 21:09:29 +0000

    Add a "fixed size" paint device
    For things like dabs, we always know the extent of the paint device,
    and they don't grow either. Create a fixed size paint device that
    simply knows its bounds, its pixelsize and has an array of bytes
    representing the pixels. This will be used in an optimized version
    of KisPainter and in the brush engines.
    
    svn path=/trunk/koffice/; revision=974278

---
+++ krita/image/kis_fixed_paint_device.cpp
@@ -0,0 +1,196 @@
+/*
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include "kis_fixed_paint_device.h"
+
+#include <KoColorSpaceRegistry.h>
+#include <KoColor.h>
+
+KisFixedPaintDevice::KisFixedPaintDevice(const KoColorSpace* colorSpace)
+        : m_colorSpace(colorSpace)
+{
+}
+
+
+KisFixedPaintDevice::~KisFixedPaintDevice()
+{
+}
+
+KisFixedPaintDevice::KisFixedPaintDevice(const KisFixedPaintDevice& rhs)
+    : KisShared(rhs)
+{
+    m_bounds = rhs.m_bounds;
+    m_colorSpace = rhs.m_colorSpace;
+    m_data = rhs.m_data;
+}
+
+void KisFixedPaintDevice::setRect(const QRect& rc)
+{
+    m_bounds = rc;
+}
+
+
+QRect KisFixedPaintDevice::bounds() const
+{
+    return m_bounds;
+}
+
+quint32 KisFixedPaintDevice::pixelSize() const
+{
+    return m_colorSpace->pixelSize();
+}
+
+bool KisFixedPaintDevice::initialize( quint8 defaultValue )
+{
+    m_data.fill(defaultValue, m_bounds.height() * m_bounds.width() * pixelSize());
+
+    return true;
+}
+
+quint8* KisFixedPaintDevice::data()
+{
+    return m_data.data();
+}
+
+quint8* KisFixedPaintDevice::data() const
+{
+    return const_cast<quint8*>(m_data.data());
+}
+
+void KisFixedPaintDevice::convertTo(const KoColorSpace* dstColorSpace, KoColorConversionTransformation::Intent renderingIntent)
+{
+    if (*m_colorSpace == *dstColorSpace) {
+        return;
+    }
+    quint32 size = m_bounds.width() * m_bounds.height();
+    QVector<quint8> dstData(size * dstColorSpace->pixelSize());
+
+    m_colorSpace->convertPixelsTo(data(), dstData.data(),
+                                  dstColorSpace,
+                                  size,
+                                  renderingIntent);
+
+    m_colorSpace = dstColorSpace;
+    m_data = dstData;
+
+}
+
+void KisFixedPaintDevice::convertFromQImage(const QImage& image, const QString &srcProfileName)
+{
+    QImage img = image;
+
+    if (img.format() != QImage::Format_ARGB32) {
+        img = img.convertToFormat(QImage::Format_ARGB32);
+    }
+    setRect(img.rect());
+    initialize();
+
+    // Don't convert if not no profile is given and both paint dev and qimage are rgba.
+    if (srcProfileName.isEmpty() && colorSpace()->id() == "RGBA") {
+        memcpy(data(), img.bits(), img.width() * img.height() * pixelSize());
+    } else {
+        KoColorSpaceRegistry::instance()
+            ->colorSpace("RGBA", srcProfileName)
+            ->convertPixelsTo(img.bits(), data(), colorSpace(), img.width() * img.height());
+    }
+}
+
+QImage KisFixedPaintDevice::convertToQImage(const KoColorProfile *  dstProfile)
+{
+    qint32 x1;
+    qint32 y1;
+    qint32 w;
+    qint32 h;
+
+    x1 = m_bounds.x();
+    y1 = m_bounds.y();
+    w = m_bounds.width();
+    h = m_bounds.height();
+
+    return convertToQImage(dstProfile, x1, y1, w, h);
+}
+
+QImage KisFixedPaintDevice::convertToQImage(const KoColorProfile *  dstProfile, qint32 x1, qint32 y1, qint32 w, qint32 h)
+{
+
+    if (w < 0)
+        return QImage();
+
+    if (h < 0)
+        return QImage();
+
+    if (QRect(x1, y1, w, h) == m_bounds) {
+        return colorSpace()->convertToQImage(data(), w, h, dstProfile,
+                                             KoColorConversionTransformation::IntentPerceptual);
+    }
+    else {
+        quint8* newData = new quint8[w * h * pixelSize()];
+        quint8* srcPtr = data();
+        quint8* dstPtr = newData;
+        int pSize = pixelSize();
+        // copy the right area out of the paint device into data
+        for (int row = y1; row < h; row++) {
+                memcpy(dstPtr, srcPtr, w * pSize);
+                srcPtr += (row * w * pSize) + (y1 * pSize);
+                dstPtr += w * pSize;
+        }
+        QImage image = colorSpace()->convertToQImage(newData, w, h, dstProfile,
+                                                     KoColorConversionTransformation::IntentPerceptual);
+        return image;
+    }
+}
+
+
+void KisFixedPaintDevice::clear(const QRect & rc)
+{
+    KoColor c(Qt::black, m_colorSpace);
+    quint8* black = m_colorSpace->allocPixelBuffer(1);
+    memcpy(black, c.data(), m_colorSpace->pixelSize());
+    m_colorSpace->setAlpha(black, OPACITY_TRANSPARENT, 1);
+    fill(rc.x(), rc.y(), rc.width(), rc.height(), black);
+    delete[] black;
+}
+
+void KisFixedPaintDevice::fill(qint32 x, qint32 y, qint32 w, qint32 h, const quint8 *fillPixel)
+{
+    if (m_data.isEmpty() || m_bounds.isEmpty()) {
+        setRect(QRect(x, y, w, h));
+        initialize();
+    }
+
+    QRect rc(x, y, w, h);
+    if (!m_bounds.contains(rc)) {
+        rc = m_bounds;
+    }
+
+    quint8 pixelSize = m_colorSpace->pixelSize();
+    quint8* dabPointer = data();
+
+    if (rc.contains(m_bounds)) {
+        for (int i = 0; i < w * h ; ++i) {
+            memcpy(dabPointer, fillPixel, pixelSize);
+            dabPointer += pixelSize;
+        }
+    }
+    else {
+        int y1 = y;
+        for (int row = y1; row < h; row++) {
+            memcpy(dabPointer, fillPixel, w * pixelSize);
+            dabPointer += (row * w * pixelSize) + (y1 * pixelSize);
+        }
+    }
+}
---
+++ krita/image/kis_fixed_paint_device.h
@@ -0,0 +1,147 @@
+/*
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#ifndef KIS_FIXED_PAINT_DEVICE_H
+#define KIS_FIXED_PAINT_DEVICE_H
+
+#include <krita_export.h>
+#include <KoColorSpace.h>
+#include "kis_global.h"
+#include "kis_shared.h"
+#include <kis_shared_ptr.h>
+
+#include <QRect>
+#include <QImage>
+#include <QVector>
+
+/**
+ * A fixed paint device is a simple paint device that consists of an array
+ * of bytes and a rectangle. It cannot grow, it cannot shrink, all you can
+ * do is fill the paint device with the right bytes and use it as an argument
+ * to KisPainter or use the bytes as an argument to KoColorSpace functions.
+ */
+class KRITAIMAGE_EXPORT KisFixedPaintDevice : public KisShared {
+
+public:
+
+    KisFixedPaintDevice( const KoColorSpace* colorSpace );
+    virtual ~KisFixedPaintDevice();
+
+    /**
+     * Deep copy the the fixed paint device, including the data.
+     */
+    KisFixedPaintDevice(const KisFixedPaintDevice& rhs);
+
+    /**
+     * setRect sets the rect of the fixed paint device to rect.
+     * This will _not_ create the associated data area.
+     *
+     * @rect the bounds in pixels. The x,y of the rect represent the origin
+     * of the fixed paint device.
+     */
+    void setRect(const QRect& rc);
+
+    /**
+     * @return the rect that the data represents
+     */
+    QRect bounds() const;
+
+    /**
+     * @return the pixelSize associated with this fixed paint device.
+     */
+    quint32 pixelSize() const;
+
+    const KoColorSpace* colorSpace() const { return m_colorSpace; }
+
+    /**
+     * initializes the paint device.
+     *
+     * @param defaultValue the default byte with which all pixels will be filled.
+     * @return false if the allocation failed.
+     */
+    bool initialize( quint8 defaultValue = 0 );
+
+    /**
+     * @return a pointer to the beginning of the data associated with this fixed paint device.
+     */
+    quint8* data();
+
+    quint8* data() const;
+
+    /**
+     *   Converts the paint device to a different colorspace
+     */
+    void convertTo(const KoColorSpace * dstColorSpace, KoColorConversionTransformation::Intent renderingIntent = KoColorConversionTransformation::IntentPerceptual);
+
+    /**
+     * Fill this paint device with the data from img
+     *
+     * @param srcProfileName name of the RGB profile to interpret the img as. "" is interpreted as sRGB
+     */
+    virtual void convertFromQImage(const QImage& img, const QString &srcProfileName);
+
+    /**
+     * Create an RGBA QImage from a rectangle in the paint device.
+     *
+     * @param x Left coordinate of the rectangle
+     * @param y Top coordinate of the rectangle
+     * @param w Width of the rectangle in pixels
+     * @param h Height of the rectangle in pixels
+     * @param dstProfile RGB profile to use in conversion. May be 0, in which
+     * case it's up to the color strategy to choose a profile (most
+     * like sRGB).
+     */
+    virtual QImage convertToQImage(const KoColorProfile *  dstProfile, qint32 x, qint32 y, qint32 w, qint32 h);
+
+    /**
+     * Create an RGBA QImage from a rectangle in the paint device. The
+     * rectangle is defined by the parent image's bounds.
+     *
+     * @param dstProfile RGB profile to use in conversion. May be 0, in which
+     * case it's up to the color strategy to choose a profile (most
+     * like sRGB).
+     */
+    virtual QImage convertToQImage(const KoColorProfile *  dstProfile = 0);
+
+    /**
+     * Clear the given rectangle to transparent black.
+     *
+     * XXX: this will not (yet) expand the paint device to contain the specified rect
+     * but if the paintdevice has not been initialized, it will be.
+     */
+    void clear(const QRect & rc);
+
+    /**
+     * Fill the given rectangle with the given pixel. This does not take the
+     * selection into account.
+     *
+     * XXX: this will not (yet) expand the paint device to contain the specified rect
+     * but if the paintdevice has not been initialized, it will be.
+     */
+    void fill(qint32 x, qint32 y, qint32 w, qint32 h, const quint8 *fillPixel);
+
+private:
+
+    KisFixedPaintDevice& operator=(const KisFixedPaintDevice& rhs);
+
+    const KoColorSpace* m_colorSpace;
+    QRect m_bounds;
+    QVector<quint8> m_data;
+
+};
+
+#endif

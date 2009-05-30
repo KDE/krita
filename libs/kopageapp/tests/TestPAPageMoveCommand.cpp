38a8790a9fa1032afd5ffc5261741913368b9bd5
Fredy Yanardi <fyanardi@gmail.com>
2009-05-30 06:29:35 +0000

    Added few more testcases to test moving multiple pages
    svn path=/trunk/koffice/; revision=975413

--- libs/kopageapp/tests/TestPAPageMoveCommand.cpp
+++ libs/kopageapp/tests/TestPAPageMoveCommand.cpp
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright ( C ) 2007 Thorsten Zachmann <zachmann@kde.org>
+ * Copyright (C) 2009 Fredy Yanardi <fyanardi@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -21,6 +22,8 @@
 
 #include "KoPAPageMoveCommand.h"
 #include "KoPAMasterPage.h"
+
+#include <KDebug>
 
 #include <qtest_kde.h>
 
@@ -91,6 +94,42 @@ void TestPAPageMoveCommand::redoUndoAfter()
     cmd3.redo();
     checkOrder( pages3 );
 
+    // Moving multiple consecutive pages
+    QList<KoPAPageBase *> multiPages1;
+    multiPages1.insert( 0, m_pages[3] );
+    multiPages1.insert( 1, m_pages[4] );
+    QList<KoPAMasterPage *> pages4;
+    pages4.insert( 0, m_pages[0] );
+    pages4.insert( 1, m_pages[3] );
+    pages4.insert( 2, m_pages[4] );
+    pages4.insert( 3, m_pages[2] );
+    pages4.insert( 4, m_pages[1] );
+
+    KoPAPageMoveCommand cmd4( &m_doc, multiPages1, m_pages[0] );
+    cmd4.redo();
+    checkOrder( pages4 );
+
+    // Moving multiple non-consecutive pages
+    QList<KoPAPageBase *> multiPages2;
+    multiPages2.insert( 0, m_pages[4] );
+    multiPages2.insert( 1, m_pages[1] );
+    QList<KoPAMasterPage *> pages5;
+    pages5.insert( 0, m_pages[0] );
+    pages5.insert( 1, m_pages[4] );
+    pages5.insert( 2, m_pages[1] );
+    pages5.insert( 3, m_pages[3] );
+    pages5.insert( 4, m_pages[2] );
+
+    KoPAPageMoveCommand cmd5( &m_doc, multiPages2, m_pages[0] );
+    cmd5.redo();
+    checkOrder( pages5 );
+
+    cmd5.undo();
+    checkOrder( pages4 );
+
+    cmd4.undo();
+    checkOrder( pages3 );
+
     cmd3.undo();
     checkOrder( pages2 );
 
@@ -139,6 +178,42 @@ void TestPAPageMoveCommand::redoUndoBefore()
     cmd3.redo();
     checkOrder( pages3 );
 
+    // Moving multiple consecutive pages
+    QList<KoPAPageBase *> multiPages1;
+    multiPages1.insert( 0, m_pages[4] );
+    multiPages1.insert( 1, m_pages[2] );
+    QList<KoPAMasterPage *> pages4;
+    pages4.insert( 0, m_pages[1] );
+    pages4.insert( 1, m_pages[0] );
+    pages4.insert( 2, m_pages[4] );
+    pages4.insert( 3, m_pages[2] );
+    pages4.insert( 4, m_pages[3] );
+
+    KoPAPageMoveCommand cmd4( &m_doc, multiPages1, m_pages[0] );
+    cmd4.redo();
+    checkOrder( pages4 );
+
+    // Moving multiple non-consecutive pages
+    QList<KoPAPageBase *> multiPages2;
+    multiPages2.insert( 0, m_pages[1] );
+    multiPages2.insert( 1, m_pages[4] );
+    QList<KoPAMasterPage *> pages5;
+    pages5.insert( 0, m_pages[0] );
+    pages5.insert( 1, m_pages[2] );
+    pages5.insert( 2, m_pages[3] );
+    pages5.insert( 3, m_pages[1] );
+    pages5.insert( 4, m_pages[4] );
+
+    KoPAPageMoveCommand cmd5( &m_doc, multiPages2, m_pages[3] );
+    cmd5.redo();
+    checkOrder( pages5 );
+
+    cmd5.undo();
+    checkOrder( pages4 );
+
+    cmd4.undo();
+    checkOrder( pages3 );
+
     cmd3.undo();
     checkOrder( pages2 );
 
@@ -146,7 +221,6 @@ void TestPAPageMoveCommand::redoUndoBefore()
     checkOrder( pages1 );
 
     cmd1.undo();
-    checkOrder( m_pages );
 }
 
 void TestPAPageMoveCommand::redoUndoStart()
@@ -185,6 +259,42 @@ void TestPAPageMoveCommand::redoUndoStart()
     pages3.insert( 4, m_pages[3] );
 
     cmd3.redo();
+    checkOrder( pages3 );
+
+    // Moving multiple consecutive pages
+    QList<KoPAPageBase *> multiPages1;
+    multiPages1.insert( 0, m_pages[1] );
+    multiPages1.insert( 1, m_pages[0] );
+    QList<KoPAMasterPage *> pages4;
+    pages4.insert( 0, m_pages[1] );
+    pages4.insert( 1, m_pages[0] );
+    pages4.insert( 2, m_pages[4] );
+    pages4.insert( 3, m_pages[2] );
+    pages4.insert( 4, m_pages[3] );
+
+    KoPAPageMoveCommand cmd4( &m_doc, multiPages1, 0 );
+    cmd4.redo();
+    checkOrder( pages4 );
+
+    // Moving multiple non-consecutive pages
+    QList<KoPAPageBase *> multiPages2;
+    multiPages2.insert( 0, m_pages[0] );
+    multiPages2.insert( 1, m_pages[2] );
+    QList<KoPAMasterPage *> pages5;
+    pages5.insert( 0, m_pages[0] );
+    pages5.insert( 1, m_pages[2] );
+    pages5.insert( 2, m_pages[1] );
+    pages5.insert( 3, m_pages[4] );
+    pages5.insert( 4, m_pages[3] );
+
+    KoPAPageMoveCommand cmd5( &m_doc, multiPages2, 0 );
+    cmd5.redo();
+    checkOrder( pages5 );
+
+    cmd5.undo();
+    checkOrder( pages4 );
+
+    cmd4.undo();
     checkOrder( pages3 );
 
     cmd3.undo();
@@ -235,6 +345,42 @@ void TestPAPageMoveCommand::redoUndoEnd()
     cmd3.redo();
     checkOrder( pages3 );
 
+    // Moving multiple consecutive pages
+    QList<KoPAPageBase *> multiPages1;
+    multiPages1.insert( 0, m_pages[2] );
+    multiPages1.insert( 1, m_pages[4] );
+    QList<KoPAMasterPage *> pages4;
+    pages4.insert( 0, m_pages[1] );
+    pages4.insert( 1, m_pages[3] );
+    pages4.insert( 2, m_pages[0] );
+    pages4.insert( 3, m_pages[2] );
+    pages4.insert( 4, m_pages[4] );
+
+    KoPAPageMoveCommand cmd4( &m_doc, multiPages1, m_pages[0] );
+    cmd4.redo();
+    checkOrder( pages4 );
+
+    // Moving multiple non-consecutive pages
+    QList<KoPAPageBase *> multiPages2;
+    multiPages2.insert( 0, m_pages[1] );
+    multiPages2.insert( 1, m_pages[0] );
+    QList<KoPAMasterPage *> pages5;
+    pages5.insert( 0, m_pages[3] );
+    pages5.insert( 1, m_pages[2] );
+    pages5.insert( 2, m_pages[4] );
+    pages5.insert( 3, m_pages[1] );
+    pages5.insert( 4, m_pages[0] );
+
+    KoPAPageMoveCommand cmd5( &m_doc, multiPages2, m_pages[4] );
+    cmd5.redo();
+    checkOrder( pages5 );
+
+    cmd5.undo();
+    checkOrder( pages4 );
+
+    cmd4.undo();
+    checkOrder( pages3 );
+
     cmd3.undo();
     checkOrder( pages2 );
 
@@ -242,6 +388,28 @@ void TestPAPageMoveCommand::redoUndoEnd()
     checkOrder( pages1 );
 
     cmd1.undo();
+    checkOrder( m_pages );
+}
+
+// Moving multiple pages, where those pages are split into two sets, one set contains pages before "after"
+// and the other set contains pages after "after"
+void TestPAPageMoveCommand::redoUndoAfterInBetween()
+{
+    QList<KoPAPageBase *> multiPages;
+    multiPages.insert( 0, m_pages[1] );
+    multiPages.insert( 1, m_pages[3] );
+    QList<KoPAMasterPage *> pages1;
+    pages1.insert( 0, m_pages[0] );
+    pages1.insert( 1, m_pages[2] );
+    pages1.insert( 2, m_pages[1] );
+    pages1.insert( 3, m_pages[3] );
+    pages1.insert( 4, m_pages[4] );
+
+    KoPAPageMoveCommand cmd( &m_doc, multiPages, m_pages[2] );
+    cmd.redo();
+    checkOrder( pages1 );
+
+    cmd.undo();
     checkOrder( m_pages );
 }
 
@@ -253,4 +421,6 @@ void TestPAPageMoveCommand::checkOrder( QList<KoPAMasterPage*> & pages )
 }
 
 QTEST_KDEMAIN( TestPAPageMoveCommand, GUI )
+
 #include "TestPAPageMoveCommand.moc"
+

c7b2d3315f5c933c34ec6200a52e7c6c15999f08
Jan Hambrecht <jaham@gmx.net>
2009-06-24 21:00:01 +0000

    SVN_SILENT forgot to update copyright year
    svn path=/trunk/koffice/; revision=986663

--- libs/flake/tools/KoCreatePathTool.h
+++ libs/flake/tools/KoCreatePathTool.h
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
  *
  * Copyright (C) 2006 Thorsten Zachmann <zachmann@kde.org>
- * Copyright (C) 2008 Jan Hambrecht <jaham@gmx.net>
+ * Copyright (C) 2008-2009 Jan Hambrecht <jaham@gmx.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public

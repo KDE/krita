4de9669513bd4510f3766e8e8a12c61613a2d1ac
Thorsten Zachmann <t.zachmann@zagge.de>
2009-07-02 05:13:52 +0000

    o move ChildrenData to its own class KoChildrenData so it is possible to   inherit from it.
      I would like to rename the class to KoShapeContainerModel but that
      name is already used as base class for the models. Any good idea for a
      rename?
    
    svn path=/trunk/koffice/; revision=990245

---
+++ libs/flake/KoChildrenData.h
@@ -0,0 +1,69 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2006-2007 Thomas Zander <zander@kde.org>
+ * Copyright (C) 2009 Thorsten Zachmann <zachmann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KOCHILDRENDATA_H
+#define KOCHILDRENDATA_H
+
+#include "KoShapeContainerModel.h"
+
+#include "flake_export.h"
+
+class FLAKE_EXPORT KoChildrenData : public KoShapeContainerModel
+{
+public:
+    KoChildrenData();
+    ~KoChildrenData();
+
+    /// reimplemented
+    virtual void add(KoShape *child);
+
+    /// reimplemented
+    void proposeMove( KoShape *shape, QPointF &move );
+
+    /// reimplemented
+    virtual void setClipping(const KoShape *child, bool clipping);
+
+    /// reimplemented
+    virtual bool childClipped(const KoShape *child) const;
+
+    // reimplemented
+    virtual void remove(KoShape *child);
+
+    /// reimplemented
+    virtual int count() const;
+
+    /// reimplemented
+    virtual QList<KoShape*> childShapes() const;
+
+    /// reimplemented
+    virtual bool isChildLocked(const KoShape *child) const;
+
+    /// reimplemented
+    virtual void containerChanged(KoShapeContainer *);
+
+    /// reimplemented
+    virtual void childChanged(KoShape *child, KoShape::ChangeType type);
+
+private:
+    class Private;
+    Private * const d;
+};
+
+#endif /* KOCHILDRENDATA_H */
--- libs/flake/KoShapeContainer.cpp
+++ libs/flake/KoShapeContainer.cpp
@@ -20,107 +20,11 @@
 #include "KoShapeContainer.h"
 #include "KoShapeContainerModel.h"
 #include "KoShapeBorderModel.h"
+#include "KoChildrenData.h"
 
 #include <QPointF>
 #include <QPainter>
 #include <QPainterPath>
-
-class ChildrenData : public KoShapeContainerModel
-{
-public:
-    ChildrenData() {}
-    ~ChildrenData() {
-        qDeleteAll(m_relations);
-    }
-
-    void add(KoShape *child) {
-        Relation *r = new Relation(child);
-        m_relations.append(r);
-    }
-    void proposeMove(KoShape *shape, QPointF &move) {
-        KoShapeContainer *parent = shape->parent();
-        bool allowedToMove = true;
-        while (allowedToMove && parent) {
-            allowedToMove = parent->isEditable();
-            parent = parent->parent();
-        }
-        if (! allowedToMove) {
-            move.setX(0);
-            move.setY(0);
-        }
-    }
-
-    void setClipping(const KoShape *child, bool clipping) {
-        Relation *relation = findRelation(child);
-        if (relation == 0) // throw exception?
-            return;
-        if (relation->m_inside == clipping)
-            return;
-        relation->m_inside = clipping;
-        relation->child()->update();
-        relation->child()->notifyChanged();
-        relation->child()->update();
-    }
-
-    bool childClipped(const KoShape *child) const {
-        Relation *relation = findRelation(child);
-        if (relation == 0) // throw exception?
-            return false;
-        return relation->m_inside;
-    }
-
-    void remove(KoShape *child) {
-        Relation *relation = findRelation(child);
-        if (relation == 0)
-            return;
-        m_relations.removeAll(relation);
-    }
-
-    int count() const {
-        return m_relations.count();
-    }
-
-    QList<KoShape*> childShapes() const {
-        QList<KoShape*> answer;
-        foreach(Relation *relation, m_relations)
-            answer.append(relation->child());
-        return answer;
-    }
-
-    bool isChildLocked(const KoShape *child) const {
-        return child->isGeometryProtected();
-    }
-
-    void containerChanged(KoShapeContainer *) { }
-    void childChanged(KoShape *, KoShape::ChangeType) { }
-
-private:
-    /**
-     * This class is a simple data-storage class for Relation objects.
-     */
-    class Relation
-    {
-    public:
-        explicit Relation(KoShape *child) : m_inside(false) , m_child(child) { }
-        KoShape* child() {
-            return m_child;
-        }
-        bool m_inside; ///< if true, the child will be clipped by the parent.
-    private:
-        KoShape *m_child;
-    };
-
-    Relation* findRelation(const KoShape *child) const {
-        foreach(Relation *relation, m_relations) {
-            if (relation->child() == child)
-                return relation;
-        }
-        return 0;
-    }
-
-private: // members
-    QList <Relation *> m_relations;
-};
 
 class KoShapeContainer::Private
 {
@@ -158,7 +62,7 @@ void KoShapeContainer::addChild(KoShape *shape)
     if (shape->parent() == this && childShapes().contains(shape))
         return;
     if (d->children == 0)
-        d->children = new ChildrenData();
+        d->children = new KoChildrenData();
     if (shape->parent() && shape->parent() != this)
         shape->parent()->removeChild(shape);
     d->children->add(shape);

cf9b0e6b8942385bdfaa62bc47c48da65e5a1b37
Jarosław Staniek <staniek@kde.org>
2009-07-04 20:28:25 +0000

    SVN_SILENT use kDebug() before assertion (kwarning is still kept in the buffer on assertion)
    svn path=/trunk/koffice/; revision=991428

--- plugins/textshape/TextShape.cpp
+++ plugins/textshape/TextShape.cpp
@@ -299,7 +299,7 @@ bool TextShape::loadOdf(const KoXmlElement & element, KoShapeLoadingContext &con
                     element.attributeNS(KoXmlNS::draw, "style-name"), "graphic",
                     context.odfLoadingContext().useStylesAutoStyles());
         if (!style)
-            kWarning(32500) << "graphic style not found:" << element.attributeNS(KoXmlNS::draw, "style-name");
+            kDebug(32500) << "graphic style not found:" << element.attributeNS(KoXmlNS::draw, "style-name");
         Q_ASSERT(style);
     }
     else if (element.hasAttributeNS(KoXmlNS::presentation, "style-name")) {
@@ -307,7 +307,7 @@ bool TextShape::loadOdf(const KoXmlElement & element, KoShapeLoadingContext &con
                     element.attributeNS(KoXmlNS::presentation, "style-name"), "presentation",
                     context.odfLoadingContext().useStylesAutoStyles());
         if (!style)
-            kWarning(32500) << "presentation style not found:" << element.attributeNS(KoXmlNS::presentation, "style-name");
+            kDebug(32500) << "presentation style not found:" << element.attributeNS(KoXmlNS::presentation, "style-name");
         Q_ASSERT(style);
     }
 

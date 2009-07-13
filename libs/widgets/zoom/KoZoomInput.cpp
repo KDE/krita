43036457fc44fb4178e198755d752ca898c396c1
Thomas Zander <zander@kde.org>
2009-07-13 10:33:30 +0000

    Relicense from "2,3 or e.V. approved" to the simpler "2+" license header. As Ok-ed on IRC by psn.
    svn path=/trunk/koffice/; revision=995756

--- libs/widgets/zoom/KoZoomInput.cpp
+++ libs/widgets/zoom/KoZoomInput.cpp
@@ -1,22 +1,20 @@
-/*
-Copyright 2008  Peter Simonsson <peter.simonsson@gmail.com>
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) version 3, or any
-later version accepted by the membership of KDE e.V. (or its
-successor approved by the membership of KDE e.V.), which shall
-act as a proxy defined in Section 6 of version 3 of the license.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2008  Peter Simonsson <peter.simonsson@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include "KoZoomInput.h"
 

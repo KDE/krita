e8c2e765911932b7552de5e0817b0f7e9d00635d
Romain Pokrzywka <romain@kdab.net>
2009-07-14 00:52:13 +0000

    -fixed the wrong symbol -removed the windows defines, that's already handled by KDE_EXPORT/KDE_IMPORT
    svn path=/trunk/koffice/; revision=996197

--- libs/kobase/kobase_export.h
+++ libs/kobase/kobase_export.h
@@ -23,23 +23,14 @@
 /* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-/* We use _WIN32/_WIN64 instead of Q_OS_WIN so that this header can be used from C files too */
-#if defined _WIN32 || defined _WIN64
-
 #ifndef KOBASE_EXPORT
-# if defined(MAKE_KOPROPERTIES_LIB)
+# if defined(MAKE_KOBASE_LIB)
 /* We are building this library */
 #  define KOBASE_EXPORT KDE_EXPORT
 # else
 /* We are using this library */
 #  define KOBASE_EXPORT KDE_IMPORT
 # endif
-#endif
-
-#else /* UNIX */
-
-#define KOBASE_EXPORT KDE_EXPORT
-
 #endif
 
 #endif

efb95e51c1907706cbd0de96b684dbaa94f78fe7
Elvis Stansvik <elvstone@gmail.com>
2009-07-15 19:04:57 +0000

    Adding my copyright.
    svn path=/trunk/koffice/; revision=997346

--- libs/kotext/KoTextDebug.cpp
+++ libs/kotext/KoTextDebug.cpp
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright (C) 2008 Girish Ramakrishnan <girish@forwardbias.in>
+ * Copyright (C) 2009 Elvis Stansvik <elvstone@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public

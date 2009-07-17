c3247e0f2b08499aeaa42db2bc96080a159684d1
Casper Boemann <cbr@boemann.dk>
2009-07-17 15:34:25 +0000

    forgot to add some copyright
    svn path=/trunk/koffice/; revision=998405

--- plugins/textshape/TableCellBorderData.h
+++ plugins/textshape/TableCellBorderData.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright (C) 2006 Thomas Zander <zander@kde.org>
+ * Copyright (C) 2009 KO GmbH <cbo@kogmbh.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public

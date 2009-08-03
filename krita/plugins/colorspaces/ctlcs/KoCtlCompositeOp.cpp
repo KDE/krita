64acff12464466685ab5c575e45bbf8f78e000be
Cyrille Berger <cyb@lepi.org>
2009-08-03 21:43:40 +0000

    compile the composite op
    svn path=/trunk/koffice/; revision=1006533

--- krita/plugins/colorspaces/ctlcs/KoCtlCompositeOp.cpp
+++ krita/plugins/colorspaces/ctlcs/KoCtlCompositeOp.cpp
@@ -25,13 +25,40 @@
 
 #include <klocale.h>
 
+#include "kis_debug.h"
+
 #include "KoCtlColorSpace.h"
 #include "KoCtlBuffer.h"
 #include <OpenCTL/Program.h>
+#include <OpenCTL/Module.h>
 
 KoCTLCompositeOp::KoCTLCompositeOp(OpenCTL::Template* _template, const KoCtlColorSpace * cs, const GTLCore::PixelDescription& _pd) : KoCompositeOp(cs, idForFile(_template->fileName()), descriptionForFile(_template->fileName()), categoryForFile(_template->fileName())), m_withMaskProgram(0), m_withoutMaskProgram(0)
 {
-  
+  OpenCTL::Module* module = _template->generateModule(_pd);
+  module->compile();
+  if(module->isCompiled())
+  {
+    std::list<GTLCore::PixelDescription> pdl;
+    pdl.push_back(_pd);
+    pdl.push_back(_pd);
+    m_withoutMaskProgram = new OpenCTL::Program("compositeWithoutmask", module, pdl, _pd );
+    if(!m_withoutMaskProgram->initialised())
+    {
+      dbgPlugins << "Without mask failed";
+      delete m_withoutMaskProgram;
+      m_withoutMaskProgram = 0;
+    }
+    pdl.push_back(GTLCore::PixelDescription(GTLCore::Type::UnsignedInteger8, 1));
+    m_withMaskProgram = new OpenCTL::Program("compositeWithmask", module, pdl, _pd);
+    if(!m_withMaskProgram->initialised())
+    {
+      dbgPlugins << "With mask failed";
+      delete m_withMaskProgram;
+      m_withMaskProgram = 0;
+    }
+  } else {
+    dbgPlugins << "Composite op compilation failure";
+  }
 }
 
 void KoCTLCompositeOp::composite(quint8 *dstRowStart, qint32 dstRowStride,

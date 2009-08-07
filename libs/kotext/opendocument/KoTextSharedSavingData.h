e0085a136674ab6c83be362f845383d9776509bf
Thomas Zander <zander@kde.org>
2009-08-07 20:10:54 +0000

    Using the table in the relicensecheck I noted that a lot of these LGPLv2-only files can easily be made LGPLv2+ to future proof ourselves Also fixed some additional spacing which threw off licenceCheck.
    svn path=/trunk/koffice/; revision=1008551

--- libs/kotext/opendocument/KoTextSharedSavingData.h
+++ libs/kotext/opendocument/KoTextSharedSavingData.h
@@ -4,7 +4,8 @@ Copyright (C) 2007-2008 Thorsten Zachmann <zachmann@kde.org>
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Library General Public
-License version 2 as published by the Free Software Foundation.
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
--- libs/odf/KoGenChanges.cpp
+++ libs/odf/KoGenChanges.cpp
@@ -1,9 +1,10 @@
 /* This file is part of the KDE project
-  Copyright (C) 2008 Pierre Stirnweiss \pierre.stirnweiss_koffice@gadz.org>
+  Copyright (C) 2008 Pierre Stirnweiss <pierre.stirnweiss_koffice@gadz.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of

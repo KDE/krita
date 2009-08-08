36874529e134421480f4f0a252850880eb93777f
Jeremias Epperlein <jeeree@web.de>
2009-08-08 18:16:24 +0000

    change loading of rows, add debug writeout
    Adds an action that writes the current element tree to kDebug(),
    changes the loading of rows and fractions, so that empty mrows
    are treated as placeholders and mrows with one child as equivalent to
    the child
    
    svn path=/trunk/koffice/; revision=1008944

--- plugins/artistictextshape/ArtisticTextShape.cpp
+++ plugins/artistictextshape/ArtisticTextShape.cpp
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public License
+ * Yoßu should have received a copy of the GNU Library General Public License
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.

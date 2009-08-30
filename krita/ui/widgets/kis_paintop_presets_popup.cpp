3aa96d012dc5fa341d60aed81944a0dfcb1d694b
Sven Langkamp <sven.langkamp@gmail.com>
2009-08-30 10:24:24 +0000

    don't leak
    svn path=/trunk/koffice/; revision=1017355

--- krita/ui/widgets/kis_paintop_presets_popup.cpp
+++ krita/ui/widgets/kis_paintop_presets_popup.cpp
@@ -60,7 +60,9 @@ KisPaintOpPresetsPopup::KisPaintOpPresetsPopup(QWidget * parent)
     m_d->uiWdgPaintOpPresets.setupUi(this);
 
     // TODO 2.2 Disables preset collections
+    QWidget * collectionWidget = m_d->uiWdgPaintOpPresets.tabPresets->widget(1);
     m_d->uiWdgPaintOpPresets.tabPresets->removeTab(1);
+    delete collectionWidget;
     m_d->uiWdgPaintOpPresets.bnSave->setEnabled(false);
 
     m_d->layout = new QGridLayout(m_d->uiWdgPaintOpPresets.frmOptionWidgetContainer);

20fdd11ed2ea2a88c2afd4c36e6e12ecc5e46813
Burkhard Lück <lueck@hube-lueck.de>
2009-09-02 14:28:21 +0000

    extract name string from ctlcs files for translation
    svn path=/trunk/koffice/; revision=1018985

--- krita/Messages.sh
+++ krita/Messages.sh
@@ -2,6 +2,7 @@
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp 
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
 $EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment plugins/extensions/scripting/scripts/*.rc >> rc.cpp || exit 12
+$EXTRACTATTR --attr=info,name plugins/colorspaces/ctlcs/ctlcolorspaces/*.ctlcs >> rc.cpp || exit 12
 perl extracti18n.pl > i18ndata
 # ignore sdk/templates since it contains templates for use a future plugins, none of the strings there will ever be seen by any user
 $XGETTEXT i18ndata rc.cpp `find . -name \*.cc -o -name \*.h  -o -name \*.cpp | grep -v './sdk/templates'` ui/kis_aboutdata.h -o $podir/krita.pot

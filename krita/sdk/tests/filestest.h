fc3e4215f4e8e016014a66af64d9b38364915e71
Boudewijn Rempt <boud@valdyas.org>
2009-09-11 17:04:35 +0000

    Make file testing more robust
    * Collect all errors and present them at the end
    * don't use KoDocument::import, but use the same code as in the
    import catcher. KoDocument::import creates a view, and that
    causes weirdness.
    
    svn path=/trunk/koffice/; revision=1022400

--- krita/sdk/tests/filestest.h
+++ krita/sdk/tests/filestest.h
@@ -17,6 +17,22 @@
  */
 
 #include "testutil.h"
+
+#include <QDir>
+
+#include <kaboutdata.h>
+#include <kimageio.h>
+#include <kcmdlineargs.h>
+#include <klocale.h>
+#include <kglobal.h>
+#include <kmimetype.h>
+#include <kapplication.h>
+#include <kdebug.h>
+#include <kio/netaccess.h>
+#include <kio/job.h>
+
+#include <KoFilterManager.h>
+
 #include <kis_doc2.h>
 #include <kis_image.h>
 #include <KoColorSpace.h>
@@ -27,39 +43,70 @@
 namespace TestUtil
 {
 
-void testFiles(const QString& _dirname)
-{
-    QDir dirSources(_dirname);
-    qDebug() << "There are " << dirSources.entryInfoList().count() << " files in " << _dirname;
-    foreach(QFileInfo sourceFileInfo, dirSources.entryInfoList()) {
-        if (!sourceFileInfo.isHidden()) {
-            qDebug() << "handling " << sourceFileInfo.fileName();
-            QFileInfo resultFileInfo(QString(FILES_DATA_DIR) + "/results/" + sourceFileInfo.fileName() + ".png");
-            QVERIFY2(resultFileInfo.exists(),
-                     QString("Result file %1 not found").arg(resultFileInfo.fileName()).toAscii().data());
-            KisDoc2 doc;
-            doc.importDocument(sourceFileInfo.absoluteFilePath());
-            QVERIFY(doc.image());
-            QString id = doc.image()->colorSpace()->id();
-            if (id != "GRAYA" && id != "GRAYA16" && id != "RGBA" && id != "RGBA16") {
-                dbgKrita << "Images need conversion";
-                doc.image()->convertTo(KoColorSpaceRegistry::instance()->rgb8());
-            }
-            KTemporaryFile tmpFile;
-            tmpFile.setSuffix(".png");
-            tmpFile.open();
-            tmpFile.setAutoRemove(false);
-            doc.setOutputMimeType("image/png");
-            doc.saveAs("file://" + tmpFile.fileName());
-            QImage resultImg(resultFileInfo.absoluteFilePath());
-            resultImg = resultImg.convertToFormat(QImage::Format_ARGB32);
-            QImage sourceImg(tmpFile.fileName());
-            sourceImg = sourceImg.convertToFormat(QImage::Format_ARGB32);
+    void testFiles(const QString& _dirname)
+    {
+        QDir dirSources(_dirname);
+        qDebug() << "There are " << dirSources.entryInfoList().count() << " files in " << _dirname;
 
-            QPoint pt;
-            QVERIFY2(TestUtil::compareQImages(pt, resultImg, sourceImg), QString("Pixel (%1,%2) has different values").arg(pt.x()).arg(pt.y()).toLatin1());
+        QStringList failuresFileInfo;
+        QStringList failuresDocImage;
+        QStringList failuresCompare;
+
+        foreach(QFileInfo sourceFileInfo, dirSources.entryInfoList()) {
+
+            if (!sourceFileInfo.isHidden()) {
+                qDebug() << "handling " << sourceFileInfo.fileName();
+                QFileInfo resultFileInfo(QString(FILES_DATA_DIR) + "/results/" + sourceFileInfo.fileName() + ".png");
+
+                if (!resultFileInfo.exists()) {
+                    failuresFileInfo << resultFileInfo.fileName();
+                    continue;
+                }
+
+                KisDoc2 doc;
+                KoFilterManager manager( &doc );
+                QByteArray nativeFormat = doc.nativeFormatMimeType();
+                KoFilter::ConversionStatus status;
+                QString s = manager.importDocument(sourceFileInfo.absoluteFilePath(), status);
+
+                if (!doc.image()) {
+                    failuresDocImage << sourceFileInfo.fileName();
+                    continue;
+                }
+
+                QString id = doc.image()->colorSpace()->id();
+                if (id != "GRAYA" && id != "GRAYA16" && id != "RGBA" && id != "RGBA16") {
+                    dbgKrita << "Images need conversion";
+                    doc.image()->convertTo(KoColorSpaceRegistry::instance()->rgb8());
+                }
+
+                KTemporaryFile tmpFile;
+                tmpFile.setSuffix(".png");
+                tmpFile.open();
+                tmpFile.setAutoRemove(false);
+                doc.setOutputMimeType("image/png");
+                doc.saveAs("file://" + tmpFile.fileName());
+                QImage resultImg(resultFileInfo.absoluteFilePath());
+                resultImg = resultImg.convertToFormat(QImage::Format_ARGB32);
+                QImage sourceImg(tmpFile.fileName());
+                sourceImg = sourceImg.convertToFormat(QImage::Format_ARGB32);
+
+                QPoint pt;
+
+                if (!TestUtil::compareQImages(pt, resultImg, sourceImg)) {
+                    failuresCompare << sourceFileInfo.fileName() + ": " + QString("Pixel (%1,%2) has different values").arg(pt.x()).arg(pt.y()).toLatin1();
+                    continue;
+                }
+            }
         }
+        if (failuresCompare.isEmpty() && failuresDocImage.isEmpty() && failuresFileInfo.isEmpty()) {
+            return;
+        }
+        qDebug() << "Comparison failures: " << failuresCompare;
+        qDebug() << "No image failures: " << failuresDocImage;
+        qDebug() << "No comparison image: " <<  failuresFileInfo;
+
+        QFAIL("Failed testing files");
     }
-}
 
 }

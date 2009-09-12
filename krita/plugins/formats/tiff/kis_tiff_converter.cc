9f95a53eb6a0c514525788704a66e2218c7d03d5
Boudewijn Rempt <boud@valdyas.org>
2009-09-12 11:45:42 +0000

    Use constants to retrieve colorspaces
    We used to use hard-coded strings to retrieve colorspaces, but
    there are useful constants now.
    
    svn path=/trunk/koffice/; revision=1022732

--- krita/plugins/formats/tiff/kis_tiff_converter.cc
+++ krita/plugins/formats/tiff/kis_tiff_converter.cc
@@ -33,6 +33,7 @@
 
 #include <KoColorSpaceRegistry.h>
 #include <KoColorSpace.h>
+#include <KoColorModelStandardIds.h>
 
 #include <kis_doc2.h>
 #include <kis_image.h>
@@ -50,95 +51,96 @@
 namespace
 {
 
-QString getColorSpaceForColorType(uint16 sampletype, uint16 color_type, uint16 color_nb_bits, TIFF *image, uint16 &nbchannels, uint16 &extrasamplescount, uint8 &destDepth)
-{
-    if (color_type == PHOTOMETRIC_MINISWHITE || color_type == PHOTOMETRIC_MINISBLACK) {
-        if (nbchannels == 0) nbchannels = 1;
-        extrasamplescount = nbchannels - 1; // FIX the extrasamples count in case of
-        if (color_nb_bits <= 8) {
-            destDepth = 8;
-            return "GRAYA";
-        } else {
-            destDepth = 16;
-            return "GRAYA16";
-        }
-    } else if (color_type == PHOTOMETRIC_RGB  /*|| color_type == */) {
-        if (nbchannels == 0) nbchannels = 3;
-        extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
-        if (sampletype == SAMPLEFORMAT_IEEEFP) {
-            if (color_nb_bits == 16) {
-                destDepth = 16;
-                return "RGBAF16HALF";
-            } else if (color_nb_bits == 32) {
-                destDepth = 32;
-                return "RGBAF32";
-            }
-            return "";
-        } else {
+    QString getColorSpaceForColorType(uint16 sampletype, uint16 color_type, uint16 color_nb_bits, TIFF *image, uint16 &nbchannels, uint16 &extrasamplescount, uint8 &destDepth)
+    {
+        if (color_type == PHOTOMETRIC_MINISWHITE || color_type == PHOTOMETRIC_MINISBLACK) {
+            if (nbchannels == 0) nbchannels = 1;
+            extrasamplescount = nbchannels - 1; // FIX the extrasamples count in case of
             if (color_nb_bits <= 8) {
                 destDepth = 8;
-                return "RGBA";
+                return KoColorSpaceRegistry::instance()->colorSpaceId(GrayAColorModelID, Integer8BitsColorDepthID);
             } else {
                 destDepth = 16;
-                return "RGBA16";
+                return KoColorSpaceRegistry::instance()->colorSpaceId(GrayAColorModelID, Integer16BitsColorDepthID);
             }
-        }
-    } else if (color_type == PHOTOMETRIC_YCBCR) {
-        if (nbchannels == 0) nbchannels = 3;
-        extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
-        if (color_nb_bits <= 8) {
-            destDepth = 8;
-            return "YCbCrAU8";
-        } else {
-            destDepth = 16;
-            return "YCbCrAU16";
-        }
-    } else if (color_type == PHOTOMETRIC_SEPARATED) {
-        if (nbchannels == 0) nbchannels = 4;
-        // SEPARATED is in general CMYK but not always, so we check
-        uint16 inkset;
-        if ((TIFFGetField(image, TIFFTAG_INKSET, &inkset) == 0)) {
-            dbgFile << "Image does not define the inkset.";
-            inkset = 2;
-        }
-        if (inkset !=  INKSET_CMYK) {
-            dbgFile << "Unsupported inkset (right now, only CMYK is supported)";
-            char** ink_names;
-            uint16 numberofinks;
-            if (TIFFGetField(image, TIFFTAG_INKNAMES, &ink_names)  == 1 && TIFFGetField(image, TIFFTAG_NUMBEROFINKS, &numberofinks)  == 1) {
-                dbgFile << "Inks are :";
-                for (uint i = 0; i < numberofinks; i++) {
-                    dbgFile << ink_names[i];
+
+        } else if (color_type == PHOTOMETRIC_RGB  /*|| color_type == */) {
+            if (nbchannels == 0) nbchannels = 3;
+            extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
+            if (sampletype == SAMPLEFORMAT_IEEEFP) {
+                if (color_nb_bits == 16) {
+                    destDepth = 16;
+                    return KoColorSpaceRegistry::instance()->colorSpaceId(RGBAColorModelID, Float16BitsColorDepthID);
+                } else if (color_nb_bits == 32) {
+                    destDepth = 32;
+                    return KoColorSpaceRegistry::instance()->colorSpaceId(RGBAColorModelID, Float32BitsColorDepthID);
                 }
+                return "";
             } else {
-                dbgFile << "inknames are not defined !";
-                // To be able to read stupid adobe files, if there are no information about inks and four channels, then it's a CMYK file :
-                if (nbchannels - extrasamplescount != 4) {
-                    return "";
+                if (color_nb_bits <= 8) {
+                    destDepth = 8;
+                    return KoColorSpaceRegistry::instance()->colorSpaceId(RGBAColorModelID, Integer8BitsColorDepthID);
+                } else {
+                    destDepth = 16;
+                    return KoColorSpaceRegistry::instance()->colorSpaceId(RGBAColorModelID, Integer16BitsColorDepthID);
                 }
             }
-        }
-        if (color_nb_bits <= 8) {
-            destDepth = 8;
-            return "CMYK";
-        } else {
+        } else if (color_type == PHOTOMETRIC_YCBCR) {
+            if (nbchannels == 0) nbchannels = 3;
+            extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
+            if (color_nb_bits <= 8) {
+                destDepth = 8;
+                return KoColorSpaceRegistry::instance()->colorSpaceId(YCbCrAColorModelID, Integer8BitsColorDepthID);
+            } else {
+                destDepth = 16;
+                return KoColorSpaceRegistry::instance()->colorSpaceId(YCbCrAColorModelID, Integer16BitsColorDepthID);
+            }
+        } else if (color_type == PHOTOMETRIC_SEPARATED) {
+            if (nbchannels == 0) nbchannels = 4;
+            // SEPARATED is in general CMYK but not always, so we check
+            uint16 inkset;
+            if ((TIFFGetField(image, TIFFTAG_INKSET, &inkset) == 0)) {
+                dbgFile << "Image does not define the inkset.";
+                inkset = 2;
+            }
+            if (inkset !=  INKSET_CMYK) {
+                dbgFile << "Unsupported inkset (right now, only CMYK is supported)";
+                char** ink_names;
+                uint16 numberofinks;
+                if (TIFFGetField(image, TIFFTAG_INKNAMES, &ink_names)  == 1 && TIFFGetField(image, TIFFTAG_NUMBEROFINKS, &numberofinks)  == 1) {
+                    dbgFile << "Inks are :";
+                    for (uint i = 0; i < numberofinks; i++) {
+                        dbgFile << ink_names[i];
+                    }
+                } else {
+                    dbgFile << "inknames are not defined !";
+                    // To be able to read stupid adobe files, if there are no information about inks and four channels, then it's a CMYK file :
+                    if (nbchannels - extrasamplescount != 4) {
+                        return "";
+                    }
+                }
+            }
+            if (color_nb_bits <= 8) {
+                destDepth = 8;
+                return KoColorSpaceRegistry::instance()->colorSpaceId(CMYKAColorModelID, Integer8BitsColorDepthID);
+            } else {
+                destDepth = 16;
+                return KoColorSpaceRegistry::instance()->colorSpaceId(CMYKAColorModelID, Integer16BitsColorDepthID);
+            }
+        } else if (color_type == PHOTOMETRIC_CIELAB || color_type == PHOTOMETRIC_ICCLAB) {
             destDepth = 16;
-            return "CMYKA16";
+            if (nbchannels == 0) nbchannels = 3;
+            extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
+            return KoColorSpaceRegistry::instance()->colorSpaceId(LABAColorModelID, Integer16BitsColorDepthID);
+        } else if (color_type ==  PHOTOMETRIC_PALETTE) {
+            destDepth = 16;
+            if (nbchannels == 0) nbchannels = 2;
+            extrasamplescount = nbchannels - 2; // FIX the extrasamples count in case of
+            // <-- we will convert the index image to RGBA16 as the palette is always on 16bits colors
+            return KoColorSpaceRegistry::instance()->colorSpaceId(RGBAColorModelID, Integer16BitsColorDepthID);
         }
-    } else if (color_type == PHOTOMETRIC_CIELAB || color_type == PHOTOMETRIC_ICCLAB) {
-        destDepth = 16;
-        if (nbchannels == 0) nbchannels = 3;
-        extrasamplescount = nbchannels - 3; // FIX the extrasamples count in case of
-        return "LABA"; // TODO add support for a 8bit LAB colorspace when it is written
-    } else if (color_type ==  PHOTOMETRIC_PALETTE) {
-        destDepth = 16;
-        if (nbchannels == 0) nbchannels = 2;
-        extrasamplescount = nbchannels - 2; // FIX the extrasamples count in case of
-        // <-- we will convert the index image to RGBA16 as the palette is always on 16bits colors
-        return "RGBA16";
+        return QString::null;
     }
-    return "";
-}
 }
 
 KisTIFFConverter::KisTIFFConverter(KisDoc2 *doc, KisUndoAdapter *adapter)
@@ -230,14 +232,15 @@ KisImageBuilder_Result KisTIFFConverter::readTIFFDirectory(TIFF* image)
         dbgFile << "Image has an undefined photometric interpretation";
         color_type = PHOTOMETRIC_MINISWHITE;
     }
+
     uint8 dstDepth;
-    QString csName = getColorSpaceForColorType(sampletype, color_type, depth, image, nbchannels, extrasamplescount, dstDepth);
-    if (csName.isEmpty()) {
+    QString colorSpaceId = getColorSpaceForColorType(sampletype, color_type, depth, image, nbchannels, extrasamplescount, dstDepth);
+    if (colorSpaceId.isEmpty()) {
         dbgFile << "Image has an unsupported colorspace :" << color_type << " for this depth :" << depth;
         TIFFClose(image);
         return KisImageBuilder_RESULT_UNSUPPORTED_COLORSPACE;
     }
-    dbgFile << "Colorspace is :" << csName << " with a depth of" << depth << " and with a nb of channels of" << nbchannels;
+    dbgFile << "Colorspace is :" << colorSpaceId << " with a depth of" << depth << " and with a nb of channels of" << nbchannels;
 
     // Read image profile
     dbgFile << "Reading profile";
@@ -259,12 +262,12 @@ KisImageBuilder_Result KisTIFFConverter::readTIFFDirectory(TIFF* image)
     const KoColorSpace* cs = 0;
     if (profile && profile->isSuitableForOutput()) {
         dbgFile << "image has embedded profile:" << profile -> name() << "";
-        cs = KoColorSpaceRegistry::instance()->colorSpace(csName, profile);
-    } else
-        cs = KoColorSpaceRegistry::instance()->colorSpace(KoID(csName, ""), "");
-
+        cs = KoColorSpaceRegistry::instance()->colorSpace(colorSpaceId, profile);
+    } else {
+        cs = KoColorSpaceRegistry::instance()->colorSpace(colorSpaceId, 0);
+    }
     if (cs == 0) {
-        dbgFile << "Colorspace" << csName << " is not available, please check your installation.";
+        dbgFile << "Colorspace" << colorSpaceId << " is not available, please check your installation.";
         TIFFClose(image);
         return KisImageBuilder_RESULT_UNSUPPORTED_COLORSPACE;
     }
@@ -273,7 +276,7 @@ KisImageBuilder_Result KisTIFFConverter::readTIFFDirectory(TIFF* image)
     KoColorTransformation* transform = 0;
     if (profile && !profile->isSuitableForOutput()) {
         dbgFile << "The profile can't be used in krita, need conversion";
-        transform = KoColorSpaceRegistry::instance()->colorSpace(csName, profile)->createColorConverter( cs );
+        transform = KoColorSpaceRegistry::instance()->colorSpace(colorSpaceId, profile)->createColorConverter( cs );
     }
 
     // Check if there is an alpha channel
@@ -355,35 +358,35 @@ KisImageBuilder_Result KisTIFFConverter::readTIFFDirectory(TIFF* image)
     uint8 nbcolorsamples = nbchannels - extrasamplescount;
     switch (color_type) {
     case PHOTOMETRIC_MINISWHITE: {
-        poses[0] = 0; poses[1] = 1;
-        postprocessor = new KisTIFFPostProcessorInvert(nbcolorsamples);
-    }
-    break;
+            poses[0] = 0; poses[1] = 1;
+            postprocessor = new KisTIFFPostProcessorInvert(nbcolorsamples);
+        }
+        break;
     case PHOTOMETRIC_MINISBLACK: {
-        poses[0] = 0; poses[1] = 1;
-        postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
-    }
-    break;
+            poses[0] = 0; poses[1] = 1;
+            postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
+        }
+        break;
     case PHOTOMETRIC_CIELAB: {
-        poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3;
-        postprocessor = new KisTIFFPostProcessorICCLABtoCIELAB(nbcolorsamples);
-    }
-    break;
+            poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3;
+            postprocessor = new KisTIFFPostProcessorICCLABtoCIELAB(nbcolorsamples);
+        }
+        break;
     case PHOTOMETRIC_ICCLAB: {
-        poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3;
-        postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
-    }
-    break;
+            poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3;
+            postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
+        }
+        break;
     case PHOTOMETRIC_RGB: {
-        poses[0] = 2; poses[1] = 1; poses[2] = 0; poses[3] = 3;
-        postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
-    }
-    break;
+            poses[0] = 2; poses[1] = 1; poses[2] = 0; poses[3] = 3;
+            postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
+        }
+        break;
     case PHOTOMETRIC_SEPARATED: {
-        poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3; poses[4] = 4;
-        postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
-    }
-    break;
+            poses[0] = 0; poses[1] = 1; poses[2] = 2; poses[3] = 3; poses[4] = 4;
+            postprocessor = new KisTIFFPostProcessor(nbcolorsamples);
+        }
+        break;
     default:
         break;
     }

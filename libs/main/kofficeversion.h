6c11e70597277e6aa5764934176c9fe7f007ba6c
Cyrille Berger <cyb@lepi.org>
2009-10-03 14:56:42 +0000

    up version to beta 3
    svn path=/trunk/koffice/; revision=1030936

--- libs/main/kofficeversion.h
+++ libs/main/kofficeversion.h
@@ -22,14 +22,14 @@
 #ifndef _KOFFICE_VERSION_H_
 #define _KOFFICE_VERSION_H_
 
-#define KOFFICE_VERSION_STRING "2.1 Beta 2"
+#define KOFFICE_VERSION_STRING "2.1 Beta 3"
 #define KOFFICE_VERSION_MAJOR 2
 #define KOFFICE_VERSION_MINOR 0
-#define KOFFICE_VERSION_RELEASE 82
+#define KOFFICE_VERSION_RELEASE 83
 #define KOFFICE_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
 // #define KOFFICE_ALPHA 1
-#define KOFFICE_BETA 2
+#define KOFFICE_BETA 3
 // #define KOFFICE_RC 1
 
 

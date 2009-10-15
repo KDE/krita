b24ca28450675f48638618a037bb9755bb8659cb
Thomas Zander <zander@kde.org>
2009-10-15 12:10:00 +0000

    Better font for demo text.
    Use a sane default font size for the demo text, not the one the user
    selected for his desktop
    
    svn path=/trunk/koffice/; revision=1035570

--- plugins/textshape/TextShape.cpp
+++ plugins/textshape/TextShape.cpp
@@ -113,6 +113,9 @@ void TextShape::setDemoText(bool on)
     m_demoText = on;
     if (m_demoText) {
         QTextCursor cursor(m_textShapeData->document());
+        QTextCharFormat cf;
+        cf.setFontPointSize(12.);
+        cursor.mergeCharFormat(cf);
         for (int i = 0; i < 10; ++i)
             cursor.insertText("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.\n");
     }

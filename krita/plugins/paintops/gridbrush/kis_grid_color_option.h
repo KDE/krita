625083b3edfb2822e5a6b0e6d1aded7b34dc7745
Lukáš Tvrdý <lukast.dev@gmail.com>
2009-10-25 21:07:18 +0000

    Fix the copyright and license texts.
    svn path=/trunk/koffice/; revision=1040219

--- krita/plugins/paintops/gridbrush/kis_grid_color_option.h
+++ krita/plugins/paintops/gridbrush/kis_grid_color_option.h
@@ -1,18 +1,20 @@
 /*
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+ * Copyright (c) 2009 Lukáš Tvrdý (lukast.dev@gmail.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 #ifndef KIS_GRID_COLOR_OPTION_H
 #define KIS_GRID_COLOR_OPTION_H
--- krita/plugins/paintops/gridbrush/kis_gridop_option.h
+++ krita/plugins/paintops/gridbrush/kis_gridop_option.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2008-2009 Lukáš Tvrdý <lukast.dev@gmail.com>
+ * Copyright (c) 2009 Lukáš Tvrdý (lukast.dev@gmail.com)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
 #ifndef KIS_GRIDOP_OPTION_H
 #define KIS_GRIDOP_OPTION_H
 

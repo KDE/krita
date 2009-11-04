8b90198d3eb9d5fdc756b5b6e67c2df584e14d96
Jean-Nicolas Artaud <jeannicolasartaud@gmail.com>
2009-11-04 15:01:48 +0000

    * Fix the bug 213045, standard was useless when another master-slide * exists
    BUG 213045
    
    svn path=/trunk/koffice/; revision=1044740

--- libs/odf/KoGenStyles.cpp
+++ libs/odf/KoGenStyles.cpp
@@ -404,8 +404,12 @@ void KoGenStyles::saveOdfMasterStyles(KoXmlWriter* xmlWriter) const
 
     QList<KoGenStyles::NamedStyle> stylesList = styles(KoGenStyle::StyleMaster);
     QList<KoGenStyles::NamedStyle>::const_iterator it = stylesList.constBegin();
+    bool standardUseless = false;
+    if( stylesList.count() > 1 )
+        standardUseless = true;
     for (; it != stylesList.constEnd() ; ++it) {
-        (*it).style->writeStyle(xmlWriter, *this, "style:master-page", (*it).name, 0);
+        if(!standardUseless and (*it).name != "Standard")
+            (*it).style->writeStyle(xmlWriter, *this, "style:master-page", (*it).name, 0);
     }
 
     if (!d->rawOdfMasterStyles.isEmpty()) {

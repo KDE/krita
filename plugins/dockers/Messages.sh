b49f8c0ed98d131d9f0bdad627b9eb8663446aca
Albert Astals Cid <tsdgeos@terra.es>
2009-11-12 00:08:44 +0000

    easier to maintain Messages.sh code
    svn path=/trunk/koffice/; revision=1047745

--- plugins/dockers/Messages.sh
+++ plugins/dockers/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$XGETTEXT *.cpp colordocker/*cpp shapeselector/*.cpp shadowdocker/*.cpp shapeproperties/*.cpp shapecollection/*.cpp strokedocker/*.cpp -o $podir/koffice-dockers.pot
+$XGETTEXT $(find . -name "*.cpp") -o $podir/koffice-dockers.pot
 

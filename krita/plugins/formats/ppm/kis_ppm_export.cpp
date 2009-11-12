56aa39c75f68ff7143f8bf89d4ad4249d29e18f4
Cyrille Berger <cyb@lepi.org>
2009-11-12 22:19:47 +0000

    test for the color space
    svn path=/trunk/koffice/; revision=1048211

--- krita/plugins/formats/ppm/kis_ppm_export.cpp
+++ krita/plugins/formats/ppm/kis_ppm_export.cpp
@@ -19,11 +19,18 @@
 #include "kis_ppm_export.h"
 
 #include <KGenericFactory>
+
+#include <KoColorSpace.h>
+#include <KoColorSpaceConstants.h>
 #include <KoFilterChain.h>
 
 #include <KDialog>
+#include <KMessageBox>
 
 #include <kis_debug.h>
+#include <kis_doc2.h>
+#include <kis_image.h>
+#include <kis_paint_device.h>
 
 #include "ui_kis_wdg_options_ppm.h"
 
@@ -44,32 +51,54 @@ KoFilter::ConversionStatus KisPPMExport::convert(const QByteArray& from, const Q
 
     if (from != "application/x-krita")
         return KoFilter::NotImplemented;
-    
+
+    KisDoc2 *output = dynamic_cast<KisDoc2*>(m_chain->inputDocument());
+    QString filename = m_chain->outputFile();
+
+    if (!output)
+        return KoFilter::CreationError;
+
+    if (filename.isEmpty()) return KoFilter::FileNotFound;
 
     KDialog* kdb = new KDialog(0);
     kdb->setWindowTitle(i18n("PPM Export Options"));
     kdb->setButtons(KDialog::Ok | KDialog::Cancel);
 
     Ui::WdgOptionsPPM optionsPPM;
-    
+
     QWidget* wdg = new QWidget(kdb);
     optionsPPM.setupUi(wdg);
 
     kdb->setMainWidget(wdg);
-    
+
     if (kdb->exec() == QDialog::Rejected) {
         return KoFilter::OK; // FIXME Cancel doesn't exist :(
     }
-    
+
+    bool rgb = (to == "image/x-portable-pixmap");
+    bool binary = optionsPPM.type->currentIndex() == 0;
+    bool bitmap = (to == "image/x-portable-bitmap");
+
+    KisImageWSP img = output->image();
+    Q_CHECK_PTR(img);
+
+    KisPaintDeviceSP pd = new KisPaintDevice(*img->projection());
+
+    // Test color space
+    if (((rgb && (pd->colorSpace()->id() != "RGBA" && pd->colorSpace()->id() != "RGBA16"))
+            || (!rgb && (pd->colorSpace()->id() != "GRAYA" && pd->colorSpace()->id() != "GRAYA16")))) {
+        KMessageBox::error(0, i18n("Cannot export images in %1.\n", pd->colorSpace()->name())) ;
+        return KoFilter::CreationError;
+    }
+
 //         if (KoID(cs->id()) == KoID("GRAYA") || KoID(cs->id()) == KoID("GRAYA16")) {
 //         return alpha ? PNG_COLOR_TYPE_GRAY_ALPHA : PNG_COLOR_TYPE_GRAY;
 //     }
 //     if (KoID(cs->id()) == KoID("RGBA") || KoID(cs->id()) == KoID("RGBA16")) {
 //         return alpha ? PNG_COLOR_TYPE_RGB_ALPHA : PNG_COLOR_TYPE_RGB;
 //     }
-// 
-//     KMessageBox::error(0, i18n("Cannot export images in %1.\n", cs->name())) ;
+//
 
-    
+
     abort();
 }

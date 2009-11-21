3328fcf95f6e3eb97d55f152aaadea9a9d0a702e
Cyrille Berger <cyb@lepi.org>
2009-11-21 20:19:01 +0000

    now that we rotate the image, the height and width are not correct anymore
    svn path=/trunk/koffice/; revision=1052470

--- krita/plugins/paintops/libbrush/kis_auto_brush.cpp
+++ krita/plugins/paintops/libbrush/kis_auto_brush.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2004,2007-2008 Cyrille Berger <cberger@cberger.net>
+ *  Copyright (c) 2004,2007-2009 Cyrille Berger <cberger@cberger.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -46,6 +46,8 @@ KisAutoBrush::KisAutoBrush(KisMaskGenerator* as, double angle)
     QImage img = createBrushPreview();
     setImage(img);
     setBrushType(MASK);
+    setWidth(d->shape->width());
+    setHeight(d->shape->height());
 }
 
 KisAutoBrush::~KisAutoBrush()

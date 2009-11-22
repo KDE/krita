0d66c965f3fd034a610ba2c3f38e1e80600dea3b
Boudewijn Rempt <boud@valdyas.org>
2009-11-22 14:32:49 +0000

    start working on reading the layer info section
    svn path=/trunk/koffice/; revision=1052809

--- krita/plugins/formats/psd/psd_layer_section.h
+++ krita/plugins/formats/psd/psd_layer_section.h
@@ -1,10 +1,36 @@
+/*
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 #ifndef PSD_LAYER_SECTION_H
 #define PSD_LAYER_SECTION_H
+
+#include <QString>
+
+class QIODevice;
 
 class PSDLayerSection
 {
 public:
     PSDLayerSection();
+    bool read(QIODevice* io);
+    bool write(QIODevice* io);
+    bool valid();
+
+    QString error;
 };
 
 #endif // PSD_LAYER_SECTION_H

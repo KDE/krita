294d484164b81d9907058192351a79c1d81ed68c
Thomas Zander <zander@kde.org>
2009-12-05 10:44:08 +0000

    Add an actual implementation to the KoViewConverter
    Looking around at KOffice I noted several used the KoZoomHandler
    where they should not have. KoZoomHandler is DPI specific and thus
    will create odd results for things like pixmaps for saving.
    Next there were several re-implementation in KOffice of this one
    so the easiest to do is to just make the KoViewConverter be a
    simple zooming implementation.
    
    The misuse of the DPI based KoZoomHandler tells me we should not
    do DPI specific stuff in Flake at all. Thats unique to KOffice.
    So next step is to move that out.
    
    svn path=/trunk/koffice/; revision=1058920

---
+++ libs/flake/KoViewConverter.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2006, 2008-2009 Thomas Zander <zander@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "KoViewConverter.h"
+
+KoViewConverter::KoViewConverter()
+    : m_zoomLevel(1.0)
+{
+}
+
+QPointF KoViewConverter::documentToView(const QPointF &documentPoint) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return documentPoint;
+    return QPointF(documentToViewX(documentPoint.x()), documentToViewY(documentPoint.y()));
+}
+
+QPointF KoViewConverter::viewToDocument(const QPointF &viewPoint) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return viewPoint;
+    return QPointF(viewToDocumentX(viewPoint.x()), viewToDocumentY(viewPoint.y()));
+}
+
+QRectF KoViewConverter::documentToView(const QRectF &documentRect) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return documentRect;
+    return QRectF(documentToView(documentRect.topLeft()), documentToView(documentRect.size()));
+}
+
+QRectF KoViewConverter::viewToDocument(const QRectF &viewRect) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return viewRect;
+    return QRectF(viewToDocument(viewRect.topLeft()), viewToDocument(viewRect.size()));
+}
+
+QSizeF KoViewConverter::documentToView(const QSizeF &documentSize) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return documentSize;
+    return QSizeF(documentToViewX(documentSize.width()), documentToViewY(documentSize.height()));
+}
+
+QSizeF KoViewConverter::viewToDocument(const QSizeF &viewSize) const
+{
+    if (qFuzzyCompare(m_zoomLevel, 1))
+        return viewSize;
+    return QSizeF(viewToDocumentX(viewSize.width()), viewToDocumentY(viewSize.height()));
+}
+
+void KoViewConverter::zoom(qreal *zoomX, qreal *zoomY) const
+{
+    *zoomX = m_zoomLevel;
+    *zoomY = m_zoomLevel;
+}
+
+qreal KoViewConverter::documentToViewX(qreal documentX) const
+{
+    return documentX * m_zoomLevel;
+}
+
+qreal KoViewConverter::documentToViewY(qreal documentY) const
+{
+    return documentY * m_zoomLevel;
+}
+
+qreal KoViewConverter::viewToDocumentX(qreal viewX) const
+{
+    return viewX / m_zoomLevel;
+}
+
+qreal KoViewConverter::viewToDocumentY(qreal viewY) const
+{
+    return viewY / m_zoomLevel;
+}
+
+void KoViewConverter::setZoom(qreal zoom)
+{
+    m_zoomLevel = zoom;
+}
+
+qreal KoViewConverter::zoom() const
+{
+    return m_zoomLevel;
+}
--- libs/flake/KoViewConverter.h
+++ libs/flake/KoViewConverter.h
@@ -23,6 +23,8 @@
 #include <QtCore/QPointF>
 #include <QtCore/QRectF>
 
+#include "flake_export.h"
+
 /**
  * The interface for view conversions.
  * All KoShape based objects are using a postscript-point (pt) based measurement system
@@ -30,85 +32,97 @@
  * we are painting, and a conversion to the normalized coordinate system if we
  * receive mouse events so we can figure out which KoShape object was touched.
  */
-class KoViewConverter
+class FLAKE_EXPORT KoViewConverter
 {
 public:
-    KoViewConverter() {}
+    KoViewConverter();
     virtual ~KoViewConverter() {}
 
     /**
      * Convert a coordinate in pt to pixels.
      * @param documentPoint the point in the document coordinate system of a KoShape.
      */
-    virtual QPointF documentToView(const QPointF &documentPoint) const = 0;
+    virtual QPointF documentToView(const QPointF &documentPoint) const;
 
     /**
      * Convert a coordinate in pixels to pt.
      * @param viewPoint the point in the coordinate system of the widget, or window.
      */
-    virtual QPointF viewToDocument(const QPointF &viewPoint) const = 0;
+    virtual QPointF viewToDocument(const QPointF &viewPoint) const;
 
     /**
      * Convert a rectangle in pt to pixels.
      * @param documentRect the rect in the document coordinate system of a KoShape.
      */
-    virtual QRectF documentToView(const QRectF &documentRect) const = 0;
+    virtual QRectF documentToView(const QRectF &documentRect) const;
 
     /**
      * Convert a rectangle in pixels to pt.
      * @param viewRect the rect in the coordinate system of the widget, or window.
      */
-    virtual QRectF viewToDocument(const QRectF &viewRect) const = 0;
+    virtual QRectF viewToDocument(const QRectF &viewRect) const;
 
     /**
      * Convert a size in pt to pixels.
      * @param documentSize the size in pt.
      * @return the size in pixels.
      */
-    virtual QSizeF documentToView(const QSizeF& documentSize) const = 0;
+    virtual QSizeF documentToView(const QSizeF& documentSize) const;
 
     /**
      * Convert a size in pixels to pt.
      * @param viewSize the size in pixels.
      * @return the size in pt.
      */
-    virtual QSizeF viewToDocument(const QSizeF& viewSize) const = 0;
+    virtual QSizeF viewToDocument(const QSizeF& viewSize) const;
 
     /**
      * Convert a single x coordinate in pt to pixels.
      * @param documentX the x coordinate in pt.
      * @return the x coordinate in pixels.
      */
-    virtual qreal documentToViewX(qreal documentX) const = 0;
+    virtual qreal documentToViewX(qreal documentX) const;
 
     /**
      * Convert a single y coordinate in pt to pixels.
      * @param documentY the y coordinate in pt.
      * @return the y coordinate in pixels.
      */
-    virtual qreal documentToViewY(qreal documentY) const = 0;
+    virtual qreal documentToViewY(qreal documentY) const;
 
     /**
      * Convert a single x coordinate in pixels to pt.
      * @param viewX the x coordinate in pixels.
      * @return the x coordinate in pt.
      */
-    virtual qreal viewToDocumentX(qreal viewX) const = 0;
+    virtual qreal viewToDocumentX(qreal viewX) const;
 
     /**
      * Convert a single y coordinate in pixels to pt.
      * @param viewY the y coordinate in pixels.
      * @return the y coordinate in pt.
      */
-    virtual qreal viewToDocumentY(qreal viewY) const = 0;
+    virtual qreal viewToDocumentY(qreal viewY) const;
 
     /**
      * Retrieve the zoom levels of the individual x and y axes.
      * @param zoomX a pointer to a qreal which will be modified to the horizontal zoom.
      * @param zoomY a pointer to a qreal which will be modified to the vertical zoom.
      */
-    virtual void zoom(qreal *zoomX, qreal *zoomY) const = 0;
+    virtual void zoom(qreal *zoomX, qreal *zoomY) const;
 
+    /**
+     * Set the zoom level
+     */
+    virtual void setZoom(qreal zoom);
+
+    /**
+     * Return the current zoom level.
+     */
+    qreal zoom() const;
+
+private:
+    qreal m_zoomLevel; // 1.0 is 100%
 };
 
 #endif

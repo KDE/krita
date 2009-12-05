86232d5057575ace3d9bdf54e7106e306ff26fcc
Thomas Zander <zander@kde.org>
2009-12-05 19:18:19 +0000

    Reverts the replacement of the changecace plugin with the template.  Seems this work was not ready for comitting (or svn is too hard...)
    Strategy agreed with Cyrille
    
    svn path=/trunk/koffice/; revision=1059078

---
+++ plugins/textediting/changecase/ChangecaseFactory.cpp
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2007 Fredy Yanardi <fyanardi@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "ChangecaseFactory.h"
+#include "Changecase.h"
+
+#include <klocale.h>
+
+ChangecaseFactory::ChangecaseFactory(QObject *parent)
+    : KoTextEditingFactory(parent, "changecase")
+{
+    setShowInMenu(true);
+    setTitle(i18n ("Change Case...") );
+}
+
+KoTextEditingPlugin *ChangecaseFactory::create() const
+{
+    return new Changecase();
+}
+

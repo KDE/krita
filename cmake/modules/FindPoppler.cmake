5d1f1934aa28cabbbbf575edd21fa1e7ed41934b
Pino Toscano <pino@kde.org>
2010-01-03 19:32:47 +0000

    sync again with the newly version i committed in kdegraphics (see r1069564 there)
    svn path=/trunk/koffice/; revision=1069570

--- cmake/modules/FindPoppler.cmake
+++ cmake/modules/FindPoppler.cmake
@@ -6,7 +6,7 @@
 #  POPPLER_LIBRARY - Link this to use poppler
 #
 
-# Copyright (c) 2006-2007, Pino Toscano, <pino@kde.org>
+# Copyright (c) 2006-2010, Pino Toscano, <pino@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -18,32 +18,44 @@ if(POPPLER_INCLUDE_DIR AND POPPLER_LIBRARY)
 
 else(POPPLER_INCLUDE_DIR AND POPPLER_LIBRARY)
 
+if(NOT WIN32)
 # use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
+  include(FindPkgConfig)
+  pkg_check_modules(_pc_poppler poppler-qt4)
+  if(_pc_poppler_FOUND AND _pc_poppler_VERSION VERSION_GREATER 0.5.3)
+    set(POPPLER_FOUND TRUE)
+  endif(_pc_poppler_FOUND AND _pc_poppler_VERSION VERSION_GREATER 0.5.3)
+else(NOT WIN32)
+  # assume so, for now
+  set(POPPLER_FOUND TRUE)
+endif(NOT WIN32)
 
-INCLUDE(FindPkgConfig)
-pkg_check_modules(POPPLER poppler-qt4>=0.5.4)
+if(POPPLER_FOUND)
+  # set it back as false
+  set(POPPLER_FOUND FALSE)
 
-if(NOT POPPLER_FOUND)
-  # try to find poppler without pkgconfig
-  find_library( LIBPOPPLER poppler )
-  find_library( LIBPOPPLER_QT4 poppler-qt4 )
-  find_path( INCLUDEPOPPLER_QT4 poppler/qt4/poppler-qt4.h )
-  find_path( INCLUDEPOPPLER poppler-qt4.h PATHS ${INCLUDEPOPPLER_QT4}/poppler/qt4 )
-  if( LIBPOPPLER_QT4 AND LIBPOPPLER AND INCLUDEPOPPLER )
-    set( POPPLER_FOUND TRUE )
-    set(POPPLER_LIBRARY ${LIBPOPPLER} ${LIBPOPPLER_QT4})
-    set(POPPLER_INCLUDE_DIR ${INCLUDEPOPPLER})
-  endif()
-endif()
+  find_library(POPPLER_LIBRARY poppler-qt4
+               HINTS ${_pc_poppler_LIBRARY_DIRS}
+  )
+
+  find_path(POPPLER_INCLUDE_DIR poppler-qt4.h
+            HINTS ${_pc_poppler_INCLUDE_DIRS}
+            PATH_SUFFIXES poppler/qt4
+  )
+  find_path(POPPLER_INCLUDE_DIR_core qt4/poppler-qt4.h
+            HINTS ${_pc_poppler_INCLUDE_DIRS}
+            PATH_SUFFIXES poppler
+  )
+
+  if(POPPLER_LIBRARY AND POPPLER_INCLUDE_DIR AND POPPLER_INCLUDE_DIR_core)
+    list(APPEND POPPLER_INCLUDE_DIR "${POPPLER_INCLUDE_DIR_core}")
+    set(POPPLER_FOUND TRUE)
+  endif(POPPLER_LIBRARY AND POPPLER_INCLUDE_DIR AND POPPLER_INCLUDE_DIR_core)
+endif(POPPLER_FOUND)
 
 if (POPPLER_FOUND)
-  # the cflags for poppler-qt4 can contain more than one include path
-  separate_arguments(POPPLER_CFLAGS)
-  foreach(_includedir ${POPPLER_CFLAGS})
-    string(REGEX REPLACE "-I(.+)" "\\1" _includedir "${_includedir}")
-    set(POPPLER_INCLUDE_DIR ${POPPLER_INCLUDE_DIR} ${_includedir})
-  endforeach(_includedir)
+  INCLUDE(CheckCXXSourceCompiles)
 
   # check whether we're using poppler 0.6
   set(CMAKE_REQUIRED_INCLUDES ${POPPLER_INCLUDE_DIR} ${QT_INCLUDE_DIR})
@@ -59,16 +71,37 @@ int main()
   return 0;
 }
 " HAVE_POPPLER_0_6 )
+check_cxx_source_compiles("
+#include <poppler-qt4.h>
+#include <poppler-form.h>
+int main()
+{
+  Poppler::FormFieldButton * button = 0;
+  button->buttonType();
+  return 0;
+}
+" HAVE_POPPLER_0_8)
+check_cxx_source_compiles("
+#include <poppler-qt4.h>
+int main()
+{
+  Poppler::Document::RenderHint hint = Poppler::Document::TextHinting;
+  return 0;
+}
+" HAVE_POPPLER_0_12_1)
   set(CMAKE_REQUIRED_INCLUDES)
   set(CMAKE_REQUIRED_LIBRARIES)
-  if (HAVE_POPPLER_0_6)
-    set(poppler06Message "yes")
-  else (HAVE_POPPLER_0_6)
-    set(poppler06Message "no")
-  endif (HAVE_POPPLER_0_6)
-
+  if (HAVE_POPPLER_0_12_1)
+    set(popplerVersionMessage "0.12.1")
+  elseif (HAVE_POPPLER_0_8)
+    set(popplerVersionMessage "0.8")
+  elseif (HAVE_POPPLER_0_6)
+    set(popplerVersionMessage "0.6")
+  else (HAVE_POPPLER_0_12_1)
+    set(popplerVersionMessage "0.5.4")
+  endif (HAVE_POPPLER_0_12_1)
   if (NOT Poppler_FIND_QUIETLY)
-    message(STATUS "Found Poppler-Qt4: ${POPPLER_LIBRARY}, 0.6.x? ${poppler06Message}")
+    message(STATUS "Found Poppler-Qt4: ${POPPLER_LIBRARY}, (>= ${popplerVersionMessage})")
   endif (NOT Poppler_FIND_QUIETLY)
 else (POPPLER_FOUND)
   if (Poppler_FIND_REQUIRED)
@@ -80,6 +113,8 @@ endif (POPPLER_FOUND)
 # ensure that they are cached
 set(POPPLER_INCLUDE_DIR ${POPPLER_INCLUDE_DIR} CACHE INTERNAL "The Poppler-Qt4 include path")
 set(POPPLER_LIBRARY ${POPPLER_LIBRARY} CACHE INTERNAL "The Poppler-Qt4 library")
-set(HAVE_POPPLER_0_6 ${HAVE_POPPLER_0_6} CACHE INTERNAL "Whether the version of Poppler-Qt4 is 0.6")
+set(HAVE_POPPLER_0_6 ${HAVE_POPPLER_0_6} CACHE INTERNAL "Whether the version of Poppler-Qt4 is >= 0.6")
+set(HAVE_POPPLER_0_8 ${HAVE_POPPLER_0_8} CACHE INTERNAL "Whether the version of Poppler-Qt4 is >= 0.8")
+set(HAVE_POPPLER_0_12_1 ${HAVE_POPPLER_0_12_1} CACHE INTERNAL "Whether the version of Poppler-Qt4 is >= 0.12.1")
 
 endif(POPPLER_INCLUDE_DIR AND POPPLER_LIBRARY)

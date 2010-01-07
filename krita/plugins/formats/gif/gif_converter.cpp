fb5f329c3576264b09ec15683a8aa2ac7b3cb440
Boudewijn Rempt <boud@valdyas.org>
2010-01-07 20:23:41 +0000

    fix copyright lines
    svn path=/trunk/koffice/; revision=1071294

--- krita/plugins/formats/gif/gif_converter.cpp
+++ krita/plugins/formats/gif/gif_converter.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2009-2010 Boudewijn Rempt <boud@valdyas.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- krita/plugins/formats/gif/gif_converter.h
+++ krita/plugins/formats/gif/gif_converter.h
@@ -1,8 +1,5 @@
 /*
- * Copyright (c) 1990-1993 David Koblas <koblas@extra.com>
- * Copyright (c) 2003 Adam D. Moss <adam@gimp.org>
- *
- *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2009-2010 Boudewijn Rempt <boud@valdyas.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

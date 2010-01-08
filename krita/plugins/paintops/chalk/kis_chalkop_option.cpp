460696fa2f2db0f098b8a010dba6731f7cd59b14
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-01-08 15:39:26 +0000

    Fix the preview of the chalk and update the properties too.
    svn path=/trunk/koffice/; revision=1071719

--- krita/plugins/paintops/chalk/kis_chalkop_option.cpp
+++ krita/plugins/paintops/chalk/kis_chalkop_option.cpp
@@ -38,6 +38,9 @@ KisChalkOpOption::KisChalkOpOption()
     m_checkable = false;
     m_options = new KisChalkOpOptionsWidget();
     connect(m_options->radiusSpinBox, SIGNAL(valueChanged(int)), SIGNAL(sigSettingChanged()));
+    connect(m_options->inkDepletionCHBox, SIGNAL(clicked(bool)), SIGNAL(sigSettingChanged()));
+    connect(m_options->opacity, SIGNAL(clicked(bool)), SIGNAL(sigSettingChanged()));
+    connect(m_options->saturation, SIGNAL(clicked(bool)), SIGNAL(sigSettingChanged()));
 
     setConfigurationPage(m_options);
 }

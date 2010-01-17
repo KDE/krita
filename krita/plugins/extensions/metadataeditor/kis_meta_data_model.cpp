d42c956d5cbfe9a0fbc06cabc9417689a4141c2c
Cyrille Berger <cyb@lepi.org>
2010-01-17 23:30:12 +0000

    add the type column --This line, and those below will be ignored--
    M    metadataeditor/kis_meta_data_model.cpp
    
    svn path=/trunk/koffice/; revision=1076297

--- krita/plugins/extensions/metadataeditor/kis_meta_data_model.cpp
+++ krita/plugins/extensions/metadataeditor/kis_meta_data_model.cpp
@@ -38,7 +38,7 @@ int KisMetaDataModel::rowCount(const QModelIndex &parent) const
 int KisMetaDataModel::columnCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent);
-    return 2;
+    return 3;
 }
 
 QVariant KisMetaDataModel::data(const QModelIndex &index, int role) const
@@ -52,7 +52,41 @@ QVariant KisMetaDataModel::data(const QModelIndex &index, int role) const
         switch (index.column()) {
         case 0:
             return m_store->keys()[index.row()];
-        case 1:
+        case 1: {
+            KisMetaData::Value::ValueType vt = m_store->entries()[index.row()].value().type();
+            switch (vt) {
+            case KisMetaData::Value::Invalid:
+                return i18n("Invalid");
+            case KisMetaData::Value::Variant: {
+                int vt = m_store->entries()[index.row()].value().asVariant().type();
+                switch (vt) {
+                case QVariant::Date:
+                case QVariant::DateTime:
+                    return i18n("Date");
+                case QVariant::Double:
+                case QVariant::Int:
+                    return i18n("Number");
+                case QVariant::String:
+                    return i18n("String");
+                default:
+                    return i18n("Variant (%1)", vt);
+                }
+            }
+            case KisMetaData::Value::OrderedArray:
+                return i18n("Ordered array");
+            case KisMetaData::Value::UnorderedArray:
+                return i18n("Unordered array");
+            case KisMetaData::Value::AlternativeArray:
+                return i18n("Alternative array");
+            case KisMetaData::Value::LangArray:
+                return i18n("Language array");
+            case KisMetaData::Value::Structure:
+                return i18n("Structure");
+            case KisMetaData::Value::Rational:
+                return i18n("Rational");
+            }
+        }
+        case 2:
             return m_store->entries()[index.row()].value().asVariant();
         }
     }
@@ -64,11 +98,13 @@ QVariant KisMetaDataModel::data(const QModelIndex &index, int role) const
 QVariant KisMetaDataModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
     if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
-        Q_ASSERT(section < 2);
+        Q_ASSERT(section < 3);
         switch (section) {
         case 0:
             return i18n("Key");
         case 1:
+            return i18n("Type");
+        case 2:
             return i18n("Value");
         }
     }

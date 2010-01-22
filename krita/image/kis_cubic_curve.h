ffb5779cd05bf87c15fce6e1d7157776f3fb350b
Cyrille Berger <cyb@lepi.org>
2010-01-22 23:17:12 +0000

    add a KisCubicCurve class that will countains data and algorithms for transvert cubic curves
    svn path=/trunk/koffice/; revision=1078828

---
+++ krita/image/kis_cubic_curve.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2010 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _KIS_CUBIC_CURVE_H_
+#define _KIS_CUBIC_CURVE_H_
+
+#include<QList>
+#include<QVector>
+
+class QPointF;
+
+/**
+ * Hold the data for a cubic curve.
+ */
+class KisCubicCurve
+{
+public:
+    KisCubicCurve();
+    KisCubicCurve(const QList<QPointF>& points);
+    KisCubicCurve(const QVector<QPointF>& points);
+public:
+    qreal value(qreal x) const;
+private:
+    struct Private;
+    Private* const d;
+};
+
+#endif

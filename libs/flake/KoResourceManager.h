e33110012d2a25201bbc77578268bf4c33acc0dd
Thomas Zander <zander@kde.org>
2010-01-23 20:18:11 +0000

    Update API docs after introducing a second namespace for enums
    svn path=/trunk/koffice/; revision=1079195

--- libs/flake/KoResourceManager.h
+++ libs/flake/KoResourceManager.h
@@ -141,36 +141,41 @@ public:
 
     /**
      * Set a resource of any type.
-     * @param key the integer key, based on KoCanvasResource::CanvasResource
+     * @param key the integer key
      * @param value the new value for the key.
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void setResource(int key, const QVariant &value);
 
     /**
      * Set a resource of type KoColor.
-     * @param key the integer key, based on KoCanvasResource::CanvasResource
+     * @param key the integer key
      * @param color the new value for the key.
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void setResource(int key, const KoColor &color);
 
     /**
      * Set a resource of type KoShape*.
-     * @param key the integer key, based on KoCanvasResource::CanvasResource
+     * @param key the integer key
      * @param id the new value for the key.
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void setResource(int key, KoShape *shape);
 
     /**
      * Set a resource of type KoUnit
-     * @param key the integer key, based on KoCanvasResource::CanvasResource
+     * @param key the integer key
      * @param id the new value for the key.
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void setResource(int key, const KoUnit &unit);
 
     /**
      * Returns a qvariant containing the specified resource or a standard one if the
      * specified resource does not exist.
-     * @param key the key, based on KoCanvasResource::CanvasResource.
+     * @param key the key
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     QVariant resource(int key) const;
 
@@ -221,62 +226,72 @@ public:
 
     /**
      * Return the resource determined by param key as a boolean.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     bool boolResource(int key) const;
 
     /**
      * Return the resource determined by param key as an integer.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     int intResource(int key) const;
 
     /**
      * Return the resource determined by param key as a qreal.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     qreal doubleResource(int key) const;
 
     /**
      * Return the resource determined by param key as a KoColor.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     KoColor koColorResource(int key) const;
 
     /**
      * Return the resource determined by param key as a pointer to a KoShape.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     KoShape *koShapeResource(int key) const;
 
     /**
      * Return the resource determined by param key as a QString .
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     QString stringResource(int key) const;
 
     /**
      * Return the resource determined by param key as a QSizeF.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     QSizeF sizeResource(int key) const;
 
     /**
      * Return the resource determined by param key as a KoUnit.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     KoUnit unitResource(int key) const;
 
     /**
      * Returns true if there is a resource set with the requested key.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the indentifying key for the resource
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     bool hasResource(int key) const;
 
     /**
      * Remove the resource with @p key from the provider.
-     * @param key the key that will be used to remove the resource, based on KoCanvasResource::CanvasResource..
+     * @param key the key that will be used to remove the resource
      * There will be a signal emitted with a variable that will return true on QVariable::isNull();
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void clearResource(int key);
 
@@ -293,8 +308,9 @@ signals:
     /**
      * This signal is emitted every time a resource is set that is either
      * new or different from the previous set value.
-     * @param key the indentifying key for the resource, based on KoCanvasResource::CanvasResource.
+     * @param key the indentifying key for the resource
      * @param value the variants new value.
+     * @see KoCanvasResource::CanvasResource KoDocumentResource::DocumentResource
      */
     void resourceChanged(int key, const QVariant &value);
 

8c2e1d7c874d0a2c80f526bda683bc21d4a6f68d
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-01-25 10:37:25 +0000

    Benchmark for KisHLineIterator added.
    svn path=/trunk/koffice/; revision=1079965

---
+++ krita/benchmarks/kis_hline_iterator_benchmark.cpp
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2010 Lukáš Tvrdý lukast.dev@gmail.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_hline_iterator_benchmark.h"
+
+#include "kis_iterators_pixel.h"
+#include "kis_paint_device.h"
+
+#include <KoColorSpace.h>
+#include <KoColorSpaceRegistry.h>
+#include <KoColor.h>
+
+#include <qtest_kde.h>
+
+#define TEST_IMAGE_WIDTH 4096
+#define TEST_IMAGE_HEIGHT 4096
+
+void KisHLineIteratorBenchmark::initTestCase()
+{
+    m_colorSpace = KoColorSpaceRegistry::instance()->rgb8();
+    m_device = new KisPaintDevice(m_colorSpace);
+    m_color = new KoColor(m_colorSpace);
+    // some random color
+    m_color->fromQColor(QColor(0,120,250));
+    m_device->fill(0,0,TEST_IMAGE_WIDTH,TEST_IMAGE_HEIGHT,m_color->data());
+}
+
+void KisHLineIteratorBenchmark::cleanupTestCase()
+{
+    delete m_color;
+    delete m_device;
+}
+
+
+void KisHLineIteratorBenchmark::benchmarkCreation()
+{
+    QBENCHMARK{
+        KisHLineIteratorPixel it = m_device->createHLineIterator(0, 0, TEST_IMAGE_WIDTH);
+    }
+}
+
+void KisHLineIteratorBenchmark::benchmarkWriteBytes()
+{
+    QBENCHMARK{
+        KisHLineIteratorPixel it = m_device->createHLineIterator(0, 0, TEST_IMAGE_WIDTH);
+        for (int j = 0; j < TEST_IMAGE_HEIGHT; j++) {
+            while (!it.isDone()) {
+                memcpy(it.rawData(), m_color->data(), m_colorSpace->pixelSize());
+                ++it;
+            }
+            it.nextRow();
+        }
+    }
+}
+
+void KisHLineIteratorBenchmark::benchmarkReadBytes()
+{
+    QBENCHMARK{
+        KisHLineIteratorPixel it = m_device->createHLineIterator(0, 0, TEST_IMAGE_WIDTH);
+        for (int j = 0; j < TEST_IMAGE_HEIGHT; j++) {
+            while (!it.isDone()) {
+                memcpy(m_color->data(), it.rawData(), m_colorSpace->pixelSize());
+                ++it;
+            }
+            it.nextRow();
+        }
+    }
+}
+
+
+void KisHLineIteratorBenchmark::benchmarkConstReadBytes()
+{
+    QBENCHMARK{
+        KisHLineConstIteratorPixel cit = m_device->createHLineConstIterator(0, 0, TEST_IMAGE_WIDTH);
+        for (int j = 0; j < TEST_IMAGE_HEIGHT; j++) {
+            while (!cit.isDone()) {
+                memcpy(m_color->data(), cit.rawData(), m_colorSpace->pixelSize());
+                ++cit;
+            }
+            cit.nextRow();
+        }
+    }
+}
+
+
+
+QTEST_KDEMAIN(KisHLineIteratorBenchmark, GUI)
+#include "kis_hline_iterator_benchmark.moc"

69bf2be510b055aae57ccb45b64e9b7c9e46cf14
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-01-28 16:39:43 +0000

    Runs all the tests as batch for random iterator. TODO: parse output to make automatic results.
    svn path=/trunk/koffice/; revision=1081530

---
+++ krita/benchmarks/runAllRandomTests.sh
@@ -0,0 +1,45 @@
+#iterators
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkWriteBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkWriteBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkWriteBytes
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadBytes  
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadBytes  
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadBytes  
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstReadBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstReadBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstReadBytes
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadWriteBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadWriteBytes
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkReadWriteBytes
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkNoMemCpy   
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkNoMemCpy   
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkNoMemCpy   
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstNoMemCpy
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstNoMemCpy
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkConstNoMemCpy
+
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTwoIteratorsNoMemCpy
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTwoIteratorsNoMemCpy 
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTwoIteratorsNoMemCpy  
+
+# own
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTileByTileWrite
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTileByTileWrite
+./KisRandomIteratorBenchmark -silent -iterations 10 benchmarkTileByTileWrite
+# those takes too long 
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandom
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandom
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandom
+
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandomConst
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandomConst
+./KisRandomIteratorBenchmark -silent -iterations 1 benchmarkTotalRandomConst
+
+    
+
+

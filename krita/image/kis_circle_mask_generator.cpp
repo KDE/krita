5df7aeb7c83a427fe7fa50335a11fdc599d7ebec
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-02-01 11:27:38 +0000

    Precompute the spikes angle and save it to d->pointer Rename m_ prefix from d->pointer members.
    svn path=/trunk/koffice/; revision=1083508

--- krita/image/kis_circle_mask_generator.cpp
+++ krita/image/kis_circle_mask_generator.cpp
@@ -22,26 +22,28 @@
 #include <QDomDocument>
 
 struct KisCircleMaskGenerator::Private {
-    double m_xcoef, m_ycoef;
-    double m_xfadecoef, m_yfadecoef;
+    double xcoef, ycoef;
+    double xfadecoef, yfadecoef;
+    double cachedSpikesAngle;
 };
 
 KisCircleMaskGenerator::KisCircleMaskGenerator(double w, double h, double fh, double fv)
         : KisMaskGenerator(w, h, 0.5 * w - fh, 0.5 * h - fv, CIRCLE), d(new Private)
 {
-    d->m_xcoef = 2.0 / w;
-    d->m_ycoef = 2.0 / h;
-    d->m_xfadecoef = (KisMaskGenerator::d->m_fh == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fh * width()));
-    d->m_yfadecoef = (KisMaskGenerator::d->m_fv == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fv * height()));
+    d->xcoef = 2.0 / w;
+    d->ycoef = 2.0 / h;
+    d->xfadecoef = (KisMaskGenerator::d->m_fh == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fh * width()));
+    d->yfadecoef = (KisMaskGenerator::d->m_fv == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fv * height()));
 }
 
 KisCircleMaskGenerator::KisCircleMaskGenerator(double radius, double ratio, double fh, double fv, int spikes)
         : KisMaskGenerator(radius, ratio, fh, fv, spikes, CIRCLE), d(new Private)
 {
-    d->m_xcoef = 2.0 / width();
-    d->m_ycoef = 2.0 / (KisMaskGenerator::d->m_ratio * width());
-    d->m_xfadecoef = (KisMaskGenerator::d->m_fh == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fh * width()));
-    d->m_yfadecoef = (KisMaskGenerator::d->m_fv == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fv * KisMaskGenerator::d->m_ratio * width()));
+    d->xcoef = 2.0 / width();
+    d->ycoef = 2.0 / (KisMaskGenerator::d->m_ratio * width());
+    d->xfadecoef = (KisMaskGenerator::d->m_fh == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fh * width()));
+    d->yfadecoef = (KisMaskGenerator::d->m_fv == 0) ? 1 : (1.0 / (KisMaskGenerator::d->m_fv * KisMaskGenerator::d->m_ratio * width()));
+    d->cachedSpikesAngle = M_PI / KisMaskGenerator::d->m_spikes;
 }
 
 KisCircleMaskGenerator::~KisCircleMaskGenerator()
@@ -58,22 +60,22 @@ quint8 KisCircleMaskGenerator::valueAt(double x, double y) const
     if (KisMaskGenerator::d->m_spikes > 2) {
         double angle = (atan2(yr, xr));
 
-        while (angle > M_PI / KisMaskGenerator::d->m_spikes) {
+        while (angle > d->cachedSpikesAngle ){
             double sx = xr, sy = yr;
 
             xr = KisMaskGenerator::d->cs * sx - KisMaskGenerator::d->ss * sy;
             yr = KisMaskGenerator::d->ss * sx + KisMaskGenerator::d->cs * sy;
 
-            angle -= 2 * M_PI / KisMaskGenerator::d->m_spikes;
+            angle -= 2 * d->cachedSpikesAngle;
         }
     }
 
-    double n = norme(xr * d->m_xcoef, yr * d->m_ycoef);
+    double n = norme(xr * d->xcoef, yr * d->ycoef);
 
     if (n > 1) {
         return 255;
     } else {
-        double normeFade = norme(xr * d->m_xfadecoef, yr * d->m_yfadecoef);
+        double normeFade = norme(xr * d->xfadecoef, yr * d->yfadecoef);
         if (normeFade > 1) {
             double xle, yle;
             // xle stands for x-coordinate limit exterior
@@ -82,15 +84,15 @@ quint8 KisCircleMaskGenerator::valueAt(double x, double y) const
             // the fade value
             if (xr == 0) {
                 xle = 0;
-                yle = yr > 0 ? 1 / d->m_ycoef : -1 / d->m_ycoef;
+                yle = yr > 0 ? 1 / d->ycoef : -1 / d->ycoef;
             } else {
                 double c = yr / (double)xr;
-                xle = sqrt(1 / norme(d->m_xcoef, c * d->m_ycoef));
+                xle = sqrt(1 / norme(d->xcoef, c * d->ycoef));
                 xle = xr > 0 ? xle : -xle;
                 yle = xle * c;
             }
             // On the internal limit of the fade area, normeFade is equal to 1
-            double normeFadeLimitE = norme(xle * d->m_xfadecoef, yle * d->m_yfadecoef);
+            double normeFadeLimitE = norme(xle * d->xfadecoef, yle * d->yfadecoef);
             return (uchar)(255 *(normeFade - 1) / (normeFadeLimitE - 1));
         } else {
             return 0;

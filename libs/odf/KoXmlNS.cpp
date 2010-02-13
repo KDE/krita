d52e79e63371f3d6a817fa09d97adba97f4f1935
Casper Boemann <cbr@boemann.dk>
2010-02-13 19:40:41 +0000

    Add anim namesspace to the list of namespaces
    svn path=/trunk/koffice/; revision=1089711

--- libs/odf/KoXmlNS.cpp
+++ libs/odf/KoXmlNS.cpp
@@ -33,6 +33,7 @@ const char* const KoXmlNS::script = "urn:oasis:names:tc:opendocument:xmlns:scrip
 const char* const KoXmlNS::style = "urn:oasis:names:tc:opendocument:xmlns:style:1.0";
 const char* const KoXmlNS::number = "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0";
 const char* const KoXmlNS::manifest = "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0";
+const char* const KoXmlNS::anim = "urn:oasis:names:tc:opendocument:xmlns:animation:1.0";
 
 const char* const KoXmlNS::math = "http://www.w3.org/1998/Math/MathML";
 const char* const KoXmlNS::svg = "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0";
--- libs/odf/KoXmlNS.h
+++ libs/odf/KoXmlNS.h
@@ -41,6 +41,7 @@ public:
     static const char* const style;
     static const char* const number;
     static const char* const manifest;
+    static const char* const anim;
 
     static const char* const math;
     static const char* const svg;

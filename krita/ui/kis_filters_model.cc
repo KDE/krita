aa7397eaa6ef3e743f3c58689353b495c5a64541
Cyrille Berger <cyb@lepi.org>
2010-02-22 21:52:55 +0000

    add some debug output
    svn path=/trunk/koffice/; revision=1094527

--- krita/ui/kis_filters_model.cc
+++ krita/ui/kis_filters_model.cc
@@ -172,12 +172,14 @@ QVariant KisFiltersModel::data(const QModelIndex &index, int role) const
                     KisPaintDeviceSP target = new KisPaintDevice(*d->thumb);
 
                     QRect rc = target->exactBounds();
-
+                    dbgUI << "Previewing " << filter->name << " on: " << rc;
                     KisConstProcessingInformation cpi(d->thumb, rc.topLeft());
                     KisProcessingInformation cp(target, rc.topLeft());
 
                     filter->filter->process(cpi, cp, rc.size()/*QSize(100, 100)*/, filter->filter->defaultConfiguration(d->thumb));
+                    dbgUI << "Resulting in: " << target->exactBounds();
                     d->previewCache[ filter->filter ] = target->convertToQImage(0);
+                    dbgUI << "And in a qimage of size: " << d->previewCache[ filter->filter ].rect();
                 }
                 return d->previewCache[ filter->filter ];
             } else {

24932446e72bb24edf42f49058b58f313e1e4ca8
Boudewijn Rempt <boud@valdyas.org>
2010-03-01 14:26:51 +0000

    Add base class for a scratchpad canvas
    svn path=/trunk/koffice/; revision=1097501

---
+++ krita/ui/widgets/kis_scratch_pad.cpp
@@ -0,0 +1,151 @@
+/* This file is part of the KDE project
+ * Copyright 2010 (C) Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#include "kis_scratch_pad.h"
+#include <QRect>
+#include <QPaintEvent>
+#include <kis_paint_device.h>
+
+KisScratchPad::KisScratchPad(QObject *parent)
+    : m_toolMode(HOVERING)
+    , m_colorSpace(0)
+    , m_backgroundMode(SOLID_COLOR)
+    , m_backgroundColor(Qt::white)
+    , m_dirty(false)
+{
+}
+
+KisScratchPad::~KisScratchPad() {
+
+}
+
+
+void KisScratchPad::setPaintColor(const KoColor& foregroundColor) {
+
+    m_foregroundColor = foregroundColor;
+}
+
+void KisScratchPad::setPreset(KisPaintOpPresetSP preset) {
+
+    m_preset = preset;
+}
+
+void KisScratchPad::setBackgroundColor(const KoColor& backgroundColor) {
+
+    m_backgroundColor = backgroundColor;
+}
+
+void KisScratchPad::setBackgroundTile(const QImage& tile) {
+
+    m_backgroundTile = tile;
+}
+
+void KisScratchPad::setColorSpace(const KoColorSpace *colorSpace) {
+
+    m_colorSpace = colorSpace;
+    m_paintDevice = new KisPaintDevice(colorSpace, "ScratchPad");
+    clear();
+}
+
+void KisScratchPad::clear() {
+
+    if (m_paintDevice) {
+        switch(m_backgroundMode) {
+        case TILED:
+        case STRETCHED:
+        case CENTERED:
+        case GRADIENT:
+        case SOLID_COLOR:
+        default:
+            KoColor c;
+            c.fromQColor(m_backgroundColor);
+            m_paintDevice->fill(0, 0, width(), height(), c.data());
+        }
+    }
+    update();
+}
+
+void KisScratchPad::contextMenuEvent ( QContextMenuEvent * event ) {
+
+}
+
+void KisScratchPad::keyPressEvent ( QKeyEvent * event ) {
+
+}
+
+void KisScratchPad::keyReleaseEvent ( QKeyEvent * event ) {
+
+}
+
+void KisScratchPad::mouseDoubleClickEvent ( QMouseEvent * event ) {
+
+}
+
+void KisScratchPad::mouseMoveEvent ( QMouseEvent * event ) {
+
+}
+
+void KisScratchPad::mousePressEvent ( QMouseEvent * event ) {
+
+}
+
+void KisScratchPad::mouseReleaseEvent ( QMouseEvent * event ) {
+
+}
+
+void KisScratchPad::moveEvent ( QMoveEvent * event ) {
+
+}
+
+void KisScratchPad::paintEvent ( QPaintEvent * event ) {
+
+    if (m_colorSpace == 0 || m_paintDevice == 0) {
+        return;
+    }
+    QRect rc = event->rect();
+    if (m_dirty) {
+
+        QPainter gc(m_image);
+        gc.drawImage(rc, m_paintDevice->convertToQImage(0,
+                                                        rc.x() + m_offset.x(),
+                                                        rc.y() + m_offset.y(),
+                                                        rc.width(),
+                                                        rc.height()));
+        gc.end();
+    }
+    QPainter gc(this);
+    gc.drawImage(rc, m_image, rc);
+    gc.end();
+}
+
+void KisScratchPad::resizeEvent ( QResizeEvent * event ) {
+
+    m_image = QImage(size(), QImage::Format_ARGB32);
+    QPainter p(&m_image);
+    p.fillRect(m_image.rect(), Qt::red);
+    p.end();
+    update();
+}
+
+void KisScratchPad::tabletEvent ( QTabletEvent * event ) {
+
+}
+
+void KisScratchPad::wheelEvent ( QWheelEvent * event ) {
+
+}
---
+++ krita/ui/widgets/kis_scratch_pad.h
@@ -0,0 +1,95 @@
+/* This file is part of the KDE project
+ * Copyright 2010 (C) Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef KIS_SCRATCH_PAD_H
+#define KIS_SCRATCH_PAD_H
+
+#include <QFrame>
+#include <QImage>
+
+#include <KoColor.h>
+
+#include <kis_types.h>
+/**
+ * A scratchpad is a painting canvas with only one zoomlevel and based on
+ * a paint device, not on a KisImage. It can have a blank, tiled background or
+ * a gradient background.
+ */
+class KisScratchPad : public QWidget
+{
+
+    Q_OBJECT
+
+public:
+
+    enum BackgroundMode {
+        TILED,
+        STRETCHED,
+        CENTERED,
+        GRADIENT,
+        SOLID_COLOR
+    };
+
+    KisScratchPad(QObject *parent);
+    virtual ~KisScratchPad();
+
+public slots:
+
+    void setPaintColor(const KoColor& foregroundColor);
+    void setPreset(KisPaintOpPresetSP preset);
+    void setBackgroundColor(const KoColor& backgroundColor);
+    void setBackgroundTile(const QImage& tile);
+    void setColorSpace(const KoColorSpace* colorSpace);
+    void clear();
+
+protected:
+
+    virtual void contextMenuEvent ( QContextMenuEvent * event );
+    virtual void keyPressEvent ( QKeyEvent * event );
+    virtual void keyReleaseEvent ( QKeyEvent * event );
+    virtual void mouseDoubleClickEvent ( QMouseEvent * event );
+    virtual void mouseMoveEvent ( QMouseEvent * event );
+    virtual void mousePressEvent ( QMouseEvent * event );
+    virtual void mouseReleaseEvent ( QMouseEvent * event );
+    virtual void moveEvent ( QMoveEvent * event );
+    virtual void paintEvent ( QPaintEvent * event );
+    virtual void resizeEvent ( QResizeEvent * event );
+    virtual void tabletEvent ( QTabletEvent * event );
+    virtual void wheelEvent ( QWheelEvent * event );
+
+private:
+
+    enum Mode {
+        PAINTING,
+        HOVERING,
+        PANNING
+    };
+
+    QPoint m_offset;
+    const KoColorSpace* m_colorSpace;
+    KoColor m_foregroundColor;
+    QImage m_backgroundTile;
+    QColor m_backgroundColor;
+    Mode m_toolMode;
+    QImage m_image;
+    KisPaintDeviceSP m_paintDevice;
+    KisPaintOpPresetSP m_preset;
+    BackgroundMode m_backgroundMode = SOLID_COLOR;
+};
+
+#endif // KIS_SCRATCH_PAD_H

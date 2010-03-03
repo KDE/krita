661eb10b2ba9f7155f053d1aadabdc3c80b49044
Boudewijn Rempt <boud@valdyas.org>
2010-03-03 20:45:40 +0000

    some more work on KisSlider -- but Sven is taking over
    svn path=/trunk/koffice/; revision=1098522

--- krita/ui/widgets/kis_slider.cpp
+++ krita/ui/widgets/kis_slider.cpp
@@ -17,8 +17,230 @@
  */
 #include "kis_slider.h"
 
+#include <QAccessible>
+#include <QApplication>
+#include <QEvent>
+#include <QPainter>
+#include <QStyle>
+#include <QStyleOption>
+#include <QResizeEvent>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QWidget>
+#include <QtGlobal>
+#include <QBasicTimer>
+#include <QTabletEvent>
+#include <QTimerEvent>
+#include <QWheelEvent>
+
+class KisSliderPrivate
+{
+public:
+
+    KisSliderPrivate()
+        : minimum(0)
+        , maximum(99)
+        , pageStep(10)
+        , value(5)
+        , position(5)
+        , pressValue(-1)
+        , singleStep(1)
+        , offsetAccumulated(0)
+        , tracking(true)
+        , blocktracking(false)
+        , pressed(false)
+        , repeatAction(KisSlider::SliderNoAction)
+    {}
+
+    double minimum;
+    double maximum;
+    double pageStep;
+    double value;
+    double position;
+    double pressValue;
+    double singleStep;
+    float offsetAccumulated;
+
+    bool tracking;
+    bool blocktracking;
+    bool pressed;
+
+    QBasicTimer repeatActionTimer;
+    int repeatActionTime;
+    KisSlider::SliderAction repeatAction;
+
+};
+
+
+
 
 KisSlider::KisSlider(QWidget *parent)
-    : QAbstractSlider(parent)
+    : QWidget(parent)
 {
+    setFocusPolicy(Qt::WheelFocus);
 }
+
+KisSlider::~KisSlider() {
+}
+
+double KisSlider::minimum() {
+
+}
+
+void KisSlider::setMinimum(double value) {
+
+}
+
+double KisSlider::maximum() {
+
+}
+
+void KisSlider::setMaximum(double value) {
+
+}
+
+void KisSlider::setRange(double minimum, double maximum) {
+
+}
+
+double KisSlider::singleStep() {
+
+}
+
+void KisSlider::setSingleStep(double value) {
+
+}
+
+void KisSlider::setPageStep(double) {
+
+}
+
+double KisSlider::pageStep() const {
+
+}
+
+void KisSlider::setTracking(bool enable) {
+
+}
+
+bool KisSlider::hasTracking() const {
+
+}
+
+void KisSlider::setSliderDown(bool) {
+
+}
+
+bool KisSlider::isSliderDown() const {
+
+}
+
+void KisSlider::setSliderPosition(int) {
+
+}
+
+int KisSlider::sliderPosition() const {
+
+}
+
+int KisSlider::decimals() const {
+
+}
+
+void KisSlider::setDecimals(int prec) {
+
+}
+
+double KisSlider::value() {
+
+}
+
+void KisSlider::triggerAction(SliderAction action) {
+
+}
+
+
+QSize KisSlider::sizeHint() const {
+
+}
+
+QSize KisSlider::minimumSizeHint() const {
+
+}
+
+
+
+bool KisSlider::event(QEvent *event) {
+
+    QWidget::event(event);
+}
+
+void KisSlider::keyReleaseEvent(QKeyEvent *event) {
+
+}
+
+void KisSlider::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+}
+
+
+void KisSlider::paintEvent(QPaintEvent *event) {
+
+    QStyleOptionFrame frameOption;
+    frameOption.initFrom(this);
+
+    QStyleOptionSlider sliderOption;
+    sliderOption.initFrom(this);
+
+    QStyleOptionSpinBox spinboxOption;
+    spinboxOption.initFrom(this);
+}
+
+void KisSlider::mousePressEvent(QMouseEvent *event) {
+    QWidget::mousePressEvent(event);
+}
+
+void KisSlider::mouseReleaseEvent(QMouseEvent *event) {
+    QWidget::mouseReleaseEvent(event);
+}
+
+void KisSlider::mouseMoveEvent(QMouseEvent *event) {
+    QWidget::event(event);
+}
+
+void KisSlider::tabletEvent(QTabletEvent* event) {
+
+}
+
+void KisSlider::wheelEvent(QWheelEvent *e) {
+
+}
+
+void KisSlider::timerEvent(QTimerEvent *event) {
+
+}
+
+void KisSlider::changeEvent(QEvent *event) {
+
+}
+
+QValidator::State KisSlider::validate(QString &input, int &pos) const {
+
+}
+
+double KisSlider::valueFromText(const QString &text) const{
+
+}
+
+QString KisSlider::textFromValue(double val) const {
+
+}
+
+void KisSlider::fixup(QString &str) const {
+
+}
+
+void KisSlider::setValue(double value) {
+
+}
+
--- krita/ui/widgets/kis_slider.h
+++ krita/ui/widgets/kis_slider.h
@@ -18,9 +18,14 @@
 #ifndef KIS_SLIDER_H
 #define KIS_SLIDER_H
 
-#include <QAbstractSlider>
+#include <QWidget>
+#include <QValidator>
 
 #include "krita_export.h"
+
+class QStyleOptionSlider;
+
+class KisSliderPrivate;
 
 /**
  * A tablet-friendly slider implementation
@@ -33,14 +38,112 @@
  * - allow manual input of numbers
  * - no drop-down
  */
-class KRITAUI_EXPORT KisSlider : public QAbstractSlider
+class KRITAUI_EXPORT KisSlider : public QWidget
 {
 
     Q_OBJECT
 
+    Q_PROPERTY(double minimum READ minimum WRITE setMinimum)
+    Q_PROPERTY(double maximum READ maximum WRITE setMaximum)
+    Q_PROPERTY(double singleStep READ singleStep WRITE setSingleStep)
+    Q_PROPERTY(double pageStep READ pageStep WRITE setPageStep)
+    Q_PROPERTY(double sliderPosition READ sliderPosition WRITE setSliderPosition NOTIFY sliderMoved)
+    Q_PROPERTY(bool tracking READ hasTracking WRITE setTracking)
+    Q_PROPERTY(bool sliderDown READ isSliderDown WRITE setSliderDown DESIGNABLE false)
+    Q_PROPERTY(double value READ value WRITE setValue NOTIFY valueChanged USER true)
+    Q_PROPERTY(int decimals READ decimals WRITE setDecimals)
+
 public:
 
-    KisSlider(QWidget* parent = 0);
+    explicit KisSlider(QWidget* parent = 0);
+    ~KisSlider();
+
+    double minimum();
+    void setMinimum(double value);
+
+    double maximum();
+    void setMaximum(double value);
+
+    void setRange(double minimum, double maximum);
+
+    double singleStep();
+    void setSingleStep(double value);
+
+    void setPageStep(double);
+    double pageStep() const;
+
+    void setTracking(bool enable);
+    bool hasTracking() const;
+
+    void setSliderDown(bool);
+    bool isSliderDown() const;
+
+    void setSliderPosition(int);
+    int sliderPosition() const;
+
+    int decimals() const;
+    void setDecimals(int prec);
+
+    enum SliderAction {
+           SliderNoAction,
+           SliderSingleStepAdd,
+           SliderSingleStepSub,
+           SliderPageStepAdd,
+           SliderPageStepSub,
+           SliderToMinimum,
+           SliderToMaximum,
+           SliderMove
+       };
+
+    double value();
+
+    void triggerAction(SliderAction action);
+
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
+
+
+    bool event(QEvent *event);
+
+protected:
+
+    void keyReleaseEvent(QKeyEvent* event);
+    void resizeEvent(QResizeEvent *event);
+    void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void tabletEvent(QTabletEvent* event);
+    void timerEvent(QTimerEvent *event);
+    void wheelEvent(QWheelEvent *e);
+    void changeEvent(QEvent *event);
+
+    virtual QValidator::State validate(QString &input, int &pos) const;
+    virtual double valueFromText(const QString &text) const;
+    virtual QString textFromValue(double val) const;
+    virtual void fixup(QString &str) const;
+
+public Q_SLOTS:
+
+    void setValue(double value);
+
+Q_SIGNALS:
+
+    void valueChanged(double);
+    void valueChanged(const QString&);
+
+    void sliderPressed();
+    void sliderMoved(int position);
+    void sliderReleased();
+
+    void rangeChanged(int min, int max);
+
+    void actionTriggered(int action);
+
+private:
+
+    Q_DISABLE_COPY(KisSlider)
+    Q_DECLARE_PRIVATE(KisSlider)
 };
 
 #endif // KIS_SLIDER_H

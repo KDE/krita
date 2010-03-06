c047a52739ce75788cb94b9cd6c46d507a01dc7e
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-03-06 13:29:47 +0000

    This was not commited and I don't know why. Part of the previous commit.
    svn path=/trunk/koffice/; revision=1099988

--- krita/plugins/paintops/libpaintop/kis_brush_size_option.cpp
+++ krita/plugins/paintops/libpaintop/kis_brush_size_option.cpp
@@ -15,18 +15,18 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-#include "kis_soft_size_option.h"
+#include "kis_brush_size_option.h"
 #include <klocale.h>
 
 #include <QWidget>
 #include <QRadioButton>
 
-#include "ui_wdgSoftSizeOptions.h"
+#include "ui_wdgBrushSizeOptions.h"
 
-class KisSoftSizeOptionsWidget: public QWidget, public Ui::WdgSoftSizeOptions
+class KisBrushSizeOptionsWidget: public QWidget, public Ui::WdgBrushSizeOptions
 {
 public:
-    KisSoftSizeOptionsWidget(QWidget *parent = 0)
+    KisBrushSizeOptionsWidget(QWidget *parent = 0)
             : QWidget(parent) {
         setupUi(this);
     }
@@ -34,11 +34,11 @@ public:
 };
 
 
-KisSoftSizeOption::KisSoftSizeOption()
+KisBrushSizeOption::KisBrushSizeOption()
         : KisPaintOpOption(i18n("Brush size"), false)
 {
     m_checkable = false;
-    m_options = new KisSoftSizeOptionsWidget();
+    m_options = new KisBrushSizeOptionsWidget();
 
     connect(m_options->diameter,SIGNAL(valueChanged(double)),SIGNAL( sigSettingChanged()));
     connect(m_options->scale,SIGNAL(valueChanged(double)),SIGNAL( sigSettingChanged()));
@@ -54,46 +54,46 @@ KisSoftSizeOption::KisSoftSizeOption()
     setConfigurationPage(m_options);
 }
 
-KisSoftSizeOption::~KisSoftSizeOption()
+KisBrushSizeOption::~KisBrushSizeOption()
 {
     delete m_options; 
 }
 
 
-int KisSoftSizeOption::diameter() const
+int KisBrushSizeOption::diameter() const
 {
         return qRound(m_options->diameter->value());
 }
 
 
-void KisSoftSizeOption::setDiameter(int diameter)
+void KisBrushSizeOption::setDiameter(int diameter)
 {
         m_options->diameter->setValue(diameter);
 }
 
 
-void KisSoftSizeOption::writeOptionSetting(KisPropertiesConfiguration* setting) const
+void KisBrushSizeOption::writeOptionSetting(KisPropertiesConfiguration* setting) const
 {
-    setting->setProperty( SOFT_DIAMETER, m_options->diameter->value() );
-    setting->setProperty( SOFT_ASPECT, m_options->aspectBox->value() );
-    setting->setProperty( SOFT_ROTATION, m_options->rotationBox->value() );
-    setting->setProperty( SOFT_SCALE, m_options->scale->value() );
-    setting->setProperty( SOFT_SPACING, m_options->spacing->value() );
-    setting->setProperty( SOFT_DENSITY, m_options->densityBox->value() );
-    setting->setProperty( SOFT_JITTER_MOVEMENT, m_options->jitterMove->value());
-    setting->setProperty( SOFT_JITTER_MOVEMENT_ENABLED, m_options->jitterMoveBox->isChecked());
+    setting->setProperty( BRUSH_DIAMETER, m_options->diameter->value() );
+    setting->setProperty( BRUSH_ASPECT, m_options->aspectBox->value() );
+    setting->setProperty( BRUSH_ROTATION, m_options->rotationBox->value() );
+    setting->setProperty( BRUSH_SCALE, m_options->scale->value() );
+    setting->setProperty( BRUSH_SPACING, m_options->spacing->value() );
+    setting->setProperty( BRUSH_DENSITY, m_options->densityBox->value() );
+    setting->setProperty( BRUSH_JITTER_MOVEMENT, m_options->jitterMove->value());
+    setting->setProperty( BRUSH_JITTER_MOVEMENT_ENABLED, m_options->jitterMoveBox->isChecked());
 }
 
-void KisSoftSizeOption::readOptionSetting(const KisPropertiesConfiguration* setting)
+void KisBrushSizeOption::readOptionSetting(const KisPropertiesConfiguration* setting)
 {
-    m_options->diameter->setValue( setting->getDouble(SOFT_DIAMETER) );
-    m_options->aspectBox->setValue( setting->getDouble(SOFT_ASPECT) );
-    m_options->rotationBox->setValue( setting->getDouble(SOFT_ROTATION) );
-    m_options->scale->setValue( setting->getDouble(SOFT_SCALE) );
-    m_options->spacing->setValue( setting->getDouble(SOFT_SPACING) );
-    m_options->densityBox->setValue( setting->getDouble(SOFT_DENSITY) );
-    m_options->jitterMove->setValue( setting->getDouble(SOFT_JITTER_MOVEMENT) );
-    m_options->jitterMoveBox->setChecked( setting->getBool(SOFT_JITTER_MOVEMENT_ENABLED) );
+    m_options->diameter->setValue( setting->getDouble(BRUSH_DIAMETER) );
+    m_options->aspectBox->setValue( setting->getDouble(BRUSH_ASPECT) );
+    m_options->rotationBox->setValue( setting->getDouble(BRUSH_ROTATION) );
+    m_options->scale->setValue( setting->getDouble(BRUSH_SCALE) );
+    m_options->spacing->setValue( setting->getDouble(BRUSH_SPACING) );
+    m_options->densityBox->setValue( setting->getDouble(BRUSH_DENSITY) );
+    m_options->jitterMove->setValue( setting->getDouble(BRUSH_JITTER_MOVEMENT) );
+    m_options->jitterMoveBox->setChecked( setting->getBool(BRUSH_JITTER_MOVEMENT_ENABLED) );
     
 }
 
--- krita/plugins/paintops/libpaintop/kis_brush_size_option.h
+++ krita/plugins/paintops/libpaintop/kis_brush_size_option.h
@@ -15,29 +15,32 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-#ifndef KIS_SOFT_SIZE_OPTION_H
-#define KIS_SOFT_SIZE_OPTION_H
+#ifndef KIS_SIZE_OPTION_H_
+#define KIS_SIZE_OPTION_H_
+
+#include <cmath>
+#include <QtGlobal>
 
 #include <kis_paintop_option.h>
 #include <krita_export.h>
 
-class KisSoftSizeOptionsWidget;
+class KisBrushSizeOptionsWidget;
 
-const QString SOFT_SHAPE = "Soft/shape";
-const QString SOFT_DIAMETER = "Soft/diameter";
-const QString SOFT_ASPECT = "Soft/aspect";
-const QString SOFT_SCALE = "Soft/scale";
-const QString SOFT_ROTATION = "Soft/rotation";
-const QString SOFT_SPACING = "Soft/spacing";
-const QString SOFT_DENSITY = "Soft/density";
-const QString SOFT_JITTER_MOVEMENT = "Soft/jitterMovement";
-const QString SOFT_JITTER_MOVEMENT_ENABLED = "Soft/jitterMovementEnabled";
+const QString BRUSH_SHAPE = "Brush/shape";
+const QString BRUSH_DIAMETER = "Brush/diameter";
+const QString BRUSH_ASPECT = "Brush/aspect";
+const QString BRUSH_SCALE = "Brush/scale";
+const QString BRUSH_ROTATION = "Brush/rotation";
+const QString BRUSH_SPACING = "Brush/spacing";
+const QString BRUSH_DENSITY = "Brush/density";
+const QString BRUSH_JITTER_MOVEMENT = "Brush/jitterMovement";
+const QString BRUSH_JITTER_MOVEMENT_ENABLED = "Brush/jitterMovementEnabled";
 
-class KisSoftSizeOption : public KisPaintOpOption
+class PAINTOP_EXPORT KisBrushSizeOption : public KisPaintOpOption
 {
 public:
-    KisSoftSizeOption();
-    ~KisSoftSizeOption();
+    KisBrushSizeOption();
+    ~KisBrushSizeOption();
 
     int diameter() const;
     void setDiameter(int diameter);
@@ -46,7 +49,40 @@ public:
     void readOptionSetting(const KisPropertiesConfiguration* setting);
 
 private:
-    KisSoftSizeOptionsWidget * m_options;
+    KisBrushSizeOptionsWidget * m_options;
 };
+
+class PAINTOP_EXPORT KisBrushSizeProperties{
+
+public:
+    quint16 shape;
+    quint16 diameter;
+    qreal aspect;
+    qreal scale;
+    qreal rotation;
+    qreal spacing;
+    qreal density;
+    
+    qreal jitterMovementAmount;
+    bool jitterEnabled;
+
+public:
+    void readOptionSetting(const KisPropertiesConfiguration * settings){
+        //TODO: shape
+        shape = 0;
+        diameter = quint16(settings->getDouble(BRUSH_DIAMETER));
+        aspect = settings->getDouble(BRUSH_ASPECT);
+        rotation = settings->getDouble(BRUSH_ROTATION) * (M_PI/180.0);
+        scale = settings->getDouble(BRUSH_SCALE);    
+        density = settings->getDouble(BRUSH_DENSITY) * 0.01;
+        spacing = settings->getDouble(BRUSH_SPACING);
+        if (jitterEnabled = settings->getBool(BRUSH_JITTER_MOVEMENT_ENABLED)){
+            jitterMovementAmount = settings->getDouble(BRUSH_JITTER_MOVEMENT);
+        }else{
+            jitterMovementAmount = 0.0;
+        }
+    }
+};
+
 
 #endif

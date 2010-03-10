e2c897cb3f1eeb5cf864ed1b5f17e351fe5e1285
Jos van den Oever <jos@vandenoever.info>
2010-03-10 22:22:22 +0000

    Improve OOoSDK detection.
    svn path=/trunk/koffice/; revision=1101791

--- cmake/modules/FindOOoSDK.cmake
+++ cmake/modules/FindOOoSDK.cmake
@@ -13,22 +13,33 @@
 
 # look for cppuhelper/bootstrap.hxx
 
-find_path(CPPUHELPER_INCLUDE_DIR
-	NAMES cppuhelper/bootstrap.hxx
-	PATHS /usr/include/openoffice
-)
-if (NOT CPPUHELPER_INCLUDE_DIR)
-	set(OOOSDK_ERROR 
-		"Could not find cppuhelper/bootstrap.hxx for OOoSDK.")
-endif (NOT CPPUHELPER_INCLUDE_DIR)
-
 find_path(OOOSDK_DIR
 	NAMES sdk/bin/cppumaker program/offapi.rdb
 	PATHS /opt/openoffice.org/basis3.2
+	      /usr/lib/openoffice/basis3.2
 	      /usr/lib/openoffice/basis3.1
 	      /usr/lib/openoffice/basis3.0
 )
-if (NOT OOOSDK_DIR)
+if (OOOSDK_DIR)
+	find_path(CPPUHELPER_INCLUDE_DIR
+		NAMES cppuhelper/bootstrap.hxx
+		PATHS ${OOOSDK_DIR}/sdk/include
+		      /usr/include/openoffice
+	)
+	if (NOT CPPUHELPER_INCLUDE_DIR)
+		set(OOOSDK_ERROR 
+			"Could not find cppuhelper/bootstrap.hxx for OOoSDK.")
+	endif (NOT CPPUHELPER_INCLUDE_DIR)
+
+	find_library(OOOSDK_LIBRARIES
+		NAMES uno_cppuhelpergcc3
+		PATHS ${OOOSDK_DIR}/sdk/lib
+		      /usr/lib/ure/lib
+	)
+	if (NOT OOOSDK_LIBRARIES)
+		set(OOOSDK_ERROR "Could not find uno_cppuhelpergcc3 for OOoSDK.")
+	endif (NOT OOOSDK_LIBRARIES)
+else (OOOSDK_DIR)
 	set(OOOSDK_ERROR "Could not find sdk/bin/cppumaker
 		or program/offapi.rdb for OOoSDK.")
 endif (NOT OOOSDK_DIR)
@@ -42,16 +53,7 @@ if (NOT OOOSDK_URE_DIR)
 	set(OOOSDK_ERROR "Could not find share/misc/types.rdb for OOoSDK.")
 endif (NOT OOOSDK_URE_DIR)
 
-find_library(OOOSDK_LIBRARIES
-	NAMES uno_cppuhelpergcc3
-	PATHS ${OOOSDK_DIR}/sdk/lib
-	      /usr/lib/ure/lib
-)
-if (NOT OOOSDK_LIBRARIES)
-	set(OOOSDK_ERROR "Could not find uno_cppuhelpergcc3 for OOoSDK.")
-endif (NOT OOOSDK_LIBRARIES)
-
-if(CPPUHELPER_INCLUDE_DIR AND OOOSDK_DIR AND OOOSDK_URE_DIR AND OOOSDK_LIBRARIES)
+if(CPPUHELPER_INCLUDE_DIR AND OOOSDK_URE_DIR AND OOOSDK_LIBRARIES)
 	set(OOOSDK_FOUND true)
 
 	# generate c++ headers
@@ -69,7 +71,7 @@ if(CPPUHELPER_INCLUDE_DIR AND OOOSDK_DIR AND OOOSDK_URE_DIR AND OOOSDK_LIBRARIES
 		DEPENDS ${_OOOINCLUDE}/com/sun/star/uno/Exception.hpp)
 
 	set(OOOSDK_INCLUDE_DIRS ${CPPUHELPER_INCLUDE_DIR} ${_OOOINCLUDE})
-endif(CPPUHELPER_INCLUDE_DIR AND OOOSDK_DIR AND OOOSDK_URE_DIR AND OOOSDK_LIBRARIES)
+endif(CPPUHELPER_INCLUDE_DIR AND OOOSDK_URE_DIR AND OOOSDK_LIBRARIES)
 
 if(NOT OOoSDK_FOUND)
 	if(OOoSDK_FIND_REQUIRED)

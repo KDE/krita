36db89ffb976401a007a6358254d437ea258bcbf
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-03-19 13:28:54 +0000

    I prefer to refer it as softness. You setup softness in softbrush. That sounds right.
    svn path=/trunk/koffice/; revision=1105184

--- krita/plugins/paintops/softbrush/kis_softop_option.cpp
+++ krita/plugins/paintops/softbrush/kis_softop_option.cpp
@@ -22,7 +22,7 @@
 #include <QRadioButton>
 
 KisSoftOpOption::KisSoftOpOption()
-        : KisPaintOpOption(i18n("Brush mask"), false)
+        : KisPaintOpOption(i18n("Softness"), false)
 {
     m_checkable = false;
     m_options = new KisSoftBrushSelectionWidget();

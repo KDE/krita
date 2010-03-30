714f943fd5dc260501d7e03bd1a02c3eab588fca
Cyrille Berger <cyb@lepi.org>
2010-03-30 13:20:09 +0000

    improve dox
    svn path=/trunk/koffice/; revision=1109053

--- libs/pigment/KoMixColorsOp.h
+++ libs/pigment/KoMixColorsOp.h
@@ -34,7 +34,8 @@ public:
     /**
      * Mix the colors.
      * @param colors a pointer toward the source pixels
-     * @param weights the coeffient of the source pixels
+     * @param weights the coeffient of the source pixels (if you want
+     *                to average the sum of weights must be equal to 255)
      * @param nColors the number of pixels in the colors array
      * @param dst the destination pixel
      *

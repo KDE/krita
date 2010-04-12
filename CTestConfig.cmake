9308c34bb069dbee60739c9dad2f9cec6d88f871
Cyrille Berger <cyb@lepi.org>
2010-04-12 18:52:18 +0000

    add a cmake file to go with CTest nightly build
    svn path=/trunk/koffice/; revision=1114103

---
+++ CTestConfig.cmake
@@ -0,0 +1,13 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "koffice")
+set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=koffice")
+set(CTEST_DROP_SITE_CDASH TRUE)

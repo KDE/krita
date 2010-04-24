336720c7601735f3349bb1d9fc4c0b0977b62725
Cyrille Berger <cyb@lepi.org>
2010-04-24 07:24:04 +0000

    add a benchmark for the mask generator
    svn path=/trunk/koffice/; revision=1118268

---
+++ krita/benchmarks/kis_mask_generator_benchmark.cpp
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2010 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <qtest_kde.h>
+
+#include "kis_mask_generator_benchmark.h"
+
+#include "kis_circle_mask_generator.h"
+#include "kis_rect_mask_generator.h"
+
+void KisMaskGeneratorBenchmark::benchmarkCircle()
+{
+    KisCircleMaskGenerator gen(1000, 0.5, 0.5, 0.5, 3);
+    QBENCHMARK{
+        for(int i = -600; i < 600; ++i)
+        {
+            for(int j = -600; j < 600; ++j)
+            {
+                gen.valueAt(i, j);
+            }
+        }
+    }
+}
+
+void KisMaskGeneratorBenchmark::benchmarkSquare()
+{
+    KisRectangleMaskGenerator gen(1000, 0.5, 0.5, 0.5, 3);
+    QBENCHMARK{
+        for(int i = -600; i < 600; ++i)
+        {
+            for(int j = -600; j < 600; ++j)
+            {
+                gen.valueAt(i, j);
+            }
+        }
+    }
+}
+
+QTEST_KDEMAIN(KisMaskGeneratorBenchmark, GUI)
+#include "kis_mask_generator_benchmark.moc"

542bfe099faf625a14ff45bce4139f3fd67b7bba
Carlos Licea <carlos_licea@hotmail.com>
2010-05-05 22:13:32 +0000

    Fix copyright.
    svn path=/trunk/koffice/; revision=1123360

--- plugins/pictureshape/WatermarkFilterEffect.cpp
+++ plugins/pictureshape/WatermarkFilterEffect.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
+    This file is part of the KDE project
+    Copyright (C) 2010 Carlos Licea carlos@kdab.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

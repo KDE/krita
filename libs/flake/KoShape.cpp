51c87e17e467a7b32e398045a85400707b331f11
Thorsten Zachmann <t.zachmann@zagge.de>
2010-05-05 05:39:33 +0000

    o Fix Bug 236395 - attribute style:protect saved but not loaded
      load attribute style:protect
      save attribute directly to style without going over additional
      attriutes which has the problem that it needs to be removed again
      after saving as otherwise it will be saved again the next time even if
      the flag is no longer set.
    
    @koffice-devel: please review so I can backport to 2.2
    
    CCBUG: 236395
    CCMAIL: koffice-devel@kde.org
    
    
    svn path=/trunk/koffice/; revision=1122951

--- libs/flake/KoShape.cpp
+++ libs/flake/KoShape.cpp
@@ -932,15 +932,17 @@ QString KoShape::saveStyle(KoGenStyle &style, KoShapeSavingContext &context) con
     }
 
     QString value;
-    if (isGeometryProtected())
+    if (isGeometryProtected()) {
         value = "position size";
+    }
     if (isContentProtected()) {
         if (! value.isEmpty())
             value += ' ';
         value += "content";
     }
-    if (!value.isEmpty())
-        (const_cast<KoShapePrivate*>(d))->additionalStyleAttributes.insert("style:protect", value);
+    if (!value.isEmpty()) {
+        style.addProperty("style:protect", value);
+    }
 
     QMap<QByteArray, QString>::const_iterator it(d->additionalStyleAttributes.constBegin());
     for (; it != d->additionalStyleAttributes.constEnd(); ++it) {
@@ -998,6 +1000,10 @@ void KoShape::loadStyle(const KoXmlElement &element, KoShapeLoadingContext &cont
     setBackground(loadOdfFill(context));
     setBorder(loadOdfStroke(element, context));
     setShadow(d->loadOdfShadow(context));
+
+    QString protect(styleStack.property(KoXmlNS::style, "protect"));
+    setGeometryProtected(protect.contains("position") || protect.contains("size"));
+    setContentProtected(protect.contains("content"));
 
     styleStack.restore();
 }

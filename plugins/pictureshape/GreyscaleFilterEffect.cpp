6d9e1e2abba19202ddb3adce362491b06966d4d6
Carlos Licea <carlos_licea@hotmail.com>
2010-05-08 02:26:58 +0000

    Relicense files to LGPLv2+.
    svn path=/trunk/koffice/; revision=1124177

--- plugins/pictureshape/GreyscaleFilterEffect.cpp
+++ plugins/pictureshape/GreyscaleFilterEffect.cpp
@@ -1,21 +1,21 @@
-/*
-    This file is part of the KDE project
-    Copyright (C) 2010 Carlos Licea carlos@kdab.com
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+/* This file is part of the KDE project
+ * Copyright (C) 2010 Carlos Licea <carlos@kdab.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include "GreyscaleFilterEffect.h"
 #include <KoFilterEffectRenderContext.h>

44a862dce607eae415ee3aa5c615a4906b5a4e32
Boudewijn Rempt <boud@valdyas.org>
2010-05-12 14:35:56 +0000

    Fix crash on closing a second window
    Sometimes, when closing a window, the tool option widget is still
    owned by the window, instead of the tool. Then the tool will first
    delete the widget, followed by the window, which causes a crash.
    
    So, only delete tool option widgets that do not have a parent.
    
    BUG:235345
    
    svn path=/trunk/koffice/; revision=1125860

--- libs/flake/KoToolBase_p.h
+++ libs/flake/KoToolBase_p.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright (C) 2006-2010 Thomas Zander <zander@kde.org>
+ * Copyright (C) 2010 KO GmbH <boud@kogmbh.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -44,7 +45,14 @@ public:
 
     ~KoToolBasePrivate()
     {
-        qDeleteAll(optionWidgets);
+        foreach(QWidget *optionWidget, optionWidgets) {
+            // Only delete the option widget if it hasn't got a parent; if it has,
+            // the parent will delete it.
+            if (optionWidget->parent() == 0) {
+                delete optionWidget;
+            }
+        }
+        optionWidgets.clear();
     }
 
     QMap<QString, QWidget *> optionWidgets; ///< the optionwidgets associated with this tool

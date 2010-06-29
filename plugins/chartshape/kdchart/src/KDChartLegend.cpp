1cbe72569fb54378edbbc58fbb28aab73277c348
Dag Andersen <danders@get2net.dk>
2010-06-29 12:11:37 +0000

    Legend: * Fetch brushes from diagram if there is one. * Rebuild if datasets are hidden/unhidden.
    svn path=/trunk/koffice/; revision=1144123

--- plugins/chartshape/kdchart/src/KDChartLegend.cpp
+++ plugins/chartshape/kdchart/src/KDChartLegend.cpp
@@ -979,7 +979,10 @@ void Legend::buildLegend()
         // as well as through the dataset brush set in the diagram, whereas the
         // MarkerAttributes are preferred.
         const QBrush markerBrush = markerAttrs[dataset].markerColor().isValid() ?
-                                   QBrush(markerAttrs[dataset].markerColor()) : brush( dataset );
+                                   QBrush(markerAttrs[dataset].markerColor()) : d->modelBrushes[ dataset ];
+
+        //qDebug()<<"Legend::buildLegend:"<<markerBrush<<markerAttrs[dataset].markerColor().isValid()<<dataset;
+
         switch( style ){
             case( MarkersOnly ):
                 markerLineItem = new KDChart::MarkerLayoutItem(
@@ -1086,6 +1089,7 @@ void Legend::buildLegend()
 void Legend::setHiddenDatasets( const QList<uint> hiddenDatasets )
 {
     d->hiddenDatasets = hiddenDatasets;
+    setNeedRebuild();
 }
 
 const QList<uint> Legend::hiddenDatasets() const
@@ -1095,10 +1099,13 @@ const QList<uint> Legend::hiddenDatasets() const
 
 void Legend::setDatasetHidden( uint dataset, bool hidden )
 {
-    if( hidden && !d->hiddenDatasets.contains( dataset ) )
+    if( hidden && !d->hiddenDatasets.contains( dataset ) ) {
         d->hiddenDatasets.append( dataset );
-    else if( !hidden && d->hiddenDatasets.contains( dataset ) )
+        setNeedRebuild();
+    } else if( !hidden && d->hiddenDatasets.contains( dataset ) ) {
         d->hiddenDatasets.removeAll( dataset );
+        setNeedRebuild();
+    }
 }
 
 bool Legend::datasetIsHidden( uint dataset ) const

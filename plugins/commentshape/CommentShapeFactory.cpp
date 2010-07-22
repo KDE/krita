242c3fe3ce484efc772eb91a8af38816a26cca1a
Carlos Licea <carlos_licea@hotmail.com>
2010-07-22 06:48:38 +0000

    Add a new shape, the CommentShape which will handle the officeooo:comment elements.
    svn path=/trunk/koffice/; revision=1152931

---
+++ plugins/commentshape/CommentShapeFactory.cpp
@@ -0,0 +1,45 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2010 Carlos Licea <carlos@kdab.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "CommentShapeFactory.h"
+#include "CommentShape.h"
+
+#include <klocale.h>
+#include <KoXmlNS.h>
+
+CommentShapeFactory::CommentShapeFactory(QObject* parent)
+: KoShapeFactoryBase(parent, COMMENTSHAPEID, i18n("Comment"))
+{
+    setOdfElementNames(KoXmlNS::officeooo, QStringList("annotation"));
+}
+
+CommentShapeFactory::~CommentShapeFactory()
+{
+}
+
+
+KoShape* CommentShapeFactory::createDefaultShape(KoResourceManager* documentResources) const
+{
+    return new CommentShape();
+}
+
+bool CommentShapeFactory::supports(const KoXmlElement& element) const
+{
+    return element.localName() == "annotation" && element.namespaceURI() == KoXmlNS::officeooo; //TODO change accordingly
+}

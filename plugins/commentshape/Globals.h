7970d1417111065bf201cf4a8b446d563c6fd975
Carlos Licea <carlos_licea@hotmail.com>
2010-07-24 07:34:17 +0000

    Add a globals file since I'll break up the shape in other shapes.
    svn path=/trunk/koffice/; revision=1153912

---
+++ plugins/commentshape/Globals.h
@@ -0,0 +1,9 @@
+#include <QPointF>
+#include <QSizeF>
+
+const QPointF initialsBoxPoint = QPointF(0.0,0.0);
+const QSizeF initialsBoxSize = QSizeF(20.0,20.0);
+const QPointF commentBoxPoint = QPointF(22.0,0.0);
+const QSizeF commentBoxSize = QSizeF(100.0,100.0);
+
+const QSizeF wholeSize = QSizeF(120.0,100.0);

fd72c0030b721f888a63b011c2e0b0713c3fad59
Adam Celarek <bugs_kde_org@xibo.at>
2010-08-04 20:47:10 +0000

    added colour spaces support for minimal shade selector
    svn path=/trunk/koffice/; revision=1159239

--- krita/plugins/extensions/dockers/colorselectorng/kis_shade_selector_line.cpp
+++ krita/plugins/extensions/dockers/colorselectorng/kis_shade_selector_line.cpp
@@ -107,52 +107,49 @@ void KisShadeSelectorLine::paintEvent(QPaintEvent *)
     m_pixelCache = QImage(width(), height(), QImage::Format_ARGB32_Premultiplied);
     QPainter painter(&m_pixelCache);
     painter.fillRect(0,0, width(), height(), m_backgroundColor);
+
+    KisMinimalShadeSelector* parent = dynamic_cast<KisMinimalShadeSelector*>(parentWidget());
+
+    const KoColorSpace* colorspace;
+    if(parent)
+        colorspace = parent->colorSpace();
+    else
+        colorspace = KoColorSpaceRegistry::instance()->rgb8();
+    KoColor koColor(colorspace);
+
+    int patchCount;
+    int patchSpacing;
+
     if(m_gradient) {
-        QColor c1;
-        qreal hue1 = m_color.hueF()-m_hueDelta;
-        if(hue1<0) hue1+=1.;
-        if(hue1>1) hue1-=1.;
-        c1.setHsvF(hue1,
-                   qBound(0., m_color.saturationF()-m_saturationDelta, 1.),
-                   qBound(0., m_color.valueF()-m_valueDelta, 1.));
-
-        QColor c2;
-        qreal hue2 = m_color.hueF()+m_hueDelta;
-        if(hue2<0) hue2+=1.;
-        if(hue2>1) hue2-=1.;
-        c2.setHsvF(hue2,
-                   qBound(0., m_color.saturationF()+m_saturationDelta, 1.),
-                   qBound(0., m_color.valueF()+m_valueDelta, 1.));
-
-        QLinearGradient gradient(0,0, width(), 0);
-        gradient.setColorAt(0, c1);
-        gradient.setColorAt(0.5, m_color);
-        gradient.setColorAt(1, c2);
-
-        painter.fillRect(0,0,width(), m_lineHeight, QBrush(gradient));
+        patchCount = width();
+        patchSpacing = 0;
     }
     else {
-        qreal patchWidth = (width()-2*m_patchCount)/qreal(m_patchCount);
-        qreal hueStep=m_hueDelta/qreal(m_patchCount);
-        qreal saturationStep=m_saturationDelta/qreal(m_patchCount);
-        qreal valueStep=m_valueDelta/qreal(m_patchCount);
+        patchCount = m_patchCount;
+        patchSpacing = 3;
+    }
+    qreal patchWidth = (width()-patchSpacing*patchCount)/qreal(patchCount);
 
-        int z=0;
-        for(int i=-m_patchCount/2; i<=m_patchCount/2; i++) {
-            if(i==0 && m_patchCount%2==0) continue;
+    qreal hueStep=m_hueDelta/qreal(patchCount);
+    qreal saturationStep=m_saturationDelta/qreal(patchCount);
+    qreal valueStep=m_valueDelta/qreal(patchCount);
 
-            qreal hue=m_color.hueF()+(i*hueStep);
-            if(hue<0) hue+=1.;
-            if(hue>1) hue-=1.;
+    int z=0;
+    for(int i=-patchCount/2; i<=patchCount/2; i++) {
+        if(i==0 && patchCount%2==0) continue;
 
-            qreal saturation = qBound(0., m_color.saturationF()+(i*saturationStep), 1.);
+        qreal hue=m_color.hueF()+(i*hueStep);
+        if(hue<0) hue+=1.;
+        if(hue>1) hue-=1.;
 
-            qreal value = qBound(0., m_color.valueF()+(i*valueStep), 1.);
+        qreal saturation = qBound(0., m_color.saturationF()+(i*saturationStep), 1.);
+
+        qreal value = qBound(0., m_color.valueF()+(i*valueStep), 1.);
 
 
-            painter.fillRect(z*(patchWidth+2)+1, 0, patchWidth, m_lineHeight, QColor::fromHsvF(hue, saturation, value));
-            z++;
-        }
+        koColor.fromQColor(QColor::fromHsvF(hue, saturation, value));
+        painter.fillRect(z*(patchWidth+patchSpacing)/*+1*/, 0, patchWidth, m_lineHeight, koColor.toQColor());
+        z++;
     }
 
     QPainter wpainter(this);

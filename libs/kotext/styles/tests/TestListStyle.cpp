d7cee5d4a54e87a3ab02bd0065212d687bf543a6
Sebastian Sauer <mail@dipe.org>
2010-08-08 01:20:05 +0000

    revert r1160356; 1) no review again(!!!), 2) you introduced a regression.
    svn path=/trunk/koffice/; revision=1160396

--- libs/kotext/styles/tests/TestListStyle.cpp
+++ libs/kotext/styles/tests/TestListStyle.cpp
@@ -1,21 +1,3 @@
-/* This file is part of the KDE project
- * Copyright (C) 2007-2010 Thomas Zander <zander@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- */
 #include "TestListStyle.h"
 
 #include "styles/KoParagraphStyle.h"
@@ -70,25 +52,6 @@ void TestListStyle::testListStyle()
     format = textList->format();
     QCOMPARE(format.intProperty(QTextListFormat::ListStyle), (int)(KoListStyle::AlphaLowerItem));
 
-    // getting a properties without setting it doesn't change the list.
-    KoListLevelProperties l4 = ls.levelProperties(4);
-    QCOMPARE(l4.level(), 4);
-    QCOMPARE(l4.displayLevel(), 0); // default
-    l4.setDisplayLevel(3);
-    QCOMPARE(l4.displayLevel(), 3);
-    QCOMPARE(ls.hasLevelProperties(4), false);
-
-    KoListLevelProperties anotherL4 = ls.levelProperties(4);
-    QCOMPARE(anotherL4.level(), 4);
-    QCOMPARE(anotherL4.displayLevel(), 0); // default
-    QCOMPARE(ls.hasLevelProperties(4), false);
-
-    QCOMPARE(ls.hasLevelProperties(5), false);
-    // new levels are a copy of the existing level.
-    KoListLevelProperties l5 = ls.levelProperties(5);
-    QCOMPARE(l5.displayLevel(), 0);
-    QCOMPARE(l5.style(), KoListStyle::AlphaLowerItem);
-    QCOMPARE(l5.indent(), 0.);
 }
 
 QTEST_MAIN(TestListStyle)

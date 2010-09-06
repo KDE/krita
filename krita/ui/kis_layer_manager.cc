811a33edafc3978fe7486ee02ccf4bb484dae8fe
Sven Langkamp <sven.langkamp@gmail.com>
2010-09-06 23:38:19 +0000

    set group layer active after adding it
    svn path=/trunk/koffice/; revision=1172334

--- krita/ui/kis_layer_manager.cc
+++ krita/ui/kis_layer_manager.cc
@@ -355,6 +355,7 @@ void KisLayerManager::addGroupLayer(KisNodeSP parent, KisNodeSP above)
             layer->setCompositeOp(COMPOSITE_OVER);
             m_commandsAdapter->addNode(layer.data(), parent.data(), above.data());
             m_view->canvas()->update();
+            m_view->nodeManager()->activateNode(layer);
         } else {
             KMessageBox::error(m_view, i18n("Could not add layer to image."), i18n("Layer Error"));
         }

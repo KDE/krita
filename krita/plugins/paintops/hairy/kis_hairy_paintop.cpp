bee2937127d28b428128d35cfd56800cc94303ac
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-09-08 16:07:48 +0000

    Fix painting with predefinded brushes. Speedup in range 1.15 - 1.30, tested with benchmarks.
    o Now it renders the dab and takes the bristles
      from it instead of using preview image.
    o use const reference to avoid copying the QVector
    
    BUG:247835
    
    svn path=/trunk/koffice/; revision=1173087

--- krita/plugins/paintops/hairy/kis_hairy_paintop.cpp
+++ krita/plugins/paintops/hairy/kis_hairy_paintop.cpp
@@ -29,8 +29,6 @@
 #include "kis_painter.h"
 #include <kis_vec.h>
 
-#include "brush_shape.h"
-
 #include <kis_hairy_ink_option.h>
 #include <kis_hairy_shape_option.h>
 #include <kis_hairy_bristle_option.h>
@@ -43,34 +41,32 @@ KisHairyPaintOp::KisHairyPaintOp(const
                                KisBrushBasedPaintOpSettings *settings, KisPainter * painter, KisImageWSP image)
         : KisPaintOp(painter)
         , m_settings(settings)
-        , m_image(image)
         , newStrokeFlag(true)
 
 {
     Q_ASSERT(settings);
 
-    KisBrushOption brushOption;
-    brushOption.readOptionSetting(settings);
-    KisBrushSP kisBrush = brushOption.brush();
-    BrushShape bs; 
-    
-    bs.setColorSpace(painter->device()->colorSpace());
-    bs.setHasColor(kisBrush->brushType() != MASK);
-    bs.fromQImageWithDensity(kisBrush->image(), settings->getDouble(HAIRY_BRISTLE_DENSITY) * 0.01);
-    m_brush.setBrushShape(bs);
-    
-    loadSettings(settings);
-    m_brush.setProperties( &m_properties );
-    
-    if (!bs.hasColor()){
-        m_brush.setInkColor(painter->paintColor());    
-    }
-    
     if (!settings->node()) {
         m_dev = 0;
     } else {
         m_dev = settings->node()->paintDevice();
+    } 
+    
+    KisBrushOption brushOption;
+    brushOption.readOptionSetting(settings);
+    KisBrushSP brush = brushOption.brush();
+    KisFixedPaintDeviceSP dab = cachedDab(source()->colorSpace());
+    if (brush->brushType() == IMAGE || brush->brushType() == PIPE_IMAGE) {
+        dab = brush->paintDevice(source()->colorSpace(), 1.0, 0.0, KisPaintInformation());
+    } else {
+        brush->mask(dab, painter->paintColor(), 1.0, 1.0, 0.0);
     }
+    
+    m_brush.fromDabWithDensity(dab, settings->getDouble(HAIRY_BRISTLE_DENSITY) * 0.01);
+    m_brush.setInkColor(painter->paintColor());
+    
+    loadSettings(settings);
+    m_brush.setProperties( &m_properties );
     
     m_rotationOption.readOptionSetting(settings);
     m_opacityOption.readOptionSetting(settings);
@@ -78,25 +74,15 @@ KisHairyPaintOp::KisHairyPaintOp(const
     m_rotationOption.sensor()->reset();
     m_opacityOption.sensor()->reset();
     m_sizeOption.sensor()->reset();
-
 }
 
 void KisHairyPaintOp::loadSettings(const KisBrushBasedPaintOpSettings* settings)
 {
-/*    m_properties.radius = settings->getInt(HAIRY_RADIUS);
-    m_properties.sigma = settings->getDouble(HAIRY_SIGMA);
-    m_properties.isbrushDimension1D = settings->getBool(HAIRY_IS_DIMENSION_1D);*/
-    
     m_properties.inkAmount = settings->getInt(HAIRY_INK_AMOUNT);
     //TODO: wait for the transfer function with variable size
-    QList<float> list;
-    KisCubicCurve curve = settings->getCubicCurve(HAIRY_INK_DEPLETION_CURVE);
-    for (int i=0;i < m_properties.inkAmount;i++){
-        list << curve.value( i/qreal(m_properties.inkAmount-1) );
-    }
-    curve.floatTransfer(m_properties.inkAmount);
+    
+    m_properties.inkDepletionCurve = settings->getCubicCurve(HAIRY_INK_DEPLETION_CURVE).floatTransfer(m_properties.inkAmount);
 
-    m_properties.inkDepletionCurve = list;
     m_properties.inkDepletionEnabled = settings->getBool(HAIRY_INK_DEPLETION_ENABLED);
     m_properties.useSaturation = settings->getBool(HAIRY_INK_USE_SATURATION);
     m_properties.useOpacity = settings->getBool(HAIRY_INK_USE_OPACITY);
@@ -116,19 +102,6 @@ void KisHairyPaintOp::loadSettings(const KisBrushBasedPaintOpSettings* settings)
     m_properties.antialias = settings->getBool(HAIRY_BRISTLE_ANTI_ALIASING);
     m_properties.useCompositing = settings->getBool(HAIRY_BRISTLE_USE_COMPOSITING);
     m_properties.connectedPath = settings->getBool(HAIRY_BRISTLE_CONNECTED);
-
-    
-    /*    BrushShape brushShape;
-    if (m_properties.isbrushDimension1D) 
-    {
-        brushShape.fromLine(m_properties.radius, m_properties.sigma);
-        brushShape.thresholdBristles(0.1);
-    } else {
-        brushShape.fromGaussian(m_properties.radius, m_properties.sigma);
-        brushShape.thresholdBristles(0.1);
-    }
-    m_brush.setBrushShape(brushShape);*/
-    
 }
 
 

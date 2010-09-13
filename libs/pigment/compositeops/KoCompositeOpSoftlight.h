94f669bc9d477d35f85d9a44a23a916bcaaf4e70
Boudewijn Rempt <boud@valdyas.org>
2010-09-13 07:51:38 +0000

    some work on softlight
    svn path=/trunk/koffice/; revision=1174704

--- libs/pigment/compositeops/KoCompositeOpSoftlight.h
+++ libs/pigment/compositeops/KoCompositeOpSoftlight.h
@@ -1,18 +1,17 @@
 /*
- *  Copyright (c) 2007 Cyrille Berger <cberger@cberger.net>
- *  Copyright (c) 2010 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2006 Cyrille Berger <cberger@cberger.net>
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * Library General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Library General Public License
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
@@ -21,55 +20,66 @@
 #ifndef KOCOMPOSITEOPSOFTLIGHT_H_
 #define KOCOMPOSITEOPSOFTLIGHT_H_
 
-#include "KoCompositeOpAlphaBase.h"
+#include "KoColorSpaceMaths.h"
+#include "KoCompositeOp.h"
+
+#define NATIVE_OPACITY_OPAQUE KoColorSpaceMathsTraits<channels_type>::unitValue
+#define NATIVE_OPACITY_TRANSPARENT KoColorSpaceMathsTraits<channels_type>::zeroValue
 
 /**
- * A template version of the soft light composite operation to use in colorspaces.
+ * A template version of the softLight composite operation to use in colorspaces<
  */
 template<class _CSTraits>
-class KoCompositeOpSoftlight : public KoCompositeOpAlphaBase<_CSTraits, KoCompositeOpSoftlight<_CSTraits>, true >
+class KoCompositeOpSoftLight : public KoCompositeOp
 {
     typedef typename _CSTraits::channels_type channels_type;
+
 public:
 
-    KoCompositeOpSoftlight(const KoColorSpace * cs)
-        : KoCompositeOpAlphaBase<_CSTraits, KoCompositeOpSoftlight<_CSTraits>, true >(cs, COMPOSITE_SOFT_LIGHT, i18n("Soft light"), KoCompositeOp::categoryLight()) {
+    KoCompositeOpSoftLight(const KoColorSpace * cs)
+            : KoCompositeOp(cs, COMPOSITE_SOFT_LIGHT, i18n("Soft Light"), KoCompositeOp::categoryLight()) {
     }
 
 public:
-    inline static channels_type selectAlpha(channels_type srcAlpha, channels_type dstAlpha) {
-        return qMin(srcAlpha, dstAlpha);
-    }
+    using KoCompositeOp::composite;
 
-    inline static void composeColorChannels(channels_type srcBlend,
-                                            const channels_type* src,
-                                            channels_type* dst,
-                                            bool allChannelFlags,
-                                            const QBitArray & channelFlags) {
-        for (uint i = 0; i < _CSTraits::channels_nb; i++) {
-            if ((int)i != _CSTraits::alpha_pos  && (allChannelFlags || channelFlags.testBit(i))) {
+    void composite(quint8 *dstRowStart,
+                   qint32 dststride,
+                   const quint8 *srcRowStart,
+                   qint32 srcstride,
+                   const quint8 *maskRowStart,
+                   qint32 maskstride,
+                   qint32 rows,
+                   qint32 cols,
+                   quint8 U8_opacity,
+                   const QBitArray & channelFlags) const {
 
-                channels_type srcChannel = src[i];
-                channels_type dstChannel = dst[i];
+        Q_UNUSED(channelFlags);
 
-                if (srcChannel <= NATIVE_MAX_VALUE / 2) {
-                    dstChannel = dstChannel - (NATIVE_MAX_VALUE - (2 * srcChannel)) * dstChannel * (NATIVE_MAX_VALUE - dstChannel);
-                }
-                else {
-                    channels_type tmpDst;
-                    if (dstChannel <= NATIVE_MAX_VALUE / 4) {
-                        tmpDst = ((16 * dstChannel - 12) * dstChannel + 4 ) * dstChannel;
-                    }
-                    else {
-                        tmpDst = sqrt(dstChannel);
-                    }
-                    dstChannel = dstChannel + (2 * srcChannel - NATIVE_MAX_VALUE) * (tmpDst - dstChannel);
-                }
-                dst[i] = KoColorSpaceMaths<channels_type>::blend(srcChannel, dstChannel, srcBlend);
+        qint32 srcInc = (srcstride == 0) ? 0 : _CSTraits::channels_nb;
+        channels_type opacity = KoColorSpaceMaths<quint8, channels_type>::scaleToA(U8_opacity);
+
+        while (rows-- > 0) {
+            const channels_type *s = reinterpret_cast<const channels_type *>(srcRowStart);
+            channels_type *d = reinterpret_cast<channels_type *>(dstRowStart);
+            const quint8 *mask = maskRowStart;
+
+            for (qint32 i = cols; i > 0; i--, s += srcInc, d += _CSTraits::channels_nb) {
+                channels_type srcAlpha = KoColorSpaceMathsTraits<channels_type>::max - s[_CSTraits::alpha_pos];
+                channels_type dstAlpha = KoColorSpaceMathsTraits<channels_type>::max - d[_CSTraits::alpha_pos];
+                channels_type gamma = srcAlpha + dstAlpha - (srcAlpha * dstAlpha);
+
+                d[_CSTraits::alpha_pos] = KoColorSpaceMathsTraits<channels_type>::max - gamma;
+
+            }
+
+            dstRowStart += dststride;
+            srcRowStart += srcstride;
+            if (maskRowStart) {
+                maskRowStart += maskstride;
             }
         }
     }
-
 };
 
 #endif

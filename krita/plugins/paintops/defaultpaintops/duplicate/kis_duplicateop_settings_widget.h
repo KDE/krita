2a1e4bf9b01b76a6dd26cd726148d241d02cbd6c
Lukáš Tvrdý <lukast.dev@gmail.com>
2010-09-16 16:39:39 +0000

    Allow paintops to disable scratch box.
    Duplicate is too complex for simple canvas like scratchbox.
    
    svn path=/trunk/koffice/; revision=1176097

--- krita/plugins/paintops/defaultpaintops/duplicate/kis_duplicateop_settings_widget.h
+++ krita/plugins/paintops/defaultpaintops/duplicate/kis_duplicateop_settings_widget.h
@@ -42,6 +42,10 @@ public:
 
     KisPropertiesConfiguration* configuration() const;
 
+    virtual bool supportScratchBox(){
+        return false;
+    }
+    
 public:
     KisDuplicateOpOption* m_duplicateOption;
 

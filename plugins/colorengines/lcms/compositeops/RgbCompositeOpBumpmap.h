4b958f4918ff8e2098f328e6e95bd6b4768c8db6
Sven Langkamp <sven.langkamp@gmail.com>
2010-09-17 14:29:40 +0000

    fix bumpmap to works like it's supposed by imagemagick BUG:251401
    svn path=/trunk/koffice/; revision=1176392

--- plugins/colorengines/lcms/compositeops/RgbCompositeOpBumpmap.h
+++ plugins/colorengines/lcms/compositeops/RgbCompositeOpBumpmap.h
@@ -20,76 +20,45 @@
 #define RGBCOMPOSITEOPBUMPMAP_H
 
 #include "KoColorSpaceMaths.h"
-#include <KoCompositeOp.h>
+#include <KoCompositeOpAlphaBase.h>
 
 template<class _CSTraits>
-class RgbCompositeOpBumpmap : public KoCompositeOp
+class RgbCompositeOpBumpmap : public KoCompositeOpAlphaBase<_CSTraits, RgbCompositeOpBumpmap<_CSTraits>, true >
 {
     typedef typename _CSTraits::channels_type channels_type;
     typedef typename KoColorSpaceMathsTraits<typename _CSTraits::channels_type>::compositetype compositetype;
 
 public:
 
-    RgbCompositeOpBumpmap(KoColorSpace * cs, const bool userVisible = true)
-            : KoCompositeOp(cs, COMPOSITE_BUMPMAP, i18n("Bumpmap"), "", userVisible) {
+    RgbCompositeOpBumpmap(KoColorSpace * cs)
+            : KoCompositeOpAlphaBase<_CSTraits, RgbCompositeOpBumpmap<_CSTraits>, true >(cs, COMPOSITE_BUMPMAP, i18n("Bumpmap"),  KoCompositeOp::categoryMisc()) {
+    }                       
+                       
+    inline static channels_type selectAlpha(channels_type srcAlpha, channels_type dstAlpha) {
+        return qMin(srcAlpha, dstAlpha);
     }
 
-    using KoCompositeOp::composite;
-
-    void composite(quint8 *dstRowStart, qint32 dstRowStride,
-                   const quint8 *srcRowStart, qint32 srcRowStride,
-                   const quint8 *maskRowStart, qint32 maskRowStride,
-                   qint32 rows, qint32 numColumns,
-                   quint8 opacity,
-                   const QBitArray & channelFlags) const {
-        Q_UNUSED(maskRowStart);
-        Q_UNUSED(maskRowStride);
-
-        if (opacity == OPACITY_TRANSPARENT_U8)
-            return;
-
-        channels_type *d;
-        const channels_type *s;
-
-        qint32 i;
-
+    inline static void composeColorChannels(channels_type srcBlend,
+                                            const channels_type* src,
+                                            channels_type* dst,
+                                            bool allChannelFlags,
+                                            const QBitArray & channelFlags) {
         qreal intensity;
+        
+        // And I'm not sure whether this is correct, either.
+        intensity = ((qreal)306.0 * src[_CSTraits::red_pos] +
+                        (qreal)601.0 * src[_CSTraits::green_pos] +
+                        (qreal)117.0 * src[_CSTraits::blue_pos]) / 1024.0;
+                        
+        for (uint i = 0; i < _CSTraits::channels_nb; i++) {
+            if ((int)i != _CSTraits::alpha_pos  && (allChannelFlags || channelFlags.testBit(i))) {
+                channels_type srcChannel = (channels_type)(((qreal)
+                                            intensity * dst[i]) / NATIVE_OPACITY_OPAQUE + 0.5);
+                channels_type dstChannel = dst[i];
 
-        while (rows-- > 0) {
-            d = reinterpret_cast<channels_type *>(dstRowStart);
-            s = reinterpret_cast<const channels_type *>(srcRowStart);
-            for (i = numColumns; i > 0; i--, d += _CSTraits::channels_nb, s += _CSTraits::channels_nb) {
-                // Is this correct? It's not this way in GM.
-                if (s[_CSTraits::alpha_pos] == NATIVE_OPACITY_TRANSPARENT)
-                    continue;
-
-                // And I'm not sure whether this is correct, either.
-                intensity = ((qreal)306.0 * s[_CSTraits::red_pos] +
-                             (qreal)601.0 * s[_CSTraits::green_pos] +
-                             (qreal)117.0 * s[_CSTraits::blue_pos]) / 1024.0;
-
-                if (channelFlags.isEmpty() || channelFlags.testBit(_CSTraits::red_pos))
-                    d[_CSTraits::red_pos] = (channels_type)(((qreal)
-                                                            intensity * d[_CSTraits::red_pos]) / NATIVE_OPACITY_OPAQUE + 0.5);
-
-                if (channelFlags.isEmpty() || channelFlags.testBit(_CSTraits::green_pos))
-                    d[_CSTraits::green_pos] = (channels_type)(((qreal)
-                                              intensity * d[_CSTraits::green_pos]) / NATIVE_OPACITY_OPAQUE + 0.5);
-
-                if (channelFlags.isEmpty() || channelFlags.testBit(_CSTraits::blue_pos))
-                    d[_CSTraits::blue_pos] = (channels_type)(((qreal)
-                                             intensity * d[_CSTraits::blue_pos]) / NATIVE_OPACITY_OPAQUE + 0.5);
-
-                if (channelFlags.isEmpty() || channelFlags.testBit(_CSTraits::alpha_pos))
-                    d[_CSTraits::alpha_pos] = (channels_type)(((qreal)
-                                              intensity * d[_CSTraits::alpha_pos]) / NATIVE_OPACITY_OPAQUE + 0.5);
-
-
+                dst[i] = KoColorSpaceMaths<channels_type>::blend(srcChannel, dstChannel, srcBlend);
             }
-            dstRowStart += dstRowStride;
-            srcRowStart += srcRowStride;
         }
-
     }
 
 };

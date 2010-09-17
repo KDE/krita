6ad09223d874fffa325d3fa21af6004ffdf5695e
Carlos Licea <carlos_licea@hotmail.com>
2010-09-17 22:07:10 +0000

    Don't look up the style on every iteration, just once. Thanks to Thorsten for the heads up.
    svn path=/trunk/koffice/; revision=1176531

--- libs/kotext/opendocument/KoTextLoader.cpp
+++ libs/kotext/opendocument/KoTextLoader.cpp
@@ -1203,8 +1203,8 @@ void KoTextLoader::loadTable(const KoXmlElement &tableElem, QTextCursor &cursor)
 
                     QString defaultCellStyleName = tblTag.attributeNS(KoXmlNS::table, "default-cell-style-name", "");
                     if (!defaultCellStyleName.isEmpty()) {
+                        KoTableCellStyle *cellStyle = d->textSharedData->tableCellStyle(defaultCellStyleName, d->stylesDotXml);
                         for (int c = columns; c < columns + repeatColumn; c++) {
-                            KoTableCellStyle *cellStyle = d->textSharedData->tableCellStyle(defaultCellStyleName, d->stylesDotXml);
                             tcarManager->setDefaultColumnCellStyle(c, cellStyle);
                         }
                     }

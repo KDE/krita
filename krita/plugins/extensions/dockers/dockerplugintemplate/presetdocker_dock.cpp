b88d31686a81dabaddffe5f184e26e414314b218
Adam Celarek <bugs_kde_org@xibo.at>
2010-09-18 01:48:21 +0000

    add a template for new docker plugins. it contains only the "plugin starter" and dockerwidget class
    svn path=/branches/work/koffice-essen/; revision=1176553

---
+++ krita/plugins/extensions/dockers/dockerplugintemplate/presetdocker_dock.cpp
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "presetdocker_dock.h"
+
+#include <QGridLayout>
+
+#include <klocale.h>
+
+#include <KoResourceManager.h>
+#include <KoCanvasBase.h>
+
+
+PresetDockerDock::PresetDockerDock( ) : QDockWidget(i18n("Preset docker")), m_canvas(0)
+{
+    setWidget(new QWidget(this));
+}
+
+void PresetDockerDock::setCanvas(KoCanvasBase * canvas)
+{
+    m_canvas = canvas;
+    connect(m_canvas->resourceManager(), SIGNAL(resourceChanged(int, const QVariant&)),
+            this, SLOT(resourceChanged(int, const QVariant&)));
+}
+
+void PresetDockerDock::resourceChanged(int key, const QVariant& v)
+{
+}
+
+#include "presetdocker_dock.moc"

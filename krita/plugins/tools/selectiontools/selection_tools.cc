df3ce19e0b5848033ccea1a1be7564b629584474
Adam Celarek <bugs_kde_org@xibo.at>
2010-09-26 16:35:12 +0000

    - disable treeshape (we don't use it) - disable magnetic select tool (it is not ready)
    svn path=/trunk/koffice/; revision=1179958

--- krita/plugins/tools/selectiontools/selection_tools.cc
+++ krita/plugins/tools/selectiontools/selection_tools.cc
@@ -37,7 +37,7 @@
 #include "kis_tool_select_path.h"
 #include "kis_tool_select_similar.h"
 #include "kis_tool_select_brush.h"
-#include "kis_tool_select_magnetic.h"
+//#include "kis_tool_select_magnetic.h"
 
 K_PLUGIN_FACTORY(SelectionToolsFactory, registerPlugin<SelectionTools>();)
 K_EXPORT_PLUGIN(SelectionToolsFactory("krita"))
@@ -58,7 +58,7 @@ SelectionTools::SelectionTools(QObject *parent, const QVariantList &)
     r->add(new KisToolSelectPathFactory(r, QStringList()));
     r->add(new KisToolSelectSimilarFactory(r, QStringList()));
     r->add(new KisToolSelectBrushFactory(r, QStringList()));
-    r->add(new KisToolSelectMagneticFactory(r, QStringList()));
+//    r->add(new KisToolSelectMagneticFactory(r, QStringList()));
 }
 
 SelectionTools::~SelectionTools()

8abfdc28dae3c88963ebac529d19a9763523894f
Casper Boemann <cbr@boemann.dk>
2010-10-10 21:57:35 +0000

    correct copyrights
    svn path=/trunk/koffice/; revision=1184597

--- libs/kotext/tests/TestDocumentLayout.cpp
+++ libs/kotext/tests/TestDocumentLayout.cpp
@@ -2,6 +2,7 @@
  *  This file is part of KOffice tests
  *
  *  Copyright (C) 2006-2010 Thomas Zander <zander@kde.org>
+ *  Copyright (C) 2009-2010 Casper Boemann <casper.boemann@kogmbh.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

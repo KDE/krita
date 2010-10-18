KDE/krita

commit 5a0a49fe389206f55975d51b4470ab1e964878d8

Fix a text layout problem of where a freefloating frame (which text is supposed to run around) doesn't work. In particular if that frame is placed such that the textline should be moved onto the next page then the old behavior put a lot of text on top of each other.
Also contain some preliminary fix for a related problem where the frame is
also anchored.

svn path=/branches/work/koffice-essen/; revision=1178051

plugins/textshape/Layout.cpp
@@ -262,7 +262,7 @@ bool Layout::addLine(QTextLine &line, bool processingLine)
             && m_parent->resizeMethod() == KoTextDocument::NoResize
             // line does not fit.
             && m_data->documentOffset() + shape->size().height() - footnoteHeight
-              < m_y + line.height() + m_shapeBorder.bottom
+              < line.y() + line.height() + m_shapeBorder.bottom
             // but make sure we don't leave the shape empty.
             && m_block.position() + line.textStart() > m_data->position()) {
         if (oldFootnoteDocLength >= 0) {
@@ -274,14 +274,37 @@ bool Layout::addLine(QTextLine &line, bool processingLine)
 
         m_data->setEndPosition(m_block.position() + line.textStart() - 1);
 
-        bool ignoreParagraph = false;
+        bool entireParagraphMoved = false;
         if (! m_newParag && m_format.nonBreakableLines()) { // then revert layouting of parag
             // TODO check height of parag so far; and if it does not fit in the rest of the shapes, just continue.
             m_data->setEndPosition(m_block.position() - 1);
             m_block.layout()->endLayout();
             m_block.layout()->beginLayout();
-            line = m_block.layout()->createLine();
-            ignoreParagraph = true;
+            line = m_block.layout()->createLine(); //TODO should probably not create this line
+            entireParagraphMoved = true;
+        } else {
+            // unfortunately we can't undo a single line, so we undo entire paragraph
+            // and redo all previous lines
+            QList<LineKeeper> lineKeeps;
+            m_block.layout()->endLayout();
+            for(int i = 0; i < m_block.layout()->lineCount()-1; i++) {
+                QTextLine l = m_block.layout()->lineAt(i);
+                LineKeeper lk;
+                lk.lineWidth = l.width();
+                lk.columns = l.textLength();
+                lk.position = l.position();
+                lineKeeps.append(lk);
+            }
+            m_block.layout()->clearLayout();
+            m_block.layout()->beginLayout();
+            foreach(const LineKeeper &lk, lineKeeps) {
+                line = m_block.layout()->createLine();
+                line.setNumColumns(lk.columns, lk.lineWidth);
+                line.setPosition(lk.position);
+            }
+            if(lineKeeps.isEmpty()) {
+                entireParagraphMoved = true;
+            }
         }
         if (m_data->endPosition() == -1) // no text at all fit in the shape!
             m_data->setEndPosition(m_data->position());
@@ -290,7 +313,7 @@ bool Layout::addLine(QTextLine &line, bool processingLine)
             m_textShape->markLayoutDone();
         nextShape();
         if (m_data)
-            m_data->setPosition(m_block.position() + (ignoreParagraph ? 0 : line.textStart()));
+            m_data->setPosition(m_block.position() + (entireParagraphMoved ? 0 : line.textStart()));
 
         // the demo-text feature means we have exactly the same amount of text as we have frame-space
         if (m_demoText)

commit 3c2126fa2e304fa713ca34198359d8147731f2c6

Fix regression introduced with r1178051 the dirty way. This solves the crash that happens if the document attached to bugreport 248343 is loaded. This 'fix' is only a temporary solution to prevent all kind of ugly crashes.
svn path=/branches/work/koffice-essen/; revision=1187135

plugins/textshape/Layout.cpp
@@ -291,6 +291,11 @@ bool Layout::addLine(QTextLine &line, bool processingLine)
             line = m_block.layout()->createLine(); //TODO should probably not create this line
             entireParagraphMoved = true;
         } else {
+//FIXME sebsauer 2010-10-18; this leads to crashes cause the clearLayout() will also invalidate m_block and other
+//state-variables what will then lead to a crash if they are accessed later (what they are in various places). See
+//here for example the document attached to BKO bug #248343 . So, this code introduced in r1178051 to fix layout
+//problems needs to be reworked to proper handle the state-variables.
+#if 0
             // unfortunately we can't undo a single line, so we undo entire paragraph
             // and redo all previous lines
             QList<LineKeeper> lineKeeps;
@@ -313,6 +318,7 @@ bool Layout::addLine(QTextLine &line, bool processingLine)
             if(lineKeeps.isEmpty()) {
                 entireParagraphMoved = true;
             }
+#endif
         }
         if (m_data->endPosition() == -1) // no text at all fit in the shape!
             m_data->setEndPosition(m_data->position());
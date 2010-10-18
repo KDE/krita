KDE/krita

commit 8a22595b55adba686c1208a6eee4438ccb237982

fix KoStore* memory leaks
We have a number of ways of dealing with KoStore* objects: delete
them, leak them, use a Finalizer or a Keeper class or hope that
KoOdfReadStore would delete it (which it doesn't).

This removes the leaks. For the future, a QSharedPointer approach
might be attractive.

svn path=/branches/work/koffice-essen/; revision=1183445

libs/main/KoDocument.cpp
@@ -867,12 +867,14 @@ bool KoDocument::saveNativeFormat(const QString & file)
         delete store;
         return false;
     }
-
+    bool success;
     if (oasis) {
-        return saveNativeFormatODF(store, mimeType);
+        success = saveNativeFormatODF(store, mimeType);
     } else {
-        return saveNativeFormatKOffice(store);
+        success = saveNativeFormatKOffice(store);
     }
+    delete store;
+    return success;
 }
 
 bool KoDocument::saveNativeFormatODF(KoStore *store, const QByteArray &mimeType)
@@ -1647,8 +1649,10 @@ bool KoDocument::loadNativeFormatFromStore(QByteArray &data)
     QBuffer buffer(&data);
     KoStore *store = KoStore::createStore(&buffer, KoStore::Read, "", backend);
 
-    if (store->bad())
+    if (store->bad()) {
+        delete store;
         return false;
+    }
 
     // Remember that the file was encrypted
     if (d->specialOutputFlag == 0 && store->isEncrypted() && !d->isImporting)

krita/plugins/formats/ora/ora_converter.cc
@@ -69,6 +69,9 @@ KisImageBuilder_Result OraConverter::buildImage(const KUrl& uri)
     KisOpenRasterStackLoadVisitor orslv(m_doc, &olc);
     orslv.loadImage();
     m_image = orslv.image();
+    
+    delete store;
+    
     return KisImageBuilder_RESULT_OK;
 
 }

krita/ui/kis_clipboard.cc
@@ -92,6 +92,7 @@ void KisClipboard::setClip(KisPaintDeviceSP selection, const QPoint& topLeft)
         if (!selection->write(store)) {
             selection->disconnect();
             store->close();
+	    delete store;
             return;
         }
         store->close();

libs/kokross/KoScriptingOdf.cpp
@@ -331,7 +331,9 @@ KoScriptingOdfStore::~KoScriptingOdfStore()
 bool KoScriptingOdfStore::hasFile(const QString &fileName)
 {
     KoStore *store = getReadStore();
-    return store ? store->hasFile(fileName) : false;
+    bool retval = store ? store->hasFile(fileName) : false;
+    delete store;
+    return retval;
 }
 
 bool KoScriptingOdfStore::isOpen() const
@@ -361,6 +363,7 @@ QObject *KoScriptingOdfStore::open(const QString &fileName)
     int errorLine, errorColumn;
     if (! doc.setContent(store->device(), &errorMsg, &errorLine, &errorColumn)) {
        kWarning(32010) << "Parse-Error message" << errorMsg << "line" << errorLine << "col" << errorColumn;
+       delete store;
        return 0;
     }
 
@@ -374,6 +377,9 @@ QObject *KoScriptingOdfStore::open(const QString &fileName)
         m_reader = new KoScriptingOdfManifestReader(this, doc);
     else
         m_reader = new KoScriptingOdfReader(this, doc);
+    
+    delete store;
+    
     return m_reader;
 }
 
@@ -393,6 +399,7 @@ QByteArray KoScriptingOdfStore::extract(const QString &fileName)
         store->close();
     QByteArray data;
     bool ok = store->extractFile(fileName, data);
+    delete store;
     return ok ? data : QByteArray();
 }
 
@@ -403,7 +410,9 @@ bool KoScriptingOdfStore::extractToFile(const QString &fileName, const QString &
         return false;
     if (store->isOpen())
         store->close();
-    return store->extractFile(fileName, toFileName);
+    bool retval = store->extractFile(fileName, toFileName);
+    delete store;
+    return retval;
 }
 
 QObject *KoScriptingOdfStore::document() const

libs/koreport/renderer/ods/KoSimpleOdsDocument.cpp
@@ -26,10 +26,12 @@
 
 KoSimpleOdsDocument::KoSimpleOdsDocument()
 {
-
+    m_store = 0;
 }
+
 KoSimpleOdsDocument::~KoSimpleOdsDocument()
 {
+    delete m_store;
     qDeleteAll(m_worksheets);
 }
 
@@ -43,6 +45,11 @@ void KoSimpleOdsDocument::addSheet(KoSimpleOdsSheet* sheet)
 QFile::FileError KoSimpleOdsDocument::saveDocument(const QString& path)
 {
     // create output store
+    if (m_store) {
+	delete m_store;
+	m_store = 0;
+    }
+    
     m_store = KoStore::createStore(path, KoStore::Write,
                                     "application/vnd.oasis.opendocument.spreadsheet", KoStore::Zip);
     if (!m_store) {
@@ -55,10 +62,12 @@ QFile::FileError KoSimpleOdsDocument::saveDocument(const QString& path)
 
     if (!createContent(&oasisStore)) {
         delete m_store;
+	m_store = 0;
         return QFile::WriteError;
     }
 
     delete m_store;
+    m_store = 0;
     return QFile::NoError;
 
 }

libs/kotext/opendocument/tests/TestChangeLoading.cpp
@@ -145,9 +145,10 @@ QString TestChangeLoading::documentToOdt(QTextDocument *document)
     KoOdfWriteStore odfWriteStore(store);
     KoXmlWriter *manifestWriter = odfWriteStore.manifestWriter("application/vnd.oasis.opendocument.text");
     manifestWriter->addManifestEntry("content.xml", "text/xml");
-    if (!store->open("content.xml"))
+    if (!store->open("content.xml")) {
+	delete store;
         return QString();
-
+    }
     KoStoreDevice contentDev(store);
     KoXmlWriter* contentWriter = KoOdfWriteStore::createOasisXmlWriter(&contentDev, "office:document-content");
 

libs/kotext/opendocument/tests/TestLoading.cpp
@@ -1071,8 +1071,10 @@ QString TestLoading::documentToOdt(QTextDocument *document)
     KoOdfWriteStore odfWriteStore(store);
     KoXmlWriter *manifestWriter = odfWriteStore.manifestWriter("application/vnd.oasis.opendocument.text");
     manifestWriter->addManifestEntry("content.xml", "text/xml");
-    if (!store->open("content.xml"))
+    if (!store->open("content.xml")) {
+	delete store;
         return QString();
+    }
 
     KoStoreDevice contentDev(store);
     KoXmlWriter* contentWriter = KoOdfWriteStore::createOasisXmlWriter(&contentDev, "office:document-content");
@@ -1150,7 +1152,6 @@ QString TestLoading::documentToOdt(QTextDocument *document)
     mainStyles.saveOdfStylesDotXml(store, manifestWriter);
 
     odfWriteStore.closeManifestWriter();
-
 
     delete store;
     delete textShapeData;

libs/main/tests/documentstructuretest.cpp
@@ -70,10 +70,12 @@ static KoDocumentInfo *loadDocumentInfo(const QString &odt)
     KoDocumentInfo *documentInfo = new KoDocumentInfo;
     QString error;
     if (!odfReadStore.loadAndParse("meta.xml", metaDoc, error)) {
+	delete store;
         delete documentInfo;
         return 0;
     }
     if (!documentInfo->loadOasis(metaDoc)) {
+	delete store;
         delete documentInfo;
         return 0;
     }

commit 0d642511ffbb58292aa3c91d215c9c1ee288f17b

Fix regression introduced with r1183445 (wasn't forward-ported to trunk) that resulted in a double-free of the KoStore (saveNativeFormatODF and saveNativeFormatKOffice to delete the passed in KoStore already).
svn path=/branches/work/koffice-essen/; revision=1187186

libs/main/KoDocument.cpp
@@ -867,14 +867,11 @@ bool KoDocument::saveNativeFormat(const QString & file)
         delete store;
         return false;
     }
-    bool success;
     if (oasis) {
-        success = saveNativeFormatODF(store, mimeType);
+        return saveNativeFormatODF(store, mimeType);
     } else {
-        success = saveNativeFormatKOffice(store);
+        return saveNativeFormatKOffice(store);
     }
-    delete store;
-    return success;
 }
 
 bool KoDocument::saveNativeFormatODF(KoStore *store, const QByteArray &mimeType)
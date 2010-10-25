d98edd2eef369af388debdf1dd2b68d8a992b474
Cyrille Berger <cyb@lepi.org>
2010-10-25 14:18:20 +0000

    update version to beta 3
    svn path=/trunk/koffice/; revision=1189663

--- libs/main/kofficeversion.h
+++ libs/main/kofficeversion.h
@@ -33,7 +33,7 @@
 * it contains the version at compile time. See KOffice::versionString() if you need
 * the KOffice version used at runtime.
 */
-#define KOFFICE_VERSION_STRING "2.3 Beta 2"
+#define KOFFICE_VERSION_STRING "2.3 Beta 3"
 
 /**
  * @def KOFFICE_VERSION_MAJOR
@@ -54,7 +54,7 @@
  * @ingroup KOfficeMacros
  * @brief Release version of KOffice, at compile time
 */
-#define KOFFICE_VERSION_RELEASE 82
+#define KOFFICE_VERSION_RELEASE 83
 
 /**
  * @ingroup KOfficeMacros
@@ -77,7 +77,7 @@
  * @ingroup KOfficeMacros
  * @brief If defined, indicates at compile time that KOffice is in beta stage
  */
-#define KOFFICE_BETA 2
+#define KOFFICE_BETA 3
 
 /**
  * @def KOFFICE_RC

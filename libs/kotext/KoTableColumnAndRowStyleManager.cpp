da318b5afcd38d9b41924a5b46207db0f4662bdb
Casper Boemann <cbr@boemann.dk>
2010-11-07 14:48:38 +0000

    fix unittests failing in the textshape
    svn path=/branches/work/koffice-essen/; revision=1193907

--- libs/kotext/KoTableColumnAndRowStyleManager.cpp
+++ libs/kotext/KoTableColumnAndRowStyleManager.cpp
@@ -68,16 +68,18 @@ KoTableColumnAndRowStyleManager::~KoTableColumnAndRowStyleManager()
 KoTableColumnAndRowStyleManager KoTableColumnAndRowStyleManager::getManager(QTextTable *table)
 {
     QTextTableFormat tableFormat = table->format();
-
+qDebug()<<"getmanager";
     if (tableFormat.hasProperty(KoTableStyle::ColumnAndRowStyleManager)) {
+qDebug()<<"getmanager returning from format";
         return  tableFormat.property(KoTableStyle::ColumnAndRowStyleManager).value<KoTableColumnAndRowStyleManager>();
     } else {
         KoTableColumnAndRowStyleManager carsManager;
+qDebug()<<"getmanager returning new one";
 
         QVariant var;
         var.setValue(carsManager);
         tableFormat.setProperty(KoTableStyle::ColumnAndRowStyleManager, var);
-
+        table->setFormat(tableFormat);
         return carsManager;
     }
 }

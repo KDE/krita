13160bab683afba2eeff1ab7c4d9f936dc41cdc8
Carlos Licea <carlos_licea@hotmail.com>
2010-11-10 02:07:37 +0000

    Add a KoCellStyle. Represents a style to be applied to any cell.
    svn path=/branches/work/koffice-essen/; revision=1194881

---
+++ libs/odf/KoCellStyle.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2010 Carlos Licea <carlos@kdab.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "KoCellStyle.h"
+#include "KoGenStyle.h"
+#include "KoGenStyles.h"
+
+KoCellStyle::KoCellStyle()
+{
+}
+
+KoCellStyle::~KoCellStyle()
+{
+}
+
+KoBorder* KoCellStyle::borders()
+{
+    return m_borders;
+}
+
+QString KoCellStyle::save(KoGenStyles& styles) const
+{
+    KoGenStyle style;
+    m_borders->saveOdf(style);
+
+    return styles.insert(style);
+}
+

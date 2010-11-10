f8bd6b9231c0f0f0283d11b17c182b4ef5b1c666
Carlos Licea <carlos_licea@hotmail.com>
2010-11-10 02:07:59 +0000

    Defualt KoCellValue that does just nothing very gracefully.
    svn path=/branches/work/koffice-essen/; revision=1194888

---
+++ libs/odf/KoDummyCellValue.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2010 Carlos Licea <carlos@kdab.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KODUMMYCELLVALUE_H
+#define KODUMMYCELLVALUE_H
+
+#include "KoCellValue.h"
+
+class KoDummyCellValue : public KoCellValue
+{
+public:
+    KoDummyCellValue();
+    ~KoDummyCellValue();
+
+protected:
+
+    virtual QString type() const;
+    virtual QList< QPair<QString, QString> > attributes()  const;
+};
+
+#endif

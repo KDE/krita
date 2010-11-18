ab051fd9e640874be964bd6d7f4cfc00136223e2
Sebastian Sauer <mail@dipe.org>
2010-11-18 16:15:44 +0000

    and followup of r1198461; still not correct. Looks as there is more work needed to figure the correct place out that doesn't result in the KoPAPageContainerModel being called on exit
    svn path=/branches/work/koffice-essen/; revision=1198478

--- plugins/textshape/TextShape.cpp
+++ plugins/textshape/TextShape.cpp
@@ -462,16 +462,6 @@ void TextShape::markLayoutDone()
     synchronized(m_mutex) {
         m_waiter.wakeAll();
     }
-    
-    if (KoTextDocument(m_textShapeData->document()).resizeMethod() == KoTextDocument::ShrinkToFitResize) {
-        // If Shrink-to-fit is enabled we need to let our model know when layouting is finally done
-        // so the ShrinkToFitShapeContainer is able to do it's job. This special-casing is needed
-        // cause if shrink-to-fit is enable we have to emit KoShape::SizeChanged even if the shape's
-        // size didn't change but only the content's size did to be able to recalc.
-        if (KoShapeContainer *c = parent())
-            if (KoShapeContainerModel *m = c->model())
-                m->containerChanged(c, KoShape::SizeChanged);
-    }
 }
 
 void TextShape::update() const

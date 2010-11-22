ae69e0a75174788622ca37cda598d5d2e46913fe
Pavol Korinek <pavol.korinek@ixonos.com>
2010-11-22 15:13:17 +0000

    loading of right variable char format fixed
    svn path=/trunk/koffice/; revision=1199737

--- libs/kotext/opendocument/KoTextLoader.cpp
+++ libs/kotext/opendocument/KoTextLoader.cpp
@@ -1071,7 +1071,7 @@ void KoTextLoader::loadSpan(const KoXmlElement &element, QTextCursor &cursor, bo
                     if (textObjectManager) {
                         KoVariableManager *varManager = textObjectManager->variableManager();
                         if (varManager) {
-                            textObjectManager->insertInlineObject(cursor, obj);
+                            textObjectManager->insertInlineObject(cursor, obj, cursor.charFormat());
                         }
                     }
                 }

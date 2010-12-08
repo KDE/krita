KDE/calligra

commit 02f875de548926ec8d06eb0617455020a1036cb2

Make sure we emit offset changes more often as it's needed to keep for example the rulers in KWord up to date.
svn path=/branches/work/koffice-essen/; revision=1195776

libs/flake/KoCanvasControllerWidget.cpp
@@ -219,6 +219,7 @@ KoCanvasControllerWidget::KoCanvasControllerWidget(QWidget *parent)
 
     connect(horizontalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(updateCanvasOffsetX()));
     connect(verticalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(updateCanvasOffsetY()));
+    connect(d->viewportWidget, SIGNAL(sizeChanged()), this, SLOT(updateCanvasOffsetX()));
     connect(proxyObject, SIGNAL(moveDocumentOffset(const QPoint&)), d->viewportWidget, SLOT(documentOffsetMoved(const QPoint&)));
 }
 
@@ -515,6 +516,7 @@ void KoCanvasControllerWidget::setToolOptionWidgets(const QMap<QString, QWidget 
 
 void KoCanvasControllerWidget::updateDocumentSize(const QSize &sz, bool recalculateCenter)
 {
+    qDebug() << "newsize of canvas"<<sz;
     if (!recalculateCenter) {
         // assume the distance from the top stays equal and recalculate the center.
         setPreferredCenterFractionX(documentSize().width() * preferredCenterFractionX() / sz.width());
@@ -528,11 +530,9 @@ void KoCanvasControllerWidget::updateDocumentSize(const QSize &sz, bool recalcul
     d->resetScrollBars();
     d->ignoreScrollSignals = oldIgnoreScrollSignals;
 
-    // in case the document got so small a slider dissapeared; emit the new offset.
-    if (horizontalScrollBar()->isHidden())
-        updateCanvasOffsetX();
-    if (verticalScrollBar()->isHidden())
-        updateCanvasOffsetY();
+    // Always emit the new offset.
+    updateCanvasOffsetX();
+    updateCanvasOffsetY();
 }
 
 void KoCanvasControllerWidget::setZoomWithWheel(bool zoom)

libs/flake/KoCanvasControllerWidgetViewport_p.cpp
@@ -364,6 +364,7 @@ void Viewport::resetLayout()
     if (m_drawShadow)
       update();
 
+    emit sizeChanged();
 #if 0
      kDebug(30006) <<"View port geom:" << geometry();
      if (m_canvas)

libs/flake/KoCanvasControllerWidgetViewport_p.h
@@ -50,6 +50,9 @@ public:
 public slots:
     void documentOffsetMoved(const QPoint &);
 
+signals:
+    void sizeChanged();
+
 public:
 
     void handleDragEnterEvent(QDragEnterEvent *event);

KDE/krita

commit 07e13d7b26d460bb16a185c3eb8b3d1a0117be41

make sure the canvas is set before calling viewportWidget->setCanvas, this is needed because this function will then trigger a signal that will call updateCanvasOffsetX. And initialization this can call an inva
I got this backtrace with braindump:

#6  0x00007ffff6d3998e in KoCanvasControllerWidget::canvasOffsetX (this=0xe22330) at /home/cyrille/Projects/kde4/src/calligra/libs/flake/KoCanvasControllerWidget.cpp:335
#7  0x00007ffff6d39c5f in KoCanvasControllerWidget::updateCanvasOffsetX (this=0x2b7f) at /home/cyrille/Projects/kde4/src/calligra/libs/flake/KoCanvasControllerWidget.cpp:362
#8  0x00007ffff6d3a6c4 in KoCanvasControllerWidget::qt_metacall (this=0xe22330, _c=QMetaObject::InvokeMetaMethod, _id=<value optimized out>, _a=0x7fffffffdb00)
    at /home/cyrille/Projects/kde4/build/calligra/libs/flake/KoCanvasControllerWidget.moc:86
#9  0x00007ffff3b206d7 in QMetaObject::activate (sender=0x3d95610, m=<value optimized out>, local_signal_index=<value optimized out>, argv=0xffffffffffffffff)
    at kernel/qobject.cpp:3280
#10 0x00007ffff6d3b4da in Viewport::resetLayout (this=0x3d95610) at /home/cyrille/Projects/kde4/src/calligra/libs/flake/KoCanvasControllerWidgetViewport_p.cpp:367
#11 0x00007ffff6d39e76 in KoCanvasControllerWidget::setCanvas (this=0xe22330, canvas=0x4452868)
    at /home/cyrille/Projects/kde4/src/calligra/libs/flake/KoCanvasControllerWidget.cpp:272

This change does not need to be backported to branch/2.3, as I suspect it is caused by 02f875de548926ec8d06eb0617455020a1036cb2 which was commited to koffice-essen as r1195776.

libs/flake/KoCanvasControllerWidget.cpp
@@ -269,8 +269,8 @@ void KoCanvasControllerWidget::setCanvas(KoCanvasBase *canvas)
         d->canvas->canvasWidget()->removeEventFilter(this);
     }
     canvas->setCanvasController(this);
-    d->viewportWidget->setCanvas(canvas->canvasWidget());
     d->canvas = canvas;
+    d->viewportWidget->setCanvas(canvas->canvasWidget());
     d->canvas->canvasWidget()->installEventFilter(this);
     d->canvas->canvasWidget()->setMouseTracking(true);
     setFocusProxy(d->canvas->canvasWidget());
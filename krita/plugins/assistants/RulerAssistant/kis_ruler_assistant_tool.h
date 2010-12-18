a26938cafa2ff50fd35e2af328eddb641a1fde1d
Geoffry Song <goffrie@gmail.com>
2010-12-18 18:55:25 -0500

    assistants: saving and loading

--- krita/plugins/assistants/RulerAssistant/kis_ruler_assistant_tool.h
+++ krita/plugins/assistants/RulerAssistant/kis_ruler_assistant_tool.h
@@ -26,6 +26,7 @@
 class RulerDecoration;
 class KisCanvas2;
 class ConstraintSolver;
+class KJob;
 
 class KisRulerAssistantTool : public KisTool
 {
@@ -47,6 +48,12 @@ public slots:
     void deactivate();
 private slots:
     void removeAllAssistants();
+
+    void saveAssistants();
+    void loadAssistants();
+
+    void saveFinish(KJob* job);
+    void openFinish(KJob* job);
 protected:
 
     virtual void paint(QPainter& gc, const KoViewConverter &converter);

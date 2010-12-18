91c919236644d53c4c494dc483e6385081a2cd7e
Cyrille Berger <cberger@cberger.net>
2010-12-18 11:09:58 +0100

    add a widget with a combobox to select a multiplier for a slider spin box

---
+++ krita/ui/widgets/kis_multiplers_double_slider_spinbox.cpp
@@ -0,0 +1,46 @@
+/* This file is part of the KDE project
+ * Copyright (c) 2010 Cyrille Berger <cberger@cberger.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_multiplers_double_slider_spinbox.h"
+
+#include "ui_wdgmultiplersdoublesliderspinbox.h"
+
+struct KisMultipliersDoubleSliderSpinBox::Private {
+    Ui::WdgMultipliersDoubleSliderSpinBox form;
+};
+
+KisMultipliersDoubleSliderSpinBox::KisMultipliersDoubleSliderSpinBox(QWidget* _parent) : QWidget(_parent), d(new Private)
+{
+    d->form.setupUi(this);
+}
+
+KisMultipliersDoubleSliderSpinBox::~KisMultipliersDoubleSliderSpinBox()
+{
+    delete d;
+}
+
+KisDoubleSliderSpinBox* KisMultipliersDoubleSliderSpinBox::spinBox()
+{
+    return d->form.sliderSpinBox;
+}
+
+void KisMultipliersDoubleSliderSpinBox::addMultiplier(double v)
+{
+  
+}

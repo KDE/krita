ce4616c116aa8500e2e8b362d597609f34d6bfed
Cyrille Berger <cberger@cberger.net>
2010-12-18 10:07:08 +0100

    Geoffry holds copyright on those files

--- krita/plugins/assistants/RulerAssistant/kis_ruler_assistant_tool.cc
+++ krita/plugins/assistants/RulerAssistant/kis_ruler_assistant_tool.cc
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008 Cyrille Berger <cberger@cberger.net>
+ * Copyright (c) 2010 Geoffry Song <goffrie@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by

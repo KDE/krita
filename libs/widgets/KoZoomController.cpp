fc9f3ae0bf5671d98ac4e8ea4aad074a6155bb39
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-01-11 01:52:50 +0100

    Better fix for kword shakes / shivers while opening this attached document. BUG:262810

--- libs/widgets/KoZoomController.cpp
+++ libs/widgets/KoZoomController.cpp
@@ -24,9 +24,6 @@
 #include <KActionCollection>
 #include <KLocale>
 #include <KDebug>
-#include <QApplication>
-#include <QStyle>
-#include <QStyleOptionSlider>
 
 #include <KoZoomHandler.h>
 #include <KoCanvasBase.h>
@@ -150,12 +147,7 @@ void KoZoomController::setZoom(KoZoomMode::Mode mode, qreal zoom)
         d->action->setZoom(zoom);
     }
     else if(mode == KoZoomMode::ZOOM_WIDTH) {
-        int margin = 2*d->fitMargin - 2;
-        if(QStyle *style = qApp->style()) {
-            QStyleOptionSlider opt;
-            margin -= style->pixelMetric(QStyle::PM_ScrollBarExtent, &opt);
-        }
-        zoom = (d->canvasController->viewportSize().width() - qMax(0, margin))
+        zoom = (d->canvasController->viewportSize().width() - 2*d->fitMargin)
                          / (d->zoomHandler->resolutionX() * d->pageSize.width());
         d->action->setSelectedZoomMode(mode);
         d->action->setEffectiveZoom(zoom);

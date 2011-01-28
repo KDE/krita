35f79a7a86091332a5a84ea79d86236e64128053
Ganesh Paramasivam <ganesh@crystalfab.com>
2011-01-28 11:57:58 +0530

    Verification and addition of unit-test to verify Pragraph Style Changes

--- libs/kotext/opendocument/tests/TestChangeTracking.cpp
+++ libs/kotext/opendocument/tests/TestChangeTracking.cpp
@@ -281,6 +281,7 @@ void TestChangeTracking::testChangeTracking_data()
     //Text Style Changes
     QTest::newRow("Text Made Bold")  << "ChangeTracking/styling/text-made-bold/text-made-bold-tracked.odt";
     QTest::newRow("Bold Made Normal")  << "ChangeTracking/styling/bold-text-unstyled/bold-text-unstyled-tracked.odt";
+    QTest::newRow("Paragraph Style Changes")  << "ChangeTracking/styling/create-new-style-for-para/new-style-for-para-tracked.odt";
 
     //Complex Delete Merges
     QTest::newRow("Paragraph with list-item - 1")  << "ChangeTracking/complex-delete-merges/paragraph-merge-with-list-item/paragraph-merge-with-list-item-tracked.odt";

429a99f45f467b0b521d26c74cd8dae4709cf600
Cyrille Berger <cberger@cberger.net>
2011-02-08 10:14:32 +0100

    use the calligra cdash project page

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,10 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "koffice")
+set(CTEST_PROJECT_NAME "calligra")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=koffice")
+set(CTEST_DROP_LOCATION "/submit.php?project=calligra")
 set(CTEST_DROP_SITE_CDASH TRUE)

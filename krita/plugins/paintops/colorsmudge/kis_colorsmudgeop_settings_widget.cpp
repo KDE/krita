e421ab2b49235a4ed8cac85f410f52a6c75d54de
Silvio Heinrich <plassy@web.de>
2011-03-16 00:50:33 +0100

    Created a new PaintOp plugin for the color smudge feature.

---
+++ krita/plugins/paintops/colorsmudge/kis_colorsmudgeop_settings_widget.cpp
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (C) 2011 Silvio Heinrich <plassy@web.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_colorsmudgeop_settings_widget.h"
+#include "kis_brush_based_paintop_settings.h"
+#include <kis_properties_configuration.h>
+#include <kis_paintop_options_widget.h>
+#include <kis_pressure_size_option.h>
+#include <kis_pressure_spacing_option.h>
+#include <kis_pressure_rate_option.h>
+#include <kis_curve_option_widget.h>
+#include <kis_pressure_rotation_option.h>
+#include <kis_pressure_scatter_option_widget.h>
+#include <kis_merged_paint_option.h>
+
+#include "kis_pressure_rate_option_widget.h"
+
+KisColorSmudgeOpSettingsWidget::KisColorSmudgeOpSettingsWidget(QWidget* parent):
+    KisBrushBasedPaintopOptionWidget(parent)
+{
+    setObjectName("brush option widget");
+
+    addPaintOpOption(new KisCurveOptionWidget(new KisPressureSizeOption()));
+    addPaintOpOption(new KisCurveOptionWidget(new KisPressureSpacingOption()));
+    addPaintOpOption(new KisPressureRateOptionWidget(i18n("Smudge Rate"), i18n("Rate: "), "SmudgeRate", true));
+    addPaintOpOption(new KisPressureRateOptionWidget(i18n("Color Rate") , i18n("Rate: "), "ColorRate" , false));
+    addPaintOpOption(new KisCurveOptionWidget(new KisPressureRotationOption()));
+    addPaintOpOption(new KisPressureScatterOptionWidget());
+    addPaintOpOption(new KisMergedPaintOptionWidget());
+}
+
+KisColorSmudgeOpSettingsWidget::~KisColorSmudgeOpSettingsWidget() { }
+
+KisPropertiesConfiguration* KisColorSmudgeOpSettingsWidget::configuration() const
+{
+    KisBrushBasedPaintOpSettings *config = new KisBrushBasedPaintOpSettings();
+    config->setOptionsWidget(const_cast<KisColorSmudgeOpSettingsWidget*>(this));
+    config->setProperty("paintop", "colorsmudge");
+    writeConfiguration(config);
+    return config;
+}
+
+
+#include "kis_colorsmudgeop_settings_widget.moc"

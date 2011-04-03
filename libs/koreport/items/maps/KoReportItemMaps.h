6c03d3b3302f1ced5e9bd5bdea77b429f31cdd3a
Radoslaw Wicik <rockford@wicik.pl>
2011-04-03 21:33:30 +0200

    FEATURE: Maps in KoReports

---
+++ libs/koreport/items/maps/KoReportItemMaps.h
@@ -0,0 +1,74 @@
+/*
+ * Kexi Report Plugin
+ * Copyright (C) 2007-2008 by Adam Pigg (adam@piggz.co.uk)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef KRIMAGEDATA_H
+#define KRIMAGEDATA_H
+#include <KoReportItemBase.h>
+#include <QRect>
+#include <QPainter>
+#include <qdom.h>
+#include "krpos.h"
+#include "krsize.h"
+#include <koproperty/Property.h>
+#include <koproperty/Set.h>
+#include <KoGlobal.h>
+#include <kdebug.h>
+#include <klocalizedstring.h>
+#include <kglobalsettings.h>
+
+namespace Scripting
+{
+class Image;
+}
+
+/**
+ @author
+*/
+class KoReportItemImage : public KoReportItemBase
+{
+public:
+    KoReportItemImage() {
+        createProperties();
+    }
+    KoReportItemImage(QDomNode & element);
+    virtual ~KoReportItemImage();
+
+    virtual QString typeName() const;
+    virtual int render(OROPage* page, OROSection* section,  QPointF offset, QVariant data, KRScriptHandler *script);
+    using KoReportItemBase::render;
+
+    virtual QString itemDataSource() const;
+
+protected:
+    KoProperty::Property * m_controlSource;
+    KoProperty::Property* m_resizeMode;
+    KoProperty::Property* m_staticImage;
+
+    void setMode(const QString&);
+    void setInlineImageData(QByteArray, const QString& = QString());
+    void setColumn(const QString&);
+    QString mode() const;
+    bool isInline() const;
+    QByteArray inlineImageData() const;
+    
+private:
+    virtual void createProperties();
+
+    friend class Scripting::Image;
+};
+
+#endif

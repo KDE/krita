277d3ce0d5ed9ff6ab597d2bfe11929ffed7903b
Elvis Stansvik <elvstone@gmail.com>
2011-05-07 16:52:26 +0200

    Document TableIterator class.

--- libs/textlayout/TableIterator.h
+++ libs/textlayout/TableIterator.h
@@ -26,23 +26,55 @@
 class FrameIterator;
 class KoTextLayoutArea;
 
+/**
+ * Convenience cursor class used during table layout.
+ *
+ * The class holds information about a table and the current row. Can also
+ * provide a FrameIterator for a given column in the table.
+ */
 class TableIterator
 {
 public:
+    /**
+     * Constructs a new iterator for the given table.
+     *
+     * @param table table to use.
+     */
     TableIterator(QTextTable *table);
+
+    /**
+     * Constructs a new iterator initialized from another.
+     *
+     * @param other iterator to initialize the iterator from.
+     */
     TableIterator(TableIterator *other);
+
+    /// Destructor.
     ~TableIterator();
 
+    /// Compare this iterator to another.
     bool operator ==(const TableIterator &other);
 
+    /**
+     * Returns a frame iterator that iterates over the frames in a given column.
+     *
+     * @param column column for which and iterator should be returned.
+     */
     FrameIterator *frameIterator(int column);
 
+    /// Table being iterated over.
     QTextTable *table;
+    /// Current row.
     int row;
+    /// Number of header rows.
     int headerRows;
+    /// X position of header.
     qreal headerPositionX;
+    /// Frame iterators, one for each column.
     QVector<FrameIterator *> frameIterators;
+    /// Header row positions.
     QVector<qreal> headerRowPositions; // we will only fill those of header rows
+    /// Rows of header cell areas.
     QVector<QVector<KoTextLayoutArea *> > headerCellAreas;
 };
 

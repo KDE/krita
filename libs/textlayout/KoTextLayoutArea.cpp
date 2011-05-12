99174aa3c199ba488e86949d562e39b672555455
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-05-12 10:28:37 +0200

    Fix past end-iteration. The first case I was able to run into with the blank document template just the moment I added the first char. Since I wasn't able to trigger the second case I at least added an assert so we don't silently eat such cases. There seems to be another case which can result in Words freezing but I didn't figured out the reason yet.

--- libs/textlayout/KoTextLayoutArea.cpp
+++ libs/textlayout/KoTextLayoutArea.cpp
@@ -113,7 +113,8 @@ int KoTextLayoutArea::hitTest(const QPointF &p, Qt::HitTestAccuracy accuracy) co
     QTextFrame::iterator it = m_startOfArea->it;
     QTextFrame::iterator stop = m_endOfArea->it;
     if(!stop.currentBlock().isValid() || m_endOfArea->lineTextStart >= 0) {
-        ++stop;
+        if(!stop.atEnd())
+            ++stop;
     }
     int tableAreaIndex = 0;
     int tocIndex = 0;
@@ -149,6 +150,7 @@ int KoTextLayoutArea::hitTest(const QPointF &p, Qt::HitTestAccuracy accuracy) co
 
         QTextLayout *layout = block.layout();
         QTextFrame::iterator next = it;
+        Q_ASSERT(!next.atEnd());
         next++;
         if (next != stop && point.y() > layout->boundingRect().bottom()) {
             // just skip this block.

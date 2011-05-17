7a62e9f6069a2673dc87c003eae745b94a75c714
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-05-17 09:09:01 +0200

    Fix regression that was resulting in adding new text sometimes it wasn't proper displayed. This also fixes the failed TestBlockLayout-unittest. That shows why unittests are such important. Lot of thanks to Cyrille for pointing me at that case.

--- libs/textlayout/KoTextDocumentLayout.cpp
+++ libs/textlayout/KoTextDocumentLayout.cpp
@@ -247,10 +247,10 @@ void KoTextDocumentLayout::documentChanged(int position, int charsRemoved, int c
 
     // Mark the previous of the corresponding and all following root areas as dirty.
     KoTextLayoutRootArea *area = rootAreaForPosition(position);
-    if (!area)
-        return;
-    for(int i = qMax(0, d->rootAreaList.indexOf(area) - 1); i < d->rootAreaList.count(); ++i)
-        d->rootAreaList[i]->setDirty();
+    int startFromIndex = area ? qMax(0, d->rootAreaList.indexOf(area) - 1) : 0;
+    for(int i = startFromIndex; i < d->rootAreaList.count(); ++i)
+        if (!d->rootAreaList[i]->isDirty())
+            d->rootAreaList[i]->setDirty();
 
     emitLayoutIsDirty();
 }

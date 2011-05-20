42ab99e9e859af77659edd4c93bbf4f2db4383a4
Casper Boemann <cbo@boemann.dk>
2011-05-20 23:31:29 +0200

    We need to set the documentLayout on the ToC document in order to layout correctly.

--- libs/textlayout/KoTextLayoutArea.cpp
+++ libs/textlayout/KoTextLayoutArea.cpp
@@ -392,6 +392,7 @@ bool KoTextLayoutArea::layout(FrameIterator *cursor)
         } else if (block.isValid()) {
             if (block.blockFormat().hasProperty(KoParagraphStyle::TableOfContentsDocument)) {
                 QVariant data = block.blockFormat().property(KoParagraphStyle::TableOfContentsDocument);
+                data.value<QTextDocument *>()->setDocumentLayout(documentLayout());
                 QTextFrame *tocFrame = data.value<QTextDocument *>()->rootFrame();
                 data = block.blockFormat().property(KoParagraphStyle::TableOfContentsData);
                 KoTableOfContentsGeneratorInfo *tocInfo = data.value<KoTableOfContentsGeneratorInfo *>();
@@ -793,6 +794,21 @@ bool KoTextLayoutArea::layoutBlock(FrameIterator *cursor)
 
     // Set up a var to keep track of where last added tab is. Conversion of tabOffset is required because Qt thinks in device units and we don't
     qreal position = tabOffset;
+
+    if (!variant.isNull()) {
+        foreach(const QVariant &tv, qvariant_cast<QList<QVariant> >(variant)) {
+            KoText::Tab koTab = tv.value<KoText::Tab>();
+            QTextOption::Tab tab;
+
+            // conversion here is required because Qt thinks in device units and we don't
+            position = (koTab.position + tabOffset) * qt_defaultDpiY() / 72. -3;
+
+            tab.position = position;
+            tab.type = koTab.type;
+            tab.delimiter = koTab.delimiter;
+            tabs.append(tab);
+        }
+    }
 
     if (!tabs.isEmpty()) {
         //unfortunately the tabs are not guaranteed to be ordered, so lets do that ourselves

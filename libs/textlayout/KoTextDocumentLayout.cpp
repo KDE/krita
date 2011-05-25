4f23c903d97b8f93390a74acd55bbaee6dacf5ae
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-05-25 23:49:19 +0200

    Extend the usDirty-logic by taking also the range of chars removed and added into account.

--- libs/textlayout/KoTextDocumentLayout.cpp
+++ libs/textlayout/KoTextDocumentLayout.cpp
@@ -245,17 +245,20 @@ void KoTextDocumentLayout::documentChanged(int position, int charsRemoved, int c
         from = block.position() + block.length();
     }
 
-    // Mark the the corresponding and it's previous and next root-areas as dirty. Also the previous and
-    // next cause changes on the to the position corresponding root-area can also affect this previous page.
-    // If there is no root-area for the position then we need to mark the first root-area as dirty.
-    KoTextLayoutRootArea *area = rootAreaForPosition(position);
-    int startFromIndex = area ? d->rootAreaList.indexOf(area) : 0;
-    KoTextLayoutRootArea* prevRootArea = (startFromIndex >= 1 && startFromIndex - 1 < d->rootAreaList.count()) ? d->rootAreaList[startFromIndex - 1] : 0;
-    if (prevRootArea) prevRootArea->setDirty();
-    KoTextLayoutRootArea* rootArea = (startFromIndex >= 0 && startFromIndex < d->rootAreaList.count()) ? d->rootAreaList[startFromIndex] : 0;
-    if (rootArea) rootArea->setDirty();
-    KoTextLayoutRootArea* nextRootArea = (startFromIndex >= -1 && startFromIndex + 1 < d->rootAreaList.count()) ? d->rootAreaList[startFromIndex + 1] : 0;
-    if (nextRootArea) nextRootArea->setDirty();
+    // Mark the to the position corresponding root-areas as dirty. We determinate the range of the root-areas
+    // the added and removed chars span. Additionaly the previous and following root-area of that range are
+    // selected too cause they can also be affect by changes done to the range. All that root-areas then marked
+    // dirty. If there is no root-area for the position then we don't need to mark anything dirty but still
+    // need to go on to force a scheduled relayout.
+    if (!d->rootAreaList.isEmpty()) {
+        KoTextLayoutRootArea *fromArea = rootAreaForPosition(position);
+        KoTextLayoutRootArea *toArea = fromArea ? rootAreaForPosition(position + qMax(charsRemoved, charsAdded)) : 0;
+        int startIndex = qMax(0, fromArea ? d->rootAreaList.indexOf(fromArea) - 1 : 0);
+        int endIndex = qMax(startIndex, qMin(d->rootAreaList.count() - 1, ((toArea && toArea != fromArea) ? d->rootAreaList.indexOf(toArea) : startIndex) + 1));
+        for(int i = startIndex; i <= endIndex; ++i) {
+            d->rootAreaList[i]->setDirty();
+        }
+    }
 
     // Once done we emit the layoutIsDirty signal. The consumer (e.g. the TextShape) will then layout dirty
     // root-areas and if needed following ones which got dirty cause content moved to them. Also this will

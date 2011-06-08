3228d9c7d073d4634909e7bd306493fc06415173
Boudewijn Rempt <boud@valdyas.org>
2011-06-08 15:35:02 +0200

    PictureShape: Make it possible to create an image shape with an image
    Implement the createShape() method to look for a parameter called
    "qimage" that contains a QImage and use that to properly create an
    image shape programmatically.

--- plugins/pictureshape/PictureShapeFactory.h
+++ plugins/pictureshape/PictureShapeFactory.h
@@ -29,6 +29,7 @@ public:
     ~PictureShapeFactory() {}
 
     virtual KoShape *createDefaultShape(KoResourceManager *documentResources = 0) const;
+    virtual KoShape *createShape(const KoProperties *params, KoResourceManager *documentResources = 0) const;
     virtual bool supports(const KoXmlElement &e, KoShapeLoadingContext &context) const;
 
     /// reimplemented

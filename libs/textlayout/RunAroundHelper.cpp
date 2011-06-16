KDE/calligra

commit 1653679a684e3b4b12a3ed9ca0c60a7d1409077e

wrapping fix
If wrapping object is crossing the right side of page content, line rectangles are not created correctly

libs/textlayout/RunAroundHelper.cpp
@@ -148,6 +148,7 @@ void RunAroundHelper::createLineParts()
     } else {
         QList<QRectF> lineParts;
         QRectF rightLineRect = m_lineRect;
+        bool lastRightRectValid = false;
         qSort(m_validObstructions.begin(), m_validObstructions.end(), KoTextLayoutObstruction::compareRectLeft);
         // Devide rect to parts, part can be invalid when obstructions are not disjunct.
         foreach (KoTextLayoutObstruction *validObstruction, m_validObstructions) {
@@ -156,10 +157,15 @@ void RunAroundHelper::createLineParts()
             QRectF lineRect = validObstruction->getRightLinePart(rightLineRect);
             if (lineRect.isValid()) {
                 rightLineRect = lineRect;
+                lastRightRectValid = true;
+            } else {
+                lastRightRectValid = false;
             }
         }
-        lineParts.append(rightLineRect);
-        Q_ASSERT(m_validObstructions.size() + 1 == lineParts.size());
+        if (lastRightRectValid) {
+            lineParts.append(rightLineRect);
+        }
+        //Q_ASSERT(m_validObstructions.size() + 1 == lineParts.size());
         // Select invalid parts because of wrap.
         for (int i = 0; i < m_validObstructions.size(); i++) {
             KoTextLayoutObstruction *obstruction = m_validObstructions.at(i);

KDE/krita

commit 7e968ce87dfc9e4255b5e573bffdad6967271737

Fix crash
The crash was introduced by 1653679a684e3b4b12a3ed9ca0c60a7d1409077e.
If the right was not added we need can't iterate over all m_validObstructions
but need to use one less.

libs/textlayout/RunAroundHelper.cpp
@@ -161,12 +161,16 @@ void RunAroundHelper::createLineParts()
                 lastRightRectValid = false;
             }
         }
+        int validObstructionsSize = m_validObstructions.size();
         if (lastRightRectValid) {
             lineParts.append(rightLineRect);
         }
-        //Q_ASSERT(m_validObstructions.size() + 1 == lineParts.size());
+        else {
+            --validObstructionsSize;
+        }
+        Q_ASSERT(validObstructionsSize + 1 == lineParts.size());
         // Select invalid parts because of wrap.
-        for (int i = 0; i < m_validObstructions.size(); i++) {
+        for (int i = 0; i < validObstructionsSize; i++) {
             KoTextLayoutObstruction *obstruction = m_validObstructions.at(i);
             if (obstruction->noTextAround()) {
                 lineParts.replace(i, QRectF());
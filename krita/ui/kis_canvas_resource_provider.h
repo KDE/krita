ea4e499d4fda9d6448f1c9a7a4d9375cf5e8560f
Boudewijn Rempt <boud@valdyas.org>
2011-06-22 18:50:38 +0200

    this is not used as a slot, make it not a slot

--- krita/ui/kis_canvas_resource_provider.h
+++ krita/ui/kis_canvas_resource_provider.h
@@ -98,7 +98,7 @@ public:
     KisFilterConfiguration* currentGeneratorConfiguration() const;
 
     static const KoColorProfile* getScreenProfile(int screen = -1);
-    
+
     void setCurrentCompositeOp(const QString& compositeOp);
     QString currentCompositeOp() const;
 
@@ -115,13 +115,14 @@ public:
     void setOpacity(qreal opacity);
     qreal opacity();
 
+    void setPaintOpPreset(const KisPaintOpPresetSP preset);
+
 public slots:
 
     void slotSetFGColor(const KoColor& c);
     void slotSetBGColor(const KoColor& c);
     void slotPatternActivated(KoResource *pattern);
     void slotGradientActivated(KoResource *gradient);
-    void slotPaintOpPresetActivated(const KisPaintOpPresetSP preset);
     void slotNodeActivated(const KisNodeSP node);
     void slotGeneratorConfigurationActivated(KisFilterConfiguration * generatorConfiguration);
     void slotPainting();

bf3538b0c8ab08cdec6b958f7bac7a38ec66948d
José Luis Vergara <pentalis@gmail.com>
2011-06-25 03:25:56 -0400

    Disable Merge with Layer Below menu entry when it's not valid

--- krita/plugins/extensions/dockers/defaultdockers/kis_layer_box.cpp
+++ krita/plugins/extensions/dockers/defaultdockers/kis_layer_box.cpp
@@ -362,7 +362,8 @@ void KisLayerBox::slotContextMenuRequested(const QPoint &pos, const QModelIndex 
         menu.addSeparator();
         menu.addAction(KIcon("edit-delete"), i18n("&Remove Layer"), this, SLOT(slotRmClicked()));
         menu.addAction(KIcon("edit-duplicate"), i18n("&Duplicate Layer or Mask"), this, SLOT(slotDuplicateClicked()));
-        menu.addAction(KIcon("edit-merge"), i18n("&Merge with Layer Below"), this, SLOT(slotMergeLayer()));
+        QAction* mergeLayerDown = menu.addAction(KIcon("edit-merge"), i18n("&Merge with Layer Below"), this, SLOT(slotMergeLayer()));
+        if (index.sibling(index.row() - 1, 0).isValid()) mergeLayerDown->setEnabled(false);
         menu.addSeparator();
 
     }

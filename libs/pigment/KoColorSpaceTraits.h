bbae2bb77a0c3a2ba6455aafd924cde88bc0b4cc
Boudewijn Rempt <boud@valdyas.org>
2011-06-30 17:10:22 +0200

    add getter/setter convenience functions for CMYK colorspace traits

--- libs/pigment/KoColorSpaceTraits.h
+++ libs/pigment/KoColorSpaceTraits.h
@@ -340,6 +340,8 @@ struct KoXyzTraits : public KoColorSpaceTrait<_channels_type_, 4, 3> {
 template<typename _channels_type_>
 struct KoCmykTraits : public KoColorSpaceTrait<_channels_type_, 5, 4> {
     typedef _channels_type_ channels_type;
+    typedef KoColorSpaceTrait<_channels_type_, 4, 3> parent;
+
     static const qint32 c_pos = 0;
     static const qint32 m_pos = 1;
     static const qint32 y_pos = 2;
@@ -355,6 +357,47 @@ struct KoCmykTraits : public KoColorSpaceTrait<_channels_type_, 5, 4> {
         channels_type black;
         channels_type alpha;
     };
+    /// @return the Cyan component
+    inline static channels_type C(quint8* data) {
+        channels_type* d = parent::nativeArray(data);
+        return d[c_pos];
+    }
+    /// Set the Cyan component
+    inline static void setC(quint8* data, channels_type nv) {
+        channels_type* d = parent::nativeArray(data);
+        d[c_pos] = nv;
+    }
+    /// @return the Magenta component
+    inline static channels_type M(quint8* data) {
+        channels_type* d = parent::nativeArray(data);
+        return d[m_pos];
+    }
+    /// Set the Magenta component
+    inline static void setM(quint8* data, channels_type nv) {
+        channels_type* d = parent::nativeArray(data);
+        d[m_pos] = nv;
+    }
+    /// @return the Yellow component
+    inline static channels_type Y(quint8* data) {
+        channels_type* d = parent::nativeArray(data);
+        return d[y_pos];
+    }
+    /// Set the Yellow component
+    inline static void setY(quint8* data, channels_type nv) {
+        channels_type* d = parent::nativeArray(data);
+        d[y_pos] = nv;
+    }
+    /// @return the Key component
+    inline static channels_type k(quint8* data) {
+        channels_type* d = parent::nativeArray(data);
+        return d[k_pos];
+    }
+    /// Set the Key component
+    inline static void setK(quint8* data, channels_type nv) {
+        channels_type* d = parent::nativeArray(data);
+        d[k_pos] = nv;
+    }
+
 };
 
 

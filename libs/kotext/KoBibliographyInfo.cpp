b628e72c104f94d6905359254109e0662965f479
Smit Patel <smitpatel24@gmail.com>
2011-07-03 03:49:29 +0530

    Reused IndexEntry and IndexEntryTemplate classes by moving them in ToCBibGeneratorInfo.h (for both ToC and Bibliography)

---
+++ libs/kotext/KoBibliographyInfo.cpp
@@ -0,0 +1,140 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2011 Smit Patel <smitpatel24@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "KoBibliographyInfo.h"
+
+#include <KoXmlReader.h>
+#include <KoXmlWriter.h>
+#include <KoXmlNS.h>
+#include <KoTextSharedLoadingData.h>
+#include <KoParagraphStyle.h>
+
+#include <QTextCursor>
+
+
+
+int KoBibliographyInfo::styleNameToStyleId(KoTextSharedLoadingData *sharedLoadingData, QString styleName)
+{
+    KoParagraphStyle * style = sharedLoadingData->paragraphStyle(styleName, true);
+    if (style) {
+        return style->styleId();
+    }
+
+    return 0;
+}
+
+KoBibliographyInfo::KoBibliographyInfo()
+  : m_generator(0)
+{
+}
+
+KoBibliographyInfo::~KoBibliographyInfo()
+{
+    foreach (const BibliographyEntryTemplate &entryTemplate, m_entryTemplate) {
+        qDeleteAll(entryTemplate.indexEntries);
+    }
+    delete m_generator;
+    m_generator = 0; // just to be safe
+}
+
+void KoBibliographyInfo::loadOdf(KoTextSharedLoadingData *sharedLoadingData, const KoXmlElement& element)
+{
+    Q_ASSERT(element.localName() == "bibliography-source" && element.namespaceURI() == KoXmlNS::text);
+
+    KoXmlElement p;
+    forEachElement(p, element) {
+        if (p.namespaceURI() != KoXmlNS::text) {
+            continue;
+        }
+
+        // first child
+        if (p.localName() == "index-title-template") {
+            m_indexTitleTemplate.styleName = p.attribute("style-name");
+            m_indexTitleTemplate.styleId = styleNameToStyleId(sharedLoadingData, m_indexTitleTemplate.styleName);
+            m_indexTitleTemplate.text = p.text();
+        // second child
+        } else if (p.localName() == "bibliography-entry-template") {
+            BibliographyEntryTemplate bibEntryTemplate;
+            bibEntryTemplate.styleName = p.attribute("style-name");
+            bibEntryTemplate.styleId = styleNameToStyleId(sharedLoadingData, bibEntryTemplate.styleName );
+
+            KoXmlElement indexEntry;
+            forEachElement(indexEntry, p) {
+                if (indexEntry.namespaceURI() != KoXmlNS::text) {
+                    continue;
+                }
+
+                if (indexEntry.localName() == "index-entry-bibliography") {
+                    // use null String if the style name is not present, it means that we inherit it from the parent
+                    IndexEntryBibliography * entryBibliography = new IndexEntryBibliography(
+                        indexEntry.attribute("style-name", QString())
+                    );
+
+                    entryBibliography->dataField = indexEntry.attribute("bibliography-data-field", "article");
+                    bibEntryTemplate.indexEntries.append(static_cast<IndexEntry*>(entryBibliography));
+
+                } else if (indexEntry.localName() == "index-entry-span") {
+                    IndexEntrySpan * entrySpan = new IndexEntrySpan(indexEntry.attribute("style-name", QString()) );
+                    entrySpan->text = indexEntry.text();
+                    bibEntryTemplate.indexEntries.append(static_cast<IndexEntry*>(entrySpan));
+
+                } else if (indexEntry.localName() == "index-entry-tab-stop") {
+                    IndexEntryTabStop * entryTabStop = new IndexEntryTabStop(indexEntry.attribute("style-name", QString()));
+
+                    QString type = indexEntry.attribute("type","right"); // left or right
+                    if (type == "left") {
+                        entryTabStop->tab.type = QTextOption::LeftTab;
+                    } else {
+                        entryTabStop->tab.type = QTextOption::RightTab;
+                    }
+                    entryTabStop->setPosition(indexEntry.attribute("position", QString()));
+                    entryTabStop->tab.leaderText = indexEntry.attribute("leader-char",".");
+                    bibEntryTemplate.indexEntries.append(static_cast<IndexEntry*>(entryTabStop));
+                }
+            }
+            m_entryTemplate.append(bibEntryTemplate);
+
+        // third child
+        }
+    }// forEachElement
+}
+
+void KoBibliographyInfo::saveOdf(KoXmlWriter * writer) const
+{
+    writer->startElement("text:bibliography-source");
+
+        m_indexTitleTemplate.saveOdf(writer);
+
+        foreach (const BibliographyEntryTemplate &entry, m_entryTemplate) {
+            entry.saveOdf(writer);
+        }
+
+    writer->endElement();
+}
+
+void KoBibliographyInfo::setGenerator(BibliographyGenerator *generator)
+{
+    delete m_generator;
+    m_generator = generator;
+}
+
+BibliographyGenerator *KoBibliographyInfo::generator() const
+{
+    return m_generator;
+}
--- libs/kotext/KoBibliographyInfo.h
+++ libs/kotext/KoBibliographyInfo.h
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2011 Lukáš Tvrdý <lukas.tvrdy@ixonos.com>
+/* This file is part of the KDE project
+ * Copyright (C) 2011 Smit Patel <smitpatel24@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -16,62 +16,42 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
+#ifndef KOBIBLIOGRAPHYINFO_H
+#define KOBIBLIOGRAPHYINFO_H
 
-#ifndef KO_TABLE_OF_CONTENTS_GENERATOR_INFO
-#define KO_TABLE_OF_CONTENTS_GENERATOR_INFO
-
-#define ppVar( var ) #var << "=" << var
-//#define DEBUG_TOC_STRUCTURE
-
-#include <QDebug>
-#include <QList>
-#include <QString>
-#include <QVariant>
-
-#include <KoXmlReader.h>
+#include "kotext_export.h"
 #include "KoText.h"
+#include "KoXmlReaderForward.h"
+#include <KoXmlReader.h>
 #include <KoXmlWriter.h>
 #include "ToCBibGeneratorInfo.h"
 
 class KoTextSharedLoadingData;
-class ToCGenerator; // not actually defined in kotext, a textlayouter is free to define
+class BibliographyGenerator;
 
-class KOTEXT_EXPORT KoTableOfContentsGeneratorInfo
+class KOTEXT_EXPORT KoBibliographyInfo
 {
 public:
-    KoTableOfContentsGeneratorInfo();
-    ~KoTableOfContentsGeneratorInfo();
+    KoBibliographyInfo();
+
+    ~KoBibliographyInfo();
+
     void loadOdf(KoTextSharedLoadingData *sharedLoadingData, const KoXmlElement &element);
     void saveOdf(KoXmlWriter *writer) const;
 
-    void setGenerator(ToCGenerator *generator);
+    void setGenerator(BibliographyGenerator *generator);
 
-    ToCGenerator *generator() const;
-
+    BibliographyGenerator *generator() const;
 
     QString m_name;
     QString m_styleName;
-    // TODO: add support for those according ODF v1.2
-    // text: protected
-    // text: protection-key
-    // text:protection-key-digest-algorithm
-    // xml:id
-    QString m_indexScope; // enum {document, chapter}
-    int m_outlineLevel;
-    bool m_relativeTabStopPosition;
-    bool m_useIndexMarks;
-    bool m_useIndexSourceStyles;
-    bool m_useOutlineLevel;
-
     IndexTitleTemplate m_indexTitleTemplate;
-    QList<TocEntryTemplate> m_entryTemplate; // N-entries
-    QList<IndexSourceStyles> m_indexSourceStyles;
+    QList<BibliographyEntryTemplate> m_entryTemplate;
 
 private:
     int styleNameToStyleId(KoTextSharedLoadingData *sharedLoadingData, QString styleName);
-    ToCGenerator * m_generator;
+    BibliographyGenerator * m_generator;
 };
 
-Q_DECLARE_METATYPE(KoTableOfContentsGeneratorInfo *)
-
-#endif
+Q_DECLARE_METATYPE(KoBibliographyInfo *);       //to avail KoBibliographyInfo * to all template based function.(QVarient)
+#endif // KOBIBLIOGRAPHYINFO_H

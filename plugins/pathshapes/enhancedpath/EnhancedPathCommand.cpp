8e27d61e416cef0bb9ed77a2c8acfb00c7e807e6
Lukáš Tvrdý <lukast.dev@gmail.com>
2011-07-26 10:12:49 +0200

    For T,U command the w and h is actually rx, ry
    Will be more clear in the ODF specification very soon.
    
    Fixes smiley shape, small ellipses, ring, flow-or,
    flowchart-summing-junction, flowchart-connector..
    
    CCBUG:277540
    CCBUG:271835

--- plugins/pathshapes/enhancedpath/EnhancedPathCommand.cpp
+++ plugins/pathshapes/enhancedpath/EnhancedPathCommand.cpp
@@ -96,7 +96,7 @@ bool EnhancedPathCommand::execute()
         bool lineTo = m_command.unicode() == 'T';
 
         for (int i = 0; i < pointsCount; i+=3) {
-            const QPointF &radii = points[i+1]/2;
+            const QPointF &radii = points[i+1];
             const QPointF &angles = points[i+2] / rad2deg;
             // compute the ellipses starting point
             QPointF start(radii.x() * cos(angles.x()), -1 * radii.y() * sin(angles.x()));

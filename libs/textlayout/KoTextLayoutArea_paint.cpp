5089dff03f775426361ee14d8d7a90fe65b1bdfa
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-07-28 16:17:39 +0200

    Fix text vanishs if selected or zoomed in The problem was that with commit b991a86 logic was introduced to handle valigned-text but that was resulting in us fetching and dealing with the wrong clipRegion what could result in us not proper updating and repainting clipped areas. Will run cstester over our large collection of tests-docs now to look what changed and if it has side-effects.

--- libs/textlayout/KoTextLayoutArea_paint.cpp
+++ libs/textlayout/KoTextLayoutArea_paint.cpp
@@ -72,11 +72,6 @@ extern int qt_defaultDpiY();
 
 void KoTextLayoutArea::paint(QPainter *painter, const KoTextDocumentLayout::PaintContext &context)
 {
-    painter->setPen(context.textContext.palette.color(QPalette::Text)); // for text that has no color.
-    const QRegion clipRegion = painter->clipRegion();
-    KoTextBlockBorderData *lastBorder = 0;
-    QRectF lastBorderRect;
-
     if (m_startOfArea == 0) // We have not been layouted yet
         return;
 
@@ -91,6 +86,11 @@ void KoTextLayoutArea::paint(QPainter *painter, const KoTextDocumentLayout::Pain
 
     painter->save();
     painter->translate(0, m_verticalAlignOffset);
+
+    painter->setPen(context.textContext.palette.color(QPalette::Text)); // for text that has no color.
+    const QRegion clipRegion = painter->clipRegion(); // fetch after painter->translate so the clipRegion is correct
+    KoTextBlockBorderData *lastBorder = 0;
+    QRectF lastBorderRect;
 
     QTextFrame::iterator it = m_startOfArea->it;
     QTextFrame::iterator stop = m_endOfArea->it;

e330d66ea796084cad5e9313d6a6585559082fbd
Boudewijn Rempt <boud@valdyas.org>
2011-07-29 14:16:24 +0200

    Remove KoDocumentRdf::ensureTextTool
    This method did two things: activate the text tool and return
    the KoTextEditor taken from the text tool's selection. Instead, we can
    now get the KoTextEditor for the active text shape object directly,
    without being dependent on which tool is active.

--- libs/main/rdf/KoDocumentRdfEditWidget.cpp
+++ libs/main/rdf/KoDocumentRdfEditWidget.cpp
@@ -459,7 +459,6 @@ void KoDocumentRdfEditWidget::defaultContactsSheetButton()
 {
     KoDocumentRdf *rdf = d->m_rdf;
     Q_ASSERT(rdf);
-    rdf->ensureTextTool();
     QString stylesheetName = d->m_ui->m_defaultContactsSheet->currentText();
     kDebug(30015) << "changing contact default stylesheet to:" << stylesheetName;
     KoSemanticStylesheet *ss = stylesheetFromComboBox(d->m_ui->m_defaultContactsSheet);
@@ -479,7 +478,6 @@ void KoDocumentRdfEditWidget::defaultEventsSheetButton()
 {
     KoDocumentRdf *rdf = d->m_rdf;
     Q_ASSERT(rdf);
-    rdf->ensureTextTool();
     QString stylesheetName = d->m_ui->m_defaultEventsSheet->currentText();
     kDebug(30015) << "changing event default stylesheet to:" << stylesheetName;
     KoSemanticStylesheet *ss = stylesheetFromComboBox(d->m_ui->m_defaultEventsSheet);
@@ -499,7 +497,6 @@ void KoDocumentRdfEditWidget::defaultLocationsSheetButton()
 {
     KoDocumentRdf *rdf = d->m_rdf;
     Q_ASSERT(rdf);
-    rdf->ensureTextTool();
     QString stylesheetName = d->m_ui->m_defaultLocationsSheet->currentText();
     kDebug(30015) << stylesheetName;
     KoSemanticStylesheet *ss = stylesheetFromComboBox(d->m_ui->m_defaultLocationsSheet);

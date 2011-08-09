69e3b8f1c0c4a530b26e743652d97ee5a3d379bd
Dmitry Kazakov <dimula73@gmail.com>
2011-08-09 18:28:42 +0400

    Added copyrights

--- krita/ui/widgets/kis_scratch_pad.cpp
+++ krita/ui/widgets/kis_scratch_pad.cpp
@@ -1,5 +1,7 @@
 /* This file is part of the KDE project
- * Copyright 2010 (C) Boudewijn Rempt <boud@valdyas.org> *
+ * Copyright 2010 (C) Boudewijn Rempt <boud@valdyas.org>
+ * Copyright 2011 (C) Dmitry Kazakov <dimula73@gmail.com>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either

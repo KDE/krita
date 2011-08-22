ae44414b64df2d1cbe83337de3cb6c965b761e29
Casper Boemann <cbo@boemann.dk>
2011-08-22 15:46:44 +0200

    Make sure we save footnotes and endnotes again

--- libs/kotext/opendocument/KoTextWriter.cpp
+++ libs/kotext/opendocument/KoTextWriter.cpp
@@ -1680,8 +1680,12 @@ void KoTextWriter::Private::writeBlocks(QTextDocument *document, int from, int t
 
         QTextCursor cursor(block);
 
-        if (cursor.currentFrame()->format().hasProperty(KoText::SubFrameType)) {
-            break; // we've reached the "end" (end/footnotes saved in another way)
+        int frameType = cursor.currentFrame()->format().intProperty(KoText::SubFrameType);
+        if (frameType == KoText::EndNotesFrameType
+            || frameType == KoText::FootNotesFrameType) {
+            break; // we've reached the "end" (end/footnotes saved by themselves)
+                   // note how NoteFrameType passes through here so the notes can
+                   // call writeBlocks to save their contents.
         }
 
         QTextBlockFormat format = block.blockFormat();

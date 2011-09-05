cfe12c692691424770f32e3d2a3fe4af1db1303c
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-09-05 22:38:41 +0200

    Fix layout-loops. Will look for a better fix that preserves the softpagebreak-no-between-bullet-and-text later.

--- libs/textlayout/KoTextShapeContainerModel.cpp
+++ libs/textlayout/KoTextShapeContainerModel.cpp
@@ -192,6 +192,7 @@ void KoTextShapeContainerModel::proposeMove(KoShape *child, QPointF &move)
         anchorPosInParag = relation.anchor->positionInDocument() - block.position();
         if (layout) {
             QTextLine tl = layout->lineForTextPosition(anchorPosInParag);
+            Q_ASSERT(tl.isValid());
             relation.anchor->setOffset(QPointF(newPosition.x() - tl.cursorToX(anchorPosInParag)
                 + tl.x(), 0));
             relayoutInlineObject(child);
@@ -208,6 +209,7 @@ void KoTextShapeContainerModel::proposeMove(KoShape *child, QPointF &move)
             KoTextShapeData *data = qobject_cast<KoTextShapeData*>(child->parent()->userData());
             Q_ASSERT(data);
             QTextLine tl = layout->lineForTextPosition(anchorPosInParag);
+            Q_ASSERT(tl.isValid());
             qreal y = tl.y() - data->documentOffset() - newPosition.y() + child->size().height();
             relation.anchor->setOffset(QPointF(relation.anchor->offset().x(), -y));
             relayoutInlineObject(child);

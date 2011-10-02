8c3949e82b306b7c44ef4c38028ca23c164e5be3
Thorsten Zachmann <t.zachmann@zagge.de>
2011-10-02 13:43:33 +0200

    Add load style properties from multiple properties.
    For embedded text shapes paragraph style properties can be saved in style:paragraph-properties
    or in graphic:paragraph-properties. It is also possible that some values are in one and others
    are in the other e.g. in parent styles.
    
    This patch add the possibilty to load the properties from more then one property type.
    
    This partly fixes bug 282617. It is not fully fixed as not all properties are used by the text
    shape yet.
    
    fo:padding-top and fo:padding-bottom seem to be not yet supported.
    
    CCBUG: 282617
    REVIEW: 102737

--- libs/kotext/styles/KoParagraphStyle.cpp
+++ libs/kotext/styles/KoParagraphStyle.cpp
@@ -1182,7 +1182,17 @@ void KoParagraphStyle::loadOdf(const KoXmlElement *element, KoShapeLoadingContex
     charstyle->loadOdf(scontext);   // load the KoCharacterStyle from the stylestack
 
     //1.6: KoTextParag::loadOasis => KoParagLayout::loadOasisParagLayout
-    context.styleStack().setTypeProperties("paragraph");   // load all style attributes from "style:paragraph-properties"
+    kDebug(32500) << "family" << family;
+    if (family == "graphic" || family == "presentation") {
+        QList<QString> typeProperties;
+        typeProperties.append("graphic");
+        typeProperties.append("paragraph");
+        // load all style attributes from "style::graphic-properties" and if not found there from "style:paragraph-properties"
+        context.styleStack().setTypeProperties(typeProperties);
+    }
+    else {
+        context.styleStack().setTypeProperties("paragraph");   // load all style attributes from "style:paragraph-properties"
+    }
 
     loadOdfProperties(scontext);   // load the KoParagraphStyle from the stylestack
 

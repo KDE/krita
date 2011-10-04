5d799df9be822d72ff49445457efad5cc906b467
C. Boemann <cbo@boemann.dk>
2011-10-04 19:08:03 +0200

    Better ui for references tool

--- plugins/textshape/ReferencesTool.h
+++ plugins/textshape/ReferencesTool.h
@@ -62,14 +62,18 @@ private slots:
     void formatTableOfContents();
     /// shows the configuration dialog for a ToC
     void showConfigureDialog(QAction *action);
-    /// insert a footnote
-    void insertFootNote();
-    /// insert an endnote
-    void insertEndNote();
+    /// insert an autonumbered footnote
+    void insertAutoFootNote();
+    /// insert a labeled footnote
+    void insertLabeledFootNote(QString label);
+    /// insert an autonumbered endnote
+    void insertAutoEndNote();
+    /// insert a labeled endnote
+    void insertLabeledEndNote(QString label);
     /// show the configuration dialog for notes
     void showNotesConfigureDialog();
-    /// disable insert notes' buttons when already in notes' body
-    void disableButtons(QTextCursor cursor);
+    /// enable/disable buttons if cursor in notes' body or not
+    void updateButtons();
 
 
 private:
@@ -78,6 +82,27 @@ private:
         SimpleFootEndNotesWidget *m_sfenw;
         KoInlineNote *m_note;
     SimpleCitationBibliographyWidget *m_scbw;
+};
+
+class KAction;
+class QLineEdit;
+
+class LabeledNoteWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    LabeledNoteWidget(KAction *action);
+    KAction *m_action;
+    QLineEdit *m_lineEdit;
+
+signals:
+    void triggered(QString label);
+
+private slots:
+    void returnPressed();
+
+protected:
+    virtual void enterEvent(QEvent *event);
 };
 
 #endif // REFERENCESTOOL_H

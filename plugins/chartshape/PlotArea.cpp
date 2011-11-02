KDE/calligra

commit 72944a90c3a69be56a65f3c8248edaa32742d084

removed the double style stack fill

plugins/chartshape/PlotArea.cpp
@@ -656,7 +656,7 @@ bool PlotArea::loadOdf( const KoXmlElement &plotAreaElement,
     // chart types like line charts, stock charts, etc.
     if ( plotAreaElement.hasAttributeNS( KoXmlNS::chart, "style-name" ) ) {
         styleStack.clear();
-        context.odfLoadingContext().fillStyleStack( plotAreaElement, KoXmlNS::chart, "style-name", "chart" );
+        //context.odfLoadingContext().fillStyleStack( plotAreaElement, KoXmlNS::chart, "style-name", "chart" );
 
         styleStack.setTypeProperties( "graphic" );
         styleStack.setTypeProperties( "chart" );

KDE/krita

commit 0a854f90338fa139e8877e48e6d12a417fb9703c

Land Thorsten's styleStack fix for kchart. This fixes a bunch of charting related regressions introduced with commit 72944a90c3a69b. Together with the kdchart stacked bar-chart fix everything should be displayed as expected now.

plugins/chartshape/PlotArea.cpp
@@ -656,7 +656,7 @@ bool PlotArea::loadOdf( const KoXmlElement &plotAreaElement,
     // chart types like line charts, stock charts, etc.
     if ( plotAreaElement.hasAttributeNS( KoXmlNS::chart, "style-name" ) ) {
         styleStack.clear();
-        //context.odfLoadingContext().fillStyleStack( plotAreaElement, KoXmlNS::chart, "style-name", "chart" );
+        context.odfLoadingContext().fillStyleStack( plotAreaElement, KoXmlNS::chart, "style-name", "chart" );
 
         styleStack.setTypeProperties( "graphic" );
         styleStack.setTypeProperties( "chart" );
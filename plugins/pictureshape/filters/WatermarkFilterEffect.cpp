4f7a81a22993b0287d55d6b270886590ce741639
Silvio Heinrich <plassy@web.de>
2011-12-09 18:54:55 +0100

    Make the Watermark filter behave like it is written in the ODF spec.
    The watermark filter increased the lightness of the picture but in the
    ODF spec is written "watermark: colors are modified to make the resulting image transparent."
    The filter will now make the picture 50% transparent.

--- plugins/pictureshape/filters/WatermarkFilterEffect.cpp
+++ plugins/pictureshape/filters/WatermarkFilterEffect.cpp
@@ -42,16 +42,22 @@ bool WatermarkFilterEffect::load(const KoXmlElement& /*element*/, const KoFilter
 
 QImage WatermarkFilterEffect::processImage(const QImage& image, const KoFilterEffectRenderContext& context) const
 {
-    QImage result = image;
-    QRgb* pixel = reinterpret_cast<QRgb*>( result.bits() );
+    QImage result = image.convertToFormat(QImage::Format_ARGB32);
+    QRgb*  pixel = reinterpret_cast<QRgb*>( result.bits() );
     const int right = context.filterRegion().right();
     const int bottom = context.filterRegion().bottom();
     const int width = result.width();
-    for( int row = context.filterRegion().top(); row < bottom; ++row ) {
-        for( int col = context.filterRegion().left(); col < right; ++col ){
-            const QColor currentPixel = pixel[row * width + col];
-            const QColor currentPixelLighter = KColorUtils::lighten(currentPixel, 0.75);
-            pixel[row * width + col] = currentPixelLighter.rgb();
+    
+    for(int row = context.filterRegion().top(); row < bottom; ++row) {
+        for(int col = context.filterRegion().left(); col < right; ++col) {
+//             const QColor currentPixel = pixel[row * width + col];
+//             const QColor currentPixelLighter = KColorUtils::lighten(currentPixel, 0.75);
+//             pixel[row * width + col] = currentPixelLighter.rgb();
+
+            quint32 color = pixel[row * width + col];
+            quint32 rgb   = 0x00FFFFFF & color;   // get rgb value without alpha
+            quint32 alpha = (color >> 25) << 24;  // get alpha value only and divide it by two
+            pixel[row * width + col] = alpha|rgb; // set rgb and alpha values to the pixel
         }
     }
     return result;

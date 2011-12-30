16a901834ee70c87bf4251abb18d9f4e125231e6
Boudewijn Rempt <boud@valdyas.org>
2011-12-30 16:56:03 +0100

    Update select by colorrange to make it work with transactions
    After the previous refactoring, the colorrange dialog used setGlobalSelection
    directly and destructively, making undo impossible. This fixes that
    problem.
    
    Also:
    
    * remove the preview window, which is a relic from 1.x days in these
    days of on-canvas preview
    * make select opaque work on all nodes, not just layers.

--- krita/plugins/extensions/colorrange/colorrange.cc
+++ krita/plugins/extensions/colorrange/colorrange.cc
@@ -81,10 +81,7 @@ ColorRange::~ColorRange()
 
 void ColorRange::slotActivated()
 {
-    KisPaintDeviceSP layer = m_view->activeDevice();
-    if (!layer) return;
-
-    DlgColorRange * dlgColorRange = new DlgColorRange(m_view, layer, m_view, "ColorRange");
+    DlgColorRange *dlgColorRange = new DlgColorRange(m_view, m_view);
     Q_CHECK_PTR(dlgColorRange);
 
     dlgColorRange->exec();
@@ -96,14 +93,14 @@ void ColorRange::selectOpaque()
     if (!canvas)
         return;
     
-    KisLayerSP layer = m_view->activeLayer();
-    if(!layer)
+    KisNodeSP node = m_view->activeNode();
+    if(!node)
         return;
     
-    KisPaintDeviceSP device = layer->paintDevice();
+    KisPaintDeviceSP device = node->paintDevice();
     if (!device) return;
     
-    KisSelectionToolHelper helper(canvas, layer, i18n("Select Opaque"));
+    KisSelectionToolHelper helper(canvas, node, i18n("Select Opaque"));
     
     qint32 x, y, w, h;
     device->exactBounds(x, y, w, h);

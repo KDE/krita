2247d43173a34f08fcd7e83f52ad190081935ce4
C. Boemann <cbo@boemann.dk>
2012-01-05 13:10:40 +0100

    Create a ChangeAnchorPropertiesCommand
    It's not used for now

---
+++ libs/kotext/commands/ChangeAnchorPropertiesCommand.cpp
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2012 C. Boemann <cbo@boemann.dk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "ChangeAnchorPropertiesCommand.h"
+#include "KoTextAnchor.h"
+
+ChangeAnchorPropertiesCommand::ChangeAnchorPropertiesCommand(KoTextAnchor *anchor, KoTextAnchor *newAnchor, KUndo2Command *parent)
+    : KUndo2Command(parent)
+    , m_anchor(anchor)
+    , m_oldAnchor(0)
+    , m_newAnchor(0)
+{
+    copyLayoutProperties(anchor, &m_oldAnchor);
+    copyLayoutProperties(newAnchor, &m_newAnchor);
+}
+
+ChangeAnchorPropertiesCommand::~ChangeAnchorPropertiesCommand()
+{
+}
+
+void ChangeAnchorPropertiesCommand::copyLayoutProperties(KoTextAnchor *from, KoTextAnchor *to)
+{
+    to->setOffset(from->offset());
+    to->setVerticalPos(from->verticalPos());
+    to->setVerticalRel(from->verticalRel());
+    to->setHorizontalPos(from->horizontalPos());
+    to->setHorizontalRel(from->horizontalRel());
+    to->setAnchorType(from->anchorType());
+}
+
+void ChangeAnchorPropertiesCommand::redo()
+{
+    KUndo2Command::redo();
+
+    copyLayoutProperties(&m_newAnchor, m_anchor);
+}
+
+void ChangeAnchorPropertiesCommand::undo()
+{
+    KUndo2Command::undo();
+
+    copyLayoutProperties(&m_oldAnchor, m_anchor);
+}

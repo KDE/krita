d8af747c28ddd99779730eea5d4f0951e9ac2d62
Cyrille Berger <cberger@cberger.net>
2012-01-14 08:50:48 +0100

    enforce OpenGTL 0.9.16

--- cmake/modules/FindOpenCTL.cmake
+++ cmake/modules/FindOpenCTL.cmake
@@ -1,17 +1,11 @@
      
 INCLUDE(FindPkgConfig)
 
-pkg_check_modules(OPENCTL OpenCTL>=0.9.13)
-pkg_check_modules(OPENCTL_913 OpenCTL>=0.9.13)
+pkg_check_modules(OPENCTL OpenCTL>=0.9.16)
 
 if (OPENCTL_FOUND)
     set(HAVE_OPENCTL TRUE)
     message(STATUS "OpenCTL Found Version: " ${OPENCTL_VERSION})
-    if (OPENCTL_913_FOUND)
-	set(HAVE_OPENCTL_913 TRUE)
-    else()
-	set(HAVE_OPENCTL_913 FALSE)
-    endif()
 
     if (NOT OpenCTL_FIND_QUIETLY )
         message(STATUS "Found OPENCTL: ${OPENCTL_LIBRARIES}")

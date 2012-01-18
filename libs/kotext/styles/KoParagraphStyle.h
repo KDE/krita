18749976141253159524543410e11a622a90581e
C. Boemann <cbo@boemann.dk>
2012-01-18 03:39:57 +0100

    Correct comment

--- libs/kotext/styles/KoParagraphStyle.h
+++ libs/kotext/styles/KoParagraphStyle.h
@@ -133,8 +133,8 @@ public:
         DefaultOutlineLevel,
 
         // numbering
-        LineNumbering,           ///< bool, specifies whether lines should be numbered in this paragraph
-        LineNumberStartValue, /*60*/ ///< integer value that specifies the number for the first line in the paragraph
+        LineNumbering,         /*60*/   ///< bool, specifies whether lines should be numbered in this paragraph
+        LineNumberStartValue, ///< integer value that specifies the number for the first line in the paragraph
         SectionStartings,            ///< list of section definitions
         SectionEndings,               ///< list <end of a named section>
 // do 15.5.24

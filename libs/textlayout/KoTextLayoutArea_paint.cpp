7f8c5abb00a1c1bdbf923d3acd500e4c7f77473e
Gopalakrishna Bhat A <gopalakbhat@gmail.com>
2012-01-21 23:44:55 +0530

    Fix the list label vertical alignment issue for custom characters. Now we use the KoListStyle::isNumberingStyle method to determine if we vertically center or base align the bullet.

--- libs/textlayout/KoTextLayoutArea_paint.cpp
+++ libs/textlayout/KoTextLayoutArea_paint.cpp
@@ -450,7 +450,7 @@ void KoTextLayoutArea::drawListItem(QPainter *painter, const QTextBlock &block)
             if (block.layout()->lineCount() > 0) {
                 // if there is text, then baseline align the counter.
                 QTextLine firstParagLine = block.layout()->lineAt(0);
-                if (KoListStyle::isNumberingStyle(listStyle) || listStyle == KoListStyle::CustomCharItem) {
+                if (KoListStyle::isNumberingStyle(listStyle)) {
                     //if numbered list baseline align
                     counterPosition += QPointF(0, firstParagLine.ascent() - layout.lineAt(0).ascent());
                 } else {

7f775856deb56ef382c714aeccd55336d7c65e0a
Sven Langkamp <sven.langkamp@gmail.com>
2012-01-25 19:39:53 +0100

    increase size of the preset popup

--- krita/ui/kis_paintop_box.cc
+++ krita/ui/kis_paintop_box.cc
@@ -215,6 +215,7 @@ KisPaintopBox::KisPaintopBox(KisView2 * view, QWidget *parent, const char * name
     m_presetsPopup->switchDetached();
 
     m_presetsChooserPopup = new KisPaintOpPresetsChooserPopup();
+    m_presetsChooserPopup->setFixedSize(500, 500);
     m_presetWidget->setPopupWidget(m_presetsChooserPopup);
 
     m_prevCompositeOpID = KoCompositeOpRegistry::instance().getDefaultCompositeOp().id();

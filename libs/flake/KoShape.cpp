KDE/calligra

commit 3fd6c20f31f946f3128af63fc433449f36c06c9d

Save z-index of shapes, but exclude KoShapeGroup from doing so
REVIEW: 10348

libs/flake/KoShape.cpp
@@ -1747,6 +1747,10 @@ void KoShape::saveOdfAttributes(KoShapeSavingContext &context, int attributes) c
         }
     }
 
+    if (attributes & OdfZIndex) {
+        context.xmlWriter().addAttribute("draw:z-index", zIndex());
+    }
+
     if (attributes & OdfSize) {
         QSizeF s(size());
         if (parent() && parent()->isClipped(this)) { // being clipped shrinks our visible size

libs/flake/KoShape.h
@@ -1023,7 +1023,7 @@ protected:
         OdfStyle = 128,              ///< Store the style
         OdfId = 256,                 ///< Store the unique ID
         OdfName = 512,               ///< Store the name of the shape
-        OdfZIndex = 1024,            ///< This only loads the z-index; when saving, it is reflected by the order of the shapes.
+        OdfZIndex = 1024,            ///< Store the z-index
         OdfViewbox = 2048,           ///< Store the viewbox
 
         /// A mask for all mandatory attributes

libs/flake/KoShapeGroup.cpp
@@ -161,7 +161,7 @@ QRectF KoShapeGroup::boundingRect() const
 void KoShapeGroup::saveOdf(KoShapeSavingContext & context) const
 {
     context.xmlWriter().startElement("draw:g");
-    saveOdfAttributes(context, (OdfMandatories ^ OdfLayer) | OdfAdditionalAttributes);
+    saveOdfAttributes(context, (OdfMandatories ^ (OdfLayer | OdfZIndex)) | OdfAdditionalAttributes);
     context.xmlWriter().addAttributePt("svg:y", position().y());
 
     QList<KoShape*> shapes = this->shapes();

words/part/frames/KWFrame.cpp
@@ -215,14 +215,11 @@ void KWFrame::saveOdf(KoShapeSavingContext &context, const KWPage &page, int pag
     // shape properties
     const qreal pagePos = page.offsetInDocument();
 
-    const int effectiveZIndex = m_shape->zIndex() + pageZIndexOffset;
-    m_shape->setAdditionalAttribute("draw:z-index", QString::number(effectiveZIndex));
     m_shape->setAdditionalAttribute("text:anchor-type", "page");
     m_shape->setAdditionalAttribute("text:anchor-page-number", QString::number(page.pageNumber()));
     context.addShapeOffset(m_shape, QTransform(1, 0, 0 , 1, 0, -pagePos));
     m_shape->saveOdf(context);
     context.removeShapeOffset(m_shape);
-    m_shape->removeAdditionalAttribute("draw:z-index");
     m_shape->removeAdditionalAttribute("fo:min-height");
     m_shape->removeAdditionalAttribute("text:anchor-page-number");
     m_shape->removeAdditionalAttribute("text:anchor-page-number");

KDE/krita

commit 0cc9c225b6d58111234670a451f55065f47ce0a9

Fix saving of shapes in the correct order.
The saving of the order is currently broken for quite a lot of files.

One example is slide 4 of

http://www.zagge.de/anchor/bug/open.umich.edu%252Fsites%252Fdefault%252Ffiles%252FSeverance-SI5%25302-W%25309-Week11-Sesons-and-Cookies.odp

This was caused by the commit 3fd6c20f31f946f3128af63fc433449f36c06c9d that saves the
z-index out to the file. However the way it is done is not fully correct. I works mostly
for words. With this patch we go to back to the original behaviour of not saving the z-index
as it is defined by the order of the shapes in the xml file. We still save the z-index for
words.

Please review.

I have run cstester --roundtip and it improves for odp and not regressions for odt files.

CCMAIL: calligra-devel@kde.org

libs/flake/KoShape.cpp
@@ -1772,7 +1772,7 @@ void KoShape::saveOdfAttributes(KoShapeSavingContext &context, int attributes) c
         }
     }
 
-    if (attributes & OdfZIndex) {
+    if (attributes & OdfZIndex && context.isSet(KoShapeSavingContext::ZIndex)) {
         context.xmlWriter().addAttribute("draw:z-index", zIndex());
     }
 

libs/flake/KoShapeSavingContext.h
@@ -69,7 +69,11 @@ public:
         /**
          * If set duplicate master pages will be merged to one
          */
-        UniqueMasterPages = 8
+        UniqueMasterPages = 8,
+        /**
+         * If set the z-index is saved in the shape
+         */
+        ZIndex = 16
     };
     Q_DECLARE_FLAGS(ShapeSavingOptions, ShapeSavingOption)
 
fde05ea2a99e29fcab2e2a7df1ce3201e2c298c3
Boudewijn Rempt <boud@valdyas.org>
2012-03-11 10:49:57 +0100

    use tiled preview for patterns

--- krita/ui/widgets/kis_pattern_chooser.cc
+++ krita/ui/widgets/kis_pattern_chooser.cc
@@ -43,6 +43,7 @@ KisPatternChooser::KisPatternChooser(QWidget *parent)
     KoAbstractResourceServerAdapter* adapter = new KoResourceServerAdapter<KisPattern>(rserver);
     m_itemChooser = new KoResourceItemChooser(adapter, this);
     m_itemChooser->showPreview(true);
+    m_itemChooser->setPreviewTiled(true);
     m_itemChooser->setPreviewOrientation(Qt::Horizontal);
     QString knsrcFile = "kritapatterns.knsrc";
     m_itemChooser->setKnsrcFile(knsrcFile);

660dce2e3e03dc46be77101c71b152b12140c618
Sven Langkamp <sven.langkamp@gmail.com>
2012-04-04 15:06:13 +0200

    make paintop setting smaller

--- krita/ui/widgets/kis_paintop_presets_popup.cpp
+++ krita/ui/widgets/kis_paintop_presets_popup.cpp
@@ -184,8 +184,8 @@ void KisPaintOpPresetsPopup::setPaintOpSettingsWidget(QWidget * widget)
     if (widget) {
         widget->setFont(m_d->smallFont);
 
-        widget->setMinimumSize(QSize(750, 450));
-        m_d->settingsWidget->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
+        widget->setFixedSize(QSize(600, 450));
+        m_d->settingsWidget->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
         m_d->layout->addWidget(widget);
 
         m_d->layout->update();

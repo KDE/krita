5220004b5bec971130b9d4caa1071e0bb5bc53cc
Boudewijn Rempt <boud@valdyas.org>
2012-04-18 14:36:41 +0200

    Remove unmaintained plugins from Calligra
    Filters:
    
    generic_wrapper (a filter that could use a python or perl script to do the
    filtering)
    xsltfilter (need Qt3 support, unmaintained since 2003)
    
    Maybe also liboofilter, if that is used nowhere anymore
    
    karbon:
    
    filter that haven't been ported since 2006
    
      ai
      kontour
      xcf
      msod
      oodraw
    
    kchart:
    
       svg (needs Qt3 support
    
    kformula:
    
       entire set of filters, none have been build since 2006
    
    stage:
    
      svg
      genericimageexport
      words
      ooimpress
      magicpoint
      kpr
      ooppt
    
       all the filters that only know kwd (whoever thought that making filters
    write
       to a file would make the filters more maintainable than linking to the
    apps
       internals was mistaken -- it only means the filters compile while being
    useless)
    
       mswrite
       html
       latex
       amipro
       applixword
       docbook
       kword1.3
       oowriter
       palmdoc
       wml
       abiword
    
    Plugins:
    
       mapshape -- unmaintained, buggy
       paragraphtool (great idea, unmaintained, does not compile)
       reporting/shape -- unfinished
       scan (rc files for krita and stage, is shown in neither)
       treeshape -- doesn't work and unmaintained
    
    Tools:
    
        maybe mobile and koabstraction -- neither is used in real life anymore
    since the Maemo platform is dead and we have Calligra Active instead.
        kthesaurus -- unmaintained since 2002
    
    Krita
    
        colorspaces/lms_f32 -- not compiled for a long time now
        extensions/backgrounds -- was unfinished
        extensions/bracketing2hdr -- commented out for a long time
        extensions/scripting -- not maintained (my fault!) since 2006
        extensions/extensionsmanager -- unfinished
        extensions/painterlyframework -- unmaintained
        extensions/tonemappaing -- crashes
        extensions/together -- looks like an unimplemented stub
        filters/bumpmap -- we've got phong bumpmap now
        tools/tool_curves -- good ideas, but not ported in 2006
    
    Conflicts:
    
    	filters/karbon/ai/karbon_ai_import.desktop
    	filters/karbon/oodraw/karbon_oodraw_import.desktop
    	filters/karbon/xcf/karbon_xcf_export.desktop
    	filters/stage/words/kodp2odt.desktop
    	filters/stage/words/kprwords.desktop
    	filters/words/abiword/words_abiword_export.desktop
    	filters/words/abiword/words_abiword_import.desktop
    	filters/words/amipro/words_amipro_export.desktop
    	filters/words/amipro/words_amipro_import.desktop
    	filters/words/mswrite/words_mswrite_export.desktop
    	filters/words/mswrite/words_mswrite_import.desktop
    	filters/words/oowriter/words_oowriter_export.desktop
    	filters/words/oowriter/words_oowriter_import.desktop
    	filters/words/palmdoc/words_palmdoc_export.desktop
    	filters/words/palmdoc/words_palmdoc_import.desktop
    	plugins/paragraphtool/paragraphtool.desktop
    	plugins/treeshape/treeshape.desktop

--- krita/plugins/extensions/tonemapping/libpfs/pfs.h
+++
@@ -1,3 +0,0 @@
-// Don't include that header, it's only useful to avoid editing source code
-// of operators coming from pfstmo
-#include "array2d.h"
--- plugins/paragraphtool/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-$EXTRACTRC */*.ui >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/ParagraphTool.pot

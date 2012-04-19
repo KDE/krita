983c1c73158c49db158b61b5565550715528e45d
Boudewijn Rempt <boud@valdyas.org>
2012-04-19 15:02:20 +0200

    Make KoColorSet a QObject so QPointer can manage it

--- libs/pigment/resources/KoColorSet.cpp
+++ libs/pigment/resources/KoColorSet.cpp
@@ -29,30 +29,6 @@
 
 #include "KoColorSpaceRegistry.h"
 
-/*
-KoColorSet::KoColorSet(const KisGradient * gradient, qint32 nColors, const QString & name)
-    : KoResource(QString("")),
-      m_name(name)
-{
-    Q_ASSERT(nColors > 0);
-    Q_ASSERT(gradient != 0);
-
-    qreal dx, cur_x;
-    qint32 i;
-    dx = 1.0 / (nColors - 1);
-
-    KoColorSetEntry e;
-    KoColor c;
-    for (i = 0, cur_x = 0; i < nColors; i++, cur_x += dx) {
-        gradient->colorAt(c,cur_x);
-        e.color = c;
-        e.name = "Untitled";
-        add(e);
-    }
-
-    m_columns = 0; // Set the default value that the GIMP uses...
-}
-*/
 KoColorSet::KoColorSet(const QString& filename)
         : KoResource(filename)
 {

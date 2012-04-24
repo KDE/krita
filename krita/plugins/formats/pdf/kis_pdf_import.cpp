a22a9116fa9fbceeb1d70eb44962883018391362
Sven Langkamp <sven.langkamp@gmail.com>
2012-04-24 18:10:42 +0200

    fix crash on opening pdf with special characters in the path BUG:298611

--- krita/plugins/formats/pdf/kis_pdf_import.cpp
+++ krita/plugins/formats/pdf/kis_pdf_import.cpp
@@ -86,7 +86,7 @@ KisPDFImport::ConversionStatus KisPDFImport::convert(const QByteArray& , const Q
         url.setPath(tmpFile);
     }
 
-    Poppler::Document* pdoc = Poppler::Document::load(QFile::encodeName(url.toLocalFile()));
+    Poppler::Document* pdoc = Poppler::Document::load(url.toLocalFile());
     pdoc->setRenderHint(Poppler::Document::Antialiasing, true);
     pdoc->setRenderHint(Poppler::Document::TextAntialiasing, true);
 

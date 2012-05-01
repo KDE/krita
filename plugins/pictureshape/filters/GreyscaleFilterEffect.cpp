c6c6cb2a2194317f07f2493b380692ca1c69af24
Inge Wallin <inge@lysator.liu.se>
2012-05-01 02:56:00 +0200

    Fix image effects 'grayscale' and 'mono'.
    The original author had forgotten to take the alpha channel into account.
    
    BUG: 260434

--- plugins/pictureshape/filters/GreyscaleFilterEffect.cpp
+++ plugins/pictureshape/filters/GreyscaleFilterEffect.cpp
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
  * Copyright (C) 2010 Carlos Licea <carlos@kdab.com>
+ * Copyright (C) 2012 Inge Wallin <inge@lysator.liu.se>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -17,8 +18,10 @@
  * Boston, MA 02110-1301, USA.
  */
 
+
 #include "GreyscaleFilterEffect.h"
 #include <KoFilterEffectRenderContext.h>
+
 
 GreyscaleFilterEffect::GreyscaleFilterEffect()
 : KoFilterEffect(GreyscaleFilterEffectId, "Grayscale effect")
@@ -40,19 +43,26 @@ bool GreyscaleFilterEffect::load(const KoXmlElement& /*element*/, const KoFilter
 
 QImage GreyscaleFilterEffect::processImage(const QImage& image, const KoFilterEffectRenderContext& context) const
 {
-    QImage result = image;
-    QRgb* pixel = reinterpret_cast<QRgb*>( result.bits() );
-    const int right = context.filterRegion().right();
+    QImage result = image.convertToFormat(QImage::Format_ARGB32);
+
     const int bottom = context.filterRegion().bottom();
+    const int left = context.filterRegion().left();
+    const int right = context.filterRegion().right();
     const int width = result.width();
-    for( int row = context.filterRegion().top(); row < bottom; ++row ) {
-        for( int col = context.filterRegion().left(); col < right; ++col ){
-            const QRgb currentPixel = pixel[row * width + col];
-            const int red = qRed(currentPixel);
-            const int green = qGreen(currentPixel);
-            const int blue = qBlue(currentPixel);
+
+    const QRgb *src = (const QRgb*)image.bits();
+    QRgb *dst = (QRgb*)result.bits();
+
+    for (int row = context.filterRegion().top(); row < bottom; ++row) {
+        for (int col = left; col < right; ++col) {
+            int index = row * width + col;
+            const QRgb &s = src[index];
+            const int red = qRed(s);
+            const int green = qGreen(s);
+            const int blue = qBlue(s);
+            const int alpha = qAlpha(s);
             const int grayValue = (red * 11 + green * 16 + blue * 5) / 32;
-            pixel[row * width + col] = qRgb(grayValue, grayValue, grayValue);
+            dst[index] = qRgba(grayValue, grayValue, grayValue, alpha);
         }
     }
     return result;

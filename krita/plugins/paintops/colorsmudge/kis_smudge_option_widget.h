0ab3808aa06fd99054ebc107802b8c6efa35b2ad
Silvio Heinrich <plassy@web.de>
2012-05-04 13:41:32 +0200

    Replace license header with a new one.

--- krita/plugins/paintops/colorsmudge/kis_smudge_option_widget.h
+++ krita/plugins/paintops/colorsmudge/kis_smudge_option_widget.h
@@ -1,22 +1,19 @@
-/* This file is part of the KDE project
- * 
- * Copyright (C) 2011 Silvio Heinrich <plassy@web.de>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- */
+/*
+    Copyright 2012  Silvio Heinrich <plassy@web.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either 
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef KIS_SMUDGE_OPTION_WIDGET_H
 #define KIS_SMUDGE_OPTION_WIDGET_H

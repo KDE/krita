5ec84fc71a6654433621ee25ba1355a9e5c9bb64
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2012-05-16 03:11:34 +0200

    it should be dc:language, not meta:language

--- libs/main/KoDocumentInfo.cpp
+++ libs/main/KoDocumentInfo.cpp
@@ -261,7 +261,7 @@ bool KoDocumentInfo::saveOasisAboutInfo(KoXmlWriter &xmlWriter)
                     xmlWriter.endElement();
                 }
             } else if (tag == "title" || tag == "description" || tag == "subject" ||
-                       tag == "date") {
+                       tag == "date" || tag == "language") {
                 QByteArray elementName(QString("dc:" + tag).toLatin1());
                 xmlWriter.startElement(elementName);
                 xmlWriter.addTextNode(aboutInfo(tag));

2e3a301022644f5f5ad997eb3004e70ac35fe844
Arjen Hiemstra <ahiemstra@heimr.nl>
2012-05-18 00:21:14 +0200

    Make sure to switch to nearest filtering only at zoom > 200%
    As correctly pointed out by LukasT, we only want to see pixels at zoom
    >200%, which I did not realise and so inadvertantly changed. This fixes
    that.

--- krita/ui/opengl/kis_opengl_canvas2.cpp
+++ krita/ui/opengl/kis_opengl_canvas2.cpp
@@ -278,9 +278,13 @@ void KisOpenGLCanvas2::drawImage()
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            if(scaleX > 2.0) {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            } else {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            }
             glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
             tile->drawPoints();
         }

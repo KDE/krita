KDE/calligra

commit 5e639ec4883390d79c6604297d4ca2939bc57fa9

remove wantsExportConfirmation virtual method
Replaced by a configuration string

libs/main/KoMainWindow.cpp
@@ -750,7 +750,11 @@ void KoMainWindow::slotSaveCompleted()
 // returns true if we should save, false otherwise.
 bool KoMainWindow::exportConfirmation(const QByteArray &outputFormat)
 {
-    if (!rootDocument()->wantExportConfirmation()) return true;
+    KConfigGroup group = KGlobal::config()->group(rootDocument()->componentData().componentName());
+    if (!group.readEntry("WantExportConfirmation", true)) {
+        return false;
+    }
+
     KMimeType::Ptr mime = KMimeType::mimeType(outputFormat);
     QString comment = mime ? mime->comment() : i18n("%1 (unknown file type)", QString::fromLatin1(outputFormat));
 

krita/data/kritarc
@@ -1,3 +1,6 @@
+[krita]
+WantExportConfirmation=false
+
 favoriteCompositeOps=normal,erase,multiply
 
 [TemplateChooserDialog]

krita/ui/kis_doc2.h
@@ -67,10 +67,6 @@ public:
     virtual ~KisDoc2();
 
 public:
-    // Overide KoDocument
-    virtual bool wantExportConfirmation() const {
-        return false;
-    }
     virtual bool completeLoading(KoStore *store);
     virtual bool completeSaving(KoStore*);
 

libs/main/KoDocument.cpp
@@ -635,11 +635,6 @@ void KoDocument::setSaveInBatchMode(const bool batchMode)
     d->filterManager->setBatchMode(batchMode);
 }
 
-bool KoDocument::wantExportConfirmation() const
-{
-    return true;
-}
-
 bool KoDocument::isImporting() const
 {
     return d->isImporting;

libs/main/KoDocument.h
@@ -319,8 +319,6 @@ public:
      */
     void setSaveInBatchMode(const bool batchMode);
 
-    virtual bool wantExportConfirmation() const;
-
     /**
      * Sets the error message to be shown to the user (use i18n()!)
      * when loading or saving fails.

KDE/krita

commit a0e5246543d6f35ffcaa90ab5d4b298cd004f8e5

Fix saving as non .kra from krita
True, apparently, isn't false. And the other way around. Fixes regression
introduced in 5e639ec4883390d79c6604297d4ca2939bc57fa9

libs/main/KoMainWindow.cpp
@@ -751,7 +751,7 @@ bool KoMainWindow::exportConfirmation(const QByteArray &outputFormat)
 {
     KConfigGroup group = KGlobal::config()->group(rootDocument()->componentData().componentName());
     if (!group.readEntry("WantExportConfirmation", true)) {
-        return false;
+        return true;
     }
 
     KMimeType::Ptr mime = KMimeType::mimeType(outputFormat);
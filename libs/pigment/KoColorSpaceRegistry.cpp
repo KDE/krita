7c1897136cab2a0785b6cbaebbde817e2800fb85
Boudewijn Rempt <boud@valdyas.org>
2012-05-31 11:39:59 +0200

    add some asserts

--- libs/pigment/KoColorSpaceRegistry.cpp
+++ libs/pigment/KoColorSpaceRegistry.cpp
@@ -386,6 +386,7 @@ const KoColorSpace * KoColorSpaceRegistry::colorSpace(const QString &csID, const
 
 const KoColorSpace * KoColorSpaceRegistry::colorSpace(const QString &csID, const KoColorProfile *profile)
 {
+    Q_ASSERT(!csID.isEmpty());
     if (profile) {
         const KoColorSpace *cs = 0;
         if (isCached(csID, profile->name())) {

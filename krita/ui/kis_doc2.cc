3eaba78bad51b1e871c423d87b18abae20a1535d
Boudewijn Rempt <boud@valdyas.org>
2012-05-31 11:40:39 +0200

    add some asserts

--- krita/ui/kis_doc2.cc
+++ krita/ui/kis_doc2.cc
@@ -385,6 +385,8 @@ bool KisDoc2::newImage(const QString& name,
                        const KoColorSpace* cs, const KoColor &bgColor,
                        const QString &description, const double imageResolution)
 {
+    Q_ASSERT(cs);
+
     if (!init())
         return false;
 
@@ -402,6 +404,7 @@ bool KisDoc2::newImage(const QString& name,
 
     connect(image.data(), SIGNAL(sigImageModified()), this, SLOT(setModified()));
     image->setResolution(imageResolution, imageResolution);
+
     image->assignImageProfile(cs->profile());
     documentInfo()->setAboutInfo("title", name);
     documentInfo()->setAboutInfo("comments", description);

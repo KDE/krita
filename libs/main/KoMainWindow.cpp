3cc0a0dc155a5bfe0be08c6d2ea6d67ae6b99d58
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2012-06-05 08:05:12 +0200

    workaround for setCanvas/unsetCanvas methods getting called in the incorrect order, hence resulting in dockers not knowing about the active canvas. By unsetting the parent for the view before deleting them, this ensures that ~KoCanvasControllerWidget can't find the KoCanvasSupervisor and thus can't call unsetCanvas on the KoCanvasObserver's

--- libs/main/KoMainWindow.cpp
+++ libs/main/KoMainWindow.cpp
@@ -481,7 +481,9 @@ void KoMainWindow::setRootDocument(KoDocument *doc)
     emit restoringDone();
 
     while(!oldRootViews.isEmpty()) {
-        delete oldRootViews.takeFirst();
+        KoView* view = oldRootViews.takeFirst();
+        view->setParent(0);
+        delete view;
     }
     if (oldRootDoc && oldRootDoc->viewCount() == 0) {
         //kDebug(30003) <<"No more views, deleting old doc" << oldRootDoc;

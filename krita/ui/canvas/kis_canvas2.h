730b062c49802884458af32b780877414a513845
Boudewijn Rempt <boud@valdyas.org>
2012-06-15 11:50:07 +0200

    fix spelling error

--- krita/ui/canvas/kis_canvas2.h
+++ krita/ui/canvas/kis_canvas2.h
@@ -25,6 +25,7 @@
 #include <QSize>
 #include <QString>
 
+#include <KoColorSpace.h>
 #include <KoCanvasBase.h>
 #include <krita_export.h>
 #include <kis_types.h>
@@ -40,6 +41,7 @@ class KisCanvasDecoration;
 class KisView2;
 class KisPaintopBox;
 class KoFavoriteResourceManager;
+class KisDisplayFilter;
 
 enum KisCanvasType {
     QPAINTER,
@@ -170,8 +172,17 @@ public slots:
     void startUpdateCanvasProjection(const QRect & rc);
     void updateCanvasProjection(KisUpdateInfoSP info);
 
+    void startUpdateInPatches(QRect imageRect);
+
+    void setMonitorProfile(KoColorProfile* monitorProfile,
+                           KoColorConversionTransformation::Intent renderingIntent);
+
+
+
+    void setDisplayFilter(KisDisplayFilter *displayFilter);
+
     void startResizingImage(qint32 w, qint32 h);
-    void finishResisingImage(qint32 w, qint32 h);
+    void finishResizingImage(qint32 w, qint32 h);
 
     /// adjust the origin of the document
     void adjustOrigin();

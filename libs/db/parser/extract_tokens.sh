7a8d77ad149dc43f96737c228431af36a295f394
Jaroslaw Staniek <staniek@kde.org>
2012-08-26 01:05:51 +0200

    proper indent in generated tokens.cpp GIT_SILENT

--- libs/db/parser/extract_tokens.sh
+++ libs/db/parser/extract_tokens.sh
@@ -4,8 +4,8 @@ echo "/* WARNING! All changes made in this file will be lost! Run 'generate_pars
 static const char* _tokens[] = {"
 for t in `grep  "\"[a-zA-Z_]*\"" sqlscanner.l | sed -e "s/\(^[^\"]*\)\"\([^\"]*\)\".*$/\2/g" | sort | uniq` ; do
 	if [ "$t" = "ZZZ" ] ; then break ; fi
-	echo "\"$t\",";
+    echo -e "    \"$t\",";
 done
 
-echo "0
+echo "    0
 };"

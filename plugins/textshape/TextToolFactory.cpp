ca8d2da230a91829bb6fb321cbb78688450de255
Gopalakrishna Bhat A <gopalakbhat@gmail.com>
2012-09-20 12:32:27 +0530

    Enable text tool by default in Author

--- plugins/textshape/TextToolFactory.cpp
+++ plugins/textshape/TextToolFactory.cpp
@@ -28,7 +28,7 @@ TextToolFactory::TextToolFactory()
         : KoToolFactoryBase("TextToolFactory_ID")
 {
     setToolTip(i18n("Text editing"));
-    setToolType(dynamicToolType()+",calligrawords");
+    setToolType(dynamicToolType()+",calligrawords,calligraauthor");
     setIconName(koIconNameCStr("tool-text"));
     setPriority(1);
     setActivationShapeId(TextShape_SHAPEID);

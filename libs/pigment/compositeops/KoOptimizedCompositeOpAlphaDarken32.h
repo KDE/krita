86cbcdb4d174631f6b9143e2187ee733b447a8af
Dmitry Kazakov <dimula73@gmail.com>
2012-10-25 19:21:40 +0400

    Added fast-path optimizations to the vector Alpha Darken composite op
    The cases of 0 or 255 alpha value are quite common in Krita

--- libs/pigment/compositeops/KoOptimizedCompositeOpAlphaDarken32.h
+++ libs/pigment/compositeops/KoOptimizedCompositeOpAlphaDarken32.h
@@ -56,6 +56,8 @@ struct AlphaDarkenCompositor32 {
 
         Vc::float_v uint8MaxRec2((float)1.0 / (255.0 * 255.0));
         Vc::float_v uint8MaxRec1((float)1.0 / 255.0);
+        Vc::float_v uint8Max((float)255.0);
+        Vc::float_v zeroValue(Vc::Zero);
 
 
         Vc::float_v msk_norm_alpha;
@@ -71,6 +73,8 @@ struct AlphaDarkenCompositor32 {
         dst_alpha = KoStreamedMath::fetch_alpha_32<true>(dst);
         src_alpha = msk_norm_alpha * opacity_vec;
 
+        Vc::float_m empty_dst_pixels_mask = dst_alpha == zeroValue;
+
         Vc::float_v src_c1;
         Vc::float_v src_c2;
         Vc::float_v src_c3;
@@ -79,38 +83,51 @@ struct AlphaDarkenCompositor32 {
         Vc::float_v dst_c2;
         Vc::float_v dst_c3;
 
-
         KoStreamedMath::fetch_colors_32<src_aligned>(src, src_c1, src_c2, src_c3);
+
+        bool srcAlphaIsZero = (src_alpha == zeroValue).isFull();
+        if (srcAlphaIsZero) return;
+
+        bool dstAlphaIsZero = empty_dst_pixels_mask.isFull();
+
         Vc::float_v dst_blend = src_alpha * uint8MaxRec1;
 
-        Vc::float_v alpha1 = src_alpha + dst_alpha -
-            dst_blend * dst_alpha;
+        bool srcAlphaIsUnit = (src_alpha == uint8Max).isFull();
 
-
-        Vc::float_m empty_pixels_mask = dst_alpha == Vc::float_v(Vc::Zero);
-
-        if (empty_pixels_mask.isFull()) {
+        if (dstAlphaIsZero) {
             dst_c1 = src_c1;
             dst_c2 = src_c2;
             dst_c3 = src_c3;
-        } else if (empty_pixels_mask.isEmpty()) {
+        } else if (srcAlphaIsUnit) {
+            bool dstAlphaIsUnit = (dst_alpha == uint8Max).isFull();
+            if (dstAlphaIsUnit) {
+                memcpy(dst, src, 4 * Vc::float_v::Size);
+                return;
+            } else {
+                dst_c1 = src_c1;
+                dst_c2 = src_c2;
+                dst_c3 = src_c3;
+            }
+        } else if (empty_dst_pixels_mask.isEmpty()) {
             KoStreamedMath::fetch_colors_32<true>(dst, dst_c1, dst_c2, dst_c3);
             dst_c1 = dst_blend * (src_c1 - dst_c1) + dst_c1;
             dst_c2 = dst_blend * (src_c2 - dst_c2) + dst_c2;
             dst_c3 = dst_blend * (src_c3 - dst_c3) + dst_c3;
         } else {
             KoStreamedMath::fetch_colors_32<true>(dst, dst_c1, dst_c2, dst_c3);
-            dst_c1(empty_pixels_mask) = src_c1;
-            dst_c2(empty_pixels_mask) = src_c2;
-            dst_c3(empty_pixels_mask) = src_c3;
+            dst_c1(empty_dst_pixels_mask) = src_c1;
+            dst_c2(empty_dst_pixels_mask) = src_c2;
+            dst_c3(empty_dst_pixels_mask) = src_c3;
 
-            Vc::float_m not_empty_pixels_mask = !empty_pixels_mask;
+            Vc::float_m not_empty_dst_pixels_mask = !empty_dst_pixels_mask;
 
-            dst_c1(not_empty_pixels_mask) = dst_blend * (src_c1 - dst_c1) + dst_c1;
-            dst_c2(not_empty_pixels_mask) = dst_blend * (src_c2 - dst_c2) + dst_c2;
-            dst_c3(not_empty_pixels_mask) = dst_blend * (src_c3 - dst_c3) + dst_c3;
+            dst_c1(not_empty_dst_pixels_mask) = dst_blend * (src_c1 - dst_c1) + dst_c1;
+            dst_c2(not_empty_dst_pixels_mask) = dst_blend * (src_c2 - dst_c2) + dst_c2;
+            dst_c3(not_empty_dst_pixels_mask) = dst_blend * (src_c3 - dst_c3) + dst_c3;
         }
 
+        Vc::float_v alpha1 = src_alpha + dst_alpha -
+            dst_blend * dst_alpha;
 
         Vc::float_m alpha2_mask = opacity_vec > dst_alpha;
         Vc::float_v opt1 = (opacity_vec - dst_alpha) * msk_norm_alpha + dst_alpha;

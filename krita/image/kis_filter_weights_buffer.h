708dbab384939a1ea2254c0986f4097fcb6809db
Dmitry Kazakov <dimula73@gmail.com>
2012-12-12 11:03:18 +0400

    Added two classes for the new version of KisTransformWorker
    KisFixedPoint now represents the .8 fixed point arithmetics. Current
    hardcoded fp.8 implementation in the KisTransformWorker has
    various flaws like rounding and dealing with negative values.
    
    KisFilterWeightsBuffer represents a part of KisTransformWorker that
    generates weights for the pixel blending. Being able to add it to
    the unittest I could catch many mathematical problems in it.
    
    Now these is one more class to be written (that will do the actual
    blending) and then it all will have to be merged into KisTransformWorker.
    
    CCBUG:289645

---
+++ krita/image/kis_filter_weights_buffer.h
@@ -0,0 +1,213 @@
+/*
+ *  Copyright (c) 2012 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __KIS_FILTER_WEIGHTS_BUFFER_H
+#define __KIS_FILTER_WEIGHTS_BUFFER_H
+
+#include "kis_fixed_point_maths.h"
+
+#ifdef SANITY_CHECKS_ENABLED
+static bool checkForAsymmetricZeros = false;
+
+#define SANITY_CENTER_POSITION()                        \
+    do {                                                \
+        Q_ASSERT(scaledIter >= beginDst);               \
+        Q_ASSERT(scaledIter <= endDst);                 \
+                                                        \
+        if (j == centerIndex) {                         \
+            Q_ASSERT(scaledIter == centerDst);          \
+        }                                               \
+    } while(0)
+
+#define SANITY_ZEROS()                                                  \
+    do {                                                                \
+        if (checkForAsymmetricZeros) {                                  \
+            for (int j = 0; j < span; j++) {                            \
+                int idx2 = span - j - 1;                                \
+                                                                        \
+                if ((m_filterWeights[i].weight[j] && !m_filterWeights[i].weight[idx2]) || \
+                    (!m_filterWeights[i].weight[j] && m_filterWeights[i].weight[idx2])) { \
+                                                                        \
+                    qDebug() << "*******";                              \
+                    qDebug() << "Non-symmetric zero found:" << centerDst; \
+                    qDebug() << "Weight" << j << ":" << m_filterWeights[i].weight[j]; \
+                    qDebug() << "Weight" << idx2 << ":" << m_filterWeights[i].weight[idx2]; \
+                    qFatal("Non-symmetric zero -> fail");               \
+                }                                                       \
+            }                                                           \
+        }                                                               \
+    } while (0)
+
+#define SANITY_CHECKSUM()                       \
+    do {                                        \
+        Q_ASSERT(sum == 255);                   \
+    } while (0)
+
+#else
+#define SANITY_CENTER_POSITION()
+#define SANITY_ZEROS()
+#define SANITY_CHECKSUM()
+#endif
+
+#ifdef DEBUG_ENABLED
+#define DEBUG_ALL()                                                     \
+    do {                                                                \
+        qDebug() << "************** i =" << i;                          \
+        qDebug() << ppVar(centerDst);                                   \
+        qDebug() << ppVar(centerIndex);                                 \
+        qDebug() << ppVar(beginSrc) << ppVar(endSrc);                   \
+        qDebug() << ppVar(beginDst) << ppVar(endDst);                   \
+        qDebug() << ppVar(scaledIter) << ppVar(scaledInc);              \
+        qDebug() << ppVar(span);                                        \
+        qDebug() << "===";                                              \
+    } while (0)
+
+#define DEBUG_SAMPLE()                                                  \
+    do {                                                                \
+        qDebug() << ppVar(scaledIter) << ppVar(t);                      \
+    } while (0)
+#else
+#define DEBUG_ALL()
+#define DEBUG_SAMPLE()
+#endif
+
+
+class KisFilterWeightsBuffer
+{
+public:
+    struct FilterWeights {
+        ~FilterWeights() {
+            delete[] weight;
+        }
+
+        qint16 *weight;
+        int span;
+        int centerIndex;
+    };
+
+public:
+    KisFilterWeightsBuffer(KisFilterStrategy *filterStrategy, qreal realScale) {
+        m_filterWeights = new FilterWeights[256];
+        m_maxSpan = 0;
+
+        KisFixedPoint transformScale(1);
+
+        KisFixedPoint supportSrc;
+        KisFixedPoint supportDst;
+
+        if (realScale < 1.0) {
+            supportSrc.from256Frac(filterStrategy->intSupport() / realScale);
+            supportDst.from256Frac(filterStrategy->intSupport());
+            transformScale = KisFixedPoint(realScale);
+        } else {
+            supportSrc.from256Frac(filterStrategy->intSupport());
+            supportDst.from256Frac(filterStrategy->intSupport());
+        }
+
+        for (int i = 0; i < 256; i++) {
+            KisFixedPoint centerDst;
+            centerDst.from256Frac(i);
+
+            KisFixedPoint beginDst = -supportDst;
+            KisFixedPoint endDst = supportDst;
+
+            KisFixedPoint beginSrc = -supportSrc - centerDst / transformScale;
+            KisFixedPoint endSrc = supportSrc - centerDst / transformScale;
+
+            int span = (2 * supportSrc).toInt() +
+                (beginSrc.isInteger() && endSrc.isInteger());
+
+            int centerIndex = -beginSrc.toInt();
+
+            m_filterWeights[i].centerIndex = centerIndex;
+            m_filterWeights[i].span = span;
+            m_filterWeights[i].weight = new qint16[span];
+            m_maxSpan = qMax(m_maxSpan, span);
+
+            // in dst coordinate system:
+            KisFixedPoint scaledIter = centerDst + beginSrc.toInt() * transformScale;
+            KisFixedPoint scaledInc = transformScale;
+
+            DEBUG_ALL();
+
+            int sum = 0;
+            for (int j = 0; j < span; j++) {
+                int t = filterStrategy->intValueAt(scaledIter.to256Frac());
+                m_filterWeights[i].weight[j] = t;
+                sum += t;
+
+                DEBUG_SAMPLE();
+                SANITY_CENTER_POSITION();
+
+                scaledIter += scaledInc;
+            }
+
+            SANITY_ZEROS();
+
+            if (sum != 255) {
+                qreal fixFactor = 255.0 / sum;
+                sum = 0;
+
+                for (int j = 0; j < span; j++) {
+                    int t = qRound(m_filterWeights[i].weight[j] * fixFactor);
+
+                    m_filterWeights[i].weight[j] = t;
+                    sum += t;
+                }
+            }
+
+            while (sum != 255) {
+                int diff = sum < 255 ? 1 : -1;
+                int index = findMaxIndex(m_filterWeights[i].weight, span);
+                m_filterWeights[i].weight[index] += diff;
+                sum += diff;
+            }
+
+            SANITY_CHECKSUM();
+        }
+    }
+
+    ~KisFilterWeightsBuffer() {
+        delete[] m_filterWeights;
+    }
+
+    FilterWeights* weights(KisFixedPoint pos) {
+        return m_filterWeights + pos.to256Frac();
+    }
+
+private:
+    int findMaxIndex(qint16 *buf, int size) {
+        int maxValue = buf[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i < size; i++) {
+            if (buf[i] > maxValue) {
+                maxValue = buf[i];
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+private:
+    FilterWeights *m_filterWeights;
+    int m_maxSpan;
+};
+
+#endif /* __KIS_FILTER_WEIGHTS_BUFFER_H */
---
+++ krita/image/tests/kis_filter_weights_buffer_test.cpp
@@ -0,0 +1,140 @@
+/*
+ *  Copyright (c) 2012 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_filter_weights_buffer_test.h"
+
+#include <qtest_kde.h>
+
+#include "kis_filter_strategy.h"
+
+//#define DEBUG_ENABLED
+#define SANITY_CHECKS_ENABLED
+#include "kis_filter_weights_buffer.h"
+
+#include "kis_debug.h"
+
+void checkWeightsBuffer(KisFilterStrategy *filter, qreal scale)
+{
+    qDebug() << "Testing:" << filter->name() << "Scale:" << scale;
+
+    KisFilterWeightsBuffer buf(filter, scale);
+
+    KisFixedPoint fp1;
+    KisFixedPoint fp2;
+
+    const int startIndex = 1;
+    const int endIndex = 255 * qMin(scale, 1.0);
+
+    fp1.from256Frac(startIndex);
+    fp2.from256Frac(endIndex);
+
+    while (fp2 > fp1) {
+        KisFilterWeightsBuffer::FilterWeights *w1 = buf.weights(fp1);
+        KisFilterWeightsBuffer::FilterWeights *w2 = buf.weights(fp2);
+
+        QCOMPARE(w1->span, w2->span);
+
+        int span = w1->span;
+
+        // Check for symmetry around center offset
+        for (int i = 0; i < span; i++) {
+            int idx2 = span - i - 1;
+
+            int v1 = w1->weight[i];
+            int v2 = w2->weight[idx2];
+
+            if (v1 != v2) {
+
+#ifdef DEBUG_ENABLED
+                qDebug() << "*******";
+                qDebug() << "Weight" << fp1 << "|" << i << ":" << v1;
+                qDebug() << "Weight" << fp2 << "|" << idx2 << ":" << v2;
+#endif /* DEBUG_ENABLED */
+
+                if (!(span & 0x1) && (qAbs(v1 - v2) <= (0.5 * span))) {
+#ifdef DEBUG_ENABLED
+                    qDebug() << "Symmetry is wrong due to evenly-sized kernel or rounding. It's ok. Accepting.";
+#endif /* DEBUG_ENABLED */
+                } else {
+                    QFAIL("Wrong weight symmetry");
+                }
+            }
+        }
+
+        fp1.inc256Frac();
+        fp2.dec256Frac();
+    }
+}
+
+void checkOneFilter(KisFilterStrategy *filter)
+{
+    checkWeightsBuffer(filter, 1.0);
+    checkWeightsBuffer(filter, 2.0);
+    checkWeightsBuffer(filter, 0.5);
+    checkWeightsBuffer(filter, 0.25);
+    checkWeightsBuffer(filter, 0.125);
+    delete filter;
+    checkForAsymmetricZeros = false;
+}
+
+
+void KisFilterWeightsBufferTest::testTriangle()
+{
+    checkForAsymmetricZeros = true;
+    checkOneFilter(new KisTriangleFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testHermite()
+{
+    checkOneFilter(new KisHermiteFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testBicubic()
+{
+    checkOneFilter(new KisBicubicFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testBox()
+{
+    checkOneFilter(new KisBoxFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testBell()
+{
+    checkOneFilter(new KisBellFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testBSpline()
+{
+    checkOneFilter(new KisBSplineFilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testLanczos3()
+{
+    checkOneFilter(new KisLanczos3FilterStrategy());
+}
+
+void KisFilterWeightsBufferTest::testMitchell()
+{
+    checkForAsymmetricZeros = true;
+    checkOneFilter(new KisMitchellFilterStrategy());
+}
+
+
+
+QTEST_KDEMAIN(KisFilterWeightsBufferTest, GUI)

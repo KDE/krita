KDE/calligra

commit dd7f3b8f2f02a57dda39e9f1c8608801b8c2cf3b

Fix crash due to acessing textrangemanager on the toc document which was never set
solution is to just make sure we set a dummy one

libs/kotext/KoTextEditor.cpp
@@ -1302,6 +1302,9 @@ void KoTextEditor::insertTableOfContents(KoTableOfContentsGeneratorInfo *info)
     tocFormat.setProperty(KoParagraphStyle::TableOfContentsData, QVariant::fromValue<KoTableOfContentsGeneratorInfo *>(newToCInfo) );
     tocFormat.setProperty(KoParagraphStyle::GeneratedDocument, QVariant::fromValue<QTextDocument*>(tocDocument));
 
+    //make sure we set up the textrangemanager on the subdocument as well
+    KoTextDocument(tocDocument).setTextRangeManager(KoTextDocument(new KoTextRangeManager);
+
     KoChangeTracker *changeTracker = KoTextDocument(d->document).changeTracker();
     if (changeTracker && changeTracker->recordChanges()) {
         QTextCharFormat charFormat = d->caret.charFormat();

libs/kotext/opendocument/KoTextLoader.cpp
@@ -2619,6 +2619,7 @@ void KoTextLoader::loadTableOfContents(const KoXmlElement &element, QTextCursor 
     // to store the contents we use an extrafor "meta-information" about the TOC we use this class
     QTextDocument *tocDocument = new QTextDocument();
     KoTextDocument(tocDocument).setStyleManager(d->styleManager);
+    KoTextDocument(tocDocument).setTextRangeManager(new KoTextRangeManager);
 
     info->m_name = element.attribute("name");
     info->m_styleName = element.attribute("style-name");

KDE/krita

commit 186d04d6cf335eccd06464f7849436bc77e01686

Fix compile broken by dd7f3b8f2f02a57dda39e9f1c8608801b8c2cf3b
(not everyone has the magic intention-knowing compiler :) )

libs/kotext/KoTextEditor.cpp
@@ -1303,7 +1303,7 @@ void KoTextEditor::insertTableOfContents(KoTableOfContentsGeneratorInfo *info)
     tocFormat.setProperty(KoParagraphStyle::GeneratedDocument, QVariant::fromValue<QTextDocument*>(tocDocument));
 
     //make sure we set up the textrangemanager on the subdocument as well
-    KoTextDocument(tocDocument).setTextRangeManager(KoTextDocument(new KoTextRangeManager);
+    KoTextDocument(tocDocument).setTextRangeManager(new KoTextRangeManager);
 
     KoChangeTracker *changeTracker = KoTextDocument(d->document).changeTracker();
     if (changeTracker && changeTracker->recordChanges()) {
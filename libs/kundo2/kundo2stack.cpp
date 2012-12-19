bbab2ef20e6a0af21e4fe3373ae6601d76318797
Arjen Hiemstra <ahiemstra@heimr.nl>
2012-12-19 15:03:03 +0100

    Always initialise m_hasParent for KUndo2Command

--- libs/kundo2/kundo2stack.cpp
+++ libs/kundo2/kundo2stack.cpp
@@ -131,7 +131,8 @@ KUndo2Command::KUndo2Command(const QString &text, KUndo2Command *parent):
     \sa ~KUndo2Command()
 */
 
-KUndo2Command::KUndo2Command(KUndo2Command *parent)
+KUndo2Command::KUndo2Command(KUndo2Command *parent):
+    m_hasParent(parent != 0)
 {
     d = new KUndo2CommandPrivate;
     if (parent != 0)

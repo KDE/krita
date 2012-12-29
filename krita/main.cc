587ca36567f8c6d62bb2bcd962a5792f7630504d
Boudewijn Rempt <boud@valdyas.org>
2012-12-29 11:54:28 +0100

    Port the setting of env variables on windows to krita (from sketch)
    This makes sure that krita can start, even without the .bat file, but
    it shouldn't hamper using the .bat file in the shared calligra windows
    package.
    
    BUG:298690

--- krita/main.cc
+++ krita/main.cc
@@ -38,6 +38,10 @@
 #include "data/splash/splash_screen.xpm"
 #include "ui/kis_aboutdata.h"
 
+#ifdef Q_OS_WIN
+#include "stdlib.h"
+#endif
+
 extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
 {
 #ifdef Q_WS_X11
@@ -62,6 +66,42 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
     QPixmap pm(splash_screen_xpm);
     QSplashScreen *splash = new KSplashScreen(pm);
     app.setSplashScreen(splash);
+
+#ifdef Q_OS_WIN
+    QDir appdir(app.applicationDirPath());
+    appdir.cdUp();
+
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+    // If there's no kdehome, set it
+
+    if (!env.contains("KDEHOME") ) {
+        _putenv_s("KDEHOME", QDesktopServices::storageLocation(QDesktopServices::DataLocation).toLocal8Bit());
+    }
+    if (!env.contains("KDESYCOCA")) {
+        _putenv_s("KDESYCOCA", QString(appdir.absolutePath() + "/sycoca").toLocal8Bit());
+    }
+    if (!env.contains("XDG_DATA_DIRS")) {
+        _putenv_s("XDG_DATA_DIRS", QString(appdir.absolutePath() + "/share").toLocal8Bit());
+    }
+    if (!env.contains("KDEDIR")) {
+        _putenv_s("KDEDIR", appdir.absolutePath().toLocal8Bit());
+    }
+    if (!env.contains("KDEDIRS")) {
+        _putenv_s("KDEDIRS", appdir.absolutePath().toLocal8Bit());
+    }
+    _putenv_s("PATH", QString(appdir.absolutePath() + "/bin" + ";"
+              + appdir.absolutePath() + "/lib" + ";"
+              + appdir.absolutePath() + "/lib"  +  "/kde4" + ";"
+              + appdir.absolutePath()).toLocal8Bit());
+
+    app.addLibraryPath(appdir.absolutePath());
+    app.addLibraryPath(appdir.absolutePath() + "/bin");
+    app.addLibraryPath(appdir.absolutePath() + "/lib");
+    app.addLibraryPath(appdir.absolutePath() + "/lib/kde4");
+#endif
+
+
+
 
     if (!app.start()) {
         return 1;

f5883459eb732a070bb8fc2f9888ab92eda78aaf
Sven Langkamp <sven.langkamp@gmail.com>
2013-01-02 14:15:52 +0100

    fix date

--- krita/plugins/formats/qml/qml_export.h
+++ krita/plugins/formats/qml/qml_export.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2007 Sven Langkamp <sven.langkamp@gmail.com>
+ *  Copyright (c) 2013 Sven Langkamp <sven.langkamp@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by

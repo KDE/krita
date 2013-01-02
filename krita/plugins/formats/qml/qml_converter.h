e811b49fef7ffffbe3e526afcdea273cbaabe7a0
Sven Langkamp <sven.langkamp@gmail.com>
2013-01-02 02:20:13 +0100

    add QML export

---
+++ krita/plugins/formats/qml/qml_converter.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2007 Sven Langkamp <sven.langkamp@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _QML_CONVERTER_H_
+#define _QML_CONVERTER_H_
+
+#include <stdio.h>
+
+#include <QObject>
+
+// #include <kio/job.h>
+
+#include "kis_types.h"
+class KisDoc2;
+
+/**
+ * Image import/export plugins can use these results to report about success or failure.
+ */
+enum KisImageBuilder_Result {
+    KisImageBuilder_RESULT_FAILURE = -400,
+    KisImageBuilder_RESULT_NOT_EXIST = -300,
+    KisImageBuilder_RESULT_NOT_LOCAL = -200,
+    KisImageBuilder_RESULT_BAD_FETCH = -100,
+    KisImageBuilder_RESULT_INVALID_ARG = -50,
+    KisImageBuilder_RESULT_OK = 0,
+    KisImageBuilder_RESULT_PROGRESS = 1,
+    KisImageBuilder_RESULT_EMPTY = 100,
+    KisImageBuilder_RESULT_BUSY = 150,
+    KisImageBuilder_RESULT_NO_URI = 200,
+    KisImageBuilder_RESULT_UNSUPPORTED = 300,
+    KisImageBuilder_RESULT_INTR = 400,
+    KisImageBuilder_RESULT_PATH = 500,
+    KisImageBuilder_RESULT_UNSUPPORTED_COLORSPACE = 600
+};
+
+class QMLConverter : public QObject
+{
+    Q_OBJECT
+public:
+    QMLConverter();
+    virtual ~QMLConverter();
+public:
+    KisImageBuilder_Result buildFile(const KUrl& uri, KisImageWSP image);
+
+private:
+    void writeString(QTextStream& out, int spacing, const QString& setting, const QString& value);
+    void writeInt(QTextStream& out, int spacing, const QString& setting, int value);
+};
+
+#endif

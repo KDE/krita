9a325eaf89fbb74450bc68e898c9d1601b79b0f2
Boudewijn Rempt <boud@valdyas.org>
2013-01-10 11:40:58 +0100

    Disable the KoM2MMLForumulaTool when compiling with msvc
    I'm not sure how to get this working; we'll need to spend more time on
    it, but I haven't got the time now.

--- plugins/formulashape/KoFormulaShapePlugin.cpp
+++ plugins/formulashape/KoFormulaShapePlugin.cpp
@@ -25,7 +25,9 @@
 
 #include <kpluginfactory.h>
 
+#ifndef _MSC_VER
 #include "KoM2MMLForumulaTool.h"
+#endif
 
 K_PLUGIN_FACTORY(KoFormulaShapePluginFactory, registerPlugin<KoFormulaShapePlugin>();)
 K_EXPORT_PLUGIN(KoFormulaShapePluginFactory("FormulaShape"))
@@ -34,7 +36,9 @@ KoFormulaShapePlugin::KoFormulaShapePlugin( QObject* parent, const QVariantList&
                     : QObject( parent )
 {
     KoToolRegistry::instance()->add( new KoFormulaToolFactory() );
+#ifndef _MSC_VER
     KoToolRegistry::instance()->add( new KoM2MMLFormulaToolFactory());
+#endif
     KoShapeRegistry::instance()->add( new KoFormulaShapeFactory() );
 }
 

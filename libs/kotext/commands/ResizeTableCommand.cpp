KDE/calligra

commit 68f274e9db08605f0911618be9ca2ecbd3858c47

Column and row styles should be implicitly shared otherwise it can cause problems when loading a file where the same style is used in more than one row/column
From now on it's important to set the style back after you have modified it.

We will have to look into how this should work with named styles, but for now
we don't even have ui to change named table styles.

libs/kotext/commands/ResizeTableCommand.cpp
@@ -92,11 +92,12 @@ void ResizeTableCommand::redo()
         m_first = false;
         if (m_horizontal) {
             m_oldColumnStyle = carsManager.columnStyle(m_band).clone();
-
             // make sure the style is set (could have been a default style)
             carsManager.setColumnStyle(m_band, carsManager.columnStyle(m_band));
 
-            carsManager.columnStyle(m_band).setColumnWidth(m_size);
+            KoTableColumnStyle style = carsManager.columnStyle(m_band);
+            style.setColumnWidth(m_size);
+            carsManager.setColumnStyle(m_band, style);
 
             m_newColumnStyle = carsManager.columnStyle(m_band).clone();
         } else {
@@ -105,7 +106,9 @@ void ResizeTableCommand::redo()
             // make sure the style is set (could have been a default style)
             carsManager.setRowStyle(m_band, carsManager.rowStyle(m_band));
 
-            carsManager.rowStyle(m_band).setMinimumRowHeight(m_size);
+            KoTableRowStyle style = carsManager.rowStyle(m_band);
+            style.setMinimumRowHeight(m_size);
+            carsManager.setRowStyle(m_band, style);
 
             m_newRowStyle = carsManager.rowStyle(m_band).clone();
         }

libs/kotext/KoTableColumnAndRowStyleManager.h
@@ -89,6 +89,8 @@ public:
     /**
      * Get the column style for the column \a column.
      *
+     * If you modify it don't forget to set it back here to actually have an effect
+     *
      * @param column the column to get the style for.
      * @return the column style.
      */
@@ -123,6 +125,8 @@ public:
 
     /**
      * Get the row style for the row \a column.
+     *
+     * If you modify it don't forget to set it back here to actually have an effect
      *
      * @param row the row to get the style for.
      * @return the row style.

libs/kotext/styles/KoTableColumnStyle.h
@@ -48,9 +48,7 @@ class KoOdfLoadingContext;
  *
  * The style has a property 'StyleId' with an integer as value. The integer value
  * corresponds to the styleId() output of a specific KoTableColumnStyle.
- *
- * TODO: Make this class implicitly shared.
- *
+ * *
  * @see KoStyleManager, KoTableRowAndColumnStyleManager
  */
 class KOTEXT_EXPORT KoTableColumnStyle
@@ -198,7 +196,7 @@ private:
     QColor propertyColor(int key) const;
 
     class Private;
-    QExplicitlySharedDataPointer<Private> d;
+    QSharedDataPointer<Private> d;
 };
 
 #endif

libs/kotext/styles/KoTableRowStyle.h
@@ -182,7 +182,7 @@ private:
     QColor propertyColor(int key) const;
 
     class Private;
-    QExplicitlySharedDataPointer<Private> d;
+    QSharedDataPointer<Private> d;
 };
 
 #endif

KDE/krita

commit cffd9e7cdcbab0ec20693ca7c2a43a1f981252c3

fix a regression making undo/redo for column and row reszes not working
was a regression caused by commit 68f274e9db08605f0911618be9ca2ecbd3858c47

libs/kotext/commands/ResizeTableCommand.cpp
@@ -64,10 +64,14 @@ void ResizeTableCommand::undo()
     KoTableColumnAndRowStyleManager carsManager = KoTableColumnAndRowStyleManager::getManager(table);
 
     if (m_oldColumnStyle) {
-        carsManager.columnStyle(m_band).copyProperties(m_oldColumnStyle);
+        KoTableColumnStyle style = carsManager.columnStyle(m_band);
+        style.copyProperties(m_oldColumnStyle);
+        carsManager.setColumnStyle(m_band, style);
     }
     if (m_oldRowStyle) {
-        carsManager.rowStyle(m_band).copyProperties(m_oldRowStyle);
+        KoTableRowStyle style = carsManager.rowStyle(m_band);
+        style.copyProperties(m_oldRowStyle);
+        carsManager.setRowStyle(m_band, style);
     }
     KUndo2Command::undo();
     m_document->markContentsDirty(m_tablePosition, table->lastPosition()-table->firstPosition());
@@ -83,9 +87,13 @@ void ResizeTableCommand::redo()
 
     if (!m_first) {
         if (m_horizontal) {
-            carsManager.columnStyle(m_band).copyProperties(m_newColumnStyle);
+            KoTableColumnStyle style = carsManager.columnStyle(m_band);
+            style.copyProperties(m_newColumnStyle);
+            carsManager.setColumnStyle(m_band, style);
         } else {
-            carsManager.rowStyle(m_band).copyProperties(m_newRowStyle);
+            KoTableRowStyle style = carsManager.rowStyle(m_band);
+            style.copyProperties(m_newRowStyle);
+            carsManager.setRowStyle(m_band, style);
         }
         KUndo2Command::redo();
     } else {
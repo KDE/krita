8a32c52a34745ded9f31eb643ed320ed422b62a3
Dmitry Kazakov <dimula73@gmail.com>
2013-01-21 15:04:06 +0400

    Use GLEW for juggling with openGL extensions

--- krita/ui/opengl/kis_opengl_canvas2_p.h
+++ krita/ui/opengl/kis_opengl_canvas2_p.h
@@ -26,142 +26,18 @@
 /**
  * This is a workaround for a very slow updates in OpenGL canvas (~6ms).
  * The delay happens because of VSync in the swapBuffers() call. At first
- * we try to select a Signle Buffer context. If it fails (may be true for
- * OpenGL ES (?) ), we just try to disable VSync.
- *
- * This file is effectively a bit of copy-paste from qgl_x11.cpp
+ * we try to disable VSync. If it fails we just disable double buffer.
  */
 
 
 #if defined Q_OS_LINUX
 
-#include <QByteArray>
-#include <QVector>
-#include <QLibrary>
 #include <QX11Info>
-#include <GL/glx.h>
-
-#ifndef GL_NUM_EXTENSIONS
-#define GL_NUM_EXTENSIONS 0x821D
-#endif
+#include <GL/glxew.h>
 
 extern const QString qt_gl_library_name();
 
 namespace VSyncWorkaround {
-
-    class QGLExtensionMatcher
-    {
-    public:
-        QGLExtensionMatcher(const char *str);
-        QGLExtensionMatcher();
-
-        bool match(const char *str) const {
-            int str_length = qstrlen(str);
-
-            Q_ASSERT(str);
-            Q_ASSERT(str_length > 0);
-            Q_ASSERT(str[str_length-1] != ' ');
-
-            for (int i = 0; i < m_offsets.size(); ++i) {
-                const char *extension = m_extensions.constData() + m_offsets.at(i);
-                if (qstrncmp(extension, str, str_length) == 0 && extension[str_length] == ' ')
-                    return true;
-            }
-            return false;
-        }
-
-    private:
-        void init(const char *str);
-
-        QByteArray m_extensions;
-        QVector<int> m_offsets;
-    };
-
-
-    typedef const GLubyte * (*qt_glGetStringi)(GLenum, GLuint);
-
-    QGLExtensionMatcher::QGLExtensionMatcher(const char *str)
-    {
-        init(str);
-    }
-
-    QGLExtensionMatcher::QGLExtensionMatcher()
-    {
-        const char *extensionStr = reinterpret_cast<const char *>(glGetString(GL_EXTENSIONS));
-
-        if (extensionStr) {
-            init(extensionStr);
-        } else {
-            // clear error state
-            while (glGetError()) {}
-
-            const QGLContext *ctx = QGLContext::currentContext();
-            if (ctx) {
-                qt_glGetStringi glGetStringi = (qt_glGetStringi)ctx->getProcAddress(QLatin1String("glGetStringi"));
-
-                GLint numExtensions;
-                glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
-
-                for (int i = 0; i < numExtensions; ++i) {
-                    const char *str = reinterpret_cast<const char *>(glGetStringi(GL_EXTENSIONS, i));
-
-                    m_offsets << m_extensions.size();
-
-                    while (*str != 0)
-                        m_extensions.append(*str++);
-                    m_extensions.append(' ');
-                }
-            }
-        }
-    }
-
-    void QGLExtensionMatcher::init(const char *str)
-    {
-        m_extensions = str;
-
-        // make sure extension string ends with a space
-        if (!m_extensions.endsWith(' '))
-            m_extensions.append(' ');
-
-        int index = 0;
-        int next = 0;
-        while ((next = m_extensions.indexOf(' ', index)) >= 0) {
-            m_offsets << index;
-            index = next + 1;
-        }
-    }
-
-    void* qglx_getProcAddress(const char* procName)
-    {
-        // On systems where the GL driver is pluggable (like Mesa), we have to use
-        // the glXGetProcAddressARB extension to resolve other function pointers as
-        // the symbols wont be in the GL library, but rather in a plugin loaded by
-        // the GL library.
-        typedef void* (*qt_glXGetProcAddressARB)(const char *);
-        static qt_glXGetProcAddressARB glXGetProcAddressARB = 0;
-        static bool triedResolvingGlxGetProcAddress = false;
-        if (!triedResolvingGlxGetProcAddress) {
-            triedResolvingGlxGetProcAddress = true;
-            QGLExtensionMatcher extensions(glXGetClientString(QX11Info::display(), GLX_EXTENSIONS));
-            if (extensions.match("GLX_ARB_get_proc_address")) {
-                QLibrary lib(::qt_gl_library_name());
-                //lib.setLoadHints(QLibrary::ImprovedSearchHeuristics);
-                glXGetProcAddressARB = (qt_glXGetProcAddressARB) lib.resolve("glXGetProcAddressARB");
-            }
-        }
-
-        void *procAddress = 0;
-        if (glXGetProcAddressARB)
-            procAddress = glXGetProcAddressARB(procName);
-
-        if (!procAddress) {
-            QLibrary lib(::qt_gl_library_name());
-            //lib.setLoadHints(QLibrary::ImprovedSearchHeuristics);
-            procAddress = lib.resolve(procName);
-        }
-
-        return procAddress;
-    }
 
     bool tryDisableVSync(QWidget *widget) {
         bool result = false;
@@ -169,47 +45,30 @@ namespace VSyncWorkaround {
         Display *dpy = info.display();
         WId wid = widget->winId();
 
-        QGLExtensionMatcher extensions(glXQueryExtensionsString(dpy, info.screen()));
+#ifdef GLX_EXT_swap_control
 
-        if (extensions.match("GLX_EXT_swap_control")) {
-            typedef void (*kis_glXSwapIntervalEXT)(Display*, WId, int);
-            kis_glXSwapIntervalEXT glXSwapIntervalEXT = (kis_glXSwapIntervalEXT)qglx_getProcAddress("glXSwapIntervalEXT");
+        if (glewIsSupported("GLX_EXT_swap_control")) {
+            glXSwapIntervalEXT(dpy, wid, 0);
 
-            if (glXSwapIntervalEXT) {
-                glXSwapIntervalEXT(dpy, wid, 0);
+            unsigned int swap = 0;
+            glXQueryDrawable(dpy, wid, GLX_SWAP_INTERVAL_EXT, &swap);
 
-                unsigned int swap = 1;
+            result = !swap;
+        }
+#else
+        if (0) {}
+#endif /* GLX_EXT_swap_control */
 
-#ifdef GLX_SWAP_INTERVAL_EXT
-                glXQueryDrawable(dpy, wid, GLX_SWAP_INTERVAL_EXT, &swap);
-#endif
-
-                result = !swap;
-            } else {
-                qDebug() << "Couldn't load glXSwapIntervalEXT extension function";
-            }
-        } else if (extensions.match("GLX_MESA_swap_control")) {
-            typedef int (*kis_glXSwapIntervalMESA)(unsigned int);
-            typedef int (*kis_glXGetSwapIntervalMESA)(void);
-
-            kis_glXSwapIntervalMESA glXSwapIntervalMESA = (kis_glXSwapIntervalMESA)qglx_getProcAddress("glXSwapIntervalMESA");
-            kis_glXGetSwapIntervalMESA glXGetSwapIntervalMESA = (kis_glXGetSwapIntervalMESA)qglx_getProcAddress("glXGetSwapIntervalMESA");
-
-            if (glXSwapIntervalMESA) {
+#ifdef GLX_MESA_swap_control
+        else if (glewIsSupported("GLX_EXT_swap_control")) {
                 int retval = glXSwapIntervalMESA(0);
-                int swap = 1;
-
-                if (glXGetSwapIntervalMESA) {
-                    swap = glXGetSwapIntervalMESA();
-                } else {
-                    qDebug() << "Couldn't load glXGetSwapIntervalMESA extension function";
-                }
+                int swap = glXGetSwapIntervalMESA();
 
                 result = !retval && !swap;
-            } else {
-                qDebug() << "Couldn't load glXSwapIntervalMESA extension function";
-            }
-        } else {
+        }
+#endif /* GLX_MESA_swap_control */
+
+        else {
             qDebug() << "There is neither GLX_EXT_swap_control or GLX_MESA_swap_control extension supported";
         }
 

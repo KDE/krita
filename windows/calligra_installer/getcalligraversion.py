47490da543fbc7431590b8f392eda290646cf5b2
Stuart Dickson <stuartmd@kogmbh.com>
2013-01-28 13:33:54 +0100

    Improved version checking in Calligra installer.

--- windows/calligra_installer/getcalligraversion.py
+++ windows/calligra_installer/getcalligraversion.py
@@ -1,10 +1,28 @@
+import sys
 import os
 import re
-# fname = 'C:/Program Files (x86)/Jenkins/jobs/cnightly25/workspace/source/libs/main/calligraversion.h'
-#print(os.environ['WORKSPACE'])
-#fname = os.environ['WORKSPACE'] + '/source/libs/main/calligraversion.h'
-#fname = os.path.normpath(fname)
-fname = 'M:/jenkins_local/calligra26/workspace/source/libs/main/calligraversion.h'
+
+fname = os.getenv('WORKSPACE')
+if fname:
+    fname = fname + '/source/libs/main/calligraversion.h'
+    os.path.normpath(fname)
+else:
+    fname = os.getenv('CALLIGRA_SRC')
+    if fname:
+        fname = fname + '/libs/main/calligraversion.h'
+        os.path.normpath(fname)
+
+if fname == None:
+    print(':: Could not find Calligra sources')
+    print(':: Please set CALLIGRA_SRC to point to the root of the sources,')
+    print(':: or set WORKSPACE, where WORKSPACE/source/ is the root of the sources.')
+    print(':: ')
+    print(':: These lines are commented, to cause the installer-build to fail')
+    print(':: set C2WINSTALL_VERSIONSTRING=(Unknown Version)')
+    print(':: set C2WINSTALL_HEADERVERSIONSEP=0.0.0')
+    print(':: set C2WINSTALL_HEADERVERSION=000')
+    sys.exit()
+    
 print(':: reading version from: ' + fname )
 f = open(fname,'r')
 
@@ -16,13 +34,21 @@ success = 0
 
 for line in f:
         if versionString == []:
-                versionString = re.findall(r'\bCALLIGRA_VERSION_STRING "([A-Za-z0-9. ]*)"', line) 
+                versionString = re.findall(r'\bCALLIGRA_VERSION_STRING  *"([^"]*)"', line) 
+                if versionString != []:
+                    print(':: C2WINSTALL_VERSIONSTRING=' + versionString[0])
         if majorVer == []:
-                majorVer = re.findall(r'\bCALLIGRA_VERSION_MAJOR ([0-9]*)', line)       
+                majorVer = re.findall(r'\bCALLIGRA_VERSION_MAJOR ([0-9]*)', line)     
+                if majorVer != []:
+                    print(':: MAJORVER=' + majorVer[0])                
         if minorVer == []:
-                minorVer = re.findall(r'\bCALLIGRA_STABLE_VERSION_MINOR ([0-9]*)', line)
+                minorVer = re.findall(r'\bCALLIGRA_STABLE_VERSION_MINOR ([0-9]*)', line)     
+                if minorVer != []:
+                    print(':: MINORVER=' + minorVer[0])    
         if releaseVer == []:
-                releaseVer = re.findall(r'\bCALLIGRA_VERSION_RELEASE ([0-9]*)', line)
+                releaseVer = re.findall(r'\bCALLIGRA_VERSION_RELEASE ([0-9]*)', line)     
+                if releaseVer != []:
+                    print(':: releaseVer=' + releaseVer[0])    
         if versionString != [] and majorVer != [] and minorVer != [] and releaseVer != []:
                 success = 1
                 break
@@ -33,3 +59,7 @@ if success:
         print('set C2WINSTALL_HEADERVERSION=' + majorVer[0] + minorVer[0] + releaseVer[0])
 else:
         print(':: Error collecting version info')
+        print(':: These lines are commented, to cause the installer-build to fail')
+        print(':: set C2WINSTALL_VERSIONSTRING=(Unknown Version)')
+        print(':: set C2WINSTALL_HEADERVERSIONSEP=0.0.0')
+        print(':: set C2WINSTALL_HEADERVERSION=000')

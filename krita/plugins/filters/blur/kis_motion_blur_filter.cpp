1e5ab6bff0bb9c5a167bb60c5ae996e4be582c55
Friedrich W. H. Kossebau <kossebau@kde.org>
2013-02-03 17:30:21 +0100

    fix possibly broken rect size calculation in Krita blur filters (minus on unsigned int gives complement)
    REVIEW: 108708
    
    thanks boud for review

--- krita/plugins/filters/blur/kis_motion_blur_filter.cpp
+++ krita/plugins/filters/blur/kis_motion_blur_filter.cpp
@@ -141,8 +141,8 @@ QRect KisMotionBlurFilter::neededRect(const QRect & rect, const KisFilterConfigu
     uint blurLength = (_config->getProperty("blurLength", value)) ? value.toUInt() : 5;
 
     qreal angleRadians = blurAngle / 360.0 * 2 * M_PI;
-    uint halfWidth = ceil(fabs(blurLength / 2.0 * cos(angleRadians)));
-    uint halfHeight = ceil(fabs(blurLength / 2.0 * cos(angleRadians)));
+    const int halfWidth = ceil(fabs(blurLength / 2.0 * cos(angleRadians)));
+    const int halfHeight = ceil(fabs(blurLength / 2.0 * cos(angleRadians)));
 
     return rect.adjusted(-halfWidth * 2, -halfHeight * 2, halfWidth * 2, halfHeight * 2);
 }
@@ -154,8 +154,8 @@ QRect KisMotionBlurFilter::changedRect(const QRect & rect, const KisFilterConfig
     uint blurLength = (_config->getProperty("blurLength", value)) ? value.toUInt() : 5;
 
     qreal angleRadians = blurAngle / 360.0 * 2 * M_PI;
-    uint halfWidth = ceil(fabs(blurLength * cos(angleRadians)));
-    uint halfHeight = ceil(fabs(blurLength * cos(angleRadians)));
+    const int halfWidth = ceil(fabs(blurLength * cos(angleRadians)));
+    const int halfHeight = ceil(fabs(blurLength * cos(angleRadians)));
 
     return rect.adjusted(-halfWidth * 2, -halfHeight * 2, halfWidth * 2, halfHeight * 2);
 }

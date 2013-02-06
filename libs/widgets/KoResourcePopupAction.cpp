301d33e7824d34cc5984c894974c48d3f42d42b3
Gopalakrishna Bhat A <gopalakbhat@gmail.com>
2013-02-06 21:17:35 +0530

    Fix crash on creating a new document in words.

--- libs/widgets/KoResourcePopupAction.cpp
+++ libs/widgets/KoResourcePopupAction.cpp
@@ -69,7 +69,11 @@ KoResourcePopupAction::KoResourcePopupAction(KoAbstractResourceServerAdapter *re
         resourceModel->setColumnCount(1);
     }
 
-    KoResource *resource = resourceAdapter->resources().at(0);
+    KoResource *resource = 0;
+    if (resourceAdapter->resources().count() > 0) {
+        resource = resourceAdapter->resources().at(0);
+    }
+
     KoAbstractGradient *gradient = dynamic_cast<KoAbstractGradient*>(resource);
     KoPattern *pattern = dynamic_cast<KoPattern*>(resource);
     if (gradient) {

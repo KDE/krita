KDE/calligra

Fix crash in Krita on the new styles combo
In Krita, there is no style loaded from a file. Instead, Krita is using
the default paragraph style. The new styles combo used to hide this
style.
This patch creates a default set of styles for these type of
applications. Also the default style is not hidden in the combo.

plugins/textshape/dialogs/DockerStylesComboModel.cpp
@@ -237,13 +237,16 @@ void DockerStylesComboModel::setCurrentParagraphStyle(int styleId)
 
 void DockerStylesComboModel::styleApplied(const KoCharacterStyle *style)
 {
+    if (!style) {
+        return;
+    }
     if (!m_usedStylesId.contains(style->styleId())) {
         m_usedStylesId.append(style->styleId());
         if (m_sourceModel->stylesType() == AbstractStylesModel::CharacterStyle) {
             QVector<int>::iterator begin = m_usedStyles.begin();
             for ( ; begin != m_usedStyles.end(); ++begin) {
                 KoCharacterStyle *s = m_styleManager->characterStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                if (KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
+                if (!s || KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
                     break;
                 }
             }
@@ -253,7 +256,7 @@ void DockerStylesComboModel::styleApplied(const KoCharacterStyle *style)
             QVector<int>::iterator begin = m_usedStyles.begin();
             for ( ; begin != m_usedStyles.end(); ++begin) {
                 KoParagraphStyle *s = m_styleManager->paragraphStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                if (KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
+                if (!s || KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
                     break;
                 }
             }
@@ -287,7 +290,7 @@ void DockerStylesComboModel::createMapping()
                         QVector<int>::iterator begin = m_unusedStyles.begin();
                         for ( ; begin != m_unusedStyles.end(); ++begin) {
                             KoParagraphStyle *style = m_styleManager->paragraphStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                            if (KStringHandler::naturalCompare(paragStyle->name(), style->name()) < 0) {
+                            if (! style || KStringHandler::naturalCompare(paragStyle->name(), style->name()) < 0) {
                                 break;
                             }
                         }
@@ -305,7 +308,7 @@ void DockerStylesComboModel::createMapping()
                         QVector<int>::iterator begin = m_unusedStyles.begin();
                         for ( ; begin != m_unusedStyles.end(); ++begin) {
                             KoCharacterStyle *style = m_styleManager->characterStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                            if (KStringHandler::naturalCompare(charStyle->name(), style->name()) < 0) {
+                            if (! style || KStringHandler::naturalCompare(charStyle->name(), style->name()) < 0) {
                                 break;
                             }
                         }

KDE/krita

REVIEW: 108532
BUG: 313414
Cherry-picked from textshape-stylesWidget-PierreSt
Conflicts:
	plugins/textshape/dialogs/DockerStylesComboModel.cpp

plugins/textshape/dialogs/DockerStylesComboModel.cpp
@@ -119,7 +119,7 @@ void DockerStylesComboModel::setStyleManager(KoStyleManager *sm)
                 for ( ; begin != m_usedStyles.end(); ++begin) {
                     if (m_sourceModel->index(*begin, 0, QModelIndex()).internalId() != -1) { //styleNone (internalId=-1) is a virtual style provided only for the UI. it does not exist in KoStyleManager
                         KoCharacterStyle *s = m_styleManager->characterStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                        if (KStringHandler::naturalCompare(compareStyle->name(), s->name()) < 0) {
+                        if (!s || KStringHandler::naturalCompare(compareStyle->name(), s->name()) < 0) {
                             break;
                         }
                     }
@@ -138,7 +138,7 @@ void DockerStylesComboModel::setStyleManager(KoStyleManager *sm)
                 for ( ; begin != m_usedStyles.end(); ++begin) {
                     if (m_sourceModel->index(*begin, 0, QModelIndex()).internalId() != -1) { //styleNone (internalId=-1) is a virtual style provided only for the UI. it does not exist in KoStyleManager
                         KoParagraphStyle *s = m_styleManager->paragraphStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                        if (KStringHandler::naturalCompare(compareStyle->name(), s->name()) < 0) {
+                        if (!s || KStringHandler::naturalCompare(compareStyle->name(), s->name()) < 0) {
                             break;
                         }
                     }
@@ -153,6 +153,9 @@ void DockerStylesComboModel::setStyleManager(KoStyleManager *sm)
 
 void DockerStylesComboModel::styleApplied(const KoCharacterStyle *style)
 {
+    if (!style) {
+        return;
+    }
     if (!m_usedStylesId.contains(style->styleId())) {
         m_usedStylesId.append(style->styleId());
         if (m_sourceModel->stylesType() == AbstractStylesModel::CharacterStyle) {
@@ -160,7 +163,7 @@ void DockerStylesComboModel::styleApplied(const KoCharacterStyle *style)
             for ( ; begin != m_usedStyles.end(); ++begin) {
                 if (m_sourceModel->index(*begin, 0, QModelIndex()).internalId() != -1) { //styleNone (internalId=-1) is a virtual style provided only for the UI. it does not exist in KoStyleManager
                     KoCharacterStyle *s = m_styleManager->characterStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                    if (KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
+                    if (!s || KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
                         break;
                     }
                 }
@@ -171,7 +174,7 @@ void DockerStylesComboModel::styleApplied(const KoCharacterStyle *style)
             QVector<int>::iterator begin = m_usedStyles.begin();
             for ( ; begin != m_usedStyles.end(); ++begin) {
                 KoParagraphStyle *s = m_styleManager->paragraphStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                if (KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
+                if (!s || KStringHandler::naturalCompare(style->name(), s->name()) < 0) {
                     break;
                 }
             }
@@ -215,7 +218,7 @@ void DockerStylesComboModel::createMapping()
                         QVector<int>::iterator begin = m_unusedStyles.begin();
                         for ( ; begin != m_unusedStyles.end(); ++begin) {
                             KoParagraphStyle *style = m_styleManager->paragraphStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                            if (KStringHandler::naturalCompare(paragStyle->name(), style->name()) < 0) {
+                            if (!style || KStringHandler::naturalCompare(paragStyle->name(), style->name()) < 0) {
                                 break;
                             }
                         }
@@ -233,7 +236,7 @@ void DockerStylesComboModel::createMapping()
                         QVector<int>::iterator begin = m_unusedStyles.begin();
                         for ( ; begin != m_unusedStyles.end(); ++begin) {
                             KoCharacterStyle *style = m_styleManager->characterStyle(m_sourceModel->index(*begin, 0, QModelIndex()).internalId());
-                            if (KStringHandler::naturalCompare(charStyle->name(), style->name()) < 0) {
+                            if (!style || KStringHandler::naturalCompare(charStyle->name(), style->name()) < 0) {
                                 break;
                             }
                         }
cb7c796203132c5bfd177b109a8c4e29ed8de54c
Friedrich W. H. Kossebau <kossebau@kde.org>
2013-02-15 22:41:11 +0100

    Bump min. kdelibs version to 4.6.0
    Additionally
    * remove no longer needed KDE_IS_VERSION branches
    * port remaining calls of deprecated KCalendarSystem methods
    
    REVIEW: 108870
    CCMAIL: calligra-devel@kde.org

--- libs/main/KoMainWindow.cpp
+++ libs/main/KoMainWindow.cpp
@@ -45,9 +45,7 @@
 #include <KoConfig.h>
 
 #include <kdeversion.h>
-#if KDE_IS_VERSION(4,6,0)
 #include <krecentdirs.h>
-#endif
 #include <krecentfilesaction.h>
 #include <kaboutdata.h>
 #include <ktoggleaction.h>
@@ -582,9 +580,7 @@ void KoMainWindow::addRecentURL(const KUrl& url)
                     ok = false; // it's in the tmp resource
             if (ok) {
                 KRecentDocument::add(path);
-#if KDE_IS_VERSION(4,6,0)
                 KRecentDirs::add(":OpenDialog", QFileInfo(path).dir().canonicalPath());
-#endif
             }
         } else {
             KRecentDocument::add(url.url(KUrl::RemoveTrailingSlash), true);

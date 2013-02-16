b94288afc3b22a74bb08a1afd9e0796ea653ac65
C. Boemann <cbo@boemann.dk>
2013-02-16 12:34:18 +0100

    fix crash in krita that I see from time to time

--- krita/ui/input/kis_shortcut_matcher.cpp
+++ krita/ui/input/kis_shortcut_matcher.cpp
@@ -299,8 +299,10 @@ bool KisShortcutMatcher::tryEndRunningShortcut(Qt::MouseButton button, QMouseEve
     Q_ASSERT(!m_d->readyShortcut);
 
     if (m_d->runningShortcut->matchBegin(button)) {
-        m_d->runningShortcut->action()->end(event);
-        m_d->runningShortcut->action()->deactivate();
+        if (m_d->runningShortcut->action()) {
+            m_d->runningShortcut->action()->end(event);
+            m_d->runningShortcut->action()->deactivate();
+        }
         m_d->runningShortcut = 0;
     }
 

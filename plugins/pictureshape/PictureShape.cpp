ecd83b7df0e537d0a35fd21f3391036ac5c03ad8
Thorsten Zachmann <t.zachmann@zagge.de>
2013-03-08 18:12:41 +0100

    Fix cstester roundtrip test with fo:clip
    When a fo:clip is set and cstester is used the ClippingRect is not correctly
    initialized when waitUntilReady is called which results in bad randering of
    the image and then on the second try all the images are different which makes
    roundtrip not work at all.

--- plugins/pictureshape/PictureShape.cpp
+++ plugins/pictureshape/PictureShape.cpp
@@ -424,6 +424,8 @@ bool PictureShape::loadOdf(const KoXmlElement &element, KoShapeLoadingContext &c
                  size().height() / imageData->imageSize().height());
 
         loadOdfClipContour(element, context, scaleFactor);
+        // this is needed so that the image is already normalized when calling waitUntilReady e.g. by cstester
+        m_clippingRect.normalize(imageData->imageSize());
 
         return true;
     }

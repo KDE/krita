e084f1e4460fb976f303b7d04f45cf23cd86f868
Boudewijn Rempt <boud@valdyas.org>
2013-03-30 12:39:09 +0100

    Toggle visibility of the popup by clicking on the popup button
    For some reason, this worked automatically on Linux, maybe because
    click the button caused a focus change, not a click + release signal.
    
    BUG:314861

--- krita/ui/widgets/kis_popup_button.cc
+++ krita/ui/widgets/kis_popup_button.cc
@@ -32,10 +32,16 @@
 
 
 struct KisPopupButton::Private {
-    Private() : frame(0), frameLayout(0), popupWidget(0) {}
+    Private()
+        : frame(0)
+        , frameLayout(0)
+        , popupWidget(0)
+        , popupVisible(false)
+    {}
     QFrame* frame;
     QHBoxLayout* frameLayout;
     QWidget* popupWidget;
+    bool popupVisible;
 };
 
 KisPopupButton::KisPopupButton(QWidget* parent)
@@ -94,11 +100,14 @@ void KisPopupButton::setPopupWidgetWidth(int w)
 
 void KisPopupButton::showPopupWidget()
 {
-    if (m_d->popupWidget) {
+    if (m_d->popupWidget && !m_d->popupVisible) {
         m_d->frame->raise();
         m_d->frame->show();
-
+        m_d->popupVisible = true;
         adjustPosition();
+    }
+    else {
+        hidePopupWidget();
     }
 }
 
@@ -106,6 +115,7 @@ void KisPopupButton::hidePopupWidget()
 {
     if (m_d->popupWidget) {
         m_d->frame->setVisible(false);
+        m_d->popupVisible = false;
     }
 }
 

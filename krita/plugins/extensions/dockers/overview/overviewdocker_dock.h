53562740e42f88890621c0b4306a2b796b260f13
Boudewijn Rempt <boud@valdyas.org>
2013-05-11 16:08:30 +0200

    Create a simple overview docker
    FEATURE: a docker that shows the whole image as a thumbail (unless
    the docker gets too small, then you only see part)

---
+++ krita/plugins/extensions/dockers/overview/overviewdocker_dock.h
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2009 Cyrille Berger <cberger@cberger.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _DIGITALMIXER_DOCK_H_
+#define _DIGITALMIXER_DOCK_H_
+
+#include <QDockWidget>
+#include <QTimer>
+
+#include <KoCanvasObserverBase.h>
+
+class KisCanvas2;
+class QLabel;
+
+class OverviewDockerDock : public QDockWidget, public KoCanvasObserverBase {
+    Q_OBJECT
+public:
+    OverviewDockerDock( );
+    virtual void setCanvas(KoCanvasBase *canvas);
+    virtual void unsetCanvas() { m_canvas = 0; }
+public slots:
+    void kickTimer();
+    void startUpdateCanvasProjection();
+protected:
+    void resizeEvent(QResizeEvent *);
+private:
+    QLabel *m_preview;
+    KisCanvas2* m_canvas;
+    QTimer m_delayTimer;
+};
+
+
+#endif

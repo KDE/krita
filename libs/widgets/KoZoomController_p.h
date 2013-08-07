2911658346c43a2edbde922de85f5b159d0658a0
Boudewijn Rempt <boud@valdyas.org>
2013-08-07 12:29:33 +0200

    Don't crash in calligraconverter
    First crash: when converting to pdf, KoZoomAction::createWidget
    hasn't been called yet, which means that the aspect buttons hasn't been
    set yet.

--- libs/widgets/KoZoomController_p.h
+++ libs/widgets/KoZoomController_p.h
@@ -37,7 +37,7 @@ class KoZoomController::Private
 {
 public:
     Private(KoZoomController *p, KoZoomAction::SpecialButtons specialButtons)
-        : canvasController(0), zoomHandler(0), textMinX(1), textMaxX(600), fitMargin(0), parent(p)
+        : canvasController(0), zoomHandler(0), action(0), textMinX(1), textMaxX(600), fitMargin(0), parent(p)
     {
         action = new KoZoomAction(KoZoomMode::ZOOM_WIDTH | KoZoomMode::ZOOM_PAGE, i18n("Zoom"), p);
         action->setSpecialButtons(specialButtons);

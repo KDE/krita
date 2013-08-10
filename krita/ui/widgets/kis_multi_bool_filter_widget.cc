7f0547ee8a18f43b3a44dd20e66b2d4042df3348
Sahil Nagpal <nagpal.sahil01@gmail.com>
2013-08-11 02:12:39 +0530

    using bool instead of double REVIEW:112003

--- krita/ui/widgets/kis_multi_bool_filter_widget.cc
+++ krita/ui/widgets/kis_multi_bool_filter_widget.cc
@@ -63,7 +63,7 @@ void KisMultiBoolFilterWidget::setConfiguration(const KisPropertiesConfiguration
 {
     if (!config) return;
     for (int i = 0; i < nbValues(); ++i) {
-        double val = config->getBool(m_boolWidgets[i]->objectName(), true);
+        bool val = config->getBool(m_boolWidgets[i]->objectName(), true);
         m_boolWidgets[i]->setChecked(val);
     }
 }

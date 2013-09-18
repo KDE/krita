2eac1b24708c920b387b3ee2b430e3e34001c519
Dmitry Kazakov <dimula73@gmail.com>
2013-09-18 15:51:15 +0400

    Implemented the Select Layer by Mouse Click feature
    Now you can select a layer by pressing 'R' key and clicking
    with mouse cursor. Krita will select the topmost editable layer.
    
    BUG:324590

---
+++ krita/ui/tool/kis_tool_utils.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include <kis_tool_utils.h>
+
+#include <KoColorSpace.h>
+#include <kis_paint_device.h>
+#include <kis_layer.h>
+
+namespace KisToolUtils {
+    KoColor pick(KisPaintDeviceSP dev, const QPoint& pos)
+    {
+        KoColor pickedColor;
+        dev->pixel(pos.x(), pos.y(), &pickedColor);
+        return pickedColor;
+    }
+
+    KisNodeSP findNode(KisNodeSP node, const QPoint &point, bool wholeGroup, bool editableOnly)
+    {
+        KisNodeSP foundNode = 0;
+        while (node) {
+            KisLayerSP layer = dynamic_cast<KisLayer*>(node.data());
+
+            if (!layer || !layer->isEditable()) {
+                node = node->prevSibling();
+                continue;
+            }
+
+            KoColor color(layer->projection()->colorSpace());
+            layer->projection()->pixel(point.x(), point.y(), &color);
+
+            if(color.opacityU8() != OPACITY_TRANSPARENT_U8) {
+                if (layer->inherits("KisGroupLayer") && (!editableOnly || layer->isEditable())) {
+                    // if this is a group and the pixel is transparent,
+                    // don't even enter it
+
+                    foundNode = findNode(node->lastChild(), point, wholeGroup, editableOnly);
+                }
+                else {
+                    foundNode = !wholeGroup ? node : node->parent();
+                }
+
+            }
+
+            if (foundNode) break;
+
+            node = node->prevSibling();
+        }
+
+        return foundNode;
+    }
+
+}

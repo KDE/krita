77fc6f848893bb6454b4f1f027d5aaa53021d498
Dan Leinir Turthra Jensen <admin@leinir.dk>
2013-10-01 13:38:51 +0100

    Set the current preset on the paintop box' preset chooser popup as well

--- krita/ui/kis_paintop_box.cc
+++ krita/ui/kis_paintop_box.cc
@@ -538,6 +538,7 @@ void KisPaintopBox::slotCanvasResourceChanged(int /*key*/, const QVariant& /*v*/
             QString compositeOp = preset->settings()->getString("CompositeOp");
             updateCompositeOp(compositeOp);
             resourceSelected(preset.data());
+            m_presetsChooserPopup->canvasResourceChanged(preset.data());
         }
         if(m_resourceProvider->currentCompositeOp() != m_currCompositeOpID)
             updateCompositeOp(m_resourceProvider->currentCompositeOp());

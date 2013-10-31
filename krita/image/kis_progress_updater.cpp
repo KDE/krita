0d7603989259b85d7a29e3aeac3c41881e856c49
Boudewijn Rempt <boud@valdyas.org>
2013-10-31 15:45:03 +0100

    Remove unused KisProgressInterface::createUpdater method

--- krita/image/kis_progress_updater.cpp
+++ krita/image/kis_progress_updater.cpp
@@ -20,9 +20,11 @@
 #include "kis_progress_updater.h"
 
 KisProgressUpdater::KisProgressUpdater(KisProgressInterface* progressInterface, KoProgressProxy* proxy, KoProgressUpdater::Mode mode)
-        : KoProgressUpdater(proxy, mode)
-        , m_interface(progressInterface)
+    : KoProgressUpdater(proxy, mode)
+    , m_interface(progressInterface)
 {
+    Q_ASSERT(progressInterface);
+    Q_ASSERT(proxy);
     m_interface->attachUpdater(this);
 }
 

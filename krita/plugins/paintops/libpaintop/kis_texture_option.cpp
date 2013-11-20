0a5621fc66a525c5baee72a44b322f4a9c9cc6cc
Boudewijn Rempt <boud@valdyas.org>
2013-11-20 15:35:01 +0100

    Do not shadow member variable

--- krita/plugins/paintops/libpaintop/kis_texture_option.cpp
+++ krita/plugins/paintops/libpaintop/kis_texture_option.cpp
@@ -196,9 +196,9 @@ void KisTextureOption::readOptionSetting(const KisPropertiesConfiguration* setti
 
 void KisTextureOption::resetGUI(KoResource* res)
 {
-    KisPattern *pattern = static_cast<KisPattern *>(res);
-    m_optionWidget->offsetSliderX->setRange(0, pattern->image().width() / 2);
-    m_optionWidget->offsetSliderY->setRange(0, pattern->image().height() / 2);
+    KisPattern *pat = static_cast<KisPattern *>(res);
+    m_optionWidget->offsetSliderX->setRange(0, pat->image().width() / 2);
+    m_optionWidget->offsetSliderY->setRange(0, pat->image().height() / 2);
 }
 
 void KisTextureProperties::recalculateMask()

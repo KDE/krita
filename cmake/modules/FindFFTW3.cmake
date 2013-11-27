484238e58f19448eb8a786d97540537618437974
Yue Liu <yue.liu@mail.com>
2013-11-27 00:35:20 -0500

    fix fftw3 cmake files for osx

--- cmake/modules/FindFFTW3.cmake
+++ cmake/modules/FindFFTW3.cmake
@@ -1,37 +1,35 @@
  # - Try to find the Fftw3 Library
 # Once done this will define
 #
-#  FFTW3_FOUND - system has Fftw3
-#  FFTW3_INCLUDE_DIR - the Fftw3 include directory
-#  FFTW3_LIBRARIES 
+#  FFTW3_FOUND - system has fftw3
+#  FFTW3_INCLUDE_DIRS - the fftw3 include directories
+#  FFTW3_LIBRARIES - the libraries needed to use fftw3
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
-if (NOT WIN32)
-INCLUDE(FindPkgConfig)
+if(NOT WIN32)
+include(FindPkgConfig)
+pkg_check_modules(FFTW3 QUIET fftw3>=3.2)
+endif()
 
-pkg_check_modules(FFTW3 fftw3>=3.2)
+find_path(FFTW3_INCLUDE_DIR
+    NAMES fftw3.h
+    HINTS ${PC_FFTW3_INCLUDEDIR} ${PC_FFTW3_INCLUDE_DIRS}
+    PATH_SUFFIXES fftw3)
 
-if (FFTW3_FOUND)
+find_library(FFTW3_LIBRARY
+    NAMES fftw3
+    HINTS ${PC_FFTW3_LIBDIR} ${PC_FFTW3_LIBRARY_DIRS})
+
+set(FFTW3_LIBRARIES ${FFTW3_LIBRARY})
+set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(fftw3 DEFAULT_MSG FFTW3_LIBRARY FFTW3_INCLUDE_DIR)
+
+mark_as_advanced(FFTW3_INCLUDE_DIR FFTW3_LIBRARY)
+
+if(FFTW3_FOUND)
     message(STATUS "FFTW Found Version: " ${FFTW_VERSION})
 endif()
-	
-else (NOT WIN32)
-
-if ( FFTW3_INCLUDE_DIR AND FFTW3_LIBRARIES )
-   # in cache already
-   SET( FFTW3_FIND_QUIETLY TRUE )
-endif ( FFTW3_INCLUDE_DIR AND FFTW3_LIBRARIES )
-
-FIND_PATH( FFTW3_INCLUDE_DIR NAMES fftw3.h PATH_SUFFIXES fftw3
-)
-
-FIND_LIBRARY( FFTW3_LIBRARIES NAMES fftw3f-3 )
-
-include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( fftw3 DEFAULT_MSG FFTW3_INCLUDE_DIR FFTW3_LIBRARIES )
-
-endif (NOT WIN32)
-
-

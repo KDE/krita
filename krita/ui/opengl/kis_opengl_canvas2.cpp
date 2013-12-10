94d9cde4e318ccb2ef4763c20d7104e5f87052b2
Boudewijn Rempt <boud@valdyas.org>
2013-12-10 14:12:56 +0100

    And disable opengl if we couldn't build the shaders.

--- krita/ui/opengl/kis_opengl_canvas2.cpp
+++ krita/ui/opengl/kis_opengl_canvas2.cpp
@@ -485,7 +485,10 @@ void KisOpenGLCanvas2::initializeCheckerShader()
     d->checkerShader->bindAttributeLocation("a_textureCoordinate", PROGRAM_TEXCOORD_ATTRIBUTE);
 
     if (!d->checkerShader->link()) {
-        QMessageBox::critical(this, "Fatal Error", QString(i18n("Krita could not initialize the OpenGL canvas:\n %1.\n Krita will close now.")).arg(d->checkerShader->log()), QMessageBox::Close);
+        QMessageBox::critical(this, "Fatal Error", QString(i18n("Krita could not initialize the OpenGL canvas:\n %1.\n Krita will disable OpenGL and close now.")).arg(d->checkerShader->log()), QMessageBox::Close);
+        KisConfig cfg;
+        cfg.setUseOpenGL(false);
+        cfg.setCanvasState("OPENGL_FAILED");
         exit(1);
     }
     Q_ASSERT(d->checkerShader->isLinked());
@@ -515,7 +518,10 @@ void KisOpenGLCanvas2::initializeDisplayShader()
     d->displayShader->bindAttributeLocation("a_textureCoordinate", PROGRAM_TEXCOORD_ATTRIBUTE);
 
     if (!d->displayShader->link()) {
-        QMessageBox::critical(this, "Fatal Error", QString(i18n("Krita could not initialize the OpenGL canvas:\n %1.\n Krita will close now.")).arg(d->displayShader->log()), QMessageBox::Close);
+        QMessageBox::critical(this, "Fatal Error", QString(i18n("Krita could not initialize the OpenGL canvas:\n %1.\n Krita will disable OpenGL and close now.")).arg(d->displayShader->log()), QMessageBox::Close);
+        KisConfig cfg;
+        cfg.setUseOpenGL(false);
+        cfg.setCanvasState("OPENGL_FAILED");
         exit(1);
     }
 

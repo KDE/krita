5362d846f0cb819d2a561e7249ce03e2e9ea9cbd
Boudewijn Rempt <boud@valdyas.org>
2013-12-11 12:26:25 +0100

    Don't popup below the desktop
    Unless we're the preset editor...
    
    BUG:327977

--- krita/ui/widgets/kis_popup_button.cc
+++ krita/ui/widgets/kis_popup_button.cc
@@ -148,8 +148,16 @@ void KisPopupButton::adjustPosition()
         popupRect.translate(screenRect.right() - popupRect.right(), 0);
     if (popupRect.left() < screenRect.left())
         popupRect.translate(screenRect.left() - popupRect.left(), 0);
-    //if (popupRect.bottom() > screenRect.bottom())
-    //    popupRect.translate(0, -m_d->frame->height());
+
+    // The preset popup never should be shown over the toolbar, but other popups need
+    // to be able to be moved up. BUG: https://bugs.kde.org/show_bug.cgi?id=327977,
+    // see also commit 275758d4d0aaf398941295a4bf7ae9755bed2cb0
+    KisPaintOpPresetsPopup* presetPopup = dynamic_cast<KisPaintOpPresetsPopup*>(m_d->popupWidget);
+    if(!presetPopup) {
+        if (popupRect.bottom() > screenRect.bottom())
+            popupRect.translate(0, -m_d->frame->height());
+    }
+
     m_d->frame->setGeometry(popupRect);
 }
 

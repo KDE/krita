91eccae4a35e116b24a3cced2b7a2f4956d01bcb
Rex Dieter <rdieter@math.unl.edu>
2014-01-15 15:48:18 -0600

    more qreal=double assumptions fixed (yay arm)

--- krita/plugins/paintops/libbrush/kis_auto_brush.cpp
+++ krita/plugins/paintops/libbrush/kis_auto_brush.cpp
@@ -62,8 +62,8 @@ KisAutoBrush::KisAutoBrush(KisMaskGenerator* as, qreal angle, qreal randomness, 
     d->density = density;
     d->idealThreadCountCached = QThread::idealThreadCount();
     setBrushType(MASK);
-    setWidth(qMax(1.0, d->shape->width()));
-    setHeight(qMax(1.0, d->shape->height()));
+    setWidth(qMax(qreal(1.0), d->shape->width()));
+    setHeight(qMax(qreal(1.0), d->shape->height()));
     setAngle(angle);
     QImage image = createBrushPreview();
     setImage(image);

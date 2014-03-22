7a0358706b4734fd1a999110637649c7a4c90ed2
Jaroslaw Staniek <staniek@kde.org>
2014-03-23 00:11:51 +0100

    Do not remove table prefix when it is needed
    BUG:332329

--- libs/db/expression.cpp
+++ libs/db/expression.cpp
@@ -752,8 +752,8 @@ bool VariableExpr::validate(ParseInfo& parseInfo)
     }
 
     //table.fieldname or tableAlias.fieldname
-    tableName = tableName;
     TableSchema *ts = parseInfo.querySchema->table(tableName);
+    int tablePosition = -1;
     if (ts) {//table.fieldname
         //check if "table" is covered by an alias
         const QList<int> tPositions = parseInfo.querySchema->tablePositions(tableName);
@@ -773,10 +773,11 @@ bool VariableExpr::validate(ParseInfo& parseInfo)
                                       "you can write \"%3\"", tableName, tableName + "." + fieldName, tableAlias + "." + QString(fieldName));
             return false;
         }
+        if (!tPositions.isEmpty()) {
+            tablePosition = tPositions.first();
+        }
     }
-
-    int tablePosition = -1;
-    if (!ts) {//try to find tableAlias
+    else {//try to find tableAlias
         tablePosition = parseInfo.querySchema->tablePositionForAlias(tableName.toLatin1());
         if (tablePosition >= 0) {
             ts = parseInfo.querySchema->tables()->at(tablePosition);

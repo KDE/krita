e80099a26c727820c45f8514ac48b91efa11be00
Boudewijn Rempt <boud@valdyas.org>
2014-03-27 10:02:41 +0100

    Swap the fg->bg and fg->transparent gradients
    Patch by Ghevan. Thanks!
    CCMAIL:ghevan@gmail.com
    BUG:329008

--- krita/ui/kis_canvas_resource_provider.cpp
+++ krita/ui/kis_canvas_resource_provider.cpp
@@ -309,7 +309,7 @@ void KisCanvasResourceProvider::slotCanvasResourceChanged(int key, const QVarian
         KoStopGradient* stopGradient = dynamic_cast<KoStopGradient*>(resource);
         if(stopGradient) {
             QList<KoGradientStop> stops;
-            stops << KoGradientStop(0.0, fgColor()) << KoGradientStop(1.0, bgColor());
+            stops << KoGradientStop(0.0, fgColor()) << KoGradientStop(1.0,  KoColor(QColor(0, 0, 0, 0), fgColor().colorSpace()));
             stopGradient->setStops(stops);
             KoResourceServerProvider::instance()->gradientServer()->updateResource(resource);
         }
@@ -317,7 +317,7 @@ void KisCanvasResourceProvider::slotCanvasResourceChanged(int key, const QVarian
         stopGradient = dynamic_cast<KoStopGradient*>(resource);
         if(stopGradient) {
             QList<KoGradientStop> stops;
-            stops << KoGradientStop(0.0, fgColor()) << KoGradientStop(1.0,  KoColor(QColor(0, 0, 0, 0), fgColor().colorSpace()));
+            stops << KoGradientStop(0.0, fgColor()) << KoGradientStop(1.0, bgColor());
             stopGradient->setStops(stops);
             KoResourceServerProvider::instance()->gradientServer()->updateResource(resource);
         }

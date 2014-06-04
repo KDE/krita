2a5b4891c6cf52719da3e46c8491dfa436533530
Jaroslaw Staniek <staniek@kde.org>
2014-06-04 12:09:03 +0200

    Simplify algorithm of QuerySchema::addTable
    Unnecessary checks were performed since alias is empty. Also display
    debug instead of warning because the case handled is nothing unusual.

--- libs/db/queryschema.h
+++ libs/db/queryschema.h
@@ -408,8 +408,8 @@ public:
     TableSchema::List* tables() const;
 
     /*! Adds \a table schema as one of tables used in a query.
-     if \a alias is not empty, it will be assigned to this table
-     using setTableAlias(position, alias)
+     If \a alias is not empty, it will be assigned to this table
+     using setTableAlias(position, alias).
     */
     void addTable(TableSchema *table, const QByteArray& alias = QByteArray());
 

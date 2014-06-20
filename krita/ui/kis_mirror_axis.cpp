696f1ae1399ec990b1ba9eeda4f38b253abf2d70
Boudewijn Rempt <boud@valdyas.org>
2014-06-20 09:19:00 +0200

    Fix copyright

--- krita/ui/kis_mirror_axis.cpp
+++ krita/ui/kis_mirror_axis.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 <copyright holder> <email>
+ * Copyright (c) 2014 Arjen Hiemstra <ahiemstra@heimr.nl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

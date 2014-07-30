e6ff73a231c3582ddabce94812e2c5872789fbf5
Boudewijn Rempt <boud@valdyas.org>
2014-07-30 16:54:31 +0200

    Add a method to add scripts to a scripting menu.

--- krita/plugins/extensions/pykrita/libkis/krita.cpp
+++ krita/plugins/extensions/pykrita/libkis/krita.cpp
@@ -1,12 +1,31 @@
+/*
+ *  Copyright (c) 2014 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 #include "krita.h"
 
 #include <KoApplication.h>
 #include <KoPart.h>
 #include <KoMainWindow.h>
 
-#include "kis_view2.h"
-#include "kis_doc2.h"
-#include "kis_image.h"
+#include <kis_view2.h>
+#include <kis_doc2.h>
+#include <kis_image.h>
+#include <kis_action.h>
+#include <kis_script_manager.h>
 
 Krita::Krita(QObject *parent) :
     QObject(parent)
@@ -57,4 +76,22 @@ QList<Image *> Krita::images()
         ret << doc->image();
     }
     return ret;
+}
+
+QAction *Krita::createAction(const QString &text)
+{
+    KisAction *action = new KisAction(text, this);
+    foreach(KoPart *part, koApp->partList()) {
+        if (part) {
+            foreach(KoMainWindow *mainWin, part->mainWindows()) {
+                if (mainWin && mainWin->rootView()) {
+                    KisView2 *view = qobject_cast<KisView2*>(view);
+                    if (view) {
+                        view->scriptManager()->addAction(action);
+                    }
+                }
+            }
+        }
+    }
+    return action;
 }
--- krita/plugins/extensions/pykrita/libkis/krita.h
+++ krita/plugins/extensions/pykrita/libkis/krita.h
@@ -1,8 +1,26 @@
+/*
+ *  Copyright (c) 2014 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 #ifndef LIBKIS_KRITA_H
 #define LIBKIS_KRITA_H
 
 #include <QObject>
 #include <QPointer>
+#include <QAction>
 
 #include "application.h"
 #include "mainwindow.h"
@@ -22,6 +40,8 @@ public:
     QList<View*> views();
     QList<Document*> documents();
     QList<Image*> images();
+
+    QAction *createAction(const QString &text);
 
 signals:
 

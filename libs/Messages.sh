c3091deb1d4c6adc2af18ac354d29b04b0c9e89e
Jaroslaw Staniek <staniek@kde.org>
2014-08-20 16:41:42 +0200

    translations: typo in koreport path...
    Don't fix it for 2.x but mark it for fix in 3.x.
    In 2.x koreport's messages is still calligra.pot.

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 source ../calligra_xgettext.sh
 
-EXCLUDE="-path ./koproperty -o -path ./koreports"
+EXCLUDE="-path ./koproperty" # TODO: -o -path ./koreport"
 LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.ui -o -name \*.rc \) -type f | grep -v -e '/\.'`
 $EXTRACTRC $LIST >> rc.cpp
 LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f | grep -v '/tests/' | grep -v -e '/\.'`

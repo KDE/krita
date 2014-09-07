KDE/calligra

commit 32dd821a55ce36a7dedfc9f81b56d4c93f6fbbe4

Fix QObject connections normalize issues
(using Krazy2)

libs/koproperty/EditorView.cpp
@@ -353,10 +353,10 @@ void EditorView::changeSetInternal(Set *set, SetOptions options,
     }
     if (d->set && setChanged) {
         //receive property changes
-        connect(d->set, SIGNAL(propertyChangedInternal(KoProperty::Set&, KoProperty::Property&)),
-                this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
-        connect(d->set, SIGNAL(propertyReset(KoProperty::Set&, KoProperty::Property&)),
-                this, SLOT(slotPropertyReset(KoProperty::Set&, KoProperty::Property&)));
+        connect(d->set, SIGNAL(propertyChangedInternal(KoProperty::Set,KoProperty::Property)),
+                this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+        connect(d->set, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
+                this, SLOT(slotPropertyReset(KoProperty::Set,KoProperty::Property)));
         connect(d->set, SIGNAL(aboutToBeCleared()), this, SLOT(slotSetWillBeCleared()));
         connect(d->set, SIGNAL(aboutToBeDeleted()), this, SLOT(slotSetWillBeDeleted()));
     }

libs/koproperty/Set.cpp
@@ -648,21 +648,21 @@ void Set::addRelatedProperty(Property *p1, Property *p2) const
 Buffer::Buffer()
         : Set(false)
 {
-    connect(this, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(intersectedChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(this, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(intersectedChanged(KoProperty::Set,KoProperty::Property)));
 
-    connect(this, SIGNAL(propertyReset(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(intersectedReset(KoProperty::Set&, KoProperty::Property&)));
+    connect(this, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(intersectedReset(KoProperty::Set,KoProperty::Property)));
 }
 
 Buffer::Buffer(const Set& set)
         : Set(false)
 {
-    connect(this, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(intersectedChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(this, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(intersectedChanged(KoProperty::Set,KoProperty::Property)));
 
-    connect(this, SIGNAL(propertyReset(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(intersectedReset(KoProperty::Set&, KoProperty::Property&)));
+    connect(this, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(intersectedReset(KoProperty::Set,KoProperty::Property)));
 
     init(set);
 }

libs/koreport/items/check/KoReportDesignerItemCheck.cpp
@@ -39,8 +39,8 @@ void KoReportDesignerItemCheck::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
 
     setZValue(Z);
 }

libs/koreport/items/field/KoReportDesignerItemField.cpp
@@ -40,8 +40,8 @@ void KoReportDesignerItemField::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
     
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
 
     setZValue(Z);
 }

libs/koreport/items/image/KoReportDesignerItemImage.cpp
@@ -48,8 +48,8 @@ void KoReportDesignerItemImage::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
 	    
     m_controlSource->setListData(m_reportDesigner->fieldKeys(), m_reportDesigner->fieldNames());
     setZValue(Z);

libs/koreport/items/label/KoReportDesignerItemLabel.cpp
@@ -40,8 +40,8 @@ void KoReportDesignerItemLabel::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
 
     setZValue(Z);
 }

libs/koreport/items/text/KoReportDesignerItemText.cpp
@@ -42,8 +42,8 @@ void KoReportDesignerItemText::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&, KoProperty::Property&)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set&, KoProperty::Property&)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 

libs/koreport/renderer/KoReportPreRenderer.cpp
@@ -426,11 +426,11 @@ void KoReportPreRendererPrivate::initEngine()
 {
     m_scriptHandler = new KRScriptHandler(m_kodata, m_reportData);
 
-    connect(this, SIGNAL(enteredGroup(const QString&, const QVariant&)), m_scriptHandler, SLOT(slotEnteredGroup(const QString&, const QVariant&)));
+    connect(this, SIGNAL(enteredGroup(QString,QVariant)), m_scriptHandler, SLOT(slotEnteredGroup(QString,QVariant)));
 
-    connect(this, SIGNAL(exitedGroup(const QString&, const QVariant&)), m_scriptHandler, SLOT(slotExitedGroup(const QString&, const QVariant&)));
+    connect(this, SIGNAL(exitedGroup(QString,QVariant)), m_scriptHandler, SLOT(slotExitedGroup(QString,QVariant)));
 
-    connect(this, SIGNAL(renderingSection(KRSectionData*, OROPage*, QPointF)), m_scriptHandler, SLOT(slotEnteredSection(KRSectionData*, OROPage*, QPointF)));
+    connect(this, SIGNAL(renderingSection(KRSectionData*,OROPage*,QPointF)), m_scriptHandler, SLOT(slotEnteredSection(KRSectionData*,OROPage*,QPointF)));
 }
 
 void KoReportPreRendererPrivate::asyncItemsFinished()
@@ -542,7 +542,8 @@ ORODocument* KoReportPreRenderer::generate()
 
             //!TODO This is a hack
             if (i.key() == "field")
-                QObject::connect(d->m_scriptHandler, SIGNAL(groupChanged(const QString&)), i.value(), SLOT(setWhere(const QString&)));
+                QObject::connect(d->m_scriptHandler, SIGNAL(groupChanged(QString)),
+                                 i.value(), SLOT(setWhere(QString)));
         }
     }
 

KDE/krita

commit 8d632bac1a7e0ad91c2fb9e675e9efbc7d3372e9

Fixup for normalization of signals
Commit 32dd821a55ce36a7dedfc9f81b56d4c93f6fbbe4 introduced problems
with signals. This commit is partially revert of it. With keeping all
the changes that were right

REVIEW: 120083

libs/koproperty/EditorView.cpp
@@ -353,10 +353,10 @@ void EditorView::changeSetInternal(Set *set, SetOptions options,
     }
     if (d->set && setChanged) {
         //receive property changes
-        connect(d->set, SIGNAL(propertyChangedInternal(KoProperty::Set,KoProperty::Property)),
-                this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
-        connect(d->set, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
-                this, SLOT(slotPropertyReset(KoProperty::Set,KoProperty::Property)));
+        connect(d->set, SIGNAL(propertyChangedInternal(KoProperty::Set&,KoProperty::Property&)),
+                this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
+        connect(d->set, SIGNAL(propertyReset(KoProperty::Set&,KoProperty::Property&)),
+                this, SLOT(slotPropertyReset(KoProperty::Set&,KoProperty::Property&)));
         connect(d->set, SIGNAL(aboutToBeCleared()), this, SLOT(slotSetWillBeCleared()));
         connect(d->set, SIGNAL(aboutToBeDeleted()), this, SLOT(slotSetWillBeDeleted()));
     }

libs/koproperty/Set.cpp
@@ -648,21 +648,21 @@ void Set::addRelatedProperty(Property *p1, Property *p2) const
 Buffer::Buffer()
         : Set(false)
 {
-    connect(this, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(intersectedChanged(KoProperty::Set,KoProperty::Property)));
+    connect(this, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(intersectedChanged(KoProperty::Set&,KoProperty::Property&)));
 
-    connect(this, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(intersectedReset(KoProperty::Set,KoProperty::Property)));
+    connect(this, SIGNAL(propertyReset(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(intersectedReset(KoProperty::Set&,KoProperty::Property&)));
 }
 
 Buffer::Buffer(const Set& set)
         : Set(false)
 {
-    connect(this, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(intersectedChanged(KoProperty::Set,KoProperty::Property)));
+    connect(this, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(intersectedChanged(KoProperty::Set&,KoProperty::Property&)));
 
-    connect(this, SIGNAL(propertyReset(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(intersectedReset(KoProperty::Set,KoProperty::Property)));
+    connect(this, SIGNAL(propertyReset(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(intersectedReset(KoProperty::Set&,KoProperty::Property&)));
 
     init(set);
 }

libs/koreport/items/check/KoReportDesignerItemCheck.cpp
@@ -39,8 +39,8 @@ void KoReportDesignerItemCheck::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     setZValue(Z);
 }

libs/koreport/items/field/KoReportDesignerItemField.cpp
@@ -40,8 +40,8 @@ void KoReportDesignerItemField::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
     
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     setZValue(Z);
 }

libs/koreport/items/image/KoReportDesignerItemImage.cpp
@@ -48,8 +48,8 @@ void KoReportDesignerItemImage::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 	    
     m_controlSource->setListData(m_reportDesigner->fieldKeys(), m_reportDesigner->fieldNames());
     setZValue(Z);

libs/koreport/items/label/KoReportDesignerItemLabel.cpp
@@ -40,8 +40,8 @@ void KoReportDesignerItemLabel::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     setZValue(Z);
 }

libs/koreport/items/text/KoReportDesignerItemText.cpp
@@ -42,8 +42,8 @@ void KoReportDesignerItemText::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(propertySet(), SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 

libs/koreport/renderer/KoReportPreRenderer.cpp
@@ -542,8 +542,7 @@ ORODocument* KoReportPreRenderer::generate()
 
             //!TODO This is a hack
             if (i.key() == "field")
-                QObject::connect(d->m_scriptHandler, SIGNAL(groupChanged(QString)),
-                                 i.value(), SLOT(setWhere(QString)));
+                QObject::connect(d->m_scriptHandler, SIGNAL(groupChanged(QString)), i.value(), SLOT(setWhere(QString)));
         }
     }
 

libs/koreport/wrtembed/KoReportDesigner.cpp
@@ -176,8 +176,8 @@ void KoReportDesigner::init()
     connect(d->pageButton, SIGNAL(released()), this, SLOT(slotPageButton_Pressed()));
     emit pagePropertyChanged(*m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     changeSet(m_set);
 
@@ -605,8 +605,8 @@ void KoReportDesigner::createProperties()
     QStringList keys, strings;
     m_set = new KoProperty::Set(0, "Report");
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     m_title = new KoProperty::Property("Title", "Report", i18n("Title"), i18n("Report Title"));
 

libs/koreport/wrtembed/KoReportDesignerItemLine.cpp
@@ -54,8 +54,8 @@ void KoReportDesignerItemLine::init(QGraphicsScene* s, KoReportDesigner *r)
     if (s)
         s->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     setZValue(Z);
 }

libs/koreport/wrtembed/reportsection.cpp
@@ -75,8 +75,8 @@ ReportSection::ReportSection(KoReportDesigner * rptdes)
         : QWidget(rptdes)
 {
     m_sectionData = new KRSectionData(this);
-    connect(m_sectionData->propertySet(), SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_sectionData->propertySet(), SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
     int dpiY = KoDpi::dpiY();
 
     m_reportDesigner = rptdes;

plugins/reporting/barcode/KoReportDesignerItemBarcode.cpp
@@ -42,8 +42,8 @@ void KoReportDesignerItemBarcode::init(QGraphicsScene * scene)
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     setMaxLength(5);
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);

plugins/reporting/chart/KoReportDesignerItemChart.cpp
@@ -40,8 +40,8 @@ void KoReportDesignerItemChart::init(QGraphicsScene* scene, KoReportDesigner *de
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
     setZValue(Z);

plugins/reporting/maps/KoReportDesignerItemMaps.cpp
@@ -50,8 +50,8 @@ void KoReportDesignerItemMaps::init(QGraphicsScene * scene)
 
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)),
+            this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
 	    
     m_controlSource->setListData(m_reportDesigner->fieldKeys(), m_reportDesigner->fieldNames());
     setZValue(Z);

plugins/reporting/web/KoReportDesignerItemWeb.cpp
@@ -45,8 +45,7 @@ void KoReportDesignerItemWeb::init(QGraphicsScene *scene) //done,compared,add fu
     if (scene)
         scene->addItem(this);
 
-    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set,KoProperty::Property)),
-            this, SLOT(slotPropertyChanged(KoProperty::Set,KoProperty::Property)));
+    connect(m_set, SIGNAL(propertyChanged(KoProperty::Set&,KoProperty::Property&)), this, SLOT(slotPropertyChanged(KoProperty::Set&,KoProperty::Property&)));
     KoReportDesignerItemRectBase::init(&m_pos, &m_size, m_set);
     setZValue(Z);
 }
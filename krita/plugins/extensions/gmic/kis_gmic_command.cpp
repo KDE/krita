a83877b6057962e1c347a4c2a699431f44f5db51
Friedrich W. H. Kossebau <kossebau@kde.org>
2014-09-20 19:17:17 +0200

    Unbreak build
    Missed custom_commands parameter in call to gmic(...)
    in KisGmicTests::filterWithGmic)
    
    Also revert to relying on default values instead of passing them explicitly

--- krita/plugins/extensions/gmic/kis_gmic_command.cpp
+++ krita/plugins/extensions/gmic/kis_gmic_command.cpp
@@ -67,7 +67,7 @@ void KisGmicCommand::redo()
             QString gmicCmd = "-* 255 ";
             gmicCmd.append(m_gmicCommandString);
             dbgPlugins << m_gmicCommandString;
-            gmic(gmicCmd.toLocal8Bit().constData(), *m_images, images_names, m_customCommands, true, 0, 0);
+            gmic(gmicCmd.toLocal8Bit().constData(), *m_images, images_names, m_customCommands);
 
         }
         // Catch exception, if an error occured in the interpreter.

f25f603f8a301579869835eacafa30583aafac0a
Dmitry Kazakov <dimula73@gmail.com>
2014-10-17 20:12:10 +0400

    Fix activation of a layer when adding a new layer
    We have an extended selection mode now, so we should use
    extended selection manipulation methods for it.
    
    BUG:339599

--- krita/plugins/extensions/dockers/defaultdockers/kis_layer_box.cpp
+++ krita/plugins/extensions/dockers/defaultdockers/kis_layer_box.cpp
@@ -442,7 +442,7 @@ void KisLayerBox::setCurrentNode(KisNodeSP node)
 {
     QModelIndex index = node ? m_nodeModel->indexFromNode(node) : QModelIndex();
 
-    m_wdgLayerBox->listLayers->setCurrentIndex(index);
+    m_wdgLayerBox->listLayers->selectionModel()->setCurrentIndex(index, QItemSelectionModel::ClearAndSelect);
     updateUI();
 }
 
@@ -734,7 +734,10 @@ void KisLayerBox::selectionChanged(const QModelIndexList selection)
     if (!m_nodeManager) return;
 
     if (selection.isEmpty() && m_nodeManager->activeNode()) {
-        m_wdgLayerBox->listLayers->setCurrentIndex(m_nodeModel->indexFromNode(m_nodeManager->activeNode()));
+        m_wdgLayerBox->listLayers->selectionModel()->
+            setCurrentIndex(
+                m_nodeModel->indexFromNode(m_nodeManager->activeNode()),
+                QItemSelectionModel::ClearAndSelect);
         return;
     }
 

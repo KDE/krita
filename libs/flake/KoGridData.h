c0dc04e277496b27eae721318421803bcb829368
Boudewijn Rempt <boud@valdyas.org>
2014-11-21 11:48:22 +0100

    Move KoGridData from komain to flake
    That's wher it belongs, along with its buddy class KoGuidesData.

--- libs/flake/KoGridData.h
+++ libs/flake/KoGridData.h
@@ -21,7 +21,7 @@
 #ifndef KOGRIDDATA_H
 #define KOGRIDDATA_H
 
-#include "komain_export.h"
+#include "flake_export.h"
 #include <KoXmlReaderForward.h>
 #include <QColor>
 
@@ -36,7 +36,7 @@ class KToggleAction;
  * Things like grid colors, distances and snap to grid are saved here.
  * \sa KoDocument::gridData()
  */
-class KOMAIN_EXPORT KoGridData
+class FLAKE_EXPORT KoGridData
 {
 public:
     KoGridData();

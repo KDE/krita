a4b7ed892239570d0c0357b8b61e282ae0374f80
Boudewijn Rempt <boud@valdyas.org>
2014-12-07 13:47:51 +0100

    BUG:339594 Add a plugin to split a layer into its color components
    Warning: this can potentially generate thousands and thousands of
    layers which is slow and will cause out-of-memory problems.

---
+++ krita/plugins/extensions/layersplit/dlg_layersplit.cpp
@@ -0,0 +1,120 @@
+/*
+ *  dlg_layersplit.cc - part of KimageShop^WKrayon^WKrita
+ *
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2011 Srikanth Tiyyagura <srikanth.tulasiram@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "dlg_layersplit.h"
+
+#include <klocale.h>
+#include <kis_debug.h>
+
+#include <KisViewManager.h>
+#include <kis_image.h>
+#include <kis_paint_device.h>
+
+#include "kis_slider_spin_box.h"
+#include <QCheckBox>
+#include <QSpinBox>
+
+#include <kis_config.h>
+
+DlgLayerSplit::DlgLayerSplit()
+    : KDialog()
+{
+    m_page = new WdgLayerSplit(this);
+
+    setCaption(i18n("Split Layer"));
+    setButtons(Apply | Cancel);
+    setDefaultButton(Apply);
+
+    m_page->intFuzziness->setRange(0, 200);
+    m_page->intFuzziness->setSingleStep(1);
+
+
+    KisConfig cfg;
+    m_page->intFuzziness->setValue(cfg.readEntry<int>("layersplit/fuzziness", 20));
+    m_page->chkCreateGroupLayer->setChecked(cfg.readEntry<bool>("layerspit/createmastergroup", true));
+    m_page->chkSeparateGroupLayers->setChecked(cfg.readEntry<bool>("layerspit/separategrouplayers", false));
+    m_page->chkAlphaLock->setChecked(cfg.readEntry<bool>("layerspit/alphalock", true));
+    m_page->chkHideOriginal->setChecked(cfg.readEntry<bool>("layerspit/hideoriginal", false));
+    m_page->chkSortLayers->setChecked(cfg.readEntry<bool>("layerspit/sortlayers", true));
+    m_page->chkDisregardOpacity->setChecked(cfg.readEntry<bool>("layerspit/disregardopacity", true));
+
+    connect(this, SIGNAL(applyClicked()), this, SLOT(applyClicked()));
+
+    setMainWidget(m_page);
+}
+
+DlgLayerSplit::~DlgLayerSplit()
+{
+}
+
+void DlgLayerSplit::applyClicked()
+{
+    KisConfig cfg;
+    cfg.writeEntry("layersplit/fuzziness", m_page->intFuzziness->value());
+    cfg.writeEntry("layerspit/createmastergroup", m_page->chkCreateGroupLayer->isChecked());
+    cfg.writeEntry("layerspit/separategrouplayers", m_page->chkSeparateGroupLayers->isChecked());
+    cfg.writeEntry("layerspit/alphalock", m_page->chkAlphaLock->isChecked());
+    cfg.writeEntry("layerspit/hideoriginal", m_page->chkHideOriginal->isChecked());
+    cfg.writeEntry("layerspit/sortlayers", m_page->chkSortLayers->isChecked());
+    cfg.writeEntry("layerspit/disregardopacity", m_page->chkDisregardOpacity->isChecked());
+
+    accept();
+}
+
+bool DlgLayerSplit::createBaseGroup() const
+{
+    return m_page->chkCreateGroupLayer->isChecked();
+}
+
+bool DlgLayerSplit::createSeparateGroups() const
+{
+    return m_page->chkSeparateGroupLayers->isChecked();
+}
+
+bool DlgLayerSplit::lockAlpha() const
+{
+    return m_page->chkAlphaLock->isChecked();
+}
+
+bool DlgLayerSplit::hideOriginal() const
+{
+    return m_page->chkHideOriginal->isChecked();
+}
+
+bool DlgLayerSplit::sortLayers() const
+{
+    return m_page->chkSortLayers->isChecked();
+}
+
+bool DlgLayerSplit::disregardOpacity() const
+{
+    return m_page->chkDisregardOpacity->isChecked();
+}
+
+int DlgLayerSplit::fuzziness() const
+{
+    return m_page->intFuzziness->value();
+
+}
+
+
+
+#include "dlg_layersplit.moc"
---
+++ krita/plugins/extensions/layersplit/dlg_layersplit.h
@@ -0,0 +1,61 @@
+/*
+ *  dlg_layersplit.h -- part of KimageShop^WKrayon^WKrita
+ *
+ *  Copyright (c) 2009 Boudewijn Rempt <boud@valdyas.org>
+ *  Copyright (c) 2011 Srikanth Tiyyagura <srikanth.tulasiram@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#ifndef DLG_LAYERSPLIT
+#define DLG_LAYERSPLIT
+
+#include <kdialog.h>
+
+#include <kis_types.h>
+
+#include "wdg_layersplit.h"
+
+class KisViewManager;
+/**
+ * This dialog allows the user to create a selection mask based
+ * on a (range of) colors.
+ */
+class DlgLayerSplit: public KDialog
+{
+
+    Q_OBJECT
+
+public:
+
+    DlgLayerSplit();
+    ~DlgLayerSplit();
+
+    bool createBaseGroup() const;
+    bool createSeparateGroups() const;
+    bool lockAlpha() const;
+    bool hideOriginal() const;
+    bool sortLayers() const;
+    bool disregardOpacity() const;
+    int fuzziness() const;
+
+private slots:
+
+    void applyClicked();
+
+private:
+    WdgLayerSplit* m_page;
+};
+
+#endif // DLG_LAYERSPLIT

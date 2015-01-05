d094eeb0734a60dba91dfbcf63eb9dedc185ba7c
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-01-05 20:07:42 +0100

    Change license to "Library General Public License v2 or any later version"
    As agreed to by...
    Aurélien Gâteau in http://lists.kde.org/?l=calligra-devel&m=142046478303370&w=2
    Paul Mendez in http://lists.kde.org/?l=calligra-devel&m=142034623013151&w=2

--- libs/widgets/KoContextBarButton.h
+++ libs/widgets/KoContextBarButton.h
@@ -3,20 +3,20 @@
 Copyright 2011 Aurélien Gâteau <agateau@kde.org>
 Copyright 2011 Paul Mendez <paulestebanms@gmail.com>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
-
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
 */
 #ifndef KOCONTEXTBARBUTTON_H
 #define KOCONTEXTBARBUTTON_H

KDE/calligra

commit ce2e71dd87b4455fa8898a00df70973bf42b9cc1

Convert only once toLower, instead on every usage

libs/koreport/renderer/KoReportRendererBase.cpp
@@ -46,25 +46,26 @@ KoReportRendererFactory::KoReportRendererFactory()
 
 KoReportRendererBase* KoReportRendererFactory::createInstance(const QString& key)
 {
-    if (key.toLower() == QLatin1String("screen")) {
+    const QString lowerKey =lowerKey;
+    if (lowerKey == QLatin1String("screen")) {
         return new KoReportScreenRenderer();
     }
-    if (key.toLower() == QLatin1String("print")) {
+    if (lowerKey == QLatin1String("print")) {
         return new KoReportPrintRenderer();
     }  
-    if (key.toLower() == QLatin1String("ods")) {
+    if (lowerKey == QLatin1String("ods")) {
         return new KoReportKSpreadRenderer();
     } 
-    if (key.toLower() == QLatin1String("htmltable")) {
+    if (lowerKey == QLatin1String("htmltable")) {
         return new KoReportHTMLTableRenderer();
     }
-    if (key.toLower() == QLatin1String("htmlcss")) {
+    if (lowerKey == QLatin1String("htmlcss")) {
         return new KoReportHTMLCSSRenderer();
     }
-    if (key.toLower() == QLatin1String("odttable") || key.toLower() == QLatin1String("odt")) {
+    if (lowerKey == QLatin1String("odttable") || lowerKey == QLatin1String("odt")) {
         return new KoReportODTRenderer();
     }
-    if (key.toLower() == QLatin1String("odtframes")) {
+    if (lowerKey == QLatin1String("odtframes")) {
         return new KoOdtFrameReportRenderer();
 }
 return 0;

kexi/plugins/tables/kexitabledesignerview.cpp
@@ -1196,7 +1196,7 @@ tristate KexiTableDesignerView::buildSchema(KexiDB::TableSchema &schema, bool be
         b = d->sets->at(i);
         if (b) {
             no_fields = false;
-            const QString name((*b)["name"].value().toString());
+            const QString name((*b)["name"].value().toString().toLower());
             if (name.isEmpty()) {
                 if (beSilent) {
                     kWarning() << QString("no field caption entered at record %1...").arg(i + 1);
@@ -1208,10 +1208,10 @@ tristate KexiTableDesignerView::buildSchema(KexiDB::TableSchema &schema, bool be
                 res = cancelled;
                 break;
             }
-            if (names.contains(name.toLower())) {
+            if (names.contains(name)) {
                 break;
             }
-            names.insert(name.toLower());   //remember
+            names.insert(name);   //remember
         }
     }
     if (res == true && no_fields) {//no fields added

libs/koreport/common/krutils.cpp
@@ -295,18 +295,20 @@ void KRUtils::buildXMLLineStyle(QDomDocument & doc, QDomElement & entity, KRLine
 
 void KRUtils::addPropertyAsAttribute(QDomElement* e, KoProperty::Property* p)
 {
+    const QString name = QLatin1String("report:") + p->name().toLower();
+
     switch (p->value().type()) {
         case QVariant::Int :
-            e->setAttribute(QLatin1String("report:") + p->name().toLower(), p->value().toInt());
+            e->setAttribute(name, p->value().toInt());
             break;
         case QVariant::Double:
-            e->setAttribute(QLatin1String("report:") + p->name().toLower(), p->value().toDouble());
+            e->setAttribute(name, p->value().toDouble());
             break;
         case QVariant::Bool:
-            e->setAttribute(QLatin1String("report:") + p->name().toLower(), p->value().toInt());
+            e->setAttribute(name, p->value().toInt());
             break;
         default:
-            e->setAttribute(QLatin1String("report:") + p->name().toLower(), p->value().toString());
+            e->setAttribute(name, p->value().toString());
             break;
     }
 }

libs/koreport/items/field/krscriptfield.cpp
@@ -42,13 +42,15 @@ void Field::setSource(const QString& s)
 
 int Field::horizontalAlignment()
 {
-    QString a = m_field->m_horizontalAlignment->value().toString();
+    const QString a = m_field->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "left") {
+    if (a == QLatin1String("left")) {
         return -1;
-    } else if (a.toLower() == "center") {
+    }
+    if (a == QLatin1String("center")) {
         return 0;
-    } else if (a.toLower() == "right") {
+    }
+    if (a == QLatin1String("right")) {
         return 1;
     }
     return -1;
@@ -73,13 +75,15 @@ void Field::setHorizonalAlignment(int a)
 
 int Field::verticalAlignment()
 {
-    QString a = m_field->m_horizontalAlignment->value().toString();
+    const QString a = m_field->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "top") {
+    if (a == QLatin1String("top")) {
         return -1;
-    } else if (a.toLower() == "middle") {
+    }
+    if (a == QLatin1String("middle")) {
         return 0;
-    } else if (a.toLower() == "bottom") {
+    }
+    if (a == QLatin1String("bottom")) {
         return 1;
     }
     return -1;

libs/koreport/items/label/krscriptlabel.cpp
@@ -41,13 +41,15 @@ void Label::setCaption(const QString& c)
 
 int Label::horizontalAlignment()
 {
-    QString a = m_label->m_horizontalAlignment->value().toString();
+    const QString a = m_label->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "left") {
+    if (a == QLatin1String("left")) {
         return -1;
-    } else if (a.toLower() == "center") {
+    }
+    if (a == QLatin1String("center")) {
         return 0;
-    } else if (a.toLower() == "right") {
+    }
+    if (a == QLatin1String("right")) {
         return 1;
     }
     return -1;
@@ -72,13 +74,15 @@ void Label::setHorizonalAlignment(int a)
 
 int Label::verticalAlignment()
 {
-    QString a = m_label->m_horizontalAlignment->value().toString();
+    const QString a = m_label->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "top") {
+    if (a == QLatin1String("top")) {
         return -1;
-    } else if (a.toLower() == "middle") {
+    }
+    if (a == QLatin1String("middle")) {
         return 0;
-    } else if (a.toLower() == "bottom") {
+    }
+    if (a == QLatin1String("bottom")) {
         return 1;
     }
     return -1;

libs/koreport/items/text/krscripttext.cpp
@@ -45,13 +45,15 @@ void Text::setSource(const QString& s)
 
 int Text::horizontalAlignment()
 {
-    QString a = m_text->m_horizontalAlignment->value().toString();
+    const QString a = m_text->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "left") {
+    if (a == "left") {
         return -1;
-    } else if (a.toLower() == "center") {
+    }
+    if (a == "center") {
         return 0;
-    } else if (a.toLower() == "right") {
+    }
+    if (a == "right") {
         return 1;
     }
     return -1;
@@ -76,13 +78,15 @@ void Text::setHorizonalAlignment(int a)
 
 int Text::verticalAlignment()
 {
-    QString a = m_text->m_horizontalAlignment->value().toString();
+    const QString a = m_text->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "top") {
+    if (a == "top") {
         return -1;
-    } else if (a.toLower() == "middle") {
+    }
+    if (a == "middle") {
         return 0;
-    } else if (a.toLower() == "bottom") {
+    }
+    if (a == "bottom") {
         return 1;
     }
     return -1;

plugins/reporting/barcode/krscriptbarcode.cpp
@@ -50,13 +50,15 @@ void Barcode::setSize(const QSizeF& s)
 
 int Barcode::horizontalAlignment()
 {
-    QString a = m_barcode->m_horizontalAlignment->value().toString();
+    const QString a = m_barcode->m_horizontalAlignment->value().toString().toLower();
 
-    if (a.toLower() == "left") {
+    if (a == QLatin1String("left")) {
         return -1;
-    } else if (a.toLower() == "center") {
+    }
+    if (a == QLatin1String("center")) {
         return 0;
-    } else if (a.toLower() == "right") {
+    }
+    if (a == QLatin1String("right")) {
         return 1;
     }
     return -1;

KDE/krita

commit 3338b50ea06f1cbe5c3e7ae1a227cc7a0690baea

Fix regression introduced in ce2e71dd87 - all reports started to crash
When talking about optimization, recommended approach is QString::compare() :)

CCMAIL:kossebau@kde.org

libs/koreport/renderer/KoReportRendererBase.cpp
@@ -46,7 +46,7 @@ KoReportRendererFactory::KoReportRendererFactory()
 
 KoReportRendererBase* KoReportRendererFactory::createInstance(const QString& key)
 {
-    const QString lowerKey =lowerKey;
+    const QString lowerKey = key.toLower();
     if (lowerKey == QLatin1String("screen")) {
         return new KoReportScreenRenderer();
     }
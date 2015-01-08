5494873a10acd3cce9d39b2e85ec1432fc8c51a7
Scott Petrovic <scottpetrovic@gmail.com>
2015-01-07 18:51:06 -0600

    make sure filters have an active node before enabling

--- krita/ui/kis_filter_manager.cc
+++ krita/ui/kis_filter_manager.cc
@@ -135,6 +135,8 @@ void KisFilterManager::insertFilter(const QString & filterName)
 
     KisAction *action = new KisAction(filter->menuEntry(), this);
     action->setShortcut(filter->shortcut(), KAction::DefaultShortcut);
+    action->setActivationFlags(KisAction::ACTIVE_NODE);
+
     d->actionManager->addAction(QString("krita_filter_%1").arg(filterName), action);
     d->filters2Action[filter.data()] = action;
 

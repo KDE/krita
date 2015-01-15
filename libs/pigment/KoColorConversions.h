a463bdbbbd14848f49c074226fb966272b6b9353
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-01-15 17:56:23 +0100

    Fix (version) typos and license naming in Lesser GPL headers
    There are a few issues with many Lesser GPL headers currently:
    * refer to version "2" while there is only version "2.1" as minimum
    * refer to "GPL" for more info, not "Lesser GPL"
    * typos & possibly unwanted sideeffects of replace-all actions
    
    This patch fixes those things. Quite a lot of files changed, so not fun to review, but you will be the hero of the day if you do.  At least I had some fun with perl and regular expressions, at least when it worked :)
    
    See also http://lists.kde.org/?l=kde-licensing&m=142039469120716&w=2
    
    REVIEW: 121996
    
    Thanks to boud and ingwa for review!

--- libs/pigment/KoColorConversions.h
+++ libs/pigment/KoColorConversions.h
@@ -1,12 +1,12 @@
 /*
  *  Copyright (c) 2005 Boudewijn Rempt <boud@valdyas.org>
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This library is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation; either version 2 of the License, or
+ *  by the Free Software Foundation; either version 2.1 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.

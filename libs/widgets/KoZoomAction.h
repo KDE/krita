33970bcd101d25105ef7c792a409b915740ae066
Boudewijn Rempt <boud@valdyas.org>
2015-01-30 15:11:28 +0100

    BUG:342709 Update the minimum zoom level after scaling the image

--- libs/widgets/KoZoomAction.h
+++ libs/widgets/KoZoomAction.h
@@ -172,6 +172,49 @@ signals:
     void zoomLevelsChanged(QStringList values);
     void currentZoomLevelChanged(QString valueString);
     void sliderChanged(int value);
+
+public:
+    /**
+     * Return the minimum zoom possible for documents.
+     *
+     * \return The minimum zoom possible.
+     */
+    qreal minimumZoom();
+    /**
+     * Return the maximum zoom possible for documents.
+     *
+     * \return The maximum zoom possible.
+     */
+    qreal maximumZoom();
+    /**
+     * Clamp the zoom value so that mimimumZoom <= zoom <= maximumZoom.
+     *
+     * \param zoom The value to clamp.
+     *
+     * \return minimumZoom if zoom < minimumZoom, maximumZoom if zoom >
+     * maximumZoom, zoom if otherwise.
+     */
+    qreal clampZoom(qreal zoom);
+
+    /**
+     * Set the minimum zoom possible for documents.
+     *
+     * Note that after calling this, any existing KoZoomAction instances
+     * should be recreated.
+     *
+     * \param zoom The minimum zoom to use.
+     */
+    void setMinimumZoom(qreal zoom);
+    /**
+     * Set the maximum zoom possible for documents.
+     *
+     * Note that after calling this, any existing KoZoomAction instances
+     * should be recreated.
+     *
+     * \param zoom The maximum zoom to use.
+     */
+    void setMaximumZoom(qreal zoom);
+
 protected:
     /// Regenerates the action's items
     void regenerateItems( const qreal zoom, bool asCurrent = false );

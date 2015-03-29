bf3a19f8b4182bc11db412fd174f774443955de0
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-03-29 23:09:08 +0200

    Readd FILEMANAGER_THUMBNAIL to build

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -104,7 +104,7 @@ calligra_define_product(DOC "Calligra Documentations" UNPORTED)
 # extras
 calligra_define_product(APP_CONVERTER "Format converter for commandline" REQUIRES LIB_CALLIGRA)
 calligra_define_product(FILEMANAGER_PROPERTIES "Plugin for the KDE file properties dialog" UNPORTED  REQUIRES LIB_CALLIGRA)
-calligra_define_product(FILEMANAGER_THUMBNAIL "Plugins for KDE filesystem thumbnailing" UNPORTED  REQUIRES LIB_CALLIGRA)
+calligra_define_product(FILEMANAGER_THUMBNAIL "Plugins for KDE filesystem thumbnailing"  REQUIRES LIB_CALLIGRA)
 calligra_define_product(FILEMANAGER_QUICKPRINT "Plugin for the filemanager adding a \"Print\" action" UNPORTED)
 calligra_define_product(FILEMANAGER_TEMPLATES "File templates for filemanager" UNPORTED)
 calligra_define_product(OKULAR_GENERATOR_ODP "Plugin for Okular adding support for ODP" UNPORTED  REQUIRES PART_STAGE)

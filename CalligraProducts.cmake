223ef66fbdeb9490271b4046b9a51fec49b3e43f
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-03-29 21:13:29 +0200

    Readd PLUGIN_VARIABLES to build

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -120,7 +120,7 @@ calligra_define_product(PLUGIN_TEXTEDITING "Textediting plugins"  REQUIRES LIB_C
 calligra_define_product(PLUGIN_KEXI_SPREADSHEETMIGRATION "Import from ODS plugin for Kexi" UNPORTED  REQUIRES APP_KEXI PART_SHEETS)
 calligra_define_product(PLUGIN_DEFAULTTOOLS "Default Flake tools plugin" REQUIRES LIB_CALLIGRA)
 calligra_define_product(PLUGIN_PATHSHAPES "Path shape plugins" REQUIRES LIB_CALLIGRA)
-calligra_define_product(PLUGIN_VARIABLES "Text variables plugin" UNPORTED  REQUIRES LIB_CALLIGRA)
+calligra_define_product(PLUGIN_VARIABLES "Text variables plugin"  REQUIRES LIB_CALLIGRA)
 calligra_define_product(PLUGIN_CHARTSHAPE "Chart shape plugin"  REQUIRES LIB_CALLIGRA)
 calligra_define_product(PLUGIN_PICTURESHAPE "Picture shape plugin" REQUIRES LIB_CALLIGRA)
 calligra_define_product(PLUGIN_PLUGINSHAPE "Plugin shape plugin" REQUIRES LIB_CALLIGRA)

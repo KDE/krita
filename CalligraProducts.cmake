49ae3590ed9af4eea3d7a757f0d731fbc7ee169d
Yue Liu <yue.liu@mail.com>
2015-03-30 02:20:00 -0700

    Readd APP_KARBON (w/o plugins) to build

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -91,7 +91,7 @@ calligra_define_product(APP_WORDS "Words app (for Desktop)"  REQUIRES PART_WORDS
 calligra_define_product(APP_STAGE "Stage app (for Desktop)"  REQUIRES PART_STAGE)
 calligra_define_product(APP_SHEETS "Sheets app (for Desktop)" UNPORTED  REQUIRES PART_SHEETS)
 calligra_define_product(APP_AUTHOR "Author app (for Desktop)" UNPORTED  REQUIRES PART_WORDS)
-calligra_define_product(APP_KARBON "Karbon app (for Desktop)" UNPORTED  REQUIRES LIB_CALLIGRA)
+calligra_define_product(APP_KARBON "Karbon app (for Desktop)"  REQUIRES LIB_CALLIGRA)
 calligra_define_product(APP_KRITA "Krita app (for Desktop)" UNPORTED REQUIRES LIB_CALLIGRA)
 calligra_define_product(APP_KEXI "Kexi app (for Desktop)" UNPORTED  REQUIRES LIB_CALLIGRA)
 calligra_define_product(APP_FLOW "Flow app (for Desktop)" UNPORTED  REQUIRES LIB_CALLIGRA LIB_KOPAGEAPP)

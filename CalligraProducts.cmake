1b78fe6d6fda830911f5e1b924319643fe820170
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-04-04 09:15:24 +0200

    Readd FILTER_HTML_TO_ODS to build

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -148,7 +148,7 @@ calligra_define_product(FILTER_SHEETS_TO_GNUMERIC "Sheets GNUMERIC import filter
 calligra_define_product(FILTER_OPENCALC_TO_SHEETS "Sheets OpenOffice.org Calc import filter" UNPORTED  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_SHEETS_TO_OPENCALC "Sheets OpenOffice.org Calc export filter" UNPORTED  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_QUATTROPRO_TO_SHEETS "Sheets Quattro Pro import filter" UNPORTED  REQUIRES PART_SHEETS)
-calligra_define_product(FILTER_HTML_TO_ODS "HTML to ODS filter" UNPORTED  REQUIRES LIB_CALLIGRA)
+calligra_define_product(FILTER_HTML_TO_ODS "HTML to ODS filter"  REQUIRES LIB_CALLIGRA)
 calligra_define_product(FILTER_SHEETS_TO_HTML "Sheets HTML export filter" UNPORTED  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_KSPREAD_TO_LATEX "KSpread to LaTeX filter" UNPORTED  REQUIRES LIB_CALLIGRA)
 

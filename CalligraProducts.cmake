6ada90d53f88e9dc892a6e9f877c9843dd512864
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-04-04 07:02:47 +0200

    Readd FILTER_APPLIXSPREAD_TO_KSPREAD to build

--- CalligraProducts.cmake
+++ CalligraProducts.cmake
@@ -141,7 +141,7 @@ calligra_define_product(FILTER_XLS_TO_SHEETS "Sheets XLS import filter"  REQUIRE
 calligra_define_product(FILTER_SHEETS_TO_XLS "Sheets XLS export filter" UNPORTED  REQUIRES LIB_MSO LIB_KOMSOOXML PART_SHEETS)
 calligra_define_product(FILTER_CSV_TO_SHEETS "Sheets CSV import filter"  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_SHEETS_TO_CSV "Sheets CSV export filter"  REQUIRES PART_SHEETS)
-calligra_define_product(FILTER_APPLIXSPREAD_TO_KSPREAD "Applix Spreadsheet to KSpread filter" UNPORTED  REQUIRES PART_SHEETS)
+calligra_define_product(FILTER_APPLIXSPREAD_TO_KSPREAD "Applix Spreadsheet to KSpread filter"  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_DBASE_TO_KSPREAD "dBASE to KSpread filter" UNPORTED  REQUIRES LIB_CALLIGRA)
 calligra_define_product(FILTER_GNUMERIC_TO_SHEETS "Sheets GNUMERIC import filter" UNPORTED  REQUIRES PART_SHEETS)
 calligra_define_product(FILTER_SHEETS_TO_GNUMERIC "Sheets GNUMERIC import filter" UNPORTED  REQUIRES PART_SHEETS)

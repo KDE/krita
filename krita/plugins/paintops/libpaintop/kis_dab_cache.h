963c00fc4d3c0514c4dcf07d702f05d33237025a
Boudewijn Rempt <boud@valdyas.org>
2015-04-18 14:54:50 +0200

    Add apidox, apidox are good to have\!

--- krita/plugins/paintops/libpaintop/kis_dab_cache.h
+++ krita/plugins/paintops/libpaintop/kis_dab_cache.h
@@ -30,6 +30,25 @@ class KisPrecisionOption;
 struct MirrorProperties;
 
 
+/**
+ * @brief The KisDabCache class provides caching for dabs into the brush paintop
+ *
+ *  This class adds caching of the dabs to the paintop system of Krita.
+ *  Such cache makes the execution of the benchmarks up to 2 times faster.
+ *  Subjectively, the real painting becomes much faster, especially with
+ *  huge brushes. Artists report up to 20% speed gain while painting.
+ *
+ *  Of course, such caching makes the painting a bit less precise: we need
+ *  to tolerate subpixel differences to allow the cache to work. Sometimes
+ *  small difference in the size of a dab can also be acceptable. That is
+ *  why I introduced levels of precision. They are graded from 1 to 5: from
+ *  the fastest and less precise to the slowest, but with the best quality.
+ *  You can see the slider in the paintop settings dialog. The ToolTip text
+ *  explains which features of the brush are sacrificed on each precision
+ *  level.
+ *
+ *  The texturing and mirroring problems are solved.
+ */
 class PAINTOP_EXPORT KisDabCache
 {
 public:

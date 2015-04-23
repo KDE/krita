KDE/calligra

commit 079234f4de9d7bf5af11f3e2fa4d467b73d9f2e9

Fix finding of the cached thumbnails.
The actual image in the case might be smaller then the image size requested.
When adding the image to the change it might be have reduced in size to have
no empty space around it. Therefore save the expected size in the cache so it
gets found when it is smaller due to empty space around it.

libs/kopageapp/KoPAPixmapCache.cpp
@@ -72,9 +72,9 @@ bool KoPAPixmapCache::find( const QString & key, const QSize & size, QPixmap & p
     return QPixmapCache::find( k, pm );
 }
 
-bool KoPAPixmapCache::insert( const QString & key, const QPixmap & pm )
+bool KoPAPixmapCache::insert( const QString & key, const QPixmap & pm, const QSize &size )
 {
-    QString k = generateKey( key, pm.size() );
+    QString k = generateKey( key, size.isValid() ? size: pm.size() );
     m_keySize[key].append( pm.size() );
     return QPixmapCache::insert( k, pm );
 }

libs/kopageapp/KoPAPageBase.cpp
@@ -275,8 +275,8 @@ QPixmap KoPAPageBase::thumbnail( const QSize& size )
     QPixmap pm;
     if ( !KoPAPixmapCache::instance()->find( key, size, pm ) ) {
         pm = generateThumbnail( size );
-        KoPAPixmapCache::instance()->insert( key, pm );
-        kDebug(30010) << "create thumbnail" << this;
+        KoPAPixmapCache::instance()->insert( key, pm, size );
+        kDebug(30010) << "create thumbnail" << this << key << size;
     }
     else {
         kDebug(30010) << "thumbnail in cache " << this;

libs/kopageapp/KoPAPixmapCache.h
@@ -21,9 +21,9 @@
 #define KOPAPIXMAPCACHE_H
 
 #include <QMap>
+#include <QSize>
 
 class QString;
-class QSize;
 class QPixmap;
 
 /**
@@ -81,7 +81,7 @@ public:
      *
      * The size is taken from the pixmap.
      */
-    bool insert( const QString & key, const QPixmap & pm );
+    bool insert( const QString & key, const QPixmap & pm, const QSize &size = QSize());
 
     /**
      * Remove all pixmaps associated with key from the cache

KDE/krita

commit 0e42d6143e5b565fedff7a1e9670bf01a0c24d44

Update page thumbnail on change
Trigger page update on master page change.
Fix bug introduced in commit 079234f4de9d7bf5af11f3e2fa4d467b73d9f2e9.
Store the size we need to lookup in the cache.

libs/kopageapp/KoPAPage.cpp
@@ -112,6 +112,7 @@ void KoPAPage::setMasterPage( KoPAMasterPage * masterPage )
 {
     Q_ASSERT(masterPage);
     m_masterPage = masterPage;
+    pageUpdated();
 }
 
 void KoPAPage::paintBackground( QPainter & painter, const KoViewConverter & converter, KoShapePaintingContext &paintContext )

libs/kopageapp/KoPAPixmapCache.cpp
@@ -75,7 +75,7 @@ bool KoPAPixmapCache::find( const QString & key, const QSize & size, QPixmap & p
 bool KoPAPixmapCache::insert( const QString & key, const QPixmap & pm, const QSize &size )
 {
     QString k = generateKey( key, size.isValid() ? size: pm.size() );
-    m_keySize[key].append( pm.size() );
+    m_keySize[key].append( size.isValid() ? size: pm.size() );
     return QPixmapCache::insert( k, pm );
 }
 
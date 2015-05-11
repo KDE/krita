d9c93d56d1919b76e39f0a5396f580ecabe6450f
Boudewijn Rempt <boud@valdyas.org>
2015-05-11 09:37:52 +0200

    BUG:344750 Add a shortcut for edit/paste into new image
    Patch by Tiffany. Thanks!
    CCMAIL:dev@tiff.nu

--- krita/ui/kis_selection_manager.cc
+++ krita/ui/kis_selection_manager.cc
@@ -135,8 +135,9 @@ void KisSelectionManager::setup(KisActionManager* actionManager)
     actionManager->addAction("cut_sharp", cutSharp);
     connect(cutSharp, SIGNAL(triggered()), this, SLOT(cutSharp()));
 
-    m_pasteNew  = new KisAction(i18n("Paste into &New Image"), this);
+    m_pasteNew = new KisAction(i18n("Paste into &New Image"), this);
     actionManager->addAction("paste_new", m_pasteNew);
+    m_pasteNew->setShortcut(QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_N));
     connect(m_pasteNew, SIGNAL(triggered()), this, SLOT(pasteNew()));
 
     m_pasteAt = new KisAction(i18n("Paste at cursor"), this);

326fa473604a9be0d1981195a2cf7e17b1636b5a
Dmitry Kazakov <dimula73@gmail.com>
2015-05-13 15:01:31 +0400

    Add fake icons for Pass Through mode so painters could just replace them

--- krita/image/kis_group_layer.cc
+++ krita/image/kis_group_layer.cc
@@ -283,7 +283,7 @@ KisDocumentSectionModel::PropertyList KisGroupLayer::sectionModelProperties() co
 {
     KisDocumentSectionModel::PropertyList l = KisLayer::sectionModelProperties();
     // XXX: get right icons
-    l << KisDocumentSectionModel::Property(i18n("Pass Through"), koIcon("transparency-locked"), koIcon("transparency-unlocked"), passThroughMode());
+    l << KisDocumentSectionModel::Property(i18n("Pass Through"), koIcon("passthrough-enabled"), koIcon("passthrough-disabled"), passThroughMode());
     return l;
 }
 

2881f9005f58c35e63ccfc516d36eb1a33de5ad8
Boudewijn Rempt <boud@valdyas.org>
2015-05-26 21:48:44 +0200

    BUG:345913 Allow the user to chose a profile for untagged RGB
    Depending on the paste settings (ask every time/assume), on loading
    a PNG file without an embedded profile we will now ask for a profile,
    or we'll assume sRGB (which makes more sense for PNG files than
    linear, if something is linear, there'll be a profile, almost always.)
    
    Note: the proliferation of places where we hard-code
    "sRGB-elle-V2-srgbtrc.icc" is becoming a problem, and that needs
    fixing.

---
+++ krita/ui/dialogs/kis_dlg_png_import.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef KIS_DLG_PNG_IMPORT_H
+#define KIS_DLG_PNG_IMPORT_H
+
+#include <kdialog.h>
+#include <QString>
+
+#include "ui_wdgdlgpngimport.h"
+
+
+class KisDlgPngImport : public KDialog
+{
+    Q_OBJECT
+
+public:
+    KisDlgPngImport(const QString &path, const QString &colorModelID, const QString &colorDepthID, QWidget *parent = 0);
+    QString profile() const;
+
+private:
+
+    Ui_WdgDlgPngImport dlgWidget;
+
+};
+
+#endif // KIS_DLG_PNG_IMPORT_H

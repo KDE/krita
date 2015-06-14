73d4770222cd7a2d084155543e1831e399915ba4
Jouni Pentikäinen <joupent@gmail.com>
2015-06-14 14:55:37 +0300

    Update email address in copyright blocks

--- krita/plugins/extensions/dockers/animation/animation_docker.cpp
+++ krita/plugins/extensions/dockers/animation/animation_docker.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015 Jouni Pentikäinen <mctyyppi42@gmail.com>
+ *  Copyright (c) 2015 Jouni Pentikäinen <joupent@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

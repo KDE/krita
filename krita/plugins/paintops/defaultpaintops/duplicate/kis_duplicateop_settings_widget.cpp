7fe949cafc7d9a1a7942332c83f1f2ac55bb481c
Dmitry Kazakov <dimula73@gmail.com>
2015-08-28 16:33:07 +0300

    Temporarily disable LoD for Clone Brush
    The brush is buggy even without LoD, so we cannot enable it yet

--- krita/plugins/paintops/defaultpaintops/duplicate/kis_duplicateop_settings_widget.cpp
+++ krita/plugins/paintops/defaultpaintops/duplicate/kis_duplicateop_settings_widget.cpp
@@ -35,6 +35,7 @@
 #include "kis_curve_option_widget.h"
 #include <kis_pressure_mirror_option_widget.h>
 #include "kis_pressure_texture_strength_option.h"
+#include "kis_paintop_lod_limitations.h"
 
 
 KisDuplicateOpSettingsWidget::KisDuplicateOpSettingsWidget(QWidget* parent)
@@ -64,6 +65,13 @@ KisPropertiesConfiguration* KisDuplicateOpSettingsWidget::configuration() const
     config->setProperty("paintop", "duplicate"); // XXX: make this a const id string
     writeConfiguration(config);
     return config;
+}
+
+KisPaintopLodLimitations KisDuplicateOpSettingsWidget::lodLimitations() const
+{
+    KisPaintopLodLimitations l = KisBrushBasedPaintopOptionWidget::lodLimitations();
+    l.blockers << KoID("clone-brush", i18nc("PaintOp LoD limitation", "Clone Brush (temporarily disabled)"));
+    return l;
 }
 
 #include "kis_duplicateop_settings_widget.moc"

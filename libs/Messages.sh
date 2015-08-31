a388e631369eaa9abe1fb95b6e6a685f1bcfc525
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-09-01 00:35:43 +0200

    Remove left-over for extraction messages from former libs/db

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 source ../calligra_xgettext.sh
 
-EXCLUDE="-path ./db/drivers/sqlite"
-LIST=`find . \( $EXCLUDE \) -prune -o \( -name \*.ui -o -name \*.rc \) -type f -print | grep -v -e '/\.'`
+LIST=`find . \( -name \*.ui -o -name \*.rc \) -type f -print | grep -v -e '/\.'`
 $EXTRACTRC $LIST >> rc.cpp
-LIST=`find . \( $EXCLUDE \) -prune -o \( -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f -print | grep -v '/tests/' | grep -v -e '/\.'`
+LIST=`find . \( -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f -print | grep -v '/tests/' | grep -v -e '/\.'`
 calligra_xgettext calligra.pot $LIST
 rm -f rc.cpp

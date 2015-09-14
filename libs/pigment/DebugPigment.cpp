24fadbd0332e6e4312f350aa8d98445222ad4445
Boudewijn Rempt <boud@valdyas.org>
2015-09-14 11:07:35 +0200

    Namespace the pigment log category as calligra.libs.pigment

--- libs/pigment/DebugPigment.cpp
+++ libs/pigment/DebugPigment.cpp
@@ -19,7 +19,7 @@
 #include "DebugPigment.h"
 const QLoggingCategory &PIGMENT_log() \
 {
-    static const QLoggingCategory category("log_pigment");
+    static const QLoggingCategory category("calligra.lib.pigment");
     return category;
 }
 

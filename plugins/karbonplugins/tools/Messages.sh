1925d07d26068a3409701db9673d513183ef03e1
Albert Astals Cid <aacid@kde.org>
2015-09-19 17:55:58 +0200

    Fix Messages.sh
    Fix path to KarbonPatternOptionsWidget.cpp
    Remove KoGradientEditWidget.cpp from the files extracted, if it's not compiled in the plugin doesn't make sense for its strings to be part of this, no?
    
    CCMAIL: boud@valdyas.org

--- plugins/karbonplugins/tools/Messages.sh
+++ plugins/karbonplugins/tools/Messages.sh
@@ -4,7 +4,6 @@ source ../../../calligra_xgettext.sh
 $EXTRACTRC ../../ui/widgets/KarbonPatternOptionsWidget.ui filterEffectTool/FilterEffectEditWidget.ui >> rc.cpp
 calligra_xgettext KarbonTools.pot rc.cpp *.cpp *.h CalligraphyTool/*.cpp \
     CalligraphyTool/*.h filterEffectTool/*cpp \
-    ../../ui/widgets/KarbonPatternOptionsWidget.cpp \
-    ../../ui/widgets/KoGradientEditWidget.cpp
+    KarbonPatternOptionsWidget.cpp
 
 rm -f rc.cpp

d7777e37280aa54b349f26f52fb7c131aba7473e
Boudewijn Rempt <boud@valdyas.org>
2015-09-30 13:07:32 +0200

    Remove unused cmake modules

--- cmake/modules/FindSpnav.cmake
+++
@@ -1,6 +0,0 @@
-find_path(SPNAV_INCLUDE_DIR spnav.h )
-
-find_library(SPNAV_LIBRARY NAMES spnav ) 
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Spnav DEFAULT_MSG SPNAV_INCLUDE_DIR SPNAV_LIBRARY )

79699a26c462b15056a267d688595cf1712853a4
Boudewijn Rempt <boud@valdyas.org>
2015-10-20 08:12:05 +0200

    BUG:353719 Fix possible crashes in the compositions docker
    Check pointers before dereferencing.

--- krita/plugins/extensions/dockers/compositiondocker/compositiondocker_dock.cpp
+++ krita/plugins/extensions/dockers/compositiondocker/compositiondocker_dock.cpp
@@ -67,7 +67,7 @@ CompositionDockerDock::CompositionDockerDock( ) : QDockWidget(i18n("Compositions
     setWidget(widget);
 
     connect( compositionView, SIGNAL(doubleClicked(QModelIndex)),
-            this, SLOT(activated ( const QModelIndex & ) ) );
+             this, SLOT(activated ( const QModelIndex & ) ) );
 
     compositionView->setContextMenuPolicy(Qt::CustomContextMenu);
     connect( compositionView, SIGNAL(customContextMenuRequested(QPoint)),
@@ -129,7 +129,7 @@ void CompositionDockerDock::activated(const QModelIndex& index)
 void CompositionDockerDock::deleteClicked()
 {
     QModelIndex index = compositionView->currentIndex();
-    if(index.isValid()) {
+    if (m_canvas && m_canvas->viewManager() && m_canvas->viewManager()->image() && index.isValid()) {
         KisLayerComposition* composition = m_model->compositionFromIndex(index);
         m_canvas->viewManager()->image()->removeComposition(composition);
         updateModel();
@@ -139,6 +139,8 @@ void CompositionDockerDock::deleteClicked()
 void CompositionDockerDock::saveClicked()
 {
     KisImageWSP image = m_canvas->viewManager()->image();
+    if (!image) return;
+
     // format as 001, 002 ...
     QString name = saveNameEdit->text();
     if (name.isEmpty()) {
@@ -172,78 +174,80 @@ void CompositionDockerDock::updateModel()
 
 void CompositionDockerDock::exportClicked()
 {
-	QString path;
+    if (m_canvas && m_canvas->viewManager() && m_canvas->viewManager()->image()) {
+        QString path;
 
-    KoFileDialog dialog(0, KoFileDialog::OpenDirectory, "krita/compositiondockerdock");
-    dialog.setCaption(i18n("Select a Directory"));
-    dialog.setDefaultDir(QDesktopServices::storageLocation(QDesktopServices::HomeLocation));
-    path = dialog.filename();
+        KoFileDialog dialog(0, KoFileDialog::OpenDirectory, "krita/compositiondockerdock");
+        dialog.setCaption(i18n("Select a Directory"));
+        dialog.setDefaultDir(QDesktopServices::storageLocation(QDesktopServices::HomeLocation));
+        path = dialog.filename();
 
 
-    if (path.isNull()) return;
+        if (path.isNull()) return;
 
-    if (!path.endsWith('/')) {
-		path.append('/');
-	}
-
-    KisImageWSP image = m_canvas->viewManager()->image();
-    QString filename = m_canvas->viewManager()->document()->localFilePath();
-    if (!filename.isEmpty()) {
-        QFileInfo info(filename);
-        path += info.baseName() + '_';
-    }
-
-    foreach(KisLayerComposition* composition, m_canvas->viewManager()->image()->compositions()) {
-        if (!composition->isExportEnabled()) {
-            continue;
+        if (!path.endsWith('/')) {
+            path.append('/');
         }
 
-        composition->apply();
-        image->refreshGraph();
-        image->lock();
+        KisImageWSP image = m_canvas->viewManager()->image();
+        QString filename = m_canvas->viewManager()->document()->localFilePath();
+        if (!filename.isEmpty()) {
+            QFileInfo info(filename);
+            path += info.baseName() + '_';
+        }
+
+        foreach(KisLayerComposition* composition, m_canvas->viewManager()->image()->compositions()) {
+            if (!composition->isExportEnabled()) {
+                continue;
+            }
+
+            composition->apply();
+            image->refreshGraph();
+            image->lock();
 #if 0
-        image->rootLayer()->projection()->convertToQImage(0, 0, 0, image->width(), image->height()).save(path + composition->name() + ".png");
+            image->rootLayer()->projection()->convertToQImage(0, 0, 0, image->width(), image->height()).save(path + composition->name() + ".png");
 #else
-        QRect r = image->bounds();
+            QRect r = image->bounds();
 
-        KisDocument *d = KisPart::instance()->createDocument();
+            KisDocument *d = KisPart::instance()->createDocument();
 
-        d->prepareForImport();
+            d->prepareForImport();
 
-        KisImageWSP dst = new KisImage(d->createUndoStore(), r.width(), r.height(), image->colorSpace(), composition->name());
-        dst->setResolution(image->xRes(), image->yRes());
-        d->setCurrentImage(dst);
-        KisPaintLayer* paintLayer = new KisPaintLayer(dst, "projection", OPACITY_OPAQUE_U8);
-        KisPainter gc(paintLayer->paintDevice());
-        gc.bitBlt(QPoint(0, 0), image->rootLayer()->projection(), r);
-        dst->addNode(paintLayer, dst->rootLayer(), KisLayerSP(0));
+            KisImageWSP dst = new KisImage(d->createUndoStore(), r.width(), r.height(), image->colorSpace(), composition->name());
+            dst->setResolution(image->xRes(), image->yRes());
+            d->setCurrentImage(dst);
+            KisPaintLayer* paintLayer = new KisPaintLayer(dst, "projection", OPACITY_OPAQUE_U8);
+            KisPainter gc(paintLayer->paintDevice());
+            gc.bitBlt(QPoint(0, 0), image->rootLayer()->projection(), r);
+            dst->addNode(paintLayer, dst->rootLayer(), KisLayerSP(0));
 
-        dst->refreshGraph();
+            dst->refreshGraph();
 
-        d->setOutputMimeType("image/png");
-        d->setSaveInBatchMode(true);
+            d->setOutputMimeType("image/png");
+            d->setSaveInBatchMode(true);
 
-        d->exportDocument(QUrl::fromLocalFile(path + composition->name() + ".png"));
+            d->exportDocument(QUrl::fromLocalFile(path + composition->name() + ".png"));
 
-        delete d;
+            delete d;
 
 #endif
-        image->unlock();
+            image->unlock();
+        }
     }
 }
 
 bool CompositionDockerDock::eventFilter(QObject* obj, QEvent* event)
 {
-     if (event->type() == QEvent::KeyPress ) {
-         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-         if (keyEvent->key() == Qt::Key_Up || keyEvent->key() == Qt::Key_Down) {
-             // new index will be set after the method is called
-             QTimer::singleShot(0, this, SLOT(activateCurrentIndex()));
-         }
-         return false;
-     } else {
-         return QObject::eventFilter(obj, event);
-     }
+    if (event->type() == QEvent::KeyPress ) {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        if (keyEvent->key() == Qt::Key_Up || keyEvent->key() == Qt::Key_Down) {
+            // new index will be set after the method is called
+            QTimer::singleShot(0, this, SLOT(activateCurrentIndex()));
+        }
+        return false;
+    } else {
+        return QObject::eventFilter(obj, event);
+    }
 }
 
 void CompositionDockerDock::activateCurrentIndex()
@@ -265,7 +269,7 @@ void CompositionDockerDock::customContextMenuRequested(QPoint pos)
 void CompositionDockerDock::updateComposition()
 {
     QModelIndex index = compositionView->currentIndex();
-    if (index.isValid()) {
+    if (m_canvas && m_canvas->viewManager() && m_canvas->viewManager()->image() && index.isValid()) {
         KisLayerComposition* composition = m_model->compositionFromIndex(index);
         composition->store();
         m_canvas->image()->setModified();
@@ -276,7 +280,7 @@ void CompositionDockerDock::renameComposition()
 {
     dbgKrita << "rename";
     QModelIndex index = compositionView->currentIndex();
-    if (index.isValid()) {
+    if (m_canvas && m_canvas->viewManager() && m_canvas->viewManager()->image() && index.isValid()) {
         KisLayerComposition* composition = m_model->compositionFromIndex(index);
         bool ok;
         QString name = QInputDialog::getText(this, i18n("Rename Composition"),

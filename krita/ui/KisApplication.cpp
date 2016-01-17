b4bac8d05ba2d63503d884d699046bdbcf21596f
Boudewijn Rempt <boud@valdyas.org>
2016-01-17 14:31:16 +0100

    Win build fix

--- krita/ui/KisApplication.cpp
+++ krita/ui/KisApplication.cpp
@@ -174,7 +174,7 @@ KisApplication::KisApplication(const QString &key, int &argc, char **argv)
      * Warn about Intel's broken video drivers
      */
 #if defined HAVE_OPENGL && defined Q_OS_WIN
-
+    KisConfig cfg;
     QString renderer = KisOpenGL::renderer();
     if (cfg.useOpenGL() && renderer.startsWith("Intel") && !cfg.readEntry("WarnedAboutIntel", false)) {
         QMessageBox::information(0,

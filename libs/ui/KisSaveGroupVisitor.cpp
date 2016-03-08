a061be4b03a05599e59e6d05d3cbec00419a0421
Boudewijn Rempt <boud@valdyas.org>
2016-03-08 11:24:45 +0100

    Extract the savegroup visitor into its own file and make public

---
+++ libs/ui/KisSaveGroupVisitor.cpp
@@ -0,0 +1,142 @@
+/*
+ *  Copyright (C) 2016 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include "KisSaveGroupVisitor.h"
+
+#include <KisDocument.h>
+#include <kis_painter.h>
+#include <kis_paint_layer.h>
+#include <KisPart.h>
+
+KisSaveGroupVisitor::KisSaveGroupVisitor(KisImageWSP image,
+                                         bool saveInvisible,
+                                         bool saveTopLevelOnly,
+                                         const QUrl &url,
+                                         const QString &baseName,
+                                         const QString &extension,
+                                         const QString &mimeFilter,
+                                         bool cropLayers)
+    : m_image(image)
+    , m_saveInvisible(saveInvisible)
+    , m_saveTopLevelOnly(saveTopLevelOnly)
+    , m_url(url)
+    , m_baseName(baseName)
+    , m_extension(extension)
+    , m_mimeFilter(mimeFilter)
+    , m_cropLayers(cropLayers)
+{
+}
+
+KisSaveGroupVisitor::~KisSaveGroupVisitor()
+{
+}
+
+bool KisSaveGroupVisitor::visit(KisNode* ) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisPaintLayer *) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisAdjustmentLayer *) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisExternalLayer *) {
+    return true;
+}
+
+
+bool KisSaveGroupVisitor::visit(KisCloneLayer *) {
+    return true;
+}
+
+
+bool KisSaveGroupVisitor::visit(KisFilterMask *) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisTransformMask *) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisTransparencyMask *) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisGeneratorLayer * ) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisSelectionMask* ) {
+    return true;
+}
+
+bool KisSaveGroupVisitor::visit(KisGroupLayer *layer)
+{
+    if (layer == m_image->rootLayer()) {
+        KisLayerSP child = dynamic_cast<KisLayer*>(layer->firstChild().data());
+        while (child) {
+            child->accept(*this);
+            child = dynamic_cast<KisLayer*>(child->nextSibling().data());
+        }
+
+    }
+    else if (layer->visible() || m_saveInvisible) {
+
+        QRect r = m_image->bounds();
+
+        KisDocument *exportDocument = KisPart::instance()->createDocument();
+
+        exportDocument->prepareForImport();
+
+        KisImageWSP dst = new KisImage(exportDocument->createUndoStore(), r.width(), r.height(), m_image->colorSpace(), layer->name());
+        dst->setResolution(m_image->xRes(), m_image->yRes());
+        exportDocument->setCurrentImage(dst);
+        KisPaintLayer* paintLayer = new KisPaintLayer(dst, "projection", layer->opacity());
+        KisPainter gc(paintLayer->paintDevice());
+        gc.bitBlt(QPoint(0, 0), layer->projection(), r);
+        dst->addNode(paintLayer, dst->rootLayer(), KisLayerSP(0));
+
+        dst->refreshGraph();
+
+        exportDocument->setOutputMimeType(m_mimeFilter.toLatin1());
+        exportDocument->setSaveInBatchMode(true);
+
+
+        QUrl url = m_url;
+
+        url = url.adjusted(QUrl::RemoveFilename);
+        url.setPath(url.path() + m_baseName + '_' + layer->name().replace(' ', '_') + '.' + m_extension);
+
+        exportDocument->exportDocument(url);
+
+        if (!m_saveTopLevelOnly) {
+            KisGroupLayerSP child = dynamic_cast<KisGroupLayer*>(layer->firstChild().data());
+            while (child) {
+                child->accept(*this);
+                child = dynamic_cast<KisGroupLayer*>(child->nextSibling().data());
+            }
+        }
+        delete exportDocument;
+    }
+
+    return true;
+}
+
+
---
+++ libs/ui/KisSaveGroupVisitor.h
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2016 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#ifndef KISSAVEGROUPVISITOR_H
+#define KISSAVEGROUPVISITOR_H
+
+#include "kritaui_export.h"
+
+#include <QUrl>
+#include <QString>
+
+#include <kis_types.h>
+#include <kis_node_visitor.h>
+#include <kis_layer.h>
+#include <kis_group_layer.h>
+#include <kis_node.h>
+#include <kis_image.h>
+
+
+/**
+ * @brief The KisSaveGroupVisitor class saves the groups in 
+ * a Krita image to separate images.
+ */
+class KRITAUI_EXPORT KisSaveGroupVisitor : public KisNodeVisitor
+{
+public:
+    
+    /**
+     * Create a KisSaveGroupVisitor
+     * 
+     * @param image: the image to save
+     * @param saveInvisible: also save invisibible layers
+     * @param saveTopLevelOnly: if true, only save the toplevel layers, otherwise 
+     *        descend into groups and save the bottom-most groups (groups that do
+     *        not contain another group.
+     * @param url
+     */    
+    KisSaveGroupVisitor(KisImageWSP image,
+                        bool saveInvisible,
+                        bool saveTopLevelOnly,
+                        const QUrl &url,
+                        const QString &baseName,
+                        const QString &extension,
+                        const QString &mimeFilter,
+                        bool cropLayers = false);
+
+    virtual ~KisSaveGroupVisitor();
+
+public:
+
+    bool visit(KisNode* );
+
+    bool visit(KisPaintLayer *);
+
+    bool visit(KisAdjustmentLayer *);
+
+    bool visit(KisExternalLayer *);
+
+    bool visit(KisCloneLayer *);
+
+    bool visit(KisFilterMask *);
+
+    bool visit(KisTransformMask *);
+
+    bool visit(KisTransparencyMask *);
+
+    bool visit(KisGeneratorLayer * );
+
+    bool visit(KisSelectionMask* );
+
+    bool visit(KisGroupLayer *layer);
+
+private:
+
+    KisImageWSP m_image;
+    bool m_saveInvisible;
+    bool m_saveTopLevelOnly;
+    QUrl m_url;
+    QString m_baseName;
+    QString m_extension;
+    QString m_mimeFilter;
+    bool m_cropLayers;
+};
+
+
+#endif // KISSAVEGROUPVISITOR_H

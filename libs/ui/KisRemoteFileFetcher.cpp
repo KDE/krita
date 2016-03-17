0129982e9e93f1e27be6fea73d499f2e04f220cd
Boudewijn Rempt <boud@valdyas.org>
2016-03-17 19:31:27 +0100

    Restore the ability to D&D remote images
    This never worked on OSX or Windows because we don't have KIO there,
    and was broken on Linux when we removed our depedency on KIO. I know
    the eventloop implementation isn't a Good Thing, but it's better than
    nothing.
    
    BUG:60638

---
+++ libs/ui/KisRemoteFileFetcher.cpp
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2016 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#include "KisRemoteFileFetcher.h"
+
+#include <QApplication>
+#include <QMessageBox>
+#include <QTemporaryFile>
+
+#include <klocalizedstring.h>
+
+KisRemoteFileFetcher::KisRemoteFileFetcher(QObject *parent)
+    : QObject(parent)
+    , m_request(0)
+    , m_reply(0)
+{
+}
+
+KisRemoteFileFetcher::~KisRemoteFileFetcher()
+{
+    delete m_request;
+    delete m_reply;
+}
+
+bool KisRemoteFileFetcher::fetchFile(const QUrl &remote, QIODevice *io)
+{
+    Q_ASSERT(!remote.isLocalFile());
+
+    qDebug() << "going to fetch" << remote;
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    m_request = new QNetworkRequest(remote);
+    m_request->setRawHeader("User-Agent", QString("Krita-%1").arg(qApp->applicationVersion()).toUtf8());
+    m_reply = manager->get(*m_request);
+    connect(m_reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(serror(QNetworkReply::NetworkError)));
+    connect(m_reply, SIGNAL(finished()), &m_loop, SLOT(quit()));
+
+    // Wait until done
+    m_loop.exec();
+
+    if (m_reply->error() != QNetworkReply::NoError) {
+        QMessageBox::critical(0, i18nc("@title:window", "Krita"), QString("Could not download %1: %2").arg(remote.toDisplayString()).arg(m_reply->errorString()), QMessageBox::Ok);
+        return false;
+    }
+
+    if (!io->isOpen()) {
+        io->open(QIODevice::WriteOnly);
+    }
+    io->write(m_reply->readAll());
+    io->close();
+
+    return true;
+
+}
+
+void KisRemoteFileFetcher::downloadProgress(qint64 bytesReceived, qint64 bytesTotal)
+{
+    qDebug() << "bytesReceived" << bytesReceived << "bytesTotal" << bytesTotal;
+}
+
+void KisRemoteFileFetcher::error(QNetworkReply::NetworkError error)
+{
+    qDebug() << "error" << m_reply->errorString();
+    m_loop.quit();
+}

cc1315e39c8612c02b51cb2c6abc5bd775c9e16b
Boudewijn Rempt <boud@valdyas.org>
2016-06-17 15:12:32 +0200

    Allow execution of any python script with access to our api
    Inspired by a very cool book: http://valdyas.org/python/book.html

--- plugins/extensions/pykrita/plugin/plugins/scripter/scripter.py
+++ plugins/extensions/pykrita/plugin/plugins/scripter/scripter.py
@@ -2,36 +2,77 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from krita import *
 from scripter import syntax
-
+import sys
+      
+class docWrapper:
+    
+    def __init__(self, textdocument):
+        self.textdocument = textdocument
+        
+    def write(self, text, view = None):
+        cursor = QTextCursor(self.textdocument)
+        cursor.clearSelection()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+      
 class ScripterViewExtension(ViewExtension):
 
-  def __init__(self, parent):
-      super().__init__(parent)
+    def __init__(self, parent):
+        super().__init__(parent)
 
-  def setup(self, viewManager):
-      action = viewManager.createAction("Scripter")
-      action.triggered.connect(self.showScripter)
+    def setup(self, viewManager):
+        action = viewManager.createAction("Scripter")
+        action.triggered.connect(self.showScripter)
+        self.currentWindow = viewManager.mainWindow()
 
-      self.currentWindow = viewManager.mainWindow()
+    def execute(self):
+        stdout = sys.stdout
+        stderr = sys.stderr
+        output = docWrapper(self.output.document())
+        output.write("======================================\n")
+        sys.stdout = output
+        sys.stderr = output
+        script = self.editor.document().toPlainText()
+        try:
+            bc = compile(script, "<string>", "exec")
+        except Exception as e:
+            QMessageBox.critical(self.editor, "Error compiling script", str(e))
+            return
+        try:
+            exec(bc)
+        except Exception as e:
+            self.editor.document().setPlainText(e)
+            return
+        
+        sys.stdout = stdout
+        sys.stderr = stderr
 
-  def showScripter(self):
-      dialog = QDialog(self.currentWindow)
-      dialog.setWindowModality(Qt.NonModal)
+    def showScripter(self):
+        dialog = QDialog(self.currentWindow)
+        dialog.setWindowModality(Qt.NonModal)
 
-      editor = QPlainTextEdit()
-      f = QFont("monospace", 10, QFont.Normal)
-      f.setFixedPitch(True)
-      editor.document().setDefaultFont(f)
-      highlight = syntax.PythonHighlighter(editor.document())
+        self.editor = QPlainTextEdit()
+        f = QFont("monospace", 10, QFont.Normal)
+        f.setFixedPitch(True)
+        self.editor.document().setDefaultFont(f)
+        highlight = syntax.PythonHighlighter(self.editor.document())
 
-      vbox = QVBoxLayout(dialog)
-      vbox.addWidget(editor)
-
-      dialog.resize(350, 200)
-      dialog.setWindowTitle("Scripter")
-      dialog.setSizeGripEnabled(True)
-      dialog.show()
-      dialog.activateWindow()
+        vbox = QVBoxLayout(dialog)
+        vbox.addWidget(self.editor)
+        
+        button = QPushButton("Execute")
+        button.clicked.connect(self.execute)
+        vbox.addWidget(button)
+        
+        self.output = QPlainTextEdit()
+        vbox.addWidget(self.output)
+        
+        dialog.resize(400, 500)
+        dialog.setWindowTitle("Scripter")
+        dialog.setSizeGripEnabled(True)
+        dialog.show()
+        dialog.activateWindow()
+    
 
 Krita.instance().addViewExtension(ScripterViewExtension(Krita.instance()))
 

bcfcd2fd63103e6e499f4969e5ddbd162d72f29f
Boudewijn Rempt <boud@valdyas.org>
2016-06-23 08:42:54 +0200

    Add some apidox

--- libs/ui/tool/kis_resources_snapshot.h
+++ libs/ui/tool/kis_resources_snapshot.h
@@ -36,7 +36,8 @@ class KoPattern;
 /**
  * @brief The KisResourcesSnapshot class takes a snapshot of the various resources
  * like colors and settings used at the begin of a stroke or a recording so subsequent
- * changes don't impact the running stroke.
+ * changes don't impact the running stroke. The main reason for the snapshot is that the
+ * user can *change* the options while the stroke is being executed in the background.
  */
 class KRITAUI_EXPORT KisResourcesSnapshot : public KisShared
 {

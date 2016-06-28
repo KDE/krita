7af8ee893a7b157b6306c364e681778db3100ded
Boudewijn Rempt <boud@valdyas.org>
2016-06-28 16:34:35 +0200

    Fully port the jpeg filter to the configuration api

--- plugins/impex/jpeg/kis_jpeg_export.h
+++ plugins/impex/jpeg/kis_jpeg_export.h
@@ -23,6 +23,24 @@
 #include <QVariant>
 
 #include <KisImportExportFilter.h>
+#include <kis_config_widget.h>
+#include "ui_kis_wdg_options_jpeg.h"
+#include <metadata/kis_meta_data_store.h>
+#include <metadata/kis_meta_data_filter_registry_model.h>
+
+
+class KisWdgOptionsJPEG : public KisConfigWidget, public Ui::WdgOptionsJPEG
+{
+    Q_OBJECT
+
+public:
+    KisWdgOptionsJPEG(QWidget *parent);
+    void setConfiguration(const KisPropertiesConfigurationSP  cfg);
+    KisPropertiesConfigurationSP configuration() const;
+private:
+    KisMetaData::FilterRegistryModel m_filterRegistryModel;
+};
+
 
 class KisJPEGExport : public KisImportExportFilter
 {

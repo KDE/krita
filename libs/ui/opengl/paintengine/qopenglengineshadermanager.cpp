060713f739b60b21ac73ada3c161850f5a7d9bab
Julian Thijssen <Nimthora@gmail.com>
2016-07-06 12:18:49 +0200

    Updated QOpenGLEngineShaderManager class to Qt 5.7.0

--- libs/ui/opengl/paintengine/qopenglengineshadermanager.cpp
+++ libs/ui/opengl/paintengine/qopenglengineshadermanager.cpp
@@ -1,31 +1,37 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL21$
+** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
 **
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
 **
 ** $QT_END_LICENSE$
 **
@@ -38,6 +44,8 @@
 
 #include <QtGui/private/qopenglcontext_p.h>
 #include <QtCore/qthreadstorage.h>
+
+#include <algorithm>
 
 #if defined(QT_DEBUG)
 #include <QMetaEnum>
@@ -191,7 +199,7 @@ QOpenGLEngineSharedShaders::QOpenGLEngineSharedShaders(QOpenGLContext* context)
 #if defined(QT_DEBUG)
         // Check that all the elements have been filled:
         for (int i = 0; i < TotalSnippetCount; ++i) {
-            if (qShaderSnippets[i] == 0) {
+            if (Q_UNLIKELY(!qShaderSnippets[i])) {
                 qFatal("Shader snippet for %s (#%d) is missing!",
                        snippetNameStr(SnippetName(i)).constData(), i);
             }
@@ -240,11 +248,11 @@ QOpenGLEngineSharedShaders::QOpenGLEngineSharedShaders(QOpenGLContext* context)
 
     simpleShaderProg->link();
 
-    if (simpleShaderProg->isLinked()) {
+    if (Q_UNLIKELY(!simpleShaderProg->isLinked())) {
+        qCritical("Errors linking simple shader: %s", qPrintable(simpleShaderProg->log()));
+    } else {
         if (!inCache)
             simpleShaderCache.store(simpleShaderProg, context);
-    } else {
-        qCritical("Errors linking simple shader: %s", qPrintable(simpleShaderProg->log()));
     }
 
     // Compile the blit shader:
@@ -281,11 +289,11 @@ QOpenGLEngineSharedShaders::QOpenGLEngineSharedShaders(QOpenGLContext* context)
     }
 
     blitShaderProg->link();
-    if (blitShaderProg->isLinked()) {
+    if (Q_UNLIKELY(!blitShaderProg->isLinked())) {
+        qCritical("Errors linking blit shader: %s", qPrintable(blitShaderProg->log()));
+    } else {
         if (!inCache)
             blitShaderCache.store(blitShaderProg, context);
-    } else {
-        qCritical("Errors linking blit shader: %s", qPrintable(blitShaderProg->log()));
     }
 
 #ifdef QT_GL_SHARED_SHADER_DEBUG
@@ -469,15 +477,16 @@ QOpenGLEngineShaderProg *QOpenGLEngineSharedShaders::findProgramInCache(const QO
 
 void QOpenGLEngineSharedShaders::cleanupCustomStage(QOpenGLCustomShaderStage* stage)
 {
-    // Remove any shader programs which has this as the custom shader src:
-    for (int i = 0; i < cachedPrograms.size(); ++i) {
-        QOpenGLEngineShaderProg *cachedProg = cachedPrograms[i];
+    auto hasStageAsCustomShaderSouce = [stage](QOpenGLEngineShaderProg *cachedProg) -> bool {
         if (cachedProg->customStageSource == stage->source()) {
             delete cachedProg;
-            cachedPrograms.removeAt(i);
-            i--;
+            return true;
         }
-    }
+        return false;
+    };
+    cachedPrograms.erase(std::remove_if(cachedPrograms.begin(), cachedPrograms.end(),
+                                        hasStageAsCustomShaderSouce),
+                         cachedPrograms.end());
 }
 
 
@@ -678,7 +687,6 @@ bool QOpenGLEngineShaderManager::useCorrectShaderProg()
     requiredProgram.positionVertexShader = QOpenGLEngineSharedShaders::InvalidSnippetName;
     requiredProgram.srcPixelFragShader = QOpenGLEngineSharedShaders::InvalidSnippetName;
     bool isAffine = brushTransform.isAffine();
-
     if ( (srcPixelType >= Qt::Dense1Pattern) && (srcPixelType <= Qt::DiagCrossPattern) ) {
         if (isAffine) {
             requiredProgram.positionVertexShader = QOpenGLEngineSharedShaders::AffinePositionWithPatternBrushVertexShader; qDebug() << "using AffinePosWithPatternBrushVertShad"; }

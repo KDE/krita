1bbce59244d4b0573d9fec18997cc3007a50d1a6
Julian Thijssen <Nimthora@gmail.com>
2016-07-06 12:38:02 +0200

    Updated QOpenGLGradientCache class to Qt 5.7.0

--- libs/ui/opengl/paintengine/qopenglgradientcache.cpp
+++ libs/ui/opengl/paintengine/qopenglgradientcache.cpp
@@ -1,31 +1,37 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL21$
+** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
 **
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
 **
 ** $QT_END_LICENSE$
 **
@@ -34,10 +40,10 @@
 #include "qopenglgradientcache_p.h"
 #include <private/qdrawhelper_p.h>
 #include <private/qopenglcontext_p.h>
-#include "qrgba64_p.h"
+#include <private/qrgba64_p.h>
 #include <QtCore/qmutex.h>
-#include <qopenglfunctions.h>
-#include <private/qopenglextensions_p.h>
+#include "qopenglfunctions.h"
+#include "qopenglextensions_p.h"
 
 #ifndef GL_RGBA16
 #define GL_RGBA16   0x805B
@@ -145,17 +151,17 @@ GLuint QOpenGL2GradientCache::addCacheElement(quint64 hash_val, const QGradient 
     CacheInfo cache_entry(gradient.stops(), opacity, gradient.interpolationMode());
     funcs->glGenTextures(1, &cache_entry.texId);
     funcs->glBindTexture(GL_TEXTURE_2D, cache_entry.texId);
-//    if (static_cast<QOpenGLExtensions *>(funcs)->hasOpenGLExtension(QOpenGLExtensions::Sized16Formats)) {
-//        QRgba64 buffer[1024];
-//        generateGradientColorTable(gradient, buffer, paletteSize(), opacity);
-//        funcs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16, paletteSize(), 1,
-//                            0, GL_RGBA, GL_UNSIGNED_SHORT, buffer);
-//    } else {
+    if (static_cast<QOpenGLExtensions *>(funcs)->hasOpenGLExtension(QOpenGLExtensions::Sized16Formats)) {
+        QRgba64 buffer[1024];
+        generateGradientColorTable(gradient, buffer, paletteSize(), opacity);
+        funcs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16, paletteSize(), 1,
+                            0, GL_RGBA, GL_UNSIGNED_SHORT, buffer);
+    } else {
         uint buffer[1024];
         generateGradientColorTable(gradient, buffer, paletteSize(), opacity);
         funcs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, paletteSize(), 1,
                             0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-//    }
+    }
     return cache.insert(hash_val, cache_entry).value().texId;
 }
 
@@ -168,7 +174,7 @@ void QOpenGL2GradientCache::generateGradientColorTable(const QGradient& gradient
     QVector<QRgba64> colors(s.size());
 
     for (int i = 0; i < s.size(); ++i)
-        colors[i] = s[i].second.rgba();
+        colors[i] = s[i].second.rgba64();
 
     bool colorInterpolation = (gradient.interpolationMode() == QGradient::ColorInterpolation);
 

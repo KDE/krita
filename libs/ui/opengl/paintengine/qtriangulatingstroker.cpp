f9476314537e3e62d408f46a6cf842cf9b33a19d
Julian Thijssen <Nimthora@gmail.com>
2016-07-06 14:25:29 +0200

    Updated QTriangulatingStroker class to Qt 5.7.0

--- libs/ui/opengl/paintengine/qtriangulatingstroker.cpp
+++ libs/ui/opengl/paintengine/qtriangulatingstroker.cpp
@@ -1,31 +1,37 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL21$
+** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
 **
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
 **
 ** $QT_END_LICENSE$
 **
@@ -33,8 +39,6 @@
 
 #include "qtriangulatingstroker_p.h"
 #include <qmath.h>
-#include <QDebug>
-#include <cstdio>
 
 QT_BEGIN_NAMESPACE
 
@@ -80,7 +84,6 @@ void QTriangulatingStroker::process(const QVectorPath &path, const QPen &pen, co
     const qreal *pts = path.points();
     const QPainterPath::ElementType *types = path.elements();
     int count = path.elementCount();
-
     if (count < 2)
         return;
 
@@ -163,7 +166,6 @@ void QTriangulatingStroker::process(const QVectorPath &path, const QPen &pen, co
 
         if (endsAtStart || path.hasImplicitClose())
             m_cap_style = Qt::FlatCap;
-
         moveTo(pts);
         m_cap_style = cap;
         pts += 2;
@@ -171,15 +173,12 @@ void QTriangulatingStroker::process(const QVectorPath &path, const QPen &pen, co
         lineTo(pts);
         pts += 2;
         skipDuplicatePoints(&pts, endPts);
-
         while (pts < endPts) {
             join(pts);
             lineTo(pts);
-
             pts += 2;
             skipDuplicatePoints(&pts, endPts);
         }
-
         endCapOrJoinClosed(startPts, pts-2, path.hasImplicitClose(), endsAtStart);
 
     } else {
@@ -261,7 +260,8 @@ void QTriangulatingStroker::moveTo(const qreal *pts)
     float y2 = pts[3];
     normalVector(m_cx, m_cy, x2, y2, &m_nvx, &m_nvy);
 
-    // To achieve jumps we insert zero-area tringles. This is done by
+
+    // To achieve jumps we insert zero-area triangles. This is done by
     // adding two identical points in both the end of previous strip
     // and beginning of next strip
     bool invisibleJump = m_vertices.size();
@@ -610,3 +610,4 @@ void QDashedStrokeProcessor::process(const QVectorPath &path, const QPen &pen, c
 }
 
 QT_END_NAMESPACE
+

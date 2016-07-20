02a408c87de6baff467f3139b4d8320d549d967a
Boudewijn Rempt <boud@valdyas.org>
2016-07-20 15:49:29 +0200

    Add desktop integration

--- packaging/linux/appimage/build-image.sh
+++ packaging/linux/appimage/build-image.sh
@@ -239,6 +239,15 @@ AppImageKit/AppImageAssistant.AppDir/package /krita.appdir/ /out/$APPIMAGE
 
 chmod a+rwx /out/$APPIMAGE # So that we can edit the AppImage outside of the Docker container
 
+# Source functions
+wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh
+-O ./functions.sh
+. ./functions.sh
+
+# Install desktopintegration in usr/bin/krita.wrapper -- feel free to edit it
+cd Krita.AppDir/
+get_desktopintegration krita
+
 cd /krita.appdir
 mv AppRun krita
 cd /

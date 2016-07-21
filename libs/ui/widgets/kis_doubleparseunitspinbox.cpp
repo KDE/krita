59d68b9d5fe64316c23f1775812940384ff6e7af
Laurent Valentin Jospin <laurent.valentin@famillejospin.ch>
2016-07-21 08:26:29 +0200

    Create a replacement for KoUnitSpinBox that is able to parse math expression.
    This class is more or less a copy of the KoUnitSpinBox class, but that inherit KisDoubleParseSpinBox instead of QDoubleSpinBox.
    
    It was impossible to use classes or functions from the ui library in the widget library du to dependance graph.
    So the best solution was to recreate the class in the ui lib.
    
    This class should be a base of work for the future conversions of units functions we want to add in the parser.
    From the discussion we had it will continue to be able to display the unit as a suffix,
    but should also be able to link with a combobox with a list of units. (still alowing to change the current unit on the keyboard).
    
    Tests where performed with the flake plugin.

---
+++ libs/ui/widgets/kis_doubleparseunitspinbox.cpp
@@ -0,0 +1,172 @@
+/*
+ *  Copyright (c) 2016 Laurent Valentin Jospin <laurent.valentin@famillejospin.ch>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "kis_doubleparseunitspinbox.h"
+
+
+class Q_DECL_HIDDEN KisDoubleParseUnitSpinBox::Private
+{
+public:
+	Private(double low, double up, double step)
+		: lowerInPoints(low),
+		upperInPoints(up),
+		stepInPoints(step),
+		unit(KoUnit(KoUnit::Point))
+	{
+	}
+
+	double lowerInPoints; ///< lowest value in points
+	double upperInPoints; ///< highest value in points
+	double stepInPoints;  ///< step in points
+	KoUnit unit;
+};
+
+KisDoubleParseUnitSpinBox::KisDoubleParseUnitSpinBox(QWidget *parent) :
+	KisDoubleParseSpinBox(parent),
+	d(new Private(-9999, 9999, 1))
+{
+	setUnit( KoUnit(KoUnit::Point) );
+	setAlignment( Qt::AlignRight );
+
+	connect(this, SIGNAL(valueChanged( double )), SLOT(privateValueChanged()));
+}
+
+KisDoubleParseUnitSpinBox::~KisDoubleParseUnitSpinBox()
+{
+	delete d;
+}
+
+void KisDoubleParseUnitSpinBox::changeValue( double newValue )
+{
+	if (d->unit.toUserValue(newValue) == _oldValue) {
+		return;
+	}
+
+	KisDoubleParseSpinBox::setValue( d->unit.toUserValue(newValue) );
+}
+
+void KisDoubleParseUnitSpinBox::setUnit( const KoUnit & unit)
+{
+	if( unit == d->unit) return;
+	double oldValue = d->unit.fromUserValue( KisDoubleParseSpinBox::value() );
+
+	KisDoubleParseSpinBox::setMinimum( unit.toUserValue( d->lowerInPoints ) );
+	KisDoubleParseSpinBox::setMaximum( unit.toUserValue( d->upperInPoints ) );
+
+	qreal step = unit.toUserValue( d->stepInPoints );
+
+	if (unit.type() == KoUnit::Pixel) {
+		// limit the pixel step by 1.0
+		step = qMax(qreal(1.0), step);
+	}
+
+	KisDoubleParseSpinBox::setSingleStep( step );
+	d->unit = unit;
+	KisDoubleParseSpinBox::setValue( KoUnit::ptToUnit( oldValue, unit ) );
+	setSuffix( unit.symbol().prepend(QLatin1Char(' ')) );
+}
+
+double KisDoubleParseUnitSpinBox::value( ) const
+{
+	return d->unit.fromUserValue( KisDoubleParseSpinBox::value() );
+}
+
+void KisDoubleParseUnitSpinBox::setMinimum(double min)
+{
+	d->lowerInPoints = min;
+	KisDoubleParseSpinBox::setMinimum( d->unit.toUserValue( min ) );
+}
+
+void KisDoubleParseUnitSpinBox::setMaximum(double max)
+{
+	d->upperInPoints = max;
+	KisDoubleParseSpinBox::setMaximum( d->unit.toUserValue( max ) );
+}
+
+void KisDoubleParseUnitSpinBox::setLineStep(double step)
+{
+	d->stepInPoints = KoUnit(KoUnit::Point).toUserValue(step);
+	KisDoubleParseSpinBox::setSingleStep( step );
+}
+
+void KisDoubleParseUnitSpinBox::setLineStepPt(double step)
+{
+	d->stepInPoints = step;
+	KisDoubleParseSpinBox::setSingleStep( d->unit.toUserValue( step ) );
+}
+
+
+void KisDoubleParseUnitSpinBox::setMinMaxStep( double min, double max, double step )
+{
+	setMinimum( min );
+	setMaximum( max );
+	setLineStepPt( step );
+}
+
+
+QValidator::State KisDoubleParseUnitSpinBox::validate(QString &input, int &pos) const
+{
+	Q_UNUSED(pos);
+
+	QRegExp regexp ("([ a-zA-Z]+)$"); // Letters or spaces at end
+	const int res = input.indexOf( regexp );
+
+	if ( res == -1 ) {
+		// Nothing like an unit? The user is probably editing the unit
+		return QValidator::Intermediate;
+	}
+
+	QString expr ( input.left( res ) );
+	const QString unitName ( regexp.cap( 1 ).trimmed().toLower() );
+
+	bool ok = true;
+	bool interm = false;
+
+	QValidator::State exprState = KisDoubleParseSpinBox::validate(expr, pos);
+
+	if (exprState == QValidator::Invalid) {
+		return exprState;
+	} else if (exprState == QValidator::Intermediate) {
+		interm = true;
+	}
+
+	//check if we can parse the unit.
+	KoUnit::fromSymbol(unitName, &ok);
+
+	if (!ok || interm) {
+		return QValidator::Intermediate;
+	}
+
+	return QValidator::Acceptable;
+}
+
+QString KisDoubleParseUnitSpinBox::textFromValue( double value ) const
+{
+	return KisDoubleParseSpinBox::textFromValue(value);
+}
+
+double KisDoubleParseUnitSpinBox::valueFromText( const QString& str ) const
+{
+	//KisDoubleParseSpinBox is supposed to remove the suffix and prefix by itself.
+	return KisDoubleParseSpinBox::valueFromText(str);
+}
+
+void KisDoubleParseUnitSpinBox::privateValueChanged()
+{
+	emit valueChangedPt( value() );
+}

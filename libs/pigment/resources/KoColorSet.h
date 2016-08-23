e0ed2fd7586e068121e22cbea31a4b7d82379c99
Wolthera van Hovell tot Westerflier <griffinvalley@gmail.com>
2016-08-23 10:29:40 +0200

    Add the ability to let the layer split use color names from a palette.
    This also implements two 'closest match' functions in kocolorset. One for the name, the other only for the index.
    
    BUG:348837

--- libs/pigment/resources/KoColorSet.h
+++ libs/pigment/resources/KoColorSet.h
@@ -85,6 +85,26 @@ public:
     KoColorSetEntry getColor(quint32 index);
     qint32 nColors();
 
+    /**
+     * @brief getIndexClosestColor
+     * function that matches the color to all colors in the colorset, and returns the index
+     * of the closest match.
+     * @param color the color you wish to compare.
+     * @param useGivenColorSpace whether to use the color space of the color given
+     * when the two colors' colorspaces don't match. Else it'll use the entry's colorspace.
+     * @return returns the int of the closest match.
+     */
+    qint32 getIndexClosestColor(KoColor color, bool useGivenColorSpace = true);
+
+    /**
+     * @brief closestColorName
+     * convenience function to get the name of the closest match.
+     * @param color
+     * @param useGivenColorSpace
+     * @return
+     */
+    QString closestColorName(KoColor color, bool useGivenColorSpace = true);
+
 private:
 
 

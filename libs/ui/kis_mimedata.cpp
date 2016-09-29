8d3639a473a77a64c8e74d33e880611b2d4a6063
Boudewijn Rempt <boud@valdyas.org>
2016-09-29 10:37:36 +0200

    Don't use loadNativeFormat, but use a filter to load a kra file

--- libs/ui/kis_mimedata.cpp
+++ libs/ui/kis_mimedata.cpp
@@ -118,6 +118,7 @@ QByteArray serializeToByteArray(QList<KisNodeSP> nodes)
     if (filter->convert(doc, &buffer) != KisImportExportFilter::OK) {
         qWarning() << "serializeToByteArray():: Could not export to our native format";
     }
+    delete filter;
     delete doc;
     return byteArray;
 }
@@ -274,10 +275,12 @@ QList<KisNodeSP> KisMimeData::loadNodes(const QMimeData *data,
     QList<KisNodeSP> nodes;
 
     if (data->hasFormat("application/x-krita-node")) {
-        QByteArray ba = data->data("application/x-krita-node");
-
         KisDocument *tempDoc = KisPart::instance()->createDocument();
-        bool result = tempDoc->loadNativeFormatFromByteArray(ba);
+        QByteArray ba = data->data("application/x-krita-node");
+        QBuffer buf(&ba);
+        KisImportExportFilter *filter = tempDoc->importExportManager()->filterForMimeType(tempDoc->nativeFormatMimeType(), KisImportExportManager::Import);
+        filter->setBatchMode(true);
+        bool result = (filter->convert(tempDoc, &buf) == KisImportExportFilter::OK);
 
         if (result) {
             KisImageWSP tempImage = tempDoc->image();
@@ -287,15 +290,15 @@ QList<KisNodeSP> KisMimeData::loadNodes(const QMimeData *data,
                 initializeExternalNode(node, image, shapeController);
             }
         }
+        delete filter;
         delete tempDoc;
     }
 
     if (nodes.isEmpty() && data->hasFormat("application/x-krita-node-url")) {
         QByteArray ba = data->data("application/x-krita-node-url");
-        QString localFile = QUrl::fromEncoded(ba).toLocalFile();
-
         KisDocument *tempDoc = KisPart::instance()->createDocument();
-        bool result = tempDoc->loadNativeFormat(localFile);
+        Q_ASSERT(QUrl::fromEncoded(ba).isLocalFile());
+        bool result = tempDoc->openUrl(QUrl::fromEncoded(ba));
 
         if (result) {
             KisImageWSP tempImage = tempDoc->image();
@@ -306,8 +309,7 @@ QList<KisNodeSP> KisMimeData::loadNodes(const QMimeData *data,
             }
         }
         delete tempDoc;
-
-        QFile::remove(localFile);
+        QFile::remove(QUrl::fromEncoded(ba).toLocalFile());
     }
 
     if (nodes.isEmpty() && data->hasImage()) {

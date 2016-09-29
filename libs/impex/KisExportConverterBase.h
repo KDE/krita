14ee3a70c9c75776bed9f9945e669eef448c608b
Boudewijn Rempt <boud@valdyas.org>
2016-09-29 13:52:56 +0200

    Add initial framework for the export capabilities checker
    See https://phabricator.kde.org/T3535 for the design

---
+++ libs/impex/KisExportConverterBase.h
@@ -0,0 +1,41 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2016 Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef KISEXPORTCONVERTERBASE_H
+#define KISEXPORTCONVERTERBASE_H
+
+#include <kis_types.h>
+#include "kritaimpex_export.h"
+
+/**
+ * @brief The KisExportConverterBase class defines the interface
+ * of converter classes that can convert an aspect of an image
+ * before exporting.
+ */
+class KRITAIMPEX_EXPORT KisExportConverterBase
+{
+public:
+    KisExportConverterBase();
+    virtual ~KisExportConverterBase();
+    virtual QString id() const = 0;
+    virtual bool convert(KisImageSP image) const = 0;
+    virtual int priority() const = 0;
+
+};
+
+#endif // KISEXPORTCONVERTERBASE_H
---
+++ libs/impex/KisPreExportChecker.cpp
@@ -0,0 +1,79 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2016 Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "KisPreExportChecker.h"
+#include "KisExportCheckBase.h"
+#include "KisExportConverterBase.h"
+
+#include <kis_image.h>
+
+KisPreExportChecker::KisPreExportChecker()
+{
+    initializeChecks();
+}
+
+void KisPreExportChecker::check(KisImageSP image, QMap<QString, KisExportCheckBase*> filterChecks)
+{
+    Q_FOREACH(const KisExportCheckBase *check, m_checks) {
+        if (check->checkNeeded(image)) {
+            if (!filterChecks.contains(check->id())) {
+                m_warnings << check->message();
+            }
+            else if (filterChecks[check->id()]->check(image) != KisExportCheckBase::SUPPORTED) {
+                m_warnings << filterChecks[check->id()]->message();
+            }
+            else {
+                continue;
+            }
+            if (check->converter()) {
+                m_conversions[check->converter()->id()] = check->converter();
+            }
+        }
+    }
+}
+
+KisImageSP KisPreExportChecker::convertedImage(KisImageSP originalImage) const
+{
+    // Sort the conversions on priority
+    // Copy the image
+    // KisImageSP copy = new KisImage(originalImage.data());
+    // Apply the conversions
+    Q_FOREACH(KisExportConverterBase *converter, m_conversions.values()) {
+        if (!converter->convert(originalImage)) {
+            qDebug() << "Failed to apply conversion" << converter->id();
+            return 0;
+        }
+    }
+    return originalImage;
+}
+
+QStringList KisPreExportChecker::warnings() const
+{
+    return m_warnings;
+}
+
+bool KisPreExportChecker::conversionNeeded() const
+{
+    return !m_conversions.isEmpty();
+}
+
+void KisPreExportChecker::initializeChecks()
+{
+    // ...
+}

3d39570435ede7501c0fc2bb891129a7479a5639
Boudewijn Rempt <boud@valdyas.org>
2016-09-30 10:54:49 +0200

    Remove old openraster capabilities check from the kra export plugin

--- plugins/impex/kra/kra_export.cpp
+++ plugins/impex/kra/kra_export.cpp
@@ -54,53 +54,13 @@ KraExport::~KraExport()
 {
 }
 
-
-bool hasShapeLayerChild(KisNodeSP node)
-{
-    if (!node) return false;
-
-    Q_FOREACH (KisNodeSP child, node->childNodes(QStringList(), KoProperties())) {
-        if (child->inherits("KisShapeLayer")
-                || child->inherits("KisGeneratorLayer")
-                || child->inherits("KisCloneLayer")) {
-            return true;
-        }
-        else {
-            if (hasShapeLayerChild(child)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 KisImportExportFilter::ConversionStatus KraExport::convert(KisDocument *document, QIODevice *io,  KisPropertiesConfigurationSP /*configuration*/)
 {
     KisImageWSP image = document->image();
     Q_CHECK_PTR(image);
 
     KisPaintDeviceSP pd = image->projection();
-    QStringList supportedColorModelIds;
-    supportedColorModelIds << RGBAColorModelID.id() << GrayAColorModelID.id() << GrayColorModelID.id();
-    QStringList supportedColorDepthIds;
-    supportedColorDepthIds << Integer8BitsColorDepthID.id() << Integer16BitsColorDepthID.id();
-    if (!supportedColorModelIds.contains(pd->colorSpace()->colorModelId().id()) ||
-            !supportedColorDepthIds.contains(pd->colorSpace()->colorDepthId().id())) {
-        if (!batchMode()) {
-            QMessageBox::critical(0, i18nc("@title:window", "Krita OpenRaster Export"), i18n("Cannot export images in this colorspace or channel depth to OpenRaster"));
-        }
-        return KisImportExportFilter::UsageError;
-    }
-
-
-    if (hasShapeLayerChild(image->root()) && !batchMode()) {
-        QMessageBox::information(0,
-                                 i18nc("@title:window", "Krita:Warning"),
-                                 i18n("This image contains vector, clone or fill layers.\nThese layers will be saved as raster layers."));
-    }
-
     KraConverter kraConverter(document);
-
     KisImageBuilder_Result res = kraConverter.buildFile(io);
 
     if (res == KisImageBuilder_RESULT_OK) {

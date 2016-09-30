56d5ad1a62501846180f3ad4d44022a2fe9c8f95
Boudewijn Rempt <boud@valdyas.org>
2016-09-30 17:22:45 +0200

    Define the capabilities of the PSD export filter

--- plugins/impex/psd/psd_export.cc
+++ plugins/impex/psd/psd_export.cc
@@ -27,7 +27,8 @@
 
 #include <KisExportCheckRegistry.h>
 #include <KisImportExportManager.h>
-
+#include <ImageSizeCheck.h>
+#include <KoColorSpaceRegistry.h>
 #include <KoColorSpaceConstants.h>
 
 #include <KisDocument.h>
@@ -110,6 +111,55 @@ KisImportExportFilter::ConversionStatus psdExport::convert(KisDocument *document
 void psdExport::initializeCapabilities()
 {
     addCapability(KisExportCheckRegistry::instance()->get("PSDLayerStyleCheck")->create(KisExportCheckBase::SUPPORTED));
+    addCapability(KisExportCheckRegistry::instance()->get("NodeTypeCheck/KisGroupLayer")->create(KisExportCheckBase::SUPPORTED));
+    addCapability(KisExportCheckRegistry::instance()->get("MultiLayerCheck")->create(KisExportCheckBase::SUPPORTED));
+    addCapability(KisExportCheckRegistry::instance()->get("sRGBProfileCheck")->create(KisExportCheckBase::SUPPORTED));
+    addCapability(KisExportCheckRegistry::instance()->get("MultiLayerCheck")->create(KisExportCheckBase::SUPPORTED));
+    addCapability(KisExportCheckRegistry::instance()->get("ColorModelHomogenousCheck")->create(KisExportCheckBase::UNSUPPORTED));
+
+    ImageSizeCheckFactory *factory = dynamic_cast<ImageSizeCheckFactory*>(KisExportCheckRegistry::instance()->get("ImageSizeCheck"));
+    if (factory) {
+        addCapability(factory->create(30000, 30000, KisExportCheckBase::SUPPORTED));
+    }
+
+    QList<KoID> supportedColorModels;
+    supportedColorModels << RGBAColorModelID << GrayAColorModelID << LABAColorModelID << CMYKAColorModelID;
+    QList<KoID> supportedColorDepths;
+    supportedColorDepths << Float32BitsColorDepthID << Float16BitsColorDepthID << Integer8BitsColorDepthID << Integer16BitsColorDepthID;
+
+    QList<KoID> allColorModels = KoColorSpaceRegistry::instance()->colorModelsList(KoColorSpaceRegistry::AllColorSpaces);
+    Q_FOREACH(const KoID &colorModelID, allColorModels) {
+        QList<KoID> allColorDepths = KoColorSpaceRegistry::instance()->colorDepthList(colorModelID.id(), KoColorSpaceRegistry::AllColorSpaces);
+        Q_FOREACH(const KoID &colorDepthID, allColorDepths) {
+
+            KisExportCheckFactory *f1 = KisExportCheckRegistry::instance()->get("ColorModelCheck/" + colorModelID.id() + "/" + colorDepthID.id());
+            KisExportCheckFactory *f2 = KisExportCheckRegistry::instance()->get("ColorModelPerLayerCheck/" + colorModelID.id() + "/" + colorDepthID.id());
+
+            if(!f1 || !f2) {
+                qDebug() << "No factory for" << colorModelID << colorDepthID;
+                continue;
+            }
+
+            if (supportedColorModels.contains(colorModelID) && supportedColorDepths.contains(colorDepthID)) {
+                addCapability(f1->create(KisExportCheckBase::SUPPORTED));
+                addCapability(f2->create(KisExportCheckBase::SUPPORTED));
+            }
+            else {
+                addCapability(f1->create(KisExportCheckBase::UNSUPPORTED,
+                                         i18nc("image conversion warning",
+                                               "EXR cannot save images with color model <b>%1</b> and depth <b>%2</b>. The image will not be saved.",
+                                               colorModelID.name(),
+                                               colorDepthID.name())));
+
+                addCapability(f2->create(KisExportCheckBase::UNSUPPORTED,
+                                        i18nc("image conversion warning",
+                                              "EXR cannot save layers with color model <b>%1</b> and depth <b>%2</b>. The layers will be skipped.",
+                                              colorModelID.name(),
+                                              colorDepthID.name())));
+            }
+        }
+    }
+
 }
 
 #include <psd_export.moc>

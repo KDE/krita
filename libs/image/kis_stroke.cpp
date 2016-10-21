KDE/krita

commit e357da4f04ea759975740a603f21e736691e6eec

Fix the artifacts when doing quick hatches with Instant Preview
When the strokes queue reaches the resume stroke ans starts
its execution, no Lod0 can execute anymore. So all the new Lod0
strokes should go to the end of the queue and wrapped into their
own Suspend/Resume pair

Fixes T2542

libs/image/kis_suspend_projection_updates_stroke_strategy.cpp
@@ -146,6 +146,16 @@ KisSuspendProjectionUpdatesStrokeStrategy::KisSuspendProjectionUpdatesStrokeStra
     m_d->image = image;
     m_d->suspend = suspend;
 
+
+    /**
+     * Here we add a dumb INIT job so that KisStrokesQueue would know that the
+     * stroke has already started or not. When the queue reaches the resume
+     * stroke ans starts its execution, no Lod0 can execute anymore. So all the
+     * new Lod0 strokes should go to the end of the queue and wrapped into
+     * their own Suspend/Resume pair.
+     */
+    enableJob(JOB_INIT, true);
+
     enableJob(JOB_DOSTROKE, true);
     enableJob(JOB_CANCEL, true);
 }

libs/image/kis_strokes_queue.cpp
@@ -141,6 +141,15 @@ bool KisStrokesQueue::Private::shouldWrapInSuspendUpdatesStroke() const
         if (stroke->isCancelled()) continue;
 
         if (stroke->type() == KisStroke::RESUME) {
+
+            /**
+             * Resuming process is long-running and consists of
+             * multiple actions, therefore, if it has already started,
+             * we cannot use it to guard our new stroke, so just skip it.
+             * see https://phabricator.kde.org/T2542
+             */
+            if (stroke->isInitialized()) continue;
+
             return false;
         }
     }
@@ -157,6 +166,9 @@ StrokesQueueIterator KisStrokesQueue::Private::findNewLod0Pos()
         if ((*it)->isCancelled()) continue;
 
         if ((*it)->type() == KisStroke::RESUME) {
+            // \see a comment in shouldWrapInSuspendUpdatesStroke()
+            if ((*it)->isInitialized()) continue;
+
             return it;
         }
     }
@@ -171,6 +183,14 @@ StrokesQueueIterator KisStrokesQueue::Private::findNewLodNPos(KisStrokeSP lodN)
 
     for (; it != end; ++it) {
         if ((*it)->isCancelled()) continue;
+
+        if (((*it)->type() == KisStroke::SUSPEND ||
+             (*it)->type() == KisStroke::RESUME) &&
+            (*it)->isInitialized()) {
+
+            // \see a comment in shouldWrapInSuspendUpdatesStroke()
+            continue;
+        }
 
         if ((*it)->type() == KisStroke::LOD0 ||
             (*it)->type() == KisStroke::SUSPEND ||

commit 2957819bdae2c46617d40b25a4b5e82a81c9f872

Fix the crash when trying to cancel the background strokes with Esc
The RESUME strategy should always have an explicit Cancel job that
reverts the updates filter.

This is a regression after: e357da4f04ea759975740a603f21e736691e6eec

libs/image/kis_stroke.cpp
@@ -142,7 +142,10 @@ void KisStroke::cancelStroke()
     // case 6
     if (m_isCancelled) return;
 
-    if(!m_strokeInitialized) {
+    const bool effectivelyInitialized =
+        m_strokeInitialized || m_strokeStrategy->needsExplicitCancel();
+
+    if(!effectivelyInitialized) {
         /**
          * Lod0 stroke cannot be suspended and !initialized at the
          * same time, because the suspend job is created iff the
@@ -157,7 +160,7 @@ void KisStroke::cancelStroke()
                                 sanityCheckAllJobsAreCancellable());
         clearQueueOnCancel();
     }
-    else if(m_strokeInitialized &&
+    else if(effectivelyInitialized &&
             (!m_jobsQueue.isEmpty() || !m_strokeEnded)) {
 
         clearQueueOnCancel();

libs/image/kis_stroke_strategy.cpp
@@ -29,6 +29,7 @@ KisStrokeStrategy::KisStrokeStrategy(QString id, const KUndo2MagicString &name)
       m_clearsRedoOnStart(true),
       m_requestsOtherStrokesToEnd(true),
       m_canForgetAboutMe(false),
+      m_needsExplicitCancel(false),
       m_id(id),
       m_name(name)
 {
@@ -41,7 +42,8 @@ KisStrokeStrategy::KisStrokeStrategy(const KisStrokeStrategy &rhs)
       m_indirectPaintingCompositeOp(rhs.m_indirectPaintingCompositeOp),
       m_clearsRedoOnStart(rhs.m_clearsRedoOnStart),
       m_requestsOtherStrokesToEnd(rhs.m_requestsOtherStrokesToEnd),
-      m_canForgetAboutMe(false),
+      m_canForgetAboutMe(rhs.m_canForgetAboutMe),
+      m_needsExplicitCancel(rhs.m_needsExplicitCancel),
       m_id(rhs.m_id),
       m_name(rhs.m_name)
 {
@@ -193,4 +195,14 @@ bool KisStrokeStrategy::canForgetAboutMe() const
 void KisStrokeStrategy::setCanForgetAboutMe(bool value)
 {
     m_canForgetAboutMe = value;
+}
+
+bool KisStrokeStrategy::needsExplicitCancel() const
+{
+    return m_needsExplicitCancel;
+}
+
+void KisStrokeStrategy::setNeedsExplicitCancel(bool value)
+{
+    m_needsExplicitCancel = value;
 }

libs/image/kis_stroke_strategy.h
@@ -81,6 +81,8 @@ public:
      */
     bool canForgetAboutMe() const;
 
+    bool needsExplicitCancel() const;
+
     QString id() const;
     KUndo2MagicString name() const;
 
@@ -110,6 +112,7 @@ protected:
     void setClearsRedoOnStart(bool value);
     void setRequestsOtherStrokesToEnd(bool value);
     void setCanForgetAboutMe(bool value);
+    void setNeedsExplicitCancel(bool value);
 
 protected:
     /**
@@ -125,6 +128,7 @@ private:
     bool m_clearsRedoOnStart;
     bool m_requestsOtherStrokesToEnd;
     bool m_canForgetAboutMe;
+    bool m_needsExplicitCancel;
 
     QString m_id;
     KUndo2MagicString m_name;

libs/image/kis_suspend_projection_updates_stroke_strategy.cpp
@@ -158,6 +158,8 @@ KisSuspendProjectionUpdatesStrokeStrategy::KisSuspendProjectionUpdatesStrokeStra
 
     enableJob(JOB_DOSTROKE, true);
     enableJob(JOB_CANCEL, true);
+
+    setNeedsExplicitCancel(true);
 }
 
 KisSuspendProjectionUpdatesStrokeStrategy::~KisSuspendProjectionUpdatesStrokeStrategy()
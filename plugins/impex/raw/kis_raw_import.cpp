7cc997781c2091dc27583ea02b636d9d671ae498
Frederik Gladhorn <gladhorn@kde.org>
2016-10-23 18:43:50 +0200

    Avoid detaching: use QByteArray::constData instead of data
    Calling data() is expensive since it detaches, creating a copy of the
    data it points to.

--- plugins/impex/raw/kis_raw_import.cpp
+++ plugins/impex/raw/kis_raw_import.cpp
@@ -135,7 +135,7 @@ KisImportExportFilter::ConversionStatus KisRawImport::convert(const QByteArray& 
         for (int y = 0; y < height; ++y) {
             do {
                 KoBgrU16Traits::Pixel* pixel = reinterpret_cast<KoBgrU16Traits::Pixel*>(it->rawData());
-                quint16* ptr = ((quint16*)imageData.data()) + (y * width + it->x()) * 3;
+                quint16* ptr = ((quint16*)imageData.constData()) + (y * width + it->x()) * 3;
 #if KDCRAW_VERSION < 0x000400
                 pixel->red = correctIndian(ptr[2]);
                 pixel->green = correctIndian(ptr[1]);
@@ -171,7 +171,7 @@ void KisRawImport::slotUpdatePreview()
         for (int y = 0; y < height; ++y) {
             QRgb *pixel= reinterpret_cast<QRgb *>(image.scanLine(y));
             for (int x = 0; x < width; ++x) {
-                quint8* ptr = ((quint8*)imageData.data()) + (y * width + x) * 3;
+                quint8* ptr = ((quint8*)imageData.constData()) + (y * width + x) * 3;
                 pixel[x] = qRgb(ptr[0], ptr[1], ptr[2]);
             }
         }

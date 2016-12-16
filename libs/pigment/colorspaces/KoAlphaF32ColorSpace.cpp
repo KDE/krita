c9c371ab5bfbafcbfcaad98908775c716e436954
Boudewijn Rempt <boud@valdyas.org>
2016-12-16 12:09:22 +0100

    Remove some more not-implemented functionality from alphaf32

--- libs/pigment/colorspaces/KoAlphaF32ColorSpace.cpp
+++ libs/pigment/colorspaces/KoAlphaF32ColorSpace.cpp
@@ -97,20 +97,20 @@ QString KoAlphaF32ColorSpace::normalisedChannelValueText(const quint8 *pixel, qu
 
 void KoAlphaF32ColorSpace::convolveColors(quint8** colors, qreal * kernelValues, quint8 *dst, qreal factor, qreal offset, qint32 nColors, const QBitArray & channelFlags) const
 {
-    qreal totalAlpha = 0;
+//    qreal totalAlpha = 0;
 
-    while (nColors--) {
-        qreal weight = *kernelValues;
+//    while (nColors--) {
+//        qreal weight = *kernelValues;
 
-        if (weight != 0) {
-            totalAlpha += (*colors)[0] * weight;
-        }
-        ++colors;
-        ++kernelValues;
-    }
+//        if (weight != 0) {
+//            totalAlpha += (*colors)[0] * weight;
+//        }
+//        ++colors;
+//        ++kernelValues;
+//    }
 
-    if (channelFlags.isEmpty() || channelFlags.testBit(0))
-        dst[0] = CLAMP((totalAlpha / factor) + offset, 0, SCHAR_MAX);
+//    if (channelFlags.isEmpty() || channelFlags.testBit(0))
+//        dst[0] = CLAMP((totalAlpha / factor) + offset, 0, SCHAR_MAX);
 }
 
 
@@ -120,16 +120,16 @@ QImage KoAlphaF32ColorSpace::convertToQImage(const quint8 *data, qint32 width, q
                                           KoColorConversionTransformation::ConversionFlags /*conversionFlags*/) const
 {
     QImage img(width, height, QImage::Format_Indexed8);
-    QVector<QRgb> table;
-    for (int i = 0; i < 256; ++i) table.append(qRgb(i, i, i));
-    img.setColorTable(table);
+//    QVector<QRgb> table;
+//    for (int i = 0; i < 256; ++i) table.append(qRgb(i, i, i));
+//    img.setColorTable(table);
 
-    quint8* data_img;
-    for (int i = 0; i < height; ++i) {
-        data_img=img.scanLine(i);
-        for (int j = 0; j < width; ++j)
-            data_img[j]=*(data++);
-    }
+//    quint8* data_img;
+//    for (int i = 0; i < height; ++i) {
+//        data_img=img.scanLine(i);
+//        for (int j = 0; j < width; ++j)
+//            data_img[j]=*(data++);
+//    }
 
     return img;
 }

3091f39a5e96ee73e6a156acf3ce64bd744c7df8
Boudewijn Rempt <boud@valdyas.org>
2017-01-22 15:38:04 +0100

    BUG:375253 Don't set the brush dab rotation to NaN.
    This follows up on the previous commit (ac257511dbec0a3191bbfdde9d886b70ec6ad574),
    which had the wrong header... It wasn't a bug in QTransform, because
    if you set the transform's rotation to NaN, sure, it'll create an
    invalid rectangle.

--- plugins/paintops/libpaintop/kis_curve_option.h
+++ plugins/paintops/libpaintop/kis_curve_option.h
@@ -118,10 +118,12 @@ public:
             const qreal realScalingPart = hasScaling ? KisDynamicSensor::scalingToAdditive(scaling) : 0.0;
             const qreal realAdditivePart = hasAdditive ? additive : 0;
 
-            return
-                wrapInRange(
-                    2 * offset + constant * realScalingPart + realAdditivePart,
-                    -1.0, 1.0);
+            qreal value = wrapInRange(2 * offset + constant * realScalingPart + realAdditivePart, -1.0, 1.0);
+            if (qIsNaN(value)) {
+                qWarning() << "rotationLikeValue returns NaN!" << normalizedBaseAngle << absoluteAxesFlipped;
+                value = 0;
+            }
+            return value;
         }
 
         qreal sizeLikeValue() const {

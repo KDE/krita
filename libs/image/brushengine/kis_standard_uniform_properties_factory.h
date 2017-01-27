35d2722e3182726340fe4e3aabd112e53d7d1c99
Boudewijn Rempt <boud@valdyas.org>
2017-01-27 10:40:35 +0100

    BUG:374928 Fix loading the translations that fit the system locale
    It is supremely important NOT to use i18n before the application
    is created.  If i18n is called too early, like when creating a static
    KoID, the translation system is initialized before we've had a chance
    to add the path to the translations to the XDG_DATA_DIRS variable. Then
    the translation system only sees the globally available translations,
    and Krita stays untranslated.
    
    Always use ki18n when initializing a static object with a translation.
    
    CCMAIL:kimageshop@kde.org

--- libs/image/brushengine/kis_standard_uniform_properties_factory.h
+++ libs/image/brushengine/kis_standard_uniform_properties_factory.h
@@ -27,26 +27,27 @@ class KisPaintopSettingsUpdateProxy;
 
 namespace KisStandardUniformPropertiesFactory
 {
-    static const KoID size("size", i18n("Size"));
-    static const KoID opacity("opacity", i18n("Opacity"));
-    static const KoID flow("flow", i18n("Flow"));
-    static const KoID angle("angle", i18n("Angle"));
-    static const KoID spacing("spacing", i18n("Spacing"));
+static const KoID size("size", ki18n("Size"));
+static const KoID opacity("opacity", ki18n("Opacity"));
+static const KoID flow("flow", ki18n("Flow"));
+static const KoID angle("angle", ki18n("Angle"));
+static const KoID spacing("spacing", ki18n("Spacing"));
 
-    /**
+
+/**
      * Overload of createProperty(const QString &id, ...)
      */
-    KisUniformPaintOpPropertySP createProperty(const KoID &id,
-                                               KisPaintOpSettingsRestrictedSP settings,
-                                               KisPaintopSettingsUpdateProxy *updateProxy);
+KisUniformPaintOpPropertySP createProperty(const KoID &id,
+                                           KisPaintOpSettingsRestrictedSP settings,
+                                           KisPaintopSettingsUpdateProxy *updateProxy);
 
-    /**
+/**
      * Factory for creating standard uniform properties. Right now
      * it supports only size, opacity and flow.
      */
-    KisUniformPaintOpPropertySP createProperty(const QString &id,
-                                               KisPaintOpSettingsRestrictedSP settings,
-                                               KisPaintopSettingsUpdateProxy *updateProxy);
+KisUniformPaintOpPropertySP createProperty(const QString &id,
+                                           KisPaintOpSettingsRestrictedSP settings,
+                                           KisPaintopSettingsUpdateProxy *updateProxy);
 }
 
 #endif /* __KIS_STANDARD_UNIFORM_PROPERTIES_FACTORY_H */

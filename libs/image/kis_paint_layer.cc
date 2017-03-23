a2a2308d0ecfd0d6f9f3c7638cab3af1fe82526a
Dmitry Kazakov <dimula73@gmail.com>
2017-03-23 17:03:20 +0300

    Upgrade the layer to an animated one when the first frame is added
    BUG:377176

--- libs/image/kis_paint_layer.cc
+++ libs/image/kis_paint_layer.cc
@@ -336,6 +336,7 @@ KisKeyframeChannel *KisPaintLayer::requestKeyframeChannel(const QString &id)
     if (id == KisKeyframeChannel::Content.id()) {
         m_d->contentChannel = m_d->paintDevice->createKeyframeChannel(KisKeyframeChannel::Content);
         m_d->contentChannel->setOnionSkinsEnabled(onionSkinEnabled());
+        enableAnimation();
         return m_d->contentChannel;
     }
 

a76768502fbdfc7019472392e7ab8a75e47ae2d3
Eliakin Costa <eliakim170@gmail.com>
2017-06-08 02:29:12 -0300

    Invalid comment removed

--- plugins/extensions/pykrita/plugin/plugins/colorspace/uicolorspace.py
+++ plugins/extensions/pykrita/plugin/plugins/colorspace/uicolorspace.py
@@ -6,25 +6,6 @@ import krita
 
 
 class UIColorSpace(object):
-    """ @param colorModel A string describing the color model of the image:
-        * <ul>
-        * <li>A: Alpha mask</li>
-        * <li>RGBA: RGB with alpha channel (The actual order of channels is most often BGR!)</li>
-        * <li>XYZA: XYZ with alpha channel</li>
-        * <li>LABA: LAB with alpha channel</li>
-        * <li>CMYKA: CMYK with alpha channel</li>
-        * <li>GRAYA: Gray with alpha channel</li>
-        * <li>YCbCrA: YCbCr with alpha channel</li>
-        * </ul>
-        * @param colorDepth A string describing the color depth of the image:
-        * <ul>
-        * <li>U8: unsigned 8 bits integer, the most common type</li>
-        * <li>U16: unsigned 16 bits integer</li>
-        * <li>F16: half, 16 bits floating point. Only available if Krita was built with OpenEXR</li>
-        * <li>F32: 32 bits floating point</li>
-        * </ul>
-        * @param colorProfile a valid color profile for this color model and color depth combination.
-        * @return false the combination of these arguments does not correspond to a colorspace."""
     def __init__(self):
         self.mainDialog = colorspacedialog.ColorSpaceDialog()
         self.layout = QFormLayout(self.mainDialog)

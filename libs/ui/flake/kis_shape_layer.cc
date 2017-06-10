4f44b5703f26cb193980b6b0a030ff2c4d0ba3ad
Dmitry Kazakov <dimula73@gmail.com>
2017-06-10 18:38:57 +0300

    Fix rendering of shapes when the vector layer has a transformation
    Adding shapes to a shape layer is done trough KoShapeCreateCommand, not
    via the usual KoShapeGroupCommand, which does the inheriting transformation
    conversion.

--- libs/ui/flake/kis_shape_layer.cc
+++ libs/ui/flake/kis_shape_layer.cc
@@ -89,9 +89,25 @@ public:
 
     void add(KoShape *child) override {
         SimpleShapeContainerModel::add(child);
+
+        /**
+         * The shape is always added with the absolute transformation set appropiately.
+         * Here we should just squeeze it into the layer's transformation.
+         */
+        KIS_SAFE_ASSERT_RECOVER_NOOP(inheritsTransform(child));
+        if (inheritsTransform(child)) {
+            QTransform parentTransform = q->absoluteTransformation(0);
+            child->applyAbsoluteTransformation(parentTransform.inverted());
+        }
     }
 
     void remove(KoShape *child) override {
+        KIS_SAFE_ASSERT_RECOVER_NOOP(inheritsTransform(child));
+        if (inheritsTransform(child)) {
+            QTransform parentTransform = q->absoluteTransformation(0);
+            child->applyAbsoluteTransformation(parentTransform);
+        }
+
         SimpleShapeContainerModel::remove(child);
     }
 

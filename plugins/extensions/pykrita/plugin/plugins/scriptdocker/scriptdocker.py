2c61195871ab540aead27c29aee0b9155cd76739
Eliakin Costa <eliakim170@gmail.com>
2017-07-28 19:44:51 -0300

    Implement Dynamic Form to ScriptDocker
    - Added empty methods to settings
    - Added widgets and layouts to main layout
    - It was implemented an addEmptyForm method to dynamic append formlayout
    - Necessary imports was made

--- plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
+++ plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QListView
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QListView, QFormLayout,
+                             QHBoxLayout, QPushButton, QLineEdit)
 import krita
 
 
@@ -9,13 +10,39 @@ class ScriptDocker(krita.DockWidget):
 
        self.baseWidget = QWidget()
        self.layout = QVBoxLayout()
+       self.scriptsLayout = QFormLayout()
+       self.addButton = QPushButton("Add Script")
+       self.actions = []
 
+       self.layout.addLayout(self.scriptsLayout)
+       self.layout.addWidget(self.addButton)
        self.baseWidget.setLayout(self.layout)
        self.setWidget(self.baseWidget)
 
        self.setWindowTitle("Script Docker")
+       self.addButton.clicked.connect(self.addEmptyForm)
 
     def canvasChanged(self, canvas):
+        pass
+
+    def addEmptyForm(self):
+        directorySelectorLayout = QHBoxLayout()
+        directoryTextField = QLineEdit()
+        directoryDialogButton = QPushButton("...")
+
+        directorySelectorLayout.addWidget(directoryTextField)
+        directorySelectorLayout.addWidget(directoryDialogButton)
+
+        self.scriptsLayout.addRow("Script {0}".format(self.scriptsLayout.rowCount() + 1), directorySelectorLayout)
+
+
+    def loadActions(self):
+        pass
+
+    def readSettings(self):
+        pass
+
+    def writeSettings(self):
         pass
 
 

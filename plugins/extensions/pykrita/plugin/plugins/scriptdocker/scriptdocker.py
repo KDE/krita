786d80e56de15407f722833dc3b48b5c9bb669a2
Eliakin Costa <eliakim170@gmail.com>
2017-08-05 04:50:26 -0300

    Implement QFileDialog to form button
    - Getting sender to find the index of the button
    - The QLine edit is found and the filePath is setted
    - Filter to .py extension

--- plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
+++ plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QListView, QFormLayout,
-                             QHBoxLayout, QPushButton, QLineEdit, QListWidget)
+                             QHBoxLayout, QPushButton, QLineEdit, QListWidget,
+                             QScrollArea, QGridLayout, QFileDialog, QKeySequenceEdit,
+                             QLabel)
 from PyQt5.QtCore import QObject
 import krita
 
@@ -11,11 +13,16 @@ class ScriptDocker(krita.DockWidget):
 
        self.baseWidget = QWidget()
        self.layout = QVBoxLayout()
+       self.scrollArea =  QScrollArea()
        self.scriptsLayout = QFormLayout()
        self.addButton = QPushButton("Add Script")
        self.actions = []
+       self.baseArea = QWidget()
 
-       self.layout.addLayout(self.scriptsLayout)
+       self.scrollArea.setWidgetResizable(True)
+       self.baseArea.setLayout(self.scriptsLayout)
+       self.scrollArea.setWidget(self.baseArea)
+       self.layout.addWidget(self.scrollArea)
        self.layout.addWidget(self.addButton)
        self.baseWidget.setLayout(self.layout)
        self.setWidget(self.baseWidget)
@@ -27,20 +34,33 @@ class ScriptDocker(krita.DockWidget):
         pass
 
     def addNewRow(self):
-        directorySelectorLayout = QHBoxLayout()
+        rowPosition = self.scriptsLayout.rowCount()
+        shortcutEdit = QKeySequenceEdit()
         directoryTextField = QLineEdit()
         directoryDialogButton = QPushButton("...")
 
-        directoryDialogButton.clicked.connect(self.test)
+        shortcutEdit.setToolTip("Shortcut (CTRL + SHIFT + 1)")
+        directoryTextField.setToolTip("Selected Path")
+        directoryDialogButton.setToolTip("Select the script")
+        directoryDialogButton.clicked.connect(self.selectScript)
 
-        directorySelectorLayout.addWidget(directoryTextField)
-        directorySelectorLayout.addWidget(directoryDialogButton)
+        self.scriptsLayout.addWidget(shortcutEdit)
+        self.scriptsLayout.addWidget(directoryTextField)
+        self.scriptsLayout.addWidget(directoryDialogButton)
 
-        self.scriptsLayout.addRow("Script {0}".format(self.scriptsLayout.rowCount() + 1), directorySelectorLayout)
+    def selectScript(self):
+        dialog = QFileDialog(self)
+        dialog.setNameFilter('Python files (*.py)')
 
-    def test(self):
-        obj = self.sender()
-        print('button', obj)
+        if dialog.exec():
+            selectedFile = dialog.selectedFiles()[0]
+            print(selectedFile)
+
+            obj = self.sender()
+            print('button', obj)
+            textField = self.scriptsLayout.itemAt(self.scriptsLayout.indexOf(obj)-1).widget()
+            textField.setText(selectedFile)
+
 
     def loadActions(self):
         pass

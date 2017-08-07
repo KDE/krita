a469cf06e5fd1cc67901e9387621c4b24a9d7026
Eliakin Costa <eliakim170@gmail.com>
2017-08-07 04:32:29 -0300

    Implement writeSettings to ScriptDocker

--- plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
+++ plugins/extensions/pykrita/plugin/plugins/scriptdocker/scriptdocker.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QListView, QFormLayout,
                              QHBoxLayout, QPushButton, QLineEdit, QListWidget,
                              QScrollArea, QGridLayout, QFileDialog, QKeySequenceEdit,
-                             QLabel)
+                             QLabel, QAction, QDialogButtonBox)
 from PyQt5.QtCore import QObject, Qt
 import krita
 
@@ -16,23 +16,33 @@ class ScriptDocker(krita.DockWidget):
        self.scrollArea =  QScrollArea()
        self.scriptsLayout = QGridLayout()
        self.addButton = QPushButton("Add Script")
+       self.baseArea = QWidget()
+       self.buttonBox = QDialogButtonBox(self)
+
        self.actions = []
        self.scripts = []
-       self.baseArea = QWidget()
 
        self.initialize()
 
     def initialize(self):
-        self.scrollArea.setWidgetResizable(True)
+        self.buttonBox.accepted.connect(self._accept)
+        self.buttonBox.rejected.connect(self.close)
+        self.addButton.clicked.connect(self.addNewRow)
+
         self.baseArea.setLayout(self.scriptsLayout)
         self.scrollArea.setWidget(self.baseArea)
+
         self.layout.addWidget(self.scrollArea)
         self.layout.addWidget(self.addButton)
+        self.layout.addWidget(self.buttonBox)
+
         self.baseWidget.setLayout(self.layout)
         self.setWidget(self.baseWidget)
 
+        self.scrollArea.setWidgetResizable(True)
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         self.setWindowTitle("Script Docker")
-        self.addButton.clicked.connect(self.addNewRow)
 
     def canvasChanged(self, canvas):
         pass
@@ -67,24 +77,43 @@ class ScriptDocker(krita.DockWidget):
     def loadActions(self):
         pass
 
+    def _accept(self):
+        self.writeSettings()
+        self.close()
+
+    def _executeScript(self):
+        print('script executed')
+
     def readSettings(self):
         self.scripts = Application.readSetting("scriptdocker", "scripts", "").split(',')
         self.shortcuts = Application.readSetting("scriptdocker", "shortcuts", "").split(',')
 
     def writeSettings(self):
-        scripts = []
+        self.actions = []
         shortcuts = []
+        scripts = []
+        index = 0
 
-        for row in range(self.scriptsLayout.rowCount()):
-            shortcutWidget = self.scriptsLayout.itemAtPosition(row, 0).widget()
-            textField = self.scriptsLayout.itemAtPosition(row, 1).widget()
-            self.actions[row].script = textField.text()
-            self.actions[row].shortcut = textField.text()
-            presets.append(button.preset)
+        for row in range(self.scriptsLayout.rowCount()-1):
+            print('index', index)
+            shortcutWidget = self.scriptsLayout.itemAt(index).widget()
+            textField = self.scriptsLayout.itemAt(index + 1).widget()
+
+            if shortcutWidget.keySequence() and textField.text():
+                action = Application.createAction("execute_script_" + str(row), "Execute Script " + str(row))
+                action.setMenu("None")
+                action.triggered.connect(self._executeScript)
+                action.setShortcut("CTRL+SHIFT+1")
+                action.script = textField.text()
+
+                shortcuts.append(shortcutWidget.keySequence().toString())
+                scripts.append(textField.text())
+                self.actions.append(action)
+
+            index += 3
 
         Application.writeSetting("scriptdocker", "scripts", ','.join(map(str, scripts)))
         Application.writeSetting("scriptdocker", "shortcuts", ','.join(map(str, shortcuts)))
-
 
 
 Application.addDockWidgetFactory(krita.DockWidgetFactory("scriptdocker", krita.DockWidgetFactoryBase.DockRight, ScriptDocker))

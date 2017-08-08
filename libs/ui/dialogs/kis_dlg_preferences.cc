dc6cd47193ee4aa6b294a739aed94071a3ca761b
Alvin Wong <alvinhochun@gmail.com>
2017-08-08 23:26:33 +0800

    Rename enableOpenGLDebugging config
    Rename enableOpenGLDebugging to enableOpenGLFramerateLogging to
    describe its function better
    
    Differential Revision: https://phabricator.kde.org/D7132

--- libs/ui/dialogs/kis_dlg_preferences.cc
+++ libs/ui/dialogs/kis_dlg_preferences.cc
@@ -692,7 +692,7 @@ void PerformanceTab::load(bool requestDefault)
 
     {
         KisConfig cfg2;
-        chkOpenGLLogging->setChecked(cfg2.enableOpenGLDebugging(requestDefault));
+        chkOpenGLFramerateLogging->setChecked(cfg2.enableOpenGLFramerateLogging(requestDefault));
         chkDisableVectorOptimizations->setChecked(cfg2.enableAmdVectorizationWorkaround(requestDefault));
     }
 }
@@ -714,7 +714,7 @@ void PerformanceTab::save()
 
     {
         KisConfig cfg2;
-        cfg2.setEnableOpenGLDebugging(chkOpenGLLogging->isChecked());
+        cfg2.setEnableOpenGLFramerateLogging(chkOpenGLFramerateLogging->isChecked());
         cfg2.setEnableAmdVectorizationWorkaround(chkDisableVectorOptimizations->isChecked());
     }
 }

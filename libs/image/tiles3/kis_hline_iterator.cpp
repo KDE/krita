44d13118027e75ce9db7fad728c188acbdebb99f
Dmitry Kazakov <dimula73@gmail.com>
2017-08-09 19:48:29 +0300

    Optimize fetching of the tiles for iterators
    Every iterator fetches both types of tiles all the time: old and new.
    For projection devices these tiles are **always** the same, but doing
    two distinct calls makes double pressure on the read-write lock in the
    hash table.
    
    Therefore the patch merges the two calls into one. It allows us to avoid
    additional tile fetch from the hash table and therefore reduce waiting
    time.

--- libs/image/tiles3/kis_hline_iterator.cpp
+++ libs/image/tiles3/kis_hline_iterator.cpp
@@ -203,12 +203,11 @@ void KisHLineIterator2::switchToTile(qint32 xInTile)
 
 void KisHLineIterator2::fetchTileDataForCache(KisTileInfo& kti, qint32 col, qint32 row)
 {
-    kti.tile = m_dataManager->getTile(col, row, m_writable);
+    m_dataManager->getTilesPair(col, row, m_writable, &kti.tile, &kti.oldtile);
+
     lockTile(kti.tile);
     kti.data = kti.tile->data();
 
-    // set old data
-    kti.oldtile = m_dataManager->getOldTile(col, row);
     lockOldTile(kti.oldtile);
     kti.oldData = kti.oldtile->data();
 }

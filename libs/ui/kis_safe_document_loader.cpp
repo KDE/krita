ce183f5050c5b66fe89b0818ede64d6e068ad6ad
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2017-09-12 18:21:49 +0200

    BUG:378147 Fix reloading file layers twice.
    Basically, this was caused by the backups.
    Krita handles backups by saying "ok, you, old file, rename to backup,
    and new file, you become the url of old file". This renaming is what
    signals QFileSystemWatcher, and because it is a rename, it removes it
    from the list of watched files.
    
    So what we do is that when we get our change in, we check if the file
    exists and is still being watched, and if so we readd it.

--- libs/ui/kis_safe_document_loader.cpp
+++ libs/ui/kis_safe_document_loader.cpp
@@ -101,6 +101,10 @@ void KisSafeDocumentLoader::reloadImage()
 void KisSafeDocumentLoader::fileChanged(QString path)
 {
     if (path == m_d->path) {
+        if (s_fileSystemWatcher->files().contains(path) == false && QFileInfo(path).exists()) {
+            //When a path is renamed it is removed, so we ought to readd it.
+            s_fileSystemWatcher->addPath(path);
+        }
         m_d->fileChangedFlag = true;
     }
 }

29d1862bcba1f0ebd9a61fac2340c4a5d3d37a89
Boudewijn Rempt <boud@valdyas.org>
2017-09-28 13:44:41 +0200

    Revert "Pass the action collection to the edit toolbar"
    This reverts commit f3605f3f77ea1fe1a1f2c2cb38931b38e78fb29e.

--- libs/ui/KisMainWindow.cpp
+++ libs/ui/KisMainWindow.cpp
@@ -1701,8 +1701,7 @@ void KisMainWindow::slotConfigureToolbars()
 {
     KConfigGroup group =  KSharedConfig::openConfig()->group("krita");
     saveMainWindowSettings(group);
-
-    KEditToolBar edit(actionCollection(), this);
+    KEditToolBar edit(factory(), this);
     connect(&edit, SIGNAL(newToolBarConfig()), this, SLOT(slotNewToolbarConfig()));
     (void) edit.exec();
     applyToolBarLayout();

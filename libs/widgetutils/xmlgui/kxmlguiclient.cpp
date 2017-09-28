c0586f76e1c76e7a6a7e67525061aee7d5c8904f
Boudewijn Rempt <boud@valdyas.org>
2017-09-28 14:11:19 +0200

    Update debug message to make clear where it comes from

--- libs/widgetutils/xmlgui/kxmlguiclient.cpp
+++ libs/widgetutils/xmlgui/kxmlguiclient.cpp
@@ -252,7 +252,7 @@ void KXMLGUIClient::setXMLFile(const QString &_file, bool merge, bool setXMLDoc)
                    QStandardPaths::locateAll(QStandardPaths::AppDataLocation, _file); // kdelibs4, KF 5.0, caller passes component name
 
         if (allFiles.isEmpty() && !compatFiles.isEmpty()) {
-            qWarning() << "KXMLGUI file found at deprecated location" << compatFiles << "-- please use ${KXMLGUI_INSTALL_DIR} to install this file instead.";
+            qWarning() << "kxmlguiclient: KXMLGUI file found at deprecated location" << compatFiles << "-- please use ${KXMLGUI_INSTALL_DIR} to install this file instead.";
         }
         allFiles += compatFiles;
     }

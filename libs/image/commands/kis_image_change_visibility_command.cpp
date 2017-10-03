edf99991b2aa14f8d04a1b3e57ebb6548da32ebd
Nikita Smirnov <pakrentos@gmail.com>
2017-10-03 11:44:07 +0300

    Merge down should not remove locked layers
    Reviewers: #krita, dkazakov
    
    Subscribers: rempt
    
    Differential Revision: https://phabricator.kde.org/D7394

---
+++ libs/image/commands/kis_image_change_visibility_command.cpp
@@ -0,0 +1,23 @@
+#include "kis_image_commands.h"
+#include "kis_image.h"
+#include "kis_node.h"
+
+#include <klocalizedstring.h>
+
+
+KisImageChangeVisibilityCommand::KisImageChangeVisibilityCommand(bool visibility, KisNodeSP node)
+    : KUndo2Command(kundo2_noi18n("change-visibility-command"), 0)
+{
+    m_node = node;
+    m_visible = visibility;
+}
+
+void KisImageChangeVisibilityCommand::redo()
+{
+    m_node->setVisible(m_visible);
+}
+
+void KisImageChangeVisibilityCommand::undo()
+{
+    m_node->setVisible(!m_visible);
+}

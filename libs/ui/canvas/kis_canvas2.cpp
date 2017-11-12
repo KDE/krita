KDE/krita

commit 3b81a464cdda251b8888b78834fe8e6d458aba4b

Correctly pass the blackpoint check

libs/ui/canvas/kis_canvas2.cpp
@@ -610,17 +610,25 @@ void KisCanvas2::setProofingOptions(bool softProof, bool gamutCheck)
         m_d->proofingConfig = cfg.defaultProofingconfiguration();
     }
     KoColorConversionTransformation::ConversionFlags conversionFlags = m_d->proofingConfig->conversionFlags;
-
-    if (softProof && this->image()->colorSpace()->colorDepthId().id().contains("U")) {
+#if QT_VERSION >= 0x07000
+    if (this->image()->colorSpace()->colorDepthId().id().contains("U")) {
+        conversionFlags.setFlag(KoColorConversionTransformation::SoftProofing, softProof);
+        if (softProof) {
+            conversionFlags.setFlag(KoColorConversionTransformation::GamutCheck, gamutCheck);
+        }
+    }
+#else
+    if (this->image()->colorSpace()->colorDepthId().id().contains("U")) {
         conversionFlags |= KoColorConversionTransformation::SoftProofing;
     } else {
-        conversionFlags = conversionFlags &  ~KoColorConversionTransformation::SoftProofing;
+        conversionFlags = conversionFlags & ~KoColorConversionTransformation::SoftProofing;
     }
     if (gamutCheck && softProof && this->image()->colorSpace()->colorDepthId().id().contains("U")) {
         conversionFlags |= KoColorConversionTransformation::GamutCheck;
     } else {
         conversionFlags = conversionFlags & ~KoColorConversionTransformation::GamutCheck;
     }
+#endif;
     m_d->proofingConfig->conversionFlags = conversionFlags;
 
     m_d->proofingConfigUpdated = true;
@@ -659,9 +667,7 @@ KisProofingConfigurationSP KisCanvas2::proofingConfiguration() const
     if (!m_d->proofingConfig) {
         m_d->proofingConfig = this->image()->proofingConfiguration();
         if (!m_d->proofingConfig) {
-            qDebug()<<"Canvas: No proofing config found, generating one.";
-            KisImageConfig cfg;
-            m_d->proofingConfig = cfg.defaultProofingconfiguration();
+            m_d->proofingConfig = KisImageConfig().defaultProofingconfiguration();
         }
     }
     return m_d->proofingConfig;

libs/ui/dialogs/kis_dlg_image_properties.cc
@@ -164,7 +164,14 @@ void KisDlgImageProperties::setProofingConfig()
     if (m_page->chkSaveProofing->isChecked()) {
 
         m_proofingConfig->conversionFlags = KoColorConversionTransformation::HighQuality;
-        if (m_page->ckbBlackPointComp) m_proofingConfig->conversionFlags |= KoColorConversionTransformation::BlackpointCompensation;
+#if QT_VERSION >= 0x07000
+        m_proofingConfig->conversionFlags.setFlag(KoColorConversionTransformation::BlackpointCompensation, m_page->ckbBlackPointComp->isChecked());
+#else
+        m_page->chkBlackPointComp->isChecked() ?
+            m_proofingConfig->conversionFlags |= KoColorConversionTransformation::BlackpointCompensation
+            : m_proofingConfig->conversionFlags = m_proofingConfig->conversionFlags & ~KoColorConversionTransformation::BlackpointCompensation;
+
+#endif
         m_proofingConfig->intent = (KoColorConversionTransformation::Intent)m_page->cmbIntent->currentIndex();
         m_proofingConfig->proofingProfile = m_page->proofSpaceSelector->currentColorSpace()->profile()->name();
         m_proofingConfig->proofingModel = m_page->proofSpaceSelector->currentColorSpace()->colorModelId().id();

commit bb3ff1abe31159124ceee50610006c002d5c1768

Fix build with Qt < 5.7
Regression in: 3b81a464cdda251b8888b78834fe8e6d458aba4b

libs/ui/canvas/kis_canvas2.cpp
@@ -610,7 +610,7 @@ void KisCanvas2::setProofingOptions(bool softProof, bool gamutCheck)
         m_d->proofingConfig = cfg.defaultProofingconfiguration();
     }
     KoColorConversionTransformation::ConversionFlags conversionFlags = m_d->proofingConfig->conversionFlags;
-#if QT_VERSION >= 0x07000
+#if QT_VERSION >= 0x050700
     if (this->image()->colorSpace()->colorDepthId().id().contains("U")) {
         conversionFlags.setFlag(KoColorConversionTransformation::SoftProofing, softProof);
         if (softProof) {

libs/ui/dialogs/kis_dlg_image_properties.cc
@@ -164,7 +164,7 @@ void KisDlgImageProperties::setProofingConfig()
     if (m_page->chkSaveProofing->isChecked()) {
 
         m_proofingConfig->conversionFlags = KoColorConversionTransformation::HighQuality;
-#if QT_VERSION >= 0x07000
+#if QT_VERSION >= 0x050700
         m_proofingConfig->conversionFlags.setFlag(KoColorConversionTransformation::BlackpointCompensation, m_page->ckbBlackPointComp->isChecked());
 #else
         m_page->chkBlackPointComp->isChecked() ?
bb3ff1abe31159124ceee50610006c002d5c1768
Alvin Wong <alvinhochun@gmail.com>
2017-11-12 20:29:59 +0800

    Fix build with Qt < 5.7
    Regression in: 3b81a464cdda251b8888b78834fe8e6d458aba4b

--- libs/ui/canvas/kis_canvas2.cpp
+++ libs/ui/canvas/kis_canvas2.cpp
@@ -610,7 +610,7 @@ void KisCanvas2::setProofingOptions(bool softProof, bool gamutCheck)
         m_d->proofingConfig = cfg.defaultProofingconfiguration();
     }
     KoColorConversionTransformation::ConversionFlags conversionFlags = m_d->proofingConfig->conversionFlags;
-#if QT_VERSION >= 0x07000
+#if QT_VERSION >= 0x050700
     if (this->image()->colorSpace()->colorDepthId().id().contains("U")) {
         conversionFlags.setFlag(KoColorConversionTransformation::SoftProofing, softProof);
         if (softProof) {

b660b7a858f642917e20c6a418d0fb507e7d9d0d
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2017-11-13 17:01:24 +0100

    Make the selectionchange dictate whether to update the icon.
    This way arrow keys to change the icon will work too!

--- libs/ui/widgets/kis_paintop_preset_icon_library.cpp
+++ libs/ui/widgets/kis_paintop_preset_icon_library.cpp
@@ -86,8 +86,8 @@ KisPaintopPresetIconLibrary::KisPaintopPresetIconLibrary(QWidget *parent): QWidg
         m_optionalModel->appendRow(image);
     }
 
-    connect(ui->vwBase, SIGNAL(clicked(const QModelIndex)), this, SLOT(updateIcon()));
-    connect(ui->vwOptional, SIGNAL(clicked(const QModelIndex)), this, SLOT(updateIcon()));
+    connect(ui->vwBase->selectionModel(), SIGNAL(selectionChanged(const QItemSelection&, const QItemSelection &)), this, SLOT(updateIcon()));
+    connect(ui->vwOptional->selectionModel(), SIGNAL(selectionChanged(const QItemSelection&, const QItemSelection &)), this, SLOT(updateIcon()));
     connect(ui->sldHue, SIGNAL(valueChanged(qreal)), this, SLOT(updateIcon()));
     connect(ui->sldSat, SIGNAL(valueChanged(qreal)), this, SLOT(updateIcon()));
     connect(ui->sldLevels, SIGNAL(valueChanged(qreal)), this, SLOT(updateIcon()));

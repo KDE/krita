a6eedd9a7b458be6871e00f214452aa6c28e6e0b
Scott Petrovic <scottpetrovic@gmail.com>
2017-11-30 08:31:30 -0600

    swapped out slider with kis slider and added my name to copyright

--- libs/ui/kis_painting_assistant.cc
+++ libs/ui/kis_painting_assistant.cc
@@ -1,6 +1,7 @@
 /*
  *  Copyright (c) 2008,2011 Cyrille Berger <cberger@cberger.net>
  *  Copyright (c) 2010 Geoffry Song <goffrie@gmail.com>
+ *  Copyright (c) 2017 Scott Petrovic <scottpetrovic@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

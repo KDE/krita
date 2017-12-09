KDE/krita

commit 9aa2b0d61ef65bcf9debdbdcab2ee67ea87d88ee

made assistant code more clear if assistant was complete. moved a couple functions to decoration

plugins/assistants/Assistants/ParallelRulerAssistant.cc
@@ -39,7 +39,8 @@ ParallelRulerAssistant::ParallelRulerAssistant()
 
 QPointF ParallelRulerAssistant::project(const QPointF& pt, const QPointF& strokeBegin)
 {
-    Q_ASSERT(handles().size() == 2);
+    Q_ASSERT(isAssistantComplete());
+
     //code nicked from the perspective ruler.
     qreal dx = pt.x() - strokeBegin.x();
     qreal dy = pt.y() - strokeBegin.y();
@@ -90,7 +91,7 @@ void ParallelRulerAssistant::drawAssistant(QPainter& gc, const QRectF& updateRec
         dbgFile<<"canvas does not exist in ruler, you may have passed arguments incorrectly:"<<canvas;
     }
     
-    if (handles().size() > 1 && isSnappingActive() && previewVisible==true) {
+    if (isAssistantComplete() > 1 && isSnappingActive() && previewVisible==true) {
         //don't draw if invalid.
         QTransform initialTransform = converter->documentToWidgetTransform();
         QLineF snapLine= QLineF(initialTransform.map(*handles()[0]), initialTransform.map(*handles()[1]));
@@ -115,11 +116,7 @@ void ParallelRulerAssistant::drawAssistant(QPainter& gc, const QRectF& updateRec
 
 void ParallelRulerAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *converter, bool assistantVisible)
 {
-    if (assistantVisible==false){
-        return;
-    }
-
-    if (handles().size() < 2) {
+    if (assistantVisible == false || !isAssistantComplete()){
         return;
     }
 
@@ -140,6 +137,11 @@ void ParallelRulerAssistant::drawCache(QPainter& gc, const KisCoordinatesConvert
 QPointF ParallelRulerAssistant::buttonPosition() const
 {
     return (*handles()[0] + *handles()[1]) * 0.5;
+}
+
+bool ParallelRulerAssistant::isAssistantComplete() const
+{
+    return handles().size() >= 2;
 }
 
 ParallelRulerAssistantFactory::ParallelRulerAssistantFactory()

libs/ui/kis_painting_assistant.cc
@@ -315,6 +315,11 @@ QRect KisPaintingAssistant::boundingRect() const
     return r.adjusted(-2, -2, 2, 2).toAlignedRect();
 }
 
+bool KisPaintingAssistant::isAssistantComplete() const
+{
+    return true;
+}
+
 QByteArray KisPaintingAssistant::saveXml(QMap<KisPaintingAssistantHandleSP, int> &handleMap)
 {
     QByteArray data;
@@ -695,6 +700,10 @@ QPointF KisPaintingAssistant::pixelToView(const QPoint pixelCoords) const
     return d->m_canvas->viewConverter()->documentToView(documentCoord);
 }
 
+double KisPaintingAssistant::norm2(const QPointF& p)
+{
+    return p.x() * p.x() + p.y() * p.y();
+}
 
 
 

libs/ui/kis_painting_assistant.h
@@ -164,12 +164,19 @@ public:
      */
     void drawPath(QPainter& painter, const QPainterPath& path, bool drawActive=true);
     void drawPreview(QPainter& painter, const QPainterPath& path);
+    static double norm2(const QPointF& p);
 
 protected:
     virtual QRect boundingRect() const;
 
     /// performance layer where the graphics can be drawn from a cache instead of generated every render update
     virtual void drawCache(QPainter& gc, const KisCoordinatesConverter *converter, bool assistantVisible=true) = 0;
+
+    /// determines if the assistant has enough handles to be considered created
+    /// new assistants get in a "creation" phase where they are currently being made on the canvas
+    /// it will return false if we are in the middle of creating the assistant.
+    virtual bool isAssistantComplete() const;
+
     void initHandles(QList<KisPaintingAssistantHandleSP> _handles);
     QList<KisPaintingAssistantHandleSP> m_handles;
 

libs/ui/kis_painting_assistants_decoration.cpp
@@ -290,3 +290,24 @@ bool KisPaintingAssistantsDecoration::isEditingAssistants()
     return d->m_isEditingAssistants;
 }
 
+QPointF KisPaintingAssistantsDecoration::snapToGuide(KoPointerEvent *e, const QPointF &offset, bool useModifiers)
+{
+    if (!d->m_canvas->currentImage())
+        return e->point;
+
+    KoSnapGuide *snapGuide = d->m_canvas->snapGuide();
+    QPointF pos = snapGuide->snap(e->point, offset, useModifiers ? e->modifiers() : Qt::NoModifier);
+
+    return pos;
+}
+
+QPointF KisPaintingAssistantsDecoration::snapToGuide(const QPointF& pt, const QPointF &offset)
+{
+    if (!d->m_canvas)
+        return pt;
+
+    KoSnapGuide *snapGuide = d->m_canvas->snapGuide();
+    QPointF pos = snapGuide->snap(pt, offset, Qt::NoModifier);
+
+    return pos;
+}

libs/ui/kis_painting_assistants_decoration.h
@@ -21,9 +21,12 @@
 
 #include <QPointF>
 #include <QColor>
+
+#include "KoPointerEvent.h"
+#include "KoSnapGuide.h"
+
 #include "canvas/kis_canvas_decoration.h"
 #include "kis_painting_assistant.h"
-
 #include <kritaui_export.h>
 
 class KisView;
@@ -96,6 +99,9 @@ public Q_SLOTS:
     void toggleAssistantVisible();
     void toggleOutlineVisible();
     void setAssistantsColor(QColor color);
+    QPointF snapToGuide(KoPointerEvent *e, const QPointF &offset, bool useModifiers);
+    QPointF snapToGuide(const QPointF& pt, const QPointF &offset);
+
 protected:
     void drawDecoration(QPainter& gc, const QRectF& updateRect, const KisCoordinatesConverter *converter,KisCanvas2* canvas) override;
 

plugins/assistants/Assistants/ConcentricEllipseAssistant.cc
@@ -36,7 +36,7 @@ ConcentricEllipseAssistant::ConcentricEllipseAssistant()
 
 QPointF ConcentricEllipseAssistant::project(const QPointF& pt, const QPointF& strokeBegin) const
 {
-    Q_ASSERT(handles().size() == 3);
+    Q_ASSERT(isAssistantComplete());
     e.set(*handles()[0], *handles()[1], *handles()[2]);
 
     qreal
@@ -89,8 +89,9 @@ void ConcentricEllipseAssistant::drawAssistant(QPainter& gc, const QRectF& updat
 
     QTransform initialTransform = converter->documentToWidgetTransform();
 
-    if (isSnappingActive() && previewVisible==true){
-        if (handles().size() > 2){    
+    if (isSnappingActive() && previewVisible == true){
+
+        if (isAssistantComplete()){
             if (e.set(*handles()[0], *handles()[1], *handles()[2])) {
                 QPointF initial = e.project(initialTransform.inverted().map(mousePos));
                 QPointF center = e.boundingRect().center();
@@ -115,10 +116,12 @@ void ConcentricEllipseAssistant::drawAssistant(QPainter& gc, const QRectF& updat
 
 void ConcentricEllipseAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *converter, bool assistantVisible)
 {
+    if (assistantVisible == false || !isAssistantComplete()) {
+        return;
+    }
 
-    if (assistantVisible==false){return;}
-    if (handles().size() < 2) return;
-        QTransform initialTransform = converter->documentToWidgetTransform();
+    QTransform initialTransform = converter->documentToWidgetTransform();
+
     if (handles().size() == 2) {
         // just draw the axis
         gc.setTransform(initialTransform);
@@ -128,6 +131,7 @@ void ConcentricEllipseAssistant::drawCache(QPainter& gc, const KisCoordinatesCon
         drawPath(gc, path, isSnappingActive());       
         return;
     }
+
     if (e.set(*handles()[0], *handles()[1], *handles()[2])) {
         // valid ellipse
 
@@ -144,7 +148,10 @@ void ConcentricEllipseAssistant::drawCache(QPainter& gc, const KisCoordinatesCon
 
 QRect ConcentricEllipseAssistant::boundingRect() const
 {
-    if (handles().size() != 3) return KisPaintingAssistant::boundingRect();
+    if (!isAssistantComplete()) {
+        return KisPaintingAssistant::boundingRect();
+    }
+
     if (e.set(*handles()[0], *handles()[1], *handles()[2])) {
         return e.boundingRect().adjusted(-2, -2, 2, 2).toAlignedRect();
     } else {
@@ -156,6 +163,12 @@ QPointF ConcentricEllipseAssistant::buttonPosition() const
 {
     return (*handles()[0] + *handles()[1]) * 0.5;
 }
+
+bool ConcentricEllipseAssistant::isAssistantComplete() const
+{
+    return handles().size() >= 3;
+}
+
 
 ConcentricEllipseAssistantFactory::ConcentricEllipseAssistantFactory()
 {

plugins/assistants/Assistants/ConcentricEllipseAssistant.h
@@ -31,6 +31,8 @@ public:
     QPointF adjustPosition(const QPointF& point, const QPointF& strokeBegin) override;
     QPointF buttonPosition() const override;
     int numHandles() const override { return 3; }
+    bool isAssistantComplete() const;
+
 protected:
     QRect boundingRect() const override;
     void drawAssistant(QPainter& gc, const QRectF& updateRect, const KisCoordinatesConverter* converter, bool cached, KisCanvas2* canvas, bool assistantVisible=true, bool previewVisible=true) override;

plugins/assistants/Assistants/EllipseAssistant.cc
@@ -36,7 +36,7 @@ EllipseAssistant::EllipseAssistant()
 
 QPointF EllipseAssistant::project(const QPointF& pt) const
 {
-    Q_ASSERT(handles().size() == 3);
+    Q_ASSERT(isAssistantComplete());
     e.set(*handles()[0], *handles()[1], *handles()[2]);
     return e.project(pt);
 }
@@ -66,7 +66,8 @@ void EllipseAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect, con
     QTransform initialTransform = converter->documentToWidgetTransform();
 
     if (isSnappingActive() && boundingRect().contains(initialTransform.inverted().map(mousePos), false) && previewVisible==true){
-        if (handles().size() > 2){    
+
+        if (isAssistantComplete()){
             if (e.set(*handles()[0], *handles()[1], *handles()[2])) {
                 // valid ellipse
                 gc.setTransform(initialTransform);
@@ -89,9 +90,12 @@ void EllipseAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect, con
 void EllipseAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *converter, bool assistantVisible)
 {
 
-    if (assistantVisible==false){return;}
-    if (handles().size() < 2) return;
-        QTransform initialTransform = converter->documentToWidgetTransform();
+    if (assistantVisible == false || handles().size() < 2){
+        return;
+    }
+
+    QTransform initialTransform = converter->documentToWidgetTransform();
+
     if (handles().size() == 2) {
         // just draw the axis
         gc.setTransform(initialTransform);
@@ -117,7 +121,10 @@ void EllipseAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *co
 
 QRect EllipseAssistant::boundingRect() const
 {
-    if (handles().size() != 3) return KisPaintingAssistant::boundingRect();
+    if (!isAssistantComplete()) {
+        return KisPaintingAssistant::boundingRect();
+    }
+
     if (e.set(*handles()[0], *handles()[1], *handles()[2])) {
         return e.boundingRect().adjusted(-2, -2, 2, 2).toAlignedRect();
     } else {
@@ -129,6 +136,13 @@ QPointF EllipseAssistant::buttonPosition() const
 {
     return (*handles()[0] + *handles()[1]) * 0.5;
 }
+
+bool EllipseAssistant::isAssistantComplete() const
+{
+    return handles().size() >= 3;
+}
+
+
 
 EllipseAssistantFactory::EllipseAssistantFactory()
 {

commit 8363444c92be4a0376f548ffc621bb2e56be212b

Fix parallel assistant not drawing
Regression in 9aa2b0d61ef65bcf9debdbdcab2ee67ea87d88ee

plugins/assistants/Assistants/ParallelRulerAssistant.cc
@@ -92,7 +92,7 @@ void ParallelRulerAssistant::drawAssistant(QPainter& gc, const QRectF& updateRec
         dbgFile<<"canvas does not exist in ruler, you may have passed arguments incorrectly:"<<canvas;
     }
     
-    if (isAssistantComplete() > 1 && isSnappingActive() && previewVisible==true) {
+    if (isAssistantComplete() && isSnappingActive() && previewVisible==true) {
         //don't draw if invalid.
         QTransform initialTransform = converter->documentToWidgetTransform();
         QLineF snapLine= QLineF(initialTransform.map(*handles()[0]), initialTransform.map(*handles()[1]));
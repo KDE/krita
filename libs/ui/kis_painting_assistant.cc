KDE/krita

commit 6e568d40a4e3ef4633034ac0d4ea2f2258fb59d0

hide the bezier points on the spline assistant when not editing

libs/ui/kis_painting_assistant.cc
@@ -123,6 +123,7 @@ struct KisPaintingAssistant::Private {
     QPixmapCache::Key cached;
     QRect cachedRect; // relative to boundingRect().topLeft()
     KisPaintingAssistantHandleSP topLeft, bottomLeft, topRight, bottomRight, topMiddle, bottomMiddle, rightMiddle, leftMiddle;
+
     struct TranslationInvariantTransform {
         qreal m11, m12, m21, m22;
         TranslationInvariantTransform() { }
@@ -252,8 +253,8 @@ void KisPaintingAssistant::addHandle(KisPaintingAssistantHandleSP handle, Handle
 void KisPaintingAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect, const KisCoordinatesConverter* converter, bool useCache,KisCanvas2* canvas, bool assistantVisible, bool previewVisible)
 {
     Q_UNUSED(updateRect);
-    Q_UNUSED(canvas);
     Q_UNUSED(previewVisible);
+
     findHandleLocation();
     if (!useCache) {
         gc.save();
@@ -261,6 +262,7 @@ void KisPaintingAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect,
         gc.restore();
         return;
     }
+
     const QRect bound = boundingRect();
     if (bound.isEmpty()) return;
     const QTransform transform = converter->documentToWidgetTransform();
@@ -291,6 +293,8 @@ void KisPaintingAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect,
         d->cached = QPixmapCache::insert(cached);
     }
     gc.drawPixmap(paintRect, cached, paintRect.translated(-widgetBound.topLeft() - d->cachedRect.topLeft()));
+
+    canvas->updateCanvas(paintRect);
 }
 
 void KisPaintingAssistant::uncache()

libs/ui/kis_painting_assistants_decoration.cpp
@@ -27,6 +27,7 @@
 #include <ktoggleaction.h>
 #include "kis_debug.h"
 #include "KisDocument.h"
+#include "kis_canvas2.h"
 
 #include <QPainter>
 
@@ -45,6 +46,9 @@ struct KisPaintingAssistantsDecoration::Private {
     KisPaintingAssistantSP firstAssistant;
     bool aFirstStroke;
     QColor m_assistantsColor;
+    bool m_isEditingAssistants = false;
+
+    KisCanvas2 * m_canvas = 0;
 };
 
 KisPaintingAssistantsDecoration::KisPaintingAssistantsDecoration(QPointer<KisView> parent) :
@@ -53,7 +57,7 @@ KisPaintingAssistantsDecoration::KisPaintingAssistantsDecoration(QPointer<KisVie
 {
     setAssistantVisible(true);
     setOutlineVisible(true);
-    d->snapOnlyOneAssistant=true;//turn on by default.
+    d->snapOnlyOneAssistant = true; //turn on by default.
 }
 
 KisPaintingAssistantsDecoration::~KisPaintingAssistantsDecoration()
@@ -165,13 +169,24 @@ void KisPaintingAssistantsDecoration::drawDecoration(QPainter& gc, const QRectF&
 {
     if (!canvas) {
         dbgFile<<"canvas does not exist in painting assistant decoration, you may have passed arguments incorrectly:"<<canvas;
+    } else {
+        d->m_canvas = canvas;
     }
 
     QList<KisPaintingAssistantSP> assistants = view()->document()->assistants();
 
     Q_FOREACH (KisPaintingAssistantSP assistant, assistants) {
         assistant->setAssistantColor(assistantsColor());
-        assistant->drawAssistant(gc, updateRect, converter, true, canvas, assistantVisibility(), outlineVisibility());
+
+        // forcing to do a clean draw helps when we go from editing to painting
+        bool useCache = true;
+        if (d->m_isEditingAssistants) {
+            useCache = false;
+        }
+
+        assistant->drawAssistant(gc, updateRect, converter, useCache, canvas, assistantVisibility(), outlineVisibility());
+
+
     }
 }
 //drawPreview//
@@ -238,6 +253,7 @@ void KisPaintingAssistantsDecoration::toggleAssistantVisible()
     setAssistantVisible(!assistantVisibility());
     uncache();
 }
+
 void KisPaintingAssistantsDecoration::toggleOutlineVisible()
 {
     setOutlineVisible(!outlineVisibility());
@@ -251,3 +267,31 @@ void KisPaintingAssistantsDecoration::setAssistantsColor(QColor color)
 {
     d->m_assistantsColor = color;
 }
+
+void KisPaintingAssistantsDecoration::activateAssistantsEditor()
+{
+    setVisible(true); // this turns on the decorations in general. we leave it on at this point
+    d->m_isEditingAssistants = true;
+}
+
+void KisPaintingAssistantsDecoration::deactivateAssistantsEditor()
+{
+    if (!d->m_canvas) {
+        return;
+    }
+
+    d->m_isEditingAssistants = false; // some elements are hidden when we aren't editing
+
+    // draw decoration again in case some elements need to be show or hidden
+    QPainter painter;
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(assistantsColor());
+    QRectF canvasSize = QRectF(QPointF(0, 0), QSizeF(d->m_canvas->image()->size()));
+    drawDecoration(painter, canvasSize, d->m_canvas->coordinatesConverter(), d->m_canvas );
+}
+
+bool KisPaintingAssistantsDecoration::isEditingAssistants()
+{
+    return d->m_isEditingAssistants;
+}
+

plugins/assistants/Assistants/kis_assistant_tool.cc
@@ -72,14 +72,14 @@ void KisAssistantTool::activate(ToolActivation toolActivation, const QSet<KoShap
     // Add code here to initialize your tool when it got activated
     KisTool::activate(toolActivation, shapes);
 
+    m_canvas->paintingAssistantsDecoration()->activateAssistantsEditor();
     m_handles = m_canvas->paintingAssistantsDecoration()->handles();
-    m_canvas->paintingAssistantsDecoration()->setVisible(true);
 
     // turn off preview while we are managing assistants. it gets in the way with all the lines it generates
     m_temporaryPreviewState = m_canvas->paintingAssistantsDecoration()->outlineVisibility();
     m_canvas->paintingAssistantsDecoration()->setOutlineVisible(false);
+    repaintDecorations();
 
-    m_canvas->updateCanvas();
     m_handleDrag = 0;
     m_internalMode = MODE_CREATION;
     m_assistantHelperYOffset = 10;
@@ -94,8 +94,10 @@ void KisAssistantTool::activate(ToolActivation toolActivation, const QSet<KoShap
 void KisAssistantTool::deactivate()
 {
     // Add code here to initialize your tool when it got deactivated
+    m_canvas->paintingAssistantsDecoration()->deactivateAssistantsEditor();
     m_canvas->paintingAssistantsDecoration()->setOutlineVisible(m_temporaryPreviewState);
-    m_canvas->updateCanvas();
+    repaintDecorations(); // helps with updating assistant decoration a non-editor look
+
     KisTool::deactivate();
 }
 
@@ -568,6 +570,7 @@ void KisAssistantTool::mouseMoveEvent(KoPointerEvent *event)
 
 void KisAssistantTool::paint(QPainter& _gc, const KoViewConverter &_converter)
 {
+    QRectF canvasSize = QRectF(QPointF(0, 0), QSizeF(m_canvas->image()->size()));
     QColor assistantColor = m_canvas->paintingAssistantsDecoration()->assistantsColor();
     QBrush fillAssistantColorBrush;
     fillAssistantColorBrush.setColor(m_canvas->paintingAssistantsDecoration()->assistantsColor());
@@ -576,7 +579,8 @@ void KisAssistantTool::paint(QPainter& _gc, const KoViewConverter &_converter)
     if (m_newAssistant) {
 
         m_newAssistant->setAssistantColor(assistantColor);
-        m_newAssistant->drawAssistant(_gc, QRectF(QPointF(0, 0), QSizeF(m_canvas->image()->size())), m_canvas->coordinatesConverter(), false,m_canvas, true, false);
+        m_newAssistant->drawAssistant(_gc, canvasSize, m_canvas->coordinatesConverter(), false,m_canvas, true, false);
+
         Q_FOREACH (const KisPaintingAssistantHandleSP handle, m_newAssistant->handles()) {
             QPainterPath path;
             path.addEllipse(QRectF(_converter.documentToView(*handle) -  QPointF(m_handleSize * 0.5, m_handleSize * 0.5), QSizeF(m_handleSize, m_handleSize)));
@@ -586,7 +590,6 @@ void KisAssistantTool::paint(QPainter& _gc, const KoViewConverter &_converter)
             _gc.setBrush(assistantColor);
             _gc.drawPath(path);
             _gc.restore();
-            //m_newAssistant->drawPath(_gc, path);
         }
     }
 
@@ -620,7 +623,6 @@ void KisAssistantTool::paint(QPainter& _gc, const KoViewConverter &_converter)
             _gc.setPen(Qt::NoPen);
             _gc.setBrush(assistantColor);
             _gc.drawPath(path);
-            //assistant->drawPath(_gc, path);
             _gc.restore();
         }
 
@@ -715,6 +717,7 @@ void KisAssistantTool::paint(QPainter& _gc, const KoViewConverter &_converter)
              _gc.drawPixmap(iconMovePosition, m_iconMove);
         }
     }
+
 }
 
 void KisAssistantTool::removeAllAssistants()

libs/ui/kis_painting_assistants_decoration.h
@@ -48,6 +48,20 @@ public:
     QList<KisPaintingAssistantHandleSP> handles();
     QList<KisPaintingAssistantSP> assistants();
 
+    /// called when assistant editor is activated
+    /// right now this happens when the assistants tool is selected
+    void activateAssistantsEditor();
+
+
+    /// called when assistant editor is deactivated
+    /// right now this happens when the assistants tool is un-selected
+    void deactivateAssistantsEditor();
+
+    /// brings back if we are currently editing assistants or not
+    /// useful for some assistants (like spline) that draw bezier curves
+    bool isEditingAssistants();
+
+
     /*sets whether the main assistant is visible*/
     void setAssistantVisible(bool set);
 

plugins/assistants/Assistants/SplineAssistant.cc
@@ -54,13 +54,13 @@ inline QPointF B(qreal t, const QPointF& P0, const QPointF& P1, const QPointF& P
 inline qreal D(qreal t, const QPointF& P0, const QPointF& P1, const QPointF& P2, const QPointF& P3, const QPointF& p)
 {
     const qreal
-        tp = 1 - t,
+        tp =  1 - t,
         tp2 = tp * tp,
-        t2 = t * t,
-        a =     tp2 * tp,
-        b = 3 * tp2 * t,
-        c = 3 * tp  * t2,
-        d =     t   * t2,
+        t2 =  t * t,
+        a =   tp2 * tp,
+        b =   3 * tp2 * t,
+        c =   3 * tp  * t2,
+        d =   t   * t2,
         x_dist = a*P0.x() + b*P1.x() + c*P2.x() + d*P3.x() - p.x(),
         y_dist = a*P0.y() + b*P1.y() + c*P2.y() + d*P3.y() - p.y();
     return x_dist * x_dist + y_dist * y_dist;
@@ -97,6 +97,7 @@ void SplineAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect, cons
     if (canvas){
         //simplest, cheapest way to get the mouse-position//
         mousePos= canvas->canvasWidget()->mapFromGlobal(QCursor::pos());
+        m_canvas = canvas;
     }
     else {
         //...of course, you need to have access to a canvas-widget for that.//
@@ -129,7 +130,6 @@ void SplineAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect, cons
     gc.restore();
     
     KisPaintingAssistant::drawAssistant(gc, updateRect, converter, cached, canvas, assistantVisible, previewVisible);
-
 }
 
 void SplineAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *converter, bool assistantVisible)
@@ -147,10 +147,20 @@ void SplineAssistant::drawCache(QPainter& gc, const KisCoordinatesConverter *con
 
     gc.setTransform(initialTransform);
     gc.setPen(QColor(0, 0, 0, 75));
-    // Draw control lines
-    gc.drawLine(pts[0], pts[2]);
-    if (handles().size() >= 4) gc.drawLine(pts[1], pts[3]);
-    gc.setPen(QColor(0, 0, 0, 125));
+
+    // Draw control lines only if we are editing the assistant
+
+    if (m_canvas->paintingAssistantsDecoration()->isEditingAssistants()) {
+        gc.drawLine(pts[0], pts[2]);
+
+        if (handles().size() >= 4) {
+            gc.drawLine(pts[1], pts[3]);
+        }
+
+        gc.setPen(QColor(0, 0, 0, 125));
+    }
+
+
     // Draw the spline
     QPainterPath path;
     path.moveTo(pts[0]);

plugins/assistants/Assistants/SplineAssistant.h
@@ -34,6 +34,9 @@ protected:
     void drawCache(QPainter& gc, const KisCoordinatesConverter *converter,  bool assistantVisible=true) override;
 private:
     QPointF project(const QPointF& pt) const;
+
+    /// used for getting the decoration so the bezier handles aren't drawn while editing
+    KisCanvas2* m_canvas;
 };
 
 class SplineAssistantFactory : public KisPaintingAssistantFactory

commit 1b11f84b2f1e55a8baeaa456953bfbf353f62a98

Stop infinite canvas update with assistants
Regression in 6e568d40a4e3ef4633034ac0d4ea2f2258fb59d0

libs/ui/kis_painting_assistant.cc
@@ -302,8 +302,6 @@ void KisPaintingAssistant::drawAssistant(QPainter& gc, const QRectF& updateRect,
     if (canvas) {
         d->m_canvas = canvas;
     }
-
-    d->m_canvas->updateCanvas(paintRect);
 }
 
 void KisPaintingAssistant::uncache()

libs/ui/kis_painting_assistants_decoration.cpp
@@ -344,6 +344,7 @@ void KisPaintingAssistantsDecoration::activateAssistantsEditor()
 {
     setVisible(true); // this turns on the decorations in general. we leave it on at this point
     d->m_isEditingAssistants = true;
+    uncache(); // updates visuals when editing
 }
 
 void KisPaintingAssistantsDecoration::deactivateAssistantsEditor()

plugins/assistants/Assistants/kis_assistant_tool.cc
@@ -76,11 +76,13 @@ void KisAssistantTool::activate(ToolActivation toolActivation, const QSet<KoShap
     m_canvas->paintingAssistantsDecoration()->setHandleSize(17);
     m_handleSize = 17;
 
+    m_canvas->updateCanvas();
 }
 
 void KisAssistantTool::deactivate()
 {
     m_canvas->paintingAssistantsDecoration()->deactivateAssistantsEditor();
+    m_canvas->updateCanvas();
     KisTool::deactivate();
 }
 
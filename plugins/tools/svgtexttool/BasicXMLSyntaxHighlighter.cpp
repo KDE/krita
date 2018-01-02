491cb167a314fc4f568f237c3e886539ca899e59
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2018-01-02 15:30:23 +0100

    Allow XML regexp to also consider '-' for attributes.
    This ensures font-family="something" has 'font-family' fully highlighted.

--- plugins/tools/svgtexttool/BasicXMLSyntaxHighlighter.cpp
+++ plugins/tools/svgtexttool/BasicXMLSyntaxHighlighter.cpp
@@ -91,7 +91,7 @@ void BasicXMLSyntaxHighlighter::highlightByRegex(const QTextCharFormat & format,
 void BasicXMLSyntaxHighlighter::setRegexes()
 {
     m_xmlElementRegex.setPattern("<[\\s]*[/]?[\\s]*([^\\n]\\w*)(?=[\\s/>])");
-    m_xmlAttributeRegex.setPattern("\\w+(?=\\=)");
+    m_xmlAttributeRegex.setPattern("[\\w\\-]+(?=\\=)");
     m_xmlValueRegex.setPattern("\"[^\\n\"]+\"(?=[\\s/>])");
     m_xmlCommentRegex.setPattern("<!--[^\\n]*-->");
 

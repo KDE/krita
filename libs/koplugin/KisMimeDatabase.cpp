KDE/krita

commit 23d10f42db59d4404148b2a29cda8c78cb28eb80

KoFileDialog: check whether the suffix reflects a mimetype
If the suffix doesn't map to a mimetype, it probably isn't a mimetype,
so we should add the suffix for the currently selected filter.

BUG:388738

libs/widgetutils/KoFileDialog.cpp
@@ -227,7 +227,13 @@ QString KoFileDialog::filename()
     }
 
     if (!url.isEmpty()) {
-        if (d->type == SaveFile && QFileInfo(url).suffix().isEmpty()) {
+
+        QString suffix = QFileInfo(url).suffix();
+        if (KisMimeDatabase::mimeTypeForSuffix(suffix).isEmpty()) {
+            suffix = "";
+        }
+
+        if (d->type == SaveFile && suffix.isEmpty()) {
             QString selectedFilter;
             // index 0 is all supported; if that is chosen, saveDocument will automatically make it .kra
             for (int i = 1; i < d->filterList.size(); ++i) {

commit 1b6b998b2dc181e78828c67b320e544a206f383f

Improve the check for mimetypes in KisMimeDataBase
This fixes the regression caused by

23d10f42db59d4404148b2a29cda8c78cb28eb80

CCBUG:388738

libs/koplugin/KisMimeDatabase.cpp
@@ -77,12 +77,7 @@ QString KisMimeDatabase::mimeTypeForSuffix(const QString &suffix)
     // make the file look like a file so Qt would recognize it
     s = "file." + s;
 
-    QMimeType mime = db.mimeTypeForName(s);
-    if (mime.name() != "application/octet-stream") {
-        debugPlugin << "mimeTypeForSuffix(). QMimeDatabase returned" << mime.name() << "for" << s;
-        return mime.name();
-    }
-    return "";
+    return mimeTypeForFile(s);
 }
 
 QString KisMimeDatabase::mimeTypeForData(const QByteArray ba)

libs/widgetutils/KoFileDialog.cpp
@@ -227,7 +227,12 @@ QString KoFileDialog::filename()
     }
 
     if (!url.isEmpty()) {
-        if (d->type == SaveFile && QFileInfo(url).suffix().isEmpty()) {
+        QString suffix = QFileInfo(url).suffix();
+        if (KisMimeDatabase::mimeTypeForSuffix(suffix).isEmpty()) {
+            suffix = "";
+        }
+
+        if (d->type == SaveFile && suffix.isEmpty()) {
             QString selectedFilter;
             // index 0 is all supported; if that is chosen, saveDocument will automatically make it .kra
             for (int i = 1; i < d->filterList.size(); ++i) {
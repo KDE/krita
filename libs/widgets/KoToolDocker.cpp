7fcae64662792fc12e24d59f2b3f83e7fd48416a
Boudewijn Rempt <boud@valdyas.org>
2018-01-30 12:08:05 +0100

    Remove dangerous asserts
    If the if condition makes it possible for the widget object to
    be null, the assert should not check from the widget's objectName,
    because if the widget is null, that would crash.

--- libs/widgets/KoToolDocker.cpp
+++ libs/widgets/KoToolDocker.cpp
@@ -99,8 +99,7 @@ public:
             currentAuxWidgets.insert(t);
             Q_FOREACH (QPointer<QWidget> widget, currentWidgetList) {
                 if (widget.isNull() || widget->objectName().isEmpty()) {
-                    Q_ASSERT(!(widget->objectName().isEmpty()));
-                    continue; // skip this docker in release build when assert don't crash
+                    continue;
                 }
                 t->addTab(widget, widget->windowTitle());
             }
@@ -115,7 +114,7 @@ public:
                 housekeeperLayout->setVerticalSpacing(0);
                 Q_FOREACH (QPointer<QWidget> widget, currentWidgetList) {
                     if (widget.isNull() || widget->objectName().isEmpty()) {
-                        KIS_SAFE_ASSERT_RECOVER(!(widget->objectName().isEmpty())) { continue; }
+                        continue;
                     }
                     if (!widget->windowTitle().isEmpty()) {
                         housekeeperLayout->addWidget(l = new QLabel(widget->windowTitle()), 0, 2*cnt);
@@ -138,7 +137,7 @@ public:
                 int specialCount = 0;
                 Q_FOREACH (QPointer<QWidget> widget, currentWidgetList) {
                     if (widget.isNull() || widget->objectName().isEmpty()) {
-                        KIS_SAFE_ASSERT_RECOVER(!(widget->objectName().isEmpty())) { continue; }
+                        continue;
                     }
                     if (!widget->windowTitle().isEmpty()) {
                         housekeeperLayout->addWidget(l = new QLabel(widget->windowTitle()), cnt++, 0);

46d2d3a445089c87b1e3bed5a949d718df7c7e9f
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2018-02-20 20:11:48 +0100

    Ten Brushes: Undelete variable.

--- plugins/extensions/pykrita/plugin/plugins/tenbrushes/uitenbrushes.py
+++ plugins/extensions/pykrita/plugin/plugins/tenbrushes/uitenbrushes.py
@@ -43,7 +43,7 @@ class UITenBrushes(object):
 
         allPresets = Application.resources("preset")
 
-        for index in enumerate(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']):
+        for index, item in enumerate(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']):
             buttonLayout = QVBoxLayout()
             button = dropbutton.DropButton(self.mainDialog)
             button.setObjectName(item)

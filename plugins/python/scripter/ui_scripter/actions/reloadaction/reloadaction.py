47b48a9d99b5f047e10d7f815a6e26676200be9b
Boudewijn Rempt <boud@valdyas.org>
2018-02-28 14:41:24 +0100

    Add a Reload feature to Scripter
    Patch by Arvind Pinto.
    
    Differential Revision: https://phabricator.kde.org/D10839

---
+++ plugins/python/scripter/ui_scripter/actions/reloadaction/reloadaction.py
@@ -0,0 +1,46 @@
+from PyQt5.QtWidgets import QAction, QFileDialog, QMessageBox
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtCore import Qt
+
+
+class ReloadAction(QAction):
+
+    def __init__(self, scripter, parent=None):
+        super(ReloadAction, self).__init__(parent)
+        self.scripter = scripter
+        self.editor = self.scripter.uicontroller.editor
+
+        self.triggered.connect(self.reloadFile)
+
+        self.setText('Reload File')
+        self.setObjectName('reloadfile')
+        self.setShortcut(QKeySequence(Qt.ALT + Qt.Key_R))
+
+    @property
+    def parent(self):
+        return 'File'
+
+    def reloadFile(self):
+        # get the currently open document's path
+        curr_doc_fpath = ''
+        document = self.scripter.documentcontroller._activeDocument
+        if document is None:
+            QMessageBox.critical(self.scripter.uicontroller.mainWidget,
+                                 'No existing document',
+                                 'Please specify a document by opening it before reloading')
+            return
+        else:
+            curr_doc_fpath = document.filePath
+
+        # clear the editor
+        self.scripter.documentcontroller.clearActiveDocument()
+        self.scripter.uicontroller.setStatusBar()
+        self.scripter.uicontroller.clearEditor()
+
+        # reload the document
+        document = self.scripter.documentcontroller.openDocument(curr_doc_fpath)
+        self.scripter.uicontroller.setDocumentEditor(document)
+        self.scripter.uicontroller.setStatusBar(document.filePath)
+        print("reload is run")
+
+        return document

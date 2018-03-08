8e976c58887c8f90f1cd82cd324d9e202af7dd63
Scott Petrovic <scottpetrovic@gmail.com>
2018-03-07 18:09:36 -0600

    Make range for spray coverage more usable

--- plugins/paintops/spray/kis_sprayop_option.cpp
+++ plugins/paintops/spray/kis_sprayop_option.cpp
@@ -55,11 +55,9 @@ KisSprayOpOption::KisSprayOpOption()
     m_options->spacingSpin->setRange(0.0, 5.0, 2);
     m_options->spacingSpin->setValue(0.5);
 
-    m_options->coverageSpin->setRange(0.0, 100.0, 3);
-    m_options->coverageSpin->setValue(0.1);
+    m_options->coverageSpin->setRange(0.001, 0.02, 3);
+    m_options->coverageSpin->setValue(0.003);
     m_options->coverageSpin->setSuffix("%");
-    m_options->coverageSpin->setExponentRatio(3);
-
 
     m_options->particlesSpinBox->setRange(1.0, 1000.0, 0);
     m_options->particlesSpinBox->setValue(12);

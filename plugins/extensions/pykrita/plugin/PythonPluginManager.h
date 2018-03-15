28ce46244c712590d5f737d283a1dcb080daad65
Boudewijn Rempt <boud@valdyas.org>
2018-03-15 22:11:29 +0100

    Remove weird unicode characters
    (cherry picked from commit b4b145922e6f1c32a116e5653f1c7fcb55633362)

--- plugins/extensions/pykrita/plugin/PythonPluginManager.h
+++ plugins/extensions/pykrita/plugin/PythonPluginManager.h
@@ -4,22 +4,22 @@
  * Copyright (C) 2013 Alex Turbov <i.zaufi@gmail.com>
  * Copyright (C) 2014-2016 Boudewijn Rempt <boud@valdyas.org>
  * Copyright (C) 2017 Jouni Pentikäinen (joupent@gmail.com)
-​ *
-​ * This library is free software; you can redistribute it and/or
-​ * modify it under the terms of the GNU Library General Public
-​ * License as published by the Free Software Foundation; either
-​ * version 2 of the License, or (at your option) any later version.
-​ *
-​ * This library is distributed in the hope that it will be useful,
-​ * but WITHOUT ANY WARRANTY; without even the implied warranty of
-​ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-​ * Library General Public License for more details.
-​ *
-​ * You should have received a copy of the GNU Library General Public License
-​ * along with this library; see the file COPYING.LIB.  If not, write to
-​ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-​ * Boston, MA 02110-1301, USA.
-​ */
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 #ifndef PYTHONMODULEMANAGER_H
 #define PYTHONMODULEMANAGER_H
 

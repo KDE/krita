512fe7640c63f9d31b034430745916d14467671a
Dmitry Kazakov <dimula73@gmail.com>
2018-03-28 15:04:39 +0300

    Do not round pressure values to any numbers

--- libs/ui/tool/kis_painting_information_builder.cpp
+++ libs/ui/tool/kis_painting_information_builder.cpp
@@ -142,7 +142,7 @@ KisPaintInformation KisPaintingInformationBuilder::hover(const QPointF &imagePoi
 
 qreal KisPaintingInformationBuilder::pressureToCurve(qreal pressure)
 {
-    return m_pressureSamples.at(qRound(pressure * LEVEL_OF_PRESSURE_RESOLUTION));
+    return KisCubicCurve::interpolateLinear(pressure, m_pressureSamples);
 }
 
 /***********************************************************************/

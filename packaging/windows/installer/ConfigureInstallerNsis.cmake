01f52202aa847a69d028764b225edb5a91819db8
Alvin Wong <alvinhochun@gmail.com>
2018-05-22 00:14:54 +0800

    Use installer files from Krita source tree

--- packaging/windows/installer/ConfigureInstallerNsis.cmake
+++ packaging/windows/installer/ConfigureInstallerNsis.cmake
@@ -13,3 +13,14 @@ configure_file(
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/MakeinstallerNsis.cmake
     DESTINATION ${CMAKE_INSTALL_PREFIX}
 )
+
+install(FILES ${CMAKE_CURRENT_LIST_DIR}/installer_krita.nsi
+              ${CMAKE_CURRENT_LIST_DIR}/krita_versions_detect.nsh
+              ${CMAKE_CURRENT_LIST_DIR}/license_gpl-3.0.rtf
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/installer
+)
+
+install(FILES ${CMAKE_CURRENT_LIST_DIR}/include/FileExists2.nsh
+              ${CMAKE_CURRENT_LIST_DIR}/include/IsFileInUse.nsh
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/installer/include
+)

807aebf90487716ba39770cacf717071cdc1dc3e
Boudewijn Rempt <boud@valdyas.org>
2018-06-05 10:07:29 +0200

    Show the correct opacity in the segment gradient editor
    BUG:394887

--- libs/ui/kis_autogradient.cc
+++ libs/ui/kis_autogradient.cc
@@ -72,11 +72,11 @@ void KisAutogradient::slotSelectedSegment(KoGradientSegment* segment)
     comboBoxInterpolationType->setCurrentIndex(segment->interpolation());
 
     int leftOpacity = segment->startColor().opacityF();
-    intNumInputLeftOpacity->setValue(leftOpacity);
+    intNumInputLeftOpacity->setValue(leftOpacity * 100);
     intNumInputLeftOpacity->setSuffix(i18n(" %"));
 
     int rightOpacity = segment->endColor().opacityF();
-    intNumInputRightOpacity->setValue(rightOpacity);
+    intNumInputRightOpacity->setValue(rightOpacity * 100);
     intNumInputRightOpacity->setSuffix(i18n(" %"));
 
     paramChanged();

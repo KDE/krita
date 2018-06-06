3f17d2f551863641d39dc6aae7b12034448aafc3
Scott Petrovic <scottpetrovic@gmail.com>
2018-06-06 12:22:35 -0500

    Allow painting assistants to have their own color

--- libs/ui/kis_painting_assistants_decoration.h
+++ libs/ui/kis_painting_assistants_decoration.h
@@ -111,11 +111,14 @@ public:
 
     /// retrieves the assistants color specified in the tool options
     /// all assistants will share the same color
-    QColor assistantsColor();
+    QColor globalAssistantsColor();
 
     int handleSize();
     void setHandleSize(int handleSize);
 
+    /// helper functions when converting between QColor when saving to XML
+    static QString qColorToQString(QColor color);
+    static QColor qStringToQColor(QString colorString);
 
 Q_SIGNALS:
     void assistantChanged();
@@ -128,7 +131,7 @@ public Q_SLOTS:
 
     /// toggles whether there will be a preview of the assistant result when painting
     void toggleOutlineVisible();
-    void setAssistantsColor(QColor color);
+    void setGlobalAssistantsColor(QColor color);
     QPointF snapToGuide(KoPointerEvent *e, const QPointF &offset, bool useModifiers);
     QPointF snapToGuide(const QPointF& pt, const QPointF &offset);
 

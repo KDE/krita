c98c0b2ec23b5288c4039ff7dc105dbcac7e348b
Andrey Kamakin <a.kamakin@icloud.com>
2018-07-11 14:51:06 +0300

    Changed list for map in KisTileDataStore
    Ref T9130

--- libs/image/tiles3/kis_tile_data_store.cc
+++ libs/image/tiles3/kis_tile_data_store.cc
@@ -70,9 +70,10 @@ KisTileDataStore::KisTileDataStore()
     : m_pooler(this),
       m_swapper(this),
       m_numTiles(0),
-      m_memoryMetric(0)
+      m_memoryMetric(0),
+      m_counter(1)
 {
-    m_clockIterator = m_tileDataList.end();
+//    m_clockIterator = m_tileDataList.end();
     m_pooler.start();
     m_swapper.start();
 }
@@ -102,7 +103,8 @@ KisTileDataStore::MemoryStatistics KisTileDataStore::memoryStatistics()
         m_pooler.forceUpdateMemoryStats();
     }
 
-    QMutexLocker lock(&m_listLock);
+//    QMutexLocker lock(&m_listLock);
+    QReadLocker lock(&m_iteratorLock);
 
     MemoryStatistics stats;
 
@@ -121,34 +123,46 @@ KisTileDataStore::MemoryStatistics KisTileDataStore::memoryStatistics()
 
 inline void KisTileDataStore::registerTileDataImp(KisTileData *td)
 {
-    td->m_listIterator = m_tileDataList.insert(m_tileDataList.end(), td);
-    m_numTiles++;
+//    td->m_listIterator = m_tileDataList.insert(m_tileDataList.end(), td);
+//    m_numTiles++;
+//    m_memoryMetric += td->pixelSize();
+    int index = m_counter.fetchAndAddOrdered(1);
+    td->m_tileNumber = index;
+    m_tileDataMap.assign(index, td);
+    m_numTiles.ref();
     m_memoryMetric += td->pixelSize();
 }
 
 void KisTileDataStore::registerTileData(KisTileData *td)
 {
-    QMutexLocker lock(&m_listLock);
+//    QMutexLocker lock(&m_listLock);
+    QReadLocker lock(&m_iteratorLock);
     registerTileDataImp(td);
 }
 
 inline void KisTileDataStore::unregisterTileDataImp(KisTileData *td)
 {
-    KisTileDataListIterator tempIterator = td->m_listIterator;
+//    KisTileDataListIterator tempIterator = td->m_listIterator;
 
-    if(m_clockIterator == tempIterator) {
-        m_clockIterator = tempIterator + 1;
-    }
+//    if(m_clockIterator == tempIterator) {
+//        m_clockIterator = tempIterator + 1;
+//    }
 
-    td->m_listIterator = m_tileDataList.end();
-    m_tileDataList.erase(tempIterator);
-    m_numTiles--;
+//    td->m_listIterator = m_tileDataList.end();
+//    m_tileDataList.erase(tempIterator);
+//    m_numTiles--;
+//    m_memoryMetric -= td->pixelSize();
+    int index = td->m_tileNumber;
+    td->m_tileNumber = -1;
+    m_tileDataMap.erase(index);
+    m_numTiles.deref();
     m_memoryMetric -= td->pixelSize();
 }
 
 void KisTileDataStore::unregisterTileData(KisTileData *td)
 {
-    QMutexLocker lock(&m_listLock);
+//    QMutexLocker lock(&m_listLock);
+    QReadLocker lock(&m_iteratorLock);
     unregisterTileDataImp(td);
 }
 
@@ -184,7 +198,8 @@ void KisTileDataStore::freeTileData(KisTileData *td)
 
     DEBUG_FREE_ACTION(td);
 
-    m_listLock.lock();
+//    m_listLock.lock();
+    m_iteratorLock.lockForRead();
     td->m_swapLock.lockForWrite();
 
     if(!td->data()) {
@@ -195,7 +210,8 @@ void KisTileDataStore::freeTileData(KisTileData *td)
     }
 
     td->m_swapLock.unlock();
-    m_listLock.unlock();
+    m_iteratorLock.unlock();
+//    m_listLock.unlock();
 
     delete td;
 }
@@ -214,7 +230,8 @@ void KisTileDataStore::ensureTileDataLoaded(KisTileData *td)
          * The order of this heavy locking is very important.
          * Change it only in case, you really know what you are doing.
          */
-        m_listLock.lock();
+//        m_listLock.lock();
+        m_iteratorLock.lockForRead();
 
         /**
          * If someone has managed to load the td from swap, then, most
@@ -238,7 +255,8 @@ void KisTileDataStore::ensureTileDataLoaded(KisTileData *td)
             td->m_swapLock.unlock();
         }
 
-        m_listLock.unlock();
+        m_iteratorLock.unlock();
+//        m_listLock.unlock();
 
         /**
          * <-- In theory, livelock is possible here...
@@ -273,47 +291,56 @@ bool KisTileDataStore::trySwapTileData(KisTileData *td)
 
 KisTileDataStoreIterator* KisTileDataStore::beginIteration()
 {
-    m_listLock.lock();
-    return new KisTileDataStoreIterator(m_tileDataList, this);
+//    m_listLock.lock();
+//    return new KisTileDataStoreIterator(m_tileDataList, this);
+    m_iteratorLock.lockForWrite();
+    return new KisTileDataStoreIterator(m_tileDataMap, this);
 }
 void KisTileDataStore::endIteration(KisTileDataStoreIterator* iterator)
 {
     delete iterator;
-    m_listLock.unlock();
+    m_iteratorLock.unlock();
+//    m_listLock.unlock();
 }
 
 KisTileDataStoreReverseIterator* KisTileDataStore::beginReverseIteration()
 {
-    m_listLock.lock();
-    return new KisTileDataStoreReverseIterator(m_tileDataList, this);
+//    m_listLock.lock();
+//    return new KisTileDataStoreReverseIterator(m_tileDataList, this);
+    m_iteratorLock.lockForWrite();
+    return new KisTileDataStoreReverseIterator(m_tileDataMap, this);
 }
 void KisTileDataStore::endIteration(KisTileDataStoreReverseIterator* iterator)
 {
     delete iterator;
-    m_listLock.unlock();
+    m_iteratorLock.unlock();
+//    m_listLock.unlock();
     DEBUG_REPORT_PRECLONE_EFFICIENCY();
 }
 
 KisTileDataStoreClockIterator* KisTileDataStore::beginClockIteration()
 {
-    m_listLock.lock();
-    return new KisTileDataStoreClockIterator(m_clockIterator, m_tileDataList, this);
+//    m_listLock.lock();
+//    return new KisTileDataStoreClockIterator(m_clockIterator, m_tileDataList, this);
+    m_iteratorLock.lockForWrite();
+    return new KisTileDataStoreClockIterator(m_tileDataMap, this);
 }
 void KisTileDataStore::endIteration(KisTileDataStoreClockIterator* iterator)
 {
-    m_clockIterator = iterator->getFinalPosition();
+//    m_clockIterator = iterator->getFinalPosition();
     delete iterator;
-    m_listLock.unlock();
+    m_iteratorLock.unlock();
+//    m_listLock.unlock();
 }
 
 void KisTileDataStore::debugPrintList()
 {
-    KisTileData *item;
-    Q_FOREACH (item, m_tileDataList) {
-        dbgTiles << "-------------------------\n"
-                 << "TileData:\t\t\t" << item
-                 << "\n  refCount:\t" << item->m_refCount;
-    }
+//    KisTileData *item;
+//    Q_FOREACH (item, m_tileDataList) {
+//        dbgTiles << "-------------------------\n"
+//                 << "TileData:\t\t\t" << item
+//                 << "\n  refCount:\t" << item->m_refCount;
+//    }
 }
 
 void KisTileDataStore::debugSwapAll()
@@ -333,17 +360,17 @@ void KisTileDataStore::debugSwapAll()
 
 void KisTileDataStore::debugClear()
 {
-    QMutexLocker lock(&m_listLock);
+//    QMutexLocker lock(&m_listLock);
 
-    Q_FOREACH (KisTileData *item, m_tileDataList) {
-        delete item;
-    }
+//    Q_FOREACH (KisTileData *item, m_tileDataList) {
+//        delete item;
+//    }
 
-    m_tileDataList.clear();
-    m_clockIterator = m_tileDataList.end();
+//    m_tileDataList.clear();
+//    m_clockIterator = m_tileDataList.end();
 
-    m_numTiles = 0;
-    m_memoryMetric = 0;
+//    m_numTiles = 0;
+//    m_memoryMetric = 0;
 }
 
 void KisTileDataStore::testingRereadConfig() {
--- libs/image/tiles3/kis_tile_data_store_iterators.h
+++ libs/image/tiles3/kis_tile_data_store_iterators.h
@@ -18,6 +18,8 @@
 #ifndef KIS_TILE_DATA_STORE_ITERATORS_H_
 #define KIS_TILE_DATA_STORE_ITERATORS_H_
 
+#include "kis_tile_data.h"
+
 /**
  * KisTileDataStoreIterator,
  * KisTileDataStoreReverseIterator,
@@ -34,141 +36,255 @@
 
 class KisTileDataStoreIterator
 {
+//public:
+//    KisTileDataStoreIterator(KisTileDataList &list, KisTileDataStore *store)
+//        : m_list(list),
+//          m_store(store)
+//    {
+//        m_iterator = m_list.begin();
+//        m_end = m_list.end();
+//    }
+
+//    inline KisTileData* peekNext() {
+//        return *m_iterator;
+//    }
+
+//    inline KisTileData* next() {
+//        return *(m_iterator++);
+//    }
+
+//    inline bool hasNext() const {
+//        return m_iterator != m_end;
+//    }
+
+//    inline bool trySwapOut(KisTileData *td) {
+//        if(td->m_listIterator == m_iterator)
+//            m_iterator++;
+
+//        return m_store->trySwapTileData(td);
+//    }
+
+//private:
+//    KisTileDataList &m_list;
+//    KisTileDataListIterator m_iterator;
+//    KisTileDataListIterator m_end;
+//    KisTileDataStore *m_store;
 public:
-    KisTileDataStoreIterator(KisTileDataList &list, KisTileDataStore *store)
-        : m_list(list),
+    KisTileDataStoreIterator(ConcurrentMap<int, KisTileData*> &map, KisTileDataStore *store)
+        : m_map(map),
           m_store(store)
     {
-        m_iterator = m_list.begin();
-        m_end = m_list.end();
+        m_iterator.setMap(m_map);
     }
 
-    inline KisTileData* peekNext() {
-        return *m_iterator;
+    inline KisTileData* peekNext()
+    {
+        return m_iterator.getValue();
     }
 
-    inline KisTileData* next() {
-        return *(m_iterator++);
+    inline KisTileData* next()
+    {
+        KisTileData *current = m_iterator.getValue();
+        m_iterator.next();
+        return current;
     }
 
-    inline bool hasNext() const {
-        return m_iterator != m_end;
+    inline bool hasNext() const
+    {
+        return m_iterator.isValid();
     }
 
-    inline bool trySwapOut(KisTileData *td) {
-        if(td->m_listIterator == m_iterator)
-            m_iterator++;
+    inline bool trySwapOut(KisTileData *td)
+    {
+        if (td == m_iterator.getValue()) {
+            m_iterator.next();
+        }
 
         return m_store->trySwapTileData(td);
     }
 
 private:
-    KisTileDataList &m_list;
-    KisTileDataListIterator m_iterator;
-    KisTileDataListIterator m_end;
+    ConcurrentMap<int, KisTileData*> &m_map;
+    ConcurrentMap<int, KisTileData*>::Iterator m_iterator;
     KisTileDataStore *m_store;
 };
 
 class KisTileDataStoreReverseIterator
 {
+//public:
+//    KisTileDataStoreReverseIterator(KisTileDataList &list, KisTileDataStore *store)
+//        : m_list(list),
+//          m_store(store)
+//    {
+//        m_iterator = m_list.end();
+//        m_begin = m_list.begin();
+//    }
+
+//    inline KisTileData* peekNext() {
+//        return *(m_iterator-1);
+//    }
+
+//    inline KisTileData* next() {
+//        return *(--m_iterator);
+//    }
+
+//    inline bool hasNext() const {
+//        return m_iterator != m_begin;
+//    }
+
+//    inline bool trySwapOut(KisTileData *td) {
+//        if(td->m_listIterator == m_iterator)
+//            m_iterator++;
+
+//        return m_store->trySwapTileData(td);
+//    }
+
+//private:
+//    KisTileDataList &m_list;
+//    KisTileDataListIterator m_iterator;
+//    KisTileDataListIterator m_begin;
+//    KisTileDataStore *m_store;
 public:
-    KisTileDataStoreReverseIterator(KisTileDataList &list, KisTileDataStore *store)
-        : m_list(list),
+    KisTileDataStoreReverseIterator(ConcurrentMap<int, KisTileData*> &map, KisTileDataStore *store)
+        : m_map(map),
           m_store(store)
     {
-        m_iterator = m_list.end();
-        m_begin = m_list.begin();
+        m_iterator.setMap(m_map);
     }
 
-    inline KisTileData* peekNext() {
-        return *(m_iterator-1);
+    inline KisTileData* peekNext()
+    {
+        return m_iterator.getValue();
     }
 
-    inline KisTileData* next() {
-        return *(--m_iterator);
+    inline KisTileData* next()
+    {
+        KisTileData *current = m_iterator.getValue();
+        m_iterator.next();
+        return current;
     }
 
-    inline bool hasNext() const {
-        return m_iterator != m_begin;
+    inline bool hasNext() const
+    {
+        return m_iterator.isValid();
     }
 
-    inline bool trySwapOut(KisTileData *td) {
-        if(td->m_listIterator == m_iterator)
-            m_iterator++;
+    inline bool trySwapOut(KisTileData *td)
+    {
+        if (td == m_iterator.getValue()) {
+            m_iterator.next();
+        }
 
         return m_store->trySwapTileData(td);
     }
 
 private:
-    KisTileDataList &m_list;
-    KisTileDataListIterator m_iterator;
-    KisTileDataListIterator m_begin;
+    ConcurrentMap<int, KisTileData*> &m_map;
+    ConcurrentMap<int, KisTileData*>::Iterator m_iterator;
     KisTileDataStore *m_store;
 };
 
 class KisTileDataStoreClockIterator
 {
+//public:
+//    KisTileDataStoreClockIterator(KisTileDataListIterator startItem, KisTileDataList &list, KisTileDataStore *store)
+//        : m_list(list),
+//          m_store(store)
+//    {
+//        m_end = m_list.end();
+
+//        if(startItem == m_list.begin() ||
+//           startItem == m_end) {
+//            m_iterator = m_list.begin();
+//            m_startItem = m_end;
+//            m_endReached = true;
+//        }
+//        else  {
+//            m_startItem = startItem;
+//            m_iterator = startItem;
+//            m_endReached = false;
+//        }
+//    }
+
+//    inline KisTileData* peekNext() {
+//        if(m_iterator == m_end) {
+//            m_iterator = m_list.begin();
+//            m_endReached = true;
+//        }
+
+//        return *m_iterator;
+//    }
+
+//    inline KisTileData* next() {
+//        if(m_iterator == m_end) {
+//            m_iterator = m_list.begin();
+//            m_endReached = true;
+//        }
+
+//        return *(m_iterator++);
+//    }
+
+//    inline bool hasNext() const {
+//        return !(m_endReached && m_iterator == m_startItem);
+//    }
+
+//    inline bool trySwapOut(KisTileData *td) {
+//        if(td->m_listIterator == m_iterator)
+//            m_iterator++;
+
+//        return m_store->trySwapTileData(td);
+//    }
+
+//private:
+//    friend class KisTileDataStore;
+//    inline KisTileDataListIterator getFinalPosition() {
+//        return m_iterator;
+//    }
+
+//private:
+//    KisTileDataList &m_list;
+//    bool m_endReached;
+//    KisTileDataListIterator m_iterator;
+//    KisTileDataListIterator m_startItem;
+//    KisTileDataListIterator m_end;
+//    KisTileDataStore *m_store;
 public:
-    KisTileDataStoreClockIterator(KisTileDataListIterator startItem, KisTileDataList &list, KisTileDataStore *store)
-        : m_list(list),
+    KisTileDataStoreClockIterator(ConcurrentMap<int, KisTileData*> &map, KisTileDataStore *store)
+        : m_map(map),
           m_store(store)
     {
-        m_end = m_list.end();
-
-        if(startItem == m_list.begin() ||
-           startItem == m_end) {
-            m_iterator = m_list.begin();
-            m_startItem = m_end;
-            m_endReached = true;
-        }
-        else  {
-            m_startItem = startItem;
-            m_iterator = startItem;
-            m_endReached = false;
-        }
+        m_iterator.setMap(m_map);
     }
 
-    inline KisTileData* peekNext() {
-        if(m_iterator == m_end) {
-            m_iterator = m_list.begin();
-            m_endReached = true;
+    inline KisTileData* peekNext()
+    {
+        return m_iterator.getValue();
+    }
+
+    inline KisTileData* next()
+    {
+        KisTileData *current = m_iterator.getValue();
+        m_iterator.next();
+        return current;
+    }
+
+    inline bool hasNext() const
+    {
+        return m_iterator.isValid();
+    }
+
+    inline bool trySwapOut(KisTileData *td)
+    {
+        if (td == m_iterator.getValue()) {
+            m_iterator.next();
         }
-
-        return *m_iterator;
-    }
-
-    inline KisTileData* next() {
-        if(m_iterator == m_end) {
-            m_iterator = m_list.begin();
-            m_endReached = true;
-        }
-
-        return *(m_iterator++);
-    }
-
-    inline bool hasNext() const {
-        return !(m_endReached && m_iterator == m_startItem);
-    }
-
-    inline bool trySwapOut(KisTileData *td) {
-        if(td->m_listIterator == m_iterator)
-            m_iterator++;
 
         return m_store->trySwapTileData(td);
     }
 
 private:
-    friend class KisTileDataStore;
-    inline KisTileDataListIterator getFinalPosition() {
-        return m_iterator;
-    }
-
-private:
-    KisTileDataList &m_list;
-    bool m_endReached;
-    KisTileDataListIterator m_iterator;
-    KisTileDataListIterator m_startItem;
-    KisTileDataListIterator m_end;
+    ConcurrentMap<int, KisTileData*> &m_map;
+    ConcurrentMap<int, KisTileData*>::Iterator m_iterator;
     KisTileDataStore *m_store;
 };
 

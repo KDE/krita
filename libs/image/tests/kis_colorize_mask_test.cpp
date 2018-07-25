59cf74cb9dd43f2460b27e5e2e4c797e5cf21ca2
Dmitry Kazakov <dimula73@gmail.com>
2018-07-25 18:49:55 +0300

    Fix KisColorizeMaskTest
    We should notify the mask that our fake testing update is finished

--- libs/image/tests/kis_colorize_mask_test.cpp
+++ libs/image/tests/kis_colorize_mask_test.cpp
@@ -98,10 +98,12 @@ void KisColorizeMaskTest::test()
 
     strokes = t.mask->fetchKeyStrokesDirect();
 
+    const QRect expectedFillRect(25,25,145,150);
+
     // Check initial bounding rects
 
     QCOMPARE(t.mask->paintDevice()->exactBounds(), QRect(0,0,160,70));
-    QCOMPARE(t.mask->coloringProjection()->exactBounds(), t.internalFillRect);
+    QCOMPARE(t.mask->coloringProjection()->exactBounds(), expectedFillRect);
     QCOMPARE(t.mask->testingFilteredSource()->exactBounds(), t.refRect);
 
     QCOMPARE(strokes[0].dev->exactBounds(), QRect(50,50,10,20));
@@ -117,7 +119,7 @@ void KisColorizeMaskTest::test()
     // KIS_DUMP_DEVICE_2(t.mask->testingFilteredSource(), t.refRect, "filteredSource2", "dd");
 
     QCOMPARE(t.mask->paintDevice()->exactBounds(), QRect(5,7,160,70));
-    QCOMPARE(t.mask->coloringProjection()->exactBounds(), t.internalFillRect.translated(5, 7));
+    QCOMPARE(t.mask->coloringProjection()->exactBounds(), expectedFillRect.translated(5, 7));
     QCOMPARE(t.mask->testingFilteredSource()->exactBounds(), t.refRect);
 
     QCOMPARE(strokes[0].dev->exactBounds(), QRect(55,57,10,20));

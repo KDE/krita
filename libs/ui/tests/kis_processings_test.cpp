866281eb79b7732e2622ca41201bb7bc54fcb257
Boudewijn Rempt <boud@valdyas.org>
2018-07-26 12:34:52 +0200

    Move tests that need kritaui to libs/ui/tests

--- libs/ui/tests/kis_processings_test.cpp
+++ libs/ui/tests/kis_processings_test.cpp
@@ -23,7 +23,7 @@
 #include "kis_undo_stores.h"
 #include "kis_processing_applicator.h"
 #include "processing/kis_crop_processing_visitor.h"
-
+#include <kis_filter_configuration.h>
 #include "testutil.h"
 #include "qimage_based_test.h"
 #include "kis_filter_strategy.h"

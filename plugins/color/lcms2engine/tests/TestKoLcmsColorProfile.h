d49e3237215334d90de91b695f8734f7ead9878d
Boudewijn Rempt <boud@valdyas.org>
2018-08-07 11:59:20 +0200

    Remove two tests that have been commented out for a decade or longer

--- plugins/color/lcms2engine/tests/TestKoLcmsColorProfile.h
+++ plugins/color/lcms2engine/tests/TestKoLcmsColorProfile.h
@@ -6,9 +6,6 @@
 class TestKoLcmsColorProfile : public QObject
 {
     Q_OBJECT
-    // commented out since I don't know when...
-    void testChromaticitiesFromProfile();
-    void testProfileCreationFromChromaticities();
 private Q_SLOTS:
     void testConversion();
     void testProofingConversion();

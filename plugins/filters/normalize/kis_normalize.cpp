797d688ab49ea01a4cdf5da6228cbd3b609a45c7
Dmitry Kazakov <dimula73@gmail.com>
2018-08-08 15:30:23 +0300

    Fix KisCrashFilterTest
    1) Alpha colro space is never used in filters
    2) Default bounds of the device do really matter
    3) To work with weird color spaces we should use
       KISTEST_MAIN to ensure the necessary plugins are
       loaded

--- plugins/filters/normalize/kis_normalize.cpp
+++ plugins/filters/normalize/kis_normalize.cpp
@@ -75,6 +75,13 @@ KisNormalizeTransformation::KisNormalizeTransformation(const KoColorSpace* cs) :
 
 void KisNormalizeTransformation::transform(const quint8* src, quint8* dst, qint32 nPixels) const
 {
+    // if the color space is not RGBA o something like that, just
+    // pass the values through
+    if (m_colorSpace->channelCount() != 4) {
+        memcpy(dst, src, nPixels * m_colorSpace->pixelSize());
+        return;
+    }
+
     QVector3D normal_vector;
     QVector<float> channelValues(4);
     //if (m_colorSpace->colorDepthId().id()!="F16" && m_colorSpace->colorDepthId().id()!="F32" && m_colorSpace->colorDepthId().id()!="F64") {

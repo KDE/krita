6b490765e7ae82f6836316f6093072dbf601733e
Dmitry Kazakov <dimula73@gmail.com>
2018-08-08 17:35:24 +0300

    Port DistanceInformationRegistrar into move semantics
    We shouldn't rely on non-guaranteed behavior of the compiler
    about RVO optimization. Let's just use move-semantics instead.

--- libs/image/brushengine/kis_paint_information.cc
+++ libs/image/brushengine/kis_paint_information.cc
@@ -160,10 +160,18 @@ DistanceInformationRegistrar(KisPaintInformation *_p, KisDistanceInformation *di
     p->d->registerDistanceInfo(distanceInfo);
 }
 
+KisPaintInformation::DistanceInformationRegistrar::DistanceInformationRegistrar(KisPaintInformation::DistanceInformationRegistrar &&rhs)
+    : p(0)
+{
+    std::swap(p, rhs.p);
+}
+
 KisPaintInformation::DistanceInformationRegistrar::
 ~DistanceInformationRegistrar()
 {
-    p->d->unregisterDistanceInfo();
+    if (p) {
+        p->d->unregisterDistanceInfo();
+    }
 }
 
 KisPaintInformation::KisPaintInformation(const QPointF & pos,

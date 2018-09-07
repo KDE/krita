0c175b33d833ccdae2de0ef344ac4c33d6a2d282
Boudewijn Rempt <boud@valdyas.org>
2018-09-07 16:23:15 +0200

    Revert "Make KisSvgTest extremely liberal"
    This reverts commit ed1a21b3e9d80cab9ffacbf3a679a67711f41b2c.

--- plugins/impex/svg/tests/kis_svg_test.cpp
+++ plugins/impex/svg/tests/kis_svg_test.cpp
@@ -33,7 +33,7 @@
 
 void KisSvgTest::testFiles()
 {
-    TestUtil::testFiles(QString(FILES_DATA_DIR) + "/sources", QStringList(), QString(), 30, 50);
+    TestUtil::testFiles(QString(FILES_DATA_DIR) + "/sources", QStringList(), QString(), 5);
 }
 
 KISTEST_MAIN(KisSvgTest)

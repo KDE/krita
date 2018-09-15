12b52e3751b5a71cb1f77515337abf4bb717da20
Boudewijn Rempt <boud@valdyas.org>
2018-09-15 16:55:28 +0200

    Show the zoom widget when switching documents
    Somehow, the widget had become invisible.
    
    BUG:398099

--- libs/ui/kis_statusbar.cc
+++ libs/ui/kis_statusbar.cc
@@ -141,12 +141,13 @@ void KisStatusBar::setView(QPointer<KisView> imageView)
     if (m_imageView == imageView) {
         return;
     }
+
     if (m_imageView) {
         m_imageView->disconnect(this);
         removeStatusBarItem(m_imageView->zoomManager()->zoomActionWidget());
-
         m_imageView = 0;
     }
+
     if (imageView) {
         m_imageView = imageView;
 
@@ -166,13 +167,13 @@ void KisStatusBar::setView(QPointer<KisView> imageView)
 void KisStatusBar::addStatusBarItem(QWidget *widget, int stretch, bool permanent)
 {
     StatusBarItem sbItem(widget);
-
     if (permanent) {
         m_statusBar->addPermanentWidget(widget, stretch);
     }
     else {
         m_statusBar->addWidget(widget, stretch);
     }
+    widget->setVisible(true);
     m_statusBarItems.append(sbItem);
 }
 

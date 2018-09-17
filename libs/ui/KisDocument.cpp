52d9592fccf2864d6bde22267d35f343191a6933
Dmitry Kazakov <dimula73@gmail.com>
2018-09-17 14:31:36 +0300

    Fix undo in the cloned KisDocument
    We should always initialize KisDocumentUndoAdapter in the
    image :)
    
    BUG:398730

--- libs/ui/KisDocument.cpp
+++ libs/ui/KisDocument.cpp
@@ -1750,6 +1750,7 @@ void KisDocument::setCurrentImage(KisImageSP image, bool forceInitialUpdate)
 {
     if (d->image) {
         // Disconnect existing sig/slot connections
+        d->image->setUndoStore(new KisDumbUndoStore());
         d->image->disconnect(this);
         d->shapeController->setImage(0);
         d->image = 0;
@@ -1758,6 +1759,7 @@ void KisDocument::setCurrentImage(KisImageSP image, bool forceInitialUpdate)
     if (!image) return;
 
     d->setImageAndInitIdleWatcher(image);
+    d->image->setUndoStore(new KisDocumentUndoStore(this));
     d->shapeController->setImage(image);
     setModified(false);
     connect(d->image, SIGNAL(sigImageModified()), this, SLOT(setImageModified()), Qt::UniqueConnection);

e6f584ed8a97e77604b74ebef35a9ece8288539e
Scott Petrovic <scottpetrovic@gmail.com>
2018-09-24 18:49:20 -0500

    Clean up overlay setting on smudge engine

--- plugins/paintops/colorsmudge/kis_overlay_mode_option.h
+++ plugins/paintops/colorsmudge/kis_overlay_mode_option.h
@@ -19,7 +19,7 @@
 #define _KIS_OVERLAYMODE_OPTION_H_
 
 #include <QLabel>
-
+#include <QLayout>
 #include <kis_paintop_option.h>
 
 #include <brushengine/kis_paintop_lod_limitations.h>
@@ -57,14 +57,27 @@ class KisOverlayModeOptionWidget: public KisOverlayModeOption
 {
 public:
     KisOverlayModeOptionWidget() {
-        QLabel* label = new QLabel(
-            i18n("Paints on the current layer\n\
-            but uses all layers that are currently visible for smudge input\n\
-            NOTE: This mode is only able to work correctly with a fully opaque background")
-        );
 
-        label->setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
-        setConfigurationPage(label);
+        QWidget* mainWidget = new QWidget();
+
+        QLabel* label = new QLabel(
+                i18n("Paints on the current layer but uses all layers that are currently visible for smudge input.")
+            );
+        label->setWordWrap(true);
+
+        QLabel* label2 = new QLabel(
+                i18n("NOTE: This mode is only able to work correctly with a fully opaque background")
+            );
+         label2->setWordWrap(true);
+
+         QVBoxLayout* verticalLayout = new QVBoxLayout();
+         verticalLayout->addWidget(label);
+         verticalLayout->addWidget(label2);
+
+         verticalLayout->addStretch(1); // pushes things to the top
+
+         mainWidget->setLayout(verticalLayout);
+         setConfigurationPage(mainWidget);
     }
 };
 

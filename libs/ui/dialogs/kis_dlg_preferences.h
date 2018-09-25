ed3cb8475a69f09c2c933ae15e33a23f5a57b075
Boudewijn Rempt <boud@valdyas.org>
2018-09-25 09:00:10 +0200

    Remember the last opened settings page

--- libs/ui/dialogs/kis_dlg_preferences.h
+++ libs/ui/dialogs/kis_dlg_preferences.h
@@ -24,6 +24,8 @@
 
 #include <QWidget>
 #include <QButtonGroup>
+#include <QMap>
+#include <QString>
 
 #include <kpagedialog.h>
 #include <kis_config.h>
@@ -337,6 +339,8 @@ protected:
     FullscreenSettingsTab *m_fullscreenSettings;
     KisInputConfigurationPage *m_inputConfiguration;
     KoConfigAuthorPage *m_authorPage;
+
+    QList<KPageWidgetItem*> m_pages;
 
 protected Q_SLOTS:
 

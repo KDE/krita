b68419b0395002d9043da6bacc2a7510bbce3844
Boudewijn Rempt <boud@valdyas.org>
2018-10-08 10:41:35 +0200

    Check whether the override language is RTL or LTR
    If we have an override language, Qt doesn't accept that, even though
    we set the entire environment correctly. This means we get translated
    dialog box buttons, but we cannot do anything about that. This attempts
    to at least fix the application layout.
    
    CCBUG:399440

--- krita/main.cc
+++ krita/main.cc
@@ -224,13 +224,20 @@ extern "C" int main(int argc, char **argv)
     // selection dialog.
 
     dbgKrita << "Override language:" << language;
-
+    bool rightToLeft = false;
     if (!language.isEmpty()) {
         KLocalizedString::setLanguages(language.split(":"));
         // And override Qt's locale, too
         qputenv("LANG", language.split(":").first().toLocal8Bit());
         QLocale locale(language.split(":").first());
         QLocale::setDefault(locale);
+
+        const QStringList rtlLanguages = QStringList()
+                << "ar" << "dv" << "he" << "ha" << "ku" << "fa" << "ps" << "ur" << "yi";
+
+        if (rtlLanguages.contains(language.split(':').first())) {
+            rightToLeft = true;
+        }
     }
     else {
         dbgKrita << "Qt UI languages:" << QLocale::system().uiLanguages() << qgetenv("LANG");
@@ -279,6 +286,14 @@ extern "C" int main(int argc, char **argv)
 
     // first create the application so we can create a pixmap
     KisApplication app(key, argc, argv);
+    if (!language.isEmpty()) {
+        if (rightToLeft) {
+            app.setLayoutDirection(Qt::RightToLeft);
+        }
+        else {
+            app.setLayoutDirection(Qt::LeftToRight);
+        }
+    }
     KLocalizedString::setApplicationDomain("krita");
 
     dbgKrita << "Available translations" << KLocalizedString::availableApplicationTranslations();
@@ -289,10 +304,10 @@ extern "C" int main(int argc, char **argv)
     QDir appdir(KoResourcePaths::getApplicationRoot());
     QString path = qgetenv("PATH");
     qputenv("PATH", QFile::encodeName(appdir.absolutePath() + "/bin" + ";"
-                                    + appdir.absolutePath() + "/lib" + ";"
-                                    + appdir.absolutePath() + "/Frameworks" + ";"
-                                    + appdir.absolutePath() + ";"
-                                    + path));
+                                      + appdir.absolutePath() + "/lib" + ";"
+                                      + appdir.absolutePath() + "/Frameworks" + ";"
+                                      + appdir.absolutePath() + ";"
+                                      + path));
 
     dbgKrita << "PATH" << qgetenv("PATH");
 #endif
@@ -334,9 +349,9 @@ extern "C" int main(int argc, char **argv)
     }
 
 #if defined HAVE_X11
-    #ifndef USE_QT_XCB
-        app.installNativeEventFilter(KisXi2EventFilter::instance());
-    #endif
+#ifndef USE_QT_XCB
+    app.installNativeEventFilter(KisXi2EventFilter::instance());
+#endif
 #endif
 
     app.installEventFilter(KisQtWidgetsTweaker::instance());
@@ -433,10 +448,10 @@ extern "C" int main(int argc, char **argv)
 
     // Set up remote arguments.
     QObject::connect(&app, SIGNAL(messageReceived(QByteArray,QObject*)),
-                    &app, SLOT(remoteArguments(QByteArray,QObject*)));
+                     &app, SLOT(remoteArguments(QByteArray,QObject*)));
 
     QObject::connect(&app, SIGNAL(fileOpenRequest(QString)),
-                    &app, SLOT(fileOpenRequested(QString)));
+                     &app, SLOT(fileOpenRequested(QString)));
 
     int state = app.exec();
 

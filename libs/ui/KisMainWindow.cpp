45e84f480bb797ba127ff835ed974bb8f08ae884
Boudewijn Rempt <boud@valdyas.org>
2018-10-12 12:23:22 +0200

    Only delete the close action, we still need the always on top action

--- libs/ui/KisMainWindow.cpp
+++ libs/ui/KisMainWindow.cpp
@@ -2127,8 +2127,9 @@ void KisMainWindow::subWindowActivated()
         if (menu) {
             Q_FOREACH (QAction *action, menu->actions()) {
                 action->setShortcut(QKeySequence());
-                action->deleteLater();
+
             }
+            menu->actions().last()->deleteLater();
         }
     }
 

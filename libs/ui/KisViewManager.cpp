dee63d46b421a9658d97d94e24c68c9484b4e2de
Scott Petrovic <scottpetrovic@gmail.com>
2018-11-01 20:01:38 -0500

    BUG:399648 Fix for preset changing incorrectly

--- libs/ui/KisViewManager.cpp
+++ libs/ui/KisViewManager.cpp
@@ -328,10 +328,8 @@ KisViewManager::~KisViewManager()
 {
     KisConfig cfg;
     if (resourceProvider() && resourceProvider()->currentPreset()) {
-        cfg.writeEntry("LastPreset", resourceProvider()->currentPreset()->name());
         cfg.writeKoColor("LastForeGroundColor",resourceProvider()->fgColor());
         cfg.writeKoColor("LastBackGroundColor",resourceProvider()->bgColor());
-
     }
 
     cfg.writeEntry("baseLength", KoResourceItemChooserSync::instance()->baseLength());
@@ -377,6 +375,11 @@ void KisViewManager::slotViewRemoved(KisView *view)
 
     if (viewCount() == 0) {
         d->statusBar.hideAllStatusBarItems();
+    }
+
+    KisConfig cfg(false);
+    if (resourceProvider() && resourceProvider()->currentPreset()) {
+        cfg.writeEntry("LastPreset", resourceProvider()->currentPreset()->name());
     }
 }
 

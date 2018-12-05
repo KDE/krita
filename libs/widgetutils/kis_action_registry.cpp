33aa12d4f8c6adfbbea9bd6e35e76aa745e220d9
Boudewijn Rempt <boud@valdyas.org>
2018-12-05 10:14:08 +0100

    We always want to see this warning

--- libs/widgetutils/kis_action_registry.cpp
+++ libs/widgetutils/kis_action_registry.cpp
@@ -350,7 +350,7 @@ void KisActionRegistry::Private::loadActionFiles()
                     }
 
                     else if (actionInfoList.contains(name)) {
-                        errAction << "NOT COOL: Duplicated action name from xml data: " << name;
+                        qWarning << "NOT COOL: Duplicated action name from xml data: " << name;
                     }
 
                     else {

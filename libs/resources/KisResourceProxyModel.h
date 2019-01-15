b6393ca667b1290e54887468ff7c2ef44e4d1835
Boudewijn Rempt <boud@valdyas.org>
2019-01-15 18:13:27 +0100

    Begin skeleton for the proxy model
    This model will implement support for the column hack in the current
    resource selector widget.

---
+++ libs/resources/KisResourceProxyModel.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019 Boudewijn Rempt <boud@valdyas.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef KISRESOURCEPROXYMODEL_H
+#define KISRESOURCEPROXYMODEL_H
+
+#include <QAbstractProxyModel>
+#include <QObject>
+
+class KisResourceProxyModel : public QAbstractProxyModel
+{
+    Q_OBJECT
+public:
+    KisResourceProxyModel();
+    ~KisResourceProxyModel() override;
+    // QAbstractItemModel interface
+public:
+    QModelIndex index(int row, int column, const QModelIndex &parent) const override;
+    QModelIndex parent(const QModelIndex &child) const override;
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+
+    // QAbstractProxyModel interface
+public:
+    QModelIndex mapToSource(const QModelIndex &proxyIndex) const override;
+    QModelIndex mapFromSource(const QModelIndex &sourceIndex) const override;
+};
+
+#endif // KISRESOURCEPROXYMODEL_H

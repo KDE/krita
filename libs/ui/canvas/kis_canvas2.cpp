75c8ec960f48cd3d15126ef84d15e678e2d0b502
Dmitry Kazakov <dimula73@gmail.com>
2019-01-20 16:15:52 +0300

    Add more docs

--- libs/ui/canvas/kis_canvas2.cpp
+++ libs/ui/canvas/kis_canvas2.cpp
@@ -615,6 +615,11 @@ void KisCanvas2::resetCanvas(bool useOpenGL)
 
 void KisCanvas2::startUpdateInPatches(const QRect &imageRect)
 {
+    /**
+     * We don't do patched loading for openGL canvas, becasue it loads
+     * the tiles, which are bascially "patches". Therefore, big chunks
+     * of memory are never allocated.
+     */
     if (m_d->currentCanvasIsOpenGL) {
         startUpdateCanvasProjection(imageRect);
     } else {

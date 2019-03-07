4edfefc4f4410097f2a63c04396ee737d96f6bd8
Rebecca Breu <rebecca@rbreu.de>
2019-03-07 20:57:14 +0100

    Python code checks: highpass, script_starter, scriptdocker
    Summary: highpass and scriptdocker aren't currently packaged, so I didn't test my changes there.
    
    Reviewers: #krita
    
    Tags: #krita
    
    Differential Revision: https://phabricator.kde.org/D19601

--- plugins/python/scriptdocker/scriptdocker.py
+++ plugins/python/scriptdocker/scriptdocker.py
@@ -1,17 +1,19 @@
-'''
-This script is licensed CC 0 1.0, so that you can learn from it.
+# This script is licensed CC 0 1.0, so that you can learn from it.
 
------- CC 0 1.0 ---------------
+# ------ CC 0 1.0 ---------------
 
-The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
+# The person who associated a work with this deed has dedicated the
+# work to the public domain by waiving all of his or her rights to the
+# work worldwide under copyright law, including all related and
+# neighboring rights, to the extent allowed by law.
 
-You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.
+# You can copy, modify, distribute and perform the work, even for
+# commercial purposes, all without asking permission.
 
-https://creativecommons.org/publicdomain/zero/1.0/legalcode
-'''
-rom PyQt5.QtWidgets import (QWidget, QVBoxLayout, QListView, QFormLayout,
-                             QHBoxLayout, QPushButton, QLineEdit, QListWidget)
-from PyQt5.QtCore import QObject
+# https://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QFormLayout,
+                             QHBoxLayout, QPushButton, QLineEdit)
 import krita
 
 
@@ -47,7 +49,9 @@ class ScriptDocker(krita.DockWidget):
         directorySelectorLayout.addWidget(directoryTextField)
         directorySelectorLayout.addWidget(directoryDialogButton)
 
-        self.scriptsLayout.addRow(str(i18n("Script {0}")).format(self.scriptsLayout.rowCount() + 1), directorySelectorLayout)
+        self.scriptsLayout.addRow(
+            str(i18n("Script {0}")).format(self.scriptsLayout.rowCount() + 1),
+            directorySelectorLayout)
 
     def test(self):
         obj = self.sender()
@@ -61,6 +65,3 @@ class ScriptDocker(krita.DockWidget):
 
     def writeSettings(self):
         pass
-
-
-Application.addDockWidgetFactory(krita.DockWidgetFactory("scriptdocker", krita.DockWidgetFactoryBase.DockRight, ScriptDocker))

eff0c7010ac3dbef4c8e1ec1e434ac689d1532e2
Alvin Wong <alvinhochun@gmail.com>
2019-04-02 16:14:54 +0800

    Fix wrong DPI when moving window across screens
    This fixes a bug causing the wrong DPI being used when moving the window
    from a screen to another screen to the left. Using the top-left position
    of the window to determine screen changes doesn't work well since the
    device pixel ratio of a window does not change until the window is
    roughly halfway across.
    
    Maniphest Tasks: T2299
    Differential Revision: https://phabricator.kde.org/D20171

--- libs/ui/KisMainWindow.h
+++ libs/ui/KisMainWindow.h
@@ -419,6 +419,7 @@ private Q_SLOTS:
     void showManual();
     void switchTab(int index);
 
+    void windowScreenChanged(QScreen *screen);
 
 protected:
 
@@ -430,8 +431,6 @@ protected:
     void dropEvent(QDropEvent * event) override;
     void dragMoveEvent(QDragMoveEvent * event) override;
     void dragLeaveEvent(QDragLeaveEvent * event) override;
-
-    void moveEvent(QMoveEvent *e) override;
 
 
 private:

bcbcf3661cefdb87b9521a888f2f908be107e718
Scott Petrovic <scottpetrovic@gmail.com>
2019-04-03 21:50:48 -0500

    Make checkbox borders the same as text color. They were still hidden on some themes

--- libs/ui/dialogs/kis_dlg_layer_style.cpp
+++ libs/ui/dialogs/kis_dlg_layer_style.cpp
@@ -142,7 +142,7 @@ KisDlgLayerStyle::KisDlgLayerStyle(KisPSDLayerStyleSP layerStyle, KisCanvasResou
     // improve the checkbox visibility by altering the style sheet list a bit
     // the dark themes make them hard to see
     QPalette newPalette = palette();
-    newPalette.setColor(QPalette::Active, QPalette::Background, palette().text().color().darker(240) );
+    newPalette.setColor(QPalette::Active, QPalette::Background, palette().text().color() );
     wdgLayerStyles.lstStyleSelector->setPalette(newPalette);
 
 

70ac5d1466cc286273e17186f91a13fa515c3e51
Scott Petrovic <scottpetrovic@gmail.com>
2019-04-13 17:29:47 -0500

    BUG:406233 Increase control point size a bit for easier grabbing

--- libs/ui/widgets/kis_curve_widget.cpp
+++ libs/ui/widgets/kis_curve_widget.cpp
@@ -350,14 +350,21 @@ void KisCurveWidget::paintEvent(QPaintEvent *)
             curveX = d->m_curve.points().at(i).x();
             curveY = d->m_curve.points().at(i).y();
 
+            int handleSize = 12; // how big should control points be (diamater size)
+
             if (i == d->m_grab_point_index) {
-                p.setPen(QPen(appPalette.color(QPalette::Text), 6, Qt::SolidLine));
-                p.drawEllipse(QRectF(curveX * wWidth - 2,
-                                     wHeight - 2 - curveY * wHeight, 4, 4));
+                // active point is slightly more "bold"
+                p.setPen(QPen(appPalette.color(QPalette::Text), 4, Qt::SolidLine));
+                p.drawEllipse(QRectF(curveX * wWidth - (handleSize*0.5),
+                                     wHeight - (handleSize*0.5) - curveY * wHeight,
+                                     handleSize,
+                                     handleSize));
             } else {
                 p.setPen(QPen(appPalette.color(QPalette::Text), 2, Qt::SolidLine));
-                p.drawEllipse(QRectF(curveX * wWidth - 3,
-                                     wHeight - 3 - curveY * wHeight, 6, 6));
+                p.drawEllipse(QRectF(curveX * wWidth - (handleSize*0.5),
+                                     wHeight - (handleSize*0.5) - curveY * wHeight,
+                                     handleSize,
+                                     handleSize));
             }
         }
     }

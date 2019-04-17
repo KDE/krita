17f2c8da37ba96b1bac7a8fcad497c3e6f457861
Agata Cacko <tamtamy.tymona@gmail.com>
2019-04-17 09:56:00 +0200

    Fix license info in KisSpinBoxSplineConverter

--- libs/ui/tests/KisSpinBoxSplineUnitConverterTest.cpp
+++ libs/ui/tests/KisSpinBoxSplineUnitConverterTest.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015 Agata Cacko <cacko.azg@gmail.com>
+ *  Copyright (c) 2019 Agata Cacko <cacko.azg@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

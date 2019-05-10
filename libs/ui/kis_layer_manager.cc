e0dec40401a21d910fec2eb9c069eeb0155cea6c
Tusooa Zhu <tusooa@vista.aero>
2019-05-10 12:46:29 -0400

    Add a dialog to change the source of clone layers
    This commit adds an action named `Set Copy From' to the context menu
    of the Layers Docker, if one or more clone layers are selected. The
    action pops up a dialog that can change the `Copy From' property of
    selected clone layers.
    
    The dialog previews the changes to these layers.
    If there is any change to the document when the dialog is open, the
    changes in the dialog will be applied and the dialog closed.

--- libs/ui/kis_layer_manager.cc
+++ libs/ui/kis_layer_manager.cc
@@ -73,6 +73,7 @@
 #include "dialogs/kis_dlg_generator_layer.h"
 #include "dialogs/kis_dlg_file_layer.h"
 #include "dialogs/kis_dlg_layer_style.h"
+#include "dialogs/KisDlgChangeCloneSource.h"
 #include "kis_filter_manager.h"
 #include "kis_node_visitor.h"
 #include "kis_paint_layer.h"
@@ -350,6 +351,35 @@ void KisLayerManager::layerProperties()
         dialog->show();
 
     }
+}
+
+void KisLayerManager::changeCloneSource()
+{
+    QList<KisNodeSP> selectedNodes = m_view->nodeManager()->selectedNodes();
+    if (selectedNodes.isEmpty()) {
+        return;
+    }
+
+    QList<KisCloneLayerSP> cloneLayers;
+    KisNodeSP node;
+    Q_FOREACH (node, selectedNodes) {
+        KisCloneLayerSP cloneLayer(qobject_cast<KisCloneLayer *>(node.data()));
+        if (cloneLayer) {
+            cloneLayers << cloneLayer;
+        }
+    }
+
+    if (cloneLayers.isEmpty()) {
+        return;
+    }
+
+    KisDlgChangeCloneSource *dialog = new KisDlgChangeCloneSource(cloneLayers, m_view);
+    dialog->setCaption(i18n("Change Clone Layer"));
+    dialog->resize(dialog->minimumSizeHint());
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    Qt::WindowFlags flags = dialog->windowFlags();
+    dialog->setWindowFlags(flags | Qt::WindowStaysOnTopHint | Qt::Dialog);
+    dialog->show();
 }
 
 void KisLayerManager::convertNodeToPaintLayer(KisNodeSP source)

fb832383eb097d8cd939732f6b2474fcd7ee8ea5
Ivan Yossi <ghevan@gmail.com>
2019-05-10 15:29:07 -0500

    Add missing License headers

--- libs/widgetutils/tests/KisDialogStateSaverTest.h
+++ libs/widgetutils/tests/KisDialogStateSaverTest.h
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2019 Boudewijn Rempt <boud@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #ifndef KISDIALOGSTATESAVERTEST_H
 #define KISDIALOGSTATESAVERTEST_H
 

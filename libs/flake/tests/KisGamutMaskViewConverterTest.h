c2dce5a46c0aef88f40bd37ae2069395d5a62516
Anna Medonosova <anna.medonosova@gmail.com>
2019-05-14 08:06:48 +0200

    Basic unittests for gamut masking classes

---
+++ libs/flake/tests/KisGamutMaskViewConverterTest.h
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2019 Anna Medonosova <anna.medonosova@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KISGAMUTMASKVIEWCONVERTERTEST_H
+#define KISGAMUTMASKVIEWCONVERTERTEST_H
+
+#include <QObject>
+#include <QTest>
+
+class KisGamutMaskViewConverterTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit KisGamutMaskViewConverterTest(QObject *parent = nullptr);
+
+private Q_SLOTS:
+    void testDocumentToViewX();
+    void testDocumentToViewX_data();
+
+    void testDocumentToViewY();
+    void testDocumentToViewY_data();
+
+    void testViewToDocumentX();
+    void testViewToDocumentX_data();
+
+    void testViewToDocumentY();
+    void testViewToDocumentY_data();
+};
+
+#endif // KISGAMUTMASKVIEWCONVERTERTEST_H

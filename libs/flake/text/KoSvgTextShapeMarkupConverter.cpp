770a61a1ef477bc33a531eb30310dd902ed0f704
Ivan Yossi <ghevan@gmail.com>
2019-05-15 15:40:36 -0500

    Fix: No underline word is ignored if all text was underlined
    formatDifference removes global styles from local set, this is ok for
    most properties, but some defaults make it impossible to set any
    style combination if we remove the property.

--- libs/flake/text/KoSvgTextShapeMarkupConverter.cpp
+++ libs/flake/text/KoSvgTextShapeMarkupConverter.cpp
@@ -683,9 +683,10 @@ bool KoSvgTextShapeMarkupConverter::convertDocumentToSvg(const QTextDocument *do
 
             for (int c = 0; c<texts.size(); c++) {
                 QTextCharFormat diff = formatDifference(charFormats.at(c), mostCommonCharFormat).toCharFormat();
-                if (!diff.properties().isEmpty()) {
+                const QString subStyle = style(diff, QTextBlockFormat(), mostCommonCharFormat);
+                if (!subStyle.isEmpty()) {
                     svgWriter.writeStartElement("tspan");
-                    svgWriter.writeAttribute("style", style(diff, QTextBlockFormat(), mostCommonCharFormat));
+                    svgWriter.writeAttribute("style", subStyle);
                     svgWriter.writeCharacters(texts.at(c));
                     svgWriter.writeEndElement();
                 } else {
@@ -1037,26 +1038,29 @@ QString KoSvgTextShapeMarkupConverter::style(QTextCharFormat format,
         }
     }
 
-    if (format.underlineStyle() != QTextCharFormat::NoUnderline &&
-        format.underlineStyle() != QTextCharFormat::SpellCheckUnderline) {
+    if (format.underlineStyle() != QTextCharFormat::SpellCheckUnderline) {
+        if(format.underlineStyle() != mostCommon.underlineStyle()){
+            QStringList values;
+            QString c;
 
-        QStringList values;
-        QString c;
+            if (format.fontUnderline()) {
+                values.append("underline");
+            }
+            if (format.fontOverline()) {
+                values.append("overline");
+            }
+            if (format.fontStrikeOut()) {
+                values.append("line-through");
+            }
+            if (values.isEmpty()) {
+                values.append("none");
+            }
+            c.append("text-decoration").append(":")
+                    .append(values.join(" "));
 
-        if (format.fontUnderline()) {
-            values.append("underline");
-        }
-        if(format.fontOverline()) {
-            values.append("overline");
-        }
-        if(format.fontStrikeOut()) {
-            values.append("line-through");
-        }
-        c.append("text-decoration").append(":")
-                .append(values.join(" "));
-
-        if (!values.isEmpty()) {
-            style.append(c);
+            if (!values.isEmpty()) {
+                style.append(c);
+            }
         }
     }
 
@@ -1237,8 +1241,13 @@ QTextFormat KoSvgTextShapeMarkupConverter::formatDifference(QTextFormat test, QT
     const QMap<int, QVariant> props = reference.properties();
     for (QMap<int, QVariant>::ConstIterator it = props.begin(), end = props.end();
          it != end; ++it)
-        if (it.value() == test.property(it.key()))
+        if (it.value() == test.property(it.key())) {
+            // Some props must not be removed as default state gets in the way.
+            if (it.key() == 0x2023) { // TextUnderlineStyle
+                continue;
+            }
             diff.clearProperty(it.key());
+        }
     return diff;
 }
 

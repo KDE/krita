fef5185cb436f3499659e444dc959eec765c800e
Boudewijn Rempt <boud@valdyas.org>
2019-05-18 14:36:45 +0200

    Revert "Recover forbidding rule for adding masks to the root layer"
    This reverts commit ad3649935d0a2a7c9b6e4f6fecc421c69e53c3ca.
    
        Revert this for now so the builds made during the weekend are not
        so very broken.
    
        CCBUG:407375
        CCBUG:407665

--- libs/ui/kis_mimedata.cpp
+++ libs/ui/kis_mimedata.cpp
@@ -101,7 +101,6 @@ KisDocument *createDocument(QList<KisNodeSP> nodes, KisImageSP srcImage)
     }
 
     KisImageSP image = new KisImage(0, rc.width(), rc.height(), nodes.first()->colorSpace(), nodes.first()->name());
-    image->setAllowMasksOnRootNode(true);
 
     {
         KisImageBarrierLockerWithFeedbackAllowNull locker(srcImage);

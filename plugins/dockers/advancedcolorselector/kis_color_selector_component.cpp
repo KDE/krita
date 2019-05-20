KDE/krita

commit 8441c26fbda908aa3bce3604a63b22df69db2372

Fix advanced color selector to adjust color when exposure changes
When "Lock current color" feature is off, the color selector should
adjust its view when exposure changes.

plugins/dockers/advancedcolorselector/kis_color_selector_component.cpp
@@ -77,9 +77,11 @@ void KisColorSelectorComponent::mouseEvent(int x, int y)
     int newX=qBound(0, (x-m_x), width());
     int newY=qBound(0, (y-m_y), height());
 
-    selectColor(newX, newY);
-    m_lastX=newX;
-    m_lastY=newY;
+    if (allowsColorSelectionAtPoint(QPoint(x, y))) {
+        m_lastSelectedColor = selectColor(newX, newY);
+        m_lastX=newX;
+        m_lastY=newY;
+    }
 }
 
 const KoColorSpace* KisColorSelectorComponent::colorSpace() const
@@ -92,6 +94,7 @@ const KoColorSpace* KisColorSelectorComponent::colorSpace() const
 void KisColorSelectorComponent::setDirty()
 {
     m_dirty = true;
+    setColor(m_lastSelectedColor);
 }
 
 void KisColorSelectorComponent::setGamutMask(KoGamutMask *gamutMask)
@@ -131,6 +134,11 @@ bool KisColorSelectorComponent::containsPointInComponentCoords(int x, int y) con
         return true;
     else
         return false;
+}
+
+bool KisColorSelectorComponent::allowsColorSelectionAtPoint(const QPoint &pt) const
+{
+    return true;
 }
 
 KoColor KisColorSelectorComponent::currentColor()
@@ -228,6 +236,11 @@ void KisColorSelectorComponent::setConfiguration(Parameter param, Type type)
 {
     m_parameter = param;
     m_type = type;
+}
+
+void KisColorSelectorComponent::setColor(const KoColor &color)
+{
+    m_lastSelectedColor = color;
 }
 
 void KisColorSelectorComponent::setLastMousePosition(int x, int y)

plugins/dockers/advancedcolorselector/kis_color_selector.cpp
@@ -194,8 +194,6 @@ void KisColorSelector::hasAtLeastOneDocument(bool value)
 
 void KisColorSelector::reset()
 {
-    KisColorSelectorBase::reset();
-
     if (m_mainComponent) {
         m_mainComponent->setDirty();
     }
@@ -203,6 +201,8 @@ void KisColorSelector::reset()
     if (m_subComponent) {
         m_subComponent->setDirty();
     }
+
+    KisColorSelectorBase::reset();
 }
 
 void KisColorSelector::paintEvent(QPaintEvent* e)

plugins/dockers/advancedcolorselector/kis_color_selector_component.h
@@ -57,7 +57,7 @@ public:
     virtual void setColor(const KoColor& color) = 0;
 
     /// force subsequent redraw of the component
-    void setDirty();
+    virtual void setDirty();
 
     /// returns true, if this component wants to grab the mouse (normally true, if containsPoint returns true)
     bool wantsGrab(int x, int y) {return containsPointInComponentCoords(x-m_x, y-m_y);}
@@ -93,6 +93,11 @@ protected:
     /// values for the subclasses are provided in component coordinates, eg (0,0) is top left of component
     virtual bool containsPointInComponentCoords(int x, int y) const;
 
+    /// a subclass can implement this method to note that the point, although it is in
+    /// containsPointInComponentCoords area, still cannot be selected as a color (e.g.
+    /// it is masked out). Default implementation always returns true.
+    virtual bool allowsColorSelectionAtPoint(const QPoint &pt) const;
+
     // Workaround for Bug 287001
     void setLastMousePosition(int x, int y);
 
@@ -120,6 +125,7 @@ private:
     int m_height;
     bool m_dirty;
     const KoColorSpace* m_lastColorSpace;
+    KoColor m_lastSelectedColor;
 };
 
 #endif // KIS_COLOR_SELECTOR_COMPONENT_H

plugins/dockers/advancedcolorselector/kis_color_selector_ring.cpp
@@ -117,7 +117,7 @@ KoColor KisColorSelectorRing::selectColor(int x, int y)
     m_lastHue=hue;
     emit update();
 
-    return KoColor();
+    return m_parent->converter()->fromHsvF(hue, 1.0, 1.0);
 }
 
 void KisColorSelectorRing::setColor(const KoColor &color)
@@ -133,6 +133,8 @@ void KisColorSelectorRing::setColor(const KoColor &color)
     }
 
     emit update();
+
+    KisColorSelectorComponent::setColor(color);
 }
 
 void KisColorSelectorRing::paintCache()

plugins/dockers/advancedcolorselector/kis_color_selector_simple.cpp
@@ -257,6 +257,7 @@ void KisColorSelectorSimple::setColor(const KoColor &color)
     emit update();
     //Workaround for bug 317648
     setLastMousePosition((m_lastClickPos.x()*width()), (m_lastClickPos.y()*height()));
+    KisColorSelectorComponent::setColor(color);
 }
 
 void KisColorSelectorSimple::paint(QPainter* painter)

plugins/dockers/advancedcolorselector/kis_color_selector_triangle.cpp
@@ -136,6 +136,7 @@ void KisColorSelectorTriangle::setColor(const KoColor &color)
 
     emit paramChanged(-1, s, v, -1, -1, -1, -1, -1, -1);
     emit update();
+    KisColorSelectorComponent::setColor(color);
 }
 
 int KisColorSelectorTriangle::triangleWidth() const

plugins/dockers/advancedcolorselector/kis_color_selector_wheel.cpp
@@ -177,6 +177,8 @@ void KisColorSelectorWheel::setColor(const KoColor &color)
 
         setLastMousePosition(pos.x(), pos.y());
     }
+
+    KisColorSelectorComponent::setColor(color);
 }
 
 void KisColorSelectorWheel::paint(QPainter* painter)
@@ -257,18 +259,6 @@ void KisColorSelectorWheel::paint(QPainter* painter)
     }
 }
 
-void KisColorSelectorWheel::mouseEvent(int x, int y)
-{
-    int newX=qBound(0, (x-m_x), width());
-    int newY=qBound(0, (y-m_y), height());
-
-    if (coordIsClear(newX,newY)) {
-        selectColor(newX, newY);
-        m_lastX=newX;
-        m_lastY=newY;
-    }
-}
-
 KoColor KisColorSelectorWheel::colorAt(int x, int y, bool forceValid)
 {
     KoColor color(Qt::transparent, m_parent->colorSpace());
@@ -327,15 +317,9 @@ KoColor KisColorSelectorWheel::colorAt(int x, int y, bool forceValid)
     return color;
 }
 
-bool KisColorSelectorWheel::coordIsClear(int x, int y)
+bool KisColorSelectorWheel::allowsColorSelectionAtPoint(const QPoint &pt) const
 {
-    bool retval = false;
-    if (m_gamutMaskOn && m_currentGamutMask) {
-        bool isClear = m_currentGamutMask->coordIsClear(m_toRenderArea.map(QPointF(x,y)), *m_viewConverter, m_maskPreviewActive);
-        retval = (isClear) ? true : false;
-    } else {
-        retval = true;
-    }
-
-    return retval;
+    return !m_gamutMaskOn || !m_currentGamutMask ||
+        m_currentGamutMask->coordIsClear(m_toRenderArea.map(QPointF(pt)),
+                                         *m_viewConverter, m_maskPreviewActive);
 }

plugins/dockers/advancedcolorselector/kis_color_selector_wheel.h
@@ -43,14 +43,13 @@ public:
 protected:
     KoColor selectColor(int x, int y) override;
     void paint(QPainter*) override;
-    void mouseEvent(int x, int y) override;
 
 private:
     friend class Acs::PixelCacheRenderer;
     KoColor colorAt(int x, int y, bool forceValid = false);
 
 private:
-    bool coordIsClear(int x, int y);
+    bool allowsColorSelectionAtPoint(const QPoint &pt) const override;
     QPointF m_lastClickPos;
     QImage m_pixelCache;
     QPoint m_pixelCacheOffset;

commit 57ce1ba509ff3d7a83a7d159ce937cbeeb1bbfe6

Fix gamut masks in advanced color selector
Commit 8441c26fbda introduced a bug into gamut mask handling in the
advanced color selector: the mouse position used to determine if a color
is selectable is not correctly adjusted, that causes a mismatch between
shapes shown on the wheel (which are correct) and selectable area.

BACKPORT:krita/4.2

plugins/dockers/advancedcolorselector/kis_color_selector_component.cpp
@@ -77,7 +77,7 @@ void KisColorSelectorComponent::mouseEvent(int x, int y)
     int newX=qBound(0, (x-m_x), width());
     int newY=qBound(0, (y-m_y), height());
 
-    if (allowsColorSelectionAtPoint(QPoint(x, y))) {
+    if (allowsColorSelectionAtPoint(QPoint(newX, newY))) {
         m_lastSelectedColor = selectColor(newX, newY);
         m_lastX=newX;
         m_lastY=newY;
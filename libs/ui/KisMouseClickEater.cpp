47b63a2d3e9e6f9efe686861ee220afc73b7e737
Dmitry Kazakov <dimula73@gmail.com>
2019-05-21 23:52:03 +0300

    Fix popup palette in WinInk mode
    We should eat synthesized events more carefully in the popup
    palette. The events may come either to the triangle widget or
    to its parent widget, depending on the implementaiton. Therefore
    we use a special event filter for that (KisMouseClickEater).
    
    CCBUG:406668
    Ref T10935

---
+++ libs/ui/KisMouseClickEater.cpp
@@ -0,0 +1,70 @@
+#include "KisMouseClickEater.h"
+
+#include <QMouseEvent>
+#include "kis_debug.h"
+
+KisMouseClickEater::KisMouseClickEater(Qt::MouseButtons buttons,
+                                       int clicksToEat,
+                                       QObject *parent)
+    : QObject(parent),
+      m_buttons(buttons),
+      m_clicksToEat(clicksToEat)
+{
+    reset();
+}
+
+KisMouseClickEater::~KisMouseClickEater()
+{
+}
+
+void KisMouseClickEater::reset()
+{
+    m_clicksHappened = 0;
+    m_timeSinceReset.start();
+    m_blockTimedRelease = false;
+}
+
+bool KisMouseClickEater::eventFilter(QObject *watched, QEvent *event)
+{
+#ifdef Q_OS_WIN
+    const int tabletMouseEventsFlowDelay = 500;
+#else
+    const int tabletMouseEventsFlowDelay = 100;
+#endif
+
+    if (event->type() == QEvent::TabletMove) {
+        m_blockTimedRelease = true;
+    }
+
+    if (!m_blockTimedRelease &&
+        m_timeSinceReset.elapsed() > tabletMouseEventsFlowDelay) {
+
+        return QObject::eventFilter(watched, event);
+    }
+
+    if (event->type() == QEvent::MouseButtonPress ||
+        event->type() == QEvent::MouseButtonRelease) {
+
+        QMouseEvent *mevent = static_cast<QMouseEvent*>(event);
+        if (mevent->button() & m_buttons) {
+            if (m_clicksHappened >= m_clicksToEat) {
+                return false;
+            }
+
+            if (event->type() == QEvent::MouseButtonRelease) {
+                m_clicksHappened++;
+            }
+
+            return true;
+        }
+    }
+
+    if (event->type() == QEvent::MouseMove) {
+        QMouseEvent *mevent = static_cast<QMouseEvent*>(event);
+        if (mevent->buttons() & m_buttons) {
+            return m_clicksHappened < m_clicksToEat;
+        }
+    }
+
+    return QObject::eventFilter(watched, event);
+}

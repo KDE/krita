d3175ce38f93b651addb935ad7ee6ccb19c8720b
Ivan Yossi <ghevan@gmail.com>
2019-05-22 00:14:22 -0500

    Fix Bristle artifacts on Wrap Around mode
    Only avoids artifacts being more prominent on wraparound mode
    
    CCBUG:387070

--- plugins/paintops/hairy/hairy_brush.cpp
+++ plugins/paintops/hairy/hairy_brush.cpp
@@ -331,8 +331,8 @@ void HairyBrush::paintParticle(QPointF pos, const KoColor& color, qreal weight)
 
     int ipx = int (pos.x());
     int ipy = int (pos.y());
-    qreal fx = pos.x() - ipx;
-    qreal fy = pos.y() - ipy;
+    qreal fx = qAbs(pos.x() - ipx);
+    qreal fy = qAbs(pos.y() - ipy);
 
     quint8 btl = qRound((1.0 - fx) * (1.0 - fy) * opacity);
     quint8 btr = qRound((fx)  * (1.0 - fy) * opacity);
@@ -370,8 +370,8 @@ void HairyBrush::paintParticle(QPointF pos, const KoColor& color)
 
     int ipx = int (pos.x());
     int ipy = int (pos.y());
-    qreal fx = pos.x() - ipx;
-    qreal fy = pos.y() - ipy;
+    qreal fx = qAbs(pos.x() - ipx);
+    qreal fy = qAbs(pos.y() - ipy);
 
     quint8 btl = qRound((1.0 - fx) * (1.0 - fy) * opacity);
     quint8 btr = qRound((fx)  * (1.0 - fy) * opacity);

375384414368b4bbd65d734f9f7d7cd7c48ef05f
Ivan Yossi <ghevan@gmail.com>
2019-05-23 23:38:12 -0500

    Re-enable svgTextTool as a modal window
    It has to be this way to avoid being covered by tool windows, 
    and before changing to nonModal as Tool window, a workaround
     for shortcuts must be made. 
    
    As a Tool window the menu is not shown, making some edition
    options unavailable.
    
    BUG:407878
    BACKPORT:krita/4.2
    (cherry picked from commit cd4d6453dac73d9d0851e7b2f63589bf0824fc82)

--- plugins/tools/svgtexttool/SvgTextTool.cpp
+++ plugins/tools/svgtexttool/SvgTextTool.cpp
@@ -225,7 +225,7 @@ void SvgTextTool::showEditor()
 
     if (!m_editor) {
         m_editor = new SvgTextEditor(QApplication::activeWindow());
-        m_editor->setWindowModality(Qt::NonModal);
+        m_editor->setWindowModality(Qt::ApplicationModal);
         m_editor->setAttribute( Qt::WA_QuitOnClose, false );
 
         connect(m_editor, SIGNAL(textUpdated(KoSvgTextShape*,QString,QString,bool)), SLOT(textUpdated(KoSvgTextShape*,QString,QString,bool)));

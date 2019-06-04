a004a2ba86ef152ce5fc44d6d8f4db5b81942509
Dmitry Kazakov <dimula73@gmail.com>
2019-06-04 13:32:15 +0300

    Fix infinite undo stack
    It might be possible that it was the reason of the slowdown bug,
    but it needs to be checked.
    
    BUG:408255
    CCBUG:408133

--- libs/ui/KisDocument.cpp
+++ libs/ui/KisDocument.cpp
@@ -1581,8 +1581,11 @@ void KisDocument::slotUndoStackCleanChanged(bool value)
 void KisDocument::slotConfigChanged()
 {
     KisConfig cfg(true);
-    if (!d->undoStack->isClean() && d->undoStack->undoLimit() != cfg.undoStackLimit()) {
-        d->undoStack->clear();
+
+    if (d->undoStack->undoLimit() != cfg.undoStackLimit()) {
+        if (!d->undoStack->isClean()) {
+            d->undoStack->clear();
+        }
         d->undoStack->setUndoLimit(cfg.undoStackLimit());
     }
 

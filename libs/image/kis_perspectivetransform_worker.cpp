2a5cb8c6ad2350db81bb9cfde07fe0e076180d11
Boudewijn Rempt <boud@valdyas.org>
2019-06-21 13:55:38 +0200

    Make all LGPLv2 licenses LGPLv2+ for compatibility with the V3 code

--- libs/image/kis_perspectivetransform_worker.cpp
+++ libs/image/kis_perspectivetransform_worker.cpp
@@ -5,19 +5,21 @@
  *  Copyright (c) 2009 Edward Apap <schumifer@hotmail.com>
  *  Copyright (c) 2010 Marc Pegon <pe.marc@free.fr>
  *
- *  This library is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; version 2.1 of the License.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
+ *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
 
 #include "kis_perspectivetransform_worker.h"
 

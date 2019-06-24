edf362943fd9d87aa83a6d97840b215d4b7a0563
Sharaf Zaman <sharafzaz121@gmail.com>
2019-06-25 03:03:25 +0530

    Android: Save state when app goes in background
    This starts saving files synchronously. So, the progress
    isn't lost.
    
    Maniphest: T10784

--- packaging/android/apk/src/org/krita/android/MainActivity.java
+++ packaging/android/apk/src/org/krita/android/MainActivity.java
@@ -7,13 +7,27 @@ import org.qtproject.qt5.android.bindings.QtActivity;
 
 public class MainActivity extends QtActivity {
 
+    public boolean isStartup = true;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		           WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
 
 		new ConfigsManager().handleAssets(this);
 	}
 
+	@Override
+	public void onPause() {
+		super.onPause();
+		// onPause() _is_ called when the app starts. If the native lib
+		// isn't loaded, it crashes.
+		if (!isStartup) {
+			JNIWrappers.saveState();
+		}
+		else {
+			isStartup = false;
+		}
+	}
 }

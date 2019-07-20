KDE/krita

commit 7a90f28096ac2fada4646c2637f134c372df6644

Add a static method to create a KisImage from a QImage
This tries its best to keep the colormodel of the qimage, now that
qimage can also have 16 bit integer or float channels.

libs/image/kis_image.cc
@@ -269,6 +269,72 @@ KisImage::~KisImage()
     disconnect(); // in case Qt gets confused
 }
 
+KisImageSP KisImage::fromQImage(const QImage &image, KisUndoStore *undoStore)
+{
+    const KoColorSpace *colorSpace = 0;
+
+    switch (image.format()) {
+    case QImage::Format_Invalid:
+    case QImage::Format_Mono:
+    case QImage::Format_MonoLSB:
+        colorSpace = KoColorSpaceRegistry::instance()->graya8();
+        break;
+    case QImage::Format_Indexed8:
+    case QImage::Format_RGB32:
+    case QImage::Format_ARGB32:
+    case QImage::Format_ARGB32_Premultiplied:
+        colorSpace = KoColorSpaceRegistry::instance()->rgb8();
+        break;
+    case QImage::Format_RGB16:
+        colorSpace = KoColorSpaceRegistry::instance()->rgb16();
+        break;
+    case QImage::Format_ARGB8565_Premultiplied:
+    case QImage::Format_RGB666:
+    case QImage::Format_ARGB6666_Premultiplied:
+    case QImage::Format_RGB555:
+    case QImage::Format_ARGB8555_Premultiplied:
+    case QImage::Format_RGB888:
+    case QImage::Format_RGB444:
+    case QImage::Format_ARGB4444_Premultiplied:
+    case QImage::Format_RGBX8888:
+    case QImage::Format_RGBA8888:
+    case QImage::Format_RGBA8888_Premultiplied:
+        colorSpace = KoColorSpaceRegistry::instance()->rgb8();
+        break;
+    case QImage::Format_BGR30:
+    case QImage::Format_A2BGR30_Premultiplied:
+    case QImage::Format_RGB30:
+    case QImage::Format_A2RGB30_Premultiplied:
+        colorSpace = KoColorSpaceRegistry::instance()->rgb8();
+        break;
+    case QImage::Format_Alpha8:
+        colorSpace = KoColorSpaceRegistry::instance()->alpha8();
+        break;
+    case QImage::Format_Grayscale8:
+        colorSpace = KoColorSpaceRegistry::instance()->graya8();
+        break;
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+    case QImage::Format_Grayscale16:
+        colorSpace = KoColorSpaceRegistry::instance()->graya16();
+        break;
+    case QImage::Format_RGBX64:
+    case QImage::Format_RGBA64:
+    case QImage::Format_RGBA64_Premultiplied:
+        colorSpace = KoColorSpaceRegistry::instance()->colorSpace(RGBAColorModelID.id(), Float32BitsColorDepthID, 0);
+        break;
+#endif
+    default:
+        colorSpace = 0;
+    }
+
+    KisImageSP img = new KisImage(undoStore, image.width(), image.height(), colorSpace, i18n("Imported Image"));
+    KisPaintLayerSP layer = new KisPaintLayer(img, img->nextLayerName(), 255);
+    layer->paintDevice()->convertFromQImage(image, 0, 0, 0);
+    img->addNode(layer.data(), img->rootLayer().data());
+
+    return img;
+}
+
 KisImage *KisImage::clone(bool exactCopy)
 {
     return new KisImage(*this, 0, exactCopy);

libs/image/kis_image.h
@@ -82,6 +82,8 @@ public:
     KisImage(KisUndoStore *undoStore, qint32 width, qint32 height, const KoColorSpace *colorSpace, const QString& name);
     ~KisImage() override;
 
+    static KisImageSP fromQImage(const QImage &image, KisUndoStore *undoStore);
+
 public: // KisNodeGraphListener implementation
 
     void aboutToAddANode(KisNode *parent, int index) override;

commit f047737e7a99f7fc525197060b84b2af098c1928

Fix conditionals for QImage supported formats on clang
QImage::Format_Grayscale16 added in Qt 5.13

fixes:  7a90f28096ac2fada4646c2637f134c372df6644

libs/image/kis_image.cc
@@ -313,14 +313,16 @@ KisImageSP KisImage::fromQImage(const QImage &image, KisUndoStore *undoStore)
     case QImage::Format_Grayscale8:
         colorSpace = KoColorSpaceRegistry::instance()->graya8();
         break;
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
     case QImage::Format_Grayscale16:
         colorSpace = KoColorSpaceRegistry::instance()->graya16();
         break;
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
     case QImage::Format_RGBX64:
     case QImage::Format_RGBA64:
     case QImage::Format_RGBA64_Premultiplied:
-        colorSpace = KoColorSpaceRegistry::instance()->colorSpace(RGBAColorModelID.id(), Float32BitsColorDepthID, 0);
+        colorSpace = KoColorSpaceRegistry::instance()->colorSpace(RGBAColorModelID.id(), Float32BitsColorDepthID.id(), 0);
         break;
 #endif
     default:
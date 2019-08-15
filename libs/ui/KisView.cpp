266c1deed69140db0282f8ae7acfa8488655ecdd
Dmitry Kazakov <dimula73@gmail.com>
2019-08-15 18:09:48 +0300

    Revert "Fix crash"
    This reverts commit 802639c913622fcd0a7b7dcfd39628fec0cf99e6.
    
    This commit crashes Krita on creation of a new document (tested
    on QPainter canvas).

--- libs/ui/KisView.cpp
+++ libs/ui/KisView.cpp
@@ -111,7 +111,6 @@ public:
         , canvasController(_q, actionCollection)
         , canvas(&viewConverter, resourceManager, _q, document->shapeController())
         , zoomManager(_q, &this->viewConverter, &this->canvasController)
-        , viewManager(viewManager)
         , paintingAssistantsDecoration(new KisPaintingAssistantsDecoration(_q))
         , referenceImagesDecoration(new KisReferenceImagesDecoration(_q, document))
         , floatingMessageCompressor(100, KisSignalCompressor::POSTPONE)

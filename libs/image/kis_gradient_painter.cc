bfeec4c4b827e617f1417a98fd4505c26f3237c2
Agata Cacko <tamtamy.tymona@gmail.com>
2019-08-16 16:18:47 +0000

    Add Reptorian's copyright information to the gradient painter

--- libs/image/kis_gradient_painter.cc
+++ libs/image/kis_gradient_painter.cc
@@ -1,5 +1,6 @@
 /*
  *  Copyright (c) 2004 Adrian Page <adrian@pagenet.plus.com>
+ *  Copyright (c) 2019 Miguel Lopez <reptillia39@live.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

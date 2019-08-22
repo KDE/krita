KDE/krita

commit c74ddaf2991833c7c587dec47f53456ec36304e2

Fix Tangent-Normal brush on rotated/mirrored canvas
(and also in multihand brush)

Now the canvas rotation/mirroing code is fully ported to the new
API, by passing via KisPaintInformation instead of paintop settings'
temporary properties.

BUG:404408

plugins/paintops/libpaintop/kis_pressure_rotation_option.cpp
@@ -26,19 +26,17 @@
 #include <KoColor.h>
 
 KisPressureRotationOption::KisPressureRotationOption()
-        : KisCurveOption("Rotation", KisPaintOpOption::GENERAL, false),
-          m_defaultAngle(0.0),
-          m_canvasAxisXMirrored(false),
-          m_canvasAxisYMirrored(false)
+        : KisCurveOption("Rotation", KisPaintOpOption::GENERAL, false)
 {
 }
 
 double KisPressureRotationOption::apply(const KisPaintInformation & info) const
 {
-    if (!isChecked()) return m_defaultAngle;
+    if (!isChecked()) return kisDegreesToRadians(info.canvasRotation());
 
-    const bool absoluteAxesFlipped = m_canvasAxisXMirrored != m_canvasAxisYMirrored;
-    const qreal normalizedBaseAngle = -m_defaultAngle / (2 * M_PI);
+    const bool absoluteAxesFlipped = info.canvasMirroredH() != info.canvasMirroredV();
+
+    const qreal normalizedBaseAngle = info.canvasRotation() / 360.0;
 
     qreal value = computeRotationLikeValue(info, normalizedBaseAngle, absoluteAxesFlipped);
 
@@ -50,11 +48,7 @@ double KisPressureRotationOption::apply(const KisPaintInformation & info) const
 
 void KisPressureRotationOption::readOptionSetting(const KisPropertiesConfigurationSP setting)
 {
-    m_defaultAngle = setting->getDouble("runtimeCanvasRotation", 0.0) * M_PI / 180.0;
     KisCurveOption::readOptionSetting(setting);
-
-    m_canvasAxisXMirrored = setting->getBool("runtimeCanvasMirroredX", false);
-    m_canvasAxisYMirrored = setting->getBool("runtimeCanvasMirroredY", false);
 }
 
 void KisPressureRotationOption::applyFanCornersInfo(KisPaintOp *op)

libs/image/brushengine/kis_paint_information.cc
@@ -85,6 +85,8 @@ struct KisPaintInformation::Private {
         directionHistoryInfo = rhs.directionHistoryInfo;
         canvasRotation = rhs.canvasRotation;
         canvasMirroredH = rhs.canvasMirroredH;
+        canvasMirroredV = rhs.canvasMirroredV;
+
         if (rhs.drawingAngleOverride) {
             drawingAngleOverride = *rhs.drawingAngleOverride;
         }
@@ -105,8 +107,9 @@ struct KisPaintInformation::Private {
     bool isHoveringMode;
     KisRandomSourceSP randomSource;
     KisPerStrokeRandomSourceSP perStrokeRandomSource;
-    int canvasRotation {0};
+    qreal canvasRotation {0};
     bool canvasMirroredH {false};
+    bool canvasMirroredV {false};
 
     boost::optional<qreal> drawingAngleOverride;
     bool sanityIsRegistered = false;
@@ -255,8 +258,9 @@ KisPaintInformation::createHoveringModeInfo(const QPointF &pos,
         qreal tangentialPressure,
         qreal perspective,
         qreal speed,
-        int canvasrotation,
-        bool canvasMirroredH)
+        qreal canvasrotation,
+        bool canvasMirroredH,
+        bool canvasMirroredV)
 {
     KisPaintInformation info(pos,
                              pressure,
@@ -267,23 +271,19 @@ KisPaintInformation::createHoveringModeInfo(const QPointF &pos,
     info.d->isHoveringMode = true;
     info.d->canvasRotation = canvasrotation;
     info.d->canvasMirroredH = canvasMirroredH;
+    info.d->canvasMirroredV = canvasMirroredV;
     return info;
 }
 
 
-int KisPaintInformation::canvasRotation() const
+qreal KisPaintInformation::canvasRotation() const
 {
     return d->canvasRotation;
 }
 
-void KisPaintInformation::setCanvasRotation(int rotation)
+void KisPaintInformation::setCanvasRotation(qreal rotation)
 {
-    if (rotation < 0) {
-        d->canvasRotation= 360- abs(rotation % 360);
-    } else {
-        d->canvasRotation= rotation % 360;
-    }
-
+    d->canvasRotation = normalizeAngleDegrees(rotation);
 }
 
 bool KisPaintInformation::canvasMirroredH() const
@@ -291,10 +291,19 @@ bool KisPaintInformation::canvasMirroredH() const
     return d->canvasMirroredH;
 }
 
-void KisPaintInformation::setCanvasHorizontalMirrorState(bool mir)
+void KisPaintInformation::setCanvasMirroredH(bool value)
 {
-    d->canvasMirroredH = mir;
+    d->canvasMirroredH = value;
+}
 
+bool KisPaintInformation::canvasMirroredV() const
+{
+    return d->canvasMirroredV;
+}
+
+void KisPaintInformation::setCanvasMirroredV(bool value)
+{
+    d->canvasMirroredV = value;
 }
 
 void KisPaintInformation::toXML(QDomDocument&, QDomElement& e) const
@@ -606,6 +615,9 @@ void KisPaintInformation::mixOtherImpl(const QPointF &p, qreal t, const KisPaint
 
         KIS_ASSERT_RECOVER_NOOP(other.isHoveringMode() == this->isHoveringMode());
         *(this->d) = Private(p, pressure, xTilt, yTilt, rotation, tangentialPressure, perspective, time, speed, other.isHoveringMode());
+        this->d->canvasRotation = other.d->canvasRotation;
+        this->d->canvasMirroredH = other.d->canvasMirroredH;
+        this->d->canvasMirroredV = other.d->canvasMirroredV;
         this->d->randomSource = other.d->randomSource;
         this->d->perStrokeRandomSource = other.d->perStrokeRandomSource;
         // this->d->isHoveringMode = other.isHoveringMode();

libs/image/brushengine/kis_paint_information.h
@@ -253,26 +253,39 @@ public:
             qreal tangentialPressure = 0.0,
             qreal perspective = 1.0,
 	        qreal speed = 0.0,
-            int canvasrotation = 0,
-            bool canvasMirroredH = false);
-    /**
-     *Returns the canvas rotation if that has been given to the kispaintinformation.
-     */
-    int canvasRotation() const;
-    /**
-     *set the canvas rotation.
-     */
-    void setCanvasRotation(int rotation);
+            qreal canvasrotation = 0,
+            bool canvasMirroredH = false,
+            bool canvasMirroredV = false);
 
-    /*
-     *Whether the canvas is mirrored for the paint-operation.
+    /**
+     * Returns the canvas rotation if that has been given to the kispaintinformation.
+     */
+    qreal canvasRotation() const;
+
+    /**
+     * Set the canvas rotation.
+     */
+    void setCanvasRotation(qreal rotation);
+
+    /**
+     * Whether the canvas is horizontally mirrored for the paint-operation.
      */
     bool canvasMirroredH() const;
 
-    /*
-     *Set whether the canvas is mirrored for the paint-operation.
+    /**
+     * Set whether the canvas is horizontally mirrored for the paint-operation.
      */
-    void setCanvasHorizontalMirrorState(bool mir);
+    void setCanvasMirroredH(bool value);
+
+    /**
+     * Whether the canvas is vertically mirrored for the paint-operation.
+     */
+    bool canvasMirroredV() const;
+
+    /**
+     * Set whether the canvas is vertically mirrored for the paint-operation.
+     */
+    void setCanvasMirroredV(bool value);
 
     void toXML(QDomDocument&, QDomElement&) const;
 

libs/image/brushengine/kis_paintop_settings.cpp
@@ -439,25 +439,6 @@ QPainterPath KisPaintOpSettings::makeTiltIndicator(KisPaintInformation const& in
     return ret;
 }
 
-void KisPaintOpSettings::setCanvasRotation(qreal angle)
-{
-    Private::DirtyNotificationsLocker locker(d.data());
-
-    setProperty("runtimeCanvasRotation", angle);
-    setPropertyNotSaved("runtimeCanvasRotation");
-}
-
-void KisPaintOpSettings::setCanvasMirroring(bool xAxisMirrored, bool yAxisMirrored)
-{
-    Private::DirtyNotificationsLocker locker(d.data());
-
-    setProperty("runtimeCanvasMirroredX", xAxisMirrored);
-    setPropertyNotSaved("runtimeCanvasMirroredX");
-
-    setProperty("runtimeCanvasMirroredY", yAxisMirrored);
-    setPropertyNotSaved("runtimeCanvasMirroredY");
-}
-
 void KisPaintOpSettings::setProperty(const QString & name, const QVariant & value)
 {
     if (value != KisPropertiesConfiguration::getProperty(name) &&

libs/image/brushengine/kis_paintop_settings.h
@@ -283,15 +283,6 @@ public:
     virtual bool isLoadable();
 
     /**
-     * These methods are populating properties with runtime
-     * information about canvas rotation/mirroring. This information
-     * is set directly by KisToolFreehand. Later the data is accessed
-     * by the pressure options to make a final decision.
-     */
-    void setCanvasRotation(qreal angle);
-    void setCanvasMirroring(bool xAxisMirrored, bool yAxisMirrored);
-
-    /**
      * Overrides the method in KisPropertiesCofiguration to allow
      * onPropertyChanged() callback
      */

libs/ui/CMakeLists.txt
@@ -19,7 +19,6 @@ set(kritaui_LIB_SRCS
     canvas/kis_canvas2.cpp
     canvas/kis_canvas_updates_compressor.cpp
     canvas/kis_canvas_controller.cpp
-    canvas/kis_paintop_transformation_connector.cpp
     canvas/kis_display_color_converter.cpp
     canvas/kis_display_filter.cpp
     canvas/kis_exposure_gamma_correction_interface.cpp

libs/ui/canvas/kis_canvas_controller.cpp
@@ -25,7 +25,6 @@
 #include <klocalizedstring.h>
 #include <kactioncollection.h>
 #include "kis_canvas_decoration.h"
-#include "kis_paintop_transformation_connector.h"
 #include "kis_coordinates_converter.h"
 #include "kis_canvas2.h"
 #include "opengl/kis_opengl_canvas2.h"
@@ -42,8 +41,7 @@ static const int gRulersUpdateDelay = 80 /* ms */;
 
 struct KisCanvasController::Private {
     Private(KisCanvasController *qq)
-        : q(qq),
-          paintOpTransformationConnector(0)
+        : q(qq)
     {
         using namespace std::placeholders;
 
@@ -60,7 +58,6 @@ struct KisCanvasController::Private {
     QPointer<KisView> view;
     KisCoordinatesConverter *coordinatesConverter;
     KisCanvasController *q;
-    KisPaintopTransformationConnector *paintOpTransformationConnector;
     QScopedPointer<KisSignalCompressorWithParam<QPoint> > mousePositionCompressor;
 
     void emitPointerPositionChangedSignals(QPoint pointerPos);
@@ -112,14 +109,8 @@ void KisCanvasController::setCanvas(KoCanvasBase *canvas)
 
         m_d->coordinatesConverter =
             const_cast<KisCoordinatesConverter*>(kritaCanvas->coordinatesConverter());
-
-        m_d->paintOpTransformationConnector =
-            new KisPaintopTransformationConnector(kritaCanvas, this);
     } else {
         m_d->coordinatesConverter = 0;
-
-        delete m_d->paintOpTransformationConnector;
-        m_d->paintOpTransformationConnector = 0;
     }
 
     KoCanvasControllerWidget::setCanvas(canvas);
@@ -200,7 +191,6 @@ void KisCanvasController::mirrorCanvas(bool enable)
     QPoint newOffset = m_d->coordinatesConverter->mirror(m_d->coordinatesConverter->widgetCenterPoint(), enable, false);
     m_d->updateDocumentSizeAfterTransform();
     setScrollBarValue(newOffset);
-    m_d->paintOpTransformationConnector->notifyTransformationChanged();
     m_d->showMirrorStateOnCanvas();
 }
 
@@ -219,7 +209,6 @@ void KisCanvasController::rotateCanvas(qreal angle, const QPointF &center)
     QPoint newOffset = m_d->coordinatesConverter->rotate(center, angle);
     m_d->updateDocumentSizeAfterTransform();
     setScrollBarValue(newOffset);
-    m_d->paintOpTransformationConnector->notifyTransformationChanged();
     m_d->showRotationValueOnCanvas();
 }
 
@@ -248,7 +237,6 @@ void KisCanvasController::resetCanvasRotation()
     QPoint newOffset = m_d->coordinatesConverter->resetRotation(m_d->coordinatesConverter->widgetCenterPoint());
     m_d->updateDocumentSizeAfterTransform();
     setScrollBarValue(newOffset);
-    m_d->paintOpTransformationConnector->notifyTransformationChanged();
     m_d->showRotationValueOnCanvas();
 }
 

libs/ui/canvas/kis_paintop_transformation_connector.cpp
@@ -1,58 +1,0 @@
-/*
- *  Copyright (c) 2013 Dmitry Kazakov <dimula73@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-#include "kis_paintop_transformation_connector.h"
-
-#include "kis_canvas_resource_provider.h"
-#include "kis_canvas2.h"
-#include "kis_coordinates_converter.h"
-#include "brushengine/kis_paintop_preset.h"
-#include "brushengine/kis_paintop_settings.h"
-
-
-KisPaintopTransformationConnector::KisPaintopTransformationConnector(KisCanvas2 *canvas, QObject *parent)
-    : QObject(parent),
-      m_canvas(canvas)
-{
-    connect(m_canvas->resourceManager(),
-            SIGNAL(canvasResourceChanged(int,QVariant)),
-            SLOT(slotCanvasResourceChanged(int,QVariant)));
-}
-
-void KisPaintopTransformationConnector::notifyTransformationChanged()
-{
-    KisPaintOpPresetSP preset =
-        m_canvas->resourceManager()->
-        resource(KisCanvasResourceProvider::CurrentPaintOpPreset).value<KisPaintOpPresetSP>();
-
-    if (preset) {
-        const KisCoordinatesConverter *converter = m_canvas->coordinatesConverter();
-        preset->settings()->setCanvasRotation(converter->rotationAngle());
-        preset->settings()->setCanvasMirroring(converter->xAxisMirrored(),
-                                               converter->yAxisMirrored());
-    }
-}
-
-void KisPaintopTransformationConnector::slotCanvasResourceChanged(int key, const QVariant &resource)
-{
-    Q_UNUSED(resource);
-
-    if (key == KisCanvasResourceProvider::CurrentPaintOpPreset) {
-        notifyTransformationChanged();
-    }
-}

commit d8f90d4a8968e50e709833de8543776860dcbbcb

[REGRESSION] Fix absolute brush rotation on rotated canvas
The regression was caused by c74ddaf2991833c7c587dec47f53456ec36304e2

BUG:292726

plugins/paintops/libpaintop/kis_pressure_rotation_option.cpp
@@ -36,7 +36,7 @@ double KisPressureRotationOption::apply(const KisPaintInformation & info) const
 
     const bool absoluteAxesFlipped = info.canvasMirroredH() != info.canvasMirroredV();
 
-    const qreal normalizedBaseAngle = info.canvasRotation() / 360.0;
+    const qreal normalizedBaseAngle = -info.canvasRotation() / 360.0;
 
     qreal value = computeRotationLikeValue(info, normalizedBaseAngle, absoluteAxesFlipped);
 
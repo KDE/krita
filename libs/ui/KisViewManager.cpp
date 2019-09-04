df19037c87ce1a7baed6b13f96b950e4e9a03c20
Sharaf Zaman <sharafzaz121@gmail.com>
2019-09-04 13:54:14 +0530

    Add reset to rotation
    Maniphest: T11355

--- libs/ui/KisViewManager.cpp
+++ libs/ui/KisViewManager.cpp
@@ -1440,4 +1440,8 @@ void KisViewManager::slotResetFgBg()
     d->canvasResourceManager.setForegroundColor(KoColor(Qt::black, KoColorSpaceRegistry::instance()->rgb8()));
 }
 
-
+void KisViewManager::slotResetRotation()
+{
+    KisCanvasController *canvasController = d->currentImageView->canvasController();
+    canvasController->resetCanvasRotation();
+}

1e6ac8ca130d7bf2833a09e7a088bc4444fde7a4
Sharaf Zaman <sharafzaz121@gmail.com>
2019-09-07 19:18:29 +0530

    Fix brush outline getting stuck
    Maniphest: T11355

--- libs/ui/input/kis_input_manager.cpp
+++ libs/ui/input/kis_input_manager.cpp
@@ -572,7 +572,7 @@ bool KisInputManager::eventFilterImpl(QEvent * event)
         break;
     }
     case QEvent::TabletRelease: {
-#ifdef Q_OS_MAC
+#if defined(Q_OS_MAC) || defined(Q_OS_ANDROID)
         d->allowMouseEvents();
 #endif
         d->debugEvent<QTabletEvent, false>(event);

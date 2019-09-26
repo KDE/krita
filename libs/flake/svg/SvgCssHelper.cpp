1406a2c80c333b8dc78ab1764090c47b42547e4a
Boudewijn Rempt <boud@valdyas.org>
2019-09-26 15:35:40 +0200

    CID 34759: Logically dead code (DEADCODE)
    dead_error_line: Execution cannot reach this statement: case Bad:.
    
    (cherry picked from commit eb4a1a3fcc5270a91908f427392f4986cf2e5b47)

--- libs/flake/svg/SvgCssHelper.cpp
+++ libs/flake/svg/SvgCssHelper.cpp
@@ -580,7 +580,6 @@ public:
                         tokenList.append(CssToken(SelectorToken, simpleSelector));
                     }
                     break;
-                case Bad:
                 default:
                     break;
             }

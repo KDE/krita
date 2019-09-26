2fd2c26e69aa927a6573bc757fa911db749f0df4
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2019-09-26 16:57:24 +0200

    Remove HDR tab when HDR is not present.
    This will remove the whole tab instead of just the settings group.
    As HDR cannot be magically added in the middle of a session, this
    should work appropriately.
    
    BUG:412306
    (cherry picked from commit 8ef91fb3f995c83d82cf61d56f08866f6b0401a4)

--- libs/ui/dialogs/kis_dlg_preferences.cc
+++ libs/ui/dialogs/kis_dlg_preferences.cc
@@ -1238,6 +1238,7 @@ DisplaySettingsTab::DisplaySettingsTab(QWidget *parent, const char *name)
 
 #ifndef HAVE_HDR
     grpHDRSettings->setVisible(false);
+    tabWidget->removeTab(tabWidget->indexOf(tabHDR));
 #endif
 
     const QStringList openglWarnings = KisOpenGL::getOpenGLWarnings();

ecc23e35616ff2c157518f5ad2d97822c451a873
Boudewijn Rempt <boud@valdyas.org>
2019-09-26 17:14:23 +0200

    fix build

--- plugins/extensions/imagesize/dlg_canvassize.cc
+++ plugins/extensions/imagesize/dlg_canvassize.cc
@@ -486,7 +486,7 @@ void DlgCanvasSize::updateOffset(int id)
 
 void DlgCanvasSize::expectedOffset(int id, double &xOffset, double &yOffset)
 {
-    const double xCoeff = (id % 3.0) * 0.5;
+    const double xCoeff = (id % 3) * 0.5;
     const double yCoeff = (id / 3.0) * 0.5;
 
     const int xDiff = m_newWidth - m_originalWidth;

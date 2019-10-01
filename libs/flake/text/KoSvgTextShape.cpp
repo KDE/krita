d33aa23df79a1c6ed3d4b8eb45c1c1a71b2a8379
Boudewijn Rempt <boud@valdyas.org>
2019-10-01 16:02:34 +0200

    Use the proper check for Qt 5.11
    (cherry picked from commit 866e5aad29b699e44d20f9a7404a10f4ac3cc440)

--- libs/flake/text/KoSvgTextShape.cpp
+++ libs/flake/text/KoSvgTextShape.cpp
@@ -329,7 +329,7 @@ struct LayoutChunkWrapper
         KIS_SAFE_ASSERT_RECOVER_RETURN_VALUE(startPos == m_addedChars, currentTextPos);
         KIS_SAFE_ASSERT_RECOVER_RETURN_VALUE(lastPos < m_layout->text().size(), currentTextPos);
 
-//        qDebug() << m_layout->text();
+        //        qDebug() << m_layout->text();
 
         QTextLine line;
         std::swap(line, m_danglingLine);
@@ -386,7 +386,7 @@ struct LayoutChunkWrapper
 private:
     qreal skipSpaceCharacter(int pos) {
         const QTextCharFormat format =
-            formatForPos(pos, m_layout->formats());
+                formatForPos(pos, m_layout->formats());
 
         const QChar skippedChar = m_layout->text()[pos];
         KIS_SAFE_ASSERT_RECOVER_NOOP(skippedChar.isSpace() || !skippedChar.isPrint());
@@ -461,9 +461,9 @@ void KoSvgTextShape::relayout()
             const bool isFinalPass = i == chunk.offsets.size();
 
             const int length =
-                !isFinalPass ?
-                chunk.offsets[i].start - lastSubChunkStart :
-                chunk.text.size() - lastSubChunkStart;
+                    !isFinalPass ?
+                        chunk.offsets[i].start - lastSubChunkStart :
+                        chunk.text.size() - lastSubChunkStart;
 
             if (length > 0) {
                 currentTextPos += lastSubChunkOffset;
@@ -508,7 +508,7 @@ void KoSvgTextShape::relayout()
 
         Q_FOREACH (const QTextLayout::FormatRange &range, layout.formats()) {
             const KoSvgCharChunkFormat &format =
-                static_cast<const KoSvgCharChunkFormat&>(range.format);
+                    static_cast<const KoSvgCharChunkFormat&>(range.format);
             AssociatedShapeWrapper wrapper = format.associatedShapeWrapper();
 
             const int rangeStart = range.start;

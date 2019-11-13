2090a0292879a86ebb485735eee5932ae16c014d
Sharaf Zaman <sharafzaz121@gmail.com>
2019-11-14 00:01:23 +0530

    Bugfix: S-pen not drawing
    When S-pen is brought close to the screen while rotating/zooming
    the canvas, Tablet Event is started without sending TouchEnd
    event. This doesn't let us draw and makes canvas behave weirdly
    to future actions.
    
    Unfortunately, no other reliable event is being sent when pen
    enters the proximity, so we have to rely on TabletPress.

--- libs/ui/input/kis_input_manager.cpp
+++ libs/ui/input/kis_input_manager.cpp
@@ -516,6 +516,15 @@ bool KisInputManager::eventFilterImpl(QEvent * event)
         if (d->tryHidePopupPalette()) {
             retval = true;
         } else {
+#ifdef Q_OS_ANDROID
+            // this means S-Pen has entered the proximity. Hence the TouchEvents
+            // will be disabled by the OS, but we won't receive TouchEnd event.
+            if (d->touchHasBlockedPressEvents)
+            {
+                QTouchEvent *touchEvent = new QTouchEvent(QEvent::Type::TouchEnd);
+                retval = d->matcher.touchEndEvent(touchEvent);
+            }
+#endif
             //Make sure the input actions know we are active.
             KisAbstractInputAction::setInputManager(this);
             retval = d->matcher.buttonPressed(tabletEvent->button(), tabletEvent);

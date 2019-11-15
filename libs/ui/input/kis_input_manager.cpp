9ab5b107d233603e2e233cad6911bb65c2cd68b5
Sharaf Zaman <sharafzaz121@gmail.com>
2019-11-15 21:14:13 +0530

    Bugfix: S-pen not drawing
    `Qt::Enter`/`Qt::Leave` don't work properly on Android. And
    because they change `d-touchHasBlockedPressEvents` it is better
    if we simply ignore these events on Android devices.

--- libs/ui/input/kis_input_manager.cpp
+++ libs/ui/input/kis_input_manager.cpp
@@ -453,6 +453,7 @@ bool KisInputManager::eventFilterImpl(QEvent * event)
         retval = !wasScrolled;
         break;
     }
+#ifndef Q_OS_ANDROID
     case QEvent::Enter:
         d->debugEvent<QEvent, false>(event);
         //Make sure the input actions know we are active.
@@ -479,6 +480,7 @@ bool KisInputManager::eventFilterImpl(QEvent * event)
 
         d->matcher.leaveEvent();
         break;
+#endif
     case QEvent::FocusIn:
         d->debugEvent<QEvent, false>(event);
         KisAbstractInputAction::setInputManager(this);

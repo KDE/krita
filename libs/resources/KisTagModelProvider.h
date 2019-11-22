e90aa1df4f4a60d40ecf8bcf4859b27c38b31de2
Agata Cacko <tamtamy.tymona@gmail.com>
2019-11-22 17:14:00 +0100

    Fix license information in KisTagModelProvider

--- libs/resources/KisTagModelProvider.h
+++ libs/resources/KisTagModelProvider.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Agata Cacko <cacko.azh@gmail.com>
+ * Copyright (c) 2019 Agata Cacko <cacko.azh@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

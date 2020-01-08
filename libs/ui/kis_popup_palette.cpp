0d1616ba0463a69484b8c5938ab101d841cf88ca
Mathias Wein <lynx.mw+kde@gmail.com>
2020-01-08 02:26:38 +0100

    Don't connect popup palette slot to non-existent signal
    Right clicks to trigger palatte hide just propagate to the palette,
    same could be done for the old triangle selector most likely.

--- libs/ui/kis_popup_palette.cpp
+++ libs/ui/kis_popup_palette.cpp
@@ -124,6 +124,7 @@ KisPopupPalette::KisPopupPalette(KisViewManager* viewManager, KisCoordinatesConv
     }
     else {
         m_triangleColorSelector  = new PopupColorTriangle(displayRenderer, this);
+        connect(m_triangleColorSelector, SIGNAL(requestCloseContainer()), this, SLOT(slotHide()));
     }
     m_triangleColorSelector->setDisplayRenderer(displayRenderer);
     m_triangleColorSelector->setConfig(true,false);
@@ -154,8 +155,6 @@ KisPopupPalette::KisPopupPalette(KisViewManager* viewManager, KisCoordinatesConv
             m_colorChangeCompressor.data(), SLOT(start()));
     connect(m_colorChangeCompressor.data(), SIGNAL(timeout()),
             SLOT(slotEmitColorChanged()));
-
-    connect(m_triangleColorSelector, SIGNAL(requestCloseContainer()), this, SLOT(slotHide()));
 
     connect(KisConfigNotifier::instance(), SIGNAL(configChanged()), m_triangleColorSelector, SLOT(configurationChanged()));
 

KDE/krita

commit f867486c5e835cd09d7894b1e4d2c69ce10dbdad

Clean up compilation log from deprecations
Bunch of misc commits that remove warnings about
deprecation in places where it doesn't affect code much.

https://invent.kde.org/kde/krita/merge_requests/150

Squashed commit of the following:

commit 104297616468769dbbf3efaf8f5bfe639eb1c2ab
Author: Krzysztof Kurek <krzysio.kurek@wp.pl>
Date:   Mon Sep 23 13:48:36 2019 +0000

    Remove a Qt5.13 function call

commit 232ac69942a6f737dee0f1a984c40b2297e10a30
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Sat Sep 21 17:14:36 2019 +0200

    Remove a CXX specific flag from other targets

commit 6b71a3cc9b734ca52ca5ff54af8248932cfd1144
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Sat Sep 21 17:12:34 2019 +0200

    Even more deprecation removal work

commit 9c4972ad9b65c87fde0125784adf1f616ff967c7
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Sat Sep 21 14:10:44 2019 +0200

    Revert some const-ref changes

    Also explicitly mark KisOptionInfo's copy-ctor as being present

commit f13ff926eb5f694a32154bd87159be9600e0df92
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Fri Sep 20 01:15:47 2019 +0200

    A bunch more deprecation fixes

    Hopefully it all works... I don't have a second screen to test it thorough.

commit 95c1ee1206f5fb7922833dacd848d97f57729d54
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Thu Sep 19 20:32:47 2019 +0200

    Get rid of a few implicit deprecation warnings

    Deleted some superfluous constructor definitions,
    explicitly declared operator= of SvgGraphicsContext as default but only for private usage.

commit 35d2f53086332a14406074df539e4fff3958e92e
Author: Krzysztof Kurek <krzysio.kurek@pm.me>
Date:   Thu Sep 19 20:30:35 2019 +0200

    Simplify KisSwatch class

    Removes a warning about implicit deprecation

libs/ui/widgets/KoDualColorButton.cpp
@@ -61,7 +61,7 @@ public:
         arrowBitmap.fill(Qt::transparent);
 
         QPainter p(&arrowBitmap);
-        p.setPen(dialogParent->palette().foreground().color());
+        p.setPen(dialogParent->palette().window().color());
 
         // arrow pointing left
         p.drawLine(0, 3, 7, 3);
@@ -321,7 +321,7 @@ void KoDualColorButton::mouseMoveEvent( QMouseEvent *event )
             KColorMimeData::createDrag( d->tmpSelection == Foreground ?
                                             getColorFromDisplayRenderer(d->foregroundColor) :
                                             getColorFromDisplayRenderer(d->backgroundColor),
-                                        this )->start();
+                                        this )->exec();
             d->dragFlag = true;
         }
     }

CMakeLists.txt
@@ -64,7 +64,7 @@ if (APPLE)
 endif()
 
 if (CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9 AND NOT WIN32)
-    add_definitions(-Wno-suggest-override -Wextra)
+    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-Wno-suggest-override> -Wextra)
 endif()
 
  ######################

libs/flake/svg/SvgGraphicContext.h
@@ -81,6 +81,8 @@ public:
     bool autoFillMarkers {false};
 
     KoSvgTextProperties textProperties;
+private:
+    SvgGraphicsContext& operator=(const SvgGraphicsContext &gc) = default; ///< used by copy constructor, shouldn't be public
 };
 
 #endif // SVGGRAPHICCONTEXT_H

libs/flake/text/KoSvgText.h
@@ -229,10 +229,6 @@ private:
 
 struct KoSvgCharChunkFormat : public QTextCharFormat
 {
-    KoSvgCharChunkFormat() {}
-    KoSvgCharChunkFormat(const KoSvgCharChunkFormat &rhs) : QTextCharFormat(rhs) {}
-    KoSvgCharChunkFormat(const QTextCharFormat &rhs) : QTextCharFormat(rhs) {}
-
     enum SvgCharProperty {
         TextAnchor = UserProperty + 1,
         AssociatedShape

libs/image/kis_name_server.cpp
@@ -18,19 +18,8 @@
 
 #include "kis_name_server.h"
 
-KisNameServer::KisNameServer(qint32 seed)
-{
-    m_generator = seed;
-}
-
-KisNameServer::KisNameServer(const KisNameServer &rhs)
-    : m_generator(rhs.m_generator)
-{
-}
-
-KisNameServer::~KisNameServer()
-{
-}
+KisNameServer::KisNameServer(qint32 seed) : m_generator(seed)
+{ }
 
 qint32 KisNameServer::currentSeed() const
 {

libs/image/kis_name_server.h
@@ -18,15 +18,13 @@
 #ifndef KIS_NAMESERVER_H_
 #define KIS_NAMESERVER_H_
 
-#include <QString>
+#include <QtGlobal>
 #include <kritaimage_export.h>
 
 class KRITAIMAGE_EXPORT KisNameServer
 {
 public:
     KisNameServer(qint32 seed = 1);
-    KisNameServer(const KisNameServer &rhs);
-    ~KisNameServer();
 
     qint32 number();
     qint32 currentSeed() const;

libs/pigment/resources/KisSwatch.cpp
@@ -21,16 +21,9 @@
 
 #include "KisSwatch.h"
 
-
-KisSwatch::KisSwatch()
-    : m_spotColor(false)
-    , m_valid(false)
-{ }
-
 KisSwatch::KisSwatch(const KoColor &color, const QString &name)
     : m_color(color)
     , m_name(name)
-    , m_spotColor(false)
     , m_valid(true)
 { }
 
@@ -56,16 +49,4 @@ void KisSwatch::setSpotColor(bool spotColor)
 {
     m_spotColor = spotColor;
     m_valid = true;
-}
-
-KisSwatch &KisSwatch::operator =(const KisSwatch &source)
-{
-    if (&source == this)
-        return *this;
-    m_color = source.m_color;
-    m_id = source.m_id;
-    m_name = source.m_name;
-    m_spotColor = source.m_spotColor;
-    m_valid = source.m_valid;
-    return *this;
 }

libs/pigment/resources/KisSwatch.h
@@ -29,7 +29,7 @@
 class KRITAPIGMENT_EXPORT KisSwatch
 {
 public:
-    KisSwatch();
+    KisSwatch() = default;
     KisSwatch(const KoColor &color, const QString &name = QString());
 
 public:
@@ -52,14 +52,12 @@ public:
         return m_color == rhs.m_color && m_name == rhs.m_name;
     }
 
-    KisSwatch &operator =(const KisSwatch &source);
-
 private:
     KoColor m_color;
     QString m_name;
     QString m_id;
-    bool m_spotColor;
-    bool m_valid;
+    bool m_spotColor {false};
+    bool m_valid {false};
 };
 
 #endif // KISSWATCH_H

commit 7572e2de933ab99a3508b0a6c8b944d4028a55ce

Fix arrow color in KoDualColorButton
commit f867486c5e835cd09d7894b1e4d2c69ce10dbdad accidentally replaced
deprecated QPalette::foreground() with window() instead of windowText().

BUG:416627

libs/ui/widgets/KoDualColorButton.cpp
@@ -61,7 +61,7 @@ public:
         arrowBitmap.fill(Qt::transparent);
 
         QPainter p(&arrowBitmap);
-        p.setPen(dialogParent->palette().window().color());
+        p.setPen(dialogParent->palette().windowText().color());
 
         // arrow pointing left
         p.drawLine(0, 3, 7, 3);
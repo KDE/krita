49e7ad10e169312b8729ecfbb81fe2a533a9185d
Boudewijn Rempt <boud@valdyas.org>
2020-01-23 14:02:18 +0100

    Make columns explicity NOT NULL

--- libs/resources/sql/create_resource_tags.sql
+++ libs/resources/sql/create_resource_tags.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS resource_tags (
     id INTEGER PRIMARY KEY
-,   resource_id INTEGER
-,   tag_id INTEGER
+,   resource_id NOT NULL INTEGER
+,   tag_id NOT NULL INTEGER
 ,   FOREIGN KEY(resource_id) REFERENCES resources(id)
 ,   FOREIGN KEY(tag_id) REFERENCES tags(id)
 );

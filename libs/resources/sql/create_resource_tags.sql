c516cc7609928d9a8f8399cc9b5718f8c65cfe46
Agata Cacko <tamtamy.tymona@gmail.com>
2020-01-24 02:10:51 +0100

    Fix crash after creating the database
    Before this commit, creating the database would be unsuccessful
    and Krita would crash short afterwards. This commit fixes the syntax
    of the query creating the database so Krita can continue to function.

--- libs/resources/sql/create_resource_tags.sql
+++ libs/resources/sql/create_resource_tags.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS resource_tags (
     id INTEGER PRIMARY KEY
-,   resource_id NOT NULL INTEGER
-,   tag_id NOT NULL INTEGER
+,   resource_id INTEGER NOT NULL
+,   tag_id INTEGER NOT NULL
 ,   FOREIGN KEY(resource_id) REFERENCES resources(id)
 ,   FOREIGN KEY(tag_id) REFERENCES tags(id)
 );

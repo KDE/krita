65a7222ae0438bffb9d033f8732320fa432ce561
Boudewijn Rempt <boud@valdyas.org>
2020-01-31 11:28:06 +0000

    Revert "For now, hard-code the version of PyQt"
    This reverts commit 451069045dcaf0b960dc5f3bbbbbc48a435f036d.

--- cmake/modules/FindPyQt5.py
+++ cmake/modules/FindPyQt5.py
@@ -6,32 +6,31 @@ import PyQt5.Qt
 import sys
 import os.path
 
-print("pyqt_version:5.12.1")
-print("pyqt_version_str:5.12.1")
+print("pyqt_version:%06.0x" % PyQt5.Qt.PYQT_VERSION)
+print("pyqt_version_str:%s" % PyQt5.Qt.PYQT_VERSION_STR)
 
-#pyqt_version_tag = ""
-#in_t = False
-#pyqt_config_list = PyQt5.QtCore.PYQT_CONFIGURATION["sip_flags"].split(' ')
-#for item in pyqt_config_list:
-    #if item == "-t":
-        #in_t = True
-    #elif in_t:
-        #if item.startswith("Qt_5"):
-            #pyqt_version_tag = item
-    #else:
-        #in_t = False
-print("pyqt_version_tag:5.12.1")
+pyqt_version_tag = ""
+in_t = False
+pyqt_config_list = PyQt5.Qt.PYQT_CONFIGURATION["sip_flags"].split(' ')
+for item in pyqt_config_list:
+    if item == "-t":
+        in_t = True
+    elif in_t:
+        if item.startswith("Qt_5"):
+            pyqt_version_tag = item
+    else:
+        in_t = False
+print("pyqt_version_tag:%s" % pyqt_version_tag)
 
-#try:
-    #index_n = pyqt_config_list.index('-n')
-    #pyqt_sip_name = '-n' + pyqt_config_list[index_n + 1]
-    #print("pyqt_sip_name:%s" % pyqt_sip_name)
-#except ValueError:
-    #pass
+try:
+    index_n = pyqt_config_list.index('-n')
+    pyqt_sip_name = '-n' + pyqt_config_list[index_n + 1]
+    print("pyqt_sip_name:%s" % pyqt_sip_name)
+except ValueError:
+    pass
 
 # FIXME This next line is just a little bit too crude.
 pyqt_sip_dir = os.path.join(sys.prefix, "share", "sip", "PyQt5")
 print("pyqt_sip_dir:%s" % pyqt_sip_dir)
 
-#print("pyqt_sip_flags:%s" % PyQt5.QtCore.PYQT_CONFIGURATION["sip_flags"])
-
+print("pyqt_sip_flags:%s" % PyQt5.Qt.PYQT_CONFIGURATION["sip_flags"])

dd8192f0caaea1cb6d21369b0953dfe5d637eb32
Boudewijn Rempt <boud@valdyas.org>
2020-02-19 12:49:08 +0100

    Fix style delegate for checkboxes

--- libs/resourcewidgets/dbexplorer/TableModel.cpp
+++ libs/resourcewidgets/dbexplorer/TableModel.cpp
@@ -18,6 +18,8 @@
 #include "TableModel.h"
 
 #include <QApplication>
+#include <QStyledItemDelegate>
+
 
 TableDelegate::TableDelegate(QObject *parent)
     : QSqlRelationalDelegate(parent)
@@ -27,16 +29,16 @@ void TableDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
 {
     QStyleOptionViewItem viewItemOption(option);
     // Only do this if we are accessing the column with boolean variables.
-    if (m_booleanColumns.contains(index.column())) {
-        // This basically changes the rectangle in which the check box is drawn.
-        const int textMargin = QApplication::style()->pixelMetric(QStyle::PM_FocusFrameHMargin) + 1;
-        QRect newRect = QStyle::alignedRect(option.direction, Qt::AlignCenter,
-                                            QSize(option.decorationSize.width() +
-                                                  5,option.decorationSize.height()),
-                                            QRect(option.rect.x() + textMargin, option.rect.y(),
-                                                  option.rect.width() -
-                                                  (2 * textMargin), option.rect.height()));
-        viewItemOption.rect = newRect;
+    if (index.column() == 7) {
+      // This basically changes the rectangle in which the check box is drawn.
+      const int textMargin = QApplication::style()->pixelMetric(QStyle::PM_FocusFrameHMargin) + 1;
+      QRect newRect = QStyle::alignedRect(option.direction, Qt::AlignCenter,
+                      QSize(option.decorationSize.width() +
+                            5,option.decorationSize.height()),
+                      QRect(option.rect.x() + textMargin, option.rect.y(),
+                            option.rect.width() -
+                            (2 * textMargin), option.rect.height()));
+      viewItemOption.rect = newRect;
     }
     // Draw the check box using the new rectangle.
     QSqlRelationalDelegate::paint(painter, viewItemOption, index);
@@ -50,7 +52,48 @@ QSize TableDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIn
 bool TableDelegate::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index)
 {
     if (m_editable) {
-        return QSqlRelationalDelegate::editorEvent(event, model, option, index);
+        Q_ASSERT(event);
+        Q_ASSERT(model);
+
+        // make sure that the item is checkable
+        Qt::ItemFlags flags = model->flags(index);
+        if (!(flags & Qt::ItemIsUserCheckable) || !(flags & Qt::ItemIsEnabled)) {
+          return false;
+        }
+
+        // make sure that we have a check state
+        QVariant value = index.data(Qt::CheckStateRole);
+        if (!value.isValid()) {
+          return false;
+        }
+
+        // make sure that we have the right event type
+        if (event->type() == QEvent::MouseButtonRelease) {
+          const int textMargin = QApplication::style()->pixelMetric(QStyle::PM_FocusFrameHMargin) + 1;
+          QRect checkRect = QStyle::alignedRect(option.direction, Qt::AlignCenter,
+                            option.decorationSize,
+                            QRect(option.rect.x() + (2 * textMargin), option.rect.y(),
+                              option.rect.width() - (2 * textMargin),
+                              option.rect.height()));
+          // We handle the mouse event...
+          QMouseEvent *mEvent = (QMouseEvent*) event;
+          if (!checkRect.contains(mEvent->pos())) {
+            return false;
+          }
+          // as well as the key press event.
+        } else if (event->type() == QEvent::KeyPress) {
+          if (static_cast<QKeyEvent*>(event)->key() !=
+          Qt::Key_Space&& static_cast<QKeyEvent*>(event)->key() != Qt::Key_Select) {
+            return false;
+          }
+        } else {
+          return false;
+        }
+        // Determine the new check state
+        Qt::CheckState state = (static_cast<Qt::CheckState>(value.toInt()) == Qt::Checked
+                    ? Qt::Unchecked : Qt::Checked);
+        // And set the new check state by calling the model's setData() function.
+        return model->setData(index, state, Qt::CheckStateRole);
     }
     return false;
 }

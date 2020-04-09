a41aa0bfe59d1206a88c02c6b83969f3c02ff125
Dmitry Kazakov <dimula73@gmail.com>
2020-04-09 13:13:16 +0300

    Add ifdefs for half type

--- libs/pigment/KoAlphaMaskApplicatorFactory.h
+++ libs/pigment/KoAlphaMaskApplicatorFactory.h
@@ -4,7 +4,11 @@
 #include "kritapigment_export.h"
 #include <KoVcMultiArchBuildSupport.h>
 #include <KoAlphaMaskApplicatorBase.h>
+
+#include <KoConfig.h>
+#ifdef HAVE_OPENEXR
 #include <half.h>
+#endif
 
 template<typename _channels_type_,
          int _channels_nb_,
@@ -22,22 +26,30 @@ public:
 
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint8,  4, 3>::create<Vc::CurrentImplementation::current()>(int);
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint16, 4, 3>::create<Vc::CurrentImplementation::current()>(int);
+#ifdef HAVE_OPENEXR
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<half,    4, 3>::create<Vc::CurrentImplementation::current()>(int);
+#endif
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<float,   4, 3>::create<Vc::CurrentImplementation::current()>(int);
 
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint8,  5, 4>::create<Vc::CurrentImplementation::current()>(int);
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint16, 5, 4>::create<Vc::CurrentImplementation::current()>(int);
+#ifdef HAVE_OPENEXR
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<half,    5, 4>::create<Vc::CurrentImplementation::current()>(int);
+#endif
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<float,   5, 4>::create<Vc::CurrentImplementation::current()>(int);
 
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint8,  2, 1>::create<Vc::CurrentImplementation::current()>(int);
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint16, 2, 1>::create<Vc::CurrentImplementation::current()>(int);
+#ifdef HAVE_OPENEXR
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<half,    2, 1>::create<Vc::CurrentImplementation::current()>(int);
+#endif
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<float,   2, 1>::create<Vc::CurrentImplementation::current()>(int);
 
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint8,  1, 0>::create<Vc::CurrentImplementation::current()>(int);
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<quint16, 1, 0>::create<Vc::CurrentImplementation::current()>(int);
+#ifdef HAVE_OPENEXR
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<half,    1, 0>::create<Vc::CurrentImplementation::current()>(int);
+#endif
 extern template KRITAPIGMENT_EXPORT KoAlphaMaskApplicatorBase* KoAlphaMaskApplicatorFactory<float,   1, 0>::create<Vc::CurrentImplementation::current()>(int);
 
 

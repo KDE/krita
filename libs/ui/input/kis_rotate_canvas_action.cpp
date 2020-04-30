KDE/krita

commit 63bef1e49334f67b4bb8549e427fdb59e119a6b0

Fix cursor drift when Pan/Zoom/Rotate
The actions should use absolute values for pan/zoom/rotation to
avoid relative drift between canvas and cursor.

NOTE: rotation still drifts a bit because of a bud in the underlying
      algorithm.

CCBUG:409460

libs/ui/input/kis_rotate_canvas_action.cpp
@@ -32,12 +32,13 @@
 class KisRotateCanvasAction::Private
 {
 public:
-    Private() : angleDrift(0), previousAngle(0) {}
+    Private() : previousAngle(0) {}
 
     Shortcut mode;
-    qreal angleDrift;
 
     qreal previousAngle;
+    qreal startRotation;
+    qreal previousRotation;
 };
 
 
@@ -92,11 +93,10 @@ void KisRotateCanvasAction::begin(int shortcut, QEvent *event)
 
     switch(shortcut) {
         case RotateModeShortcut:
-            d->mode = (Shortcut)shortcut;
-            break;
         case DiscreteRotateModeShortcut:
             d->mode = (Shortcut)shortcut;
-            d->angleDrift = 0;
+            d->startRotation = inputManager()->canvas()->rotationAngle();
+            d->previousRotation = 0;
             break;
         case RotateLeftShortcut:
             canvasController->rotateCanvasLeft15();
@@ -110,29 +110,27 @@ void KisRotateCanvasAction::begin(int shortcut, QEvent *event)
     }
 }
 
-void KisRotateCanvasAction::cursorMoved(const QPointF &lastPos, const QPointF &pos)
+void KisRotateCanvasAction::cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos)
 {
     const KisCoordinatesConverter *converter = inputManager()->canvas()->coordinatesConverter();
-    QPointF centerPoint = converter->flakeToWidget(converter->flakeCenterPoint());
-    QPointF oldPoint = lastPos - centerPoint;
-    QPointF newPoint = pos - centerPoint;
+    const QPointF centerPoint = converter->flakeToWidget(converter->flakeCenterPoint());
+    const QPointF startPoint = startPos - centerPoint;
+    const QPointF newPoint = pos - centerPoint;
 
-    qreal oldAngle = atan2(oldPoint.y(), oldPoint.x());
-    qreal newAngle = atan2(newPoint.y(), newPoint.x());
+    const qreal oldAngle = atan2(startPoint.y(), startPoint.x());
+    const qreal newAngle = atan2(newPoint.y(), newPoint.x());
 
-    qreal angle = (180 / M_PI) * (newAngle - oldAngle);
+    qreal newRotation = (180 / M_PI) * (newAngle - oldAngle);
 
     if (d->mode == DiscreteRotateModeShortcut) {
         const qreal angleStep = 15;
-        qreal initialAngle = inputManager()->canvas()->rotationAngle();
-        qreal roundedAngle = qRound((initialAngle + angle + d->angleDrift) / angleStep) * angleStep - initialAngle;
-        d->angleDrift += angle - roundedAngle;
-        angle = roundedAngle;
+        newRotation = qRound(newRotation / angleStep) * angleStep;
     }
 
     KisCanvasController *canvasController =
         dynamic_cast<KisCanvasController*>(inputManager()->canvas()->canvasController());
-    canvasController->rotateCanvas(angle);
+    canvasController->rotateCanvas(newRotation - d->previousRotation);
+    d->previousRotation = newRotation;
 }
 
 void KisRotateCanvasAction::inputEvent(QEvent* event)

libs/ui/input/kis_abstract_input_action.cpp
@@ -32,6 +32,7 @@ public:
     QHash<QString, int> indexes;
 
     QPointF lastCursorPosition;
+    QPointF startCursorPosition;
 
     static KisInputManager *inputManager;
 };
@@ -66,6 +67,7 @@ void KisAbstractInputAction::begin(int shortcut, QEvent *event)
 
     if (event) {
         d->lastCursorPosition = eventPosF(event);
+        d->startCursorPosition = d->lastCursorPosition;
     }
 }
 
@@ -74,6 +76,7 @@ void KisAbstractInputAction::inputEvent(QEvent *event)
     if (event) {
         QPointF newPosition = eventPosF(event);
         cursorMoved(d->lastCursorPosition, newPosition);
+        cursorMovedAbsolute(d->startCursorPosition, newPosition);
         d->lastCursorPosition = newPosition;
     }
 }
@@ -86,6 +89,12 @@ void KisAbstractInputAction::end(QEvent *event)
 void KisAbstractInputAction::cursorMoved(const QPointF &lastPos, const QPointF &pos)
 {
     Q_UNUSED(lastPos);
+    Q_UNUSED(pos);
+}
+
+void KisAbstractInputAction::cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos)
+{
+    Q_UNUSED(startPos);
     Q_UNUSED(pos);
 }
 

libs/ui/input/kis_abstract_input_action.h
@@ -203,6 +203,7 @@ protected:
      * The default implementation of inputEvent calls this function.
      */
     virtual void cursorMoved(const QPointF &lastPos, const QPointF &pos);
+    virtual void cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos);
 
     /**
      * Convenience method to extract the position from a cursor movement event.

libs/ui/input/kis_gamma_exposure_action.cpp
@@ -171,9 +171,9 @@ void KisGammaExposureAction::begin(int shortcut, QEvent *event)
     }
 }
 
-void KisGammaExposureAction::cursorMoved(const QPointF &lastPos, const QPointF &pos)
+void KisGammaExposureAction::cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos)
 {
-    QPointF diff = -(pos - lastPos);
+    QPointF diff = -(pos - startPos);
 
     const int step = 200;
 
@@ -184,11 +184,11 @@ void KisGammaExposureAction::cursorMoved(const QPointF &lastPos, const QPointF &
 
 
     if (d->mode == ExposureShortcut) {
-        d->baseExposure += qreal(diff.y()) / step;
-        interface->setCurrentExposure(d->baseExposure);
+        const qreal currentExposure = d->baseExposure + qreal(diff.y()) / step;
+        interface->setCurrentExposure(currentExposure);
     } else if (d->mode == GammaShortcut) {
-        d->baseGamma += qreal(diff.y()) / step;
-        interface->setCurrentGamma(d->baseGamma);
+        const qreal currentGamma = d->baseExposure + qreal(diff.y()) / step;
+        interface->setCurrentGamma(currentGamma);
     }
 }
 

libs/ui/input/kis_gamma_exposure_action.h
@@ -50,7 +50,7 @@ public:
     void deactivate(int shortcut) override;
 
     void begin(int shortcut, QEvent *event = 0) override;
-    void cursorMoved(const QPointF &lastPos, const QPointF &pos) override;
+    void cursorMovedAbsolute(const QPointF &lastPos, const QPointF &pos) override;
 
     bool isShortcutRequired(int shortcut) const override;
 

libs/ui/input/kis_pan_action.cpp
@@ -41,6 +41,7 @@ public:
     const int panDistance;
 
     QPointF lastPosition;
+    QPointF originalPreferredCenter;
 };
 
 KisPanAction::KisPanAction()
@@ -104,6 +105,8 @@ void KisPanAction::begin(int shortcut, QEvent *event)
                 break;
             }
 
+            d->originalPreferredCenter = inputManager()->canvas()->canvasController()->preferredCenter();
+
             break;
         }
         case PanLeftShortcut:
@@ -161,10 +164,9 @@ void KisPanAction::inputEvent(QEvent *event)
     KisAbstractInputAction::inputEvent(event);
 }
 
-void KisPanAction::cursorMoved(const QPointF &lastPos, const QPointF &pos)
+void KisPanAction::cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos)
 {
-    QPointF relMovement = -(pos - lastPos);
-    inputManager()->canvas()->canvasController()->pan(relMovement.toPoint());
+    inputManager()->canvas()->canvasController()->setPreferredCenter(-pos + startPos + d->originalPreferredCenter);
 }
 
 QPointF KisPanAction::Private::averagePoint( QTouchEvent* event )

libs/ui/input/kis_pan_action.h
@@ -52,7 +52,7 @@ public:
     void end(QEvent *event) override;
 
     void inputEvent(QEvent* event) override;
-    void cursorMoved(const QPointF &lastPos, const QPointF &pos) override;
+    void cursorMovedAbsolute(const QPointF &lastPos, const QPointF &pos) override;
 
     bool isShortcutRequired(int shortcut) const override;
 

libs/ui/input/kis_rotate_canvas_action.h
@@ -49,7 +49,7 @@ public:
     void activate(int shortcut) override;
     void deactivate(int shortcut) override;
     void begin(int shortcut, QEvent *event) override;
-    void cursorMoved(const QPointF &lastPos, const QPointF &pos) override;
+    void cursorMovedAbsolute(const QPointF &startPos, const QPointF &pos);
     void inputEvent(QEvent* event) override;
 
     KisInputActionGroup inputActionGroup(int shortcut) const override;

commit dc1530054dfc4b371bb7c1a9b280b3074d6b6d49

Fix descrete rotation to be aligned to absolute position, not relative
That was a regression from 63bef1e49334f67b4bb8549e427fdb59e119a6b0

BUG:410554

libs/ui/input/kis_rotate_canvas_action.cpp
@@ -124,7 +124,15 @@ void KisRotateCanvasAction::cursorMovedAbsolute(const QPointF &startPos, const Q
 
     if (d->mode == DiscreteRotateModeShortcut) {
         const qreal angleStep = 15;
-        newRotation = qRound(newRotation / angleStep) * angleStep;
+
+        // avoid jumps at the beginning of the rotation action
+        if (qAbs(newRotation) > 0.5 * angleStep) {
+            const qreal currentCanvasRotation = converter->rotationAngle();
+            const qreal desiredOffset = newRotation - d->previousRotation;
+            newRotation = qRound((currentCanvasRotation + desiredOffset) / angleStep) * angleStep - currentCanvasRotation + d->previousRotation;
+        } else {
+            newRotation = d->previousRotation;
+        }
     }
 
     KisCanvasController *canvasController =
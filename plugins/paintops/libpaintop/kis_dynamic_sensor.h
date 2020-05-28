d42139b351416468a449519f402b339d2ea2094d
Boudewijn Rempt <boud@valdyas.org>
2020-05-28 11:46:22 +0200

    Disambiguate two different kinds of fade
    BUG:422162

--- plugins/paintops/libpaintop/kis_dynamic_sensor.h
+++ plugins/paintops/libpaintop/kis_dynamic_sensor.h
@@ -38,18 +38,18 @@
 class QWidget;
 class KisPaintInformation;
 
-const KoID FuzzyPerDabId("fuzzy", ki18n("Fuzzy Dab")); ///< generate a random number
-const KoID FuzzyPerStrokeId("fuzzystroke", ki18n("Fuzzy Stroke")); ///< generate a random number
-const KoID SpeedId("speed", ki18n("Speed")); ///< generate a number depending on the speed of the cursor
-const KoID FadeId("fade", ki18n("Fade")); ///< generate a number that increase every time you call it (e.g. per dab)
-const KoID DistanceId("distance", ki18n("Distance")); ///< generate a number that increase with distance
-const KoID TimeId("time", ki18n("Time")); ///< generate a number that increase with time
-const KoID DrawingAngleId("drawingangle", ki18n("Drawing angle")); ///< number depending on the angle
-const KoID RotationId("rotation", ki18n("Rotation")); ///< rotation coming from the device
-const KoID PressureId("pressure", ki18n("Pressure")); ///< number depending on the pressure
-const KoID PressureInId("pressurein", ki18n("PressureIn")); ///< number depending on the pressure
-const KoID XTiltId("xtilt", ki18n("X-Tilt")); ///< number depending on X-tilt
-const KoID YTiltId("ytilt", ki18n("Y-Tilt")); ///< number depending on Y-tilt
+const KoID FuzzyPerDabId("fuzzy", ki18ncc("Context: dynamic sensors", "Fuzzy Dab")); ///< generate a random number
+const KoID FuzzyPerStrokeId("fuzzystroke", ki18ncc("Context: dynamic sensors", "Fuzzy Stroke")); ///< generate a random number
+const KoID SpeedId("speed", ki18ncc("Context: dynamic sensors", "Speed")); ///< generate a number depending on the speed of the cursor
+const KoID FadeId("fade", ki18nc("Context: dynamic sensors", "Fade")); ///< generate a number that increase every time you call it (e.g. per dab)
+const KoID DistanceId("distance", ki18ncc("Context: dynamic sensors", "Distance")); ///< generate a number that increase with distance
+const KoID TimeId("time", ki18ncc("Context: dynamic sensors", "Time")); ///< generate a number that increase with time
+const KoID DrawingAngleId("drawingangle", ki18ncc("Context: dynamic sensors", "Drawing angle")); ///< number depending on the angle
+const KoID RotationId("rotation", ki18ncc("Context: dynamic sensors", "Rotation")); ///< rotation coming from the device
+const KoID PressureId("pressure", ki18ncc("Context: dynamic sensors", "Pressure")); ///< number depending on the pressure
+const KoID PressureInId("pressurein", ki18ncc("Context: dynamic sensors", "PressureIn")); ///< number depending on the pressure
+const KoID XTiltId("xtilt", ki18ncc("Context: dynamic sensors", "X-Tilt")); ///< number depending on X-tilt
+const KoID YTiltId("ytilt", ki18ncc("Context: dynamic sensors", "Y-Tilt")); ///< number depending on Y-tilt
 
 /**
  * "TiltDirection" and "TiltElevation" parameters are written to
@@ -57,11 +57,11 @@ const KoID YTiltId("ytilt", ki18n("Y-Tilt")); ///< number depending on Y-tilt
  * compatibility with older presets from the days when they were called
  * differently.
  */
-const KoID TiltDirectionId("ascension", ki18n("Tilt direction")); /// < number depending on the X and Y tilt, tilt direction is 0 when stylus nib points to you and changes clockwise from -180 to +180.
-const KoID TiltElevationId("declination", ki18n("Tilt elevation")); /// < tilt elevation is 90 when stylus is perpendicular to tablet and 0 when it's parallel to tablet
+const KoID TiltDirectionId("ascension", ki18ncc("Context: dynamic sensors", "Tilt direction")); /// < number depending on the X and Y tilt, tilt direction is 0 when stylus nib points to you and changes clockwise from -180 to +180.
+const KoID TiltElevationId("declination", ki18ncc("Context: dynamic sensors", "Tilt elevation")); /// < tilt elevation is 90 when stylus is perpendicular to tablet and 0 when it's parallel to tablet
 
-const KoID PerspectiveId("perspective", ki18n("Perspective")); ///< number depending on the distance on the perspective grid
-const KoID TangentialPressureId("tangentialpressure", ki18n("Tangential pressure")); ///< the wheel on an airbrush device
+const KoID PerspectiveId("perspective", ki18ncc("Context: dynamic sensors", "Perspective")); ///< number depending on the distance on the perspective grid
+const KoID TangentialPressureId("tangentialpressure", ki18ncc("Context: dynamic sensors", "Tangential pressure")); ///< the wheel on an airbrush device
 const KoID SensorsListId("sensorslist", "SHOULD NOT APPEAR IN THE UI !"); ///< this a non user-visible sensor that can store a list of other sensors, and multiply their output
 
 class KisDynamicSensor;

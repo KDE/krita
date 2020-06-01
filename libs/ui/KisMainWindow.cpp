3cf35fd689d9cfd0ce28775b3ef87cfbdbe669b0
Sharaf Zaman <sharafzaz121@gmail.com>
2020-06-01 15:55:30 +0530

    Fix the way URIs are handled
    Prior to this commit, URIs were expected to resolve to
    some file on Android, but this assumption turned out to
    be flawed. URIs may or may not resolve. What is expected
    way of opening a file using them is to either use InputStreams
    or File Descriptors. Later is being used out of the two.
    
    BUG:422058
    
    # Conflicts:
    #	libs/ui/KisMainWindow.cpp

--- libs/ui/KisMainWindow.cpp
+++ libs/ui/KisMainWindow.cpp
@@ -564,7 +564,7 @@ KisMainWindow::KisMainWindow(QUuid uuid)
     connect(window, SIGNAL(screenChanged(QScreen *)), this, SLOT(windowScreenChanged(QScreen *)));
 
 #ifdef Q_OS_ANDROID
-    connect(d->fileManager, SIGNAL(sigFileSelected(QString)), this, SLOT(slotFileSelected(QString)));
+    connect(d->fileManager, SIGNAL(sigFileSelected(QUrl)), this, SLOT(slotFileSelected(QUrl)));
     connect(d->fileManager, SIGNAL(sigEmptyFilePath()), this, SLOT(slotEmptyFilePath()));
 
     QScreen *s = QGuiApplication::primaryScreen();
@@ -805,15 +805,9 @@ void KisMainWindow::setCanvasDetached(bool detach)
     }
 }
 
-void KisMainWindow::slotFileSelected(QString path)
+void KisMainWindow::slotFileSelected(QUrl url)
 {
-    QString url = path;
-    if (!url.isEmpty()) {
-        bool res = openDocument(QUrl::fromLocalFile(url), Import);
-        if (!res) {
-            warnKrita << "Loading" << url << "failed";
-        }
-    }
+    openDocumentInternal(url);
 }
 
 void KisMainWindow::slotEmptyFilePath()
@@ -1005,10 +999,17 @@ bool KisMainWindow::openDocument(const QUrl &url, OpenFlags flags)
 
 bool KisMainWindow::openDocumentInternal(const QUrl &url, OpenFlags flags)
 {
+#ifndef Q_OS_ANDROID
     if (!url.isLocalFile()) {
         qWarning() << "KisMainWindow::openDocumentInternal. Not a local file:" << url;
         return false;
     }
+#else
+    if (!QFile(url.toString()).exists()) {
+        qWarning() << "KisMainWindow::openDocumentInternal. Could not open:" << url;
+        return false;
+    }
+#endif
 
     KisDocument *newdoc = KisPart::instance()->createDocument();
 
@@ -1035,10 +1036,6 @@ bool KisMainWindow::openDocumentInternal(const QUrl &url, OpenFlags flags)
 
     KisPart::instance()->addDocument(newdoc);
     updateReloadFileAction(newdoc);
-
-    if (!QFileInfo(url.toLocalFile()).isWritable()) {
-        setReadWrite(false);
-    }
 
     // Try to determine whether this was an unnamed autosave
     if (flags & RecoveryFile &&

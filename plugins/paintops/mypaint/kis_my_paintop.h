f337f42b28dea8fcb331605ffe52048922910a6e
Ashwin Dhakaita <ashwingpdhakaita@gmail.com>
2020-06-13 12:18:52 +0530

    Added KisMyPaintBrush and KisMyPaintSurface classes

--- plugins/paintops/mypaint/kis_my_paintop.h
+++ plugins/paintops/mypaint/kis_my_paintop.h
@@ -3,14 +3,10 @@
 
 #include <brushengine/kis_paintop.h>
 #include <kis_types.h>
+#include "kis_mypaint_surface.h"
+#include "kis_mypaint_brush.h"
 
-//#include "kis_spray_paintop_settings.h"
-//#include "kis_brush_option.h"
-// #include <kis_airbrush_option_widget.h>
-// #include <kis_pressure_rotation_option.h>
-// #include <kis_pressure_opacity_option.h>
-// #include <kis_pressure_size_option.h>
-// #include <kis_pressure_rate_option.h>
+#include <libmypaint/mypaint-brush.h>
 
 class KisPainter;
 
@@ -32,9 +28,14 @@ protected:
     //KisTimingInformation updateTimingImpl(const KisPaintInformation &info) const override;
 
 private:
-    //KisSpacingInformation computeSpacing(const KisPaintInformation &info, qreal lodScale) const;
+    KisSpacingInformation computeSpacing(const KisPaintInformation &info, qreal lodScale) const;
 
 private:
+    KisMyPaintBrush *m_brush;
+    KisMyPaintSurface *m_surface;
+    KisPaintOpSettingsSP m_settings;
+    KisNodeSP m_node;
+    double dtime, m_radius;
 };
 
 #endif // KIS_MY_PAINTOP_H_

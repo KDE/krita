55a30377a0a19257724c23c15629edb6c49d51a1
L. E. Segovia <amy@amyspark.me>
2020-06-15 21:59:26 +0000

    Restore a generator's saved configuration if it exists first
    This prevents:
    * flickering  when opening properties for the first time
    * losing configuration if switching between generators

--- libs/ui/widgets/kis_wdg_generator.cpp
+++ libs/ui/widgets/kis_wdg_generator.cpp
@@ -162,7 +162,7 @@ void KisWdgGenerator::slotGeneratorActivated(int row)
             connect( widget, SIGNAL(sigConfigurationUpdated()), this, SIGNAL(previewConfiguration()));
 
             widget->setView(d->view);
-            widget->setConfiguration(d->currentGenerator->defaultConfiguration());
+            widget->setConfiguration(configuration());
         }
     }
     d->widgetLayout->addWidget(d->centralWidget, 0 , 0);

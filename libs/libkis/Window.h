4ca407bcc26ca4ee490fe81f71ebcbae20845058
Scott Petrovic <scottpetrovic@gmail.com>
2020-07-15 17:14:44 -0500

    Add signal when active view changes in python
    (cherry picked from commit 7fb9f99e835bf5571bf2ead188d7d17bdd56b47d)

--- libs/libkis/Window.h
+++ libs/libkis/Window.h
@@ -104,6 +104,9 @@ Q_SIGNALS:
     ///  Emitted when we change the color theme
     void themeChanged();
 
+    /// Emitted when the active view changes
+    void activeViewChanged();
+
 private:
     struct Private;
     Private *const d;

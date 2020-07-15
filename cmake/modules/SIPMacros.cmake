49ca998e2435d3ced3a809610341c543bbffc9d9
Boudewijn Rempt <boud@valdyas.org>
2020-07-15 20:28:39 +0100

    Revert "Generate sip.h in ${CMAKE_CURRENT_SIP_OUTPUT_DIR} for SIP v5"
    This reverts commit cee6af91dbccedb6e7b24469190e45c5acd95a45.

--- cmake/modules/SIPMacros.cmake
+++ cmake/modules/SIPMacros.cmake
@@ -99,12 +99,6 @@ MACRO(ADD_SIP_PYTHON_MODULE MODULE_NAME MODULE_SIP)
         COMMAND ${SIP_EXECUTABLE} ${_sip_tags} ${_sip_x} ${SIP_EXTRA_OPTIONS} -j ${SIP_CONCAT_PARTS} -c ${CMAKE_CURRENT_SIP_OUTPUT_DIR} ${_sip_includes} ${_abs_module_sip}
         DEPENDS ${_abs_module_sip} ${SIP_EXTRA_FILES_DEPEND}
     )
-    IF (SIP_MODULE_EXECUTABLE)
-        ADD_CUSTOM_COMMAND(
-            OUTPUT ${_sip_output_files} APPEND
-            COMMAND ${SIP_MODULE_EXECUTABLE} --target-dir ${CMAKE_CURRENT_SIP_OUTPUT_DIR} --sip-h ${PYQT5_SIP_NAME}
-        )
-    ENDIF (SIP_MODULE_EXECUTABLE)
     # not sure if type MODULE could be uses anywhere, limit to cygwin for now
     IF (WIN32 OR CYGWIN OR APPLE)
         ADD_LIBRARY(${_logical_name} MODULE ${_sip_output_files} )

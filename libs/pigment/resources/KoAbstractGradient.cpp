b6a93e9f6717e917fa75f25e58006bba3e1d9fc0
Dmitry Kazakov <dimula73@gmail.com>
2020-07-16 23:44:57 +0300

    Fix gradients that depend on Fg/Bg colors
    When a gradient is used for a stroke, it is cloned via
    cloneAndBakeVariableColors(), which basically converts it into
    a normal gradient with statci colors.
    
    The current Fg/Bg colors are fetched via provided
    KoCanvasResourcesInterfaceSP pointer.
    
    The patch also implements a version of generatePreview() that
    takes requiredCanvasResources() into account, but it is not used yet.

--- libs/pigment/resources/KoAbstractGradient.cpp
+++ libs/pigment/resources/KoAbstractGradient.cpp
@@ -56,6 +56,18 @@ KoAbstractGradient::KoAbstractGradient(const KoAbstractGradient &rhs)
 {
 }
 
+KoAbstractGradientSP KoAbstractGradient::cloneAndBakeVariableColors(KoCanvasResourcesInterfaceSP canvasResourcesInterface) const
+{
+    KoAbstractGradientSP result = this->clone().dynamicCast<KoAbstractGradient>();
+    result->bakeVariableColors(canvasResourcesInterface);
+    return result;
+}
+
+void KoAbstractGradient::bakeVariableColors(KoCanvasResourcesInterfaceSP canvasResourcesInterface)
+{
+    Q_UNUSED(canvasResourcesInterface);
+}
+
 void KoAbstractGradient::colorAt(KoColor&, qreal t) const
 {
     Q_UNUSED(t);
@@ -142,6 +154,20 @@ QImage KoAbstractGradient::generatePreview(int width, int height) const
     }
 
     return image;
+}
+
+QImage KoAbstractGradient::generatePreview(int width, int height, KoCanvasResourcesInterfaceSP canvasResourcesInterface) const
+{
+    QImage result;
+
+    if (!requiredCanvasResources().isEmpty()) {
+        KoAbstractGradientSP gradient = cloneAndBakeVariableColors(canvasResourcesInterface);
+        result = gradient->generatePreview(width, height);
+    } else {
+        result = generatePreview(width, height);
+    }
+
+    return result;
 }
 
 void KoAbstractGradient::updatePreview()

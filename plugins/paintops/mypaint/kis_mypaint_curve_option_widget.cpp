679bd449c0ff2faff194edfe051260e313135830
Ashwin Dhakaita <ashwingpdhakaita@gmail.com>
2020-08-02 20:01:00 +0530

    add missing licenses

--- plugins/paintops/mypaint/kis_mypaint_curve_option_widget.cpp
+++ plugins/paintops/mypaint/kis_mypaint_curve_option_widget.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008 Boudewijn Rempt <boud@valdyas.org>
+ * Copyright (C) 2009 Sven Langkamp   <sven.langkamp@gmail.com>
+ * Copyright (C) 2011 Silvio Heinrich <plassy@web.de>
+ * Copyright (c) 2020 Ashwin Dhakaita <ashwingpdhakaita@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
 #include "kis_mypaint_curve_option_widget.h"
 
 #include "ui_wdgmypaintcurveoption.h"

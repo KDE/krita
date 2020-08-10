KDE/krita

commit e39e6379564b5f6c4c72960a6e1ee3ca7716d5bd

Fix undo of image colorspace conversion
The color space of the image projection should be change **before**
other layers are converted. Some layers, which are updated using
resetCache() fetch this information from the image, therefore it
should be prepared beforehand.

libs/image/kis_paint_device_data.h
@@ -100,13 +100,12 @@ public:
         }
 
         void redo() override {
-            KUndo2Command::redo();
-
-            if (!m_firstRun) {
+            if (m_firstRun) {
                 m_firstRun = false;
                 return;
             }
 
+            KUndo2Command::redo();
             forcedRedo();
         }
 

libs/image/CMakeLists.txt
@@ -85,7 +85,6 @@ set(kritaimage_LIB_SRCS
    commands/kis_image_change_layers_command.cpp
    commands/kis_image_change_visibility_command.cpp
    commands/kis_image_command.cpp
-   commands/kis_image_set_projection_color_space_command.cpp
    commands/kis_image_layer_add_command.cpp
    commands/kis_image_layer_move_command.cpp
    commands/kis_image_layer_remove_command.cpp

libs/image/commands/kis_image_commands.h
@@ -21,7 +21,6 @@
 
 #include "kis_image_change_layers_command.h"
 #include "kis_image_command.h"
-#include "kis_image_set_projection_color_space_command.h"
 #include "kis_image_layer_add_command.h"
 #include "kis_image_layer_move_command.h"
 #include "kis_image_layer_remove_command.h"

libs/image/commands/kis_image_set_projection_color_space_command.cpp
@@ -1,52 +1,0 @@
-/*
- *  Copyright (c) 2002 Patrick Julien <freak@codepimps.org>
- *  Copyright (c) 2007 Sven Langkamp <sven.langkamp@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-#include "kis_image_commands.h"
-
-#include "KoColorSpace.h"
-#include "kis_image.h"
-
-#include <klocalizedstring.h>
-
-
-KisImageSetProjectionColorSpaceCommand::KisImageSetProjectionColorSpaceCommand(KisImageWSP image, const KoColorSpace * afterColorSpace)
-    : KisImageCommand(kundo2_i18n("Convert Image Type"), image)
-{
-    KisImageSP imageSP = image.toStrongRef();
-    if (imageSP) {
-        m_beforeColorSpace = imageSP->colorSpace();
-        m_afterColorSpace = afterColorSpace;
-    }
-}
-
-void KisImageSetProjectionColorSpaceCommand::redo()
-{
-    KisImageSP image = m_image.toStrongRef();
-    if (image) {
-        image->setProjectionColorSpace(m_afterColorSpace);
-    }
-}
-
-void KisImageSetProjectionColorSpaceCommand::undo()
-{
-    KisImageSP image = m_image.toStrongRef();
-    if (image) {
-        image->setProjectionColorSpace(m_beforeColorSpace);
-    }
-}

libs/image/commands/kis_image_set_projection_color_space_command.h
@@ -1,41 +1,0 @@
-/*
- *  Copyright (c) 2002 Patrick Julien <freak@codepimps.org>
- *  Copyright (c) 2007 Sven Langkamp <sven.langkamp@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-#ifndef KIS_IMAGE_SET_PROJECTION_COLOR_SPACE_COMMAND_H_
-#define KIS_IMAGE_SET_PROJECTION_COLOR_SPACE_COMMAND_H_
-
-#include "kis_image_command.h"
-
-class KoColorSpace;
-
-class KisImageSetProjectionColorSpaceCommand : public KisImageCommand
-{
-public:
-    KisImageSetProjectionColorSpaceCommand(KisImageWSP image, const KoColorSpace * afterColorSpace);
-
-    void redo() override;
-    void undo() override;
-
-private:
-    const KoColorSpace * m_beforeColorSpace;
-    const KoColorSpace * m_afterColorSpace;
-};
-
-
-#endif /* KIS_IMAGE_SET_PROJECTION_COLOR_SPACE_COMMAND_H_ */

libs/image/kis_image.cc
@@ -244,6 +244,8 @@ public:
     bool tryCancelCurrentStrokeAsync();
 
     void notifyProjectionUpdatedInPatches(const QRect &rc, QVector<KisRunnableStrokeJobData *> &jobs);
+
+    struct SetImageProjectionColorSpace;
 };
 
 KisImage::KisImage(KisUndoStore *undoStore, qint32 width, qint32 height, const KoColorSpace * colorSpace, const QString& name)
@@ -1128,6 +1130,29 @@ void KisImage::convertLayerColorSpace(KisNodeSP node,
     applicator.end();
 }
 
+struct KisImage::KisImagePrivate::SetImageProjectionColorSpace : public KisCommandUtils::FlipFlopCommand
+{
+    SetImageProjectionColorSpace(const KoColorSpace *cs, KisImageWSP image,
+                                 State initialState, KUndo2Command *parent = 0)
+        : KisCommandUtils::FlipFlopCommand(initialState, parent),
+          m_cs(cs),
+          m_image(image)
+    {
+    }
+
+    void partA() override {
+        KisImageSP image = m_image;
+
+        if (image) {
+            image->setProjectionColorSpace(m_cs);
+        }
+    }
+
+private:
+    const KoColorSpace *m_cs;
+    KisImageWSP m_image;
+};
+
 void KisImage::convertImageColorSpace(const KoColorSpace *dstColorSpace,
                                       KoColorConversionTransformation::Intent renderingIntent,
                                       KoColorConversionTransformation::ConversionFlags conversionFlags)
@@ -1148,14 +1173,23 @@ void KisImage::convertImageColorSpace(const KoColorSpace *dstColorSpace,
                                        emitSignals, actionName);
 
     applicator.applyCommand(
-        new KisImageSetProjectionColorSpaceCommand(KisImageWSP(this), dstColorSpace),
-                KisStrokeJobData::SEQUENTIAL);
+        new KisImagePrivate::SetImageProjectionColorSpace(dstColorSpace,
+                                                          KisImageWSP(this),
+                                                          KisCommandUtils::FlipFlopCommand::INITIALIZING),
+        KisStrokeJobData::BARRIER);
 
     applicator.applyVisitor(
         new KisConvertColorSpaceProcessingVisitor(
             srcColorSpace, dstColorSpace,
             renderingIntent, conversionFlags),
         KisStrokeJobData::CONCURRENT);
+
+    applicator.applyCommand(
+        new KisImagePrivate::SetImageProjectionColorSpace(srcColorSpace,
+                                                          KisImageWSP(this),
+                                                          KisCommandUtils::FlipFlopCommand::FINALIZING),
+        KisStrokeJobData::BARRIER);
+
 
     applicator.end();
 }
@@ -1212,13 +1246,22 @@ bool KisImage::assignImageProfile(const KoColorProfile *profile)
                                        emitSignals, actionName);
 
     applicator.applyCommand(
-        new KisImageSetProjectionColorSpaceCommand(KisImageWSP(this), dstColorSpace),
-                KisStrokeJobData::SEQUENTIAL);
+        new KisImagePrivate::SetImageProjectionColorSpace(dstColorSpace,
+                                                          KisImageWSP(this),
+                                                          KisCommandUtils::FlipFlopCommand::INITIALIZING),
+        KisStrokeJobData::BARRIER);
 
     applicator.applyVisitor(
         new KisAssignProfileProcessingVisitor(
             srcColorSpace, dstColorSpace),
         KisStrokeJobData::CONCURRENT);
+
+    applicator.applyCommand(
+        new KisImagePrivate::SetImageProjectionColorSpace(srcColorSpace,
+                                                          KisImageWSP(this),
+                                                          KisCommandUtils::FlipFlopCommand::FINALIZING),
+        KisStrokeJobData::BARRIER);
+
 
     applicator.end();
 

libs/image/kis_image.h
@@ -1159,7 +1159,6 @@ private:
     friend class KisImageResizeCommand;
     void setSize(const QSize& size);
 
-    friend class KisImageSetProjectionColorSpaceCommand;
     void setProjectionColorSpace(const KoColorSpace * colorSpace);
 
 

libs/image/kis_paint_device.cc
@@ -917,13 +917,12 @@ public:
 
     void redo() override
     {
-        KUndo2Command::redo();
-
-        if (!m_firstRun) {
+        if (m_firstRun) {
             m_firstRun = false;
             return;
         }
 
+        KUndo2Command::redo();
         emitNotifications();
     }
 

commit 88aa1a2bd9d04dbb3e6cc3468de8ba81f5024427

Fix first redo() after paint device color space conversion
There is a bug in all versions of Krita since November, 5th 2019.

BUG
===

1) Change image color space from RGBA to GrayA.
2) Undo with Ctrl+Z
3) Redo with Ctrl+Shift+Z

The image looks as if the color space has changed back to GrayA,
but it is not so. Only the color space of the projection has changed
to GrayA, the layers are still in RGBA. It can be seen by the state
of Advanced Color Selector (it shows colors instead of shades of gray)
and in layers' properties.

WORKAROUND
==========

1) Press Undo (Ctrl+Z) and Redo (Ctrl+Shift+Z) again.

The image will enter the correct state. All further undo/redo
operations for this particular action will be executed correctly.

TECHNICAL DETAILS
=================

The bug was called by commit e39e6379564b5f6c4c72960a6e1ee3ca7716d5bd,
which caused DeviceChangeColorSpaceCommand to skip the first redo of
the operation, which was not in its design.

The bug has been caught by KisColorizeMaskTest.

BUG:416584
CC:kimageshop@kde.org

libs/command/kis_command_utils.cpp
@@ -198,10 +198,10 @@ namespace KisCommandUtils
     }
 
     void CompositeCommand::undo() {
-        KUndo2Command::undo();
         for (auto it = m_commands.rbegin(); it != m_commands.rend(); ++it) {
             (*it)->undo();
         }
+        KUndo2Command::undo();
     }
 
 }

libs/image/kis_paint_device_data.h
@@ -94,19 +94,11 @@ public:
         {
         }
 
-        virtual void forcedRedo() {
+        void redo() override {
+            KUndo2Command::redo();
+
             m_data->m_colorSpace = m_newCs;
             m_data->m_cache.setupCache();
-        }
-
-        void redo() override {
-            if (m_firstRun) {
-                m_firstRun = false;
-                return;
-            }
-
-            KUndo2Command::redo();
-            forcedRedo();
         }
 
         void undo() override {
@@ -138,9 +130,9 @@ public:
         {
         }
 
-        void forcedRedo() override {
+        void redo() override {
+            ChangeProfileCommand::redo();
             m_data->m_dataManager = m_newDm;
-            ChangeProfileCommand::forcedRedo();
         }
 
         void undo() override {
@@ -162,7 +154,11 @@ public:
             new ChangeProfileCommand(this,
                                      m_colorSpace, dstColorSpace,
                                      parentCommand);
-        cmd->forcedRedo();
+
+        // NOTE: first redo is skipped on a higher level,
+        //       at DeviceChangeColorSpaceCommand
+        cmd->redo();
+
         if (!parentCommand) {
             delete cmd;
         }
@@ -216,7 +212,11 @@ public:
                                         m_dataManager, dstDataManager,
                                         m_colorSpace, dstColorSpace,
                                         parentCommand);
-        cmd->forcedRedo();
+
+        // NOTE: first redo is skipped on a higher level,
+        //       at DeviceChangeColorSpaceCommand
+        cmd->redo();
+
         if (!parentCommand) {
             delete cmd;
         }
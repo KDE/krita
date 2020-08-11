2bf0737d3b7e4b547bf43627caa964bd6c9be468
Dmitry Kazakov <dimula73@gmail.com>
2020-08-11 22:02:49 +0300

    Fix invalid memory access when using prepareClone() on devices with different color spaces
    The bug has been caughed by KisPaintDeviceTest runnning under ASAN

--- libs/image/kis_paint_device.cc
+++ libs/image/kis_paint_device.cc
@@ -536,10 +536,20 @@ private:
          * default bounds object, and only after that ask
          * currentData() to start cloning.
          */
-        q->setDefaultPixel(KoColor(srcData->dataManager()->defaultPixel(), colorSpace()));
         q->setDefaultBounds(src->defaultBounds());
 
         currentData()->prepareClone(srcData);
+
+
+        /**
+         * Default pixel must be updated **after** the color space
+         * of the device has been adjusted in prpareClone(). Otherwise,
+         * colorSpace() of the resulting KoColor object will be
+         * incorrect.
+         */
+        KIS_SAFE_ASSERT_RECOVER_RETURN(*colorSpace() == *src->colorSpace());
+        q->setDefaultPixel(KoColor(srcData->dataManager()->defaultPixel(), colorSpace()));
+
     }
 
     bool fastBitBltPossibleImpl(Data *srcData)

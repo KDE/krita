KDE/krita

commit 7956d4e1457b6f03704602f7250e6958877bcc55

Disable doing bitBlt outside extent when alpha channel is locked
The the user paints outside the contours of the layers with alpha
channel locked, we shouldn't do noop-writes to the layer. Instead,
we should just skip these areas alltogether.

BUG:394439

libs/image/kis_painter.cc
@@ -500,6 +500,19 @@ inline bool KisPainter::Private::tryReduceSourceRect(const KisPaintDevice *srcDe
         needsReadjustParams = true;
     }
 
+    if (!paramInfo.channelFlags.isEmpty()) {
+        const QBitArray onlyColor = colorSpace->channelFlags(true, false);
+        KIS_SAFE_ASSERT_RECOVER_NOOP(onlyColor.size() == paramInfo.channelFlags.size());
+
+        // check if we have alpha channel locked
+        if ((paramInfo.channelFlags & onlyColor) == paramInfo.channelFlags) {
+            *srcRect &= device->extent();
+
+            if (srcRect->isEmpty()) return true;
+            needsReadjustParams = true;
+        }
+    }
+
     if (needsReadjustParams) {
         // Readjust the function paramenters to the new dimensions.
         *dstX += srcRect->x() - *srcX;    // This will only add, not subtract

commit 099e27d47aeee930169266a480bc393012636ec6

Fix painting with colorsmudge brush on a alpha-locked layer
That was a regression from my commit 7956d4e1457 in early June.
The cropping algorithm didn't take into account the possible offset
between src and dst rects.

BUG:425152

libs/image/kis_painter.cc
@@ -508,7 +508,8 @@ inline bool KisPainter::Private::tryReduceSourceRect(const KisPaintDevice *srcDe
 
         // check if we have alpha channel locked
         if ((paramInfo.channelFlags & onlyColor) == paramInfo.channelFlags) {
-            *srcRect &= device->extent();
+            *srcRect &= device->extent().translated(*srcX - *dstX,
+                                                    *srcY - *dstY);
 
             if (srcRect->isEmpty()) return true;
             needsReadjustParams = true;
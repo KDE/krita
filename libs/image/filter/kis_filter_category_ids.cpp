4cb433540c24ed5a3657add6789fa3e5256dd810
Dmitry Kazakov <dimula73@gmail.com>
2020-08-28 12:16:28 +0300

    Fix comma at the end of "Blur," filter category
    It was accidentally added when adding contexts to translations

--- libs/image/filter/kis_filter_category_ids.cpp
+++ libs/image/filter/kis_filter_category_ids.cpp
@@ -23,7 +23,7 @@
 
 const KoID FiltersCategoryAdjustId("adjust_filters", ki18nc("The category of color adjustment filters, like levels. Verb.", "Adjust"));
 const KoID FiltersCategoryArtisticId("artistic_filters", ki18nc("The category of artistic filters, like raindrops. Adjective.", "Artistic"));
-const KoID FiltersCategoryBlurId("blur_filters", ki18nc("The category of blur filters, like gaussian blur. Verb.", "Blur,"));
+const KoID FiltersCategoryBlurId("blur_filters", ki18nc("The category of blur filters, like gaussian blur. Verb.", "Blur"));
 const KoID FiltersCategoryColorId("color_filters", ki18nc("The category of color transfer filters, like color to alpha. Noun.", "Colors"));
 const KoID FiltersCategoryEdgeDetectionId("edge_filters", ki18nc("The category of edge detection filters. Noun.", "Edge Detection"));
 const KoID FiltersCategoryEmbossId("emboss_filters", ki18nc("The category of emboss filters. Verb.", "Emboss"));

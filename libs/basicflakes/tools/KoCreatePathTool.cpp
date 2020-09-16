695025dfdb839f4941eb3517453ded5cb4d54a7d
Dmitry Kazakov <dimula73@gmail.com>
2020-09-16 16:12:22 +0300

    Fix snapping decorations in Create Path Tool
    It was a regression fron November 2019
    
    BUG:426514

--- libs/basicflakes/tools/KoCreatePathTool.cpp
+++ libs/basicflakes/tools/KoCreatePathTool.cpp
@@ -130,7 +130,7 @@ void KoCreatePathTool::paint(QPainter &painter, const KoViewConverter &converter
     }
 
     painter.save();
-    painter.setTransform(converter.documentToView());
+    painter.setTransform(converter.documentToView(), true);
     canvas()->snapGuide()->paint(painter, converter);
     painter.restore();
 }

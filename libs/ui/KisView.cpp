0fcb61926705c19cafac723f76e5eee3393b091a
Dmitry Kazakov <dimula73@gmail.com>
2020-09-22 12:36:51 +0300

    Fix crash when loading a file with reference images
    Initialization of reference images decorations should be postponed
    until KisView is fully initialized.
    
    BUG:426839

--- libs/ui/KisView.cpp
+++ libs/ui/KisView.cpp
@@ -111,8 +111,6 @@ public:
         , canvas(&viewConverter, viewManager->canvasResourceProvider()->resourceManager(), viewManager->mainWindow(), _q, document->shapeController())
         , zoomManager(_q, &this->viewConverter, &this->canvasController)
         , viewManager(viewManager)
-        , paintingAssistantsDecoration(new KisPaintingAssistantsDecoration(_q))
-        , referenceImagesDecoration(new KisReferenceImagesDecoration(_q, document))
         , floatingMessageCompressor(100, KisSignalCompressor::POSTPONE)
     {
     }
@@ -236,9 +234,11 @@ KisView::KisView(KisDocument *document, KisViewManager *viewManager, QWidget *pa
     connect(d->document, SIGNAL(sigLoadingFinished()), this, SLOT(slotLoadingFinished()));
     connect(d->document, SIGNAL(sigSavingFinished()), this, SLOT(slotSavingFinished()));
 
+    d->referenceImagesDecoration = new KisReferenceImagesDecoration(this, document);
     d->canvas.addDecoration(d->referenceImagesDecoration);
     d->referenceImagesDecoration->setVisible(true);
 
+    d->paintingAssistantsDecoration = new KisPaintingAssistantsDecoration(this);
     d->canvas.addDecoration(d->paintingAssistantsDecoration);
     d->paintingAssistantsDecoration->setVisible(true);
 

KDE/krita

commit d60b315d4c51895eec12288e96790b6a4a602b01

Fix sanity assert when destroying image right after pasting a layer into it
Basically, KisNodeJugglerCompressed should stop storing **any** pointers
to KisImage after slotEndStrokeRequested() has arrived.

The pointers are stored in Private and BatchMoveUpdateData.

CCBUG:423752

libs/ui/kis_node_juggler_compressed.cpp
@@ -694,10 +694,6 @@ struct KisNodeJugglerCompressed::Private
 KisNodeJugglerCompressed::KisNodeJugglerCompressed(const KUndo2MagicString &actionName, KisImageSP image, KisNodeManager *nodeManager, int timeout)
     : m_d(new Private(this, actionName, image, nodeManager, timeout))
 {
-    connect(m_d->image, SIGNAL(sigStrokeCancellationRequested()), SLOT(slotEndStrokeRequested()));
-    connect(m_d->image, SIGNAL(sigUndoDuringStrokeRequested()), SLOT(slotCancelStrokeRequested()));
-    connect(m_d->image, SIGNAL(sigStrokeEndRequestedActiveNodeFiltered()), SLOT(slotEndStrokeRequested()));
-    connect(m_d->image, SIGNAL(sigAboutToBeDeleted()), SLOT(slotImageAboutToBeDeleted()));
 
     KisImageSignalVector emitSignals;
     emitSignals << ModifiedSignal;
@@ -709,6 +705,11 @@ KisNodeJugglerCompressed::KisNodeJugglerCompressed(const KUndo2MagicString &acti
                                     actionName));
     connect(this, SIGNAL(requestUpdateAsyncFromCommand()), SLOT(startTimers()));
     connect(&m_d->compressor, SIGNAL(timeout()), SLOT(slotUpdateTimeout()));
+
+    connect(m_d->image, SIGNAL(sigStrokeCancellationRequested()), SLOT(slotEndStrokeRequested()));
+    connect(m_d->image, SIGNAL(sigUndoDuringStrokeRequested()), SLOT(slotCancelStrokeRequested()));
+    connect(m_d->image, SIGNAL(sigStrokeEndRequestedActiveNodeFiltered()), SLOT(slotEndStrokeRequested()));
+    connect(m_d->image, SIGNAL(sigAboutToBeDeleted()), SLOT(slotImageAboutToBeDeleted()));
 
     m_d->applicator->applyCommand(
         new UpdateMovedNodesCommand(m_d->updateData, false));
@@ -849,6 +850,8 @@ void KisNodeJugglerCompressed::cleanup()
 {
     m_d->applicator.reset();
     m_d->compressor.stop();
+    m_d->image.clear();
+    m_d->updateData.clear();
     m_d->isStarted = false;
 
     if (m_d->autoDelete) {

libs/image/kis_idle_watcher.cpp
@@ -121,7 +121,9 @@ void KisIdleWatcher::slotIdleCheckTick()
     if (isIdle()) {
         if (m_d->idleCheckCounter >= Private::IDLE_CHECK_COUNT) {
             stopIdleCheck();
-            emit startedIdleMode();
+            if (!m_d->trackedImages.isEmpty()) {
+                emit startedIdleMode();
+            }
         } else {
             m_d->idleCheckCounter++;
             m_d->idleCheckTimer.start();

libs/ui/KisDocument.cpp
@@ -543,6 +543,7 @@ KisDocument::~KisDocument()
 {
     // wait until all the pending operations are in progress
     waitForSavingToComplete();
+    d->imageIdleWatcher.setTrackedImage(0);
 
     /**
      * Push a timebomb, which will try to release the memory after

commit 1ff0d2027a1945fd9117ebfb4f7de67cb1b6d819

Fix a crash when moving local selection mask
That is a regression from d60b315d4c51895eec12288e96790b6a4a602b01

BUG:426816

libs/ui/kis_node_juggler_compressed.cpp
@@ -832,6 +832,11 @@ void KisNodeJugglerCompressed::startTimers()
 
 void KisNodeJugglerCompressed::slotUpdateTimeout()
 {
+    // The juggler could have been already finished explicitly
+    // by slotEndStrokeRequested(). In such a case the final updates
+    // will be issued by the last command of the stroke.
+
+    if (!m_d->updateData) return;
     m_d->updateData->processUnhandledUpdates();
 }
 
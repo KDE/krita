04d3a19a7f0249cef27e1f86838a17e11b744bd7
Sharaf Zaman <sharafzaz121@gmail.com>
2020-10-09 04:40:18 +0000

    Regression fix: reference image from clipboard throws error
    BUG:427466
    
    
    (cherry picked from commit 7f7b5c17a1ff1887113d86a43ea14b11174ff7f8)

--- plugins/tools/defaulttool/referenceimagestool/ToolReferenceImages.cpp
+++ plugins/tools/defaulttool/referenceimagestool/ToolReferenceImages.cpp
@@ -108,9 +108,10 @@ void ToolReferenceImages::addReferenceImage()
     if (!QFileInfo(filename).exists()) return;
 
     auto *reference = KisReferenceImage::fromFile(filename, *kisCanvas->coordinatesConverter(), canvas()->canvasWidget());
-    if (document()->referenceImagesLayer() && reference) {
-        reference->setZIndex(document()->referenceImagesLayer()->shapes().size());
-
+    if (reference) {
+        if (document()->referenceImagesLayer()) {
+            reference->setZIndex(document()->referenceImagesLayer()->shapes().size());
+        }
         KisDocument *doc = document();
         doc->addCommand(KisReferenceImagesLayer::addReferenceImages(doc, {reference}));
     }
@@ -122,9 +123,10 @@ void ToolReferenceImages::pasteReferenceImage()
     KIS_ASSERT_RECOVER_RETURN(kisCanvas);
 
     KisReferenceImage* reference = KisReferenceImage::fromClipboard(*kisCanvas->coordinatesConverter());
-    if (document()->referenceImagesLayer() && reference) {
-        reference->setZIndex(document()->referenceImagesLayer()->shapes().size());
-
+    if (reference) {
+        if (document()->referenceImagesLayer()) {
+            reference->setZIndex(document()->referenceImagesLayer()->shapes().size());
+        }
         KisDocument *doc = document();
         doc->addCommand(KisReferenceImagesLayer::addReferenceImages(doc, {reference}));
     } else {

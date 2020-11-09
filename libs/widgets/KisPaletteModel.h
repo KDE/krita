def626e1138e836ae5b6bee0474697b403ce0ac9
Boudewijn Rempt <boud@valdyas.org>
2020-11-09 11:34:36 +0100

    remove old bits of instrumentation

--- libs/widgets/KisPaletteModel.h
+++ libs/widgets/KisPaletteModel.h
@@ -15,8 +15,6 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
-
 #ifndef KIS_PALETTEMODEL_H
 #define KIS_PALETTEMODEL_H
 

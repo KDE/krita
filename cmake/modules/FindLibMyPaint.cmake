845d78da03c2c764663fc4e8af128a5fc5ac7108
L. E. Segovia <amy@amyspark.me>
2020-11-18 01:35:32 +0000

    Various fixes and improvements to libmypaint
    - Added config-based lookup to @rempt's toolchain
    - Standardized LibMyPaint target prefix
    - Restore support for pkg-config

--- cmake/modules/FindLibMyPaint.cmake
+++ cmake/modules/FindLibMyPaint.cmake
@@ -1,27 +1,47 @@
 #For finding LibMyPaint library in the system
 
+find_package(LibMyPaint ${LibMyPaint_FIND_VERSION} QUIET NO_MODULE)
+
+if(LibMyPaint_FOUND)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibMyPaint
+    CONFIG_MODE
+    REQUIRED_VARS
+        libmypaint_INCLUDE_DIRS
+        libmypaint_LIBRARY_DIRS
+    VERSION_VAR
+        libmypaint_VERSION
+)
+
+else()
+
 include(LibFindMacros)
 libfind_pkg_check_modules(MYPAINT_PKGCONF libmypaint)
 
-find_path(LIBMYPAINT_INCLUDE_DIR
-    NAMES mypaint-config.h
-    /usr/include
-    /usr/local/include
-    /sw/include
-    /opt/local/include
-    ${MYPAINT_PKGCONF_INCLUDE_DIRS}
-    ${MYPAINT_PKGCONF_INCLUDEDIR}
+find_path(LibMyPaint_INCLUDE_DIR
+    NAMES libmypaint/mypaint-config.h
+    HINTS ${MYPAINT_PKGCONF_INCLUDE_DIRS} ${MYPAINT_PKGCONF_INCLUDEDIR}
     PATH_SUFFIXES libmypaint
 )
 
-find_library(LIBMYPAINT_LIBRARY
+find_library(LibMyPaint_LIBRARY
     NAMES libmypaint mypaint
     HINTS ${MYPAINT_PKGCONF_LIBRARY_DIRS} ${MYPAINT_PKGCONF_LIBDIR}
     DOC "Libraries to link against for mypaint brush engine Support"
 )
 
-string(REGEX MATCH "(.*)/libmypaint.so" LIBMYPAINT_LIBRARIES ${LIBMYPAINT_LIBRARY})
+set(LibMyPaint_PROCESS_LIBS LibMyPaint_LIBRARY)
+set(LibMyPaint_PROCESS_LIBS LibMyPaint_INCLUDE_DIR)
+set(LibMyPaint_VERSION ${MYPAINT_PKGCONF_VERSION})
+libfind_process(LibMyPaint)
 
-set(LIBMYPAINT_LIBRARIES ${CMAKE_MATCH_1})
-set(LIBMYPAINT_FOUND ${MYPAINT_PKGCONF_FOUND})
-set(LIBMYPAINT_VERSION ${MYPAINT_PKGCONF_VERSION})
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibMyPaint
+    REQUIRED_VARS
+        LibMyPaint_INCLUDE_DIR
+        LibMyPaint_LIBRARY
+    VERSION_VAR
+        LibMyPaint_VERSION
+)
+endif()

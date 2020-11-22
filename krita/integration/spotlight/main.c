d079ead743733e2b70482935f61f57ce485f52c4
Samuel Gaist <samuel.gaist@kde.org>
2020-11-22 09:18:35 +0100

    Migrate new set of files to SPDX format

--- krita/integration/spotlight/main.c
+++ krita/integration/spotlight/main.c
@@ -3,20 +3,7 @@
  *
  * Copyright (c) 2020 L. E. Segovia <amy@amyspark.me>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
- * USA.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 /* clang-format off */
@@ -61,12 +48,12 @@
 // -----------------------------------------------------------------------------
 
 // The import function to be implemented in GetMetadataForFile.c
-Boolean GetMetadataForFile(void *thisInterface, 
-               CFMutableDictionaryRef attributes, 
+Boolean GetMetadataForFile(void *thisInterface,
+               CFMutableDictionaryRef attributes,
                CFStringRef contentTypeUTI,
                CFStringRef pathToFile);
-               
-// The layout for an instance of MetaDataImporterPlugIn 
+
+// The layout for an instance of MetaDataImporterPlugIn
 typedef struct __MetadataImporterPluginType
 {
     MDImporterInterfaceStruct *conduitInterface;

53ccea4129b224721a1f7da3f7240441dc163d91
Dmitry Kazakov <dimula73@gmail.com>
2020-12-10 22:13:23 +0300

    Implement a GUI option for flipping the "symmetrical" mode for mesh handles
    It looks like different painters expect different default behavior from
    the mesh handles. The new checkbox in the mesh mode options will let them
    choose what behavior should be without any modifier.
    
    # Conflicts:
    #	plugins/tools/tool_transform2/tool_transform_args.cc
    #	plugins/tools/tool_transform2/tool_transform_args.h

--- plugins/tools/tool_transform2/tool_transform_args.h
+++ plugins/tools/tool_transform2/tool_transform_args.h
@@ -304,6 +304,9 @@ public:
     bool meshShowHandles() const;
     void setMeshShowHandles(bool value);
 
+    bool meshSymmetricalHandles() const;
+    void setMeshSymmetricalHandles(bool meshSymmetricalHandles);
+
 private:
     void clear();
     void init(const ToolTransformArgs& args);
@@ -349,6 +352,7 @@ private:
 
     KisBezierTransformMesh m_meshTransform;
     bool m_meshShowHandles = true;
+    bool m_meshSymmetricalHandles = true;
 
     /**
      * When we continue a transformation, m_continuedTransformation

KDE/krita

commit 23a6ab6d871a2c1760f03d1fcf4b8a2b9e7098ee

Fix automatic cropping of semi-transparent patterns in the fill layer.
We needed to use the official bounds, not the one of the paintdevice,
as that crops as much as it can.

libs/image/kis_perspectivetransform_worker.cpp
@@ -152,16 +152,17 @@ void KisPerspectiveTransformWorker::runPartialDst(KisPaintDeviceSP srcDev,
                                                   const QRect &dstRect)
 {
 
-    QRectF srcClipRect = srcDev->exactBounds();
+    QRectF srcClipRect = srcDev->defaultBounds()->imageBorderRect();
     if (srcClipRect.isEmpty()) return;
 
     if (m_isIdentity) {
 
         if (srcDev->defaultBounds()->wrapAroundMode()) {
-            KisProgressUpdateHelper progressHelper(m_progressUpdater, 100, dstRect.height()/ srcClipRect.height());
-            for (int y = dstRect.y(); y < dstRect.y() + dstRect.height(); y+=srcClipRect.height()) {
-                for (int x = dstRect.x(); x < dstRect.x() + dstRect.width(); x+=srcClipRect.width()) {
-                    KisPainter::copyAreaOptimizedOldData(QPoint(x, y), srcDev, dstDev, srcClipRect.toRect());
+            QRect srcRect = srcClipRect.toRect();
+            KisProgressUpdateHelper progressHelper(m_progressUpdater, 100, dstRect.height()/ srcRect.height());
+            for (int y = dstRect.y(); y < dstRect.y() + dstRect.height(); y+= srcRect.height()) {
+                for (int x = dstRect.x(); x < dstRect.x() + dstRect.width(); x+= srcRect.width()) {
+                    KisPainter::copyAreaOptimizedOldData(QPoint(x, y), srcDev, dstDev, srcRect);
                 }
                 progressHelper.step();
             }

commit 36e2a2a9deba50c99fcf70a2da2e4232774ac795

Fix cropping of the source area in transfrom masks after moving the source layer
That was a regression from 23a6ab6d871a2c1760f03d1fcf4b8a2b9e7098ee.

BUG:430199

libs/image/kis_perspectivetransform_worker.cpp
@@ -164,7 +164,7 @@ void KisPerspectiveTransformWorker::runPartialDst(KisPaintDeviceSP srcDev,
     KIS_SAFE_ASSERT_RECOVER_RETURN(srcDev->pixelSize() == dstDev->pixelSize());
     KIS_SAFE_ASSERT_RECOVER_NOOP(*srcDev->colorSpace() == *dstDev->colorSpace());
 
-    QRectF srcClipRect = srcDev->defaultBounds()->imageBorderRect();
+    QRectF srcClipRect = srcDev->exactBounds() | srcDev->defaultBounds()->imageBorderRect();
     if (srcClipRect.isEmpty()) return;
 
     if (m_isIdentity || m_isTranslating) {
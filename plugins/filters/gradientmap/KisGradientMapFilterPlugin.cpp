1fde651951f48f0175b13647cc4c4c1b6ba4cf23
Samuel Gaist <samuel.gaist@kde.org>
2020-12-23 20:40:41 +0100

    Migrate gradientmap copyright information to SPDX format

--- plugins/filters/gradientmap/KisGradientMapFilterPlugin.cpp
+++ plugins/filters/gradientmap/KisGradientMapFilterPlugin.cpp
@@ -1,8 +1,8 @@
 /*
  * This file is part of the KDE project
  *
- * Copyright (c) 2016 Spencer Brown <sbrown655@gmail.com>
- * Copyright (c) 2020 Deif Lou <ginoba@gmail.com>
+ * SPDX-FileCopyrightText: 2016 Spencer Brown <sbrown655@gmail.com>
+ * SPDX-FileCopyrightText: 2020 Deif Lou <ginoba@gmail.com>
  * 
  * SPDX-License-Identifier: GPL-2.0-or-later
  */

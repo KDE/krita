06c722ddc98bae9e476998bea5f6dab71310c569
Agata Cacko <tamtamy.tymona@gmail.com>
2021-01-08 18:55:10 +0100

    Make the warning more helpful on adding the resource
    Before this commit, the warning one would get would be
    hard to parse - it would say that the addition to the database
    failed, and only after a moment one can see that the reason was
    that the id of the resource was -1.
    This commit checks for resource id that equals -1 and prints
    more specific message.
    
    Example of old warning message:
    
    Could not execute initial addResourceVersion statement
    QMap((":location", QVariant(QString, "03_default-paper.pat"))
    (":md5sum", QVariant(QByteArray, "8caab2e65d683d84eed59be386fae118"))
    (":resource_id", QVariant(int, -1))
    (":storage_location", QVariant(QString, "memory"))
    (":timestamp", QVariant(qlonglong, 1610124014))
    (":version", QVariant(int, -1)))
    QSqlError("19", "Nie można pobrać wiersza danych", "UNIQUE constraint
    failed: versioned_resources.resource_id, versioned_resources.storage_id,
    versioned_resources.version")
    
    Example of the new warning message:
    
    Adding to database failed, resource id after adding is  -1 !
    (Probable reason: the same name and MD5 as some other resource).
    Resource is:  "tex9.png" "/home/tymon/.local/share/krita//tex9.png.pat"
    "patterns" "memory"

--- libs/resources/KisResourceCacheDb.cpp
+++ libs/resources/KisResourceCacheDb.cpp
@@ -514,6 +514,12 @@ bool KisResourceCacheDb::addResource(KisResourceStorageSP storage, QDateTime tim
     }
 
     resourceId = resourceIdForResource(resource->name(), resource->filename(), resourceType, KisResourceLocator::instance()->makeStorageLocationRelative(storage->location()));
+    if (resourceId < 0) {
+        qWarning() << "Adding to database failed, resource id after adding is " << resourceId << "! (Probable reason: the same name and MD5 as some other resource). Resource is: " << resource->name() << resource->filename()
+                   << resourceType << KisResourceLocator::instance()->makeStorageLocationRelative(storage->location());
+        return false;
+    }
+
     resource->setResourceId(resourceId);
 
     // Then add a new version

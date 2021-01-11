dcdab9260683561fb2b6d47a4ffc8581015c6de5
Deif Lou <ginoba@gmail.com>
2021-01-11 11:23:21 +0100

    Change the lens blur filter to use KisAngleSelector

--- plugins/filters/blur/kis_wdg_lens_blur.cpp
+++ plugins/filters/blur/kis_wdg_lens_blur.cpp
@@ -38,6 +38,9 @@ KisWdgLensBlur::KisWdgLensBlur(QWidget * parent) : KisConfigWidget(parent)
     m_widget = new Ui_WdgLensBlur();
     m_widget->setupUi(this);
 
+    m_widget->irisRotationSelector->setDecimals(0);
+    m_widget->irisRotationSelector->setIncreasingDirection(KisAngleGauge::IncreasingDirection_Clockwise);
+
     m_shapeTranslations[i18n("Triangle")] = "Triangle";
     m_shapeTranslations[i18n("Quadrilateral (4)")] = "Quadrilateral (4)";
     m_shapeTranslations[i18n("Pentagon (5)")] = "Pentagon (5)";
@@ -47,7 +50,7 @@ KisWdgLensBlur::KisWdgLensBlur(QWidget * parent) : KisConfigWidget(parent)
 
     connect(m_widget->irisShapeCombo, SIGNAL(currentIndexChanged(int)), SIGNAL(sigConfigurationItemChanged()));
     connect(m_widget->irisRadiusSlider, SIGNAL(valueChanged(int)), SIGNAL(sigConfigurationItemChanged()));
-    connect(m_widget->irisRotationSlider, SIGNAL(valueChanged(int)), SIGNAL(sigConfigurationItemChanged()));
+    connect(m_widget->irisRotationSelector, SIGNAL(angleChanged(qreal)), SIGNAL(sigConfigurationItemChanged()));
 }
 
 KisWdgLensBlur::~KisWdgLensBlur()
@@ -60,7 +63,7 @@ KisPropertiesConfigurationSP KisWdgLensBlur::configuration() const
     KisFilterConfigurationSP config = new KisFilterConfiguration("lens blur", 1);
     config->setProperty("irisShape", m_shapeTranslations[m_widget->irisShapeCombo->currentText()]);
     config->setProperty("irisRadius", m_widget->irisRadiusSlider->value());
-    config->setProperty("irisRotation", m_widget->irisRotationSlider->value());
+    config->setProperty("irisRotation", static_cast<int>(m_widget->irisRotationSelector->angle()));
 
     QSize halfSize = KisLensBlurFilter::getKernelHalfSize(config, 0);
     config->setProperty("halfWidth", halfSize.width());
@@ -83,7 +86,7 @@ void KisWdgLensBlur::setConfiguration(const KisPropertiesConfigurationSP config)
         m_widget->irisRadiusSlider->setValue(value.toInt());
     }
     if (config->getProperty("irisRotation", value)) {
-        m_widget->irisRotationSlider->setValue(value.toInt());
+        m_widget->irisRotationSelector->setAngle(static_cast<qreal>(value.toInt()));
     }
 }
 

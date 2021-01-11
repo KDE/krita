351b4bfb1fe01d8f4b7a9e9a690b6551f84a700a
Dmitry Kazakov <dimula73@gmail.com>
2021-01-11 10:11:57 +0300

    Add copyrights to KisBezierMesh class

--- libs/global/KisBezierMesh.h
+++ libs/global/KisBezierMesh.h
@@ -1,3 +1,9 @@
+/*
+ *  SPDX-FileCopyrightText: 2020 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #ifndef KISBEZIERMESH_H
 #define KISBEZIERMESH_H
 

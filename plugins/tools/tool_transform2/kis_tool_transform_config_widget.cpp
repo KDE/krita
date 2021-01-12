8589bedd3c6e6789fff799b3e71649d55f40439e
Dmitry Kazakov <dimula73@gmail.com>
2021-01-12 08:09:32 +0300

    Make sure changes to the mesh done with the widget are undoable
    (this patch is portable to 4.4.2)

--- plugins/tools/tool_transform2/kis_tool_transform_config_widget.cpp
+++ plugins/tools/tool_transform2/kis_tool_transform_config_widget.cpp
@@ -290,6 +290,8 @@ KisToolTransformConfigWidget::KisToolTransformConfigWidget(TransformTransactionP
     connect(chkScaleHandles, SIGNAL(toggled(bool)), this, SLOT(slotMeshScaleHandlesChanged()));
     connect(intNumColumns, SIGNAL(valueChanged(int)), this, SLOT(slotMeshSizeChanged()));
     connect(intNumRows, SIGNAL(valueChanged(int)), this, SLOT(slotMeshSizeChanged()));
+    connect(intNumColumns, SIGNAL(editingFinished()), this, SLOT(notifyEditingFinished()));
+    connect(intNumRows, SIGNAL(editingFinished()), this, SLOT(notifyEditingFinished()));
 
     // Mode switch buttons
     connect(freeTransformButton, SIGNAL(clicked(bool)), this, SLOT(slotSetFreeTransformModeButtonClicked(bool)));
@@ -1293,6 +1295,7 @@ void KisToolTransformConfigWidget::slotMeshShowHandlesChanged()
     ToolTransformArgs *config = m_transaction->currentConfig();
     config->setMeshShowHandles(this->chkShowControlPoints->isChecked());
     notifyConfigChanged();
+    notifyEditingFinished();
 }
 
 void KisToolTransformConfigWidget::slotMeshSymmetricalHandlesChanged()
@@ -1301,6 +1304,7 @@ void KisToolTransformConfigWidget::slotMeshSymmetricalHandlesChanged()
     ToolTransformArgs *config = m_transaction->currentConfig();
     config->setMeshSymmetricalHandles(this->chkSymmetricalHandles->isChecked());
     notifyConfigChanged();
+    notifyEditingFinished();
 }
 
 void KisToolTransformConfigWidget::slotMeshScaleHandlesChanged()

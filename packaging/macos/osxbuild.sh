55d77c5689c45d655a60d2876efe539aa8b96943
Ivan Yossi <ghevan@gmail.com>
2021-02-09 18:59:45 -0600

    macos: avoid sending commands as args for setting dir

--- packaging/macos/osxbuild.sh
+++ packaging/macos/osxbuild.sh
@@ -693,7 +693,7 @@ script_run() {
         fix_boost_rpath
 
     elif [[ ${1} = "build" ]]; then
-        OSXBUILD_DIR=$(get_directory_fromargs "${@}")
+        OSXBUILD_DIR=$(get_directory_fromargs "${@:2}")
 
         build_krita "${OSXBUILD_DIR}"
         exit
@@ -738,7 +738,7 @@ osxbuild.sh install ${KIS_BUILD_DIR}"
         exit
 
     elif [[ ${1} = "install" ]]; then
-        OSXBUILD_DIR=$(get_directory_fromargs "${@}")
+        OSXBUILD_DIR=$(get_directory_fromargs "${@:2}")
 
         install_krita "${OSXBUILD_DIR}"
         build_plugins "${OSXBUILD_DIR}"
@@ -746,7 +746,7 @@ osxbuild.sh install ${KIS_BUILD_DIR}"
         fix_boost_rpath
 
     elif [[ ${1} = "buildinstall" ]]; then
-        OSXBUILD_DIR=$(get_directory_fromargs "${@}")
+        OSXBUILD_DIR=$(get_directory_fromargs "${@:2}")
 
         build_krita "${OSXBUILD_DIR}"
         install_krita "${OSXBUILD_DIR}"

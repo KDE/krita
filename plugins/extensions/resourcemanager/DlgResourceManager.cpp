1cc459c20fee4d7712f14f6b58f0be6b3438467b
Agata Cacko <tamtamy.tymona@gmail.com>
2021-02-11 21:21:39 +0100

    Implement basic interactions in Resource Manager
    Before this commit, the Resource Manager was just a skeleton
    with widgets that didn't do anything.
    This commit introduces basic interactions like:
    - all comboboxes show proper values (except that the storages combobox
    still doesn't show "All" entry, because it doesn't come with the model)
    - the tag combobox reacts to the resource type changes
    - the viewmodel of the resources shows only the specific resources
    according to the comboboxes
    - standard multiple selection mode is used (ExtendedSelection, cltr to
    select more than one resource)
    - thumbnail, location, filename and name are taken from the resource
    and all the labels are disabled if there are multiple resources selected

--- plugins/extensions/resourcemanager/DlgResourceManager.cpp
+++ plugins/extensions/resourcemanager/DlgResourceManager.cpp
@@ -9,6 +9,14 @@
 #include "ui_WdgDlgResourceManager.h"
 
 #include <KisResourceTypeModel.h>
+#include <KisStorageModel.h>
+#include <KisTagModel.h>
+#include <KisResourceModel.h>
+#include <KisTagFilterResourceProxyModel.h>
+#include <kis_assert.h>
+#include <KisResourceItemDelegate.h>
+#include <QItemSelection>
+
 
 DlgResourceManager::DlgResourceManager(QWidget *parent)
     : KoDialog(parent)
@@ -28,10 +36,43 @@ DlgResourceManager::DlgResourceManager(QWidget *parent)
     m_ui->cmbResourceType->setModelColumn(KisResourceTypeModel::Name);
     connect(m_ui->cmbResourceType, SIGNAL(activated(int)), SLOT(slotResourceTypeSelected(int)));
 
+    m_storageModel = new KisStorageModel(this);
+
+    m_ui->cmbStorage->setModel(m_storageModel);
+    m_ui->cmbStorage->setModelColumn(KisStorageModel::DisplayName);
+    connect(m_ui->cmbStorage, SIGNAL(activated(int)), SLOT(slotStorageSelected(int)));
+
+    QString selectedResourceType = getCurrentResourceType();
+
+    m_tagModel = new KisTagModel(selectedResourceType);
+    m_tagModelsForResourceType.insert(selectedResourceType, m_tagModel);
+
+    m_ui->cmbTag->setModel(m_tagModel);
+    m_ui->cmbTag->setModelColumn(KisAllTagsModel::Name);
+    connect(m_ui->cmbTag, SIGNAL(activated(int)), SLOT(slotTagSelected(int)));
+
+    KisResourceModel* resourceModel = new KisResourceModel(selectedResourceType);
+    resourceModel->setStorageFilter(KisResourceModel::ShowAllStorages);
+    resourceModel->setResourceFilter(m_ui->chkShowDeleted->isChecked() ? KisResourceModel::ShowAllResources : KisResourceModel::ShowActiveResources);
+    m_resourceModelsForResourceType.insert(selectedResourceType, resourceModel);
+
+    KisTagFilterResourceProxyModel* proxyModel = new KisTagFilterResourceProxyModel(selectedResourceType);
+    proxyModel->setResourceModel(resourceModel);
+    proxyModel->setTagFilter(0);
+    proxyModel->setStorageFilter(true, getCurrentStorageId());
+    m_resourceProxyModelsForResourceType.insert(selectedResourceType, proxyModel);
+
+    m_ui->resourceItemView->setModel(proxyModel);
+    m_ui->resourceItemView->setItemDelegate(new KisResourceItemDelegate(this));
+    m_ui->resourceItemView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+
+    connect(m_ui->resourceItemView, SIGNAL(currentResourceChanged(QModelIndex)), SLOT(slotResourcesSelectionChanged(QModelIndex)));
+
     connect(m_ui->btnImportBundle, SIGNAL(clicked(bool)), SLOT(slotImportBundle()));
     connect(m_ui->btnOpenResourceFolder, SIGNAL(clicked(bool)), SLOT(slotOpenResourceFolder()));
     connect(m_ui->btnImportResources, SIGNAL(clicked(bool)), SLOT(slotImportResources()));
     connect(m_ui->btnDeleteBackupFiles, SIGNAL(clicked(bool)), SLOT(slotDeleteBackupFiles()));
+
 }
 
 DlgResourceManager::~DlgResourceManager()
@@ -41,7 +82,95 @@ DlgResourceManager::~DlgResourceManager()
 
 void DlgResourceManager::slotResourceTypeSelected(int)
 {
-    // TODO: handle resource type selection change
+    QString selectedResourceType = getCurrentResourceType();
+    if (!m_tagModelsForResourceType.contains(selectedResourceType)) {
+        m_tagModelsForResourceType.insert(selectedResourceType, new KisTagModel(selectedResourceType));
+    }
+
+    m_ui->cmbTag->setModel(m_tagModelsForResourceType[selectedResourceType]);
+
+    if (!m_resourceProxyModelsForResourceType.contains(selectedResourceType)) {
+        KisResourceModel* resourceModel;
+        if (!m_resourceModelsForResourceType.contains(selectedResourceType)) {
+            resourceModel = new KisResourceModel(selectedResourceType);
+            KIS_SAFE_ASSERT_RECOVER_RETURN(resourceModel);
+            resourceModel->setStorageFilter(KisResourceModel::ShowAllStorages);
+            resourceModel->setResourceFilter(m_ui->chkShowDeleted->isChecked() ? KisResourceModel::ShowAllResources : KisResourceModel::ShowActiveResources);
+
+            m_resourceModelsForResourceType.insert(selectedResourceType, resourceModel);
+        } else {
+            resourceModel = m_resourceModelsForResourceType[selectedResourceType];
+        }
+
+        KisTagFilterResourceProxyModel* proxyModel = new KisTagFilterResourceProxyModel(selectedResourceType);
+        KIS_SAFE_ASSERT_RECOVER_RETURN(proxyModel);
+        proxyModel->setResourceModel(resourceModel);
+        proxyModel->setTagFilter(getCurrentTag());
+        proxyModel->setStorageFilter(true, getCurrentStorageId());
+        m_resourceProxyModelsForResourceType.insert(selectedResourceType, proxyModel);
+    }
+    m_ui->resourceItemView->setModel(m_resourceProxyModelsForResourceType[selectedResourceType]);
+}
+
+void DlgResourceManager::slotStorageSelected(int)
+{
+    if (!m_resourceProxyModelsForResourceType.contains(getCurrentResourceType())) {
+        return;
+    }
+    m_resourceProxyModelsForResourceType[getCurrentResourceType()]->setStorageFilter(true, getCurrentStorageId());
+}
+
+void DlgResourceManager::slotTagSelected(int)
+{
+    if (!m_resourceProxyModelsForResourceType.contains(getCurrentResourceType())) {
+        return;
+    }
+    m_resourceProxyModelsForResourceType[getCurrentResourceType()]->setTagFilter(getCurrentTag());
+}
+
+void DlgResourceManager::slotResourcesSelectionChanged(QModelIndex index)
+{
+    QModelIndexList list = m_ui->resourceItemView->selectionModel()->selection().indexes();
+    KisTagFilterResourceProxyModel* model = m_resourceProxyModelsForResourceType[getCurrentResourceType()];
+    if (list.size() == 1) {
+        QModelIndex idx = list[0];
+        m_ui->lblFilename->setText(model->data(idx, Qt::UserRole + KisAllResourcesModel::Filename).toString());
+        m_ui->lneName->setText(model->data(idx, Qt::UserRole + KisAllResourcesModel::Name).toString());
+        m_ui->lblLocation->setText(model->data(idx, Qt::UserRole + KisAllResourcesModel::Location).toString());
+        m_ui->lblId->setText(model->data(idx, Qt::UserRole + KisAllResourcesModel::Id).toString());
+
+        QImage thumb = model->data(idx, Qt::UserRole + KisAllResourcesModel::Thumbnail).value<QImage>();
+        thumb.setDevicePixelRatio(this->devicePixelRatioF());
+        qCritical() << thumb.size() << "and should be " << m_ui->lblThumbnail->size();
+        QPixmap pix = QPixmap::fromImage(thumb);
+        pix = pix.scaled(m_ui->lblThumbnail->size()*devicePixelRatioF(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        m_ui->lblThumbnail->setPixmap(pix);
+
+        m_ui->lblFilename->setDisabled(false);
+        m_ui->lblLocation->setDisabled(false);
+        m_ui->lblThumbnail->setDisabled(false);
+        m_ui->lneName->setDisabled(false);
+        m_ui->lblId->setDisabled(false);
+
+    } else {
+        QString multipleSelectedText = i18nc("In Resource manager, this is text shown instead of filename, name or location, "
+                                             "when multiple resources are shown so there is no one specific filename", "(Multiple selected)");
+        m_ui->lblFilename->setText(multipleSelectedText);
+        m_ui->lblLocation->setText(multipleSelectedText);
+        m_ui->lneName->setText(multipleSelectedText);
+        m_ui->lblThumbnail->setText(multipleSelectedText);
+        QPixmap pix;
+        m_ui->lblThumbnail->setPixmap(pix);
+
+        m_ui->lblFilename->setDisabled(true);
+        m_ui->lblLocation->setDisabled(true);
+        m_ui->lblThumbnail->setDisabled(true);
+        m_ui->lneName->setDisabled(true);
+        m_ui->lblId->setDisabled(false);
+    }
+    Q_FOREACH(QModelIndex index, list) {
+
+    }
 }
 
 void DlgResourceManager::slotImportResources()
@@ -62,4 +191,19 @@ void DlgResourceManager::slotImportBundle()
 void DlgResourceManager::slotDeleteBackupFiles()
 {
 
+}
+
+QString DlgResourceManager::getCurrentResourceType()
+{
+    return m_ui->cmbResourceType->currentData(Qt::UserRole + KisResourceTypeModel::ResourceType).toString();
+}
+
+int DlgResourceManager::getCurrentStorageId()
+{
+    return m_ui->cmbStorage->currentData(Qt::UserRole + KisStorageModel::Id).toInt();
+}
+
+QSharedPointer<KisTag> DlgResourceManager::getCurrentTag()
+{
+    return m_ui->cmbTag->currentData(Qt::UserRole + KisAllTagsModel::KisTagRole).value<KisTagSP>();
 }
--- plugins/extensions/resourcemanager/DlgResourceManager.h
+++ plugins/extensions/resourcemanager/DlgResourceManager.h
@@ -9,8 +9,16 @@
 
 #include <KoDialog.h>
 #include <QScopedPointer>
+#include <QSortFilterProxyModel>
+#include <QSharedPointer>
+#include <QItemSelection>
 
 class KisResourceTypeModel;
+class KisStorageModel;
+class KisTagModel;
+class KisResourceModel;
+class KisTagFilterResourceProxyModel;
+class KisTag;
 
 namespace Ui
 {
@@ -24,16 +32,38 @@ public:
     DlgResourceManager(QWidget *parent = 0);
     ~DlgResourceManager() override;
 
+
+
+
 private Q_SLOTS:
     void slotResourceTypeSelected(int);
+    void slotStorageSelected(int);
+    void slotTagSelected(int);
+
+    void slotResourcesSelectionChanged(QModelIndex selected);
+
     void slotImportResources();
     void slotOpenResourceFolder();
     void slotImportBundle();
     void slotDeleteBackupFiles();
 private:
+    QString getCurrentResourceType();
+    int getCurrentStorageId();
+    QSharedPointer<KisTag> getCurrentTag();
+
+
+private:
     QWidget *m_page;
     QScopedPointer<Ui::WdgDlgResourceManager> m_ui;
     KisResourceTypeModel *m_resourceTypeModel {0};
+    KisStorageModel *m_storageModel {0};
+    KisTagModel *m_tagModel {0};
+    QMap<QString, KisTagModel*> m_tagModelsForResourceType;
+
+    KisResourceModel *m_resourceModel;
+    QMap<QString, KisResourceModel*> m_resourceModelsForResourceType;
+    QMap<QString, KisTagFilterResourceProxyModel*> m_resourceProxyModelsForResourceType;
+
 };
 
 #endif // DLGRESOURCEMANAGER_H

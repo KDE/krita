90dd89e6774ef11f517c1b61c1aab30b794404b8
Halla Rempt <halla@valdyas.org>
2021-02-21 13:20:05 +0100

    Make string translatable
    BUG:433368
    (cherry picked from commit 2795d051ee0ecea6b4cf7fcacc512a7bbbb9eed7)

--- libs/ui/dialogs/kis_about_application.cpp
+++ libs/ui/dialogs/kis_about_application.cpp
@@ -250,10 +250,10 @@ QWidget *KisAboutApplication::createTranslatorsWidget(const QList<KAboutPerson> 
     }
 
     translatorHtml.append("<ul></p>");
-    translatorHtml.append("<p>KDE is translated into many languages thanks to the work of the "
+    translatorHtml.append(i18n("<p>KDE is translated into many languages thanks to the work of the "
                           "translation teams all over the world.</p><p>For more information on KDE "
                           "internationalization visit <a href=\"http://l10n.kde.org\">http://l10n."
-                          "kde.org</a></p>");
+                          "kde.org</a></p>"));
     translatorHtml.append("</body></html>");
 
     lblTranslators->setText(translatorHtml);

9d29c36b060a20f9f2e5de5090cb966415611f53
Halla Rempt <halla@valdyas.org>
2021-02-23 15:43:04 +0100

    Only open the bug dialog when krita is in alpha or beta
    Otherwise show the long and intimidating Reporting Bugs page
    (see https://invent.kde.org/documentation/docs-krita-org/-/merge_requests/205)
    so people make fewer nonsense reports.
    
    (cherry picked from commit 16c0924ca3595c854641f19e8837b30fb825e521)

--- libs/widgetutils/xmlgui/khelpmenu.cpp
+++ libs/widgetutils/xmlgui/khelpmenu.cpp
@@ -35,6 +35,8 @@
 #include <klocalizedstring.h>
 #include <kstandardaction.h>
 
+#include <kritaversion.h>
+
 using namespace KDEPrivate;
 
 class KHelpMenuPrivate
@@ -239,11 +241,15 @@ void KHelpMenu::aboutKDE()
 
 void KHelpMenu::reportBug()
 {
+#ifdef KRITA_STABLE
+    QDesktopServices::openUrl(QUrl("https://docs.krita.org/en/untranslatable_pages/reporting_bugs.html"));
+#else
     if (!d->mBugReport) {
         d->mBugReport = new KBugReport(d->mAboutData, d->mParent);
         connect(d->mBugReport, SIGNAL(finished(int)), this, SLOT(dialogFinished()));
     }
     d->mBugReport->show();
+#endif
 }
 
 void KHelpMenu::switchApplicationLanguage()

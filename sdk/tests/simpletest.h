f75f1750b533e712b6d892953fec4927c19f997c
Halla Rempt <halla@valdyas.org>
2021-03-02 17:23:57 +0100

    Try to fix build with Qt < 5.15

--- sdk/tests/simpletest.h
+++ sdk/tests/simpletest.h
@@ -3,12 +3,22 @@
 
 #include <QtTest>
 #include <QTest>
+#include <QStandardPaths>
+
+#define SIMPLE_MAIN_IMPL(TestObject) \
+    QT_PREPEND_NAMESPACE(QTest::Internal::callInitMain)<TestObject>(); \
+    QApplication app(argc, argv); \
+    app.setAttribute(Qt::AA_Use96Dpi, true); \
+    QTEST_DISABLE_KEYPAD_NAVIGATION \
+    TestObject tc; \
+    QTEST_SET_MAIN_SOURCE_PATH \
+    return QTest::qExec(&tc, argc, argv);
 
 #define SIMPLE_TEST_MAIN(TestObject) \
 int main(int argc, char *argv[]) \
 { \
     QStandardPaths::setTestModeEnabled(true); \
-    QTEST_MAIN_IMPL(TestObject) \
+    SIMPLE_MAIN_IMPL(TestObject) \
 }
 
 #endif // SIMPLETEST_H

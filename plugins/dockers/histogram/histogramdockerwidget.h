c2e8b767f5be400812d6bcd8dbae2676c73fd5ff
Agata Cacko <tamtamy.tymona@gmail.com>
2021-03-12 15:12:08 +0100

    Change typedef to using in HistogramDockerWidget

--- plugins/dockers/histogram/histogramdockerwidget.h
+++ plugins/dockers/histogram/histogramdockerwidget.h
@@ -20,7 +20,7 @@ class KisCanvas2;
 class KoColorSpace;
 
 
-typedef std::vector<std::vector<quint32> > HistVector; //Don't use QVector here - it's too slow for this purpose
+using HistVector = std::vector<std::vector<quint32> >; //Don't use QVector here - it's too slow for this purpose
 
 struct HistogramData
 {
@@ -30,7 +30,6 @@ struct HistogramData
     HistVector bins;
     const KoColorSpace* colorSpace;
 };
-typedef QSharedPointer<HistogramData> HistogramDataSP;
 Q_DECLARE_METATYPE(HistogramData)
 
 

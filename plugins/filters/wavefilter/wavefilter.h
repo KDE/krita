335fbba6dd217e09ef901c7eb573f511dcc02e7f
Ivan Yossi <ghevan@gmail.com>
2021-03-19 15:38:31 -0600

    Reverse direction in wavefilter
    BUG:409564

--- plugins/filters/wavefilter/wavefilter.h
+++ plugins/filters/wavefilter/wavefilter.h
@@ -41,7 +41,7 @@ public:
 
     KisFilterConfigurationSP defaultConfiguration(KisResourcesInterfaceSP resourcesInterface) const override;
 public:
-    QRect neededRect(const QRect& rect, const KisFilterConfigurationSP config = 0, int lod = 0) const override;
+    QRect changedRect(const QRect& rect, const KisFilterConfigurationSP config = 0, int lod = 0) const override;
 
     KisConfigWidget * createConfigurationWidget(QWidget* parent, const KisPaintDeviceSP dev, bool useForMasks) const override;
 };

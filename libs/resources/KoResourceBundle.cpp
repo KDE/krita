52eca1f618491f02b0716cc0bc9a03a575978ed7
Halla Rempt <halla@valdyas.org>
2021-03-30 12:19:19 +0200

    Fix saving metadata
    KoXmlWriter is highly dangerous: if you pass a temporary
    QByteArray created from a QString in the argument of a method,
    the char* of the QByteArray will be deleted after the call to that
    method has ended. Then the char* stored in KoXmlWriter to be
    used as the end tag becomes invalid, and, in this case, likely to
    be overwritten by the next QByteArray that get created from a
    QString.

--- libs/resources/KoResourceBundle.cpp
+++ libs/resources/KoResourceBundle.cpp
@@ -272,8 +272,10 @@ void KoResourceBundle::setThumbnail(QString filename)
 void KoResourceBundle::writeMeta(const QString &metaTag, KoXmlWriter *writer)
 {
     if (m_metadata.contains(metaTag)) {
-        writer->startElement(metaTag.toUtf8());
-        writer->addTextNode(m_metadata[metaTag].toUtf8());
+        QByteArray mt = metaTag.toUtf8();
+        QByteArray tx = m_metadata[metaTag].toUtf8();
+        writer->startElement(mt);
+        writer->addTextNode(tx);
         writer->endElement();
     }
 }
@@ -348,8 +350,10 @@ void KoResourceBundle::saveMetadata(QScopedPointer<KoStore> &store)
 
     writeMeta(KisResourceStorage::s_meta_generator, &metaWriter);
 
-    metaWriter.startElement(KisResourceStorage::s_meta_version.toUtf8());
-    metaWriter.addTextNode(m_bundleVersion.toUtf8());
+    QByteArray ba1 = KisResourceStorage::s_meta_version.toUtf8();
+    metaWriter.startElement(ba1);
+    QByteArray ba2  = m_bundleVersion.toUtf8();
+    metaWriter.addTextNode(ba2);
     metaWriter.endElement();
 
     writeMeta(KisResourceStorage::s_meta_author, &metaWriter);
@@ -370,9 +374,12 @@ void KoResourceBundle::saveMetadata(QScopedPointer<KoStore> &store)
 
 
     Q_FOREACH (const QString &tag, m_bundletags) {
-        metaWriter.startElement(KisResourceStorage::s_meta_user_defined.toUtf8());
-        metaWriter.addAttribute(KisResourceStorage::s_meta_name.toUtf8(), "tag");
-        metaWriter.addAttribute(KisResourceStorage::s_meta_value.toUtf8(), tag);
+        QByteArray ba1 = KisResourceStorage::s_meta_user_defined.toUtf8();
+        QByteArray ba2 = KisResourceStorage::s_meta_name.toUtf8();
+        QByteArray ba3 = KisResourceStorage::s_meta_value.toUtf8();
+        metaWriter.startElement(ba1);
+        metaWriter.addAttribute(ba2, "tag");
+        metaWriter.addAttribute(ba3, tag);
         metaWriter.endElement();
     }
 

KDE/krita

commit 8a7bae681a1f9925050196d7e058a0686144502c

fix loading tab bar style on startup
fix loading the QTabBar icon and QHeaderView spacing on startup

the tab bar icon and headerview spacing were not loaded anymore
unless manually changing the theme, so moving that in a dedicated function
and use it on startup.

libs/ui/KisMainWindow.cpp
@@ -838,7 +838,13 @@ void KisMainWindow::slotThemeChanged()
         }
     }
 
+    customizeTabBar();
 
+    emit themeChanged();
+}
+
+void KisMainWindow::customizeTabBar()
+{
     // update MDI area theme
     // Tab close button override
     // just switch this icon out for all OSs so it is easier to see
@@ -855,16 +861,14 @@ void KisMainWindow::slotThemeChanged()
            )";
 
     if (KisIconUtils::useDarkIcons()) {
-        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/pics/dark_close-tab.svg");
+        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/dark_close-tab.svg");
     }
     else {
-        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/pics/light_close-tab.svg");
+        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/light_close-tab.svg");
 
     }
 
     qApp->setStyleSheet(globalStyleSheet);
-
-    emit themeChanged();
 }
 
 bool KisMainWindow::canvasDetached() const
@@ -2879,6 +2883,9 @@ void KisMainWindow::createActions()
     connect(d->themeManager, SIGNAL(signalThemeChanged()), this, SLOT(slotThemeChanged()), Qt::QueuedConnection);
     connect(d->themeManager, SIGNAL(signalThemeChanged()), d->welcomePage, SLOT(slotUpdateThemeColors()), Qt::UniqueConnection);
     d->toggleDockers = actionManager->createAction("view_toggledockers");
+
+    //load customized tab bar style
+    customizeTabBar();
 
     KisConfig(true).showDockers(true);
     d->toggleDockers->setChecked(true);

krita/krita.qrc
@@ -9,8 +9,4 @@
     <qresource prefix="/kxmlgui5">
         <file alias="krita5.xmlgui">krita5.xmlgui</file>
     </qresource>
-    <qresource prefix="/">
-        <file>pics/dark_close-tab.svg</file>
-        <file>pics/light_close-tab.svg</file>
-    </qresource>
 </RCC>

krita/pics/svg/svg-icons.qrc
@@ -31,6 +31,7 @@
         <file>dark_convert-to-transparencyMask.svg</file>
         <file>dark_chain-broken-icon.svg</file>
         <file>dark_chain-icon.svg</file>
+        <file>dark_close-tab.svg</file>
         <file>dark_deletekeyframe.svg</file>
         <file>dark_dialog-cancel.svg</file>
         <file>dark_document-edit.svg</file>
@@ -129,6 +130,7 @@
         <file>light_convert-to-transparencyMask.svg</file>
         <file>light_chain-broken-icon.svg</file>
         <file>light_chain-icon.svg</file>
+        <file>light_close-tab.svg</file>
         <file>light_deletekeyframe.svg</file>
         <file>light_dialog-cancel.svg</file>
         <file>light_document-edit.svg</file>

libs/ui/KisMainWindow.h
@@ -504,6 +504,8 @@ private:
 
     QByteArray borrowWorkspace(KisMainWindow *borrower);
 
+    void customizeTabBar();
+
 
 private:
 

commit e7bd37c699944a78cde620b52118d254a254b708

Fix crash on exit by setting styleSheet only on QTabBar
Before this commit, Krita would crash on exit on Qt 5.12.8
with segfault in the destructor for the Python plugin
widgets. It turned out that this crash was introduced in
8a7bae681a1f992, and the reason turned out to be
a method setStyleSheet() with non-empty string.
Setting empty stylesheet later (on closing) didn't make a difference.
However setting the stylesheet on just the widget
that needs it (QTabBar that keeps the document tabs)
fixes/prevents the crash.

Alternative fix would be to find the widget for the Python plugin
and explicitely set empty stylesheet after every non-empty
stylesheet set for the whole application. It wasn't tested,
but in case more widgets need non-empty stylesheet, it sounds
like it should work.

BUG:427137
BUG:420612

libs/ui/KisMainWindow.cpp
@@ -627,6 +627,8 @@ KisMainWindow::KisMainWindow(QUuid uuid)
     QTabBar *tabBar = d->findTabBarHACK();
     if (tabBar) {
         tabBar->setElideMode(Qt::ElideRight);
+        // load customized tab style
+        customizeTabBar();
     }
 
 }
@@ -849,7 +851,7 @@ void KisMainWindow::customizeTabBar()
     // update MDI area theme
     // Tab close button override
     // just switch this icon out for all OSs so it is easier to see
-    QString globalStyleSheet = R"(
+    QString tabStyleSheet = R"(
             QTabBar::close-button {
                 image: url({close-button-location});
                 padding-top: 3px;
@@ -862,14 +864,18 @@ void KisMainWindow::customizeTabBar()
            )";
 
     if (KisIconUtils::useDarkIcons()) {
-        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/dark_close-tab.svg");
+        tabStyleSheet = tabStyleSheet.replace("{close-button-location}", ":/dark_close-tab.svg");
     }
     else {
-        globalStyleSheet = globalStyleSheet.replace("{close-button-location}", ":/light_close-tab.svg");
+        tabStyleSheet = tabStyleSheet.replace("{close-button-location}", ":/light_close-tab.svg");
 
     }
 
-    qApp->setStyleSheet(globalStyleSheet);
+    QTabBar* tabBar = d->findTabBarHACK();
+    if (tabBar) {
+        tabBar->setStyleSheet(tabStyleSheet);
+    }
+
 }
 
 bool KisMainWindow::canvasDetached() const
@@ -2885,8 +2891,6 @@ void KisMainWindow::createActions()
     connect(d->themeManager, SIGNAL(signalThemeChanged()), d->welcomePage, SLOT(slotUpdateThemeColors()), Qt::UniqueConnection);
     d->toggleDockers = actionManager->createAction("view_toggledockers");
 
-    //load customized tab bar style
-    customizeTabBar();
 
     KisConfig(true).showDockers(true);
     d->toggleDockers->setChecked(true);
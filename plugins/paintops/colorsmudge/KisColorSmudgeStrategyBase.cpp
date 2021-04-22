0901ad0899d657f81f7929369488f43adc5637b9
Dmitry Kazakov <dimula73@gmail.com>
2021-04-22 13:33:04 +0300

    Add copyright for Peter Schatz for the new colorsmudge algorithm
    The original proof-of-concept of the new blending algorithm was
    implemented by Peter Schatz. The strategy-based implementation
    uses the same coefficients as that POC.
    
    Lightness mode's algorithm has been changed significantly though.

--- plugins/paintops/colorsmudge/KisColorSmudgeStrategyBase.cpp
+++ plugins/paintops/colorsmudge/KisColorSmudgeStrategyBase.cpp
@@ -1,4 +1,5 @@
 /*
+ *  SPDX-FileCopyrightText: 2020 Peter Schatz <voronwe13@gmail.com>
  *  SPDX-FileCopyrightText: 2021 Dmitry Kazakov <dimula73@gmail.com>
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later

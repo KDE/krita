KDE/krita

commit 3acd2425ebf851c476d9e40364fa4070fb05b316

Fix uninitializzed values in various classes
CID 329508
CID 329504
CID 329503
CID 329502
CID 329501

libs/image/kis_warptransform_worker.h
@@ -76,12 +76,14 @@ private:
 
 private:
     WarpMathFunction m_warpMathFunction;
-    WarpCalculation m_warpCalc;
+    WarpCalculation m_warpCalc {GRID};
     QVector<QPointF> m_origPoint;
     QVector<QPointF> m_transfPoint;
-    qreal m_alpha;
-    KisPaintDeviceSP m_dev;
+    
     KoUpdater *m_progress;
+    qreal m_alpha {1.0};
+    KisPaintDeviceSP m_dev;
+    KoUpdater *m_progress {0};
 };
 
 #endif

plugins/dockers/advancedcolorselector/kis_color_selector_ring.cpp
@@ -28,11 +28,7 @@
 
 
 KisColorSelectorRing::KisColorSelectorRing(KisColorSelector *parent) :
-    KisColorSelectorComponent(parent),
-    m_cachedColorSpace(0),
-    m_cachedSize(0),
-    m_lastHue(0),
-    m_innerRingRadiusFraction(0.85)
+    KisColorSelectorComponent(parent)
 {
 }
 

plugins/dockers/advancedcolorselector/kis_color_selector_ring.h
@@ -42,16 +42,16 @@ private:
     int outerRadius() const;
 
     QImage m_pixelCache;
-    const KoColorSpace* m_cachedColorSpace;
-    int m_cachedSize;
-    qreal m_lastHue;
+    const KoColorSpace* m_cachedColorSpace {0};
+    int m_cachedSize {0};
+    qreal m_lastHue {0.0};
     QList<QRgb> m_cachedColors;
-    qreal m_innerRingRadiusFraction;
+    qreal m_innerRingRadiusFraction {0.85};
 
-    qreal R;
-    qreal G;
-    qreal B;
-    qreal Gamma;
+    qreal R {0.0};
+    qreal G {0.0};
+    qreal B {0.0};
+    qreal Gamma {1.0};
 };
 
 #endif // KIS_COLOR_SELECTOR_RING_H

plugins/tools/defaulttool/defaulttool/KoShapeMeshGradientHandles.h
@@ -54,11 +54,12 @@ public:
             return SvgMeshPosition {row, col, segmentType};
         }
 
-        Type type;
+        Type type {None};
         QPointF pos;
-        int row, col;
-        SvgMeshPatch::Type segmentType;
-        Index index; // first or the second bezier handle
+        int row {0};
+        int col {0};
+        SvgMeshPatch::Type segmentType {SvgMeshPatch::Top};
+        Index index { First }; // first or the second bezier handle
     };
 
 public:
@@ -93,8 +94,8 @@ private:
     QTransform abosoluteTransformation(KoFlake::CoordinateSystem system) const;
 
 private:
-    KoFlake::FillVariant m_fillVariant;
-    KoShape *m_shape;
+    KoFlake::FillVariant m_fillVariant {KoFlake::Fill};
+    KoShape *m_shape {0};
 };
 
 #endif // __KOSHAPEMESHGRADIENTHANDLES_H_

commit c68f37ae32919ed5bfeea2ecdc7ebfdb1a1a6cca

Fix build
Commit 3acd2425eb introduced a build failure because of
broken cherry-pick (due to a conflict). This commit fixes it
by removing an unnecessary variable definition.

libs/image/kis_warptransform_worker.h
@@ -80,7 +80,6 @@ private:
     QVector<QPointF> m_origPoint;
     QVector<QPointF> m_transfPoint;
     
-    KoUpdater *m_progress;
     qreal m_alpha {1.0};
     KisPaintDeviceSP m_dev;
     KoUpdater *m_progress {0};
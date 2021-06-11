87d6f75406557cd69d654695fdd30154d7823770
Sachin Jindal <jindalsachin1@gmail.com>
2021-06-12 03:16:20 +0530

    Add decoration of crop tool in ToolReferenceImage
    This create a new kisreferenceimagecropdecorator file which can
    be invoked from ToolReferenceImage paint() to add decoration .

---
+++ plugins/tools/defaulttool/referenceimagestool/kisreferenceimagecropdecorator.cpp
@@ -0,0 +1,6 @@
+#include "kisreferenceimagecropdecorator.h"
+
+KisReferenceImageCropDecorator::KisReferenceImageCropDecorator()
+{
+
+}
---
+++ plugins/tools/defaulttool/referenceimagestool/kisreferenceimagecropdecorator.h
@@ -0,0 +1,11 @@
+#ifndef KISREFERENCEIMAGECROPDECORATOR_H
+#define KISREFERENCEIMAGECROPDECORATOR_H
+
+
+class KisReferenceImageCropDecorator
+{
+public:
+    KisReferenceImageCropDecorator();
+};
+
+#endif // KISREFERENCEIMAGECROPDECORATOR_H

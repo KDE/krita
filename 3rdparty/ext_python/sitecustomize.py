38fa2e42c4f1693b6770f12ea3f53ede66dbd164
L. E. Segovia <amy@amyspark.me>
2021-06-17 14:04:45 +0000

    Windows/macOS: simplify sitecustomize.py
    The workaround in pypa/setuptools#2165 should also apply to macOS.

--- 3rdparty/ext_python/sitecustomize.py
+++ 3rdparty/ext_python/sitecustomize.py
@@ -2,14 +2,10 @@ import os
 import site
 import sysconfig
 
-# https://github.com/pypa/setuptools/issues/2165
-
-if os.name == "nt":
-    import pathlib
-    framework_path = pathlib.Path(__file__).parent.parent
-else:
-    framework_path = os.path.dirname(os.path.abspath(__file__))
+framework_path = os.path.dirname(os.path.abspath(__file__))
 data_path = sysconfig.get_paths()['data']
+# https://github.com/pypa/setuptools/issues/2165 (windows)
+site.addsitedir(framework_path)
 site.addsitedir(os.path.join(data_path, 'site-packages'))
 site.addsitedir(os.path.join(framework_path,'site-packages'))
 site.addsitedir(os.path.join(framework_path,'site-packages', 'PyQt5'))

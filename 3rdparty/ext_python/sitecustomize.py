b4139c8de027315767efbbc8d96fca069cc63ab5
L. E. Segovia <amy@amyspark.me>
2021-06-17 02:30:59 +0000

    Windows: enforce activation of our PYTHONPATH's eggs
    Since setuptools 49.0.0 [1], their easy_install command no longer installs
    site.py to PYTHONPATH directories. This didn't hit me (or anyone using,
    as per 3rdparty, stock Python 3.8.1) because that version ships
    Setuptools 41; but anyone using v49 onwards would've run on importlib
    errors when trying to use sip-build or any of our 3rdparty tools.
    This bug can be easily triggered by making a virtual environment using
    virtualenv and then executing build.cmd inside it.
    
    This commit reuses Iván's sitecustomize.py for macOS and adds
    the entry suggested in [2] to inject our directory.
    
    While at it, let's ensure all PYTHONPATH entries are sanitized and
    stored in KRITA_PYTHONPATH, not just ours.
    
    [1]: https://setuptools.readthedocs.io/en/latest/history.html#v49-0-0
    
    [2]: https://github.com/pypa/setuptools/issues/2165
    
    CCMAIL: kimageshop@kde.org

--- 3rdparty/ext_python/sitecustomize.py
+++ 3rdparty/ext_python/sitecustomize.py
@@ -2,9 +2,14 @@ import os
 import site
 import sysconfig
 
-framework_path = os.path.dirname(os.path.abspath(__file__))
+# https://github.com/pypa/setuptools/issues/2165
+
+if os.name == "nt":
+    import pathlib
+    framework_path = pathlib.Path(__file__).parent.parent
+else:
+    framework_path = os.path.dirname(os.path.abspath(__file__))
 data_path = sysconfig.get_paths()['data']
 site.addsitedir(os.path.join(data_path, 'site-packages'))
 site.addsitedir(os.path.join(framework_path,'site-packages'))
 site.addsitedir(os.path.join(framework_path,'site-packages', 'PyQt5'))
-

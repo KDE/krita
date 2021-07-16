07cee9cea5f5de5c59479200abaf11c56ba20d92
Alvin Wong <alvin@alvinhc.com>
2021-07-17 00:33:46 +0800

    Fix i18n of `swatche.gpl`
    Removes the trailing `\t` from the extracted name of it.

--- extracti18n.pl
+++ extracti18n.pl
@@ -253,6 +253,7 @@ foreach my $filename (@filenames)
       my @splited = split(/: /, $lines[1]);
       my $name = $splited[1];
       chomp($name);
+      $name =~ s/\t$//; # Trim trailing \t, fixes the name for "swatche.gpl"
       printi18n($name, $filename, $filename, 2);
     }
     else

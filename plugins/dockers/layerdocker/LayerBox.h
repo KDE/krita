20ad2e7100ccb85804ee8ce74f8e6629eee28282
Sharaf Zaman <sharafzaz121@gmail.com>
2021-07-27 16:56:11 +0000

    Fix Layers Docker's opacity slider
    CCBUG:437039

--- plugins/dockers/layerdocker/LayerBox.h
+++ plugins/dockers/layerdocker/LayerBox.h
@@ -132,6 +132,8 @@ Q_SIGNALS:
 private:
     inline void connectActionToButton(KisViewManager* view, QAbstractButton *button, const QString &id);
     inline void addActionToMenu(QMenu *menu, const QString &id);
+    // reimp from KisNodeManager
+    qint32 convertOpacityToInt(qreal opacity);
 
     KisNodeSP findNonHidableNode(KisNodeSP startNode);
 private:

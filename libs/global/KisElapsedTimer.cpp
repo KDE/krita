58213f56d08f52b7af08be97095a769e432e7a96
Eoin O'Neill <eoinoneill1991@gmail.com>
2021-08-17 19:27:54 -0700

    Added wrapper class for QElapsedTimer, KisElapsedTimer.
    This should allow us to better encapsulate multiple, persistant
    timing values for accurate frame timing...

---
+++ libs/global/KisElapsedTimer.cpp
@@ -0,0 +1,29 @@
+#include "KisElapsedTimer.h"
+
+KisElapsedTimer::KisElapsedTimer()
+{
+
+}
+
+KisElapsedTimer::~KisElapsedTimer()
+{
+
+}
+
+void KisElapsedTimer::start()
+{
+    m_internalTimer.start();
+}
+
+void KisElapsedTimer::restart(int carryover)
+{
+    m_carryover = carryover;
+    m_internalTimer.restart();
+}
+
+qint64 KisElapsedTimer::elapsed() const noexcept
+{
+    return m_internalTimer.elapsed() + m_carryover;
+}
+
+

6dadff3a302d815f33fcfc457d3c855313ead4cf
Alvin Wong <alvin@alvinhc.com>
2021-08-27 17:44:22 +0800

    Fix zoom level on floating message mismatch with other places
    BUG: 429569
    (cherry picked from commit ba89aa33d5d1e934da9842570197cb0893da5188)

--- libs/ui/kis_zoom_manager.cc
+++ libs/ui/kis_zoom_manager.cc
@@ -309,10 +309,8 @@ void KisZoomManager::setRulersPixelMultiple2(bool enabled)
 
 void KisZoomManager::slotUpdateGuiAfterZoomChange()
 {
-    const qreal effectiveZoom =
-        m_view->canvasBase()->coordinatesConverter()->effectiveZoom();
-
-    const qreal humanZoom = effectiveZoom * 100.0;
+    const qreal zoomValue = m_view->canvasBase()->coordinatesConverter()->zoom();
+    const qreal humanZoom = zoomValue * 100.0;
 
     // XXX: KOMVC -- this is very irritating in MDI mode
 
@@ -325,8 +323,6 @@ void KisZoomManager::slotUpdateGuiAfterZoomChange()
     }
 
     updateCurrentZoomResource();
-
-    m_view->canvasBase()->resourceManager()->setResource(KoCanvasResource::EffectiveZoom, effectiveZoom);
 }
 
 void KisZoomManager::setMinMaxZoom()

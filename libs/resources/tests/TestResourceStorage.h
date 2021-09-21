759bc5ea8fec5153f8f4ae305816fddfe962731d
Agata Cacko <tamtamy.tymona@gmail.com>
2021-09-21 23:29:27 +0200

    Add StorageVersioningHelper tests
    Before this commit, there was no StorageVersioningHelper class
    tests. This commit adds checking for the chooseUniqueName() function.

--- libs/resources/tests/TestResourceStorage.h
+++ libs/resources/tests/TestResourceStorage.h
@@ -19,6 +19,7 @@ private Q_SLOTS:
     void testStorage();
     void testImportResourceFile();
     void testAddResource();
+    void testStorageVersioningHelperCounting();
     void cleanupTestCase();
 private:
     QString m_dstLocation;

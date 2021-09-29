9ad96d114c88e24c3a9a3c1161e746cdc8646a19
Agata Cacko <tamtamy.tymona@gmail.com>
2021-09-29 04:41:13 +0200

    Add import, rename and add functions with user input
    Before this commit, there was no central place where the code
    for importing, adding or renaming resources with user input
    was located. Every widget had to use KisResourceModel
    and handle the user input (asking the user for confirmation,
    showing the error messages etc.) on their own. This led to
    differences between handling different resources
    and problems making sure the logic is the same everywhere.
    This commit adds the functions in one central place.

--- libs/resourcewidgets/KisResourceOverwriteDialog.h
+++ libs/resourcewidgets/KisResourceOverwriteDialog.h
@@ -9,14 +9,30 @@
 
 #include <QWidget>
 #include "kritaresourcewidgets_export.h"
+#include <KoResource.h>
+#include <KisResourceModel.h>
 
 
 class KRITARESOURCEWIDGETS_EXPORT KisResourceOverwriteDialog
 {
 
 public:
+    // used when the user tries to import or create a new resource that already exists
+    // for example if they import the same resource file (the filename matches)
+    // or when they try to create a new resource with the same name and filename
     static bool userAllowsOverwrite(QWidget* widgetParent, QString resourceFilepath);
     static bool resourceExistsInResourceFolder(QString resourceType, QString filepath);
+
+    // used when the user tries to rename a resource to a name that already exists
+    // (it's permitted but can confuse the user later)
+    static bool userAllowsRename(QWidget* widgetParent, QString name);
+
+
+
+    static KoResourceSP importResourceFileWithUserInput(QWidget *widgetParent, KisResourceModel* resourceModel, QString storageLocation, QString resourceType, QString resourceFilepath);
+    static bool renameResourceWithUserInput(QWidget* widgetParent, KisResourceModel* resourceModel, KoResourceSP resource, QString resourceName);
+    static bool addResourceWithUserInput(QWidget* widgetParent, KisResourceModel* resourceModel, KoResourceSP resource, QString storageLocation = "");
+
 
 
 

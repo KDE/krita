b141bcfe11b63777f603e48f4703675e96576398
Dmitry Kazakov <dimula73@gmail.com>
2021-10-06 09:08:53 +0300

    Add APIDOX to the new KoResourceCacheInterface-based classes
    The patch also adds a huge wall of explanations for the whole
    preset-interfaces infrastructure.

--- libs/image/brushengine/kis_paintop_preset.h
+++ libs/image/brushengine/kis_paintop_preset.h
@@ -30,8 +30,71 @@ using KoResourceCacheInterfaceSP = QSharedPointer<KoResourceCacheInterface>;
  * associated with a paintop, like brush, paintopsettings.
  * A new property in this class is to make it dirty. That means the
  * user can now temporarily save any tweaks in the Preset throughout
- * the session. The Dirty Preset setting/unsetting is handled by KisPaintOpPresetSettings
+ * the session. The Dirty Preset setting/unsetting is handled
+ * by KisPaintOpPresetSettings
+ *
+ * KisPaintOpPreset is a **serialized** representation. That is,
+ * it stores only "metadata" needed to paint with a paintop. This
+ * metadata can be used to create painting c++-objects (e.g.
+ * brushes) or caches or embedded resources, **but** these objects
+ * are never stored inside the preset itself.
+ *
+ * It is done intentionally, because our GUI elements work with
+ * "metadata" only. They just read/write strings into the XML
+ * document, they don't know anything about the internal structure
+ * of the preset's C++ object. Therefore, if we stored any caches/
+ * resources inside the preset, they would become out-of-sync with
+ * the XML representation quite easily.
+ *
+ * To overcome this issue, we use a set of provider-like
+ * interfaces to store caches/resources outside the preset:
+ *
+ * 1) KisResourcesInterface is the interface that lets the preset
+ *    to fetch dependent resources from the cenralized storage.
+ *    Theoretically, the preset could create dependent resources
+ *    from its XML representation internally all the time, but
+ *    there are two reasond against that:
+ *
+ *       1) That is inefficient to load the brushes and/or textures
+ *          for every stroke. It can create significant delay in the
+ *          beginning of every stroke.
+ *
+ *       2) When we show an embedded resource in the GUI, it would
+ *          be nice to know the resource's 'resourceId', so we could
+ *          display it correctly. If we load the embedded resource
+ *          every time, 'resourceId' field would be empty.
+ *
+ *    Abstract KisResourcesInterface also allows us to replace a link
+ *    to the centralized storage with a snapshot of cloned resources,
+ *    when we start rendering data in the non-gui thread.
+ *
+ * 2) KoCanvasResourcesInterface is the interface that lets the preset
+ *    link to the global storage of "canvas resources", like current
+ *    gradient, current fg color and current bg color.
+ *
+ *    When we start a stroke, we make a snapshot of the current state
+ *    of the required canvas resources and replace the link to the
+ *    global storage with the created local snapshot.
+ *
+ *    Please take it into account that some of the embedded/linked
+ *    resources privided by KisResourcesInterface may **change**
+ *    while cloning. It happens when these resources "bake" the state
+ *    of canvas resources into themselves (e.g. gradient may change
+ *    the "fg-stop" with real fg color).
+ *
+ * 3) KoResourceCacheInterface is an interface that provides a storage
+ *    for the internal preset's caches (e.g. a brush with a pregenerated
+ *    pyramid or a pregenerated template for a rotated pattern).
+ *
+ *    The cache interface link is reset when any change is made to
+ *    the preset's metadata (which, effectively, resets all the caches).
+ *
+ *    The cache link should also be reset manually if the preset
+ *    depends on some canvas resource (e.g. fg-color) and this
+ *    canvas resource has changed. That is tracked by
+ *    KisPresetShadowUpdater.
  */
+
 class KRITAIMAGE_EXPORT KisPaintOpPreset : public KoResource
 {
 public:

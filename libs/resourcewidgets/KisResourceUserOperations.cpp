3c1e396ead42b8fe4faf7c4ec878e57bd7cd8130
Agata Cacko <tamtamy.tymona@gmail.com>
2021-10-06 06:02:18 +0200

    Fix logic for adding or renaming resources with the same names
    Adds:
    - checking for the same name regardless whether it uses "_" or " "
    - ensuring that it looks up in the resource folder only if
    we do want to add the resource to the resource folder
    (other storages are not handled, maybe yet, maybe never)

--- libs/resourcewidgets/KisResourceUserOperations.cpp
+++ libs/resourcewidgets/KisResourceUserOperations.cpp
@@ -19,7 +19,8 @@
 bool KisResourceUserOperations::userAllowsOverwrite(QWidget* widgetParent, QString resourceFilepath)
 {
     return QMessageBox::question(widgetParent, i18nc("Dialog title", "Overwrite the file?"),
-                          i18nc("Question in a dialog/messagebox", "This resource file already exists in the resource folder. Do you want to overwrite it?\nResource filename: %1", QFileInfo(resourceFilepath).fileName()),
+                          i18nc("Question in a dialog/messagebox", "This resource file already exists in the resource folder. "
+                                                                   "Do you want to overwrite it?\nResource filename: %1", QFileInfo(resourceFilepath).fileName()),
                                  QMessageBox::Yes | QMessageBox::Cancel, QMessageBox::Cancel) != QMessageBox::Cancel;
 }
 
@@ -31,12 +32,19 @@ bool KisResourceUserOperations::resourceExistsInResourceFolder(QString resourceT
     return fi.exists();
 }
 
+bool KisResourceUserOperations::resourceNameIsAlreadyUsed(KisResourceModel *resourceModel, QString resourceName)
+{
+    QVector<KoResourceSP> resourcesWithTheSameExactName = resourceModel->resourcesForName(resourceName);
+    QVector<KoResourceSP> resourcesWithSpacesReplacedByUnderlines = resourceModel->resourcesForName(resourceName.replace(" ", "_"));
+    return resourcesWithTheSameExactName.size() > 0 || resourcesWithSpacesReplacedByUnderlines.size() > 0;
+}
+
 KoResourceSP KisResourceUserOperations::importResourceFileWithUserInput(QWidget *widgetParent, KisResourceModel* resourceModel, QString storageLocation, QString resourceType, QString resourceFilepath)
 {
     if (!resourceModel) return KoResourceSP();
 
     KoResourceSP resource = resourceModel->importResourceFile(resourceFilepath, false, storageLocation);
-    if (resource.isNull() && KisResourceUserOperations::resourceExistsInResourceFolder(resourceType, resourceFilepath)) {
+    if (resource.isNull() && storageLocation == "" && KisResourceUserOperations::resourceExistsInResourceFolder(resourceType, resourceFilepath)) {
         if (KisResourceUserOperations::userAllowsOverwrite(widgetParent, resourceFilepath)) {
             resource = resourceModel->importResourceFile(resourceFilepath, true, storageLocation);
         } else {
@@ -58,7 +66,9 @@ bool KisResourceUserOperations::renameResourceWithUserInput(QWidget *widgetParen
             return true; // no rename needed
         }
         bool userWantsRename = QMessageBox::question(widgetParent, i18nc("@title:window", "Rename the resource?"),
-                              i18nc("Question in a dialog/messagebox", "This name is already used for another resource. Do you want to use the same name for multiple resources?"),
+                              i18nc("Question in a dialog/messagebox", "This name is already used for another resource. "
+                                                                       "Do you want to use the same name for multiple resources?"
+                                                                       "(If you decline now, the resource won't be renamed)."),
                                      QMessageBox::Yes | QMessageBox::Cancel, QMessageBox::Cancel) != QMessageBox::Cancel;
         if (!userWantsRename) {
             return false;
@@ -74,7 +84,8 @@ bool KisResourceUserOperations::renameResourceWithUserInput(QWidget *widgetParen
 bool KisResourceUserOperations::addResourceWithUserInput(QWidget *widgetParent, KisResourceModel *resourceModel, KoResourceSP resource, QString storageLocation)
 {
     if (!resourceModel) return false;
-    if (resourceExistsInResourceFolder(resource->resourceType().first, resource->filename())) {
+    // check if adding the resource is possible: it is not if there is a resource with the same filename in the storage the user want to save the resource to
+    if (storageLocation == "" && resourceExistsInResourceFolder(resource->resourceType().first, resource->filename())) {
         // TODO: potentially, ask the user to rename the resource etc.
         int resourceWithThatFilenameId;
         if (KisResourceCacheDb::getResourceIdFromVersionedFilename(resource->filename(), resource->resourceType().first, storageLocation, resourceWithThatFilenameId)) {
@@ -89,6 +100,19 @@ bool KisResourceUserOperations::addResourceWithUserInput(QWidget *widgetParent, 
                                    resource->filename(), resource->name()));
         return false;
     }
+    // check if there are any other resources with the same name, even in different storages or with different filenames
+    if (resourceNameIsAlreadyUsed(resourceModel, resource->name()))
+    {
+        bool userWantsAdd = QMessageBox::question(widgetParent, i18nc("@title:window", "Add the resource?"),
+                              i18nc("Question in a dialog/messagebox", "This name is already used for another resource. "
+                                                                       "Do you want to use the same name for multiple resources? "
+                                                                       "(If you decline now, the resource won't be added)."),
+                                     QMessageBox::Yes | QMessageBox::Cancel, QMessageBox::Cancel) != QMessageBox::Cancel;
+
+        if (!userWantsAdd) {
+            return false;
+        }
+
     bool res = resourceModel->addResource(resource, storageLocation);
     if (!res) {
         QMessageBox::warning(widgetParent, i18nc("@title:window", "Failed to add resource"), i18nc("Warning message", "Failed to add the resource."));
@@ -103,8 +127,7 @@ bool KisResourceUserOperations::updateResourceWithUserInput(QWidget *widgetParen
     QString oldName = resourceModel->data(resourceModel->indexForResourceId(resource->resourceId()), Qt::UserRole + KisAllResourcesModel::Name).toString();
     if (resource->name() != oldName) {
         // rename in action
-        QVector<KoResourceSP> resources = resourceModel->resourcesForName(resource->name());
-        if (resources.size() > 0) {
+        if (resourceNameIsAlreadyUsed(resourceModel, resource->name()) > 0) {
             bool userWantsRename = QMessageBox::question(widgetParent, i18nc("@title:window", "Rename the resource?"),
                                   i18nc("Question in a dialog/messagebox", "This name is already used for another resource. Do you want to overwrite "
                                                                            "and use the same name for multiple resources?"

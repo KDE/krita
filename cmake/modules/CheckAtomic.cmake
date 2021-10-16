410ed417d5ac317f85eefc0f3b5c437b53537d07
L. E. Segovia <amy@amyspark.me>
2021-10-16 17:38:10 +0000

    Upgrade CheckAtomic module to a copy of ECM 5.75
    I am making this commit since !498 seems to not have progressed for
    quite a few months.
    
    BUG: 430872
    BUG: 443817
    Closes !498
    
    (cherry picked from commit f963f4f42b66715405c9bf087c630df8725332a2)

--- cmake/modules/CheckAtomic.cmake
+++ cmake/modules/CheckAtomic.cmake
@@ -1,66 +1,67 @@
-# ==============================================================================
-# LLVM Release License
-# ==============================================================================
-# University of Illinois/NCSA
-# Open Source License
-#
 # SPDX-FileCopyrightText: 2003-2018 University of Illinois at Urbana-Champaign.
-# All rights reserved.
-#
-# Developed by:
-#
-# LLVM Team
-#
-# University of Illinois at Urbana-Champaign
-#
-# http://llvm.org
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-INCLUDE(CheckCXXSourceCompiles)
-INCLUDE(CheckLibraryExists)
+#[=======================================================================[.rst:
+CheckAtomic
+-----------
+
+Check if the compiler supports std:atomic out of the box or if libatomic is
+needed for atomic support. If it is needed libatomicis added to
+``CMAKE_REQUIRED_LIBRARIES``. So after running CheckAtomic you can use
+std:atomic.
+
+Since 5.75.0.
+#]=======================================================================]
+
+include(CheckCXXSourceCompiles)
+include(CheckLibraryExists)
 
 # Sometimes linking against libatomic is required for atomic ops, if
 # the platform doesn't support lock-free atomics.
 
 function(check_working_cxx_atomics varname)
-set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
-set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11")
-CHECK_CXX_SOURCE_COMPILES("
-#include <atomic>
-std::atomic<int> x;
-int main() {
-return std::atomic_is_lock_free(&x);
-}
-" ${varname})
-set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
-endfunction(check_working_cxx_atomics)
+	set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+	set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11")
+	check_cxx_source_compiles("
+	#include <atomic>
+	std::atomic<int> x;
+	std::atomic<short> y;
+	std::atomic<char> z;
+	int main() {
+		++z;
+		++y;
+		return ++x;
+	}
+	" ${varname})
+	set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+endfunction()
 
 function(check_working_cxx_atomics64 varname)
-set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
-set(CMAKE_REQUIRED_FLAGS "-std=c++11 ${CMAKE_REQUIRED_FLAGS}")
-CHECK_CXX_SOURCE_COMPILES("
-#include <atomic>
-#include <cstdint>
-std::atomic<uint64_t> x (0);
-int main() {
-uint64_t i = x.load(std::memory_order_relaxed);
-return std::atomic_is_lock_free(&x);
-}
-" ${varname})
-set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
-endfunction(check_working_cxx_atomics64)
+	set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+	set(CMAKE_REQUIRED_FLAGS "-std=c++11 ${CMAKE_REQUIRED_FLAGS}")
+	check_cxx_source_compiles("
+	#include <atomic>
+	#include <cstdint>
+	std::atomic<uint64_t> x (0);
+	int main() {
+		uint64_t i = x.load(std::memory_order_relaxed);
+		return 0;
+	}
+	" ${varname})
+	set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+endfunction()
 
-
-# This isn't necessary on MSVC, so avoid command-line switch annoyance
-# by only running on GCC-like hosts.
-if (LLVM_COMPILER_IS_GCC_COMPATIBLE)
+# Check for (non-64-bit) atomic operations.
+if(MSVC)
+	set(HAVE_CXX_ATOMICS_WITHOUT_LIB True)
+elseif(LLVM_COMPILER_IS_GCC_COMPATIBLE)
 	# First check if atomics work without the library.
 	check_working_cxx_atomics(HAVE_CXX_ATOMICS_WITHOUT_LIB)
 	# If not, check if the library exists, and atomics work with it.
 	if(NOT HAVE_CXX_ATOMICS_WITHOUT_LIB)
 		check_library_exists(atomic __atomic_fetch_add_4 "" HAVE_LIBATOMIC)
-		if( HAVE_LIBATOMIC )
+		if(HAVE_LIBATOMIC)
 			list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
 			check_working_cxx_atomics(HAVE_CXX_ATOMICS_WITH_LIB)
 			if (NOT HAVE_CXX_ATOMICS_WITH_LIB)
@@ -71,23 +72,24 @@ if (LLVM_COMPILER_IS_GCC_COMPATIBLE)
 		endif()
 	endif()
 endif()
+
 # Check for 64 bit atomic operations.
 if(MSVC)
 	set(HAVE_CXX_ATOMICS64_WITHOUT_LIB True)
-else()
+elseif(LLVM_COMPILER_IS_GCC_COMPATIBLE)
+	# First check if atomics work without the library.
 	check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITHOUT_LIB)
-endif()
-
-# If not, check if the library exists, and atomics work with it.
-if(NOT HAVE_CXX_ATOMICS64_WITHOUT_LIB)
-	check_library_exists(atomic __atomic_load_8 "" HAVE_CXX_LIBATOMICS64)
-	if(HAVE_CXX_LIBATOMICS64)
-		list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
-		check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITH_LIB)
-		if (NOT HAVE_CXX_ATOMICS64_WITH_LIB)
-			message(FATAL_ERROR "Host compiler must support std::atomic!")
+	# If not, check if the library exists, and atomics work with it.
+	if(NOT HAVE_CXX_ATOMICS64_WITHOUT_LIB)
+		check_library_exists(atomic __atomic_load_8 "" HAVE_CXX_LIBATOMICS64)
+		if(HAVE_CXX_LIBATOMICS64)
+			list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
+			check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITH_LIB)
+			if (NOT HAVE_CXX_ATOMICS64_WITH_LIB)
+				message(FATAL_ERROR "Host compiler must support 64-bit std::atomic!")
+			endif()
+		else()
+			message(FATAL_ERROR "Host compiler appears to require libatomic for 64-bit operations, but cannot find it.")
 		endif()
-	else()
-		message(FATAL_ERROR "Host compiler appears to require libatomic, but cannot find it.")
 	endif()
 endif()

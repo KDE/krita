02a5f822b320e2e92b61610ce756ccb58729c853
L. E. Segovia <amy@amyspark.me>
2021-11-09 16:49:42 +0000

    Return failed status on ASL imports
    (cherry picked from commit c321b4f77fef7e1152ab3565ca27034edde6d674)

--- libs/image/kis_asl_layer_style_serializer.cpp
+++ libs/image/kis_asl_layer_style_serializer.cpp
@@ -1249,11 +1249,11 @@ bool KisAslLayerStyleSerializer::readFromFile(const QString& filename)
         dbgKrita << "Can't open file " << filename;
         return false;
     }
-    readFromDevice(file);
-    m_initialized = true;
+
+    readFromDevice(&file);
     file.close();
 
-    return true;
+    return m_initialized;
 }
 
 QVector<KisPSDLayerStyleSP> KisAslLayerStyleSerializer::collectAllLayerStyles(KisNodeSP root)
@@ -1331,7 +1331,12 @@ void KisAslLayerStyleSerializer::readFromDevice(QIODevice &device)
     m_catcher.subscribeNewStyleStarted(std::bind(&KisAslLayerStyleSerializer::newStyleStarted, this, false));
 
     KisAslReader reader;
-    QDomDocument doc = reader.readFile(device);
+    const QDomDocument doc = reader.readFile(device);
+
+    if (doc.isNull()) {
+        m_initialized = false;
+        return;
+    }
 
     //dbgKrita << ppVar(doc.toString());
 

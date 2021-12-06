a5214a7dfc606e279ba11f979788fde8f45eb8c6
Sharaf Zaman <shzam@sdf.org>
2021-12-06 22:49:02 +0000

    Android: Fix flipped colors of Red and Blue channels in 16i mode
    This was caused by eb920fadf553ca827b4ff783ff6f82395bc88144 since it
    explicitly handles Integer-16bit channel which has GL_BGRA format for
    its textures.

--- libs/ui/opengl/kis_texture_tile.cpp
+++ libs/ui/opengl/kis_texture_tile.cpp
@@ -29,7 +29,11 @@ void KisTextureTile::setTextureParameters()
     f->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
     f->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, m_numMipmapLevels);
 
-    if (m_texturesInfo->internalFormat == GL_RGBA8 && m_texturesInfo->format == GL_RGBA) {
+    if ((m_texturesInfo->internalFormat == GL_RGBA8 && m_texturesInfo->format == GL_RGBA)
+#ifdef QT_OPENGL_ES_3
+        || (m_texturesInfo->internalFormat == GL_RGBA16_EXT && m_texturesInfo->format == GL_RGBA)
+#endif
+    ) {
         // If image format is RGBA8, swap the red and blue channels for the proper colour
         // This is for OpenGL ES support and only used if lacking GL_EXT_texture_format_BGRA8888
         f->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE);

97d98970145f80b5ee283ce5465faf04a6068eb4
Dmitry Kazakov <dimula73@gmail.com>
2021-12-24 15:40:17 +0300

    Fix memory leak of KisColorHistory::m_clearButton
    The object should ahve aparent to be deleted in the end

--- plugins/dockers/advancedcolorselector/kis_color_history.cpp
+++ plugins/dockers/advancedcolorselector/kis_color_history.cpp
@@ -21,15 +21,13 @@ KisColorHistory::KisColorHistory(QWidget *parent)
     : KisColorPatches("lastUsedColors", parent)
     , m_resourceProvider(0)
 {
-    m_clearButton = new QToolButton();
+    m_clearButton = new QToolButton(this);
     m_clearButton->setIcon(KisIconUtils::loadIcon("dialog-cancel-16"));
     m_clearButton->setToolTip(i18n("Clear all color history"));
     m_clearButton->setAutoRaise(true);
     connect(m_clearButton, SIGNAL(clicked()), this, SLOT(clearColorHistory()));
 
-    QList<QWidget*> tmpList;
-    tmpList.append(m_clearButton);
-    setAdditionalButtons(tmpList);
+    setAdditionalButtons({m_clearButton});
 }
 
 void KisColorHistory::unsetCanvas()

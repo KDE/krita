78dc4550cbdfa4615506f3b0f73f4269a124824a
Agata Cacko <tamtamy.tymona@gmail.com>
2022-02-10 00:44:31 +0100

    Add unit tests for signals in KisTagResourceModel
    Before this commit, there was no way to check whether the signals
    in KisTagResourceModel are correct/expected without doing manual
    testing in the Resource Manager (and even then, it was never
    clear if the signals are really always correct or not).
    This commit adds unit tests checking for the correctness
    of signals sent by the KisTagResourceModel.
    
    In the future, the ModelSignalChecker class can be used for
    other models to ensure their signal correctness.
    
    Note that when having multiple beginInsert... and beginRemove...
    signals, they can end up in the checker in a different
    (opposite) order than they are called in the model
    (note that our models don't emit the signals directly but only
    use the functions begin...(); and the Qt underlying
    I observed it with tagResources() and I'm not sure yet
    of the reason why that happens. The documentation of those signals
    is not detailed enough for complex scenerios (like sending multiple
    beginInsert... or beginRemove... signals).
    
    ---
    
    Note that when having multiple beginInsert... and beginRemove...
    calls, the rowsAboutToBeInserted and rowsAboutToBeRemoved
    signals can end up in the checked in a different (opposite) order.
    I observed it specifically in testing tagResources()
    (with beginInsert... call), and I assume the reason for it
    are internal indices row calculations within the
    QAbstractItemModel class code.
    
    The documentation of those signals and calls is not detailed
    enough for complex scenerios (like calling beginInsert... or
    beginRemove... multiple times). (Hence explanations in comments
    in tagResources() and untagResources() functions).
    
    (cherry picked from commit 9f19205a421efecf37e24a500978ec29591c2643)

--- libs/resources/tests/TestTagResourceModel.h
+++ libs/resources/tests/TestTagResourceModel.h
@@ -8,6 +8,7 @@
 
 #include <QObject>
 #include <QtSql>
+#include <QString>
 #include "KisResourceTypes.h"
 
 class KisResourceLocator;
@@ -27,6 +28,8 @@ private Q_SLOTS:
 
     void testFilterTagResource();
 
+    void testBeginEndInsert();
+
     void cleanupTestCase();
 
 private:
@@ -36,6 +39,85 @@ private:
 
     KisResourceLocator *m_locator;
     const QString resourceType = ResourceType::PaintOpPresets;
+
+};
+
+
+// *** MODEL SIGNAL CHECKER *** //
+
+// Simple class checking the beginInsert... etc. signals
+//  and comparing with expected ones.
+// Make sure to connect the slots to the signals when using.
+// Class must be defined in .h because it uses Q_OBJECT macro
+class ModelSignalChecker : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    enum TYPE
+    {
+        AboutInsert,
+        Insert,
+        AboutRemove,
+        Remove
+    };
+
+    ///
+    /// \brief SignalChecker constructor
+    /// \param _expectedAmount number of signals expected
+    /// \param _expectedFirsts list of expected first indices (in order)
+    /// \param _expectedLasts list of expected last indices (in order)
+    /// \param _expectedTypes list of expected types of signals (in order)
+    ///
+    ModelSignalChecker(QList<int> _expectedFirsts, QList<int> _expectedLasts, QList<TYPE> _expectedTypes);
+
+
+    ///
+    /// \brief setInfo a function to set a new set of expected infos to reuse the instance
+    /// \param _expectedAmount number of signals expected
+    /// \param _expectedFirsts list of expected first indices (in order)
+    /// \param _expectedLasts list of expected last indices (in order)
+    /// \param _expectedTypes list of expected types of signals (in order)
+    ///
+    void setInfo(QList<int> _expectedFirsts, QList<int> _expectedLasts, QList<TYPE> _expectedTypes);
+    ///
+    /// \brief reset reset the information collected by the instance so it can be reused
+    ///
+    void reset();
+    ///
+    /// \brief isCorrect
+    /// \return if the collected signals information is the same as the expected
+    ///
+    bool isCorrect();
+    ///
+    /// \brief writeOut simple debug function (prints out signal information using qCritical())
+    ///
+    void printOut();
+
+
+public Q_SLOTS:
+    void rowsAboutToBeInserted(const QModelIndex &parent, int first, int last);
+    void rowsInserted(const QModelIndex &parent, int first, int last);
+    void rowsAboutToBeRemoved(const QModelIndex &parent, int first, int last);
+    void rowsRemoved(const QModelIndex &parent, int first, int last);
+
+private:
+    void addSignalInfo(int first, int last, TYPE type);
+
+
+public:
+    // collected information
+    QList<int> firsts;
+    QList<int> lasts;
+    QList<TYPE> types;
+    int amount;
+
+    // expected information
+    QList<int> expectedFirsts;
+    QList<int> expectedLasts;
+    QList<TYPE> expectedTypes;
+    int expectedAmount;
 
 };
 

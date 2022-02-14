7e064bb21c7664a7294f29ecf675a6e3336a307e
Agata Cacko <tamtamy.tymona@gmail.com>
2022-02-14 20:18:09 +0100

    Add log line when the database is created from scratch
    There is a log line when the database is up to date,
    it makes sense to add a log line when it's created from scratch, too.
    
    (cherry picked from commit 39a5980a4fc5a1314841302f321d22752adb4bfb)

--- libs/resources/KisResourceCacheDb.cpp
+++ libs/resources/KisResourceCacheDb.cpp
@@ -110,6 +110,11 @@ QSqlError createDatabase(const QString &location)
         return db.lastError();
     }
 
+    // will be filled correctly later
+    QVersionNumber oldSchemaVersionNumber;
+    QVersionNumber newSchemaVersionNumber = QVersionNumber::fromString(KisResourceCacheDb::databaseVersion);
+
+
     QStringList tables = QStringList() << "version_information"
                                        << "storage_types"
                                        << "resource_types"
@@ -159,8 +164,8 @@ QSqlError createDatabase(const QString &location)
             kritaVersion = q.value(1).toString();
             creationDate = q.value(2).toInt();
 
-            QVersionNumber oldSchemaVersionNumber = QVersionNumber::fromString(schemaVersion);
-            QVersionNumber newSchemaVersionNumber = QVersionNumber::fromString(KisResourceCacheDb::databaseVersion);
+            oldSchemaVersionNumber = QVersionNumber::fromString(schemaVersion);
+            newSchemaVersionNumber = QVersionNumber::fromString(KisResourceCacheDb::databaseVersion);
 
             if (QVersionNumber::compare(oldSchemaVersionNumber, newSchemaVersionNumber) != 0) {
 
@@ -241,6 +246,10 @@ QSqlError createDatabase(const QString &location)
             return QSqlError();
         }
     }
+
+    KisUsageLogger::log(QString("Creating database from scratch (%1, %2).")
+                        .arg(oldSchemaVersionNumber.toString().isEmpty() ? QString("database didn't exist") : ("old schema version: " + oldSchemaVersionNumber.toString()))
+                        .arg("new schema version: " + newSchemaVersionNumber.toString()));
 
     // Create tables
     Q_FOREACH(const QString &table, tables) {

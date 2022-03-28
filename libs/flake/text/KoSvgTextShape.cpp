57fc33f8bd1bc246a37069dec803d2cce453ae24
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2022-03-28 13:35:56 +0200

    Fix anchoring on closed textpaths.

--- libs/flake/text/KoSvgTextShape.cpp
+++ libs/flake/text/KoSvgTextShape.cpp
@@ -436,6 +436,7 @@ void KoSvgTextShape::relayout() const
         // convert the outline to a painter path
         // This is taken from qfontengine_ft.cpp.
         QPainterPath glyph;
+        glyph.setFillRule(Qt::WindingFill);
         int i = 0;
         for (int j = 0; j < glyphSlot->outline.n_contours; ++j) {
             int last_point = glyphSlot->outline.contours[j];
@@ -888,18 +889,12 @@ void KoSvgTextShape::Private::applyTextPath(const KoShape *rootShape,
                     b = qMax(b, qMax(pos, pos+advance));
                 }
             }
+            qreal baWidth = b-a;
 
             for (int i = currentIndex; i < endIndex; i++) {
                 qreal startpointOnThePath = -a + offset;
                 CharacterResult cr = result[i];
                 bool rtl = (cr.direction == KoSvgText::DirectionRightToLeft);
-
-                if ((cr.anchor == KoSvgText::AnchorEnd  && !rtl)
-                 || (cr.anchor == KoSvgText::AnchorStart && rtl)) {
-                    startpointOnThePath = -a + (length-(b-a + offset));
-                } else if (cr.anchor == KoSvgText::AnchorMiddle){
-                    startpointOnThePath = -a + (length - (b-a))*0.5 + offset;
-                }
 
                 if (cr.middle == false) {
                     qreal mid = cr.finalPosition.x() + (cr.advance.x() * 0.5) + startpointOnThePath;
@@ -915,19 +910,28 @@ void KoSvgTextShape::Private::applyTextPath(const KoShape *rootShape,
                             }
                         } else if ((cr.anchor == KoSvgText::AnchorEnd  && !rtl)
                                 || (cr.anchor == KoSvgText::AnchorStart && rtl)) {
+                            mid -= baWidth;
                             if (mid - offset < -length || mid - offset > 0) {
                                 cr.hidden = true;
                             }
                         } else {
+                            mid -= baWidth*0.5;
                             if (mid - offset < -(length*0.5) || mid - offset > (length*0.5)) {
                                 cr.hidden = true;
                             }
                         }
-                        if (mid > length) {
-                            mid -= length;
+                        if (a < 0) { mid += length;}
+                        mid = fmod(mid, length);
+                    } else {
+                        if ((cr.anchor == KoSvgText::AnchorEnd  && !rtl)
+                         || (cr.anchor == KoSvgText::AnchorStart && rtl)) {
+                            mid += (length-(baWidth + offset*2));
+                        } else if (cr.anchor == KoSvgText::AnchorMiddle){
+                            mid += (length - baWidth)*0.5;
                         }
-                    } else if(mid < 0 || mid > length){
-                        cr.hidden = true;
+                        if(mid < 0 || mid > length){
+                            cr.hidden = true;
+                        }
                     }
                     if (!cr.hidden) {
                         qreal percent = path.percentAtLength(mid);

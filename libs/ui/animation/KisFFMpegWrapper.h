d57118db1f13c3314876cade5023b6d84b01c63c
Eoin O'Neill <eoinoneill1991@gmail.com>
2022-03-31 14:29:54 -0700

    FFMPGEG Create method for checking ffprobe stream validity.
    More cleanup of code flow, extension of last commit.

--- libs/ui/animation/KisFFMpegWrapper.h
+++ libs/ui/animation/KisFFMpegWrapper.h
@@ -38,9 +38,7 @@ struct KRITAUI_EXPORT KisFFMpegWrapperSettings
 enum FFProbeErrorCodes {
     NONE = 0,
     UNSUPPORTED_CODEC,
-    INVALID_JSON,
-    INVALID_JSON_FORMAT,
-
+    INVALID_JSON
 };
 
 const int FFMPEG_TIMEOUT = 600000;
@@ -88,6 +86,14 @@ private Q_SLOTS:
 
 private:
     void updateProgressDialog(int progressValue);
+
+    /**
+     * @brief ffprobeCheckStreamsValid
+     * @param ffprobeJsonObj JsonObject resulting from ffprobe proces.
+     * @param ffprobeSTDERR STDERR output from ffprobe.
+     * @return bool -> Whether we support a video format based on it's internal streams.
+     */
+    bool ffprobeCheckStreamsValid(const QJsonObject& ffprobeJsonObj, const QString& ffprobeSTDERR);
     
 private:
     QScopedPointer<QProcess> m_process;

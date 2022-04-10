7a5f142fe14c7ed5f4bb1ad1163c712baf9b2606
Alvin Wong <alvin@alvinhc.com>
2022-04-10 17:15:34 +0800

    3rdparty: Add distutils workaround for llvm-mingw
    distutils's cygwinccompiler module contains some very old version
    checking code that had only been removed upstream recently in [1]. This
    includes a version check for `ld`. The llvm-mingw toolchain only
    contains `ld.lld.exe` as an executable but not `ld.exe`, which breaks
    this check. Here we just monkey patch this check to return some modern
    version for `ld` to make the check not fail.
    
    [1]: https://github.com/pypa/distutils/commit/221a2f2888aa1a48887003c5afa10c1d18ae3a92

--- 3rdparty/ext_python/_distutils_system_mod.py
+++ 3rdparty/ext_python/_distutils_system_mod.py
@@ -28,6 +28,27 @@ print("[*] Check: {} {}".format(cygwinccompiler.get_msvcr,
       cygwinccompiler.get_msvcr()))
 
 
+# distutils's cygwinccompiler module contains some very old version checking
+# code that had only been removed upstream recently in [1]. This includes a
+# version check for `ld`. The llvm-mingw toolchain only contains `ld.lld.exe`
+# as an executable but not `ld.exe`, which breaks this check. Here we just
+# monkey patch this check to return some modern version for `ld` to make the
+# check not fail.
+# [1]: https://github.com/pypa/distutils/commit/221a2f2888aa1a48887003c5afa10c1d18ae3a92
+
+_original_get_versions = cygwinccompiler.get_versions
+
+def _get_versions_replacement():
+    gcc_ver, ld_ver, dllwrap_ver = _original_get_versions()
+    if ld_ver is None:
+        ld_ver = "2.37"
+    return gcc_ver, ld_ver, dllwrap_ver
+
+if callable(cygwinccompiler.get_versions):
+    print("[*] Krita is patching cygwinccompiler.get_versions... ({})".format(cygwinccompiler.__file__))
+    cygwinccompiler.get_versions = _get_versions_replacement
+
+
 # HACK: Override distutils.cygwinccompiler lookup
 # https://github.com/pypa/setuptools/blob/298f5a6368397977ee09cb0b39d5f76aa544b048/setuptools/_distutils/ccompiler.py#L1024
 print("[*] Freezing distutils.cygwinccompiler module")

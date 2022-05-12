c63018e3b7f65658c34c23e8b2d43e2b8a64f239
L. E. Segovia <amy@amyspark.me>
2022-05-11 23:12:45 -0300

    Port Dmitry's U8 <-> U16 fast converter to Arm

--- libs/pigment/KoOptimizedPixelDataScalerU8ToU16.h
+++ libs/pigment/KoOptimizedPixelDataScalerU8ToU16.h
@@ -12,10 +12,7 @@
 #include "KoMultiArchBuildSupport.h"
 #include "kis_debug.h"
 
-#if defined(__i386__) || defined(__x86_64__)
-#include <immintrin.h>
-#endif
-
+#include <xsimd_extensions/xsimd.hpp>
 
 template<typename _impl = xsimd::current_arch>
 class KoOptimizedPixelDataScalerU8ToU16 : public KoOptimizedPixelDataScalerU8ToU16Base
@@ -26,20 +23,33 @@ public:
     {
     }
 
-    void convertU8ToU16(const quint8 *src, int srcRowStride,
-                        quint8 *dst, int dstRowStride,
-                        int numRows, int numColumns) const override
+    void convertU8ToU16(const quint8 *src, int srcRowStride, quint8 *dst, int dstRowStride, int numRows, int numColumns) const override
     {
         const int numColorChannels = m_channelsPerPixel * numColumns;
 
-#if defined __AVX2__
+#if XSIMD_WITH_AVX2
+        using uint16_avx_v = xsimd::batch<uint16_t, _impl>;
+        using uint16_v = xsimd::batch<uint16_t, xsimd::sse4_1>;
+        using uint8_v = xsimd::batch<uint8_t, xsimd::sse4_1>;
+
         const int channelsPerAvx2Block = 16;
         const int channelsPerSse2Block = 8;
         const int avx2Block = numColorChannels / channelsPerAvx2Block;
         const int rest = numColorChannels % channelsPerAvx2Block;
         const int sse2Block = rest / channelsPerSse2Block;
         const int scalarBlock = rest % channelsPerSse2Block;
-#elif defined __SSE4_1__
+#elif XSIMD_WITH_SSE4_1 || XSIMD_WITH_NEON || XSIMD_WITH_NEON64
+#if XSIMD_WITH_SSE4_1
+        using uint16_v = xsimd::batch<uint16_t, xsimd::sse4_1>;
+        using uint8_v = xsimd::batch<uint8_t, xsimd::sse4_1>;
+#elif XSIMD_WITH_NEON64
+        using uint16_v = xsimd::batch<uint16_t, xsimd::neon64>;
+        using uint8_v = xsimd::batch<uint8_t, xsimd::neon64>;
+#else
+        using uint16_v = xsimd::batch<uint16_t, xsimd::neon>;
+        using uint8_v = xsimd::batch<uint8_t, xsimd::neon>;
+#endif
+
         const int channelsPerSse2Block = 8;
         const int avx2Block = 0;
         const int sse2Block = numColorChannels / channelsPerSse2Block;
@@ -50,22 +60,22 @@ public:
         const int scalarBlock = numColorChannels;
 #endif
 
-        //qWarning() << ppVar(avx2Block) << ppVar(sse2Block);
+        // qWarning() << ppVar(avx2Block) << ppVar(sse2Block);
 
         for (int row = 0; row < numRows; row++) {
-
             const quint8 *srcPtr = src;
-            quint16 *dstPtr = reinterpret_cast<quint16*>(dst);
+            auto *dstPtr = reinterpret_cast<quint16 *>(dst);
 
-#ifdef __AVX2__
+#if XSIMD_WITH_AVX2
             for (int i = 0; i < avx2Block; i++) {
-                __m128i x = _mm_loadu_si128(reinterpret_cast<const __m128i*>(srcPtr));
+                const auto x = uint8_v::load_unaligned(srcPtr);
 
-                __m256i y = _mm256_cvtepu8_epi16(x);
-                __m256i y_shifted = _mm256_slli_epi16(y, 8);
-                y = _mm256_or_si256(y, y_shifted);
+                uint16_avx_v y(_mm256_cvtepu8_epi16(x));
+                const auto y_shifted = y << 8;
+                y |= y_shifted;
 
-                _mm256_storeu_si256(reinterpret_cast<__m256i*>(dstPtr), y);
+                y.store_unaligned(
+                    reinterpret_cast<typename uint16_avx_v::value_type *>(dstPtr));
 
                 srcPtr += channelsPerAvx2Block;
                 dstPtr += channelsPerAvx2Block;
@@ -74,15 +84,24 @@ public:
             Q_UNUSED(avx2Block);
 #endif
 
-#ifdef __SSE4_1__
+#if XSIMD_WITH_SSE4_1 || XSIMD_WITH_NEON || XSIMD_WITH_NEON64
             for (int i = 0; i < sse2Block; i++) {
-                __m128i x = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(srcPtr));
+#if XSIMD_WITH_SSE4_1
+                const uint8_v x(_mm_loadl_epi64(reinterpret_cast<const __m128i *>(srcPtr)));
+#else
+                const uint8_v x(vreinterpretq_u8_u32(vcombine_u32(
+                    vld1_u32(reinterpret_cast<const uint32_t *>(srcPtr)),
+                    vcreate_u32(0))));
+#endif
+#if XSIMD_WITH_SSE4_1
+                uint16_v y(_mm_cvtepu8_epi16(x.data));
+#else
+                uint16_v y(vmovl_u8(vget_low_u8(x.data)));
+#endif
+                const auto y_shifted = y << 8;
+                y |= y_shifted;
 
-                __m128i y = _mm_cvtepu8_epi16(x);
-                __m128i y_shifted = _mm_slli_epi16(y, 8);
-                y = _mm_or_si128(y, y_shifted);
-
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(dstPtr), y);
+                y.store_unaligned(reinterpret_cast<typename uint16_v::value_type *>(dstPtr));
 
                 srcPtr += channelsPerSse2Block;
                 dstPtr += channelsPerSse2Block;
@@ -94,25 +113,25 @@ public:
             for (int i = 0; i < scalarBlock; i++) {
                 const quint16 value = *srcPtr;
 
-                *dstPtr = value | (value << 8);
+                *dstPtr = static_cast<quint16>(value | (value << 8));
 
                 srcPtr++;
                 dstPtr++;
             }
-
 
             src += srcRowStride;
             dst += dstRowStride;
         }
     }
 
-    void convertU16ToU8(const quint8 *src, int srcRowStride,
-                        quint8 *dst, int dstRowStride,
-                        int numRows, int numColumns) const override
+    void convertU16ToU8(const quint8 *src, int srcRowStride, quint8 *dst, int dstRowStride, int numRows, int numColumns) const override
     {
         const int numColorChannels = m_channelsPerPixel * numColumns;
 
-#if defined __AVX2__
+#if XSIMD_WITH_AVX2
+        using uint16_avx_v = xsimd::batch<uint16_t, _impl>;
+        using uint16_v = xsimd::batch<uint16_t, xsimd::sse4_1>;
+
         const int channelsPerAvx2Block = 32;
         const int channelsPerSse2Block = 16;
         const int avx2Block = numColorChannels / channelsPerAvx2Block;
@@ -120,87 +139,96 @@ public:
         const int sse2Block = rest / channelsPerSse2Block;
         const int scalarBlock = rest % channelsPerSse2Block;
 
-        __m256i offset1 = _mm256_set1_epi16(128);
-        __m128i offset2 = _mm_set1_epi16(128);
+        const auto offset1 = uint16_avx_v(128);
+        const auto offset2 = uint16_v(128);
 
-#elif defined __SSE2__
+#elif XSIMD_WITH_SSE2 || XSIMD_WITH_NEON || XSIMD_WITH_NEON64
+        // SSE2, unlike the previous function, is a perfectly valid option
+        // while under generic.
+#if XSIMD_WITH_SSE2
+        using uint16_v = xsimd::batch<uint16_t, xsimd::sse2>;
+#elif XSIMD_WITH_NEON64
+        using uint16_v = xsimd::batch<uint16_t, xsimd::neon64>;
+#else
+        using uint16_v = xsimd::batch<uint16_t, xsimd::neon>;
+#endif
+
         const int channelsPerSse2Block = 16;
         const int avx2Block = 0;
         const int sse2Block = numColorChannels / channelsPerSse2Block;
         const int scalarBlock = numColorChannels % channelsPerSse2Block;
 
-        __m128i offset2 = _mm_set1_epi16(128);
+        const auto offset2 = uint16_v(128);
 #else
         const int avx2Block = 0;
         const int sse2Block = 0;
         const int scalarBlock = numColorChannels;
 #endif
 
-        //qWarning() << ppVar(avx2Block) << ppVar(sse2Block);
+        // qWarning() << ppVar(avx2Block) << ppVar(sse2Block);
 
         for (int row = 0; row < numRows; row++) {
-
-            const quint16 *srcPtr = reinterpret_cast<const quint16*>(src);
+            const quint16 *srcPtr = reinterpret_cast<const quint16 *>(src);
             quint8 *dstPtr = dst;
 
-#ifdef __AVX2__
+#if XSIMD_WITH_AVX2
             for (int i = 0; i < avx2Block; i++) {
+                auto x1 = uint16_avx_v::load_unaligned(srcPtr);
+                auto x2 = uint16_avx_v::load_unaligned(srcPtr + uint16_avx_v::size);
 
-                __m256i x1 = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(srcPtr));
-                __m256i x2 = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(srcPtr + 16));
+                const auto x1_shifted = x1 >> 8;
+                const auto x2_shifted = x2 >> 8;
 
-                __m256i x1_shifted = _mm256_srli_epi16(x1, 8);
-                __m256i x2_shifted = _mm256_srli_epi16(x2, 8);
+                x1 -= x1_shifted;
+                x1 += offset1;
+                x1 >>= 8;
 
-                x1 = _mm256_sub_epi16(x1, x1_shifted);
-                x1 = _mm256_add_epi16(x1, offset1);
-                x1 = _mm256_srli_epi16(x1, 8);
+                x2 -= x2_shifted;
+                x2 += offset1;
+                x2 >>= 8;
 
-                x2 = _mm256_sub_epi16(x2, x2_shifted);
-                x2 = _mm256_add_epi16(x2, offset1);
-                x2 = _mm256_srli_epi16(x2, 8);
-
-                x1 = _mm256_packus_epi16(x1, x2);
+                x1.data = _mm256_packus_epi16(x1, x2);
 
                 // Packing in AVX2 does a bit different thing, not
                 // what you expect that after seeing a SSE2 version :)
                 // Therefore we need to permute the result...
-                x1 = _mm256_permute4x64_epi64(x1, 0xd8);
+                x1.data = _mm256_permute4x64_epi64(x1, 0xd8);
 
-                _mm256_storeu_si256(reinterpret_cast<__m256i*>(dstPtr), x1);
+                x1.store_unaligned(reinterpret_cast<typename uint16_v::value_type *>(dstPtr));
 
                 srcPtr += channelsPerAvx2Block;
                 dstPtr += channelsPerAvx2Block;
             }
 #else
-            Q_UNUSED(avx2Block);
+        Q_UNUSED(avx2Block);
 #endif
 
-#ifdef __SSE2__
+#if XSIMD_WITH_SSE2 || XSIMD_WITH_NEON || XSIMD_WITH_NEON64
             for (int i = 0; i < sse2Block; i++) {
-                __m128i x1 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(srcPtr));
-                __m128i x2 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(srcPtr + 8));
+                auto x1 = uint16_v::load_unaligned(srcPtr);
+                auto x2 = uint16_v::load_unaligned(srcPtr + uint16_v::size);
 
-                __m128i x1_shifted = _mm_srli_epi16(x1, 8);
-                __m128i x2_shifted = _mm_srli_epi16(x2, 8);
+                const uint16_v x1_shifted = x1 >> 8;
+                const uint16_v x2_shifted = x2 >> 8;
 
-                x1 = _mm_sub_epi16(x1, x1_shifted);
-                x1 = _mm_add_epi16(x1, offset2);
-                x1 = _mm_srli_epi16(x1, 8);
+                x1 -= x1_shifted;
+                x1 += offset2;
+                x1 >>= 8;
 
-                x2 = _mm_sub_epi16(x2, x2_shifted);
-                x2 = _mm_add_epi16(x2, offset2);
-                x2 = _mm_srli_epi16(x2, 8);
-
-                x1 = _mm_packus_epi16(x1, x2);
-
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(dstPtr), x1);
-
+                x2 -= x2_shifted;
+                x2 += offset2;
+                x2 >>= 8;
+#if XSIMD_WITH_SSE2
+                x1.data = _mm_packus_epi16(x1, x2);
+#else
+                x1.data = vreinterpretq_u16_u8(vcombine_u8(vqmovun_s16(vreinterpretq_s16_u16(x1)), vqmovun_s16(vreinterpretq_s16_u16(x2))));
+#endif
+                x1.store_unaligned(reinterpret_cast<typename uint16_v::value_type *>(dstPtr));
                 srcPtr += channelsPerSse2Block;
                 dstPtr += channelsPerSse2Block;
             }
 #else
-            Q_UNUSED(sse2Block);
+        Q_UNUSED(sse2Block);
 #endif
 
             for (int i = 0; i < scalarBlock; i++) {
@@ -211,7 +239,6 @@ public:
                 srcPtr++;
                 dstPtr++;
             }
-
 
             src += srcRowStride;
             dst += dstRowStride;

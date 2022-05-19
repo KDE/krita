927878e033099ffef68c93f8c7ee23af4d74ce52
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2022-05-19 10:38:21 +0200

    Implement lengthAdjust="spacingAndGlyphs" for textLength.

--- libs/flake/text/KoSvgTextShape.cpp
+++ libs/flake/text/KoSvgTextShape.cpp
@@ -1088,6 +1088,7 @@ void KoSvgTextShape::Private::applyTextLength(const KoShape *rootShape,
         qreal delta =
             chunkShape->layoutInterface()->textLength().customValue - (b - a);
         QPointF d(delta / n, 0);
+
         if (!isHorizontal) {
             d = QPointF(0, delta / n);
         }
@@ -1096,6 +1097,16 @@ void KoSvgTextShape::Private::applyTextLength(const KoShape *rootShape,
             CharacterResult cr = result[typographicToIndex.value(k)];
             if (cr.addressable) {
                 cr.finalPosition += shift;
+                if (chunkShape->layoutInterface()->lengthAdjust()
+                    == KoSvgText::LengthAdjustSpacingAndGlyphs) {
+                    QPointF scale(
+                        d.x() != 0 ? (d.x() / cr.advance.x()) + 1 : 1.0,
+                        d.y() != 0 ? (d.y() / cr.advance.y()) + 1 : 1.0);
+                    QTransform tf = QTransform::fromScale(scale.x(), scale.y());
+                    cr.path = tf.map(cr.path);
+                    cr.advance = tf.map(cr.advance);
+                    cr.boundingBox = tf.mapRect(cr.boundingBox);
+                }
                 if (!cr.textLengthApplied) {
                     shift += d;
                 }

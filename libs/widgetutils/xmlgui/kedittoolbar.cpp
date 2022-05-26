bb4c285dc396c89e0d640c01d1feb8badb412bbd
Alvin Wong <alvin@alvinhc.com>
2022-05-26 17:47:23 +0800

    Revert "Fix action text being untranslated in Configure Toolbar"
    Now that iconText can be properly translated, we probably can revert to
    it so that the text shown in the right-hand list actually matches that
    when shown on the toolbar.
    
    This text may revert to English until the translations have been
    updated.
    
    This reverts commit 713e98803eaec18a2292d99649c4ce1a72251860.
    
    CCBUG: 448027
    CCBUG: 454167

--- libs/widgetutils/xmlgui/kedittoolbar.cpp
+++ libs/widgetutils/xmlgui/kedittoolbar.cpp
@@ -1223,7 +1223,7 @@ void KisKEditToolBarWidgetPrivate::loadActions(const QDomElement &elem)
             if (it.attribute(attrName) == action->objectName()) {
                 // we have a match!
                 ToolBarItem *act = new ToolBarItem(m_activeList, it.tagName(), action->objectName(), action->toolTip());
-                act->setText(nameFilter.subs(KLocalizedString::removeAcceleratorMarker(action->text())).toString());
+                act->setText(nameFilter.subs(KLocalizedString::removeAcceleratorMarker(action->iconText())).toString());
                 act->setIcon(!action->icon().isNull() ? action->icon() : m_emptyIcon);
                 act->setTextAlongsideIconHidden(action->priority() < QAction::NormalPriority);
 

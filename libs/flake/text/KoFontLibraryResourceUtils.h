0282f23d4b1d570aceb398327d5038e935b77ace
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2022-05-29 15:00:24 +0200

    Add missing license headers.

--- libs/flake/text/KoFontLibraryResourceUtils.h
+++ libs/flake/text/KoFontLibraryResourceUtils.h
@@ -1,3 +1,8 @@
+/*
+ *  SPDX-FileCopyrightText: 2022 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #ifndef KOFONTLIBRARYRESOURCEUTILS_H
 #define KOFONTLIBRARYRESOURCEUTILS_H
 

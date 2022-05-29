ea4fbd2d06f8b3a79d592643b29facdfc7adb7b2
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2022-05-29 15:00:24 +0200

    Add missing license headers.

--- libs/global/KisLibraryResourcePointer.h
+++ libs/global/KisLibraryResourcePointer.h
@@ -1,3 +1,8 @@
+/*
+ *  SPDX-FileCopyrightText: 2022 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #ifndef KISLIBRARYRESOURCEPOINTER_H
 #define KISLIBRARYRESOURCEPOINTER_H
 

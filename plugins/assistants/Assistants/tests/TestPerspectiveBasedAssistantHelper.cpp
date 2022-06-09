37194a5e9c2ec39455bed180b6de78d0925d20ea
Agata Cacko <tamtamy.tymona@gmail.com>
2022-06-09 18:03:45 +0200

    Improve unit test for Perspect.AssistantHelper

--- plugins/assistants/Assistants/tests/TestPerspectiveBasedAssistantHelper.cpp
+++ plugins/assistants/Assistants/tests/TestPerspectiveBasedAssistantHelper.cpp
@@ -12,6 +12,9 @@
 
 #include <kis_painting_assistant.h>
 #include <PerspectiveBasedAssistantHelper.h>
+#include <kis_algebra_2d.h>
+#include <kis_global.h>
+
 
 
 void TestPerspectiveBasedAssistantHelper::testDistanceInGrid()
@@ -22,10 +25,51 @@ void TestPerspectiveBasedAssistantHelper::testDistanceInGrid()
         pointsToCheck << QPointF(0, i);
     }
 
+    QPolygonF poly;
+    bool correct = PerspectiveBasedAssistantHelper::getTetragon(handles, true, poly);
+    ENTER_FUNCTION() << correct;
+
+    PerspectiveBasedAssistantHelper::CacheData cache;
+    PerspectiveBasedAssistantHelper::updateCacheData(cache, poly);
+
+    auto fuzzyCompare = [] (double a, double b) {
+        return qAbs(a - b) < 0.0000001;
+    };
+
+
     for (int i = 0; i < pointsToCheck.size(); i++) {
-        qreal response = PerspectiveBasedAssistantHelper::distanceInGrid(handles, true, pointsToCheck[i]);
-        ENTER_FUNCTION() << ppVar(pointsToCheck[i]) << ppVar(response) << "should be: " << i/8.0;
+        qreal response = PerspectiveBasedAssistantHelper::distanceInGrid(cache, pointsToCheck[i]);
+        QVERIFY(fuzzyCompare(response, i/8.0));
     }
+
+    // let's say: vps in (-10, 0) and (10, 0)
+    // small point:
+    ENTER_FUNCTION() << ppVar(6.0/19) << ppVar(4.0/19);
+    QLineF first = QLineF(QPointF(10, 0), QPointF(-10, 8));
+    QLineF second = QLineF(QPointF(-10, 0), QPointF(0, 13));
+    QPointF inters;
+    if (first.intersect(second, &inters) != QLineF::NoIntersection) {
+        ENTER_FUNCTION() << "Intersection is: " << inters << "and was supposed to be: " << QPointF(-5 - 15.0/19, 6 + 6.0/19);
+    }
+
+
+    handles = getHandles({QPointF(0, 4), inters, QPointF(0, 13), QPointF(-inters.x(), inters.y())});
+    pointsToCheck.clear();
+    for (int i = 0; i <= 13; i++) {
+        pointsToCheck << QPointF(0, i);
+    }
+
+    correct = PerspectiveBasedAssistantHelper::getTetragon(handles, true, poly);
+    ENTER_FUNCTION() << correct;
+
+    PerspectiveBasedAssistantHelper::updateCacheData(cache, poly);
+
+    for (int i = 0; i < pointsToCheck.size(); i++) {
+        qreal response = PerspectiveBasedAssistantHelper::distanceInGrid(cache, pointsToCheck[i]);
+        QVERIFY(fuzzyCompare(response, i/13.0));
+
+    }
+
 }
 
 QList<KisPaintingAssistantHandleSP> TestPerspectiveBasedAssistantHelper::getHandles(QList<QPointF> points)

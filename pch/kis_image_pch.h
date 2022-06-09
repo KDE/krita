066406bfa0d909809b220985d61594864e588f7e
Dmitry Kazakov <dimula73@gmail.com>
2022-06-09 08:29:38 +0300

    Add an options to build Krita with precompiled headers support
    Now there is a CMake option KRITA_ENABLE_PCH (**enabled** by default)
    that makes build process use precompiled headers in a per-library basis.
    It means that every library we build in Krita (we should use kis_add_library
    instead of normal add_library for that) will have its own local PCH file.
    
    This patch doesn't implement PCH support for unittests still. It will be
    a separate patch.

---
+++ pch/kis_image_pch.h
@@ -0,0 +1,10 @@
+#include <QObject>
+#include <QThread>
+#include <QtConcurrent>
+#include <QList>
+#include <QVector>
+#include <QVariant>
+#include <QPainter>
+#include <kis_types.h>
+#include <kis_debug.h>
+#include <kis_global.h>

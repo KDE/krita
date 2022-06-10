311f3388c17c8fbe6322021efc04d75b76992bd9
L. E. Segovia <amy@amyspark.me>
2022-06-10 14:08:21 -0300

    LCMS: fix detection of valid output profiles
    The current code is only valid for ICC v2 profiles, and moreover, only
    for Matrix/CLUT based profiles. Which means that anything more complex
    has been happily ignored for the past 12 years.
    
    This enables using ICC v4 AtoB0 profiles, particularly for the La*b* and
    YCbCr colour spaces.
    
    CCBUG: 230693
    (cherry picked from commit 5fe993cc830552608d608f9e014f743670bea0b2)

--- plugins/color/lcms2engine/colorprofiles/LcmsColorProfileContainer.cpp
+++ plugins/color/lcms2engine/colorprofiles/LcmsColorProfileContainer.cpp
@@ -232,9 +232,9 @@ bool LcmsColorProfileContainer::init()
         }
 
         // Check if the profile can convert (something->this)
-        d->suitableForOutput = cmsIsMatrixShaper(d->profile)
-                               || (cmsIsCLUT(d->profile, INTENT_PERCEPTUAL, LCMS_USED_AS_INPUT) &&
-                                   cmsIsCLUT(d->profile, INTENT_PERCEPTUAL, LCMS_USED_AS_OUTPUT));
+        d->suitableForOutput = cmsIsIntentSupported(d->profile,
+                                                    INTENT_PERCEPTUAL,
+                                                    LCMS_USED_AS_OUTPUT);
 
         d->version = cmsGetProfileVersion(d->profile);
         d->defaultIntent = cmsGetHeaderRenderingIntent(d->profile);

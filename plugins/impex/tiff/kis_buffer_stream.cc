dd4d2b4b793a42b2c7406dfc63fc108ea8905ec5
L. E. Segovia <amy@amyspark.me>
2022-06-12 18:26:16 -0300

    tiff: support YCbCr + JPEG imports
    This commit enables reading JPEG-compressed TIFFs that use the YCbCr
    colour space. This is not possible to do with the standard libtiff API
    because of Very Bad Decisions :tm: that were taken in the past, both
    during the 6.0 standardization [1] and its implementation [2].
    
    To do so, I've implemented a custom made strip decoder that, when the
    combination of COMPRESSION_JPEG and PHOTOMETRIC_YCBCR is detected,
    redirects to a (also custom made) buffer stream that performs automatic
    interleaving and upsampling of the image planes. This data is shaped
    into the form specified in the TIFF 6.0 Specification [3], then fed
    to the existing YCbCr readers. The planes are decoded using
    libjpeg-turbo (to avoid the mindbending API of libjpeg) into three
    separate planes without upsampling.
    
    There's a catch, however. Unlike libjpeg-turbo, libjpeg provides a
    direct API to prime the decoder with the coefficient tables that are
    provided in TIFFTAG_JPEGTABLES, and which libtiff uses as part of
    TIFFjpeg_read_header. To remediate this, I've patched libtiff-turbo to
    add a tjReadCoefficientTables API, that takes a reference to the tables
    and forwards it to the decoder.
    
    BUG: 454116
    
    CCMAIL: kimageshop@kde.org
    
    [1]: DRAFT TIFF Technical Note #2, 17-Mar-95.
    <http://libtiff.maptools.org/TIFFTechNote2.html>
    
    [2]: 2022.06.11 07:23 "Re: [Tiff] How to read JPEG-compressed YCbCr
    TIFFs", by Joris Van Damme. <https://www.asmail.be/msg0054881320.html>
    
    [3]: "Ordering of Component Samples". In: TIFF Revision 6.0 Final. June
    3, 1992: Adobe Developers Association, pp. 93-94.

--- plugins/impex/tiff/kis_buffer_stream.cc
+++ plugins/impex/tiff/kis_buffer_stream.cc
@@ -26,13 +26,39 @@ KisBufferStreamContigBase::KisBufferStreamContigBase(uint8_t *src,
 void KisBufferStreamContigBase::restart()
 {
     m_srcIt = m_src;
+    m_lineOffset = 0;
+    m_lineNumber = 0;
     m_posinc = 8;
 }
 
 void KisBufferStreamContigBase::moveToLine(tsize_t lineNumber)
 {
-    m_srcIt = m_src + lineNumber * m_lineSize;
+    KIS_ASSERT(lineNumber >= 0);
+    moveToPos(0, lineNumber);
+}
+
+void KisBufferStreamContigBase::moveToPos(tsize_t x, tsize_t y)
+{
+    KIS_ASSERT(x >= 0 && y >= 0);
+    m_lineNumber = y;
+    m_lineOffset = (x * m_depth) / 8;
+    m_srcIt = m_src + y * m_lineSize + m_lineOffset;
     m_posinc = 8;
+}
+
+tsize_t KisBufferStreamContigBase::x() const
+{
+    return (m_lineOffset * 8) / m_depth;
+}
+
+tsize_t KisBufferStreamContigBase::y() const
+{
+    return m_lineNumber;
+}
+
+tsize_t KisBufferStreamContigBase::width() const
+{
+    return (m_lineSize * 8) / m_depth;
 }
 
 uint32_t KisBufferStreamContigBelow16::nextValue()
@@ -47,8 +73,13 @@ uint32_t KisBufferStreamContigBelow16::nextValue()
         value = (value << toread) | (((*m_srcIt) >> (m_posinc)) & ((1 << toread) - 1));
         if (m_posinc == 0) {
             m_srcIt++;
+            m_lineOffset++; // we consumed a byte
             m_posinc = 8;
         }
+    }
+    if (m_lineOffset >= m_lineSize) {
+        m_lineNumber += 1;
+        m_lineOffset = 0;
     }
     return value;
 }
@@ -65,8 +96,13 @@ uint32_t KisBufferStreamContigBelow32::nextValue()
         value = (value) | ((((*m_srcIt) >> (m_posinc)) & ((1 << toread) - 1U)) << (m_depth - 8U - remain));
         if (m_posinc == 0) {
             m_srcIt++;
+            m_lineOffset++; // we consumed a byte
             m_posinc = 8U;
         }
+    }
+    if (m_lineOffset >= m_lineSize) {
+        m_lineNumber += 1;
+        m_lineOffset = 0;
     }
     return value;
 }
@@ -86,8 +122,13 @@ uint32_t KisBufferStreamContigAbove32::nextValue()
         }
         if (m_posinc == 0) {
             m_srcIt++;
+            m_lineOffset++; // we consumed a byte
             m_posinc = 8U;
         }
+    }
+    if (m_lineOffset >= m_lineSize) {
+        m_lineNumber += 1;
+        m_lineOffset = 0;
     }
     return value;
 }
@@ -142,7 +183,102 @@ void KisBufferStreamSeparate::restart()
 
 void KisBufferStreamSeparate::moveToLine(tsize_t lineNumber)
 {
+    KIS_ASSERT(lineNumber >= 0);
+    moveToPos(0, lineNumber);
+}
+
+void KisBufferStreamSeparate::moveToPos(tsize_t x, tsize_t y)
+{
     for (const auto &stream : streams) {
-        stream->moveToLine(lineNumber);
+        stream->moveToPos(x, y);
     }
+}
+
+tsize_t KisBufferStreamSeparate::x() const
+{
+    return streams[m_current_sample]->x();
+}
+
+tsize_t KisBufferStreamSeparate::y() const
+{
+    return streams[m_current_sample]->y();
+}
+
+tsize_t KisBufferStreamSeparate::width() const
+{
+    return streams[m_current_sample]->width();
+}
+
+KisBufferStreamInterleaveUpsample::KisBufferStreamInterleaveUpsample(
+    uint8_t **srcs,
+    uint16_t nb_samples,
+    uint16_t depth,
+    tsize_t *lineSize,
+    uint16_t hsubsample,
+    uint16_t vsubsample)
+    : KisBufferStreamSeparate(srcs, nb_samples, depth, lineSize)
+    , m_hsubsample(hsubsample)
+    , m_vsubsample(vsubsample)
+{
+}
+
+uint32_t KisBufferStreamInterleaveUpsample::nextValue()
+{
+    uint32_t value = streams[m_currentPlane]->nextValue();
+    if (m_currentPlane == 0) {
+        m_current_sample++;
+        if (m_current_sample % m_hsubsample == 0) {
+            if (m_current_sample >= m_hsubsample * m_vsubsample) {
+                // Fix up the position of the luminance plane
+                // If it's already 0, the cursor has already looped (correctly)
+                // to the next line
+                if (streams[m_currentPlane]->x() != 0) {
+                    streams[m_currentPlane]->moveToPos(
+                        streams[m_currentPlane]->x(),
+                        streams[m_currentPlane]->y() - m_vsubsample + 1);
+                }
+                // Move to Cb/Cr
+                m_currentPlane += 1;
+                m_current_sample = 0;
+            } else {
+                // Go to next line
+                // If the position is already 0, we need to correct the row
+                // AND column
+                if (streams[m_currentPlane]->x() != 0) {
+                    streams[m_currentPlane]->moveToPos(
+                        streams[m_currentPlane]->x() - m_hsubsample,
+                        streams[m_currentPlane]->y() + 1);
+                } else {
+                    streams[m_currentPlane]->moveToPos(
+                        streams[m_currentPlane]->width() - m_hsubsample,
+                        streams[m_currentPlane]->y());
+                }
+            }
+        }
+    } else if (m_currentPlane < m_nb_samples - 1) {
+        m_currentPlane += 1;
+    } else {
+        m_currentPlane = 0;
+    }
+    return value;
+}
+
+void KisBufferStreamInterleaveUpsample::moveToPos(tsize_t x, tsize_t y)
+{
+    // Needs to subsample
+    for (uint16_t i = 0; i < m_nb_samples; i++) {
+        const tsize_t realX = i == 0 ? x : x / m_hsubsample;
+        const tsize_t realY = i == 0 ? y : y / m_vsubsample;
+        streams.at(i)->moveToPos(realX, realY);
+    }
+}
+
+tsize_t KisBufferStreamInterleaveUpsample::x() const
+{
+    return streams[0]->x();
+}
+
+tsize_t KisBufferStreamInterleaveUpsample::y() const
+{
+    return streams[0]->y();
 }
--- plugins/impex/tiff/kis_buffer_stream.h
+++ plugins/impex/tiff/kis_buffer_stream.h
@@ -23,7 +23,11 @@ public:
     virtual uint32_t nextValue() = 0;
     virtual void restart() = 0;
     virtual void moveToLine(tsize_t lineNumber) = 0;
-    virtual ~KisBufferStreamBase() {}
+    virtual void moveToPos(tsize_t x, tsize_t y) = 0;
+    virtual tsize_t x() const = 0;
+    virtual tsize_t y() const = 0;
+    virtual tsize_t width() const = 0;
+
 protected:
     uint16_t m_depth;
 };
@@ -39,11 +43,21 @@ public:
 
     void moveToLine(tsize_t lineNumber) override;
 
+    void moveToPos(tsize_t x, tsize_t y) override;
+
+    tsize_t x() const override;
+
+    tsize_t y() const override;
+
+    tsize_t width() const override;
+
 protected:
     uint8_t *const m_src;
     uint8_t *m_srcIt;
     uint16_t m_posinc = 0;
     const tsize_t m_lineSize;
+    tsize_t m_lineNumber = 0;
+    tsize_t m_lineOffset = 0;
 };
 
 class KisBufferStreamContigBelow16 : public KisBufferStreamContigBase
@@ -98,10 +112,52 @@ public:
 
     void moveToLine(tsize_t lineNumber) override;
 
+    void moveToPos(tsize_t x, tsize_t y) override;
+
+    tsize_t x() const override;
+
+    tsize_t y() const override;
+
+    tsize_t width() const override;
+
 protected:
     QVector<QSharedPointer<KisBufferStreamBase>> streams;
     uint16_t m_current_sample = 0;
     uint16_t m_nb_samples;
+};
+
+class KisBufferStreamInterleaveUpsample : public KisBufferStreamSeparate
+{
+public:
+    KisBufferStreamInterleaveUpsample(uint8_t **srcs,
+                                      uint16_t nb_samples,
+                                      uint16_t depth,
+                                      tsize_t *lineSize,
+                                      uint16_t hsubsample,
+                                      uint16_t vsubsample);
+
+    uint32_t nextValue() override;
+
+    void moveToPos(tsize_t x, tsize_t y) override;
+
+    tsize_t x() const override;
+
+    tsize_t y() const override;
+
+    tsize_t width() const override
+    {
+        return streams[0]->width();
+    }
+
+    void restart() override
+    {
+        KisBufferStreamSeparate::restart();
+        m_currentPlane = 0;
+    }
+
+protected:
+    uint16_t m_hsubsample, m_vsubsample;
+    uint16_t m_currentPlane = 0;
 };
 
 #endif

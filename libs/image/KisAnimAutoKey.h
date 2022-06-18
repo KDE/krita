f133aa8501519ca0cedcc96654ab2aa649e750cc
L. E. Segovia <amy@amyspark.me>
2022-06-17 21:06:39 -0300

    KisAnimAutoKey: fix instantiation on Windows
    (cherry picked from commit 6e6289b88ca18b7970f83025595b010b861bc20a)

--- libs/image/KisAnimAutoKey.h
+++ libs/image/KisAnimAutoKey.h
@@ -8,23 +8,17 @@
 #ifndef _KIS_ANIM_AUTOKEY_H
 #define _KIS_ANIM_AUTOKEY_H
 
-#include "kis_image_config.h"
 #include "kritaimage_export.h"
 
 namespace KisAutoKey {
-    enum KRITAIMAGE_EXPORT Mode {
-        NONE,     // AutoKey is disabled. Keyframes must be created manually.
-        BLANK,    // AutoKey creates an empty/blank frame. Acts like DUPLICATE when not applicable (i.e.: filters, transforms, etc.).
-        DUPLICATE // AutoKey will duplicate the active frame.
-    };
+enum Mode {
+    NONE, // AutoKey is disabled. Keyframes must be created manually.
+    BLANK, // AutoKey creates an empty/blank frame. Acts like DUPLICATE when not
+           // applicable (i.e.: filters, transforms, etc.).
+    DUPLICATE // AutoKey will duplicate the active frame.
+};
 
-    inline KRITAIMAGE_EXPORT Mode activeMode() {
-        KisImageConfig cfg(true);
-        if (cfg.autoKeyEnabled()) {
-            return cfg.autoKeyModeDuplicate() ? KisAutoKey::DUPLICATE : KisAutoKey::BLANK;
-        }
-        return KisAutoKey::NONE;
-    }
+KRITAIMAGE_EXPORT Mode activeMode();
 }
 
 #endif //_KIS_ANIM_AUTOKEY_H

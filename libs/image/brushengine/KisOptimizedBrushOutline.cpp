e3c1c2d6756bc50fa5f4ecd8d15a1e78d42e9304
Dmitry Kazakov <dimula73@gmail.com>
2022-07-01 12:10:10 +0300

    Add license headers

--- libs/image/brushengine/KisOptimizedBrushOutline.cpp
+++ libs/image/brushengine/KisOptimizedBrushOutline.cpp
@@ -1,3 +1,9 @@
+/*
+ *  SPDX-FileCopyrightText: 2022 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #include "KisOptimizedBrushOutline.h"
 
 #include <QPainterPath>
--- libs/image/brushengine/KisOptimizedBrushOutline.h
+++ libs/image/brushengine/KisOptimizedBrushOutline.h
@@ -1,3 +1,9 @@
+/*
+ *  SPDX-FileCopyrightText: 2022 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #ifndef KISOPTIMIZEDBRUSHOUTLINE_H
 #define KISOPTIMIZEDBRUSHOUTLINE_H
 

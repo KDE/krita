21b69e56a7a836560eb9cd19b25c11b69aba5d88
Moses Miller <Majora320@gmail.com>
2022-07-10 13:04:40 -0700

    Increase maximum pattern fill scale to 10000%

--- plugins/tools/basictools/kis_tool_fill.cc
+++ plugins/tools/basictools/kis_tool_fill.cc
@@ -353,7 +353,8 @@ QWidget* KisToolFill::createOptionWidget()
         optionButtonStripFillWith->addButton(KisIconUtils::loadIcon("pattern"));
     m_buttonFillWithFG->setChecked(true);
     m_sliderPatternScale = new KisDoubleSliderSpinBox;
-    m_sliderPatternScale->setRange(0, 500, 2);
+    m_sliderPatternScale->setRange(0, 10000, 2);
+    m_sliderPatternScale->setSoftMaximum(500);
     m_sliderPatternScale->setPrefix(i18nc("The pattern 'scale' spinbox prefix in fill tool options", "Scale: "));
     m_sliderPatternScale->setSuffix(i18n("%"));
     m_angleSelectorPatternRotation = new KisAngleSelector;

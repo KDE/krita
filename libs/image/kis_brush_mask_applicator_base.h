172fc5d2dce10e7baedfdb7a2c8f70a78695ff76
L. E. Segovia <amy@amyspark.me>
2022-08-05 15:31:35 -0300

    clang-tidy KisBrushMaskApplicatorBase

--- libs/image/kis_brush_mask_applicator_base.h
+++ libs/image/kis_brush_mask_applicator_base.h
@@ -1,5 +1,6 @@
 /*
  *  SPDX-FileCopyrightText: 2012 Dmitry Kazakov <dimula73@gmail.com>
+ *  SPDX-FileCopyrightText: 2022 L. E. Segovia <amy@amyspark.me>
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  */
@@ -7,34 +8,32 @@
 #ifndef __KIS_BRUSH_MASK_APPLICATOR_BASE_H
 #define __KIS_BRUSH_MASK_APPLICATOR_BASE_H
 
-#include "kis_types.h"
-#include "kis_fixed_paint_device.h"
-#include "math.h"
+#include <cmath>
 
+#include "kis_fixed_paint_device.h"
+#include "kis_types.h"
 
 struct MaskProcessingData {
     MaskProcessingData(KisFixedPaintDeviceSP _device,
-                       const KoColorSpace* _colorSpace,
-                       const quint8* _color,
+                       const KoColorSpace *_colorSpace,
+                       const quint8 *_color,
                        qreal _randomness,
                        qreal _density,
                        double _centerX,
                        double _centerY,
                        double _angle)
-        {
-            device = _device;
-            colorSpace = _colorSpace;
-            color = _color;
-            randomness = _randomness;
-            density = _density;
-            centerX = _centerX;
-            centerY = _centerY;
-            cosa = cos(_angle);
-            sina = sin(_angle);
-            pixelSize = colorSpace->pixelSize();
-        }
-
-
+        : device(_device)
+        , colorSpace(_colorSpace)
+        , color(_color)
+        , randomness(_randomness)
+        , density(_density)
+        , centerX(_centerX)
+        , centerY(_centerY)
+        , cosa(std::cos(_angle))
+        , sina(std::sin(_angle))
+        , pixelSize(colorSpace->pixelSize())
+    {
+    }
 
     KisFixedPaintDeviceSP device;
     const KoColorSpace* colorSpace;
@@ -47,13 +46,13 @@ struct MaskProcessingData {
     double cosa;
     double sina;
 
-    qint32 pixelSize;
+    quint32 pixelSize;
 };
 
 class KisBrushMaskApplicatorBase
 {
 public:
-    virtual ~KisBrushMaskApplicatorBase() {}
+    virtual ~KisBrushMaskApplicatorBase() = default;
     virtual void process(const QRect &rect) = 0;
 
     inline void initializeData(const MaskProcessingData *data) {
@@ -61,14 +60,15 @@ public:
     }
 
 protected:
-    const MaskProcessingData *m_d;
+    const MaskProcessingData *m_d = nullptr;
 };
 
 struct OperatorWrapper {
     OperatorWrapper(KisBrushMaskApplicatorBase *applicator)
         : m_applicator(applicator) {}
 
-    inline void operator() (const QRect& rect) {
+    inline void operator()(const QRect &rect) const
+    {
         m_applicator->process(rect);
     }
 

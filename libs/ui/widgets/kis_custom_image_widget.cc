138a77c103b6288e22a96fd5ea84e62ac861fc14
L. E. Segovia <amy@amyspark.me>
2022-10-25 13:31:33 -0300

    KisCustomImageWidget, KisImageFromClipboardWidget: remove spurious handling of the clipboard group spacer
    These are already handled by the group container, manually setting the
    policy is completely useless.

--- libs/ui/widgets/kis_custom_image_widget.cc
+++ libs/ui/widgets/kis_custom_image_widget.cc
@@ -91,7 +91,6 @@ KisCustomImageWidget::KisCustomImageWidget(QWidget* parent, qint32 defWidth, qin
     doubleResolution->setValue(72.0 * resolution);
     doubleResolution->setDecimals(2);
 
-    imageGroupSpacer->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
     grpClipboard->hide();
 
     sliderOpacity->setRange(0, 100, 0);

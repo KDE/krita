96811144b450d1f1ba99733a8ff5b237860f54c8
Agata Cacko <tamtamy.tymona@gmail.com>
2022-11-13 21:52:09 +0100

    Still the same result 13

--- plugins/assistants/Assistants/EllipseInPolygon.cpp
+++ plugins/assistants/Assistants/EllipseInPolygon.cpp
@@ -1409,7 +1409,10 @@ QPointF EllipseInPolygon::projectModifiedEberlySecond(QPointF point)
     // Q - orthogonal, D - diagonal
     //
 
-    auto rotateLikeArticleSays = [f, debug] () mutable {
+
+    Eigen::Matrix<long double, 2, 2> Q;
+
+    auto rotateLikeArticleSays = [debug] (Formulas& f, Eigen::Matrix<long double, 2, 2>& Q) mutable {
         Eigen::Matrix<long double, 2, 2> M;
         M << f.formCCanonized.A, f.formCCanonized.B,
              f.formCCanonized.B, f.formCCanonized.C;
@@ -1436,8 +1439,6 @@ QPointF EllipseInPolygon::projectModifiedEberlySecond(QPointF point)
             }
         }
 
-
-        Eigen::Matrix<long double, 2, 2> Q;
         Eigen::Matrix<long double, 2, 2> Dm;
 
         if (debug) ENTER_FUNCTION() << "(4)";
@@ -1481,91 +1482,14 @@ QPointF EllipseInPolygon::projectModifiedEberlySecond(QPointF point)
         f.formDRotated.E = DEvec[1]; // E
         f.formDRotated.F = f.formCCanonized.F; // F
 
-        f.formDRotated.Name = "form D - rotated (original article version)";
+        f.formDRotated.Name = "form D - rotated (original article version) (cf)";
         f.formDRotated.Type = ConicFormula::SPECIAL;
     };
 
 
-    // -----
-    Eigen::Matrix<long double, 2, 2> M;
-    M << f.formCCanonized.A, f.formCCanonized.B,
-         f.formCCanonized.B, f.formCCanonized.C;
-    if (debug) ENTER_FUNCTION() << "(1)";
+    rotateLikeArticleSays(f, Q);
 
-    Eigen::EigenSolver<Eigen::Matrix<long double, 2, 2>> eigenSolver(M);
-    if (debug) ENTER_FUNCTION() << "(2)";
-
-    Eigen::Matrix<std::complex<long double>, 2, 2> QC = eigenSolver.eigenvectors();
-    Eigen::Matrix<std::complex<long double>, 2, 2> DC;
-    if (debug) ENTER_FUNCTION() << "(3)a";
-    DC(0, 0) = eigenSolver.eigenvalues()(0);
-    DC(1, 1) = eigenSolver.eigenvalues()(1);
-    if (debug) ENTER_FUNCTION() << "(3)";
-
-    Eigen::Matrix<std::complex<long double>, 2, 2> cRM = QC*DC*QC.transpose();
-    if (debug) ENTER_FUNCTION() << "Check correctness of the eigen decomposition: ";
-    if (debug) {
-        for (int i = 0; i < 2; i++)  {
-            for (int j = 0; j < 2; j++) {
-                ENTER_FUNCTION() << ppVar(i) << ppVar(j) << "|" << ppVar((double)M(i, j)) << ppVar((double)std::real(cRM(i, j)))
-                                 << ppVar((bool)(M(i, j) == std::real(cRM(i, j)))) << ppVar((double)(qAbs(M(i, j) - std::real(cRM(i, j)))));
-            }
-        }
-    }
-
-
-    Eigen::Matrix<long double, 2, 2> Q;
-    Eigen::Matrix<long double, 2, 2> Dm;
-
-    if (debug) ENTER_FUNCTION() << "(4)";
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (std::imag(DC(i, j)) == 0 && std::imag(QC(i, j)) == 0) {
-                Dm(i, j) = std::real(DC(i, j));
-                Q(i, j) = std::real(QC(i, j));
-            } else { // if any of that is complex, then something went wrong
-                return originalPoint;
-            }
-        }
-    }
-
-    if (debug) ENTER_FUNCTION() << "D = " << (double)Dm(0, 0) << (double)Dm(0, 1) << (double)Dm(1, 0) << (double)Dm(1, 1);
     if (debug) ENTER_FUNCTION() << "Q = " << (double)Q(0, 0) << (double)Q(0, 1) << (double)Q(1, 0) << (double)Q(1, 1);
-
-
-    if (debug) ENTER_FUNCTION() << "(5)";
-
-
-
-    auto vectorMultipliedByMatrix = [debug] (QVector<double> vector2, Eigen::Matrix<long double, 2, 2> matrix) {
-        QVector<double> res;
-        res << vector2[0]*matrix(0, 0) + vector2[1]*matrix(0, 1);
-        res << vector2[0]*matrix(1, 0) + vector2[1]*matrix(1, 1);
-        return res;
-    };
-
-
-    f.formDRotated.A = Dm(0, 0); // A
-    f.formDRotated.B = 0; // D(1, 0); // B
-    f.formDRotated.C = Dm(1, 1); // C
-
-    QVector<double> DEvec;
-    DEvec << f.formCCanonized.D << f.formCCanonized.E;
-    DEvec = vectorMultipliedByMatrix(DEvec, Q);
-
-    f.formDRotated.D = DEvec[0]; // D
-    f.formDRotated.E = DEvec[1]; // E
-    f.formDRotated.F = f.formCCanonized.F; // F
-
-    f.formDRotated.Name = "form D - rotated (original article version)";
-    f.formDRotated.Type = ConicFormula::SPECIAL;
-
-
-    // -----
-
-
-    rotateLikeArticleSays();
 
 
 
@@ -1894,6 +1818,19 @@ QPointF EllipseInPolygon::projectModifiedEberlySecond(QPointF point)
         ENTER_FUNCTION() << "The values were: " << ppVar(polygon) << ppVar(originalPoint) << "and unfortunate result:" << ppVar(result);
     }
     //KIS_ASSERT_RECOVER_RETURN_VALUE(qAbs(calculateFormula(result)) < eps, result);
+
+    if (debug) {
+        f.formA.printOutInAllForms();
+        f.formBNormalized.printOutInAllForms();
+        f.formCCanonized.printOutInAllForms();
+        f.formDRotated.printOutInAllForms();
+        f.formDRotatedSecondVersion.printOutInAllForms();
+        f.formEMovedToOrigin.printOutInAllForms();
+        f.formFSwappedXY.printOutInAllForms();
+        f.formGNegatedAllSigns.printOutInAllForms();
+        f.formHNegatedX.printOutInAllForms();
+        f.formINegatedY.printOutInAllForms();
+    }
 
 
     return result;

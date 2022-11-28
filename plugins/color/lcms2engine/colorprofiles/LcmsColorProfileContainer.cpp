f2335c36dc85621e0284ecf688621a66f55e67b2
Rasyuqa A. H <qampidh@gmail.com>
2022-11-28 15:09:18 +0700

    LCMS: fix ICC color primaries and whitepoint detection
    (cherry picked from commit 8e409aa546b28082c71bc71dc6ee992edcd9a093)

--- plugins/color/lcms2engine/colorprofiles/LcmsColorProfileContainer.cpp
+++ plugins/color/lcms2engine/colorprofiles/LcmsColorProfileContainer.cpp
@@ -147,6 +147,8 @@ bool LcmsColorProfileContainer::init()
         //present. This is necessary for profiles following the v4 spec.
         cmsCIEXYZ baseMediaWhitePoint;//dummy to hold copy of mediawhitepoint if this is modified by chromatic adaption.
         cmsCIEXYZ *mediaWhitePointPtr;
+        bool whiteComp[3];
+        bool whiteIsD50;
         // Possible bug in profiles: there are in fact some that says they contain that tag
         //    but in fact the pointer is null.
         //    Let's not crash on it anyway, and assume there is no white point instead.
@@ -156,10 +158,17 @@ bool LcmsColorProfileContainer::init()
 
             d->mediaWhitePoint = *(mediaWhitePointPtr);
             baseMediaWhitePoint = d->mediaWhitePoint;
+
+            whiteComp[0] = std::fabs(baseMediaWhitePoint.X - cmsD50_XYZ()->X) < 0.00001;
+            whiteComp[1] = std::fabs(baseMediaWhitePoint.Y - cmsD50_XYZ()->Y) < 0.00001;
+            whiteComp[2] = std::fabs(baseMediaWhitePoint.Z - cmsD50_XYZ()->Z) < 0.00001;
+            whiteIsD50 = std::all_of(std::begin(whiteComp), std::end(whiteComp), [](bool b) {return b;});
+
             cmsXYZ2xyY(&d->whitePoint, &d->mediaWhitePoint);
             cmsCIEXYZ *CAM1;
             if (cmsIsTag(d->profile, cmsSigChromaticAdaptationTag)
-                    && (CAM1 = (cmsCIEXYZ *)cmsReadTag(d->profile, cmsSigChromaticAdaptationTag))) {
+                    && (CAM1 = (cmsCIEXYZ *)cmsReadTag(d->profile, cmsSigChromaticAdaptationTag))
+                    && whiteIsD50) {
                 //the chromatic adaption tag represent a matrix from the actual white point of the profile to D50.
 
                 //We first put all our data into structures we can manipulate.
@@ -199,9 +208,9 @@ bool LcmsColorProfileContainer::init()
             tempColorants.Green = *tempColorantsGreen;
             tempColorants.Blue = *tempColorantsBlue;
             //convert to d65, this is useless.
-            cmsAdaptToIlluminant(&d->colorants.Red, &baseMediaWhitePoint, &d->mediaWhitePoint, &tempColorants.Red);
-            cmsAdaptToIlluminant(&d->colorants.Green, &baseMediaWhitePoint, &d->mediaWhitePoint, &tempColorants.Green);
-            cmsAdaptToIlluminant(&d->colorants.Blue, &baseMediaWhitePoint, &d->mediaWhitePoint, &tempColorants.Blue);
+            cmsAdaptToIlluminant(&d->colorants.Red, cmsD50_XYZ(), &d->mediaWhitePoint, &tempColorants.Red);
+            cmsAdaptToIlluminant(&d->colorants.Green, cmsD50_XYZ(), &d->mediaWhitePoint, &tempColorants.Green);
+            cmsAdaptToIlluminant(&d->colorants.Blue, cmsD50_XYZ(), &d->mediaWhitePoint, &tempColorants.Blue);
             //d->colorants = tempColorants;
             d->hasColorants = true;
         } else {

b8b042041f7814fe36c5a9d227bc4da570c99ad4
Halla Rempt <halla@valdyas.org>
2022-12-01 16:11:18 +0100

    Add copyright header

--- libs/resourcewidgets/ResourceListViewModes.h
+++ libs/resourcewidgets/ResourceListViewModes.h
@@ -1,3 +1,10 @@
+/* This file is part of the Krita project
+ *
+ * SPDX-FileCopyrightText: 2022 Măcelaru Tiberiu <macelarutiberiu@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
 #ifndef RESOURCELISTVIEWMODES_H
 #define RESOURCELISTVIEWMODES_H
 

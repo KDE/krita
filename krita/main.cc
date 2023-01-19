f6a428efddd6ca1a75401fd8691cb15a13143fc7
Dmitry Kazakov <dimula73@gmail.com>
2023-01-19 11:57:13 +0300

    Revert "krita: Use nullptr instead of 0"
    This reverts commit 0d250b2a46e2190abf92842501589db3def4a435.

--- krita/main.cc
+++ krita/main.cc
@@ -523,9 +523,7 @@ extern "C" MAIN_EXPORT int MAIN_FN(int argc, char **argv)
     installTranslators(app);
 
     if (KisApplication::platformName() == "wayland") {
-        QMessageBox::critical(nullptr,
-                              i18nc("@title:window", "Fatal Error"),
-                              i18n("Krita does not support the Wayland platform. Use XWayland to run Krita on Wayland. Krita will close now."));
+        QMessageBox::critical(0, i18nc("@title:window", "Fatal Error"), i18n("Krita does not support the Wayland platform. Use XWayland to run Krita on Wayland. Krita will close now."));
         return -1;
     }
 

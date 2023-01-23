8ca74839bf346821a93a9612e5689ec0d2eee3df
Dmitry Kazakov <dimula73@gmail.com>
2023-01-23 15:45:59 +0300

    Add documentation to the future support functions

--- libs/global/KisFutureUtils.h
+++ libs/global/KisFutureUtils.h
@@ -8,6 +8,12 @@
 
 namespace kismpl {
 
+/**
+ * Create a future whose value has already been evaluated
+ *
+ * See rejected C++ proposal for details:
+ * https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3721.pdf
+ */
 template <typename T>
 std::future<std::decay_t<T>> make_ready_future(T &&value) {
     std::promise<T> promise;
@@ -21,6 +27,15 @@ std::future<void> make_ready_future() {
     return promise.get_future();
 }
 
+/**
+ * Execute a given function \p func when the provided
+ * future \p future is completed. The future is not
+ * deferefenced outside the passed function to avoid
+ * spilling the exceptions.
+ *
+ * See rejected C++ proposal for details:
+ * https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3721.pdf
+ */
 template<class T, class Function>
 auto then(std::future<T>&& future, Function&& func)
     -> std::future<decltype(func(std::move(future)))>

e659b1ba3bf3c67e4006cf0243160ddd4c43075a
Halla Rempt <halla@valdyas.org>
2023-01-27 15:32:01 +0100

    fix copyright

--- libs/widgets/tests/TestKisPaletteModel.cpp
+++ libs/widgets/tests/TestKisPaletteModel.cpp
@@ -1,5 +1,5 @@
 /*
- *  SPDX-FileCopyrightText: 2020 Agata Cacko <cacko.azh@gmail.com>
+ *  SPDX-FileCopyrightText: 2022 Halla Rempt <halla@valdyas.org>
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  */

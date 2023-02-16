f5595e09a3b90815ac6c9b7ce6b75b09bb9ea690
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2023-02-16 13:18:43 +0100

    Update to Raqm 0.10.0
    Raqm 0.10.0 contains all our upstreamed patches so that makes things easier.
    
    CCMAIL:kimageshop@kde.org
    BUG:465773
    
    Part-of: <https://invent.kde.org/graphics/krita/-/merge_requests/1740>

--- libs/flake/text/KoSvgTextShape.cpp
+++ libs/flake/text/KoSvgTextShape.cpp
@@ -558,7 +558,6 @@ void KoSvgTextShape::relayout() const
             if (!letterSpacing.isAuto) {
                 raqm_set_letter_spacing_range(layout.data(),
                                               static_cast<int>(letterSpacing.customValue * ftFontUnit * scaleToPixel),
-                                              false,
                                               static_cast<size_t>(start),
                                               static_cast<size_t>(length));
             }
@@ -566,7 +565,6 @@ void KoSvgTextShape::relayout() const
             if (!wordSpacing.isAuto) {
                 raqm_set_word_spacing_range(layout.data(),
                                             static_cast<int>(wordSpacing.customValue * ftFontUnit * scaleToPixel),
-                                            false,
                                             static_cast<size_t>(start),
                                             static_cast<size_t>(length));
             }

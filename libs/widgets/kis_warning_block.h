f25e331a265dcda1da0a6b43936e3c4be35ede71
L. E. Segovia <amy@amyspark.me>
2023-02-16 08:37:07 -0300

    widgets: Add warning box
    Part-of: <https://invent.kde.org/graphics/krita/-/merge_requests/1742>

---
+++ libs/widgets/kis_warning_block.h
@@ -0,0 +1,40 @@
+/*
+ *  SPDX-FileCopyrightText: 2023 L. E. Segovia <amy@amyspark.me>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef KIS_WARNING_BLOCK_H
+#define KIS_WARNING_BLOCK_H
+
+#include <QGroupBox>
+
+#include "kritawidgets_export.h"
+
+class KRITAWIDGETS_EXPORT KisWarningBlock : public QGroupBox
+{
+    Q_OBJECT
+public:
+    Q_PROPERTY(QString text READ text WRITE setText)
+    Q_PROPERTY(QPixmap pixmap READ pixmap WRITE setPixmap)
+
+    KisWarningBlock(QWidget *parent = nullptr);
+    ~KisWarningBlock() override;
+
+    QString text() const;
+    QPixmap pixmap() const;
+
+    void setText(const QString &);
+    void setPixmap(const QPixmap &);
+
+Q_SIGNALS:
+    void linkActivated(const QString &link);
+
+private:
+    Q_DISABLE_COPY_MOVE(KisWarningBlock)
+
+    struct Private;
+    QScopedPointer<Private> m_d;
+};
+
+#endif

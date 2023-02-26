e71f490044101fd51619897324c76799e55b079d
L. E. Segovia <amy@amyspark.me>
2023-02-26 16:52:31 -0300

    VideoImport: don't query the file to import immediately on dialog open
    That avoids leaving the user with an empty dialog.
    
    Part-of: <https://invent.kde.org/graphics/krita/-/merge_requests/1772>

--- libs/ui/animation/KisDlgImportVideoAnimation.cpp
+++ libs/ui/animation/KisDlgImportVideoAnimation.cpp
@@ -148,9 +148,6 @@ KisDlgImportVideoAnimation::KisDlgImportVideoAnimation(KisMainWindow *mainWindow
     connect(m_ui.ffmpegPickerButton, SIGNAL(clicked()), SLOT(slotFFMpegFile()));
 
     connect(m_ui.exportDurationSpinbox, SIGNAL(valueChanged(qreal)), SLOT(slotImportDurationChanged(qreal)));
-
-    slotAddFile();
-
 }
 
 KisPropertiesConfigurationSP KisDlgImportVideoAnimation::loadLastUsedConfiguration(QString configurationID) {

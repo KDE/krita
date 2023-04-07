f3dc97c4c9d2e77db4d54e8d733720bed7302465
Dmitry Kazakov <dimula73@gmail.com>
2023-04-07 16:46:05 +0200

    Add mlt to windows build scripts

--- build-tools/windows/build-msvc.cmd
+++ build-tools/windows/build-msvc.cmd
@@ -946,7 +946,7 @@ set EXT_TARGETS=%EXT_TARGETS% python sip pyqt
 set EXT_TARGETS=%EXT_TARGETS% lzma quazip openjpeg libde265 libx265 libheif
 set EXT_TARGETS=%EXT_TARGETS% seexpr mypaint webp jpegxl xsimd
 set EXT_TARGETS=%EXT_TARGETS% freetype fontconfig poppler fribidi raqm unibreak
-set EXT_TARGETS=%EXT_TARGETS% ffmpeg lager
+set EXT_TARGETS=%EXT_TARGETS% ffmpeg lager mlt
 
 for %%a in (%EXT_TARGETS%) do (
     set TEST_HAS_TARGET=

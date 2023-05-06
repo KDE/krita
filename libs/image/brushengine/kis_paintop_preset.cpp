7a56f76a6d193c6b027bfe2536e77f5dd1d8f960
Quytelda Kahja <quytelda@tamalin.org>
2023-05-05 17:48:28 -0700

    Simplify XML parsing when loading embedded resources.
    We can find and traverse the list of embedded resources more
    succinctly using firstChildElement() and nextSiblingElement().
    This removes the need to manually process QDomNodes and
    reduces nesting.

--- libs/image/brushengine/kis_paintop_preset.cpp
+++ libs/image/brushengine/kis_paintop_preset.cpp
@@ -191,51 +191,44 @@ bool KisPaintOpPreset::loadFromDevice(QIODevice *dev, KisResourcesInterfaceSP re
         return false;
     }
 
+    QDomElement root = doc.documentElement();
+
     if (d->version == "5.0" && resourceCount > 0) {
-        // Load the embedded resources
-        QDomNode n = doc.firstChild();
-        while (!n.isNull()) {
-            QDomElement e = n.toElement();
-            if (!e.isNull()) {
-                if (e.tagName() == "resources") {
-                    QDomNode n2 = n.firstChild();
-                    while (!n2.isNull()) {
-                        n2 = n2.nextSibling();
-                        QDomElement e2 = n2.toElement();
-                        QString resourceType = e2.attribute("type");
-                        QString md5sum = e2.attribute("md5sum");
-                        QString name = e2.attribute("name");
-                        QString filename = e2.attribute("filename");
+        // Load any embedded resources
+        QDomElement e = root.firstChildElement("resources");
+        for (e = e.firstChildElement("resource"); !e.isNull(); e = e.nextSiblingElement("resource")) {
+            QString name = e.attribute("name");
+            QString filename = e.attribute("filename");
+            QString resourceType = e.attribute("type");
+            QString md5sum = e.attribute("md5sum");
 
-                        KoResourceSP existingResource = resourcesInterface->source(resourceType).bestMatch(md5sum, filename, name);
+            KoResourceSP existingResource = resourcesInterface
+                ->source(resourceType)
+                .bestMatch(md5sum, filename, name);
 
-                        if (!existingResource) {
-                            QByteArray ba = QByteArray::fromBase64(e2.text().toLatin1());
-
-                            QBuffer buf(&ba);
-                            buf.open(QBuffer::ReadOnly);
-
-                            /// HACK ALERT: Calling importResource()
-                            /// here is technically undefined
-                            /// behavior, because this code is
-                            /// called from inside the storage's
-                            /// loadVersionedResource(). Basically
-                            /// we change underlying storage's
-                            /// storage while it is reading from
-                            /// it.
-
-                            KisResourceModel model(resourceType);
-                            model.importResource(filename, &buf, false, "memory");
-                        }
-                    }
-                    break;
-                }
+            if (existingResource) {
+                continue;
             }
-            n = n.nextSibling();
+
+            QByteArray ba = QByteArray::fromBase64(e.text().toLatin1());
+            QBuffer buf(&ba);
+            buf.open(QBuffer::ReadOnly);
+
+            /// HACK ALERT: Calling importResource()
+            /// here is technically undefined
+            /// behavior, because this code is
+            /// called from inside the storage's
+            /// loadVersionedResource(). Basically
+            /// we change underlying storage's
+            /// storage while it is reading from
+            /// it.
+
+            KisResourceModel model(resourceType);
+            model.importResource(filename, &buf, false, "memory");
         }
     }
 
-    fromXML(doc.documentElement(), resourcesInterface);
+    fromXML(root, resourcesInterface);
 
     if (!d->settings) {
         return false;

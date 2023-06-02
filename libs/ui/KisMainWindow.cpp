5f8ebb64522169a5cb0d9a5f56a6742cbba05ea7
Dmitry Kazakov <dimula73@gmail.com>
2023-06-02 16:50:07 +0200

    Fix double switch of the view in KisViewManager
    The view is switched twice, once in d->mdiArea->setActiveSubWindow(subwin)
    and once in setActiveView(view).
    
    CCBUG:364237

--- libs/ui/KisMainWindow.cpp
+++ libs/ui/KisMainWindow.cpp
@@ -2682,7 +2682,6 @@ void KisMainWindow::setActiveSubWindow(QWidget *window)
         KisView *view = qobject_cast<KisView *>(subwin->widget());
         //dbgKrita << "\t" << view << activeView();
         if (view && view != activeView()) {
-            d->mdiArea->setActiveSubWindow(subwin);
             setActiveView(view);
         }
         d->activeSubWindow = subwin;

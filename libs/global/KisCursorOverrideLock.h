d49660a234467afdddd7a4875c21fd14d8b22184
Dmitry Kazakov <dimula73@gmail.com>
2023-07-11 14:55:51 +0200

    Fix license

--- libs/global/KisCursorOverrideLock.h
+++ libs/global/KisCursorOverrideLock.h
@@ -1,4 +1,8 @@
-
+/*
+ *  SPDX-FileCopyrightText: 2017 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 #ifndef KISCURSOROVERRIDELOCK_H
 #define KISCURSOROVERRIDELOCK_H

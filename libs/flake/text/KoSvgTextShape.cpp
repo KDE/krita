77cf67f87d6090c62f26eb5349b546b0eddd7307
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2023-07-23 15:25:02 +0200

    Fix line-height using resolution, do empty-line-calculation later.

--- libs/flake/text/KoSvgTextShape.cpp
+++ libs/flake/text/KoSvgTextShape.cpp
@@ -878,7 +878,7 @@ void KoSvgTextShape::relayout() const
 
                     if (!lineHeight.isNormal) {
                         if (lineHeight.isNumber) {
-                            leading = (fontSize*ftFontUnit)*lineHeight.value;
+                            leading = (fontSize*scaleToPixel*ftFontUnit)*lineHeight.value;
                             leading -= (ascender-descender);
                         } else {
                             QPointF val = ftTF.inverted().map(QPointF(lineHeight.value, lineHeight.value));
@@ -1546,7 +1546,7 @@ void addWordToLine(QVector<CharacterResult> &result,
     Q_FOREACH (const int j, wordIndices) {
         CharacterResult cr = result.at(j);
         if (currentChunk.boundingBox.isEmpty() && j == wordIndices.first()) {
-            if (result.at(j).lineStart == Collapse && cr.breakType != HardBreak) {
+            if (result.at(j).lineStart == Collapse) {
                 result[j].addressable = false;
                 result[j].hidden = true;
                 continue;
@@ -1591,6 +1591,24 @@ QPointF lineHeightOffset(KoSvgText::WritingMode writingMode,
     QPointF lineTop;
     QPointF lineBottom;
     QPointF correctionOffset;
+
+    if (currentLine.chunks.isEmpty()) {
+        return QPointF();
+    } else if (currentLine.chunks.size() == 1 && currentLine.actualLineTop == 0 &&
+               currentLine.actualLineBottom == 0){
+        /**
+         * When the line is empty, but caused by a hardbreak, we will need to use that hardbreak
+         * to space the line. This can only be done at this point as it would otherwise need to use
+         * visible characters.
+         */
+        QVector<int> chunkIndices = currentLine.chunks[0].chunkIndices;
+        if (chunkIndices.size() > 0) {
+            CharacterResult cr = result[chunkIndices.first()];
+            currentLine.actualLineTop = qMax(fabs(cr.ascent-cr.halfLeading), currentLine.actualLineTop);
+            currentLine.actualLineBottom = qMax(fabs(cr.descent+cr.halfLeading), currentLine.actualLineBottom);
+        }
+    }
+
     qreal expectedLineTop = qMax(currentLine.expectedLineTop, currentLine.actualLineTop);
     if (writingMode == KoSvgText::HorizontalTB) {
         lineTop = QPointF(0, currentLine.actualLineTop);

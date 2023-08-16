60d6b3d196780f7eb564267f0f640de204bfda7c
Dmitry Kazakov <dimula73@gmail.com>
2023-08-16 19:33:00 +0300

    Update download_release_artifacts.sh
    1) Sync actual artifact names with what we have on binary factory
    
    2) Add AppBundle packages
    
    3) Autodetect channel name for AppImage's updater

--- packaging/download_release_artifacts.sh
+++ packaging/download_release_artifacts.sh
@@ -7,6 +7,15 @@
 set -e
 
 KRITA_VERSION=5.2.0-beta2
+CHANNEL=Stable
+
+if echo $KRITA_VERSION | grep beta > /dev/null;
+   CHANNEL=Beta
+elif echo $KRITA_VERSION | grep rc > /dev/null;
+   # release candidates are also considered as "beta" for updater
+   CHANNEL=Beta
+fi
+
 
 WIN64_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Windows64_Build/lastSuccessfulBuild/artifact
 APPIMAGE_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Appimage_Build/lastSuccessfulBuild/artifact
@@ -16,6 +25,7 @@ ANDROID_ARM64_V8A_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release
 ANDROID_ARMEABI_V7A_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Android_armeabi-v7a_Build/lastSuccessfulBuild/artifact
 ANDROID_X86_64_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Android_x86_64_Build/lastSuccessfulBuild/artifact
 ANDROID_X86_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Android_x86_Build/lastSuccessfulBuild/artifact
+ANDROID_APPBUNDLE_ARTIFACTS_URL=https://binary-factory.kde.org/job/Krita_Release_Android_AppBundle_Build/lastSuccessfulBuild/artifact
 
 wget $WIN64_ARTIFACTS_URL/krita-x64-$KRITA_VERSION-dbg.zip
 wget $WIN64_ARTIFACTS_URL/krita-x64-$KRITA_VERSION.zip
@@ -24,20 +34,15 @@ wget $WIN64_ARTIFACTS_URL/krita-x64-$KRITA_VERSION-setup.exe
 wget $APPIMAGE_ARTIFACTS_URL/krita-$KRITA_VERSION-x86_64.appimage
 wget $APPIMAGE_ARTIFACTS_URL/Krita-CHANGEME-x86_64.appimage.zsync
 
-mv Krita-CHANGEME-x86_64.appimage.zsync krita-$KRITA_VERSION-x86_64.appimage.zsync
+mv Krita-CHANGEME-x86_64.appimage.zsync Krita-$CHANNEL-x86_64.appimage.zsync
 
-wget $ANDROID_ARM64_V8A_ARTIFACTS_URL/krita_build_apk-release-unsigned.apk
-mv krita_build_apk-release-unsigned.apk krita-arm64-$KRITA_VERSION-unsigned.apk
+wget $ANDROID_ARM64_V8A_ARTIFACTS_URL/krita-arm64-v8a-$KRITA_VERSION-release-unsigned.apk
+wget $ANDROID_ARMEABI_V7A_ARTIFACTS_URL/krita-armeabi-v7a-$KRITA_VERSION-release-unsigned.apk
+wget $ANDROID_X86_ARTIFACTS_URL/krita-x86-$KRITA_VERSION-release-unsigned.apk
+wget $ANDROID_X86_64_ARTIFACTS_URL/krita-x86_64-$KRITA_VERSION-release-unsigned.apk
 
-wget $ANDROID_ARMEABI_V7A_ARTIFACTS_URL/krita_build_apk-release-unsigned.apk
-mv krita_build_apk-release-unsigned.apk krita-arm32-$KRITA_VERSION-unsigned.apk
-
-wget $ANDROID_X86_ARTIFACTS_URL/krita_build_apk-release-unsigned.apk
-mv krita_build_apk-release-unsigned.apk krita-x86-$KRITA_VERSION-unsigned.apk
-
-wget $ANDROID_X86_64_ARTIFACTS_URL/krita_build_apk-release-unsigned.apk
-mv krita_build_apk-release-unsigned.apk krita-x86_64-$KRITA_VERSION-unsigned.apk
-
+wget $ANDROID_APPBUNDLE_ARTIFACTS_URL/krita-$KRITA_VERSION-release.aab
+mv krita-$KRITA_VERSION-release.aab krita-$KRITA_VERSION-release-unsigned.aab
 
 if [[ -f archive.zip ]]; then
    rm archive.zip

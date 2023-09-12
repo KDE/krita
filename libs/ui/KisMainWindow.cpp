KDE/krita

commit 56f801118cfca72852f42fe37de348e4bc3c126c

Welcome Page: Fix bug where docker size is not preserved
The bug happened because when we booted Krita we called the
saveMainWindowSettings, which resulted in bogus settings being saved. So
we don't call that method when we are booting. But only when we go back
and forth from canvas page.

(cherry picked from commit 953196046c27ff9413b76fad1a3d38582a180ca4)

libs/ui/KisMainWindow.cpp
@@ -1617,7 +1617,7 @@ void KisMainWindow::showEvent(QShowEvent *event)
 {
     // we're here because, we need to make sure everything (dockers, toolbars etc) is loaded and ready before
     // we can hide it.
-    setMainWindowLayoutForCurrentMainWidget(0);
+    adjustLayoutForWelcomePage();
     return KXmlGuiWindow::showEvent(event);
 }
 
@@ -1626,20 +1626,7 @@ void KisMainWindow::setMainWindowLayoutForCurrentMainWidget(int widgetIndex)
     if (widgetIndex == 0) {
         // save the state of the window which existed up-to now (this is before we stop auto-saving).
         saveMainWindowSettings(d->windowStateConfig);
-        // This makes sure we don't save window state when we're in welcome page mode, because all the dockers
-        // etc are hidden while the user is here.
-        resetAutoSaveSettings();
-
-        toggleDockersVisibility(false);
-        if (statusBar()) {
-            statusBar()->hide();
-        }
-        QList<QToolBar *> toolbars = findChildren<QToolBar *>();
-        for (QToolBar *toolbar : toolbars) {
-            if (toolbar->objectName() == "BrushesAndStuff" || toolbar->objectName() == "editToolBar") {
-                toolbar->hide();
-            }
-        }
+        adjustLayoutForWelcomePage();
     }
     else {
         setAutoSaveSettings(d->windowStateConfig, false);
@@ -1650,6 +1637,24 @@ void KisMainWindow::setMainWindowLayoutForCurrentMainWidget(int widgetIndex)
     for (QAction *action : actions) {
         if (action) {
             action->setEnabled(widgetIndex);
+        }
+    }
+}
+
+void KisMainWindow::adjustLayoutForWelcomePage()
+{
+    // This makes sure we don't save window state when we're in welcome page mode, because all the dockers
+    // etc are hidden while the user is here.
+    resetAutoSaveSettings();
+
+    toggleDockersVisibility(false);
+    if (statusBar()) {
+        statusBar()->hide();
+    }
+    QList<QToolBar *> toolbars = findChildren<QToolBar *>();
+    for (QToolBar *toolbar : toolbars) {
+        if (toolbar->objectName() == "BrushesAndStuff" || toolbar->objectName() == "editToolBar") {
+            toolbar->hide();
         }
     }
 }

libs/ui/KisMainWindow.h
@@ -483,6 +483,7 @@ private:
     void customizeTabBar();
 
     void setMainWindowLayoutForCurrentMainWidget(int widgetIndex);
+    void adjustLayoutForWelcomePage();
 
 private:
 

commit 8a4ca4e3e9530c29a1b6e3a46f7df98f9adc7d52

Save window state before hiding everything
This fixes bug 473967 introduced in 56f801118c, however the docker size
not preserved on krita restart is reintroduced.

BUG:473967

libs/ui/KisMainWindow.cpp
@@ -1616,7 +1616,7 @@ void KisMainWindow::showEvent(QShowEvent *event)
     // we're here because, we need to make sure everything (dockers, toolbars etc) is loaded and ready before
     // we can hide it.
     if (!event->spontaneous()) {
-        adjustLayoutForWelcomePage();
+        setMainWindowLayoutForCurrentMainWidget(0);
     }
     return KXmlGuiWindow::showEvent(event);
 }
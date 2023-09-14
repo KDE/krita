KDE/krita

commit ef521dac6125ec718200ca9b689fb7ae69f51482

Disable partial updates for fractional HiDPI cases
There is some bug either in QPainter or in our partial updates
patches that cause an update issue in reference images decoration.

Basically, the crop rect of the update should be aligned to the
physical pixels, but it is not.

CCBUG:441216

libs/ui/opengl/kis_opengl_canvas2.cpp
@@ -232,7 +232,17 @@ void KisOpenGLCanvas2::paintEvent(QPaintEvent *e)
 {
     KIS_SAFE_ASSERT_RECOVER_RETURN(!d->updateRect);
 
-    d->updateRect = e->rect();
+    if (qFuzzyCompare(devicePixelRatioF(), qRound(devicePixelRatioF()))) {
+        /**
+         * Enable partial updates **only** for the case when we have
+         * integer scaling. There is a bug in Qt that causes artifacts
+         * otherwise:
+         *
+         * See https://bugs.kde.org/show_bug.cgi?id=441216
+         */
+        d->updateRect = e->rect();
+    }
+
     QOpenGLWidget::paintEvent(e);
     d->updateRect = boost::none;
 }

commit d486a0605f8d120130eaeb09753829a484d9654f

Set updateRect to widget rect on fractional hidpi screen
This fixes the warning `KoShapeManager::paint  Painting with a painter
that has no clipping will lead to too much being painted!`, regression
from ef521dac6125ec718200ca9b689fb7ae69f51482.

CCBUG:441216

libs/ui/opengl/kis_opengl_canvas2.cpp
@@ -252,6 +252,8 @@ void KisOpenGLCanvas2::paintEvent(QPaintEvent *e)
          * See https://bugs.kde.org/show_bug.cgi?id=441216
          */
         d->updateRect = e->rect();
+    } else {
+        d->updateRect = this->rect();
     }
 
     QOpenGLWidget::paintEvent(e);
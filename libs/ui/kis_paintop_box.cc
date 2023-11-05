KDE/krita

commit 5370c53cd8540769c795c6e00d269a810af844c4

Apply i18n to spinbox percent values

libs/ui/kis_paintop_box.cc
@@ -37,6 +37,7 @@
 
 #include <KoResource.h>
 #include <KisDirtyStateSaver.h>
+#include <KisDoubleSpinBoxPluralHelper.h>
 
 #include <kis_paint_device.h>
 #include <brushengine/kis_paintop_registry.h>
@@ -278,7 +279,9 @@ KisPaintopBox::KisPaintopBox(KisViewManager *viewManager, QWidget *parent, const
         slOpacity->setRange(0, 100, 0);
         slOpacity->setValue(100);
         slOpacity->setSingleStep(5);
-        slOpacity->setSuffix(i18n("%"));
+        KisDoubleSpinBoxPluralHelper::install(slOpacity, [](double value) {
+            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+        });
         slOpacity->setMinimumWidth(qMax(sliderWidth, slOpacity->sizeHint().width()));
         slOpacity->setFixedHeight(buttonsize);
         slOpacity->setBlockUpdateSignalOnDrag(true);
@@ -286,7 +289,9 @@ KisPaintopBox::KisPaintopBox(KisViewManager *viewManager, QWidget *parent, const
         slFlow->setRange(0, 100, 0);
         slFlow->setValue(100);
         slFlow->setSingleStep(5);
-        slFlow->setSuffix(i18n("%"));
+        KisDoubleSpinBoxPluralHelper::install(slFlow, [](double value) {
+            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+        });
         slFlow->setMinimumWidth(qMax(sliderWidth, slFlow->sizeHint().width()));
         slFlow->setFixedHeight(buttonsize);
         slFlow->setBlockUpdateSignalOnDrag(true);

libs/ui/dialogs/kis_dlg_layer_properties.cc
@@ -18,6 +18,7 @@
 
 #include <KoColorSpace.h>
 
+#include <KisSpinBoxPluralHelper.h>
 #include "KisViewManager.h"
 #include <kis_debug.h>
 #include <kis_global.h>
@@ -94,7 +95,9 @@ KisDlgLayerProperties::KisDlgLayerProperties(KisNodeList nodes, KisViewManager *
     connect(d->page->editName, SIGNAL(textChanged(QString)), SLOT(slotNameValueChangedExternally()));
 
     d->page->intOpacity->setRange(0, 100);
-    d->page->intOpacity->setSuffix(i18n("%"));
+    KisSpinBoxPluralHelper::install(d->page->intOpacity, [](int value) {
+        return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+    });
     d->opacityProperty.reset(new KisMultinodeOpacityProperty(nodes));
     d->opacityProperty->connectIgnoreCheckBox(d->page->chkOpacity);
     d->opacityProperty->connectAutoEnableWidget(d->page->intOpacity);

libs/ui/kis_node_model.cpp
@@ -592,10 +592,10 @@ QVariant KisNodeModel::data(const QModelIndex &index, int role) const
                     return QString(compositeOpDesc);
                 }
                 if (compositeOpId == defaultOpId) {
-                    return QString("%1%2").arg(opacityString).arg(i18n("%"));
+                    return i18nc("%1 is the percent value, % is the percent sign", "%1%", opacityString);
                 }
             }
-            infoText = QString("%1%2 %3").arg(opacityString).arg(i18n("%")).arg(compositeOpDesc);
+            infoText = i18nc("%1 is the percent value, % is the percent sign", "%1% %2", opacityString, compositeOpDesc);
         }
         return infoText;
     }

libs/ui/widgets/KisAutoLevelsWidget.cpp
@@ -7,13 +7,16 @@
  */
 
 #include "KisAutoLevelsWidget.h"
+#include <KisDoubleSpinBoxPluralHelper.h>
 
 KisAutoLevelsWidget::KisAutoLevelsWidget(QWidget *parent)
     : QWidget(parent)
 {
     m_ui.setupUi(this);
 
-    m_ui.sliderShadowsClipping->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(m_ui.sliderShadowsClipping, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+    });
     m_ui.sliderShadowsClipping->setRange(0.0, 10.0, 2);
     m_ui.sliderShadowsClipping->setValue(0.1);
     m_ui.sliderShadowsClipping->setSingleStep(0.1);

libs/ui/widgets/gradient/KisGradientColorEditor.cpp
@@ -11,6 +11,7 @@
 #include <kis_slider_spin_box.h>
 #include <kis_color_button.h>
 #include <kis_icon_utils.h>
+#include <KisDoubleSpinBoxPluralHelper.h>
 
 #include "KisGradientColorEditor.h"
 
@@ -89,15 +90,17 @@ KisGradientColorEditor::KisGradientColorEditor(QWidget *parent)
 
     m_d->opacitySlider = new KisDoubleSliderSpinBox;
     m_d->opacitySlider->setRange(0, 100, 2);
-    m_d->opacitySlider->setPrefix(i18n("Opacity: "));
-    m_d->opacitySlider->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(m_d->opacitySlider, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "Opacity: {n}%", value);
+    });
     m_d->opacitySlider->setVisible(false);
     connect(m_d->opacitySlider, SIGNAL(valueChanged(double)), this, SIGNAL(opacityChanged(qreal)));
 
     m_d->positionSlider = new KisDoubleSliderSpinBox;
     m_d->positionSlider->setRange(0, 100, 2);
-    m_d->positionSlider->setPrefix(i18n("Position: "));
-    m_d->positionSlider->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(m_d->positionSlider, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "Position: {n}%", value);
+    });
     connect(m_d->positionSlider, SIGNAL(valueChanged(double)), this, SIGNAL(positionChanged(qreal)));
 
     QHBoxLayout *colorTypeButtonsLayout = new QHBoxLayout;

libs/ui/widgets/gradient/KisSegmentGradientEditor.cpp
@@ -25,6 +25,7 @@
 
 #include <kis_icon_utils.h>
 #include <kis_signals_blocker.h>
+#include <KisDoubleSpinBoxPluralHelper.h>
 
 #include "KisSegmentGradientEditor.h"
 
@@ -158,11 +159,13 @@ KisSegmentGradientEditor::KisSegmentGradientEditor(QWidget *parent)
     constrainStopButton->setKeepAspectRatio(false);
     constrainStopButton->setToolTip(i18nc("Button to link both end colors of a stop handle in the segment gradient editor", "Link colors"));
     stopPositionSlider->setRange(0, 100, 2);
-    stopPositionSlider->setPrefix(i18n("Position: "));
-    stopPositionSlider->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(stopPositionSlider, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "Position: {n}%", value);
+    });
     midPointPositionSlider->setRange(0, 100, 2);
-    midPointPositionSlider->setPrefix(i18n("Position: "));
-    midPointPositionSlider->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(midPointPositionSlider, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "Position: {n}%", value);
+    });
 
     setCompactMode(false);
     setGradient(0);

libs/ui/widgets/kis_custom_image_widget.cc
@@ -48,6 +48,7 @@
 #include "KisPart.h"
 #include "KisDocument.h"
 #include "widgets/kis_cmb_idlist.h"
+#include <KisDoubleSpinBoxPluralHelper.h>
 #include <KisSqueezedComboBox.h>
 #include "kis_signals_blocker.h"
 
@@ -95,7 +96,9 @@ KisCustomImageWidget::KisCustomImageWidget(QWidget* parent, qint32 defWidth, qin
 
     sliderOpacity->setRange(0, 100, 0);
     sliderOpacity->setValue(100);
-    sliderOpacity->setSuffix(i18n("%"));
+    KisDoubleSpinBoxPluralHelper::install(sliderOpacity, [](double value) {
+        return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+    });
 
     connect(cmbPredefined, SIGNAL(activated(int)), SLOT(predefinedClicked(int)));
     connect(doubleResolution, SIGNAL(valueChanged(double)),

libs/widgets/kis_color_input.cpp
@@ -31,6 +31,7 @@
 #include <KoColorSpace.h>
 #include <KisHsvColorSlider.h>
 #include <KoColorConversions.h>
+#include <KisSpinBoxPluralHelper.h>
 
 #include "kis_double_parse_spin_box.h"
 #include "kis_int_parse_spin_box.h"
@@ -145,7 +146,9 @@ QWidget* KisIntegerColorInput::createInput()
     m_colorSlider->setMinimum(0);
 
     if (m_usePercentage) {
-        m_intNumInput->setSuffix(i18n("%"));
+        KisSpinBoxPluralHelper::install(m_intNumInput, [](int value) {
+            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+        });
     } else {
         m_intNumInput->setSuffix("");
     }
@@ -188,7 +191,9 @@ void KisIntegerColorInput::setPercentageWise(bool val)
     m_usePercentage = val;
 
     if (m_usePercentage) {
-        m_intNumInput->setSuffix(i18n("%"));
+        KisSpinBoxPluralHelper::install(m_intNumInput, [](int value) {
+            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+        });
     } else {
         m_intNumInput->setSuffix("");
     }

commit 425a846b430ceb01673a27130a3c408345517979

Fix toolbar opacity/flow sliders missing text
Regression from 5370c53c.

libs/ui/kis_paintop_box.cc
@@ -262,8 +262,12 @@ KisPaintopBox::KisPaintopBox(KisViewManager *viewManager, QWidget *parent, const
             slRotation    = m_sliderChooser[i]->addWidget<KisAngleSelector>("rotation");
             slPatternSize = m_sliderChooser[i]->addWidget<KisMultipliersDoubleSliderSpinBox>("patternsize");
 
-            slOpacity->setPrefix(QString("%1 ").arg(i18n("Opacity:")));
-            slFlow->setPrefix(QString("%1 ").arg(i18n("Flow:")));
+            KisDoubleSpinBoxPluralHelper::install(slOpacity, [](double value) {
+                return i18nc("{n} is the number value, % is the percent sign", "Opacity: {n}%", value);
+            });
+            KisDoubleSpinBoxPluralHelper::install(slFlow, [](double value) {
+                return i18nc("{n} is the number value, % is the percent sign", "Flow: {n}%", value);
+            });
             slSize->setPrefix(QString("%1 ").arg(i18n("Size:")));
             slRotation->setPrefix(QString("%1 ").arg(i18n("Rotation:")));
             slPatternSize->setPrefix(QString("%1 ").arg(i18n("Pattern Scale:")));
@@ -274,14 +278,18 @@ KisPaintopBox::KisPaintopBox(KisViewManager *viewManager, QWidget *parent, const
             slSize        = m_sliderChooser[i]->addWidget<KisDoubleSliderSpinBox>("size", i18n("Size:"));
             slRotation    = m_sliderChooser[i]->addWidget<KisAngleSelector>("rotation", i18n("Rotation:"));
             slPatternSize = m_sliderChooser[i]->addWidget<KisMultipliersDoubleSliderSpinBox>("patternsize", i18n("Pattern Scale:"));
+
+            KisDoubleSpinBoxPluralHelper::install(slOpacity, [](double value) {
+                return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+            });
+            KisDoubleSpinBoxPluralHelper::install(slFlow, [](double value) {
+                return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
+            });
         }
 
         slOpacity->setRange(0, 100, 0);
         slOpacity->setValue(100);
         slOpacity->setSingleStep(5);
-        KisDoubleSpinBoxPluralHelper::install(slOpacity, [](double value) {
-            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
-        });
         slOpacity->setMinimumWidth(qMax(sliderWidth, slOpacity->sizeHint().width()));
         slOpacity->setFixedHeight(buttonsize);
         slOpacity->setBlockUpdateSignalOnDrag(true);
@@ -289,9 +297,6 @@ KisPaintopBox::KisPaintopBox(KisViewManager *viewManager, QWidget *parent, const
         slFlow->setRange(0, 100, 0);
         slFlow->setValue(100);
         slFlow->setSingleStep(5);
-        KisDoubleSpinBoxPluralHelper::install(slFlow, [](double value) {
-            return i18nc("{n} is the number value, % is the percent sign", "{n}%", value);
-        });
         slFlow->setMinimumWidth(qMax(sliderWidth, slFlow->sizeHint().width()));
         slFlow->setFixedHeight(buttonsize);
         slFlow->setBlockUpdateSignalOnDrag(true);
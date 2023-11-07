3353155fc39fc35c0632fb81eca7c92a4abe9c3e
Dmitry Kazakov <dimula73@gmail.com>
2023-11-07 13:29:01 +0100

    Extract the cache in KisMyPaintOpSettings to properly handle size updates
    Brush size cache was not properly reset on preset change. Now it is a
    part of a cache structure, so it is quite hard to forget about that.
    
    BUG:476141

---
+++ libs/global/tests/KisValueCacheTest.h
@@ -0,0 +1,20 @@
+/*
+ *  SPDX-FileCopyrightText: 2023 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef KISVALUECACHETEST_H
+#define KISVALUECACHETEST_H
+
+#include <QObject>
+
+class KisValueCacheTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void test();
+
+};
+
+#endif // KISVALUECACHETEST_H

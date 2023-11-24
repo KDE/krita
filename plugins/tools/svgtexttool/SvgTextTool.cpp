9fbde7cd5f41de617b4e8a684a7b7b04fa565fdb
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2023-11-24 16:17:55 +0100

    Text: move drawn origin point and move snap point back to origin.

--- plugins/tools/svgtexttool/SvgTextTool.cpp
+++ plugins/tools/svgtexttool/SvgTextTool.cpp
@@ -588,7 +588,7 @@ void SvgTextTool::paint(QPainter &gc, const KoViewConverter &converter)
         } else {
             handlePainter.setHandleStyle(KisHandleStyle::primarySelection());
         }
-        handlePainter.drawHandleCircle(shape->initialTextPosition(), KoToolBase::handleRadius() * 0.75);
+        handlePainter.drawHandleCircle(QPointF(), KoToolBase::handleRadius() * 0.75);
     }
 
     gc.setTransform(converter.documentToView(), true);

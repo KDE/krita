b07c217764aaf918176de3ca7e9dd9eff5b4f6c5
Freya Lupen <penguinflyer2222@gmail.com>
2024-01-05 09:55:36 -0600

    Prevent Scripter staying open on mainwindow close
    Krita wouldn't quit until the Scripter was also closed,
    scripters found this annoying.

--- plugins/python/scripter/scripterdialog.py
+++ plugins/python/scripter/scripterdialog.py
@@ -4,13 +4,14 @@ SPDX-FileCopyrightText: 2017 Eliakin Costa <eliakim170@gmail.com>
 SPDX-License-Identifier: GPL-2.0-or-later
 """
 from PyQt5.QtWidgets import QDialog
-from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 
 
 class ScripterDialog(QDialog):
 
     def __init__(self, uicontroller, parent=None):
         super(ScripterDialog, self).__init__(parent)
+        self.setAttribute(Qt.WA_QuitOnClose, False)
         self.uicontroller = uicontroller
 
     def closeEvent(self, event):

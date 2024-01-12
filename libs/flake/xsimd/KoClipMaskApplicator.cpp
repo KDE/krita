6645fb49159ae0cb23edf9682c5f37011e452aa9
Dmitry Kazakov <dimula73@gmail.com>
2024-01-12 15:01:03 +0100

    Remove unused .cpp file
    KoClipMaskApplicator.h does not define any non-template class, neither
    does it have QObject-based stuff that need .moc compilation, so it doesn't
    see a separate .cpp file. The file will be built by
    KoClipMaskApplicatorFactoryImpl.cpp.

--- libs/flake/xsimd/KoClipMaskApplicator.cpp
+++
@@ -1,6 +0,0 @@
-/*
- *  SPDX-FileCopyrightText: 2023 Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
- *
- *  SPDX-License-Identifier: GPL-2.0-or-later
- */
-#include "KoClipMaskApplicator.h"

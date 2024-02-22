b6fc9741472a4a6c2bd590eeabd840b0b9918489
Dmitry Kazakov <dimula73@gmail.com>
2024-02-22 14:44:59 +0100

    Fix license

--- plugins/dockers/channeldocker/KisChannelsThumbnailsStrokeStrategy.h
+++ plugins/dockers/channeldocker/KisChannelsThumbnailsStrokeStrategy.h
@@ -1,3 +1,9 @@
+/*
+ *  SPDX-FileCopyrightText: 2023 Dmitry Kazakov <dimula73@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #ifndef KISCHANNELSTHUMBNAILSSTROKESTRATEGY_H
 #define KISCHANNELSTHUMBNAILSSTROKESTRATEGY_H
 

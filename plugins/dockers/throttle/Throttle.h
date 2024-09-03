e959198d9a2c10722db0df799e41e71b9c73650e
Halla Rempt <halla@valdyas.org>
2024-09-03 14:46:01 +0200

    Re-add the throttle docker to 5.2
    It's not very useful, but it uses QML, which apparently is enough
    to make the initialization of the canvas work on Android...
    
    BUG:488606

---
+++ plugins/dockers/throttle/Throttle.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2017 Boudewijn Rempt <boud@valdyas.org>
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; version 2.1 of the License.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef THROTTLE_H
+#define THROTTLE_H
+
+#include <QtQuickWidgets/QQuickWidget>
+
+class KisCanvas2;
+
+class ThreadManager : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(int threadCount READ threadCount WRITE setThreadCount NOTIFY threadCountChanged)
+    Q_PROPERTY(int maxThreadCount READ maxThreadCount)
+public:
+    ThreadManager(QObject *parent = 0);
+    ~ThreadManager() override;
+
+    void setThreadCount(int threadCount);
+    int threadCount() const;
+    int maxThreadCount() const;
+
+Q_SIGNALS:
+    void threadCountChanged();
+private:
+    int m_threadCount {0};
+};
+
+class Throttle : public QQuickWidget  {
+    Q_OBJECT
+public:
+    Throttle(QWidget *parent);
+    ~Throttle() override;
+private Q_SLOTS:
+    void onQuit();
+    void onExit(int retCode);
+    void onWarnings(const QList<QQmlError> &warnings);
+private:
+    ThreadManager *m_threadManager {0};
+};
+
+
+#endif
+

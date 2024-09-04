055329c53e948c0e6065ffbad0e77471ea7868b8
Halla Rempt <halla@valdyas.org>
2024-09-04 11:01:20 +0200

    Add missing languages to feed button
    All these languages have a news.po, so they should have a feed.
    
    However, the news widget still cannot actually load the feeds,
    either the url we generate (krita.org/pt/feed, for instance) is
    wrong, or the hugo configuration for multilang rss feeds is
    wrong.
    
    CCMAIL:kimageshop@kde.org
    CCBUG:489477
    (cherry picked from commit ae318ad7aeeca1db32c619aa63cccdc5f59f5608)

--- libs/ui/widgets/KisNewsWidget.cpp
+++ libs/ui/widgets/KisNewsWidget.cpp
@@ -118,7 +118,7 @@ void KisNewsWidget::toggleNewsLanguage(QString langCode, bool enabled)
     // Sanity check: Since the code is adding the language code directly into
     // the URL, this prevents any nasty surprises with malformed URLs.
     Q_FOREACH(const char &ch, langCode.toLatin1()) {
-        bool isValidChar = (ch >= 'a' && ch <= 'z') || ch == '-';
+        bool isValidChar = (ch >= 'a' && ch <= 'z') || ch == '-' || ch = '@';
         if (!isValidChar) {
             warnUI << "Ignoring attempt to toggle malformed news lang:" << langCode;
             return;

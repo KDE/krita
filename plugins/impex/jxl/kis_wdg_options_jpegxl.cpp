839e1e69991ee245fd192aa3cb371280f118e2aa
Rasyuqa A. H. <qampidh@gmail.com>
2024-10-31 04:37:23 +0700

    JPEG XL: Fix potential lockup when loading multipage images
    Also fix some values in advanced export options to correctly reflect
    with libjxl API docs

--- plugins/impex/jxl/kis_wdg_options_jpegxl.cpp
+++ plugins/impex/jxl/kis_wdg_options_jpegxl.cpp
@@ -52,7 +52,7 @@ KisWdgOptionsJPEGXL::KisWdgOptionsJPEGXL(QWidget *parent)
     {
         modular->addItem(i18nc("JPEG-XL encoder options", "Default (encoder chooses)"), -1);
         modular->addItem(i18nc("JPEG-XL encoder options", "VarDCT mode (e.g. for photographic images)"), 0);
-        modular->addItem(i18nc("JPEG-XL encoder options", "Modular mode (e.g. for lossless images)"), -1);
+        modular->addItem(i18nc("JPEG-XL encoder options", "Modular mode (e.g. for lossless images)"), 1);
     }
 
     {
@@ -64,7 +64,7 @@ KisWdgOptionsJPEGXL::KisWdgOptionsJPEGXL(QWidget *parent)
     {
         groupOrder->addItem(i18nc("JPEG-XL encoder options", "Default (encoder chooses)"), -1);
         groupOrder->addItem(i18nc("JPEG-XL encoder options", "Scanline order"), 0);
-        groupOrder->addItem(i18nc("JPEG-XL encoder options", "Center-first order"), -1);
+        groupOrder->addItem(i18nc("JPEG-XL encoder options", "Center-first order"), 1);
     }
 
     {

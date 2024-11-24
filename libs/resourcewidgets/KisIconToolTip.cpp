d61463fc5b3bf647a527beb84ed3b4e25e4af019
Freya Lupen <penguinflyer2222@gmail.com>
2024-11-24 09:01:58 -0600

    Fix aspect ratio of Resource Manager tooltips
    Resource tooltip thumbnails with a fixed size were using
    IgnoreAspectRatio, intended for gradients, for everything,
    causing things to be stretched.
    
    (This didn't affect thumbnails in the Brush Editor,
     which are based on the resource's size instead.)

--- libs/resourcewidgets/KisIconToolTip.cpp
+++ libs/resourcewidgets/KisIconToolTip.cpp
@@ -54,22 +54,18 @@ QTextDocument *KisIconToolTip::createDocument(const QModelIndex &index)
     if (!m_fixedToolTipThumbnailSize.isEmpty() && !thumb.isNull()) {
         int pixelSize = 48; //  // let's say, 48x48?
         if (!thumb.isNull()) {
-            if (thumb.size().width() < pixelSize || thumb.size().height() < pixelSize) {
-                // this allows the pixel patterns to be displayed correctly,
-                // while the presets (which have 200x200 thumbnails) will still be pretty
-                // Fast Transformation == Nearest Neighbour
+            // affects mostly gradients, which are very long but only 1px tall
+            Qt::AspectRatioMode aspectRatioMode = thumb.height() == 1 ? Qt::IgnoreAspectRatio : Qt::KeepAspectRatio;
+            // this allows the pixel patterns to be displayed correctly,
+            // while the presets (which have 200x200 thumbnails) will still be pretty
+            // Fast Transformation == Nearest Neighbour
+            Qt::TransformationMode transformationMode = (thumb.width() < pixelSize || thumb.height() < pixelSize) ? Qt::FastTransformation : Qt::SmoothTransformation;
+
                 thumb = KisResourceThumbnailCache::instance()->getImage(index,
                                                                         m_fixedToolTipThumbnailSize
                                                                             * devicePixelRatioF(),
-                                                                        Qt::IgnoreAspectRatio,
-                                                                        Qt::FastTransformation);
-            } else {
-                thumb = KisResourceThumbnailCache::instance()->getImage(index,
-                                                                        m_fixedToolTipThumbnailSize
-                                                                            * devicePixelRatioF(),
-                                                                        Qt::IgnoreAspectRatio,
-                                                                        Qt::SmoothTransformation);
-            }
+                                                                        aspectRatioMode,
+                                                                        transformationMode);
         }
     }
 

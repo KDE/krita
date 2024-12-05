31880f41b776c248f1991154f34f072053f298c9
Halla Rempt <halla@valdyas.org>
2024-12-05 10:58:12 +0100

    Remove our own redifinition of std::is_integral for half
    It is officially UB and breaks with every compiler upgrade. Instead
    we should use std::numeric_limits<T>::is_integer, which can officially
    be overridden (and is overridden for half).

--- libs/global/KisHalfTraits.h
+++ libs/global/KisHalfTraits.h
@@ -17,43 +17,6 @@
 
 namespace std {
 
-#if defined __GLIBCXX__
-
-template<>
-  struct __is_integral_helper<half>
-  : public false_type { };
-
-template<>
-  struct __is_floating_point_helper<half>
-  : public true_type { };
-
-#elif defined _LIBCPP_VERSION
-
-template<>
-  struct __libcpp_is_integral<half>
-  : public false_type { };
-
-template<>
-  struct __libcpp_is_floating_point<half>
-  : public true_type { };
-
-
-#else
-  // these are fallback implementations that
-  // don't support cv-qualifiers removal
-
-  template<>
-  struct is_integral<half>
-    : public false_type
-    { };
-
-  template<>
-  struct is_floating_point<half>
-    : public true_type
-    { };
-
-#endif
-
   inline bool
   isfinite(half __x)
   { return __x.isFinite(); }

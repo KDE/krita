2321b4bbf73de31b06f1b33e4b1aef4d29c9e7e7
Halla Rempt <halla@valdyas.org>
2025-01-08 10:41:32 +0100

    Qt6: ifdef between qreal* and float* for QColor
    Qt6 QColor now uses float* in its api instead of qreal*, while
    keeping qreal a double. This breaks:
    
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp: In member function ‘void KisHsvColorInput::getHsxF(const QColor&, qreal*, qreal*, qreal*)’:
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:684:23: error: cannot convert ‘qreal*’ {aka ‘double*’} to ‘float*’
      684 |         color.getHslF(&tempH, s, x);
          |                       ^~~~~~
          |                       |
          |                       qreal* {aka double*}
    In file included from /usr/include/x86_64-linux-gnu/qt6/QtGui/qpixmap.h:9,
                     from /usr/include/x86_64-linux-gnu/qt6/QtGui/qicon.h:10,
                     from /usr/include/x86_64-linux-gnu/qt6/QtGui/qaction.h:11,
                     from /usr/include/x86_64-linux-gnu/qt6/QtWidgets/qwidget.h:12,
                     from /usr/include/x86_64-linux-gnu/qt6/QtWidgets/QWidget:1,
                     from /home/halla/dev/krita/libs/widgets/kis_color_input.h:10,
                     from /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:9:
    /usr/include/x86_64-linux-gnu/qt6/QtGui/qcolor.h:158:25: note:   initializing argument 1 of ‘void QColor::getHslF(float*, float*, float*, float*) const’
      158 |     void getHslF(float *h, float *s, float *l, float *a = nullptr) const;
          |                  ~~~~~~~^
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:699:31: error: cannot convert ‘qreal*’ {aka ‘double*’} to ‘float*’
      699 |                 color.getHsvF(&tempH, s, x);
          |                               ^~~~~~
          |                               |
          |                               qreal* {aka double*}
    /usr/include/x86_64-linux-gnu/qt6/QtGui/qcolor.h:128:25: note:   initializing argument 1 of ‘void QColor::getHsvF(float*, float*, float*, float*) const’
      128 |     void getHsvF(float *h, float *s, float *v, float *a = nullptr) const;
          |                  ~~~~~~~^
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp: In constructor ‘KisHsvColorInput::KisHsvColorInput(QWidget*, KoColor*)’:
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:458:22: warning: this statement may fall through [-Wimplicit-fallthrough=]
      458 |             m_hInput = input;
          |             ~~~~~~~~~^~~~~~~
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:459:9: note: here
      459 |         case 1:
          |         ^~~~
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:461:22: warning: this statement may fall through [-Wimplicit-fallthrough=]
      461 |             m_sInput = input;
          |             ~~~~~~~~~^~~~~~~
    /home/halla/dev/krita/libs/widgets/kis_color_input.cpp:462:9: note: here
      462 |         case 2:
          |
    So use temporary float instead of qreal.

--- libs/widgets/kis_color_input.cpp
+++ libs/widgets/kis_color_input.cpp
@@ -679,24 +679,44 @@ void KisHsvColorInput::fillColor(QColor& c, const qreal& h, const qreal& s, cons
 void KisHsvColorInput::getHsxF(const QColor& color, qreal* h, qreal* s, qreal* x)
 {
     qreal tempH;
-	switch (m_mixMode) {
-	case KisHsvColorSlider::MIX_MODE::HSL:
+#if (QT_VERSION > QT_VERSION_CHECK(6, 0, 0))
+    float fS = *s;
+    float fX = *x;
+#endif
+    switch (m_mixMode) {
+    case KisHsvColorSlider::MIX_MODE::HSL:
+    {
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
         color.getHslF(&tempH, s, x);
+#else
+        float th;
+        color.getHslF(&th, &fS, &fX);
+        tempH = th;
+#endif
+    }
 		break;
 
 	case KisHsvColorSlider::MIX_MODE::HSY: {
-		RGBToHSY(color.redF(), color.greenF(), color.blueF(), &tempH, s, x);
+        RGBToHSY(color.redF(), color.greenF(), color.blueF(), &tempH, s, x);
 		break;
 	}
 
 	case KisHsvColorSlider::MIX_MODE::HSI: {
-		RGBToHSI(color.redF(), color.greenF(), color.blueF(), &tempH, s, x);
+        RGBToHSI(color.redF(), color.greenF(), color.blueF(), &tempH, s, x);
 		break;
 	}
 
 	default: // fallthrough
 	case KisHsvColorSlider::MIX_MODE::HSV:
+    {
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
 		color.getHsvF(&tempH, s, x);
+#else
+        float th;
+        color.getHsvF(&th, &fS, &fX);
+        tempH = th;
+#endif
+    }
 		break;
 	}
 

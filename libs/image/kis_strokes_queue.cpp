1d40421d507442c2d981502c0060ae9aa808e905
Halla Rempt <halla@valdyas.org>
2025-01-08 11:04:06 +0100

    Qt6: explicitly check whether the shared point is null

--- libs/image/kis_strokes_queue.cpp
+++ libs/image/kis_strokes_queue.cpp
@@ -397,7 +397,7 @@ bool KisStrokesQueue::cancelStroke(KisStrokeId id)
             buddy->cancelStroke();
         }
     }
-    return stroke;
+    return !stroke.isNull();
 }
 
 bool KisStrokesQueue::Private::hasUnfinishedStrokes() const

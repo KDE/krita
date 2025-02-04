a3e7b2ec5394136cfa019c24cd3754da0f9baebb
Dmitry Kazakov <dimula73@gmail.com>
2025-02-04 15:24:26 +0100

    Fix OCIO compilation on MacOS
    It seems like reinterpret cast was just forgotten

--- plugins/dockers/lut/ocio_display_filter_vfx2021.cpp
+++ plugins/dockers/lut/ocio_display_filter_vfx2021.cpp
@@ -363,8 +363,7 @@ bool OcioDisplayFilter::updateShader()
 #if defined(Q_OS_MAC) && defined(QT_OPENGL_3_2)
         QOpenGLVersionProfile profile(QOpenGLContext::currentContext()->surface()->format());
         profile.setVersion(3,2);
-        QAbstractOpenGLFunctions *f = QOpenGLVersionFunctionsFactory::get(profile, QOpenGLContext::currentContext());
-
+        QOpenGLFunctions_3_2 *f = reinterpret_cast<QOpenGLFunctions_3_2 *>(QOpenGLVersionFunctionsFactory::get(profile, QOpenGLContext::currentContext()));
 #else
         QOpenGLVersionProfile profile(QOpenGLContext::currentContext()->surface()->format());
         profile.setVersion(3,0);

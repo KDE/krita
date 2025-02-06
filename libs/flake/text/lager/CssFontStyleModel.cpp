d7c545946d23bcb8b2818be4a76f1ae77ae41063
Dmitry Kazakov <dimula73@gmail.com>
2025-02-06 11:34:34 +0100

    Reformat newly added lambdas in CssFontStyleModel

--- libs/flake/text/lager/CssFontStyleModel.cpp
+++ libs/flake/text/lager/CssFontStyleModel.cpp
@@ -7,36 +7,39 @@
 
 namespace  {
 
-auto getValue =  lager::lenses::getset (
-            [] (const KoSvgText::CssFontStyleData &data) -> qreal {
-    if (data.style == QFont::StyleOblique) {
-        if (data.slantValue.isAuto) {
-            return 14;
-        } else {
-            return data.slantValue.customValue;
+auto getValue = lager::lenses::getset(
+    [](const KoSvgText::CssFontStyleData &data) -> qreal {
+        if (data.style == QFont::StyleOblique) {
+            if (data.slantValue.isAuto) {
+                return 14;
+            } else {
+                return data.slantValue.customValue;
+            }
         }
-    }
-    return 0;
-}, [] (KoSvgText::CssFontStyleData data, const qreal &val) -> KoSvgText::CssFontStyleData {
+        return 0;
+    },
+    [](KoSvgText::CssFontStyleData data, const qreal &val) -> KoSvgText::CssFontStyleData {
         if (data.style == QFont::StyleOblique) {
             data.slantValue.isAuto = false;
             data.slantValue = val;
         }
         return data;
-});
+    });
 
-auto getStyle =  lager::lenses::getset (
-            [] (const KoSvgText::CssFontStyleData &data) -> CssFontStyleModel::FontStyle {
-    return CssFontStyleModel::FontStyle(data.style);
-}, [] (KoSvgText::CssFontStyleData data, const CssFontStyleModel::FontStyle &val) -> KoSvgText::CssFontStyleData {
+auto getStyle = lager::lenses::getset(
+    [](const KoSvgText::CssFontStyleData &data) -> CssFontStyleModel::FontStyle {
+        return CssFontStyleModel::FontStyle(data.style);
+    },
+    [](KoSvgText::CssFontStyleData data, const CssFontStyleModel::FontStyle &val) -> KoSvgText::CssFontStyleData {
         data.style = QFont::Style(val);
         if (data.style != QFont::StyleOblique) {
             data.slantValue.isAuto = true;
             data.slantValue = 0;
         }
         return data;
-});
-}
+    });
+
+} // namespace
 
 CssFontStyleModel::CssFontStyleModel(lager::cursor<KoSvgText::CssFontStyleData> _data)
     : data(_data)

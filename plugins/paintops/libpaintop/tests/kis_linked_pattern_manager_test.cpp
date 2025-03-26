1ddd1d6fb697bb170c2b710532f69bf008fb0d37
Dmitry Kazakov <dimula73@gmail.com>
2025-03-26 19:13:37 +0100

    Fix KisLinkedPatternManagerTest
    Now it also tests the issue with `PatternMD5` paintop preset's tag that should be in base64
    encoding

--- plugins/paintops/libpaintop/tests/kis_linked_pattern_manager_test.cpp
+++ plugins/paintops/libpaintop/tests/kis_linked_pattern_manager_test.cpp
@@ -6,7 +6,8 @@
 
 #include "kis_linked_pattern_manager_test.h"
 
-#include <simpletest.h>
+#include <testresources.h>
+
 #include <QPainter>
 
 #include <KoResourceServerProvider.h>
@@ -20,44 +21,102 @@
 #include <KoResourceServerProvider.h>
 #include <KoResourceLoadResult.h>
 
-#include <testresources.h>
+#include <KoMD5Generator.h>
 
-KoPatternSP KisLinkedPatternManagerTest::createPattern()
+KoPatternSP createPattern(const QString &name, const QString &fileName)
 {
     QImage image(512, 512, QImage::Format_ARGB32);
     image.fill(255);
 
     QPainter gc(&image);
-    gc.fillRect(100, 100, 312, 312, Qt::red);
+
+    /**
+     * Make sure that MD5 of every generated resource is different
+     */
+    const QString hash = KoMD5Generator::generateHash(fileName.toLatin1());
+    const QColor fillColor(
+            hash.toLatin1()[0],
+            hash.toLatin1()[1],
+            hash.toLatin1()[2],
+            hash.toLatin1()[3]);
+
+    gc.fillRect(100, 100, 312, 312, fillColor);
 
     KoPatternSP pattern (new KoPattern(image,
-                                       "__test_pattern",
-                                       KoResourceServerProvider::instance()->patternServer()->saveLocation()));
+                                       name,
+                                       fileName));
     return pattern;
+}
+
+
+void KisLinkedPatternManagerTest::testRoundTrip_data()
+{
+    QTest::addColumn<QString>("loadMode");
+
+    QTest::newRow("old-md5") << "old-md5";
+    QTest::newRow("new-md5") << "new-md5";
+    QTest::newRow("name") << "name";
+    QTest::newRow("filename") << "filename";
+    QTest::newRow("filename-with-path") << "filename-with-path";
 }
 
 void KisLinkedPatternManagerTest::testRoundTrip()
 {
-    KoPatternSP pattern = createPattern();
+    QFETCH(QString, loadMode);
+
+    QLatin1String tagName(QTest::currentDataTag());
+    const QString fileName = QLatin1String(QTest::currentTestFunction()) + "_" + tagName + "_pattern.pat";
+    const QString name = QLatin1String(QTest::currentTestFunction()) + "_" + tagName + "_pattern";
+
+    KoPatternSP pattern = createPattern(name, fileName);
 
     KoResourceServer<KoPattern> *resourceServer = KoResourceServerProvider::instance()->patternServer();
-    resourceServer->addResource(pattern, false);
+    resourceServer->addResource(pattern);
 
     KisPropertiesConfigurationSP config(new KisPropertiesConfiguration);
 
     KisEmbeddedTextureData data1 = KisEmbeddedTextureData::fromPattern(pattern);
     data1.write(config.data());
 
+    /**
+     * Test if each of the four tags alone is enough to load the pattern.
+     * Basically, we test if each tag is round-tripped correctly
+     */
+
+    if (loadMode != "old-md5") {
+        config->removeProperty("Texture/Pattern/PatternMD5");
+    }
+
+    if (loadMode != "new-md5") {
+        config->removeProperty("Texture/Pattern/PatternMD5Sum");
+    }
+
+    if (loadMode != "name") {
+        config->removeProperty("Texture/Pattern/Name");
+    }
+
+    if (loadMode != "filename") {
+        config->removeProperty("Texture/Pattern/PatternFileName");
+    }
+
+    if (loadMode == "filename-with-path") {
+        QString path = KoResourceServerProvider::instance()->patternServer()->saveLocation() + "/" + fileName;
+        config->setProperty("Texture/Pattern/PatternFileName", path);
+    }
+
     KisEmbeddedTextureData data2;
     data2.read(config.data());
 
-    KoPatternSP newPattern = data2.loadLinkedPattern(KisGlobalResourcesInterface::instance()).resource<KoPattern>();
+    KoResourceLoadResult result = data2.loadLinkedPattern(KisGlobalResourcesInterface::instance());
+
+    QCOMPARE(result.type(), KoResourceLoadResult::ExistingResource);
+    KoPatternSP newPattern = result.resource<KoPattern>();
+    QVERIFY(newPattern);
 
     QCOMPARE(newPattern->pattern(), pattern->pattern());
     QCOMPARE(newPattern->name(), pattern->name());
     QCOMPARE(QFileInfo(newPattern->filename()).fileName(),
              QFileInfo(pattern->filename()).fileName());
-
 }
 
 void KisLinkedPatternManagerTest::init()
@@ -75,45 +134,41 @@ void KisLinkedPatternManagerTest::init()
     }
 }
 
-KisPropertiesConfigurationSP KisLinkedPatternManagerTest::createXML(NameStatus nameStatus, bool hasMd5)
+KisPropertiesConfigurationSP KisLinkedPatternManagerTest::createXML(SaveDataFlags flags,
+                                                                    KoPatternSP pattern)
 {
 
     KisPropertiesConfigurationSP setting(new KisPropertiesConfiguration);
 
-    switch (nameStatus) {
-    case VALID: {
-        setting->setProperty("Texture/Pattern/PatternFileName", "./__test_pattern_path.pat");
-        setting->setProperty("Texture/Pattern/Name", "__test_pattern");
-        break;
+    if (flags.testFlag(SaveFileName)) {
+        setting->setProperty("Texture/Pattern/PatternFileName", pattern->filename());
     }
-    case PATH: {
-        QString path = KoResourceServerProvider::instance()->patternServer()->saveLocation() + "/__test_pattern.pat";
+
+    if (flags.testFlag(SaveFileNameWithPath)) {
+        QString path = KoResourceServerProvider::instance()->patternServer()->saveLocation() + "/" + pattern->filename();
         setting->setProperty("Texture/Pattern/PatternFileName", path);
-        setting->setProperty("Texture/Pattern/Name", "__test_pattern");
-        break;
-    }
-    case EMPTY: {
-        setting->setProperty("Texture/Pattern/PatternFileName", "./__test_pattern_path.pat");
-        setting->setProperty("Texture/Pattern/Name", "");
-        break;
-    }
     }
 
-    {
-        KoPatternSP pattern = createPattern();
+    if (flags.testFlag(SaveName)) {
+        setting->setProperty("Texture/Pattern/Name", pattern->name());
+    }
 
-        if (hasMd5) {
-            QString patternMD5 = pattern->md5Sum();
-            Q_ASSERT(!patternMD5.isEmpty());
-            setting->setProperty("Texture/Pattern/PatternMD5", patternMD5);
-        }
+    if (flags.testFlag(SaveOldMd5Base64)) {
+        QString patternMD5 = pattern->md5Sum();
+        KIS_ASSERT(!patternMD5.isEmpty());
 
+        /// WARNING: KisPropertiesConfiguration saved QByteArray as a base64 string!
+        ///          We don't do this conversion manually here!
+        setting->setProperty("Texture/Pattern/PatternMD5",
+                             QString(QByteArray::fromHex(patternMD5.toLatin1()).toBase64()));
+    }
+
+    if (flags.testFlag(SaveEmbeddedData)) {
         QBuffer buffer;
         buffer.open(QIODevice::WriteOnly);
-        pattern->pattern().save(&buffer, "PNG");
+        pattern->saveToDevice(&buffer);
         setting->setProperty("Texture/Pattern/Pattern", buffer.data().toBase64());
     }
-
 
     return setting;
 }
@@ -129,86 +184,92 @@ KoPatternSP findOnServer(const QString &md5)
     return pattern;
 }
 
-void KisLinkedPatternManagerTest::checkOneConfig(NameStatus nameStatus, bool hasMd5, QString expectedName, bool isOnServer)
+void KisLinkedPatternManagerTest::testLoadingLegacyXML_data()
 {
-    QSharedPointer<KoPattern> basePattern(createPattern());
+    QTest::addColumn<bool>("isOnServer");
+    QTest::addColumn<SaveDataFlags>("saveDataFlags");
 
-    KoPatternSP initialPattern = findOnServer(basePattern->md5Sum());
-    QCOMPARE((bool)initialPattern, isOnServer);
 
-    KisPropertiesConfigurationSP setting = createXML(nameStatus, hasMd5);
+    QTest::newRow("lnk-filename") << true << (SaveFileName | SaveEmbeddedData);
+    QTest::newRow("lnk-filename-with-path") << true << (SaveFileNameWithPath | SaveEmbeddedData);
+    QTest::newRow("lnk-name") << true << (SaveName | SaveEmbeddedData);
+    QTest::newRow("lnk-old-md5base64") << true << (SaveOldMd5Base64 | SaveEmbeddedData);
+
+    QTest::newRow("emb-filename") << false << (SaveFileName | SaveEmbeddedData);
+    QTest::newRow("emb-filename-with-path") << false << (SaveFileNameWithPath | SaveEmbeddedData);
+    QTest::newRow("emb-name") << false << (SaveName | SaveEmbeddedData);
+    QTest::newRow("emb-old-md5base64") << false << (SaveOldMd5Base64 | SaveEmbeddedData);
+
+}
+
+void KisLinkedPatternManagerTest::testLoadingLegacyXML()
+{
+    QFETCH(SaveDataFlags, saveDataFlags);
+    QFETCH(bool, isOnServer);
+
+    QLatin1String tagName(QTest::currentDataTag());
+    const QString fileName = QLatin1String(QTest::currentTestFunction()) + "_" + tagName + "_pattern.pat";
+    const QString name = QLatin1String(QTest::currentTestFunction()) + "_" + tagName + "_pattern";
+    QSharedPointer<KoPattern> basePattern(createPattern(name, fileName));
+
+    if (isOnServer) {
+        // upload the resource to the server if requested
+        KoResourceServerProvider::instance()->patternServer()->addResource(basePattern);
+        QVERIFY(findOnServer(basePattern->md5Sum()));
+    }
+
+    KisPropertiesConfigurationSP setting = createXML(saveDataFlags, basePattern);
 
     KisEmbeddedTextureData data2;
     data2.read(setting.data());
 
-    KoPatternSP pattern = data2.loadLinkedPattern(KisGlobalResourcesInterface::instance()).resource<KoPattern>();
+    KoResourceLoadResult result = data2.loadLinkedPattern(KisGlobalResourcesInterface::instance());
 
-    QVERIFY(pattern);
-    QCOMPARE(pattern->pattern(), basePattern->pattern());
-    QVERIFY(pattern->name().startsWith(expectedName));
+    if (isOnServer) {
+        KoPatternSP pattern = result.resource<KoPattern>();
 
-    QFileInfo info(pattern->filename());
-    QVERIFY(info.completeBaseName().startsWith(expectedName));
-    QCOMPARE(info.dir().path(), QDir(KoResourceServerProvider::instance()->patternServer()->saveLocation()).path());
+        QVERIFY(pattern);
+        QCOMPARE(pattern->pattern(), basePattern->pattern());
+        QCOMPARE(pattern->name(), basePattern->name());
+        QCOMPARE(pattern->filename(), basePattern->filename());
+    } else {
+        QCOMPARE(result.type(), KoResourceLoadResult::EmbeddedResource);
+        KoEmbeddedResource embeddedResource = result.embeddedResource();
+        QVERIFY(embeddedResource.sanityCheckMd5());
 
-    // We can only find things on the server by name or by md5; the file path as an identifier does not work.
-    if (isOnServer && nameStatus != EMPTY && !hasMd5) {
-        QCOMPARE(initialPattern, pattern);
+        if (saveDataFlags.testFlag(SaveOldMd5Base64)) {
+            QCOMPARE(embeddedResource.signature().md5sum, basePattern->md5Sum());
+        }
+
+        /// WARNING: it seems like mimeTypeForFile doesn't handle it gracefully
+        /// when the filename is empty. This code does **not** test handling of
+        /// this issue in the real code.
+        const QString effectiveFileName =
+                !embeddedResource.signature().filename.isEmpty() ?
+                    embeddedResource.signature().filename :
+                    basePattern->filename();
+
+        KisResourceLoaderBase *loader =
+            KisResourceLoaderRegistry::instance()->loader(
+                embeddedResource.signature().type,
+                KisMimeDatabase::mimeTypeForFile(effectiveFileName));
+
+        QByteArray ba = embeddedResource.data();
+        QBuffer buf(&ba);
+        buf.open(QBuffer::ReadOnly);
+
+        KoResourceSP resource = loader->load(effectiveFileName, buf, KisGlobalResourcesInterface::instance());
+
+        QVERIFY(resource);
+        QCOMPARE(resource->name(), basePattern->name());
+        QCOMPARE(resource->filename(), basePattern->filename());
+        // NOTE: md5 is explicitly set by the loading code, we cannot
+        //       verify it, since it can change after loading
+
+        KoPatternSP loadedPattern = resource.dynamicCast<KoPattern>();
+        QVERIFY(loadedPattern);
+        QCOMPARE(basePattern->pattern(), loadedPattern->pattern());
     }
-
-    // will be deleted by the server
-    // delete pattern;
-}
-
-void KisLinkedPatternManagerTest::testLoadingNotOnServerValidName()
-{
-    checkOneConfig(VALID, false, "__test_pattern", false);
-}
-
-void KisLinkedPatternManagerTest::testLoadingNotOnServerEmptyName()
-{
-    checkOneConfig(EMPTY, false, "__test_pattern_path", false);
-}
-
-void KisLinkedPatternManagerTest::testLoadingNotOnServerPathName()
-{
-    checkOneConfig(PATH, false, "__test_pattern", false);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerValidName()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), false);
-    checkOneConfig(VALID, false, "__test_pattern", true);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerEmptyName()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), false);
-    checkOneConfig(EMPTY, false, "__test_pattern_path", true);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerPathName()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), false);
-    checkOneConfig(PATH, false, "__test_pattern", true);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerValidNameMd5()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), true);
-    checkOneConfig(VALID, true, "__test_pattern", true);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerEmptyNameMd5()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), true);
-    checkOneConfig(EMPTY, true, "__test_pattern", true);
-}
-
-void KisLinkedPatternManagerTest::testLoadingOnServerPathNameMd5()
-{
-    KoResourceServerProvider::instance()->patternServer()->addResource(createPattern(), false);
-    checkOneConfig(PATH, true, "__test_pattern", true);
 }
 
 KISTEST_MAIN(KisLinkedPatternManagerTest)
--- plugins/paintops/libpaintop/tests/kis_linked_pattern_manager_test.h
+++ plugins/paintops/libpaintop/tests/kis_linked_pattern_manager_test.h
@@ -7,8 +7,6 @@
 #ifndef __KIS_LINKED_PATTERN_MANAGER_TEST_H
 #define __KIS_LINKED_PATTERN_MANAGER_TEST_H
 
-#include <simpletest.h>
-
 #include <kis_properties_configuration.h>
 
 #include <KoPattern.h>
@@ -16,33 +14,33 @@
 class KisLinkedPatternManagerTest : public QObject
 {
     Q_OBJECT
+public:
+    enum SaveDataFlag {
+        None = 0x0,
+        SaveName = 0x1,
+        SaveFileName = 0x2,
+        SaveFileNameWithPath = 0x4,
+        SaveOldMd5Base64 = 0x8,
+        SaveEmbeddedData = 0x10,
+    };
+    Q_DECLARE_FLAGS(SaveDataFlags, SaveDataFlag)
+
+
 private Q_SLOTS:
+    void testRoundTrip_data();
     void testRoundTrip();
 
     void init();
 
-    void testLoadingNotOnServerValidName();
-    void testLoadingNotOnServerEmptyName();
-    void testLoadingNotOnServerPathName();
-
-    void testLoadingOnServerValidName();
-    void testLoadingOnServerEmptyName();
-    void testLoadingOnServerPathName();
-
-    void testLoadingOnServerValidNameMd5();
-    void testLoadingOnServerEmptyNameMd5();
-    void testLoadingOnServerPathNameMd5();
+    void testLoadingLegacyXML_data();
+    void testLoadingLegacyXML();
 
 private:
-
-    enum NameStatus {
-        VALID,
-        PATH,
-        EMPTY
-    };
-    void checkOneConfig(NameStatus nameStatus, bool hasMd5, QString expectedName, bool isOnServer);
-    KisPropertiesConfigurationSP createXML(NameStatus nameStatus, bool hasMd5);
-    KoPatternSP createPattern();
+    KisPropertiesConfigurationSP createXML(SaveDataFlags flags, KoPatternSP pattern);
 };
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(KisLinkedPatternManagerTest::SaveDataFlags)
+Q_DECLARE_METATYPE(KisLinkedPatternManagerTest::SaveDataFlags)
+
 
 #endif /* __KIS_LINKED_PATTERN_MANAGER_TEST_H */

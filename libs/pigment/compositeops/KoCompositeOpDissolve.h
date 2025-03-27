KDE/krita

commit af99e90fa8a4dbbb98059c3c8a4d72daffbbda1d

Use QRandomGenerator instead of deprecated qrand()
qrand is deprecated and will be removed in Qt 6, so it makes sense to replace it.

libs/pigment/compositeops/KoCompositeOpDissolve.h
@@ -7,8 +7,10 @@
 #ifndef _KOCOMPOSITEOP_DISSOLVE_H_
 #define _KOCOMPOSITEOP_DISSOLVE_H_
 
-#include <KoCompositeOp.h>
 #include <KoColorSpaceMaths.h>
+#include <KoCompositeOp.h>
+
+#include <QRandomGenerator>
 
 template<class Traits>
 class KoCompositeOpDissolve: public KoCompositeOp
@@ -75,8 +77,8 @@ public:
                 channels_type dstAlpha = (alpha_pos == -1) ? unitValue : dst[alpha_pos];
                 channels_type blend    = useMask ? mul(opacity, scale<channels_type>(*mask), srcAlpha) : mul(opacity, srcAlpha);
 
-//                 if(getRandomValue(ctr) <= scale<quint8>(blend) && blend != KoColorSpaceMathsTraits<channels_type>::zeroValue) {
-                if((qrand() % 256) <= scale<quint8>(blend) && blend != KoColorSpaceMathsTraits<channels_type>::zeroValue) {
+                if (QRandomGenerator::global()->bounded(256) <= scale<quint8>(blend)
+                    && blend != KoColorSpaceMathsTraits<channels_type>::zeroValue) {
                     for(qint32 i=0; i <channels_nb; i++) {
                         if(i != alpha_pos && flags.testBit(i))
                             dst[i] = src[i];

plugins/tools/basictools/kis_tool_multihand.cpp
@@ -22,17 +22,10 @@
 
 #include "kis_tool_multihand_helper.h"
 
+#include <QtGlobal>
+#include <QRandomGenerator>
 
 static const int MAXIMUM_BRUSHES = 50;
-
-#include <QtGlobal>
-#ifdef Q_OS_WIN
-// quoting DRAND48(3) man-page:
-// These functions are declared obsolete by  SVID  3,
-// which  states  that rand(3) should be used instead.
-#define drand48() (static_cast<double>(qrand()) / static_cast<double>(RAND_MAX))
-#endif
-
 
 KisToolMultihand::KisToolMultihand(KoCanvasBase *canvas)
     : KisToolBrush(canvas),
@@ -385,8 +378,8 @@ void KisToolMultihand::initTransformations()
          * strokes
          */
         for (int i = 0; i < m_handsCount; i++){
-            const qreal angle = drand48() * M_PI * 2;
-            const qreal length = drand48();
+            const qreal angle = QRandomGenerator::global()->bounded(2.0 * M_PI);
+            const qreal length = QRandomGenerator::global()->bounded(1.0);
 
             // convert the Polar coordinates to Cartesian coordinates
             qreal nx = (m_translateRadius * cos(angle) * length);

krita/main.cc
@@ -32,8 +32,6 @@
 #include <QThread>
 #include <QTranslator>
 
-#include <random>
-
 #include <KisApplication.h>
 #include <KisMainWindow.h>
 #include <KisSupportedArchitectures.h>
@@ -244,11 +242,6 @@ extern "C" MAIN_EXPORT int MAIN_FN(int argc, char **argv)
     }
 #endif
 
-    // The global initialization of the random generator
-    {
-        std::random_device urandom;
-        qsrand(urandom());
-    }
     bool runningInKDE = !qgetenv("KDE_FULL_SESSION").isEmpty();
 
 #if defined HAVE_X11

libs/brush/tests/kis_gbr_brush_test.cpp
@@ -6,9 +6,11 @@
 
 #include "kis_gbr_brush_test.h"
 
-#include <simpletest.h>
+#include <QRandomGenerator>
 #include <QString>
 #include <QDir>
+
+#include <simpletest.h>
 #include <KoColor.h>
 #include <KoColorSpace.h>
 #include <KoColorSpaceRegistry.h>
@@ -89,16 +91,16 @@ void KisGbrBrushTest::testImageGeneration()
     Q_UNUSED(res);
     Q_ASSERT(res);
     QVERIFY(!brush->brushTipImage().isNull());
-    qsrand(1);
+    QRandomGenerator rng{};
 
     const KoColorSpace* cs = KoColorSpaceRegistry::instance()->rgb8();
     KisPaintInformation info(QPointF(100.0, 100.0), 0.5);
     KisFixedPaintDeviceSP dab;
 
     for (int i = 0; i < 200; i++) {
-        qreal scale = qreal(qrand()) / RAND_MAX * 2.0;
-        qreal rotation = qreal(qrand()) / RAND_MAX * 2 * M_PI;
-        qreal subPixelX = qreal(qrand()) / RAND_MAX * 0.5;
+        qreal scale = rng.bounded(2.0);
+        qreal rotation = rng.bounded(2.0 * M_PI);
+        qreal subPixelX = rng.bounded(0.5);
         QString testName =
             QString("brush_%1_sc_%2_rot_%3_sub_%4")
             .arg(i).arg(scale).arg(rotation).arg(subPixelX);
@@ -135,7 +137,7 @@ void KisGbrBrushTest::benchmarkScaling()
     QScopedPointer<KisGbrBrush> brush(new KisGbrBrush(QString(FILES_DATA_DIR) + '/' + "testing_brush_512_bars.gbr"));
     brush->load(KisGlobalResourcesInterface::instance());
     QVERIFY(!brush->brushTipImage().isNull());
-    qsrand(1);
+    QRandomGenerator rng{};
 
     const KoColorSpace* cs = KoColorSpaceRegistry::instance()->rgb8();
     KisPaintInformation info(QPointF(100.0, 100.0), 0.5);
@@ -143,13 +145,13 @@ void KisGbrBrushTest::benchmarkScaling()
 
     {
         // warm up the pyramid!
-        dab = brush->paintDevice(cs, KisDabShape(qreal(qrand()) / RAND_MAX * 2.0, 1.0, 0.0), info);
+        dab = brush->paintDevice(cs, KisDabShape(rng.bounded(2.0), 1.0, 0.0), info);
         QVERIFY(dab); // avoid compiler elimination of unused code!
         dab.clear();
     }
 
     QBENCHMARK {
-        dab = brush->paintDevice(cs, KisDabShape(qreal(qrand()) / RAND_MAX * 2.0, 1.0, 0.0), info);
+        dab = brush->paintDevice(cs, KisDabShape(rng.bounded(2.0), 1.0, 0.0), info);
         //dab->convertToQImage(0).save(QString("dab_%1_new_smooth.png").arg(i++));
     }
 }
@@ -159,14 +161,14 @@ void KisGbrBrushTest::benchmarkRotation()
     QScopedPointer<KisGbrBrush> brush(new KisGbrBrush(QString(FILES_DATA_DIR) + '/' + "testing_brush_512_bars.gbr"));
     brush->load(KisGlobalResourcesInterface::instance());
     QVERIFY(!brush->brushTipImage().isNull());
-    qsrand(1);
+    QRandomGenerator rng{};
 
     const KoColorSpace* cs = KoColorSpaceRegistry::instance()->rgb8();
     KisPaintInformation info(QPointF(100.0, 100.0), 0.5);
     KisFixedPaintDeviceSP dab;
 
     QBENCHMARK {
-        dab = brush->paintDevice(cs, KisDabShape(1.0, 1.0, qreal(qrand()) / RAND_MAX * 2 * M_PI), info);
+        dab = brush->paintDevice(cs, KisDabShape(1.0, 1.0, rng.bounded(2 * M_PI)), info);
     }
 }
 
@@ -175,7 +177,7 @@ void KisGbrBrushTest::benchmarkMaskScaling()
     QScopedPointer<KisGbrBrush> brush(new KisGbrBrush(QString(FILES_DATA_DIR) + '/' + "testing_brush_512_bars.gbr"));
     brush->load(KisGlobalResourcesInterface::instance());
     QVERIFY(!brush->brushTipImage().isNull());
-    qsrand(1);
+    QRandomGenerator rng{};
 
     const KoColorSpace* cs = KoColorSpaceRegistry::instance()->rgb8();
     KisPaintInformation info(QPointF(100.0, 100.0), 0.5);
@@ -183,7 +185,7 @@ void KisGbrBrushTest::benchmarkMaskScaling()
 
     QBENCHMARK {
         KoColor c(Qt::black, cs);
-        qreal scale = qreal(qrand()) / RAND_MAX * 2.0;
+        qreal scale = rng.bounded(2.0);
         brush->mask(dab, c, KisDabShape(scale, 1.0, 0.0), info, 0.0, 0.0, 1.0);
     }
 }

libs/image/brushengine/KisPerStrokeRandomSource.cpp
@@ -9,10 +9,11 @@
 #include <QHash>
 #include <QMutex>
 #include <QMutexLocker>
+#include <QRandomGenerator>
 
+#include <boost/random/normal_distribution.hpp>
 #include <boost/random/taus88.hpp>
 #include <boost/random/uniform_smallint.hpp>
-#include <boost/random/normal_distribution.hpp>
 
 struct KisPerStrokeRandomSource::Private
 {
@@ -39,7 +40,7 @@ struct KisPerStrokeRandomSource::Private
 };
 
 KisPerStrokeRandomSource::KisPerStrokeRandomSource()
-    : m_d(new Private(qrand()))
+    : m_d(new Private(QRandomGenerator::global()->generate()))
 {
 
 }

libs/image/brushengine/kis_random_source.cpp
@@ -6,15 +6,16 @@
 
 #include "kis_random_source.h"
 
+#include <boost/random/normal_distribution.hpp>
 #include <boost/random/taus88.hpp>
 #include <boost/random/uniform_smallint.hpp>
-#include <boost/random/normal_distribution.hpp>
 
+#include <QRandomGenerator>
 
 struct KisRandomSource::Private
 {
     Private()
-        : uniformSource(qrand()) {}
+        : uniformSource(QRandomGenerator::global()->generate()) {}
 
     Private(int seed)
         : uniformSource(seed) {}

libs/image/tests/kis_cage_transform_worker_test.cpp
@@ -8,6 +8,8 @@
 
 #include <simpletest.h>
 
+#include <QRandomGenerator>
+
 #include <KoProgressUpdater.h>
 #include <KoUpdater.h>
 
@@ -151,10 +153,10 @@ void KisCageTransformWorkerTest::testCageCounterclockwiseUnity()
 #include <QtGlobal>
 
 
-QPointF generatePoint(const QRectF &rc)
+QPointF generatePoint(const QRectF &rc, QRandomGenerator &rng)
 {
-    qreal cx = qreal(qrand()) / RAND_MAX;
-    qreal cy = qreal(qrand()) / RAND_MAX;
+    qreal cx = rng.bounded(1.0);
+    qreal cy = rng.bounded(1.0);
 
     QPointF diff = rc.bottomRight() - rc.topLeft();
 
@@ -179,7 +181,7 @@ void KisCageTransformWorkerTest::stressTestRandomCages()
     const int pixelPrecision = 8;
     QRectF bounds(dev->exactBounds());
 
-    qsrand(1);
+    QRandomGenerator rng{};
 
     for (int numPoints = 4; numPoints < 15; numPoints+=5) {
         for (int j = 0; j < 200; j++) {
@@ -189,8 +191,8 @@ void KisCageTransformWorkerTest::stressTestRandomCages()
             dbgKrita << ppVar(j);
 
             for (int i = 0; i < numPoints; i++) {
-                origPoints << generatePoint(bounds);
-                transfPoints << generatePoint(bounds);
+                origPoints << generatePoint(bounds, rng);
+                transfPoints << generatePoint(bounds, rng);
             }
 
             // no just hope it doesn't crash ;)

libs/image/tests/kis_fixed_paint_device_test.cpp
@@ -8,6 +8,7 @@
 #include <simpletest.h>
 
 #include <QTime>
+#include <QRandomGenerator>
 
 #include <KoColorSpace.h>
 #include <KoColor.h>
@@ -301,15 +302,15 @@ void KisFixedPaintDeviceTest::testMirroring()
 
     KoColor c(Qt::black, cs);
 
-    qsrand(1);
+    QRandomGenerator rng{};
     int value = 0;
 
     for (int i = rc.x(); i < rc.x() + rc.width(); i++) {
         for (int j = rc.y(); j < rc.y() + rc.height(); j++) {
             setPixel(dev, i, j, value);
-            value = qrand() % 255;
+            value = rng.bounded(255);
         }
-        value = qrand() % 255;
+        value = rng.bounded(255);
     }
 
     //dev->convertToQImage(0).save("0_a.png");
@@ -344,15 +345,15 @@ void KisFixedPaintDeviceTest::testMirroring()
         incY = 1;
     }
 
-    qsrand(1);
+    rng.seed(1);
     value = 0;
 
     for (int i = startX; i != endX ; i += incX) {
         for (int j = startY; j != endY; j += incY) {
             QCOMPARE(pixel(dev, i, j), (quint8)value);
-            value = qrand() % 255;
+            value = rng.bounded(255);
         }
-        value = qrand() % 255;
+        value = rng.bounded(255);
     }
 }
 

commit d211566997174204c611148941f8683399c76678

Fix speed regression in Dissolve blendmode
When porting to Qt6 qrand() was directly replaced to
QRandomGenerator::global(), while the latter one uses a global
mutex inside, which makes Dissolve blendmode work extremely slow.
Hence we should use a local copy of a random generator to remove this
bottle-neck.

The bug was introduced by:
https://invent.kde.org/graphics/krita/-/commit/af99e90fa8a4dbbb98059c3c8a4d72daffbbda1d

BUG:501932

libs/pigment/compositeops/KoCompositeOpDissolve.h
@@ -11,6 +11,7 @@
 #include <KoCompositeOp.h>
 
 #include <QRandomGenerator>
+#include <QThreadStorage>
 
 template<class Traits>
 class KoCompositeOpDissolve: public KoCompositeOp
@@ -45,13 +46,24 @@ class KoCompositeOpDissolve: public KoCompositeOp
         return randomValues[i];
     }
 
+    mutable QThreadStorage<QRandomGenerator> m_randomGenerator;
+
 public:
     KoCompositeOpDissolve(const KoColorSpace* cs, const QString& category)
-        : KoCompositeOp(cs, COMPOSITE_DISSOLVE, category) { }
+        : KoCompositeOp(cs, COMPOSITE_DISSOLVE, category)
+    {
+    }
 
     using KoCompositeOp::composite;
 
     void composite(const KoCompositeOp::ParameterInfo& params) const override {
+        /**
+         * Initialize local per-thread random generator from the global one, it will
+         * reduce contestion over the global generator in the main compositing loop.
+         */
+        if (!m_randomGenerator.hasLocalData()) {
+            m_randomGenerator.setLocalData(QRandomGenerator(QRandomGenerator::global()->generate()));
+        }
 
         const QBitArray& flags       = params.channelFlags.isEmpty() ? QBitArray(channels_nb,true) : params.channelFlags;
         bool             alphaLocked = (alpha_pos != -1) && !flags.testBit(alpha_pos);
@@ -80,7 +92,7 @@ public:
                 channels_type dstAlpha = (alpha_pos == -1) ? unitValue : dst[alpha_pos];
                 channels_type blend    = useMask ? mul(opacity, scale<channels_type>(*mask), srcAlpha) : mul(opacity, srcAlpha);
 
-                if (QRandomGenerator::global()->bounded(256) <= scale<quint8>(blend)
+                if (m_randomGenerator.localData().bounded(256) <= scale<quint8>(blend)
                     && blend != KoColorSpaceMathsTraits<channels_type>::zeroValue) {
                     for(qint32 i=0; i <channels_nb; i++) {
                         if(i != alpha_pos && flags.testBit(i))

plugins/tools/basictools/kis_tool_multihand.cpp
@@ -23,7 +23,7 @@
 #include "kis_tool_multihand_helper.h"
 
 #include <QtGlobal>
-#include <QRandomGenerator>
+
 
 static const int MAXIMUM_BRUSHES = 50;
 
@@ -40,6 +40,7 @@ KisToolMultihand::KisToolMultihand(KoCanvasBase *canvas)
       m_addSubbrushesMode(false),
       m_intervalX(0),
       m_intervalY(0)
+    , m_randomGenerator(QRandomGenerator::global()->generate())
     , customUI(0)
 {
 
@@ -381,8 +382,8 @@ void KisToolMultihand::initTransformations()
          * strokes
          */
         for (int i = 0; i < m_handsCount; i++){
-            const qreal angle = QRandomGenerator::global()->bounded(2.0 * M_PI);
-            const qreal length = QRandomGenerator::global()->bounded(1.0);
+            const qreal angle = m_randomGenerator.bounded(2.0 * M_PI);
+            const qreal length = m_randomGenerator.bounded(1.0);
 
             // convert the Polar coordinates to Cartesian coordinates
             qreal nx = (m_translateRadius * cos(angle) * length);

plugins/tools/basictools/kis_tool_multihand.h
@@ -11,6 +11,7 @@
 #include "kis_tool_brush.h"
 #include <kis_icon.h>
 #include "kis_tool_multihand_config.h"
+#include <QRandomGenerator>
 
 class QPushButton;
 class QCheckBox;
@@ -82,6 +83,7 @@ private:
 
     int m_intervalX;
     int m_intervalY;
+    QRandomGenerator m_randomGenerator;
 
     KisToolMultiHandConfigWidget* customUI;
 };
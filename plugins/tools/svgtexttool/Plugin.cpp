452dd4ecba2989dbe2d02794cb34a8ae6529351c
Dmitry Kazakov <dimula73@gmail.com>
2025-05-14 10:48:09 +0200

    Remove some forgotten unrelated changes

--- plugins/tools/svgtexttool/Plugin.cpp
+++ plugins/tools/svgtexttool/Plugin.cpp
@@ -21,7 +21,6 @@
 K_PLUGIN_FACTORY_WITH_JSON(PluginFactory, "krita_tool_svgtext.json", registerPlugin<Plugin>();)
 
 KIS_DECLARE_STATIC_INITIALIZER {
-    //qmlRegisterType<SvgTextLabel>("org.krita.tools.text", 1, 0, "SvgTextLabel");
     qmlRegisterType<GlyphPaletteProxyModel>("org.krita.tools.text", 1, 0, "GlyphPaletteProxyModel");
 }
 Plugin::Plugin(QObject *parent, const QVariantList &)

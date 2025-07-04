1220c4f9cba981c1665eb366f4e25a54ee8b3fbd
ross rosales <ross.erosales@gmail.com>
2025-07-04 12:06:17 -0500

    remove placeholder summary for KisSelectionAssistantDecoration

--- libs/ui/kis_selection_assistants_decoration.h
+++ libs/ui/kis_selection_assistants_decoration.h
@@ -16,17 +16,6 @@ class KisView;
 class KisSelectionAssistantsDecoration;
 typedef KisSharedPtr<KisSelectionAssistantsDecoration> KisSelectionAssistantsDecorationSP;
 
-/**
- * placeholder
- * KisSelectionAssistantsDecoration draws the assistants stored in the document on
- * the canvas.
- * In the application flow, each canvas holds one of these classes to manage the assistants
- * There is an assistants manager, but that is higher up in the flow and makes sure each view gets one of these
- * Since this is off the canvas level, the decoration can be seen across all tools. The contents from here will be in
- * front of the kis_assistant_tool, which hold and displays the editor controls.
- *
- * Many of the events this receives such as adding and removing assistants comes from kis_assistant_tool
- */
 class KRITAUI_EXPORT KisSelectionAssistantsDecoration
 {
     //Q_OBJECT

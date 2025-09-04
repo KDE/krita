KDE/krita

commit 0316f63a970191a421d5c34bdedd9774d674193b

UI/Animation/TimelineDocker: Can now re-select previous selected frame after pausing.
The default behavior of QAbstractItemModel was causing an edge case
where users couldn't re-select the starting frame of their animation after pausing
because it was technically already paused. Now, we've made it so that pausing
the animation playback will move the selection to the frame that the
animation was paused on, solving this issue.

BUG:500885

plugins/dockers/animation/KisAnimTimelineDocker.cpp
@@ -618,7 +618,7 @@ void KisAnimTimelineDocker::setViewManager(KisViewManager *view)
     action = actionManager->createAction("toggle_playback");
     action->setActivationFlags(KisAction::ACTIVE_IMAGE);
     connect(action, &KisAction::triggered, this, [this](bool){
-        m_d->playbackEngine->playPause();
+        togglePlayback(); //TODO: bind normally
     });
 
     action = actionManager->createAction("stop_playback");
@@ -724,7 +724,7 @@ void KisAnimTimelineDocker::setPlaybackEngine(KisPlaybackEngine *playbackEngine)
     connect(m_d->titlebar->transport, SIGNAL(skipBack()), playbackEngine, SLOT(previousKeyframe()));
     connect(m_d->titlebar->transport, SIGNAL(back()), playbackEngine, SLOT(previousFrame()));
     connect(m_d->titlebar->transport, SIGNAL(stop()), playbackEngine, SLOT(stop()));
-    connect(m_d->titlebar->transport, SIGNAL(playPause()), playbackEngine, SLOT(playPause()));
+    connect(m_d->titlebar->transport, SIGNAL(playPause()), this, SLOT(togglePlayback()));
     connect(m_d->titlebar->transport, SIGNAL(forward()), playbackEngine, SLOT(nextFrame()));
     connect(m_d->titlebar->transport, SIGNAL(skipForward()), playbackEngine, SLOT(nextKeyframe()));
 
@@ -733,6 +733,30 @@ void KisAnimTimelineDocker::setPlaybackEngine(KisPlaybackEngine *playbackEngine)
     m_d->controlsModel.connectPlaybackEngine(playbackEngine);
 
     m_d->playbackEngine = playbackEngine;
+}
+
+void KisAnimTimelineDocker::togglePlayback()
+{
+    m_d->playbackEngine->playPause();
+
+    KisImageAnimationInterface *animInterface = m_d->canvas->image()->animationInterface();
+    KisCanvasAnimationState *animState = m_d->canvas->animationState();
+
+    if (!animInterface || !animState) return;
+
+    if (animState->playbackState() == PlaybackState::PAUSED) {
+        QModelIndexList selectedIndices = m_d->framesView->selectionModel()->selectedIndexes();
+        QModelIndex currentIndex = m_d->framesView->currentIndex();
+        const int pausedTime = animInterface->currentTime();
+
+        QModelIndex pauseIndex = m_d->framesModel->index(currentIndex.row(), pausedTime);
+
+        if (!pauseIndex.isValid()) return;
+
+        if (!selectedIndices.contains(pauseIndex)) {
+            m_d->framesView->setCurrentIndex(pauseIndex);
+        }
+    }
 }
 
 void KisAnimTimelineDocker::setAutoKey(bool value)

libs/ui/KisPlaybackEngine.cpp
@@ -52,7 +52,11 @@ void KisPlaybackEngine::play()
 void KisPlaybackEngine::pause()
 {
     KIS_SAFE_ASSERT_RECOVER_RETURN(activeCanvas() && activeCanvas()->animationState());
-    activeCanvas()->animationState()->setPlaybackState(PAUSED);
+    KisCanvasAnimationState* animationState = activeCanvas()->animationState();
+
+    animationState->setPlaybackState(PAUSED);
+
+    seek(animationState->displayProxy()->activeFrame(), SEEK_FINALIZE);
 }
 
 void KisPlaybackEngine::playPause()
@@ -61,8 +65,7 @@ void KisPlaybackEngine::playPause()
     KisCanvasAnimationState* animationState = activeCanvas()->animationState();
 
     if (animationState->playbackState() == PLAYING) {
-        pause();
-        seek(animationState->displayProxy()->activeFrame(), SEEK_FINALIZE);
+        pause();  
     } else {
         play();
     }

libs/ui/KisPlaybackEngineQT.cpp
@@ -180,6 +180,7 @@ void KisPlaybackEngineQT::seek(int frameIndex, SeekOptionFlags flags)
         return;
 
     KIS_SAFE_ASSERT_RECOVER_RETURN(activeCanvas()->animationState());
+
     KisFrameDisplayProxy* displayProxy = activeCanvas()->animationState()->displayProxy();
     KIS_SAFE_ASSERT_RECOVER_RETURN(displayProxy);
 

plugins/dockers/animation/KisAnimTimelineDocker.h
@@ -101,6 +101,7 @@ public:
     void setPlaybackEngine(KisPlaybackEngine *playbackEngine);
 
 public Q_SLOTS:
+    void togglePlayback();
     void setAutoKey(bool value);
 
     void handleFrameRateChange();

plugins/dockers/animation/KisAnimTimelineFramesView.cpp
@@ -898,7 +898,6 @@ void KisAnimTimelineFramesView::mousePressEvent(QMouseEvent *event)
     QPersistentModelIndex index = indexAt(event->pos());
 
     if (m_d->modifiersCatcher->modifierPressed("pan-zoom")) {
-
         if (event->button() == Qt::RightButton) {
             // TODO: try calculate index under mouse cursor even when
             //       it is outside any visible row
@@ -913,7 +912,6 @@ void KisAnimTimelineFramesView::mousePressEvent(QMouseEvent *event)
         event->accept();
 
     } else if (event->button() == Qt::RightButton) {
-
         int numSelectedItems = selectionModel()->selectedIndexes().size();
 
         if (index.isValid() &&

commit ef74e03a4058953926971d2177f3164e3b51770f

Animation/Audio: Fixed playback when audio is attached.
The bugfix associated with commit 0316f63a970191a421d5c34bdedd9774d674193b
inadvertently broke animation playback after audio was attached due to
a 2x signal/slot connection causing the play/pause button to activate
twice upon switching to the PlaybackEngineMLT.

Switching the connections between the transport controls and playback engine
to use Qt::UniqueConnection fixes this problem.

CCBUG:500885

plugins/dockers/animation/KisAnimTimelineDocker.cpp
@@ -701,14 +701,14 @@ void KisAnimTimelineDocker::setPlaybackEngine(KisPlaybackEngine *playbackEngine)
     if (!playbackEngine) return;
 
     // Connect transport controls..
-    connect(m_d->titlebar->transport, SIGNAL(skipBack()), playbackEngine, SLOT(previousKeyframe()));
-    connect(m_d->titlebar->transport, SIGNAL(back()), playbackEngine, SLOT(previousFrame()));
-    connect(m_d->titlebar->transport, SIGNAL(stop()), playbackEngine, SLOT(stop()));
-    connect(m_d->titlebar->transport, SIGNAL(playPause()), this, SLOT(togglePlayback()));
-    connect(m_d->titlebar->transport, SIGNAL(forward()), playbackEngine, SLOT(nextFrame()));
-    connect(m_d->titlebar->transport, SIGNAL(skipForward()), playbackEngine, SLOT(nextKeyframe()));
+    connect(m_d->titlebar->transport, SIGNAL(skipBack()), playbackEngine, SLOT(previousKeyframe()), Qt::UniqueConnection);
+    connect(m_d->titlebar->transport, SIGNAL(back()), playbackEngine, SLOT(previousFrame()), Qt::UniqueConnection);
+    connect(m_d->titlebar->transport, SIGNAL(stop()), playbackEngine, SLOT(stop()), Qt::UniqueConnection);
+    connect(m_d->titlebar->transport, SIGNAL(playPause()), this, SLOT(togglePlayback()), Qt::UniqueConnection);
+    connect(m_d->titlebar->transport, SIGNAL(forward()), playbackEngine, SLOT(nextFrame()), Qt::UniqueConnection);
+    connect(m_d->titlebar->transport, SIGNAL(skipForward()), playbackEngine, SLOT(nextKeyframe()), Qt::UniqueConnection);
 
-    connect(m_d->titlebar->frameRegister, SIGNAL(valueChanged(int)), playbackEngine, SLOT(seek(int)));
+    connect(m_d->titlebar->frameRegister, SIGNAL(valueChanged(int)), playbackEngine, SLOT(seek(int)), Qt::UniqueConnection);
 
     m_d->controlsModel.connectPlaybackEngine(playbackEngine);
 
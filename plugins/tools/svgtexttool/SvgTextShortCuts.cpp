8ca09c1b94ed4e64b366a9cff2682ad7bd69000e
Roald Clark <roaldclark@gmail.com>
2025-09-24 13:34:50 +0000

    Fix Qt6 compilation in SvgTextShortCuts.cpp
    Move Q_DECLARE_METATYPE(SvgTextShortcutInfo) below the struct definition
    to ensure the type is fully defined before registration.
    
    Signed-off-by: Roald Clark <roaldclark@gmail.com>

--- plugins/tools/svgtexttool/SvgTextShortCuts.cpp
+++ plugins/tools/svgtexttool/SvgTextShortCuts.cpp
@@ -10,7 +10,6 @@
 #include <KisStaticInitializer.h>
 
 class SvgTextShortcutInfo;
-Q_DECLARE_METATYPE(SvgTextShortcutInfo)
 KIS_DECLARE_STATIC_INITIALIZER {
     qRegisterMetaType<SvgTextShortcutInfo>("SvgTextShortcutInfo");
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
@@ -75,6 +74,8 @@ struct SvgTextShortcutInfo : public boost::equality_comparable<SvgTextShortcutIn
                 && testValue == other.testValue);
     }
 };
+
+Q_DECLARE_METATYPE(SvgTextShortcutInfo)
 
 
 

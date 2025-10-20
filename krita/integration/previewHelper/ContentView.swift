955bbc3d6cb3210910cc99413905ab31123285b9
Ivan Yossi <ghevan@gmail.com>
2025-10-20 02:37:44 -0600

    macos: working version of thumbnailer
    This adds a PreviewHelper app to embed the thumbnailer to it. We need this
    as application extensions does not register to lsregister if they are not
    embedded inside a bundle application.
    
    Small fixes to all project configurations
    
    The thumbnailer works as expected, special care needs to be taken with
    request.scale as it has the current screen scaling, cgImage need to take
    this into account.
    
    CCBUG:493474
    (cherry picked from commit 921f9b73682df40f5194f6e60831c9d4e5460d75)

---
+++ krita/integration/previewHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  previewHelper
+//
+//  Created by Iván Yossi on 19/10/25.
+//  Copyright © 2025 Stichting Krita Foundation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ krita/integration/previewHelper/previewHelperApp.swift
@@ -0,0 +1,18 @@
+//
+//  previewHelperApp.swift
+//  previewHelper
+//
+//  Created by Iván Yossi on 19/10/25.
+//  Copyright © 2025 Stichting Krita Foundation. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct previewHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

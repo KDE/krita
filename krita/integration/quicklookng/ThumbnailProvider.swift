921f9b73682df40f5194f6e60831c9d4e5460d75
Ivan Yossi <ghevan@gmail.com>
2025-10-20 02:37:44 -0600

    macos: working version of thumbnailer
    This adds a PreviewHelper app to embed the thumbnailer to it. We need this
    as application extensions does not register to lsregister if they are not
    embedded inside a bundle application.
    
    Small fixes to all project configurations
    
    The thumbnailer works as expected, special care needs to be taken with
    request.scale as it has the current screen scaling, cgImage need to take
    this into account.
    
    CCBUG:493474

--- krita/integration/quicklookng/ThumbnailProvider.swift
+++ krita/integration/quicklookng/ThumbnailProvider.swift
@@ -22,6 +22,7 @@
 import AppKit
 import QuickLookThumbnailing
 
+
 class ThumbnailProvider: QLThumbnailProvider {
 
 //    override init() {
@@ -29,7 +30,9 @@ class ThumbnailProvider: QLThumbnailProvider {
 //        super.init()
 //    }
     override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        print("handler called org.krita.kra")
+    #if DEBUG
+        NSLog("handler called org.krita.kra")
+    #endif
         // There are three ways to provide a thumbnail through a QLThumbnailReply. Only one of them should be used.
 
         // First way: Draw the thumbnail into the current context, set up with UIKit's coordinate system.
@@ -39,44 +42,69 @@ class ThumbnailProvider: QLThumbnailProvider {
             // Return true if the thumbnail was successfully drawn inside this block.
             return true
         }), nil) */
-
+        
 
 
         // Second way: Draw the thumbnail into a context passed to your block, set up with Core Graphics's coordinate system.
-//        handler(QLThumbnailReply(contextSize: request.maximumSize, drawing: { (context: CGContext) -> Bool in
-        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
-            // Draw the thumbnail here.
+        
+        var appPlist = UnzipTask(request.fileURL.path, "preview.png")
 
-            var appPlist = UnzipTask(request.fileURL.path, "preview.png")
+        // Not made with Krita. Find ORA thumbnail instead.
+        if (appPlist == nil || appPlist!.isEmpty) {
+            appPlist = UnzipTask(request.fileURL.path, "Thumbnails/thumbnail.png");
+        }
 
-            // Not made with Krita. Find ORA thumbnail instead.
-            if (appPlist == nil || appPlist!.isEmpty) {
-                appPlist = UnzipTask(request.fileURL.path, "Thumbnails/thumbnail.png");
-            }
-
-            if (appPlist != nil && !appPlist!.isEmpty) {
-                let appIcon = NSImage.init(data: appPlist!)
-
-//                let rep = appIcon!.representations.first!
-
-                let renderRect = NSMakeRect(0.0, 0.0, request.maximumSize.width, request.maximumSize.height)
+        if (appPlist != nil && !appPlist!.isEmpty) {
+            let appIcon = NSImage.init(data: appPlist!)
             
-                appIcon?.draw(in: renderRect)
-                
-//              guard let cgImage = appIcon?.cgImage(forProposedRect: &renderRect, context: nil, hints: nil)
-//                else {
-//                  return false
-//              }
-//
-//              context.draw(cgImage, in: renderRect);
-            } else {
-              return false
+            let rep = appIcon!.representations.first!
+
+#if DEBUG
+            NSLog("kritaquicklookngs file: \(request.fileURL.path)")
+            for representation in appIcon!.representations {
+                if let bitmapRep = representation as? NSBitmapImageRep {
+                    NSLog("kritaquicklookngs bmp: \(bitmapRep.pixelsWide)x\(bitmapRep.pixelsHigh)")
+                    // Access bitmap data, properties, etc.
+                } else {
+                    NSLog("kritaquicklookngs: no rep")
+                }
             }
-
-            // Return true if the thumbnail was successfully drawn inside this block.
-            return true
-        }), nil)
-
+#endif
+            
+            // asume request rect is a rectangle
+            var dstwidth: Float
+            var dstheight: Float
+            let ratio: Float = Float(rep.pixelsHigh) / Float(rep.pixelsWide)
+            if ratio > 1.0 {
+                dstwidth = Float(request.maximumSize.height) * (1 / ratio)
+                dstheight = Float(request.maximumSize.height)
+            } else {
+                dstwidth = Float(request.maximumSize.width)
+                dstheight = Float(request.maximumSize.width) * ratio
+            }
+            var renderRect = NSMakeRect(0.0, 0.0,
+                        CGFloat(dstwidth),
+                        CGFloat(dstheight)
+            )
+#if DEBUG
+            NSLog("kritaquicklookngs: \(renderRect.size) \(request.maximumSize) \(dstwidth)x\(dstheight) - \(request.maximumSize.width) x \(request.maximumSize.height)")
+            NSLog("kritaquicklookngs_calc: ratio: \(ratio) scale \(request.scale)")
+#endif
+            
+            handler(QLThumbnailReply(contextSize: renderRect.size, drawing: { (context: CGContext) -> Bool in
+                //          handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
+                // Draw the thumbnail here.
+                //                appIcon?.draw(in: renderRect)
+                
+                let cgImage = appIcon?.cgImage(forProposedRect: &renderRect, context: nil, hints: nil);
+                renderRect.size.width *= request.scale
+                renderRect.size.height *= request.scale
+                context.draw(cgImage!, in: renderRect);
+                
+                // Return true if the thumbnail was successfully drawn inside this block.
+                return true
+            }), nil)
+        }
         // Third way: Set an image file URL.
         /* handler(QLThumbnailReply(imageFileURL: Bundle.main.url(forResource: "fileThumbnail", withExtension: "jpg")!), nil) */
     }

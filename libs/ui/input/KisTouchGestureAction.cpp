8f7d433eb3768e74b823067e900010b86ae8da7f
Carsten Hartenfels <carsten.hartenfels@pm.me>
2025-10-26 16:54:27 +0100

    Fix various touch shortcuts with wrong actions
    The freehand, color sampler and transform tool had outright nonexistent
    actions assigned to them. The freehand selection tool instead caused the
    magic wand to activate. Those are now all corrected to actually call
    upon the tools they proclaim to summon.
    
    This also removes a bunch of duplication and adds warnings when an
    action isn't found, rather than just silently doing nothing and leaving
    you wondering.
    
    BUG:485161

--- libs/ui/input/KisTouchGestureAction.cpp
+++ libs/ui/input/KisTouchGestureAction.cpp
@@ -36,52 +36,41 @@ void KisTouchGestureAction::begin(int shortcut, QEvent *event)
 void KisTouchGestureAction::end(QEvent *event)
 {
     Q_UNUSED(event)
-    KisKActionCollection *actionCollection = KisPart::instance()->currentMainwindow()->actionCollection();
+
+    QString actionName;
     switch (m_shortcut) {
-    case UndoActionShortcut: {
-        QAction *action = actionCollection->action("edit_undo");
-        if (action) {
-            action->trigger();
-        }
+    case UndoActionShortcut:
+        actionName = QStringLiteral("edit_undo");
+        break;
+    case RedoActionShortcut:
+        actionName = QStringLiteral("edit_redo");
+        break;
+    case ToggleCanvasOnlyShortcut:
+        actionName = QStringLiteral("view_show_canvas_only");
+        break;
+    case ToggleEraserMode:
+        actionName = QStringLiteral("erase_action");
+        break;
+    case ResetDisplay:
+        actionName = QStringLiteral("reset_display");
+        break;
+    case ToggleEraserPreset:
+        actionName = QStringLiteral("eraser_preset_action");
         break;
     }
-    case RedoActionShortcut: {
-        QAction *action = actionCollection->action("edit_redo");
+
+    if (actionName.isEmpty()) {
+        qWarning("KisTouchGestureAction: Unhandled shortcut %d", m_shortcut);
+    } else {
+        KisKActionCollection *actionCollection = KisPart::instance()->currentMainwindow()->actionCollection();
+        QAction *action = actionCollection->action(actionName);
         if (action) {
             action->trigger();
+        } else {
+            qWarning("KisTouchGestureAction: unable to find action '%s' for shortcut %d",
+                     qUtf8Printable(actionName),
+                     m_shortcut);
         }
-        break;
-    }
-    case ToggleCanvasOnlyShortcut: {
-        QAction *action = actionCollection->action("view_show_canvas_only");
-        if (action) {
-            action->trigger();
-        }
-        break;
-    }
-    case ToggleEraserMode: {
-        QAction *action = actionCollection->action("erase_action");
-        if (action) {
-            action->trigger();
-        }
-        break;
-    }
-    case ResetDisplay: {
-        QAction *action = actionCollection->action("reset_display");
-        if (action) {
-            action->trigger();
-        }
-        break;
-    }
-    case ToggleEraserPreset: {
-        QAction *action = actionCollection->action("eraser_preset_action");
-        if (action) {
-            action->trigger();
-        }
-        break;
-    }
-    default:
-        break;
     }
 }
 

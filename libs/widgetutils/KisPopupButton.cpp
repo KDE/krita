8cf630e0a2e04d1f885b80733c6a91d63892273c
Dmitry Kazakov <dimula73@gmail.com>
2025-10-29 17:12:15 +0300

    Fix hidden brush editor titlebar (Windows + Android)
    It hides behind top left screen corner upon first open
    on Windows and Android
    
    BUG:452804
    
    
    (cherry picked from commit eb4d34a82ed01922c536c609f62f3a32cefaea97)
    
    7f14ea10 Fix brush editor titlebar position behind screen area
    dd9ca3ed Return m_d->frame->raise()
    
    Co-authored-by: Pavel shlop <shishlo360@gmail.com>

--- libs/widgetutils/KisPopupButton.cpp
+++ libs/widgetutils/KisPopupButton.cpp
@@ -156,7 +156,6 @@ void KisPopupButton::showPopupWidget()
 {
     if (m_d->popupWidget && !m_d->frame->isVisible()) {
         setPopupWidgetVisible(true);
-        adjustPosition();
     } else {
         hidePopupWidget();
     }
@@ -171,6 +170,10 @@ void KisPopupButton::setPopupWidgetVisible(bool visible)
 {
     if (m_d->popupWidget) {
         if (visible) {
+            // Force layout sizing before positioning
+            m_d->popupWidget->adjustSize();
+            m_d->frame->adjustSize();
+            adjustPosition();
             m_d->frame->raise();
             m_d->frame->show();
             m_d->frame->activateWindow();

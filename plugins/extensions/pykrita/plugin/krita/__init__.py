7a40a9227b88609e804b2983738074a3dc9d4e3d
Dmitry Kazakov <dimula73@gmail.com>
2026-03-04 10:47:52 +0300

    Avoid loading conflicting version of PyQt
    If the user has another version of PyQt installed,
    loading it would end up crashing Krita, so avoid that
    by removing its parent folder from Python's sys.path.
    
    BUG:513241
    
    
    (cherry picked from commit 38bbeb8a1882150379fafde54f25222e5fade146)
    
    Co-authored-by: Freya Lupen <penguinflyer2222@gmail.com>

--- plugins/extensions/pykrita/plugin/krita/__init__.py
+++ plugins/extensions/pykrita/plugin/krita/__init__.py
@@ -8,6 +8,29 @@ import pykrita
 import os
 import sys
 
+# Try to make sure there's no conflicting version of PyQt on sys.path, because
+# loading that would load a conflicting version of Qt and crash Krita when used.
+# However, this will also cause any neighboring modules to be removed.
+import importlib.util
+pyqt_wrong = "PyQt" + ("6" if pykrita.qt_major_version() == 5 else "5")
+if (spec := importlib.util.find_spec(pyqt_wrong)):
+    parent_folder = os.path.dirname(os.path.dirname(spec.origin))
+    if sys.path.__contains__(parent_folder):
+        print(f"Found {pyqt_wrong} in '{parent_folder}', "
+               "removing from Python's search path to avoid crashes.", file=sys.stderr)
+        sys.path.remove(parent_folder)
+
+# Look for PyQt
+try:
+    from PyQt6 import QtCore
+except ImportError:
+    try:
+        from PyQt5 import QtCore
+    except ImportError:
+        print("Python cannot find the Qt5 bindings.", file=sys.stderr)
+        print("Please make sure, that the needed packages are installed.", file=sys.stderr)
+        raise
+
 from .api import *
 from .decorators import *
 from .dockwidgetfactory import *
@@ -19,17 +42,6 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 krita_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, krita_path)
 print("%s added to PYTHONPATH" % krita_path, file=sys.stderr)
-
-# Look for PyQt
-try:
-    from PyQt6 import QtCore
-except:
-    try:
-        from PyQt5 import QtCore
-    except ImportError:
-        print("Python cannot find the Qt5 bindings.", file=sys.stderr)
-        print("Please make sure, that the needed packages are installed.", file=sys.stderr)
-        raise
 
 # Shows nice looking error dialog if an unhandled exception occurs.
 import excepthook

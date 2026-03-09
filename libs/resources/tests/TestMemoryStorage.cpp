fe3c46e1c06a06247985f43605e9bf78ad40c85a
Dmitry Kazakov <dimula73@gmail.com>
2026-03-09 15:20:28 +0100

    Fix memory storage to handle resources with subfolders
    BUG:517221

--- libs/resources/tests/TestMemoryStorage.cpp
+++ libs/resources/tests/TestMemoryStorage.cpp
@@ -7,6 +7,8 @@
 #include "TestMemoryStorage.h"
 #include <simpletest.h>
 
+#include <QBuffer>
+
 #include <KisMemoryStorage.h>
 #include <KoResource.h>
 
@@ -62,6 +64,54 @@ void TestMemoryStorage::testAddResource()
 
     ResourceTestHelper::testVersionedStorage(memoryStorage, ResourceType::Brushes, "brushes/test1.0000.gbr");
     ResourceTestHelper::testVersionedStorageIterator(memoryStorage, ResourceType::Brushes, "brushes/test1.0000.gbr");
+}
+
+void TestMemoryStorage::testVersionSaveWithASubfolder()
+{
+    KisMemoryStorage memoryStorage;
+    KoResourceSP res1(new DummyResource("subfolder/test1.gbr", ResourceType::Brushes));
+    memoryStorage.saveAsNewVersion(ResourceType::Brushes, res1);
+
+    // saving a version of a resource is expected to strip all the subfolders
+    // from the resource file path
+
+    QString url = QString("brushes/test1.0000.gbr");
+    KoResourceSP resource = memoryStorage.resource(url);
+    QVERIFY(resource);
+    QCOMPARE(resource->filename(), "test1.0000.gbr");
+}
+
+void TestMemoryStorage::testImportExportWithASubfolder()
+{
+    KisMemoryStorage memoryStorage;
+    KoResourceSP res1(new DummyResource("test1.gbr", ResourceType::Brushes));
+
+    {
+        QBuffer buf;
+        buf.open(QBuffer::WriteOnly);
+        res1->saveToDevice(&buf);
+        buf.close();
+
+        KIS_ASSERT(buf.size() > 0);
+
+        buf.open(QBuffer::ReadOnly);
+        memoryStorage.importResource("brushes/subfolder/test1.gbr", &buf);
+        buf.close();
+    }
+
+    KoResourceSP resource = memoryStorage.resource("brushes/subfolder/test1.gbr");
+    QVERIFY(resource);
+    QCOMPARE(resource->filename(), "subfolder/test1.gbr");
+    QVERIFY(!resource->md5Sum().isEmpty());
+
+    {
+        QBuffer buf;
+        buf.open(QBuffer::WriteOnly);
+        memoryStorage.exportResource("brushes/subfolder/test1.gbr", &buf);
+        buf.close();
+        QVERIFY(buf.size() > 0);
+    }
+
 }
 
 SIMPLE_TEST_MAIN(TestMemoryStorage)

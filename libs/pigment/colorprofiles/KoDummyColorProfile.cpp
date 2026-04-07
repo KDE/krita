f3a74a99ae51036bb630b8b96e518b25918da6c1
Dmitry Kazakov <dimula73@gmail.com>
2026-04-07 10:58:34 +0200

    Fix colorants values in KoDummyColorProfile
    We don't use this class in Krita normally, only in unittests.

--- libs/pigment/colorprofiles/KoDummyColorProfile.cpp
+++ libs/pigment/colorprofiles/KoDummyColorProfile.cpp
@@ -80,28 +80,43 @@ bool KoDummyColorProfile::isLinear() const
 }
 QVector<double> KoDummyColorProfile::getColorantsXYZ() const
 {
-    QVector<double> d50Dummy(3);
-    d50Dummy<<0.34773<<0.35952<<1.0;
-    return d50Dummy;
+    QVector<double> sRGBStandardAdaptedColorants = {
+        0.43603516, 0.22248840, 0.01391602,
+        0.38511658, 0.71690369, 0.09706116,
+        0.14305115, 0.06060791, 0.71392822
+    };
+    return sRGBStandardAdaptedColorants;
 }
 
 QVector<double> KoDummyColorProfile::getColorantsxyY() const
 {
-    QVector<double> d50Dummy(3);
-    d50Dummy<<0.34773<<0.35952<<1.0;
-    return d50Dummy;
+    QVector<double> result(9);
+    auto srgb = getColorantsXYZ();
+    result[0] = srgb[0] / (srgb[0] + srgb[1] + srgb[2]);
+    result[1] = srgb[1] / (srgb[0] + srgb[1] + srgb[2]);
+    result[2] = srgb[1];
+
+    result[3] = srgb[3] / (srgb[3] + srgb[4] + srgb[5]);
+    result[4] = srgb[4] / (srgb[3] + srgb[4] + srgb[5]);
+    result[5] = srgb[4];
+
+    result[6] = srgb[6] / (srgb[6] + srgb[7] + srgb[8]);
+    result[7] = srgb[7] / (srgb[6] + srgb[7] + srgb[8]);
+    result[8] = srgb[7];
+
+    return result;
 }
 
 QVector<double> KoDummyColorProfile::getWhitePointXYZ() const
 {
-    QVector<double> d50Dummy(3);
+    QVector<double> d50Dummy;
     d50Dummy<<0.9642<<1.0000<<0.8249;
     return d50Dummy;
 }
 
 QVector<double> KoDummyColorProfile::getWhitePointxyY() const
 {
-    QVector<double> d50Dummy(3);
+    QVector<double> d50Dummy;
     d50Dummy<<0.34773<<0.35952<<1.0;
     return d50Dummy;
 }

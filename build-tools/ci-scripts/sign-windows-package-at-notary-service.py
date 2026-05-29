7e47b0fb66cd91c68a4bbf837858c3bc2776bf2a
Dmitry Kazakov <dimula73@gmail.com>
2026-05-29 10:14:13 +0200

    Implement verification of signing on Windows for release builds

--- build-tools/ci-scripts/sign-windows-package-at-notary-service.py
+++ build-tools/ci-scripts/sign-windows-package-at-notary-service.py
@@ -10,6 +10,10 @@ parser.add_argument("pkg_root", action='store', help="Specify the package root")
 args = parser.parse_args()
 pkg_root = args.pkg_root
 
+release_package_naming = False
+if 'KRITACI_RELEASE_PACKAGE_NAMING' in os.environ:
+    release_package_naming = (os.environ['KRITACI_RELEASE_PACKAGE_NAMING'].lower() in ['true', '1', 't', 'y', 'yes'])
+
 print(f"Signing binaries in {pkg_root}")
 if not os.path.isdir(pkg_root):
     print(f"ERROR: No packaging dir {pkg_root}")
@@ -32,3 +36,11 @@ with open("files-to-sign.txt", 'w') as toSign:
 
 commandToRun = f"{sys.executable} -u ci-notary-service/signwindowsbinaries.py --config {KRITACI_WINDOWS_SIGN_CONFIG} --files-from files-to-sign.txt"
 subprocess.check_call(commandToRun, stdout=sys.stdout, stderr=sys.stderr, shell=True )
+
+if release_package_naming:
+    print(f"Verify that all executables have a signature in {pkg_root}")
+    commandToRun = f"{sys.executable} -u packaging/windows/find-libs-with-debug.py -s -d {pkg_root}"
+    subprocess.check_call(commandToRun, stdout=sys.stdout, stderr=sys.stderr, shell=True )
+
+
+

b9fe0ac4f9d1973d51674a7fe2504aaf7bbfd6cb
Luna Lovecraft <ciubix8514@gmail.com>
2026-09-16 20:14:52 +0400

    Fix tab close button styling
    Now it's actually centered, and has rounded corners to go along with the
    slightly rounded corners of the tab

--- libs/ui/KisMainWindow.cpp
+++ libs/ui/KisMainWindow.cpp
@@ -1012,7 +1012,8 @@ void KisMainWindow::customizeTabBar()
     QString tabStyleSheet = QStringLiteral(R"(
             QTabBar::close-button {
                 image: url(%1);
-                padding-top: 3px;
+                padding: 1.5px;
+                border-radius: 3px;
             }
             QTabBar::close-button:hover {
                 background-color: %2;
